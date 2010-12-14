d90abf8f6ec6f504ed8337328a53c3a11134a296
oheger <oliver.heger@oliver-heger.de>
2010-12-14 21:21:14 +0100

    Added a specialized SingleElementHandler implementation which navigates to a configurable page.

---
+++ server/test/de/oliver_heger/mediastore/client/pages/overview/TestOpenPageSingleElementHandler.java
@@ -0,0 +1,38 @@
+package de.oliver_heger.mediastore.client.pages.overview;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import de.oliver_heger.mediastore.client.pages.MockPageManager;
+import de.oliver_heger.mediastore.client.pages.Pages;
+
+/**
+ * Test class for {@code OpenPageSingleElementHandler}.
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class TestOpenPageSingleElementHandler extends GWTTestCase
+{
+    /** Constant for the ID of the element. */
+    private static final Long ELEM_ID = 20101213222945L;
+
+    @Override
+    public String getModuleName()
+    {
+        return "de.oliver_heger.mediastore.RemoteMediaStore";
+    }
+
+    /**
+     * Tests whether the handler opens the correct page.
+     */
+    public void testHandleElement()
+    {
+        MockPageManager pm = new MockPageManager();
+        pm.expectCreatePageSpecification(Pages.OVERVIEW, null)
+                .withParameter(ELEM_ID).open();
+        OpenPageSingleElementHandler handler =
+                new OpenPageSingleElementHandler(pm, Pages.OVERVIEW);
+        handler.handleElement(ELEM_ID);
+        pm.verify();
+    }
+}
