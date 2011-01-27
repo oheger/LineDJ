a2a9ca9a13f25533cefc5c2dc346dda2a00d888d
oheger <oliver.heger@oliver-heger.de>
2011-01-27 08:37:28 +0100

    Added AlbumDetailInfo class.

---
+++ server/src/de/oliver_heger/mediastore/shared/model/AlbumDetailInfo.java
@@ -0,0 +1,99 @@
+package de.oliver_heger.mediastore.shared.model;
+
+import java.util.List;
+import java.util.Set;
+
+import de.oliver_heger.mediastore.shared.ObjectUtils;
+
+/**
+ * <p>
+ * A specialized data object with detail information about an album.
+ * </p>
+ * <p>
+ * This class extends the plain {@link AlbumInfo} class by providing more
+ * detailed information about an album, e.g. synonyms or songs. This information
+ * is used to populate the details page for albums.
+ * </p>
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class AlbumDetailInfo extends AlbumInfo implements HasSynonyms
+{
+    /**
+     * The serial version UID.
+     */
+    private static final long serialVersionUID = 20110127L;
+
+    /** A set with the synonyms of this album. */
+    private Set<String> synonyms;
+
+    /** A list with the songs associated for this album. */
+    private List<SongInfo> songs;
+
+    /** A list with the artists that occur on this album. */
+    private List<ArtistInfo> artists;
+
+    /**
+     * Returns a set with the synonyms of this album.
+     *
+     * @return the synonyms of this album
+     */
+    public Set<String> getSynonyms()
+    {
+        return ObjectUtils.nonNullSet(synonyms);
+    }
+
+    /**
+     * Sets the synonyms of this album.
+     *
+     * @param synonyms the synonyms
+     */
+    public void setSynonyms(Set<String> synonyms)
+    {
+        this.synonyms = synonyms;
+    }
+
+    /**
+     * Returns a list with the songs of this album. Note that the list may be
+     * incomplete; only the songs stored in this database are taken into
+     * account.
+     *
+     * @return a list with the songs
+     */
+    public List<SongInfo> getSongs()
+    {
+        return ObjectUtils.nonNullList(songs);
+    }
+
+    /**
+     * Sets the list with the songs of this album.
+     *
+     * @param songs the list with songs
+     */
+    public void setSongs(List<SongInfo> songs)
+    {
+        this.songs = songs;
+    }
+
+    /**
+     * Returns a list with the artists related to this album. This list contains
+     * the artists associated with the songs on this album.
+     *
+     * @return the artists of this album
+     */
+    public List<ArtistInfo> getArtists()
+    {
+        return ObjectUtils.nonNullList(artists);
+    }
+
+    /**
+     * Sets the list with the artists of this album.
+     *
+     * @param artists the list with artists
+     */
+    public void setArtists(List<ArtistInfo> artists)
+    {
+        this.artists = artists;
+    }
+}
---
+++ server/test/de/oliver_heger/mediastore/shared/model/TestAlbumDetailInfo.java
@@ -0,0 +1,107 @@
+package de.oliver_heger.mediastore.shared.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.oliver_heger.mediastore.shared.RemoteMediaStoreTestHelper;
+
+/**
+ * Test class for {@code AlbumDetailInfo}.
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class TestAlbumDetailInfo
+{
+    /** The instance to be tested. */
+    private AlbumDetailInfo info;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        info = new AlbumDetailInfo();
+    }
+
+    /**
+     * Tests whether an empty set is returned if there are no synonyms.
+     */
+    @Test
+    public void testGetSynonymsEmpty()
+    {
+        assertTrue("Got synonyms", info.getSynonyms().isEmpty());
+    }
+
+    /**
+     * Tests whether an empty list is returned if there are no songs.
+     */
+    @Test
+    public void testGetSongsEmpty()
+    {
+        assertTrue("Got songs", info.getSongs().isEmpty());
+    }
+
+    /**
+     * Tests whether an empty list is returned if there are no artists.
+     */
+    @Test
+    public void testGetArtistsEmpty()
+    {
+        assertTrue("Got artists", info.getArtists().isEmpty());
+    }
+
+    /**
+     * Tests whether an instance can be serialized.
+     */
+    @Test
+    public void testSerialization() throws IOException
+    {
+        Set<String> synonyms = new HashSet<String>();
+        synonyms.add("syn1");
+        synonyms.add("syn2");
+        info.setSynonyms(synonyms);
+        List<SongInfo> songs = new ArrayList<SongInfo>();
+        SongInfo song = new SongInfo();
+        song.setName("LaLa1");
+        songs.add(song);
+        song = new SongInfo();
+        song.setName("LaLa2");
+        songs.add(song);
+        info.setSongs(songs);
+        List<ArtistInfo> artists = new ArrayList<ArtistInfo>();
+        ArtistInfo art = new ArtistInfo();
+        art.setName("Artist1");
+        artists.add(art);
+        art = new ArtistInfo();
+        art.setName("Another Artist");
+        artists.add(art);
+        info.setArtists(artists);
+        AlbumDetailInfo info2 = RemoteMediaStoreTestHelper.serialize(info);
+        assertEquals("Wrong synonyms", synonyms, info2.getSynonyms());
+        assertEquals("Wrong number of songs", songs.size(), info2.getSongs()
+                .size());
+        Iterator<SongInfo> songIt = info2.getSongs().iterator();
+        for (SongInfo si : songs)
+        {
+            assertEquals("Wrong song name", si.getName(), songIt.next()
+                    .getName());
+        }
+        assertEquals("Wrong number of artists", artists.size(), info2
+                .getArtists().size());
+        Iterator<ArtistInfo> artIt = info2.getArtists().iterator();
+        for (ArtistInfo ai : artists)
+        {
+            assertEquals("Wrong artist name", ai.getName(), artIt.next()
+                    .getName());
+        }
+    }
+}
