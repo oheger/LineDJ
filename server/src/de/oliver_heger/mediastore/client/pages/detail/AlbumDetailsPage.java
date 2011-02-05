e761e087a8b4ce47b997b3be883019fbea26e18e
oheger <oliver.heger@oliver-heger.de>
2011-02-05 16:29:29 +0100

    Initial implementation of AlbumDetailsPage.

---
+++ server/src/de/oliver_heger/mediastore/client/pages/detail/AlbumDetailsPage.java
@@ -0,0 +1,281 @@
+package de.oliver_heger.mediastore.client.pages.detail;
+
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.DisclosurePanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Hyperlink;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+import de.oliver_heger.mediastore.client.GridTableModel;
+import de.oliver_heger.mediastore.client.pageman.PageManager;
+import de.oliver_heger.mediastore.client.pages.Pages;
+import de.oliver_heger.mediastore.shared.model.AlbumDetailInfo;
+import de.oliver_heger.mediastore.shared.model.ArtistInfo;
+import de.oliver_heger.mediastore.shared.search.MediaSearchServiceAsync;
+
+/**
+ * <p>
+ * This class implements the details page for albums.
+ * </p>
+ * <p>
+ * This page expects the ID of the album to be displayed in its page
+ * configuration. It loads the details of this album and displays all data
+ * available.
+ * </p>
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class AlbumDetailsPage extends AbstractDetailsPage<AlbumDetailInfo>
+{
+    /** Our binder. */
+    private static AlbumDetailsPageUiBinder uiBinder = GWT
+            .create(AlbumDetailsPageUiBinder.class);
+
+    /** The name of the songs table. */
+    private static final String SONG_TABLE_NAME = "Songs";
+
+    /** The name of the artists table. */
+    private static final String ARTIST_TABLE_NAME = "Artists";
+
+    /** Constant for the opening bracket. */
+    private static final String BRACK_OPEN = " (";
+
+    /** Constant for the closing bracket. */
+    private static final String BRACK_CLOSE = ")";
+
+    /** Constant of the initial buffer size. */
+    private static final int BUF_SIZE = 16;
+
+    /** The span for the album name. */
+    @UiField
+    SpanElement spanName;
+
+    /** The hyper link to the album's main artist. */
+    @UiField
+    Hyperlink lnkArtist;
+
+    /** A label which is displayed if there are multiple artists. */
+    @UiField
+    Label labMultiArtists;
+
+    /** The span for the album's duration. */
+    @UiField
+    SpanElement spanDuration;
+
+    /** The span for the album's inception year. */
+    @UiField
+    SpanElement spanYear;
+
+    /** The span for the album's creation date. */
+    @UiField
+    SpanElement spanCreationDate;
+
+    /** The span for the album's synonyms. */
+    @UiField
+    SpanElement spanSynonyms;
+
+    /** The table for the songs of this album. */
+    @UiField
+    Grid tabSongs;
+
+    /** The table for the artists of this album. */
+    @UiField
+    Grid tabArtists;
+
+    /** The panel for displaying the associated songs. */
+    @UiField
+    DisclosurePanel pnlSongs;
+
+    /** The panel for displaying the associated artists. */
+    @UiField
+    DisclosurePanel pnlArtists;
+
+    /** The details handler used by this page. */
+    private final DetailsEntityHandler<AlbumDetailInfo> detailsHandler;
+
+    /** The table model for the songs of this album. */
+    private SongGridTableModel songModel;
+
+    /** The table model for the artists of this album. */
+    private ArtistGridTableModel artistModel;
+
+    /**
+     * Creates a new instance of {@code AlbumDetailsPage}.
+     */
+    public AlbumDetailsPage()
+    {
+        initWidget(uiBinder.createAndBindUi(this));
+        detailsHandler = new AlbumDetailsEntityHandler();
+    }
+
+    /**
+     * Initializes this page. This implementation also creates the table models.
+     *
+     * @param pm the page manager
+     */
+    @Override
+    public void initialize(PageManager pm)
+    {
+        super.initialize(pm);
+
+        songModel = new SongGridTableModel(tabSongs, pm);
+        artistModel = new ArtistGridTableModel(tabArtists, pm);
+    }
+
+    /**
+     * Returns the {@link DetailsEntityHandler} used by this page.
+     *
+     * @return the details handler
+     */
+    @Override
+    protected DetailsEntityHandler<AlbumDetailInfo> getDetailsEntityHandler()
+    {
+        return detailsHandler;
+    }
+
+    /**
+     * Fills the page with data. The handling of artists is a bit more complex:
+     * If there is no artist, the artist link is not displayed. If there is
+     * exactly one artist, the link points to this artist's details page. If
+     * there are multiple artists, the link is not active, but shows a message
+     * that there are multiple artists; the table with the artists is open.
+     *
+     * @param data the data to be displayed
+     */
+    @Override
+    protected void fillPage(AlbumDetailInfo data)
+    {
+        spanCreationDate.setInnerText(getFormatter().formatDate(
+                data.getCreationDate()));
+        spanDuration.setInnerText(data.getFormattedDuration());
+        spanName.setInnerText(data.getName());
+        spanYear.setInnerText((data.getInceptionYear() != null) ? String
+                .valueOf(data.getInceptionYear()) : null);
+        spanSynonyms.setInnerText(formatSynonyms(data.getSynonyms()));
+
+        initArtistLink(data.getArtists());
+
+        fillTabPanel(pnlSongs, getSongTableModel(), SONG_TABLE_NAME,
+                data.getSongs(), 1);
+        fillTabPanel(pnlArtists, getArtistTableModel(), ARTIST_TABLE_NAME,
+                data.getArtists(), 2);
+    }
+
+    /**
+     * Clears this page. All fields are set to empty values.
+     */
+    @Override
+    protected void clearPage()
+    {
+        fillPage(new AlbumDetailInfo());
+    }
+
+    /**
+     * Returns the handler for synonym queries. This implementation creates a
+     * new album synonym query handler.
+     *
+     * @return the handler for querying synonyms
+     */
+    @Override
+    protected SynonymQueryHandler getSynonymQueryHandler(
+            MediaSearchServiceAsync searchService)
+    {
+        return new AlbumSynonymQueryHandler(searchService);
+    }
+
+    /**
+     * Returns the table model for the artist table.
+     *
+     * @return the artists table model
+     */
+    ArtistGridTableModel getArtistTableModel()
+    {
+        return artistModel;
+    }
+
+    /**
+     * Returns the table model for the song table.
+     *
+     * @return the songs table model
+     */
+    SongGridTableModel getSongTableModel()
+    {
+        return songModel;
+    }
+
+    /**
+     * Initializes the link for the artist.
+     *
+     * @param artists the list with all artists
+     */
+    private void initArtistLink(List<ArtistInfo> artists)
+    {
+        if (artists.size() == 1)
+        {
+            lnkArtist.setVisible(true);
+            ArtistInfo art = artists.get(0);
+            lnkArtist.setText(art.getName());
+            lnkArtist.setTargetHistoryToken(getPageManager()
+                    .createPageSpecification(Pages.ARTISTDETAILS)
+                    .withParameter(art.getArtistID()).toToken());
+        }
+        else
+        {
+            lnkArtist.setVisible(false);
+        }
+
+        labMultiArtists.setVisible(artists.size() > 1);
+    }
+
+    /**
+     * Generates the header text for a table. This text consists of the table
+     * name and the number of rows.
+     *
+     * @param name the name of the table
+     * @param rows the number of rows
+     * @return the complete text
+     */
+    private static String generateTableHeader(String name, int rows)
+    {
+        StringBuilder buf = new StringBuilder(BUF_SIZE);
+        buf.append(name);
+        buf.append(BRACK_OPEN).append(rows).append(BRACK_CLOSE);
+        return buf.toString();
+    }
+
+    /**
+     * Fills a disclosure panel with a table with data. This method can handle
+     * both the artists and the songs table.
+     *
+     * @param <T> the type of the data
+     * @param pnl the disclosure panel
+     * @param model the table model
+     * @param tabName the name of the table
+     * @param data the list with the content of the table
+     * @param openThreshold the threshold when the panel should be open
+     */
+    private static <T> void fillTabPanel(DisclosurePanel pnl,
+            GridTableModel<T> model, String tabName, List<T> data,
+            int openThreshold)
+    {
+        pnl.getHeaderTextAccessor().setText(
+                generateTableHeader(tabName, data.size()));
+        model.initData(data);
+        pnl.setOpen(data.size() >= openThreshold);
+    }
+
+    /**
+     * The binder interface used for this component.
+     */
+    interface AlbumDetailsPageUiBinder extends
+            UiBinder<Widget, AlbumDetailsPage>
+    {
+    }
+}
---
+++ server/test/de/oliver_heger/mediastore/client/pages/detail/TestAlbumDetailsPage.java
@@ -0,0 +1,395 @@
+package de.oliver_heger.mediastore.client.pages.detail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.DisclosurePanel;
+
+import de.oliver_heger.mediastore.client.pages.MockPageManager;
+import de.oliver_heger.mediastore.client.pages.Pages;
+import de.oliver_heger.mediastore.shared.model.AlbumDetailInfo;
+import de.oliver_heger.mediastore.shared.model.ArtistInfo;
+import de.oliver_heger.mediastore.shared.model.SongInfo;
+import de.oliver_heger.mediastore.shared.search.MediaSearchService;
+import de.oliver_heger.mediastore.shared.search.MediaSearchServiceAsync;
+
+/**
+ * Test class for {@code AlbumDetailsPage}.
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class TestAlbumDetailsPage extends AbstractTestDetailsPage
+{
+    /**
+     * Creates an info object with some basic properties.
+     *
+     * @return the info object
+     */
+    private AlbumDetailInfo createBasicInfo()
+    {
+        AlbumDetailInfo info = new AlbumDetailInfo();
+        info.setCreationDate(new Date());
+        info.setDuration((60 * 60 + 11) * 1000L);
+        info.setInceptionYear(2010);
+        info.setName("A brand new test album");
+        return info;
+    }
+
+    /**
+     * Tests whether the member fields have been created correctly.
+     */
+    public void testInit()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        checkBaseFields(page);
+        assertNotNull("No name span", page.spanName);
+        assertNotNull("No artist link", page.lnkArtist);
+        assertNotNull("No duration span", page.spanDuration);
+        assertNotNull("No synonyms span", page.spanSynonyms);
+        assertNotNull("No panel for songs", page.pnlSongs);
+        assertNotNull("No panel for artists", page.pnlArtists);
+        assertNotNull("No table for songs", page.tabSongs);
+        assertNotNull("No table for artists", page.tabArtists);
+        assertNotNull("No label for multiple artists", page.labMultiArtists);
+    }
+
+    /**
+     * Tests whether the expected query handler is returned.
+     */
+    public void testGetDetailsQueryHandler()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        assertTrue(
+                "Wrong details entity handler",
+                page.getDetailsEntityHandler() instanceof AlbumDetailsEntityHandler);
+    }
+
+    /**
+     * Tests that only a single instance of the details query handler exists.
+     */
+    public void testGetDetailsQueryHandlerCached()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        DetailsEntityHandler<AlbumDetailInfo> handler =
+                page.getDetailsEntityHandler();
+        assertSame("Multiple details query handler", handler,
+                page.getDetailsEntityHandler());
+    }
+
+    /**
+     * Tests whether the synonym query handler can be retrieved.
+     */
+    public void testGetSynonymQueryHandler()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        MediaSearchServiceAsync searchService =
+                GWT.create(MediaSearchService.class);
+        AlbumSynonymQueryHandler handler =
+                (AlbumSynonymQueryHandler) page
+                        .getSynonymQueryHandler(searchService);
+        assertSame("Wrong search service", searchService,
+                handler.getSearchService());
+    }
+
+    /**
+     * Tests whether the grid with the songs and its model are correctly set up.
+     */
+    public void testInitSongGridModel()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        MockPageManager pm = initializePage(page);
+        SongGridTableModel model = page.getSongTableModel();
+        assertEquals("Wrong number of columns", 3, model.getColumnCount());
+        assertSame("Wrong page manager", pm, model.getPageManager());
+        assertEquals("Wrong property (1)", "name", model.getProperty(0));
+        assertEquals("Wrong property (2)", "duration", model.getProperty(1));
+        assertEquals("Wrong property (3)", "playCount", model.getProperty(2));
+        assertSame("Wrong grid", page.tabSongs, model.getGrid());
+    }
+
+    /**
+     * Tests whether the grid with the albums and its model are correctly set
+     * up.
+     */
+    public void testInitAlbumGridModel()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        MockPageManager pm = initializePage(page);
+        ArtistGridTableModel model = page.getArtistTableModel();
+        assertEquals("Wrong number of columns", 1, model.getColumnCount());
+        assertEquals("Wrong property (1)", "name", model.getProperty(0));
+        assertSame("Wrong page manager", pm, model.getPageManager());
+        assertSame("Wrong grid", page.tabArtists, model.getGrid());
+    }
+
+    /**
+     * Generates a header text.
+     *
+     * @param txt the text
+     * @param count the number
+     * @return the header text
+     */
+    private static String generateHeaderString(String txt, int count)
+    {
+        StringBuilder buf = new StringBuilder();
+        buf.append(txt);
+        buf.append(" (").append(count).append(")");
+        return buf.toString();
+    }
+
+    /**
+     * Helper method for testing the state of a disclosure panel.
+     *
+     * @param pnl the panel
+     * @param txt the expected header text
+     * @param count the number of elements
+     * @param open the open flag
+     */
+    private void checkDisclosurePanel(DisclosurePanel pnl, String txt,
+            int count, boolean open)
+    {
+        assertEquals("Wrong header text", generateHeaderString(txt, count), pnl
+                .getHeaderTextAccessor().getText());
+        assertEquals("Wrong open state", open, pnl.isOpen());
+    }
+
+    /**
+     * Helper method for checking the panel of the artists table.
+     *
+     * @param page the page
+     * @param count the expected number of artists
+     * @param open a flag whether the panel with artists should be open
+     */
+    private void checkArtistTable(AlbumDetailsPage page, int count, boolean open)
+    {
+        checkDisclosurePanel(page.pnlArtists, "Artists", count, open);
+    }
+
+    /**
+     * Helper method for checking the panel of the songs table.
+     *
+     * @param page the page
+     * @param count the expected number of songs
+     * @param open a flag whether the panel with songs should be open
+     */
+    private void checkSongTable(AlbumDetailsPage page, int count, boolean open)
+    {
+        checkDisclosurePanel(page.pnlSongs, "Songs", count, open);
+    }
+
+    /**
+     * Tests fillPage() if only basic attributes are available.
+     */
+    public void testFillPageSimple()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        initializePage(page);
+        AlbumDetailInfo info = createBasicInfo();
+        page.fillPage(info);
+        assertEquals("Wrong name", info.getName(), page.spanName.getInnerText());
+        assertEquals("Wrong duration", info.getFormattedDuration(),
+                page.spanDuration.getInnerText());
+        assertEquals("Wrong year", String.valueOf(info.getInceptionYear()),
+                page.spanYear.getInnerText());
+        assertTrue("No creation date", page.spanCreationDate.getInnerText()
+                .length() > 0);
+        assertFalse("Got an artist link", page.lnkArtist.isVisible());
+        assertFalse("Got label for multiple artists",
+                page.labMultiArtists.isVisible());
+        checkSongTable(page, 0, false);
+        checkArtistTable(page, 0, false);
+    }
+
+    /**
+     * Tests whether synonyms are correctly displayed.
+     */
+    public void testFillPageSynonyms()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        initializePage(page);
+        AlbumDetailInfo info = createBasicInfo();
+        Set<String> synonyms =
+                new HashSet<String>(Arrays.asList("AlbumSyn1", "Cool album",
+                        "Total cool album"));
+        info.setSynonyms(synonyms);
+        page.fillPage(info);
+        String synTxt = page.spanSynonyms.getInnerText();
+        for (String syn : synonyms)
+        {
+            assertTrue("Synonym not found: " + syn, synTxt.contains(syn));
+        }
+    }
+
+    /**
+     * Tests fillPage() if there is a single artist.
+     */
+    public void testFillPageSingleArtist()
+    {
+        AlbumDetailInfo info = createBasicInfo();
+        ArtistInfo art = new ArtistInfo();
+        art.setArtistID(20110202223020L);
+        art.setName("Harry Hirsch");
+        info.setArtists(Collections.singletonList(art));
+        AlbumDetailsPageTestImpl page = new AlbumDetailsPageTestImpl();
+        MockPageManager pm = initializePage(page);
+        pm.expectCreatePageSpecification(Pages.ARTISTDETAILS, null)
+                .withParameter(art.getArtistID()).toToken();
+        page.fillPage(info);
+        checkArtistTable(page, 1, false);
+        assertEquals("Wrong link text", art.getName(), page.lnkArtist.getText());
+        assertEquals("Wrong link target",
+                MockPageManager.defaultToken(Pages.ARTISTDETAILS),
+                page.lnkArtist.getTargetHistoryToken());
+        assertTrue("Link not visible", page.lnkArtist.isVisible());
+        assertFalse("Got label for multiple artists",
+                page.labMultiArtists.isVisible());
+        pm.verify();
+        assertSame("Wrong artist list", info.getArtists(), page.getArtists());
+    }
+
+    /**
+     * Tests fillPage() if there are multiple artists.
+     */
+    public void testFillPageMultipleArtists()
+    {
+        AlbumDetailInfo info = createBasicInfo();
+        final int artistCount = 8;
+        List<ArtistInfo> artists = new ArrayList<ArtistInfo>(artistCount);
+        for (int i = 0; i < artistCount; i++)
+        {
+            ArtistInfo ai = new ArtistInfo();
+            ai.setArtistID(Long.valueOf(i));
+            ai.setName("Artist" + i);
+            artists.add(ai);
+        }
+        info.setArtists(artists);
+        AlbumDetailsPageTestImpl page = new AlbumDetailsPageTestImpl();
+        initializePage(page);
+        page.fillPage(info);
+        checkArtistTable(page, artistCount, true);
+        assertFalse("Link is visible", page.lnkArtist.isVisible());
+        assertTrue("Label for multiple artists not visible",
+                page.labMultiArtists.isVisible());
+        assertNull("Got a target", page.lnkArtist.getTargetHistoryToken());
+        assertSame("Wrong artist list", info.getArtists(), page.getArtists());
+    }
+
+    /**
+     * Tests whether the songs are correctly displayed.
+     */
+    public void testFillPageSongs()
+    {
+        AlbumDetailInfo info = new AlbumDetailInfo();
+        final int songCount = 16;
+        List<SongInfo> songs = new ArrayList<SongInfo>(songCount);
+        for (int i = 0; i < songCount; i++)
+        {
+            SongInfo song = new SongInfo();
+            song.setSongID(String.valueOf(i));
+            song.setName("Song" + i);
+            songs.add(song);
+        }
+        info.setSongs(songs);
+        AlbumDetailsPageTestImpl page = new AlbumDetailsPageTestImpl();
+        initializePage(page);
+        page.fillPage(info);
+        checkSongTable(page, songCount, true);
+        assertSame("Wrong song list", songs, page.getSongs());
+    }
+
+    /**
+     * Tests whether the page can be cleared.
+     */
+    public void testClearPage()
+    {
+        AlbumDetailsPage page = new AlbumDetailsPage();
+        initializePage(page);
+        page.fillPage(createBasicInfo());
+        page.clearPage();
+        assertEmpty("Got a name", page.spanName.getInnerText());
+        assertEmpty("Got a creation date", page.spanCreationDate.getInnerText());
+        assertEmpty("Got a duration", page.spanDuration.getInnerText());
+        assertEmpty("Got a year", page.spanYear.getInnerText());
+        assertEmpty("Got synonyms", page.spanSynonyms.getInnerText());
+        checkArtistTable(page, 0, false);
+        checkSongTable(page, 0, false);
+    }
+
+    /**
+     * A test implementation of the details page class which supports mocking
+     * the artist and song tables.
+     */
+    private static class AlbumDetailsPageTestImpl extends AlbumDetailsPage
+    {
+        /** A list with the artists passed to the artists table. */
+        private List<ArtistInfo> artists;
+
+        /** A list with the songs passed to the songs table. */
+        private List<SongInfo> songs;
+
+        /**
+         * Returns the artists to be displayed by the artists table.
+         *
+         * @return the artists
+         */
+        public List<ArtistInfo> getArtists()
+        {
+            return artists;
+        }
+
+        /**
+         * Returns the songs to be displayed by the songs table.
+         *
+         * @return the songs
+         */
+        public List<SongInfo> getSongs()
+        {
+            return songs;
+        }
+
+        /**
+         * Returns a mock table model which stores the passed in artists in an
+         * internal member field.
+         *
+         * @return the mock table model
+         */
+        @Override
+        ArtistGridTableModel getArtistTableModel()
+        {
+            return new ArtistGridTableModel(tabArtists, new MockPageManager())
+            {
+                @Override
+                public void initData(List<ArtistInfo> data)
+                {
+                    artists = data;
+                }
+            };
+        }
+
+        /**
+         * Returns a mock table model which stores the passed in songs in an
+         * internal member field.
+         *
+         * @return the mock table model
+         */
+        @Override
+        SongGridTableModel getSongTableModel()
+        {
+            return new SongGridTableModel(tabSongs, new MockPageManager())
+            {
+                @Override
+                public void initData(List<SongInfo> data)
+                {
+                    songs = data;
+                }
+            };
+        }
+    }
+}
