53c0f986b99de18488b5191077e9f695d5517ae7
oheger <oliver.heger@oliver-heger.de>
2011-03-14 22:17:59 +0100

    Added special command class for updating song data.

---
+++ localstore/src/main/java/de/oliver_heger/mediastore/localstore/UpdateLocalStoreCommand.java
@@ -0,0 +1,184 @@
+package de.oliver_heger.mediastore.localstore;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import de.oliver_heger.mediastore.localstore.model.AlbumEntity;
+import de.oliver_heger.mediastore.localstore.model.ArtistEntity;
+import de.oliver_heger.mediastore.localstore.model.SongEntity;
+import de.oliver_heger.mediastore.service.SongData;
+
+/**
+ * <p>
+ * A specialized command implementation for updating the local data storage with
+ * information about a song.
+ * </p>
+ * <p>
+ * This command is used when information about a new song becomes available. The
+ * command checks whether this song (and also its artist and album if known) is
+ * already contained in the local database. If not, the missing information is
+ * added. Otherwise the play counter is increased.
+ * </p>
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+class UpdateLocalStoreCommand extends JPACommand
+{
+    /** Stores the song data object defining the song to be updated. */
+    private final SongData songData;
+
+    /**
+     * Creates a new instance of {@code UpdateLocalStoreCommand} and initializes
+     * it with the {@code EntityManagerFactory} and the object with the song
+     * information.
+     *
+     * @param emf the {@code EntityManagerFactory} (must not be <b>null</b>)
+     * @param data the data object for the song to be updated (must not be
+     *        <b>null</b>)
+     * @throws NullPointerException if a required parameter is missing
+     */
+    public UpdateLocalStoreCommand(EntityManagerFactory emf, SongData data)
+    {
+        super(emf, false);
+        if (data == null)
+        {
+            throw new NullPointerException("SongData must not be null!");
+        }
+        songData = data;
+    }
+
+    /**
+     * Implements the logic of this command. This implementation checks which of
+     * the entities specified by the current {@code SongData} object already
+     * exist in the local database. Entities that are not yet present are newly
+     * created.
+     *
+     * @param em the {@code EntityManager}
+     */
+    @Override
+    protected void executeJPAOperation(EntityManager em)
+    {
+        SongEntity song =
+                Finders.findSong(em, songData.getName(),
+                        toInteger(songData.getDuration()),
+                        songData.getArtistName());
+
+        if (song == null)
+        {
+            if (getLog().isInfoEnabled())
+            {
+                getLog().info(
+                        "Creating new SongEntity for song "
+                                + songData.getName());
+            }
+
+            song = createEntityFromSongData(em);
+            em.persist(song);
+        }
+
+        song.incrementPlayCount();
+    }
+
+    /**
+     * Creates a {@link SongEntity} object from the {@code SongData} object of
+     * this command.
+     *
+     * @param em the {@code EntityManager}
+     * @return the {@link SongEntity}
+     */
+    private SongEntity createEntityFromSongData(EntityManager em)
+    {
+        SongEntity song;
+        song = new SongEntity();
+        song.setDuration(toInteger(songData.getDuration()));
+        song.setInceptionYear(toInteger(songData.getInceptionYear()));
+        song.setName(songData.getName());
+        song.setTrackNo(toInteger(songData.getTrackNo()));
+
+        if (songData.getArtistName() != null)
+        {
+            ArtistEntity artist = fetchOrCreateArtist(em);
+            artist.addSong(song);
+        }
+
+        if (songData.getAlbumName() != null)
+        {
+            AlbumEntity album = fetchOrCreateAlbum(em);
+            album.addSong(song);
+        }
+        return song;
+    }
+
+    /**
+     * Handles the artist of the current song data object. If necessary, a new
+     * instance is created.
+     *
+     * @param em the {@code EntityManager}
+     * @return the corresponding artist entity
+     */
+    private ArtistEntity fetchOrCreateArtist(EntityManager em)
+    {
+        ArtistEntity artist = Finders.findArtist(em, songData.getArtistName());
+
+        if (artist == null)
+        {
+            if (getLog().isInfoEnabled())
+            {
+                getLog().info(
+                        "Creating new ArtistEntity for "
+                                + songData.getArtistName());
+            }
+
+            artist = new ArtistEntity();
+            artist.setName(songData.getArtistName());
+            em.persist(artist);
+        }
+
+        return artist;
+    }
+
+    /**
+     * Handles the album of the current song data object. If necessary, a new
+     * instance is created.
+     *
+     * @param em the {@code EntityManager}
+     * @return the corresponding album entity
+     */
+    private AlbumEntity fetchOrCreateAlbum(EntityManager em)
+    {
+        Integer year = toInteger(songData.getInceptionYear());
+        AlbumEntity album =
+                Finders.findAlbum(em, songData.getAlbumName(), year);
+
+        if (album == null)
+        {
+            if (getLog().isInfoEnabled())
+            {
+                getLog().info(
+                        "Creating new AlbumEntity for "
+                                + songData.getAlbumName());
+            }
+
+            album = new AlbumEntity();
+            album.setName(songData.getAlbumName());
+            album.setInceptionYear(year);
+            em.persist(album);
+        }
+
+        return album;
+    }
+
+    /**
+     * Helper method for transforming a number to an integer value. This is
+     * needed for dealing with the BigInteger properties of the song data
+     * object.
+     *
+     * @param num the number (may be <b>null</b>)
+     * @return the corresponding Integer object
+     */
+    private static Integer toInteger(Number num)
+    {
+        return (num != null) ? Integer.valueOf(num.intValue()) : null;
+    }
+}
---
+++ localstore/src/test/java/de/oliver_heger/mediastore/localstore/TestUpdateLocalStoreCommand.java
@@ -0,0 +1,338 @@
+package de.oliver_heger.mediastore.localstore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.oliver_heger.mediastore.localstore.model.AlbumEntity;
+import de.oliver_heger.mediastore.localstore.model.ArtistEntity;
+import de.oliver_heger.mediastore.localstore.model.SongEntity;
+import de.oliver_heger.mediastore.service.ObjectFactory;
+import de.oliver_heger.mediastore.service.SongData;
+
+/**
+ * Test class for {@code UpdateLocalStoreCommand}.
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class TestUpdateLocalStoreCommand
+{
+    /** Constant for the name of an artist. */
+    private static final String ARTIST_NAME = "Within Temptation";
+
+    /** Constant for the name of the test song. */
+    private static final String SONG_NAME = "Restless";
+
+    /** Constant for the duration of the test song in seconds. */
+    private static final int SONG_DURATION = 368;
+
+    /** Constant for the name of the test album. */
+    private static final String ALBUM_NAME = "Live Tilburg";
+
+    /** Constant for the name of the album's inception year. */
+    private static final int ALBUM_YEAR = 2002;
+
+    /** Constant for the track number. */
+    private static final int TRACK = 3;
+
+    /** Constant for the play count. */
+    private static final int PLAY_COUNT = 2;
+
+    /** The JPA test helper. */
+    private JPATestHelper helper;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        helper = new JPATestHelper();
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        helper.close();
+    }
+
+    /**
+     * Loads all entities of the given type.
+     *
+     * @param <T> the entity type
+     * @param entityCls the entity class
+     * @return a list with the found entities
+     */
+    private <T> List<T> load(Class<T> entityCls)
+    {
+        @SuppressWarnings("unchecked")
+        List<T> result =
+                (List<T>) helper
+                        .getEM()
+                        .createQuery(
+                                "select e from " + entityCls.getName() + " e")
+                        .getResultList();
+        return result;
+    }
+
+    /**
+     * Checks the number of entities for the given class.
+     *
+     * @param entityCls the entity class
+     * @param expCount the expected number of entities
+     */
+    private void checkCount(Class<?> entityCls, int expCount)
+    {
+        List<?> list = load(entityCls);
+        assertEquals("Wrong number of entities for " + entityCls, expCount,
+                list.size());
+    }
+
+    /**
+     * Checks whether the test song has been correctly added to the local
+     * database.
+     *
+     * @param expPlayCount the expected play count
+     * @return the entity for the test song
+     */
+    private SongEntity checkSyncedSong(int expPlayCount)
+    {
+        List<SongEntity> songs = load(SongEntity.class);
+        assertEquals("Wrong number of songs", 1, songs.size());
+        SongEntity song = songs.get(0);
+        assertEquals("Wrong song name", SONG_NAME, song.getName());
+        assertEquals("Wrong duration", SONG_DURATION, song.getDuration()
+                .intValue());
+        assertEquals("Wrong song year", ALBUM_YEAR, song.getInceptionYear()
+                .intValue());
+        assertEquals("Wrong track number", TRACK, song.getTrackNo().intValue());
+        assertEquals("Wrong play count", expPlayCount,
+                song.getCurrentPlayCount());
+        return song;
+    }
+
+    /**
+     * Checks whether the local database contains the expected entities.
+     *
+     * @param expPlayCount the expected play count
+     * @return the entity for the test song
+     */
+    private SongEntity checkLocalStore(int expPlayCount)
+    {
+        SongEntity song = checkSyncedSong(expPlayCount);
+        ArtistEntity artist = song.getArtist();
+        assertEquals("Wrong artist name", ARTIST_NAME, artist.getName());
+        assertEquals("Wrong number of artist songs", 1, artist.getSongs()
+                .size());
+        checkCount(ArtistEntity.class, 1);
+        AlbumEntity album = song.getAlbum();
+        assertEquals("Wrong album name", ALBUM_NAME, album.getName());
+        assertEquals("Wrong album year", ALBUM_YEAR, album.getInceptionYear()
+                .intValue());
+        assertEquals("Wrong number of album songs", 1, album.getSongs().size());
+        checkCount(AlbumEntity.class, 1);
+        return song;
+    }
+
+    /**
+     * Creates a song data object with default values.
+     *
+     * @return the data object
+     */
+    private SongData createSongData()
+    {
+        SongData data = new ObjectFactory().createSongData();
+        data.setName(SONG_NAME);
+        data.setDuration(BigInteger.valueOf(SONG_DURATION));
+        data.setInceptionYear(BigInteger.valueOf(ALBUM_YEAR));
+        data.setTrackNo(BigInteger.valueOf(TRACK));
+        data.setArtistName(ARTIST_NAME);
+        data.setAlbumName(ALBUM_NAME);
+        return data;
+    }
+
+    /**
+     * Tests whether the command is correctly initialized.
+     */
+    @Test
+    public void testInit()
+    {
+        UpdateLocalStoreCommand cmd =
+                new UpdateLocalStoreCommand(helper.getEMF(), createSongData());
+        assertSame("Wrong factory", helper.getEMF(),
+                cmd.getEntityManagerFactory());
+        assertFalse("UI update", cmd.isUpdateGUI());
+    }
+
+    /**
+     * Tries to create an instance without a song data object.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testInitNoSongData()
+    {
+        new UpdateLocalStoreCommand(helper.getEMF(), null);
+    }
+
+    /**
+     * Tests the command if a new song entity has to be added which has no
+     * references.
+     */
+    @Test
+    public void testUpdateNewSong() throws Exception
+    {
+        SongData data = createSongData();
+        data.setAlbumName(null);
+        data.setArtistName(null);
+        UpdateLocalStoreCommand cmd =
+                new UpdateLocalStoreCommand(helper.getEMF(), data);
+        cmd.execute();
+        checkSyncedSong(1);
+    }
+
+    /**
+     * Helper method for executing the test command and checking results.
+     *
+     * @param data the song data object
+     * @throws Exception if an error occurs
+     */
+    private void checkUpdateCommand(SongData data) throws Exception
+    {
+        UpdateLocalStoreCommand cmd =
+                new UpdateLocalStoreCommand(helper.getEMF(), data);
+        cmd.execute();
+        checkLocalStore(1);
+    }
+
+    /**
+     * Tests the command if all entities have to be created.
+     */
+    @Test
+    public void testUpdateNewEntities() throws Exception
+    {
+        checkUpdateCommand(createSongData());
+    }
+
+    /**
+     * Checks an update operation if the artist entity already exists.
+     *
+     * @param artName the name of the artist
+     * @throws Exception if an error occurs
+     */
+    private void checkUpdateExistingArtist(String artName) throws Exception
+    {
+        ArtistEntity art = new ArtistEntity();
+        art.setName(ARTIST_NAME);
+        helper.persist(art, true);
+        helper.closeEM();
+        SongData data = createSongData();
+        data.setArtistName(artName);
+        checkUpdateCommand(data);
+    }
+
+    /**
+     * Tests an update operation if the artist entity already exists with the
+     * name specified.
+     */
+    @Test
+    public void testUpdateExistingArtist() throws Exception
+    {
+        checkUpdateExistingArtist(ARTIST_NAME);
+    }
+
+    /**
+     * Tests whether case of the artist name is ignored when updating a song.
+     */
+    @Test
+    public void testUpdateExistingArtistCase() throws Exception
+    {
+        checkUpdateExistingArtist(ARTIST_NAME.toLowerCase(Locale.ENGLISH));
+    }
+
+    /**
+     * Checks an update operation of the album entity already exists.
+     *
+     * @param albName the name of the album
+     * @throws Exception if an error occurs
+     */
+    private void checkUpdateExistingAlbum(String albName) throws Exception
+    {
+        AlbumEntity alb = new AlbumEntity();
+        alb.setName(ALBUM_NAME);
+        alb.setInceptionYear(ALBUM_YEAR);
+        helper.persist(alb, true);
+        helper.closeEM();
+        SongData data = createSongData();
+        data.setAlbumName(albName);
+        checkUpdateCommand(data);
+    }
+
+    /**
+     * Tests an update operation if the album entity already exists with the
+     * name specified.
+     */
+    @Test
+    public void testUpdateExistingAlbum() throws Exception
+    {
+        checkUpdateExistingAlbum(ALBUM_NAME);
+    }
+
+    /**
+     * Tests whether the case of the album name is ignored when updating a song.
+     */
+    @Test
+    public void testUpdateExistingAlbumCase() throws Exception
+    {
+        checkUpdateExistingAlbum(ALBUM_NAME.toLowerCase(Locale.ENGLISH));
+    }
+
+    /**
+     * Checks an update operation if the song already exists.
+     *
+     * @param songName the name of the song
+     */
+    private void checkUpdateExistingSong(String songName) throws Exception
+    {
+        SongEntity song = new SongEntity();
+        song.setName(SONG_NAME);
+        song.setDuration(SONG_DURATION);
+        song.setCurrentPlayCount(PLAY_COUNT);
+        helper.persist(song, true);
+        SongData data = createSongData();
+        data.setArtistName(null);
+        data.setAlbumName(null);
+        data.setName(songName);
+        UpdateLocalStoreCommand cmd =
+                new UpdateLocalStoreCommand(helper.getEMF(), data);
+        cmd.execute();
+        helper.closeEM();
+        checkCount(SongEntity.class, 1);
+        song = helper.getEM().find(SongEntity.class, song.getId());
+        assertEquals("Wrong play count", PLAY_COUNT + 1,
+                song.getCurrentPlayCount());
+    }
+
+    /**
+     * Tests an update operation if the song already exists with the name
+     * specified.
+     */
+    @Test
+    public void testUpdateExistingSong() throws Exception
+    {
+        checkUpdateExistingSong(SONG_NAME);
+    }
+
+    /**
+     * Tests whether the case of the song name is ignored when updating a song.
+     */
+    @Test
+    public void testUpdateExistingSongCase() throws Exception
+    {
+        checkUpdateExistingSong(SONG_NAME.toLowerCase(Locale.ENGLISH));
+    }
+}
