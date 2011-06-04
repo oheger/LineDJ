8db2535844805b2473897210a1aa9883ce3624a4
oheger <oliver.heger@oliver-heger.de>
2011-06-04 17:20:48 +0200

    Initial implementation of SongDataManager.

--- jplaya-engine/src/main/java/de/olix/playa/engine/mediainfo/SongDataManager.java
+++ jplaya-engine/src/main/java/de/olix/playa/engine/mediainfo/SongDataManager.java
@@ -1,5 +1,17 @@
 package de.olix.playa.engine.mediainfo;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.oliver_heger.mediastore.service.SongData;
+import de.olix.playa.engine.AudioReadMonitor;
+
 /**
  * <p>
  * A class providing access to media information for the song files to be
@@ -32,5 +44,256 @@ package de.olix.playa.engine.mediainfo;
  */
 public class SongDataManager
 {
+    /** The logger. */
+    private final Log log = LogFactory.getLog(getClass());
 
+    /** The executor service. */
+    private final ExecutorService executor;
+
+    /** The loader for song data objects. */
+    private final SongDataLoader songDataLoader;
+
+    /** The audio read monitor. */
+    private final AudioReadMonitor monitor;
+
+    /** A map for accessing song data by file URI. */
+    private final Map<String, SongData> uriMapping;
+
+    /** A map for accessing song data by URI. */
+    private final Map<Object, SongData> idMapping;
+
+    /** A list for the event listeners registered at this object. */
+    private final List<SongDataListener> listeners;
+
+    /**
+     * Creates a new instance of {@code SongDataManager} and sets references to
+     * dependent objects.
+     *
+     * @param exec the executor service for background execution (must not be
+     *        <b>null</b>)
+     * @param loader the loader for extracting media data (must not be
+     *        <b>null</b>)
+     * @param mon the monitor for synchronizing with the audio buffer (must not
+     *        be <b>null</b>)
+     * @throws IllegalArgumentException if a required parameter is missing
+     */
+    public SongDataManager(ExecutorService exec, SongDataLoader loader,
+            AudioReadMonitor mon)
+    {
+        if (exec == null)
+        {
+            throw new IllegalArgumentException(
+                    "ExecutorService must not be null!");
+        }
+        if (loader == null)
+        {
+            throw new IllegalArgumentException(
+                    "SongDataLoader must not be null!");
+        }
+        if (mon == null)
+        {
+            throw new IllegalArgumentException("Monitor must not be null!");
+        }
+
+        executor = exec;
+        songDataLoader = loader;
+        monitor = mon;
+
+        uriMapping = new ConcurrentHashMap<String, SongData>();
+        idMapping = new ConcurrentHashMap<Object, SongData>();
+        listeners = new CopyOnWriteArrayList<SongDataListener>();
+    }
+
+    /**
+     * Adds the specified {@code SongDataListener} to this object. It will be
+     * notified when new media information about a song becomes available.
+     *
+     * @param l the listener (must not be <b>null</b>)
+     * @throws IllegalArgumentException if the listener is <b>null</b>)
+     */
+    public void addSongDataListener(SongDataListener l)
+    {
+        if (l == null)
+        {
+            throw new IllegalArgumentException("Listener must not be null!");
+        }
+        listeners.add(l);
+    }
+
+    /**
+     * Removes the specified {@code SongDataListener} from this object. If the
+     * listener is not registered, this method has no effect.
+     *
+     * @param l the listener to be removed
+     */
+    public void removeSongDataListener(SongDataListener l)
+    {
+        listeners.remove(l);
+    }
+
+    /**
+     * Schedules the specified audio file for extracting media information. This
+     * method creates a background task which tries to lookup media data for the
+     * audio file with the specified URI. If this is successful, the data is
+     * stored internally and can be accessed using the various {@code get}
+     * methods. Also, an event is fired notifying registered listeners about new
+     * media data being available. The ID is an arbitrary object associated with
+     * the audio file. If it is not <b>null</b>, it is associated with the media
+     * information and can be used for querying this data.
+     *
+     * @param uri the URI of the audio file in question (must not be
+     *        <b>null</b>)
+     * @param id the optional ID of this audio file
+     * @throws IllegalArgumentException if the URI is <b>null</b>
+     */
+    public void extractSongData(String uri, Object id)
+    {
+        if (uri == null)
+        {
+            throw new IllegalArgumentException(
+                    "URI for audio file must not be null!");
+        }
+        executor.execute(createExtractionTask(uri, id));
+    }
+
+    /**
+     * Returns the {@code SongData} object that was extracted for the specified
+     * audio file. If the data is not yet available or could not be obtained,
+     * result is <b>null</b>.
+     *
+     * @param uri the URI of the audio file in question
+     * @return the {@code SongData} object extracted for this file or
+     *         <b>null</b>
+     */
+    public SongData getDataForFile(String uri)
+    {
+        return (uri != null) ? uriMapping.get(uri) : null;
+    }
+
+    /**
+     * Returns the {@code SongData} object that was extracted for the audio file
+     * with the specified ID. If the data is not yet available or could not be
+     * obtained, result is <b>null</b>.
+     *
+     * @param id the ID of the file in question as passed to the
+     *        {@code extractSongData()} method
+     * @return the {@code SongData} object extracted for this file or
+     *         <b>null</b>
+     */
+    public SongData getDataForID(Object id)
+    {
+        return (id != null) ? idMapping.get(id) : null;
+    }
+
+    /**
+     * Shuts down this manager. This means that no more {@code SongData} objects
+     * can be loaded for audio files.
+     */
+    public void shutdown()
+    {
+        log.info("Shutdown of SongDataManager.");
+        executor.shutdown();
+    }
+
+    /**
+     * Creates a task for extracting media information for a file in a
+     * background thread. This method is called for each audio file passed to
+     * this object. The executor service passed to the constructor is used to
+     * execute this task.
+     *
+     * @param uri the URI of the audio file
+     * @param id the ID of the audio file
+     * @return the task for retrieving media information for this file
+     */
+    Runnable createExtractionTask(String uri, Object id)
+    {
+        return new ExtractionTask(uri, id);
+    }
+
+    /**
+     * Notifies all registered event listeners that a {@code SongData} object
+     * for an audio file has become available. This fill create an event object
+     * which is then passed to all event listeners.
+     *
+     * @param uri the URI of the audio file in question
+     * @param id the ID of the audio file
+     */
+    void fireSongDataEvent(String uri, Object id)
+    {
+        if (log.isInfoEnabled())
+        {
+            log.info("Retrieved song data for " + uri);
+        }
+        SongDataEvent event = null;
+
+        for (SongDataListener l : listeners)
+        {
+            if (event == null)
+            {
+                event = new SongDataEvent(this, uri, id); // create lazy
+            }
+            l.songDataLoaded(event);
+        }
+    }
+
+    /**
+     * A specialized task implementation which calls the song data loader to
+     * extract media information for a specific audio file.
+     */
+    private class ExtractionTask implements Runnable
+    {
+        /** The URI of the file to be extracted. */
+        private final String uri;
+
+        /** The ID of the file to be extracted. */
+        private final Object id;
+
+        /**
+         * Creates a new instance of {@code ExtractionTask} and initializes it
+         * with the parameters of the audio file to be processed.
+         *
+         * @param fileURI the URI of the audio file
+         * @param fileID the optional ID of the audio file
+         */
+        public ExtractionTask(String fileURI, Object fileID)
+        {
+            uri = fileURI;
+            id = fileID;
+        }
+
+        /**
+         * Executes this task. Tries to extract media information. If this is
+         * successful, the data is stored in the internal maps of this instance.
+         */
+        @Override
+        public void run()
+        {
+            try
+            {
+                monitor.waitForMediumIdle();
+                SongData data = songDataLoader.extractSongData(uri);
+                if (data == null)
+                {
+                    if (log.isInfoEnabled())
+                    {
+                        log.info("No song data available for file " + uri);
+                    }
+                }
+
+                else
+                {
+                    uriMapping.put(uri, data);
+                    if (id != null)
+                    {
+                        idMapping.put(id, data);
+                    }
+                    fireSongDataEvent(uri, id);
+                }
+            }
+            catch (InterruptedException iex)
+            {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
---
+++ jplaya-engine/src/test/java/de/olix/playa/engine/mediainfo/TestSongDataManager.java
@@ -0,0 +1,365 @@
+package de.olix.playa.engine.mediainfo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.oliver_heger.mediastore.service.SongData;
+import de.olix.playa.engine.AudioReadMonitor;
+
+/**
+ * Test class for {@code SongDataManager}.
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class TestSongDataManager
+{
+    /** Constant for the URI of an audio file. */
+    private static final String URI = "file://mylala.mp3";
+
+    /** Constant for the ID of the test audio file. */
+    private static final Object ID = 20110603211008L;
+
+    /** A mock executor service. */
+    private ExecutorService exec;
+
+    /** A mock loader. */
+    private SongDataLoader loader;
+
+    /** A mock monitor. */
+    private AudioReadMonitor monitor;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        exec = EasyMock.createMock(ExecutorService.class);
+        loader = EasyMock.createMock(SongDataLoader.class);
+        monitor = EasyMock.createMock(AudioReadMonitor.class);
+    }
+
+    /**
+     * Creates a default test instance.
+     *
+     * @return the test instance
+     */
+    private SongDataManagerTestImpl createManager()
+    {
+        return new SongDataManagerTestImpl(exec, loader, monitor);
+    }
+
+    /**
+     * Tries to create an instance without an executor service.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitNoExecutor()
+    {
+        new SongDataManager(null, loader, monitor);
+    }
+
+    /**
+     * Tries to create an instance without a loader.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitNoLoader()
+    {
+        new SongDataManager(exec, null, monitor);
+    }
+
+    /**
+     * Tries to create an instance without a monitor.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitNoMonitor()
+    {
+        new SongDataManager(exec, loader, null);
+    }
+
+    /**
+     * Tries to register a null listener.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddSongDataListenerNull()
+    {
+        SongDataManagerTestImpl manager = createManager();
+        manager.addSongDataListener(null);
+    }
+
+    /**
+     * Tests whether an event listener can be registered.
+     */
+    @Test
+    public void testAddSongDataListener()
+    {
+        SongDataManagerTestImpl manager = createManager();
+        SongDataListenerTestImpl l = new SongDataListenerTestImpl();
+        manager.addSongDataListener(l);
+        manager.fireSongDataEvent(URI, ID);
+        SongDataEvent event = l.nextEvent();
+        assertEquals("Wrong URI", URI, event.getMediaFileURI());
+        assertEquals("Wrong ID", ID, event.getMediaFileID());
+        assertSame("Wrong manager", manager, event.getManager());
+        l.verify();
+    }
+
+    /**
+     * Tests whether a song data listener can be removed.
+     */
+    @Test
+    public void testRemoveSongDataListener()
+    {
+        SongDataManagerTestImpl manager = createManager();
+        SongDataListenerTestImpl l = new SongDataListenerTestImpl();
+        manager.addSongDataListener(l);
+        manager.fireSongDataEvent(URI, ID);
+        manager.removeSongDataListener(l);
+        final String uri2 = URI + "_other";
+        manager.fireSongDataEvent(uri2, null);
+        SongDataEvent event = l.nextEvent();
+        assertEquals("Wrong URI", URI, event.getMediaFileURI());
+        l.verify();
+    }
+
+    /**
+     * Helper method for checking a successful execution of the extraction task.
+     *
+     * @param id the ID to be passed to the task
+     * @return the manager
+     * @throws InterruptedException should not happen
+     */
+    private SongDataManager checkExtractionTask(Object id)
+            throws InterruptedException
+    {
+        SongData data = EasyMock.createMock(SongData.class);
+        monitor.waitForMediumIdle();
+        EasyMock.expect(loader.extractSongData(URI)).andReturn(data);
+        EasyMock.replay(exec, loader, monitor, data);
+        SongDataListenerTestImpl l = new SongDataListenerTestImpl();
+        SongDataManagerTestImpl manager = createManager();
+        manager.addSongDataListener(l);
+        Runnable task = manager.createExtractionTask(URI, id);
+        task.run();
+        SongDataEvent event = l.nextEvent();
+        assertEquals("Wrong URI", URI, event.getMediaFileURI());
+        assertEquals("Wrong ID", id, event.getMediaFileID());
+        l.verify();
+        assertSame("Wrong song data for URI", data, manager.getDataForFile(URI));
+        EasyMock.verify(exec, loader, monitor, data);
+        return manager;
+    }
+
+    /**
+     * Tests whether the extraction task works as expected if a song Id is
+     * passed.
+     */
+    @Test
+    public void testExtractionTaskWithID() throws InterruptedException
+    {
+        SongDataManager manager = checkExtractionTask(ID);
+        assertNotNull("No song data for ID", manager.getDataForID(ID));
+    }
+
+    /**
+     * Tests the extraction task if no song ID is provided.
+     */
+    @Test
+    public void testExtractionTaskNoID() throws InterruptedException
+    {
+        SongDataManager manager = checkExtractionTask(null);
+        assertNull("Got data for null ID", manager.getDataForID(null));
+    }
+
+    /**
+     * Tests the extraction task if waiting at the monitor is interrupted.
+     */
+    @Test
+    public void testExtractionTaskInterrupted() throws InterruptedException
+    {
+        monitor.waitForMediumIdle();
+        EasyMock.expectLastCall().andThrow(new InterruptedException());
+        EasyMock.replay(exec, loader, monitor);
+        SongDataListenerTestImpl l = new SongDataListenerTestImpl();
+        SongDataManagerTestImpl manager = createManager();
+        manager.addSongDataListener(l);
+        Runnable task = manager.createExtractionTask(URI, ID);
+        task.run();
+        l.verify();
+        assertNull("Got data", manager.getDataForFile(URI));
+        assertTrue("Not interrupted", Thread.interrupted());
+        EasyMock.verify(exec, loader, monitor);
+    }
+
+    /**
+     * Tests the extraction task if no extraction data can be obtained.
+     */
+    @Test
+    public void testExtractionTaskNoData() throws InterruptedException
+    {
+        monitor.waitForMediumIdle();
+        EasyMock.expect(loader.extractSongData(URI)).andReturn(null);
+        EasyMock.replay(exec, loader, monitor);
+        SongDataListenerTestImpl l = new SongDataListenerTestImpl();
+        SongDataManagerTestImpl manager = createManager();
+        manager.addSongDataListener(l);
+        Runnable task = manager.createExtractionTask(URI, ID);
+        task.run();
+        l.verify();
+        assertNull("Got data", manager.getDataForFile(URI));
+        EasyMock.verify(exec, loader, monitor);
+    }
+
+    /**
+     * Tries to obtain song data for a null URI.
+     */
+    @Test
+    public void testGetDataForFileNull()
+    {
+        SongDataManagerTestImpl manager = createManager();
+        assertNull("Got data", manager.getDataForFile(null));
+    }
+
+    /**
+     * Tries to obtain song data for a null URI.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testExtractSongDataNoURI()
+    {
+        SongDataManagerTestImpl manager = createManager();
+        manager.extractSongData(null, ID);
+    }
+
+    /**
+     * Tests whether song data is obtained in a background thread.
+     */
+    @Test
+    public void testExtractSongData()
+    {
+        SongDataManagerTestImpl manager = createManager();
+        Runnable task = manager.installMockTask();
+        exec.execute(task);
+        EasyMock.replay(exec, loader, monitor, task);
+        manager.extractSongData(URI, ID);
+        EasyMock.verify(exec, loader, monitor, task);
+    }
+
+    /**
+     * Tests whether the manager can be shut down.
+     */
+    @Test
+    public void testShutdown()
+    {
+        exec.shutdown();
+        EasyMock.replay(exec, loader, monitor);
+        SongDataManagerTestImpl manager = createManager();
+        manager.shutdown();
+        EasyMock.verify(exec, loader, monitor);
+    }
+
+    /**
+     * Tests that it is not possible to request new data after a shutdown.
+     */
+    @Test(expected = RejectedExecutionException.class)
+    public void testExtractAfterShutdown()
+    {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        SongDataManagerTestImpl manager =
+                new SongDataManagerTestImpl(service, loader, monitor);
+        manager.shutdown();
+        manager.extractSongData(URI, ID);
+    }
+
+    /**
+     * A test implementation with some mocking facilities.
+     */
+    private static class SongDataManagerTestImpl extends SongDataManager
+    {
+        /** A mock task. */
+        private Runnable mockTask;
+
+        public SongDataManagerTestImpl(ExecutorService exec,
+                SongDataLoader loader, AudioReadMonitor mon)
+        {
+            super(exec, loader, mon);
+        }
+
+        /**
+         * Creates and installs a mock task to be returned by
+         * {@code createExtractionTask()}.
+         *
+         * @return the mock task
+         */
+        public Runnable installMockTask()
+        {
+            mockTask = EasyMock.createMock(Runnable.class);
+            return mockTask;
+        }
+
+        /**
+         * Either calls the super method or checks parameters and returns the
+         * mock task.
+         */
+        @Override
+        Runnable createExtractionTask(String uri, Object id)
+        {
+            if (mockTask != null)
+            {
+                assertEquals("Wrong URI", URI, uri);
+                assertEquals("Wrong ID", ID, id);
+                return mockTask;
+            }
+            return super.createExtractionTask(uri, id);
+        }
+    }
+
+    /**
+     * A test event listener implementation.
+     */
+    private static class SongDataListenerTestImpl implements SongDataListener
+    {
+        /** Stores the events received by the listener. */
+        private final List<SongDataEvent> events =
+                new LinkedList<SongDataEvent>();
+
+        /**
+         * Returns the next event received by this listener.
+         *
+         * @return the next event
+         */
+        public SongDataEvent nextEvent()
+        {
+            assertFalse("Too few events", events.isEmpty());
+            return events.remove(0);
+        }
+
+        /**
+         * Verifies that all received events have been processed.
+         */
+        public void verify()
+        {
+            assertTrue("Too many events", events.isEmpty());
+        }
+
+        /**
+         * Stores the received event.
+         */
+        @Override
+        public void songDataLoaded(SongDataEvent event)
+        {
+            assertNotNull("No event", event);
+            events.add(event);
+        }
+    }
+}
