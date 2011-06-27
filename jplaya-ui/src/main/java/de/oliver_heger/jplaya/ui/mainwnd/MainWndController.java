f319e8869e6e6c669f223223db3c7782a85c9631
oheger <oliver.heger@oliver-heger.de>
2011-06-27 22:23:55 +0200

    Initial (incomplete) implementation of MainWndController.

---
+++ jplaya-ui/src/main/java/de/oliver_heger/jplaya/ui/mainwnd/MainWndController.java
@@ -0,0 +1,442 @@
+package de.oliver_heger.jplaya.ui.mainwnd;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import net.sf.jguiraffe.di.BeanContext;
+import net.sf.jguiraffe.gui.app.Application;
+import net.sf.jguiraffe.gui.builder.action.ActionStore;
+import net.sf.jguiraffe.gui.builder.action.FormAction;
+import net.sf.jguiraffe.gui.builder.window.WindowEvent;
+import net.sf.jguiraffe.gui.builder.window.WindowListener;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.oliver_heger.jplaya.ui.ConfigurationConstants;
+import de.oliver_heger.mediastore.localstore.MediaStore;
+import de.olix.playa.engine.AudioPlayer;
+import de.olix.playa.engine.AudioPlayerEvent;
+import de.olix.playa.engine.AudioPlayerListener;
+import de.olix.playa.engine.AudioReader;
+import de.olix.playa.engine.DataBuffer;
+import de.olix.playa.engine.mediainfo.SongDataEvent;
+import de.olix.playa.engine.mediainfo.SongDataListener;
+import de.olix.playa.playlist.CurrentPositionInfo;
+import de.olix.playa.playlist.FSScanner;
+import de.olix.playa.playlist.PlaylistController;
+import de.olix.playa.playlist.PlaylistOrder;
+
+/**
+ * <p>
+ * The controller of the JPlaya application's main window.
+ * </p>
+ * <p>
+ * This class implements the major part of the functionality for initializing
+ * the audio engine and playing songs. It is created by the Jelly script for the
+ * main window.
+ * </p>
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class MainWndController implements AudioPlayerListener,
+        SongDataListener, WindowListener
+{
+    /** Constant for the action group for media player actions. */
+    static final String ACTGRP_PLAYER = "playerActions";
+
+    /** Constant for the action for initializing the playlist. */
+    static final String ACTION_INIT_PLAYLIST = "playerInitPlaylistAction";
+
+    /** The logger. */
+    private final Log log = LogFactory.getLog(getClass());
+
+    /** The bean context. */
+    private final BeanContext beanContext;
+
+    /** The media store. */
+    private final MediaStore mediaStore;
+
+    /** The action store. */
+    private ActionStore actionStore;
+
+    /** The file system scanner. */
+    private FSScanner scanner;
+
+    /** The playlist controller. */
+    private PlaylistController playlistController;
+
+    /** The current audio player. */
+    private AudioPlayer audioPlayer;
+
+    /**
+     * The object for synchronizing song data objects. Note: Although this
+     * object is accessed from other threads, there is no need to synchronize it
+     * or make it volatile because it is created before these other threads are
+     * started. After that it is not changed any more.
+     */
+    private SongDataSynchronizer songDataSynchronizer;
+
+    /**
+     * Creates a new instance of {@code MainWndController} and initializes it.
+     *
+     * @param ctx the current bean context providing access to all beans defined
+     *        by the current builder script (must not be <b>null</b>)
+     * @param store the {@code MediaStore} (must not be <b>null</b>)
+     * @throws NullPointerException if a required parameter is missing
+     */
+    public MainWndController(BeanContext ctx, MediaStore store)
+    {
+        if (ctx == null)
+        {
+            throw new NullPointerException("Bean context must not be null!");
+        }
+        if (store == null)
+        {
+            throw new NullPointerException("Media store must not be null!");
+        }
+
+        beanContext = ctx;
+        mediaStore = store;
+    }
+
+    /**
+     * Returns the action store.
+     *
+     * @return the action store
+     */
+    public ActionStore getActionStore()
+    {
+        return actionStore;
+    }
+
+    /**
+     * Sets the action store.
+     *
+     * @param actionStore the action store
+     */
+    public void setActionStore(ActionStore actionStore)
+    {
+        this.actionStore = actionStore;
+    }
+
+    /**
+     * Returns the bean context.
+     *
+     * @return the bean context
+     */
+    public BeanContext getBeanContext()
+    {
+        return beanContext;
+    }
+
+    /**
+     * Returns the media store.
+     *
+     * @return the media store
+     */
+    public MediaStore getMediaStore()
+    {
+        return mediaStore;
+    }
+
+    /**
+     * Returns the file system scanner.
+     *
+     * @return the file system scanner
+     */
+    public FSScanner getScanner()
+    {
+        return scanner;
+    }
+
+    /**
+     * Sets the file system scanner.
+     *
+     * @param scanner the file system scanner
+     */
+    public void setScanner(FSScanner scanner)
+    {
+        this.scanner = scanner;
+    }
+
+    /**
+     * Returns the playlist controller used by this object.
+     *
+     * @return the playlist controller
+     */
+    public PlaylistController getPlaylistController()
+    {
+        return playlistController;
+    }
+
+    /**
+     * Sets the playlist controller. This object is typically injected when the
+     * application is initialized.
+     *
+     * @param playlistController the playlist controller
+     */
+    public void setPlaylistController(PlaylistController playlistController)
+    {
+        this.playlistController = playlistController;
+    }
+
+    @Override
+    public void songDataLoaded(SongDataEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void error(AudioPlayerEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void fatalError(AudioPlayerEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void playListEnds(AudioPlayerEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void positionChanged(AudioPlayerEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void streamEnds(AudioPlayerEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public void streamStarts(AudioPlayerEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    /**
+     * Dummy implementation of this interface method.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowActivated(WindowEvent event)
+    {
+    }
+
+    /**
+     * Dummy implementation of this interface method.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowClosing(WindowEvent event)
+    {
+    }
+
+    /**
+     * Dummy implementation of this interface method.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowClosed(WindowEvent event)
+    {
+    }
+
+    /**
+     * Dummy implementation of this interface method.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowDeactivated(WindowEvent event)
+    {
+    }
+
+    /**
+     * Dummy implementation of this interface method.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowDeiconified(WindowEvent event)
+    {
+    }
+
+    /**
+     * Dummy implementation of this interface method.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowIconified(WindowEvent event)
+    {
+    }
+
+    /**
+     * The application window has been opened. This implementation tests whether
+     * sufficient configuration data is available to construct a playlist. If
+     * so, it calls {@link #initAudioEngine()}.
+     *
+     * @param event the event
+     */
+    @Override
+    public void windowOpened(WindowEvent event)
+    {
+        Application app = getApplication();
+        Configuration config = app.getUserConfiguration();
+
+        String mediaDir =
+                config.getString(ConfigurationConstants.PROP_MEDIA_DIR);
+        if (mediaDir != null)
+        {
+            getScanner().setRootURI(mediaDir);
+            FormAction action =
+                    getActionStore().getAction(ACTION_INIT_PLAYLIST);
+            action.execute(event);
+        }
+        else
+        {
+            getActionStore().enableGroup(ACTGRP_PLAYER, false);
+        }
+    }
+
+    /**
+     * Returns the current {@code AudioPlayer} object. This object was
+     * initialized by {@code initAudioEngine()}.
+     *
+     * @return the current {@code AudioPlayer}
+     */
+    protected AudioPlayer getAudioPlayer()
+    {
+        return audioPlayer;
+    }
+
+    /**
+     * Initializes the audio engine for playing the songs of a playlist. This
+     * method is called if a new playlist is setup. It is also called after
+     * starting the application, provided that a directory with music files has
+     * been configured.
+     *
+     * @throws IOException if an error occurs when initializing the playlist
+     */
+    protected void initAudioEngine() throws IOException
+    {
+        getPlaylistController()
+                .initializePlaylist(
+                        getDefaultPlaylistOrder(getApplication()
+                                .getUserConfiguration()));
+        CurrentPositionInfo positionInfo =
+                getPlaylistController().getPlaylistManager()
+                        .getInitialPositionInfo();
+
+        songDataSynchronizer = new SongDataSynchronizer();
+        audioPlayer = getBeanContext().getBean(AudioPlayer.class);
+        audioPlayer.setSkipPosition(positionInfo.getPosition());
+        audioPlayer.setSkipTime(positionInfo.getTime());
+        audioPlayer.addAudioPlayerListener(getPlaylistController());
+        audioPlayer.addAudioPlayerListener(this);
+
+        AudioReader reader =
+                createAudioReader((DataBuffer) audioPlayer.getAudioSource());
+        reader.start();
+        audioPlayer.start();
+    }
+
+    /**
+     * Creates an {@code AudioReader} object. This method is called when the
+     * audio engine is setup.
+     *
+     * @param buffer the current {@code DataBuffer}
+     * @return the {@code AudioReader}
+     */
+    protected AudioReader createAudioReader(DataBuffer buffer)
+    {
+        return new AudioReader(buffer, getPlaylistController());
+    }
+
+    /**
+     * Starts the audio player. This method is called in reaction of the start
+     * action.
+     */
+    protected void startPlayback()
+    {
+        // TODO implementation
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    /**
+     * Obtains the default order for a playlist from the given configuration
+     * object. If the configuration property for the order is set, it is
+     * evaluated. Otherwise a "default" default order is returned.
+     *
+     * @param config the configuration object
+     * @return the default playlist order
+     */
+    protected PlaylistOrder getDefaultPlaylistOrder(Configuration config)
+    {
+        PlaylistOrder defOrder = PlaylistOrder.DIRECTORIES;
+        String value =
+                config.getString(ConfigurationConstants.PROP_DEF_PLAYLIST_ORDER);
+
+        if (value != null)
+        {
+            try
+            {
+                defOrder =
+                        PlaylistOrder
+                                .valueOf(value.toUpperCase(Locale.ENGLISH));
+            }
+            catch (IllegalArgumentException iex)
+            {
+                log.warn("Invalid default playlist order in configuration: "
+                        + value);
+            }
+        }
+
+        return defOrder;
+    }
+
+    /**
+     * Returns the current {@code SongDataSynchronizer}. This object is created
+     * when the audio engine is initialized.
+     *
+     * @return the {@code SongDataSynchronizer}
+     */
+    SongDataSynchronizer getSongDataSynchronizer()
+    {
+        return songDataSynchronizer;
+    }
+
+    /**
+     * Convenience method for obtaining the current {@code Application} object.
+     *
+     * @return the application
+     */
+    private Application getApplication()
+    {
+        Application app =
+                (Application) getBeanContext().getBean(
+                        Application.BEAN_APPLICATION);
+        return app;
+    }
+}
---
+++ jplaya-ui/src/test/java/de/oliver_heger/jplaya/ui/mainwnd/TestMainWndController.java
@@ -0,0 +1,353 @@
+package de.oliver_heger.jplaya.ui.mainwnd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import net.sf.jguiraffe.di.BeanContext;
+import net.sf.jguiraffe.gui.app.Application;
+import net.sf.jguiraffe.gui.builder.action.ActionStore;
+import net.sf.jguiraffe.gui.builder.action.FormAction;
+import net.sf.jguiraffe.gui.builder.window.WindowEvent;
+
+import org.apache.commons.configuration.Configuration;
+import org.easymock.EasyMock;
+import org.easymock.EasyMockSupport;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.oliver_heger.jplaya.ui.ConfigurationConstants;
+import de.oliver_heger.mediastore.localstore.MediaStore;
+import de.olix.playa.engine.AudioPlayer;
+import de.olix.playa.engine.AudioReader;
+import de.olix.playa.engine.AudioStreamSource;
+import de.olix.playa.engine.DataBuffer;
+import de.olix.playa.playlist.CurrentPositionInfo;
+import de.olix.playa.playlist.FSScanner;
+import de.olix.playa.playlist.PlaylistController;
+import de.olix.playa.playlist.PlaylistManager;
+import de.olix.playa.playlist.PlaylistOrder;
+
+/**
+ * Test class for {@code MainWndController}.
+ *
+ * @author Oliver Heger
+ * @version $Id: $
+ */
+public class TestMainWndController extends EasyMockSupport
+{
+    /** A mock for the bean context. */
+    private BeanContext mockContext;
+
+    /** A mock for the media store. */
+    private MediaStore store;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        mockContext = createMock(BeanContext.class);
+        store = createMock(MediaStore.class);
+    }
+
+    /**
+     * Tries to create an instance without a bean context.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testInitNoContext()
+    {
+        new MainWndController(null, store);
+    }
+
+    /**
+     * Tries to create an instance without a media store.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testInitNoStore()
+    {
+        new MainWndController(mockContext, null);
+    }
+
+    /**
+     * Tests the window activated event. We can only check that the event is not
+     * touched.
+     */
+    @Test
+    public void testWindowActivated()
+    {
+        WindowEvent event = createMock(WindowEvent.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.windowActivated(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests the window closing event. We can only check that the event is not
+     * touched.
+     */
+    @Test
+    public void testWindowClosing()
+    {
+        WindowEvent event = createMock(WindowEvent.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.windowClosing(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests the window closed event. We can only check that the event is not
+     * touched.
+     */
+    @Test
+    public void testWindowClosed()
+    {
+        WindowEvent event = createMock(WindowEvent.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.windowClosed(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests the window deactivated event. We can only check that the event is
+     * not touched.
+     */
+    @Test
+    public void testWindowDeactivated()
+    {
+        WindowEvent event = createMock(WindowEvent.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.windowDeactivated(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests the window deiconified event. We can only check that the event is
+     * not touched.
+     */
+    @Test
+    public void testWindowDeiconified()
+    {
+        WindowEvent event = createMock(WindowEvent.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.windowDeiconified(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests the window iconified event. We can only check that the event is not
+     * touched.
+     */
+    @Test
+    public void testWindowIconified()
+    {
+        WindowEvent event = createMock(WindowEvent.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.windowIconified(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests reaction on the window opened event if there is no configuration
+     * data for setting up a playlist.
+     */
+    @Test
+    public void testWindowOpenedNoMediaDir()
+    {
+        Application app = createMock(Application.class);
+        Configuration config = createMock(Configuration.class);
+        ActionStore actStore = createMock(ActionStore.class);
+        WindowEvent event = createMock(WindowEvent.class);
+        EasyMock.expect(mockContext.getBean(Application.BEAN_APPLICATION))
+                .andReturn(app);
+        EasyMock.expect(app.getUserConfiguration()).andReturn(config);
+        EasyMock.expect(config.getString(ConfigurationConstants.PROP_MEDIA_DIR))
+                .andReturn(null);
+        actStore.enableGroup(MainWndController.ACTGRP_PLAYER, false);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store)
+        {
+            @Override
+            protected void initAudioEngine()
+            {
+                throw new UnsupportedOperationException(
+                        "Unexpected method call!");
+            }
+        };
+        ctrl.setActionStore(actStore);
+        ctrl.windowOpened(event);
+        verifyAll();
+    }
+
+    /**
+     * Tests reaction on the window opened event if the directory for media
+     * files has been configured. In this case the audio player should be
+     * started immediately.
+     */
+    @Test
+    public void testWindowOpenedMediaDir()
+    {
+        Application app = createMock(Application.class);
+        Configuration config = createMock(Configuration.class);
+        FSScanner scanner = createMock(FSScanner.class);
+        ActionStore actStore = createMock(ActionStore.class);
+        FormAction action = createMock(FormAction.class);
+        WindowEvent event = createMock(WindowEvent.class);
+        final String mediaDir = "R:\\";
+        EasyMock.expect(mockContext.getBean(Application.BEAN_APPLICATION))
+                .andReturn(app);
+        EasyMock.expect(app.getUserConfiguration()).andReturn(config);
+        EasyMock.expect(config.getString(ConfigurationConstants.PROP_MEDIA_DIR))
+                .andReturn(mediaDir);
+        scanner.setRootURI(mediaDir);
+        EasyMock.expect(
+                actStore.getAction(MainWndController.ACTION_INIT_PLAYLIST))
+                .andReturn(action);
+        action.execute(event);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.setScanner(scanner);
+        ctrl.setActionStore(actStore);
+        ctrl.windowOpened(event);
+        verifyAll();
+    }
+
+    /**
+     * Helper method for checking whether the default order can be obtained from
+     * a configuration object.
+     *
+     * @param propValue the value of the configuration property
+     * @param expOrder the expected default order
+     */
+    private void checkGetDefaultPlaylistOrder(String propValue,
+            PlaylistOrder expOrder)
+    {
+        Configuration config = createMock(Configuration.class);
+        EasyMock.expect(
+                config.getString(ConfigurationConstants.PROP_DEF_PLAYLIST_ORDER))
+                .andReturn(propValue);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        assertEquals("Wrong default order", expOrder,
+                ctrl.getDefaultPlaylistOrder(config));
+        verifyAll();
+    }
+
+    /**
+     * Tests getDefaultPlaylistOrder() if no order is defined in the
+     * configuration.
+     */
+    @Test
+    public void testGetDefaultPlaylistOrderUndefined()
+    {
+        checkGetDefaultPlaylistOrder(null, PlaylistOrder.DIRECTORIES);
+    }
+
+    /**
+     * Tests getDefaultPlaylistOrder() if the configuration has an invalid
+     * value.
+     */
+    @Test
+    public void testGetDefaultPlaylistOrderInvalidConfigValue()
+    {
+        checkGetDefaultPlaylistOrder("not a valid order!",
+                PlaylistOrder.DIRECTORIES);
+    }
+
+    /**
+     * Tests getDefaultPlaylistOrder() if the configuration property is set to a
+     * valid value.
+     */
+    @Test
+    public void testGetDefaultPlaylistOrderDefined()
+    {
+        checkGetDefaultPlaylistOrder(
+                PlaylistOrder.RANDOM.name().toLowerCase(Locale.ENGLISH),
+                PlaylistOrder.RANDOM);
+    }
+
+    /**
+     * Tests whether an audio reader is correctly created.
+     */
+    @Test
+    public void testCreateAudioReader()
+    {
+        PlaylistController plc = createMock(PlaylistController.class);
+        DataBuffer buffer = createMock(DataBuffer.class);
+        replayAll();
+        MainWndController ctrl = new MainWndController(mockContext, store);
+        ctrl.setPlaylistController(plc);
+        AudioReader reader = ctrl.createAudioReader(buffer);
+        assertSame("Wrong buffer", buffer, reader.getAudioBuffer());
+        assertSame("Wrong source", plc, reader.getStreamSource());
+        verifyAll();
+    }
+
+    /**
+     * Tests a successful initialization of the audio engine.
+     */
+    @Test
+    public void testInitAudioEngineSuccess() throws IOException
+    {
+        PlaylistController plc = createMock(PlaylistController.class);
+        AudioPlayer player = createMock(AudioPlayer.class);
+        Application app = createMock(Application.class);
+        Configuration config = createMock(Configuration.class);
+        PlaylistManager pm = createMock(PlaylistManager.class);
+        final DataBufferStreamSource bufferSource =
+                createMock(DataBufferStreamSource.class);
+        final AudioReader reader = createMock(AudioReader.class);
+        EasyMock.expect(mockContext.getBean(Application.BEAN_APPLICATION))
+                .andReturn(app);
+        EasyMock.expect(app.getUserConfiguration()).andReturn(config);
+        EasyMock.expect(
+                config.getString(ConfigurationConstants.PROP_DEF_PLAYLIST_ORDER))
+                .andReturn(PlaylistOrder.RANDOM.name());
+        plc.initializePlaylist(PlaylistOrder.RANDOM);
+        EasyMock.expect(plc.getPlaylistManager()).andReturn(pm);
+        EasyMock.expect(mockContext.getBean(AudioPlayer.class)).andReturn(
+                player);
+        EasyMock.expect(player.getAudioSource()).andReturn(bufferSource);
+        final CurrentPositionInfo posInfo =
+                new CurrentPositionInfo(20110627215305L, 201106272125621L);
+        EasyMock.expect(pm.getInitialPositionInfo()).andReturn(posInfo);
+        player.setSkipPosition(posInfo.getPosition());
+        player.setSkipTime(posInfo.getTime());
+        MainWndController ctrl = new MainWndController(mockContext, store)
+        {
+            @Override
+            protected AudioReader createAudioReader(DataBuffer buffer)
+            {
+                assertSame("Wrong buffer", bufferSource, buffer);
+                return reader;
+            }
+        };
+        EasyMock.expect(reader.start()).andReturn(null);
+        player.addAudioPlayerListener(plc);
+        player.addAudioPlayerListener(ctrl);
+        player.start();
+        replayAll();
+        ctrl.setPlaylistController(plc);
+        ctrl.initAudioEngine();
+        assertSame("Wrong audio player", player, ctrl.getAudioPlayer());
+        assertNotNull("No synchronizer", ctrl.getSongDataSynchronizer());
+        verifyAll();
+    }
+
+    /**
+     * An interface which combines the data buffer with the audio stream source
+     * interface. This is needed because the source of the player is also a data
+     * buffer.
+     */
+    private static interface DataBufferStreamSource extends DataBuffer,
+            AudioStreamSource
+    {
+    }
+}
