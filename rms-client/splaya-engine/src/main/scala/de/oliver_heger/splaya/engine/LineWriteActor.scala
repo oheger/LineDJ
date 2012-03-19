e1e50a553284aa5f9b11caf2adc8ffaee943990e
oheger <oliver.heger@oliver-heger.de>
2012-03-19 21:49:19 +0100

    Tests for LineWriteActor and initial implementation.

--- rms-client/splaya-engine/src/main/scala/de/oliver_heger/splaya/engine/LineWriteActor.scala
+++ rms-client/splaya-engine/src/main/scala/de/oliver_heger/splaya/engine/LineWriteActor.scala
@@ -6,33 +6,68 @@ import javax.sound.sampled.LineListener
 import javax.sound.sampled.LineEvent
 
 /**
- * <p>An actor that feeds a line for audio playback.</p>
- * <p>This actor receives messages for playing a chunk of audio data. It writes
+ * An actor that feeds a line for audio playback.
+ *
+ * This actor receives messages for playing a chunk of audio data. It writes
  * this chunk into the current line. When the chunk has been played completely
  * it sends back a message to the playback actor. This is the signal to load
- * the next chunk of data.</p>
+ * the next chunk of data.
  */
 class LineWriteActor extends Actor {
+  /**
+   * The main message loop of this actor. It processes messages for playing a
+   * chunk of audio data. If an ''Exit'' message is received, the main loop
+   * ends.
+   */
   def act() {
     var running = true
 
-    while(running) {
+    while (running) {
       receive {
-        case Exit => running = false
+        case ex: Exit =>
+          ex.confirmed(this)
+          running = false
 
-        case PlayChunk(line, buf, len, pos, skip) =>
-          playAudioChunk(line, buf, len)
+        case pc: PlayChunk =>
+          playAudioChunk(pc)
       }
     }
   }
 
   /**
-   * Plays a chunk of audio data using the given line.
-   * @param line the line
-   * @param buf the data to be played
+   * Returns a string representation for this actor. This string just contains
+   * the name of this actor.
+   * @return a string for this object
    */
-  private def playAudioChunk(line:SourceDataLine, buf:Array[Byte], len: Int) {
-    line.write(buf, 0, len)
-    Gateway ! Gateway.ActorPlayback -> ChunkPlayed
+  override def toString = "LineWriteActor"
+
+  /**
+   * Plays a chunk of audio data using the given line.
+   * @param pc the object with the data about the chunk to be played
+   */
+  private def playAudioChunk(pc: PlayChunk) {
+    val (ofs, len) = handleSkip(pc)
+    val played = ofs +
+      (if (len <= 0) pc.len
+      else pc.line.write(pc.chunk, ofs, len))
+    Gateway ! Gateway.ActorPlayback -> ChunkPlayed(played)
+  }
+
+  /**
+   * Determines the start index and the length to be played from the current
+   * data buffer based on the skip position. If parts of the buffer - or even
+   * the whole buffer - are to be skipped, index and length are adapted
+   * correspondingly.
+   * @param pc the data object about the chunk to be played
+   * @return a tuple with the adapted offset and length of the data buffer
+   */
+  private def handleSkip(pc: PlayChunk): Tuple2[Int, Int] = {
+    if (pc.skipPos > pc.currentPos + pc.len) {
+      (0, 0)
+    } else {
+      val ofs = scala.math.max(pc.skipPos - pc.currentPos, 0).toInt
+      val len = scala.math.max(pc.len - ofs, 0)
+      (ofs, len)
+    }
   }
 }
---
+++ rms-client/splaya-engine/src/test/scala/de/oliver_heger/splaya/engine/TestLineWriteActor.scala
@@ -0,0 +1,168 @@
+package de.oliver_heger.splaya.engine
+
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+import org.junit.Before
+import org.junit.After
+import org.junit.Test
+import javax.sound.sampled.SourceDataLine
+import org.junit.Ignore
+import org.easymock.EasyMock
+
+/**
+ * Test class for ''LineWriteActor''.
+ */
+class TestLineWriteActor extends JUnitSuite with EasyMockSugar {
+  /** Constant for the buffer length. */
+  private val BufferLength = 4096
+
+  /** A mock for the data line. */
+  private var line: SourceDataLine = _
+
+  /** The actor to be tested. */
+  private var actor: LineWriteActor = _
+
+  @Before def setUp() {
+    Gateway.start()
+    line = mock[SourceDataLine]
+    actor = new LineWriteActor
+    actor.start()
+  }
+
+  @After def tearDown() {
+    if (actor != null) {
+      actor ! Exit
+    }
+  }
+
+  /**
+   * Causes the test actor to exit and waits until its shutdown is complete.
+   */
+  private def shutdownActor() {
+    val exitCmd = new WaitForExit
+    if (!exitCmd.shutdownActor(actor, 500000)) {
+      fail("Actor did not exit!")
+    }
+    actor = null
+  }
+
+  /**
+   * Creates a play chunk message with the specified parameters. Other
+   * properties of the message are set to defaults.
+   * @param pos the current position in the stream
+   * @param skip the skip position
+   * @param len the length of the chunk
+   * @return a corresponding ''PlayChunk'' message
+   */
+  private def chunkData(pos: Long, skip: Long, len: Int = BufferLength) =
+    PlayChunk(line, new Array[Byte](BufferLength), len, pos, skip)
+
+  /**
+   * Creates a test actor which mocks the playback actor. It is started and
+   * installed on the gateway.
+   * @return the mock playback actor
+   */
+  private def installPlaybackActor(): QueuingActor = {
+    val playbackActor = new QueuingActor
+    playbackActor.start()
+    Gateway += Gateway.ActorPlayback -> playbackActor
+    playbackActor
+  }
+
+  /**
+   * Helper method for executing a test which sends a ''PlayChunk'' message to
+   * the test actor. The mock for the line must have been prepared
+   * correspondingly.
+   * @param data the ''PlayChunk'' message
+   */
+  private def executePlayChunkTest(data: PlayChunk) {
+    whenExecuting(data.line) {
+      actor ! data
+      shutdownActor()
+    }
+  }
+
+  /**
+   * Helper method for executing a test which first installs a mock playback
+   * actor and then sends a ''PlayChunk'' message to the test actor. The mock
+   * for the line must have been prepared correspondingly.
+   * @param data the ''PlayChunk'' message
+   * @return the mock for the playback actor
+   */
+  private def executePlayChunkTestWithPlaybackActor(data: PlayChunk): QueuingActor = {
+    val playbackActor = installPlaybackActor()
+    executePlayChunkTest(data)
+    playbackActor
+  }
+
+  /**
+   * Tests whether a complete chunk is played if no skip data is set.
+   */
+  @Test def testPlayChunkCompletelyNoSkip() {
+    val data = chunkData(0, 0)
+    expecting {
+      EasyMock.expect(data.line.write(data.chunk, 0, BufferLength))
+        .andReturn(BufferLength)
+    }
+    val playbackActor = executePlayChunkTestWithPlaybackActor(data)
+    playbackActor.expectMessage(ChunkPlayed(BufferLength))
+    playbackActor.ensureNoMessages()
+    playbackActor.shutdown()
+  }
+
+  /**
+   * Tests whether a partly played chunk is handled correctly.
+   */
+  @Test def testPlayChunkPartly() {
+    val data = chunkData(0, 0)
+    val len = BufferLength / 3
+    expecting {
+      EasyMock.expect(data.line.write(data.chunk, 0, BufferLength))
+        .andReturn(len)
+    }
+    val playbackActor = executePlayChunkTestWithPlaybackActor(data)
+    playbackActor.expectMessage(ChunkPlayed(len))
+    playbackActor.shutdown()
+  }
+
+  /**
+   * Tests a play chunk operation if the chunk has to be skipped completely.
+   */
+  @Test def testPlayChunkSkipTotal() {
+    val data = chunkData(100, Long.MaxValue)
+    val playbackActor = executePlayChunkTestWithPlaybackActor(data)
+    playbackActor.expectMessage(ChunkPlayed(BufferLength))
+    playbackActor.shutdown()
+  }
+
+  /**
+   * Tests a play chunk operation if the first byte of the buffer has to be
+   * skipped.
+   */
+  @Test def testPlayChunkSkipBeginning() {
+    val len = BufferLength - 100
+    val data = chunkData(10, 11, len)
+    expecting {
+      EasyMock.expect(data.line.write(data.chunk, 1, len - 1)).andReturn(len - 1)
+    }
+    val playbackActor = executePlayChunkTestWithPlaybackActor(data)
+    playbackActor.expectMessage(ChunkPlayed(len))
+    playbackActor.shutdown()
+  }
+
+  /**
+   * Tests a play chunk operation if just the last byte from the buffer can be
+   * played due to the skip position.
+   */
+  @Test def testPlayChunkSkipEnd() {
+    val pos = 1000
+    val skip = pos + BufferLength - 1
+    val data = chunkData(pos, skip)
+    expecting {
+      EasyMock.expect(data.line.write(data.chunk, BufferLength-1, 1)).andReturn(1)
+    }
+    val playbackActor = executePlayChunkTestWithPlaybackActor(data)
+    playbackActor.expectMessage(ChunkPlayed(BufferLength))
+    playbackActor.shutdown()
+  }
+}
