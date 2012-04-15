6c2670f7bea74cc53a3bbcad2a7c27a81773803b
oheger <oliver.heger@oliver-heger.de>
2012-04-15 17:02:28 +0200

    Added AudioSourceDataExtractorActor.

---
+++ rms-client/splaya-engine/src/main/scala/de/oliver_heger/splaya/playlist/impl/AudioSourceDataExtractorActor.scala
@@ -0,0 +1,79 @@
+package de.oliver_heger.splaya.playlist.impl
+
+import scala.actors.Actor
+import de.oliver_heger.splaya.playlist.AudioSourceDataExtractor
+import de.oliver_heger.splaya.AudioSourceData
+import de.oliver_heger.splaya.engine.msg.Exit
+
+/**
+ * An actor implementation which extracts meta data from audio sources.
+ *
+ * This actor reacts on messages specifying a single audio source. It then uses
+ * a [[de.oliver_heger.splaya.playlist.AudioSourceDataExtractor]] implementation
+ * to obtain meta data about this audio source. The information extracted is
+ * then sent back to the sender.
+ *
+ * @param extractor the ''AudioSourceDataExtractor'' to be used
+ */
+class AudioSourceDataExtractorActor(extractor: AudioSourceDataExtractor)
+  extends Actor {
+  /**
+   * The main method of this actor. The actor mainly reacts on messages
+   * requesting meta data for audio sources. Such requests are delegated to the
+   * ''AudioSourceDataExtractor'' used by this object. Results are sent back to
+   * the requesting actor.
+   */
+  def act() {
+    var running = true
+
+    while (running) {
+      receive {
+        case ex: Exit =>
+          ex.confirmed(this)
+          running = false
+
+        case req: ExtractSourceDataRequest =>
+          handleRequest(req)
+      }
+    }
+  }
+
+  /**
+   * Returns a string representation for this actor. This string contains the
+   * name of this actor.
+   * @return a string for this actor
+   */
+  override def toString = "AudioSourceDataExtractorActor"
+
+  /**
+   * Obtains meta data for an audio source based on the given request.
+   * @param req the request for audio source data
+   */
+  private def handleRequest(req: ExtractSourceDataRequest) {
+    val data = extractor.extractAudioSourceData(req.uri)
+    req.sender ! ExtractSourceDataResult(req.index, data)
+  }
+}
+
+/**
+ * A message which can be sent to the ''AudioSourceDataExtractorActor'' to
+ * request audio data for a specific audio source. The audio source in question
+ * is identified by its URI. The result of the extraction is sent to the
+ * specified actor in form of an ''ExtractSourceDataResult'' message.
+ * @param uri the URI of the audio source to be processed
+ * @param index the index of the audio source affected by this operation in the
+ * playlist
+ * @param sender the actor to which to sent the result
+ */
+case class ExtractSourceDataRequest(uri: String, index: Int, sender: Actor)
+
+/**
+ * A message providing the result of a request for extracting audio source data.
+ * A message of this type is sent by the
+ * [[de.oliver_heger.splaya.playlist.impl.AudioSourceDataExtractorActor]] as
+ * answer for an ''ExtractSourceDataRequest'' message. This answer is sent in
+ * any case, even if data extraction failed.
+ * @param index the index of the affected audio source in the playlist
+ * @param data an ''Option'' with the meta data extracted for the source
+ */
+case class ExtractSourceDataResult(index: Int, data: Option[AudioSourceData])
---
+++ rms-client/splaya-engine/src/test/scala/de/oliver_heger/splaya/playlist/impl/TestAudioSourceDataExtractorActor.scala
@@ -0,0 +1,66 @@
+package de.oliver_heger.splaya.playlist.impl
+
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.mock.EasyMockSugar
+import de.oliver_heger.splaya.playlist.AudioSourceDataExtractor
+import de.oliver_heger.splaya.AudioSourceData
+import org.junit.Before
+import org.junit.After
+import de.oliver_heger.splaya.engine.msg.Exit
+import de.oliver_heger.splaya.tsthlp.WaitForExit
+import org.junit.Test
+import org.easymock.EasyMock
+import de.oliver_heger.splaya.tsthlp.ActorTestImpl
+
+/**
+ * Test class for ''AudioSourceDataExtractorActor''.
+ */
+class TestAudioSourceDataExtractorActor extends JUnitSuite with EasyMockSugar {
+  /** Constant for an URI.*/
+  private val URI = "audio://TestMusic.la"
+
+  /** A mock for the extractor. */
+  private var extractor: AudioSourceDataExtractor = _
+
+  /** The actor to be tested. */
+  private var actor: AudioSourceDataExtractorActor = _
+
+  @Before def setUp() {
+    extractor = mock[AudioSourceDataExtractor]
+    actor = new AudioSourceDataExtractorActor(extractor)
+    actor.start()
+  }
+
+  @After def tearDown() {
+    if(actor != null) {
+      actor ! Exit
+    }
+  }
+
+  /**
+   * Sends the test actor an Exit message and waits until its completion.
+   */
+  private def shutdownActor() {
+    val msg = new WaitForExit
+    if(!msg.shutdownActor(actor)) {
+      fail("Actor did not exit!")
+    }
+    actor = null
+  }
+
+  /**
+   * Tests whether a request for extracting audio data is processed correctly.
+   */
+  @Test def testExtractData() {
+    val data = mock[AudioSourceData]
+    val index = 42
+    EasyMock.expect(extractor.extractAudioSourceData(URI)).andReturn(Some(data))
+    val rec = new ActorTestImpl
+    val msg = ExtractSourceDataRequest(URI, index, rec)
+    whenExecuting(data, extractor) {
+      actor ! msg
+      shutdownActor()
+    }
+    rec.expectMessage(ExtractSourceDataResult(index, Some(data)))
+  }
+}
