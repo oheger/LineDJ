e53568c5fbd34625fcfd1c73b482678e5611cb2f
oheger <oliver.heger@oliver-heger.de>
2012-06-26 17:48:53 +0200

    Use internal classes to circumvent problems with JavaSound SPI.

--- rms-client/splaya-engine/src/main/scala/de/oliver_heger/splaya/engine/PlaybackContextFactoryImpl.scala
+++ rms-client/splaya-engine/src/main/scala/de/oliver_heger/splaya/engine/PlaybackContextFactoryImpl.scala
@@ -1,11 +1,18 @@
 package de.oliver_heger.splaya.engine
 
+
 import java.io.InputStream
+
+import com.sun.media.sound.DirectAudioDeviceProvider
+
+import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
-import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.DataLine
+import javax.sound.sampled.Mixer
 import javax.sound.sampled.SourceDataLine
+import javazoom.spi.mpeg.sampled.convert.MpegFormatConversionProvider
+import javazoom.spi.mpeg.sampled.file.MpegAudioFileReader
 
 /**
  * A default implementation of the ''PlaybackContextFactory'' interface.
@@ -15,15 +22,17 @@ class PlaybackContextFactoryImpl extends PlaybackContextFactory {
   private val BufferSize = 4096
 
   def createPlaybackContext(stream: InputStream): PlaybackContext = {
-    val sourceStream = AudioSystem.getAudioInputStream(stream)
+    val reader = new MpegAudioFileReader
+    val sourceStream = reader.getAudioInputStream(stream)
     val baseFormat = sourceStream.getFormat
     val decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
       baseFormat.getSampleRate(), 16,
       baseFormat.getChannels(), baseFormat.getChannels() * 2,
       baseFormat.getSampleRate(), false);
-    val decodedStream = AudioSystem.getAudioInputStream(decodedFormat, sourceStream)
-    val lineInfo = new DataLine.Info(classOf[SourceDataLine], decodedFormat);
-    val line = AudioSystem.getLine(lineInfo).asInstanceOf[SourceDataLine];
+    val provider = new MpegFormatConversionProvider
+    val decodedStream = provider.getAudioInputStream(decodedFormat, sourceStream)
+    val lineInfo = new DataLine.Info(classOf[SourceDataLine], decodedFormat)
+    val line = createLine(lineInfo)
 
     PlaybackContext(line, decodedStream, calculateStreamSize(decodedStream),
       calculateBufferSize(decodedFormat))
@@ -32,8 +41,7 @@ class PlaybackContextFactoryImpl extends PlaybackContextFactory {
   /**
    * Determines the size of the specified audio stream. Calculates the size
    * based on the frame length and size if possible. If the size cannot be
-   * determined, <code>AudioPlayerEvent.UNKNOWN_STREAM_LENGTH</code> will be
-   * returned.
+   * determined, a negative value is returned.
    *
    * @param stream the affected stream
    * @return the size of this stream
@@ -44,7 +52,7 @@ class PlaybackContextFactoryImpl extends PlaybackContextFactory {
     if (stream.getFrameLength() > 0) {
       val fmt = stream.getFormat();
       if (fmt.getFrameSize != AudioSystem.NOT_SPECIFIED) {
-        result = fmt.getFrameSize() * stream.getFrameLength();
+        result = fmt.getFrameSize() * stream.getFrameLength()
       }
     }
 
@@ -61,5 +69,31 @@ class PlaybackContextFactoryImpl extends PlaybackContextFactory {
     if (BufferSize % format.getFrameSize != 0)
       ((BufferSize / format.getFrameSize) + 1) * format.getFrameSize
     else BufferSize
+  }
+
+  /**
+   * Creates a line object based on the given format info.
+   * @param info the info object for the line to be created
+   * @return the newly created line
+   */
+  private def createLine(info: DataLine.Info): SourceDataLine = {
+    //TODO extract into its own service so that access to this internal class
+    // is wrapped
+    val provider = new DirectAudioDeviceProvider
+    val minfos = provider.getMixerInfo
+    var mixer: Option[Mixer] = None
+    minfos find { minfo =>
+      val mx = provider.getMixer(minfo)
+      if(mx isLineSupported info) {
+        mixer = Some(mx)
+        true
+      } else {
+        false
+      }
+    }
+    if(mixer.isEmpty) {
+      throw new IllegalArgumentException("Cannot obtain line for " + info)
+    }
+    mixer.get.getLine(info).asInstanceOf[SourceDataLine]
   }
 }
