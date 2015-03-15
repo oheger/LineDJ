041d932c31fe3ef7917f36e42405dbfd3863abb1
oheger <oliver.heger@oliver-heger.de>
2015-03-15 20:04:21 +0100

    Added DirectoryScannerActor.
    This actor implementation is a thin wrapper around a DirectoryScanner. The idea
    is to scan multiple directory structures with media files in parallel.

---
+++ splaya-akka/src/test/scala/de/oliver_heger/splaya/media/DirectoryScannerActorSpec.scala
@@ -0,0 +1,45 @@
+package de.oliver_heger.splaya.media
+
+import java.nio.file.Paths
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import de.oliver_heger.splaya.media.DirectoryScannerActor.ScanPath
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+
+object DirectoryScannerActorSpec {
+  /** A test path. */
+  val Path = Paths.get("TestPath")
+
+  /** A test result object returned by the mock scanner. */
+  val TestResult = MediaScanResult(Map(Path -> List(Path)), List(Path))
+}
+
+/**
+ * Test class for ''DirectoryScannerActor''.
+ */
+class DirectoryScannerActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+ImplicitSender with FlatSpecLike with Matchers with BeforeAndAfterAll with MockitoSugar {
+
+  import de.oliver_heger.splaya.media.DirectoryScannerActorSpec._
+
+  def this() = this(ActorSystem("DirectoryScannerActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    system.shutdown()
+    system awaitTermination 10.seconds
+  }
+
+  "A DirectoryScannerActor" should "handle a ScanPath message" in {
+    val scanner = mock[DirectoryScanner]
+    when(scanner.scan(Path)).thenReturn(TestResult)
+    val scanActor = system.actorOf(Props(classOf[DirectoryScannerActor], scanner))
+
+    scanActor ! ScanPath(Path)
+    expectMsg(TestResult)
+  }
+}
