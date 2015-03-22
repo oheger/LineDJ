36966bac4d96ec53a4d111ebb1dbc1f0e4d0cc96
oheger <oliver.heger@oliver-heger.de>
2015-03-22 18:27:11 +0100

    Added MediumInfoParserActor class.
    This is an actor implementation around a MediumInfoParser object. Wrapping the
    parser by an actor provides an easy means to handle multiple parse operations
    in parallel.

---
+++ splaya-akka/src/main/scala/de/oliver_heger/splaya/media/MediumInfoParserActor.scala
@@ -0,0 +1,50 @@
+package de.oliver_heger.splaya.media
+
+import akka.actor.Actor
+
+/**
+ * Companion object.
+ */
+object MediumInfoParserActor {
+  /** The name of an unknown medium. */
+  private val UnknownMedium = "unknown"
+
+  /**
+   * Template for a default settings object to be returned in case of a
+   * parsing error.
+   */
+  private lazy val DummyMediumSettingsData = MediumSettingsData(name = UnknownMedium,
+    description = "", mediumURI = null, orderMode = "", orderParams = xml.NodeSeq.Empty)
+
+  /**
+   * A message processed by ''MediumInfoParserActor'' which tells it to parse a
+   * medium description. The actor will delegate the received data to its
+   * associated parser and send a [[MediumInfo]] object as answer.
+   * @param descriptionData the data of the description in binary form
+   * @param mediumURI the medium URI
+   */
+  case class ParseMediumInfo(descriptionData: Array[Byte], mediumURI: String)
+
+}
+
+/**
+ * A specialized actor implementation which wraps a [[MediumInfoParser]].
+ *
+ * This actor class is used to parse medium description files in parallel. At
+ * construction time a ''MediumInfoParser'' is passed. This actor accepts a
+ * message for parsing a medium description file. The file content is delegated
+ * to the associated parser. The result is sent back as a
+ * [[MediumSettingsData]] object.
+ *
+ * @param parser the parser for medium description data
+ */
+class MediumInfoParserActor(parser: MediumInfoParser) extends Actor {
+
+  import MediumInfoParserActor._
+
+  override def receive: Receive = {
+    case ParseMediumInfo(desc, mediumURI) =>
+      sender ! parser.parseMediumInfo(desc, mediumURI).getOrElse(DummyMediumSettingsData.copy
+        (mediumURI = mediumURI))
+  }
+}
