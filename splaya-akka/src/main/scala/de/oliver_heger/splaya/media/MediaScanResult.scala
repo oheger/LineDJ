ffcbbdcf7c1682ae24d2ec24c2aa712765d1b9c0
oheger <oliver.heger@oliver-heger.de>
2015-07-04 16:21:05 +0200

    Introduced a new MediumID trait.
    The purpose is to simplify the handling of media and their files and to stop
    the separation between normal media and a list of other files. A
    MediaScanResult now only consists of a single map; the additional list for
    other files is no longer necessary.
    
    Note that this commit breaks the build because other classes have to be
    adapted to this change.

--- splaya-akka/src/main/scala/de/oliver_heger/splaya/media/MediaScanResult.scala
+++ splaya-akka/src/main/scala/de/oliver_heger/splaya/media/MediaScanResult.scala
@@ -14,16 +14,52 @@ import java.nio.file.Path
 case class MediaFile(path: Path, size: Long)
 
 /**
+ * A trait representing the ID of a medium.
+ *
+ * A medium is a root of a directory structure containing media files. The
+ * medium is described by a medium description file.
+ *
+ * Alternatively, all media files that do not belong to a specific medium are
+ * assigned to a synthetic medium with no medium description path. These
+ * scenarios are implemented by the implementations of this trait.
+ */
+sealed trait MediumID {
+  /**
+   * The optional path to the medium description file. If defined, this file
+   * contains information about this medium.
+   */
+  val mediumDescriptionPath: Option[Path]
+}
+
+/**
+ * An implementation of ''MediumID'' for an existing medium. The path to the
+ * medium description file actually exists and is passed to the constructor.
+ *
+ * @param path the path to the medium description file
+ */
+case class DefinedMediumID(path: Path) extends MediumID {
+  override val mediumDescriptionPath = Option(path)
+}
+
+/**
+ * An implementation of ''MediumID'' representing the synthetic medium that
+ * contains all files without a real medium. This implementation does not
+ * provide a path to a description file.
+ */
+case object UndefinedMediumID extends MediumID {
+  override val mediumDescriptionPath = None
+}
+
+/**
  * A data class storing the results of a directory scan for media files (in its
  * raw form).
  *
- * This class consists of a map with files that could be assigned to a medium
- * and all other files encountered in the processed directory structure. A
- * medium is represented by a path pointing to it medium description file.
+ * This class consists of a map with ''MediumID'' objects and the files of this
+ * medium assigned to it. Files which could not be assigned to a medium are
+ * stored under the key [[UndefinedMediumID]]. The medium ID can be used to
+ * obtain a path pointing to the corresponding medium description file.
  *
  * @param root the root path that has been scanned
  * @param mediaFiles a map with files assigned to a medium
- * @param otherFiles a list with other files
  */
-case class MediaScanResult(root: Path, mediaFiles: Map[Path, List[MediaFile]], otherFiles:
-List[MediaFile])
+case class MediaScanResult(root: Path, mediaFiles: Map[MediumID, List[MediaFile]])
