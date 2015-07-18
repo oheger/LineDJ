87d63b1d0c0816caea62add8ab820a0266854b93
oheger <oliver.heger@oliver-heger.de>
2015-07-18 22:14:27 +0200

    Fixed a potential NumberFormatException in ID3TagProvider.
    The conversion to numeric values could not handle empty strings. This has been
    fixed.

--- server/src/main/scala/de/oliver_heger/splaya/mp3/ID3TagProvider.scala
+++ server/src/main/scala/de/oliver_heger/splaya/mp3/ID3TagProvider.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.oliver_heger.splaya.mp3
 
 /**
@@ -62,12 +77,11 @@ trait ID3TagProvider {
    * @return an option for the corresponding numeric value
    */
   private def parseNumericProperty(value: String): Option[Int] = {
-    val idx = value.indexWhere(!Character.isDigit(_))
-    if (idx == 0) {
+    val numericValue = value takeWhile Character.isDigit
+    if (numericValue.isEmpty) {
       None
     } else {
-      val part = if (idx < 0) value else value.substring(0, idx)
-      Some(part.toInt)
+      Some(numericValue.toInt)
     }
   }
 }
