f0d35996649aa1e417843dd2e15a1ccda2443ead
oheger <oliver.heger@oliver-heger.de>
2015-07-31 21:33:02 +0200

    Added RemoteRelayActor class.
    This actor handles the communication with the backend. It can also monitor the
    server's available state.

---
+++ browser/src/main/scala/de/oliver_heger/linedj/remoting/RemoteRelayActor.scala
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.remoting
+
+import akka.actor.{Actor, ActorRef, Props}
+import de.oliver_heger.linedj.remoting.RemoteActors.RemoteActor
+import de.oliver_heger.splaya.utils.ChildActorFactory
+
+object RemoteRelayActor {
+
+  /**
+   * A message sent by ''RemoteRelayActor'' when the server is available. This
+   * means that all references to remote actors have been resolved.
+   */
+  case object ServerAvailable
+
+  /**
+   * A message sent by ''RemoteRelayActor'' when the server becomes
+   * unavailable. This message is sent on the message bus when at least one of
+   * the tracked remote actors becomes unavailable.
+   */
+  case object ServerUnavailable
+
+  /**
+   * A message to be processed by ''RemoteRelayActor'' telling it to send a
+   * message to a remote actor. The actor is determined by the ''target''
+   * parameter. If this remote actor is currently available, the message is
+   * sent to it.
+   * @param target the remote actor type which receives the message
+   * @param msg the message to be sent
+   */
+  case class RemoteMessage(target: RemoteActor, msg: Any)
+
+  /** The delay sequence for looking up remote actors. */
+  private val DelaySequence = new BoundedDelaySequence(90, 5, 2)
+
+  private class RemoteRelayActorImpl(remoteAddress: String, remotePort: Int, messageBus:
+  MessageBus) extends RemoteRelayActor(remoteAddress, remotePort, messageBus) with ChildActorFactory
+
+  /**
+   * Creates a ''Props'' object for creating a new actor instance.
+   * @param remoteAddress the address of the remote actor system
+   * @param remotePort the port of the remote actor system
+   * @param messageBus the message bus
+   * @return creation properties for a new actor instance
+   */
+  def apply(remoteAddress: String, remotePort: Int, messageBus: MessageBus): Props =
+    Props(classOf[RemoteRelayActorImpl], remoteAddress, remotePort, messageBus)
+
+  /**
+   * A data class holding the current tracking state of remote actors. This
+   * class stores the references to remote actors and their types. It also
+   * allows access to a specific remote actor.
+   * @param trackedActors a map with information about tracked actors
+   */
+  private class RemoteActorTrackingState(trackedActors: Map[RemoteActor, ActorRef]) {
+    /**
+     * Returns a flag whether all remote actors are currently available. This
+     * is used as indication that the server is now available.
+     * @return '''true''' if all tracked remote actors are available
+     */
+    def trackingComplete: Boolean =
+      trackedActors.size == RemoteActors.values.size
+
+    /**
+     * Returns an updated tracking state object when a remote actor becomes
+     * available.
+     * @param a option for the type of the remote actor
+     * @param ref the actor reference
+     * @return the updated tracking state
+     */
+    def remoteActorFound(a: Option[RemoteActor], ref: ActorRef): RemoteActorTrackingState =
+      updateState(a) { (act, m) => m + (act -> ref) }
+
+    /**
+     * Returns an updated tracking state object when a remote actor becomes
+     * unavailable.
+     * @param a option for the type of the remote actor
+     * @return the updated tracking state
+     */
+    def remoteActorLost(a: Option[RemoteActor]): RemoteActorTrackingState =
+      updateState(a) { (act, m) => m - act }
+
+    /**
+     * Returns an option for the current remote actor of the given type. This
+     * is ''None'' if this actor is currently not available.
+     * @param a the remote actor type
+     * @return an option for the reference to this remote actor
+     */
+    def remoteActorOption(a: RemoteActor): Option[ActorRef] = trackedActors get a
+
+    /**
+     * Calculates the new tracking state based on a function to be applied on
+     * the current map with remote actors. This function is called when change
+     * events for remote actors are processed.
+     * @param a option for the remote actor type affected by the change; this
+     *          may be ''None'' if an invalid actor path was passed
+     * @param f the function to update the state map
+     * @return the new tracking state
+     */
+    private def updateState(a: Option[RemoteActor])
+                           (f: (RemoteActor, Map[RemoteActor, ActorRef]) => Map[RemoteActor,
+                             ActorRef]): RemoteActorTrackingState = {
+      a map (ra => new RemoteActorTrackingState(f(ra, trackedActors))) getOrElse this
+    }
+  }
+
+}
+
+/**
+ * An actor class which handles the communication with the remote actor system.
+ *
+ * This actor class is responsible for establishing a connection to the remote
+ * actor system running the LineDJ backend. This is achieved by looking up a
+ * number of remote actors the client needs to access; only if references to
+ * all required remote actors have been resolved, the server system is
+ * considered available. Address and port of the remote actor system have to be
+ * specified as constructor arguments.
+ *
+ * All message exchange with remote actors is routed via this actor. In order
+ * to send a message to a remote actor, the message is wrapped into a
+ * ''RemoteMessage'' object which identifies the target actor. Messages
+ * received from the remote system (as answers to requests) are published via
+ * the specified [[MessageBus]].
+ *
+ * @param remoteAddress the address of the remote actor system
+ * @param remotePort the port of the remote actor system
+ * @param messageBus the message bus
+ */
+class RemoteRelayActor(remoteAddress: String, remotePort: Int, messageBus: MessageBus) extends
+Actor {
+  this: ChildActorFactory =>
+
+  import RemoteRelayActor._
+
+  /** A map for assigning lookup paths to corresponding remote actors. */
+  private val pathMapping = createLookupPathMapping()
+
+  /** The current remote actor tracking state. */
+  private var trackingState = new RemoteActorTrackingState(Map.empty)
+
+  /**
+   * @inheritdoc This implementation creates lookup actors for the remote
+   *             actors to be tracked.
+   */
+  @throws[Exception](classOf[Exception])
+  override def preStart(): Unit = {
+    super.preStart()
+
+    pathMapping.keys foreach { p =>
+      createChildActor(Props(classOf[RemoteLookupActor], p, self, DelaySequence))
+    }
+  }
+
+  override def receive: Receive = {
+    case RemoteLookupActor.RemoteActorAvailable(path, ref) =>
+      updateTrackingState(trackingState.remoteActorFound(pathMapping get path, ref))
+
+    case RemoteLookupActor.RemoteActorUnavailable(path) =>
+      updateTrackingState(trackingState.remoteActorLost(pathMapping get path))
+
+    case RemoteMessage(target, msg) =>
+      trackingState remoteActorOption target foreach (_ ! msg)
+
+    case msg =>
+      messageBus publish msg
+  }
+
+  /**
+   * Updates the remote actor tracking state. This method is called when a
+   * message from a lookup actor was received indicating that a remote actor
+   * was detected or became unavailable. If necessary, the changed server state
+   * has to be published.
+   * @param newState the updated tracking state
+   */
+  private def updateTrackingState(newState: RemoteActorTrackingState): Unit = {
+    val oldState = trackingState
+    trackingState = newState
+    if (oldState.trackingComplete != newState.trackingComplete) {
+      messageBus.publish(if (newState.trackingComplete) ServerAvailable else ServerUnavailable)
+    }
+  }
+
+  /**
+   * Creates a map that allows mapping a remote lookup path to the
+   * corresponding remote actor type.
+   * @return the map
+   */
+  private def createLookupPathMapping(): Map[String, RemoteActor] = {
+    val mapping = RemoteActors.values map (a => lookupPath(a) -> a)
+    Map(mapping.toSeq: _*)
+  }
+
+  /**
+   * Generates the lookup path for a remote actor based on the parameters
+   * passed to this instance.
+   * @param actor the remote actor
+   * @return the lookup path to this actor
+   */
+  private def lookupPath(actor: RemoteActor): String =
+    s"akka.tcp://LineDJ-Server@$remoteAddress:$remotePort/user/${actor.name}"
+}
---
+++ browser/src/test/scala/de/oliver_heger/linedj/remoting/RemoteRelayActorSpec.scala
@@ -0,0 +1,260 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.remoting
+
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import de.oliver_heger.splaya.utils.ChildActorFactory
+import org.mockito.Mockito._
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+
+object RemoteRelayActorSpec {
+  /** The address of the remote system. */
+  private val Host = "remoteHost"
+
+  /** The port of the remote system. */
+  private val Port = 1414
+
+  /** A test message. */
+  private val Message = new Object
+}
+
+/**
+ * Test class for ''RemoteRelayActor''.
+ */
+class RemoteRelayActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+
+  import RemoteRelayActorSpec._
+
+  def this() = this(ActorSystem("RemoteRelayActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    system.shutdown()
+    system awaitTermination 10.seconds
+  }
+
+  "A RemoteRelayActor" should "create correct creation properties" in {
+    val bus = mock[MessageBus]
+    val props = RemoteRelayActor(Host, Port, bus)
+    classOf[RemoteRelayActor].isAssignableFrom(props.actorClass()) shouldBe true
+    classOf[ChildActorFactory].isAssignableFrom(props.actorClass()) shouldBe true
+    props.args should have size 3
+    props.args should contain inOrderOnly(Host, Port, bus)
+  }
+
+  it should "create the expected child actors" in {
+    val helper = new RemoteRelayActorTestHelper
+
+    helper.awaitChildrenCreation() shouldBe true
+  }
+
+  it should "send a ServerAvailable message when all remote actors have been retrieved" in {
+    val helper = new RemoteRelayActorTestHelper
+
+    helper.provideRemoteActors()
+  }
+
+  it should "send any message received from outside to the message bus" in {
+    val helper = new RemoteRelayActorTestHelper
+    val actor = helper.provideRemoteActors()
+
+    actor ! Message
+    expectMsg(Message)
+  }
+
+  it should "send a server unavailable message when a remote actor is lost" in {
+    val helper = new RemoteRelayActorTestHelper
+    helper.provideRemoteActors()
+
+    helper.unregisterRemoteActor(helper.probeMediaManager)
+    expectMsg(RemoteRelayActor.ServerUnavailable)
+  }
+
+  it should "monitor the server state" in {
+    val helper = new RemoteRelayActorTestHelper
+    helper.provideRemoteActors()
+
+    helper.unregisterRemoteActor(helper.probeMetaDataManager)
+    expectMsg(RemoteRelayActor.ServerUnavailable)
+    helper.unregisterRemoteActor(helper.probeMediaManager)
+    helper.registerRemoteActor(helper.probeMetaDataManager)
+    helper.registerRemoteActor(helper.probeMediaManager)
+    expectMsg(RemoteRelayActor.ServerAvailable)
+  }
+
+  it should "allow sending a message to the media manager actor" in {
+    val helper = new RemoteRelayActorTestHelper
+    helper.provideRemoteActors() ! RemoteRelayActor.RemoteMessage(RemoteActors.MediaManager,
+      Message)
+
+    helper.probeMediaManager.expectMsg(Message)
+  }
+
+  it should "allow sending a message to the meta data manager actor" in {
+    val helper = new RemoteRelayActorTestHelper
+    helper.provideRemoteActors() ! RemoteRelayActor.RemoteMessage(RemoteActors.MetaDataManager,
+      Message)
+
+    helper.probeMetaDataManager.expectMsg(Message)
+  }
+
+  it should "ignore messages to remote actors not available" in {
+    val helper = new RemoteRelayActorTestHelper
+    helper registerRemoteActor helper.probeMediaManager
+
+    helper.relayActor ! RemoteRelayActor.RemoteMessage(RemoteActors.MetaDataManager, "ignore")
+    helper registerRemoteActor helper.probeMetaDataManager
+    expectMsg(RemoteRelayActor.ServerAvailable)
+    helper.relayActor ! RemoteRelayActor.RemoteMessage(RemoteActors.MetaDataManager, Message)
+    helper.probeMetaDataManager.expectMsg(Message)
+  }
+
+  it should "ignore invalid remote actor paths" in {
+    val helper = new RemoteRelayActorTestHelper
+    helper registerRemoteActor helper.probeMetaDataManager
+
+    helper.relayActor ! RemoteLookupActor.RemoteActorAvailable("invalid path", testActor)
+    helper registerRemoteActor helper.probeMediaManager
+    expectMsg(RemoteRelayActor.ServerAvailable)
+  }
+
+  /**
+   * A helper class for testing the relay actor. It manages a set of
+   * dependent objects.
+   */
+  private class RemoteRelayActorTestHelper {
+    /** Test probe for the lookup actor for the media manager. */
+    val probeMediaManagerLookup = TestProbe()
+
+    /** Test probe for the lookup actor for the meta data manager actor. */
+    val probeMetaDataManagerLookup = TestProbe()
+
+    /** Test probe for the remote media manager actor. */
+    val probeMediaManager = TestProbe()
+
+    /** Test probe for the remote meta data manager actor. */
+    val probeMetaDataManager = TestProbe()
+
+    /** A mock for the message bus. */
+    val messageBus = createMessageBus()
+
+    /** The test relay actor. */
+    val relayActor = system.actorOf(createProps())
+
+    /** The prefix for lookup paths. */
+    private val LookupPrefix = "akka.tcp://LineDJ-Server@" + Host + ":" + Port + "/user/"
+
+    /** A map that stores references for the paths to remote actors. */
+    private val pathActorMapping = Map(
+      LookupPrefix + "mediaManager" ->(probeMediaManagerLookup.ref, probeMediaManager.ref),
+      LookupPrefix + "metaDataManager" ->(probeMetaDataManagerLookup.ref, probeMetaDataManager.ref))
+
+    /** A map for retrieving the lookup path for a remote actor reference. */
+    private val lookupMap = pathActorMapping map { e => e._2._2 -> e._1 }
+
+    /** A latch for synchronizing with the child actor creation. */
+    private val childActorLatch = new CountDownLatch(pathActorMapping.size)
+
+    /** Temporary map for creating child actors. */
+    private var lookupPaths = pathActorMapping
+
+    /**
+     * Waits until all expected child actors have been created and returns a
+     * flag whether this was successful.
+     * @return a flag whether the child actors have been created
+     */
+    def awaitChildrenCreation(): Boolean = {
+      childActorLatch.await(5, TimeUnit.SECONDS)
+    }
+
+    /**
+     * Initializes the test actor with dependencies to the remote actors to be
+     * tracked. It is checked whether the initial server available message is
+     * received.
+     * @return a reference to the test actor
+     */
+    def provideRemoteActors(): ActorRef = {
+      pathActorMapping foreach { e => relayActor ! RemoteLookupActor.RemoteActorAvailable(e._1, e
+        ._2._2) }
+      expectMsg(RemoteRelayActor.ServerAvailable)
+      relayActor
+    }
+
+    /**
+     * Makes the specified remote actor available. Sends a corresponding
+     * message to the test actor.
+     * @param probe the probe representing the remote actor
+     */
+    def registerRemoteActor(probe: TestProbe): Unit = {
+      relayActor ! RemoteLookupActor.RemoteActorAvailable(lookupMap(probe.ref), probe.ref)
+    }
+
+    /**
+     * Removes the specified remote actor. Sends a corresponding message to the
+     * test actor.
+     * @param probe the probe representing the remote actor
+     */
+    def unregisterRemoteActor(probe: TestProbe): Unit = {
+      relayActor ! RemoteLookupActor.RemoteActorUnavailable(lookupMap(probe.ref))
+    }
+
+    /**
+     * Creates a properties object for the test relay actor.
+     * @return the properties for creating the test relay actor
+     */
+    private def createProps(): Props = {
+      Props(new RemoteRelayActor(Host, Port, messageBus) with ChildActorFactory {
+        override def createChildActor(p: Props): ActorRef = {
+          p.actorClass() should be(classOf[RemoteLookupActor])
+          p.args should have size 3
+          lookupPaths should contain key p.args.head
+          p.args(1) should be(relayActor)
+          childActorLatch.countDown()
+          val path = p.args.head.toString
+          val child = lookupPaths(path)._1
+          lookupPaths -= path
+          child
+        }
+      })
+    }
+
+    /**
+     * Creates a mock for the message bus. This mock simply passes all messages
+     * to be published to the test actor.
+     * @return the mock message bus
+     */
+    private def createMessageBus(): MessageBus = {
+      val bus = mock[MessageBus]
+      when(bus.publish(org.mockito.Matchers.any())).then(new Answer[Boolean] {
+        override def answer(invocationOnMock: InvocationOnMock): Boolean = {
+          testActor ! invocationOnMock.getArguments()(0)
+          true
+        }
+      })
+      bus
+    }
+  }
+
+}
