04a82a3883abe5cbc25d84e4c7aab9beece46acf
oheger <oliver.heger@oliver-heger.de>
2015-08-01 16:00:12 +0200

    Added RemoteMessageBus class.
    This class simplifies the communication with the backend by wrapping a relay
    actor and implementing a simple ask pattern.

---
+++ browser/src/main/scala/de/oliver_heger/linedj/remoting/RemoteMessageBus.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.remoting
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import akka.actor.{Actor, ActorRef}
+import de.oliver_heger.linedj.remoting.RemoteActors.RemoteActor
+
+/**
+ * An enhanced [[MessageBus]] implementation which can be used for
+ * communication with the backend.
+ *
+ * This class wraps a ''MessageBus'' and extends it by functionality for
+ * sending messages to remote actors and receiving their responses. The basic
+ * idea is that this bus is connected to a [[RemoteRelayActor]] and offers
+ * convenience methods for sending a message to this actor. The sender is then
+ * automatically subscribed to the underlying message bus. When a message is
+ * received which the original sender can handler it is again unsubscribed.
+ *
+ * @param relayActor the ''RemoteRelayActor''
+ * @param bus the underlying message bus
+ */
+class RemoteMessageBus(val relayActor: ActorRef, val bus: MessageBus) {
+  /**
+   * Sends a message to a remote actor. No answer is expected; so the message
+   * is just passed to the specified target remote actor.
+   * @param target the target actor
+   * @param msg the message
+   */
+  def send(target: RemoteActor, msg: Any): Unit = {
+    relayActor ! RemoteRelayActor.RemoteMessage(target, msg)
+  }
+
+  /**
+   * Sends a message to a remote actor and registers a listener at the
+   * underlying message bus to handle the response. The response listener stays
+   * connected until a message is received which it can handle.
+   * @param target the target actor
+   * @param msg the message
+   * @param responseListener the handler for the response
+   */
+  def ask(target: RemoteActor, msg: Any)(responseListener: Actor.Receive): Unit = {
+    val refListenerID = new AtomicInteger
+    val listenerID = bus registerListener wrapReceive(responseListener, refListenerID)
+    refListenerID set listenerID
+    send(target, msg)
+  }
+
+  /**
+   * Wraps a receive function to ensure that it is removed from the message bus
+   * when a response message is received.
+   * @param r the function to be wrapped
+   * @param refRegistrationID a reference to the ID of the listener registration
+   * @return the wrapper function
+   */
+  private def wrapReceive(r: Actor.Receive, refRegistrationID: AtomicInteger): Actor.Receive = {
+    new PartialFunction[Any, Unit] {
+      override def isDefinedAt(x: Any): Boolean = r isDefinedAt x
+
+      override def apply(msg: Any): Unit = {
+        r(msg)
+        bus removeListener refRegistrationID.get()
+      }
+    }
+  }
+}
---
+++ browser/src/test/scala/de/oliver_heger/linedj/remoting/RemoteMessageBusSpec.scala
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.remoting
+
+import akka.actor.Actor.Receive
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+
+object RemoteMessageBusSpec {
+  /** Constant for test message. */
+  private val Message = new Object
+
+  /** Constant for the test message wrapped in a remote message. */
+  private val RemoteMessage = RemoteRelayActor.RemoteMessage(RemoteActors.MediaManager, Message)
+
+  /** ID for a message listener registration. */
+  private val ListenerID = 20150731
+}
+
+/**
+ * Test class for ''RemoteMessageBus''.
+ */
+class RemoteMessageBusSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+
+  import RemoteMessageBusSpec._
+
+  def this() = this(ActorSystem("RemoteMessageBusSpec"))
+
+  override protected def afterAll(): Unit = {
+    system.shutdown()
+    system awaitTermination 10.seconds
+  }
+
+  /**
+   * Creates the test object, initialized with the test actor serving as relay
+   * actor.
+   * @return the test message bus
+   */
+  private def createRemoteBus(): RemoteMessageBus = {
+    val bus = mock[MessageBus]
+    new RemoteMessageBus(testActor, bus)
+  }
+
+  /**
+   * Obtains the listener that has been registered on the message bus mock.
+   * @param bus the test remote bus
+   * @return the listener
+   */
+  private def fetchListener(bus: RemoteMessageBus): Receive = {
+    val captor = ArgumentCaptor.forClass(classOf[Receive])
+    verify(bus.bus).registerListener(captor.capture())
+    captor.getValue
+  }
+
+  /**
+   * Prepares the remote bus to expect the registration of a response listener.
+   * @param bus the test bus object
+   * @return the remote message bus
+   */
+  private def expectListenerRegistration(bus: RemoteMessageBus): RemoteMessageBus = {
+    when(bus.bus.registerListener(any(classOf[Receive]))).thenReturn(ListenerID)
+    bus
+  }
+
+  "A RemoteMessageBus" should "simplify sending remote messages" in {
+    val bus = createRemoteBus()
+
+    bus.send(RemoteActors.MediaManager, Message)
+    expectMsg(RemoteMessage)
+    verifyZeroInteractions(bus.bus)
+  }
+
+  it should "support waiting for a response and ignore unhandled messages" in {
+    val bus = expectListenerRegistration(createRemoteBus())
+    val responseFunc: Receive = {
+      case "Ping" => // not relevant, just ignore test message
+    }
+
+    bus.ask(RemoteMessage.target, Message)(responseFunc)
+    expectMsg(RemoteMessage)
+    val listener = fetchListener(bus)
+    listener isDefinedAt Message shouldBe false
+    verifyNoMoreInteractions(bus.bus)
+  }
+
+  it should "handle a response message correctly" in {
+    val bus = expectListenerRegistration(createRemoteBus())
+    val PingMsg = "Ping"
+    val PongMsg = "Pong"
+    val responseFunc: Receive = {
+      case PingMsg =>
+        testActor ! PongMsg
+    }
+
+    bus.ask(RemoteMessage.target, Message)(responseFunc)
+    expectMsg(RemoteMessage)
+    val listener = fetchListener(bus)
+    listener isDefinedAt PingMsg shouldBe true
+    listener(PingMsg)
+    expectMsg(PongMsg)
+    verify(bus.bus).removeListener(ListenerID)
+  }
+}
