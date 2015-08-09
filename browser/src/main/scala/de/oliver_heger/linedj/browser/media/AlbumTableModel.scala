8a34891f25be4c98d02e41cb6e6ca87612e65ea2
oheger <oliver.heger@oliver-heger.de>
2015-08-09 22:07:07 +0200

    Added AlbumTableModel class.
    This class serves as model for a table that shows the songs of an album on a
    medium.

---
+++ browser/src/main/scala/de/oliver_heger/linedj/browser/media/AlbumTableModel.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.browser.media
+
+import de.oliver_heger.linedj.browser.media.AlbumTableModel.AlbumData
+import de.oliver_heger.linedj.browser.model.SongData
+
+private object AlbumTableModel {
+  /**
+   * An empty table model object.
+   */
+  val empty: AlbumTableModel = new AlbumTableModel(Map.empty)
+
+  /**
+   * A helper class storing information about an album. It allows querying an
+   * ordered song list which is dynamically constructed on demand.
+   * @param songs the list of songs
+   * @param ordered a flag whether the list is ordered
+   */
+  private case class AlbumData(songs: List[SongData], ordered: Boolean) {
+    /** The ordered song list.*/
+    lazy val orderedSongs = createOrderedSongs()
+
+    /**
+     * Creates a new instance that contains the specified song.
+     * @param song the song to be added
+     * @return the new instance
+     */
+    def add(song: SongData): AlbumData = {
+      AlbumData(song :: songs, ordered =ordered && song > songs.head)
+    }
+
+    /**
+     * Creates an ordered list of songs based on the current song list.
+     * @return the ordered list of songs
+     */
+    private def createOrderedSongs() = if(ordered) songs.reverse
+    else songs.sortWith(_ < _)
+  }
+}
+
+/**
+ * An internal helper class serving as a model for a table that displays the
+ * albums of an artist.
+ *
+ * An instance of this class can manage the albums of multiple artists. A
+ * sorted list of the songs on a specific albums can be queried (songs are
+ * sorted by track number - if available - and name).
+ *
+ * The model is constructed by adding information about single songs step by
+ * step in arbitrary order. This reflects the way in which meta data can come
+ * in while the server scans its directory structure.
+ *
+ * @param data the map with the data of this model
+ */
+private class AlbumTableModel(data: Map[AlbumKey, AlbumData]) {
+  /**
+   * Returns a sequence with songs on the specified album. If the album cannot
+   * be resolved, result is an empty sequence.
+   * @param key the key of the album
+   * @return an ordered sequence with the songs on this album
+   */
+  def songsFor(key: AlbumKey): Seq[SongData] =
+    data get key map(_.orderedSongs) getOrElse Nil
+
+  /**
+   * Creates a new model instance with the specified song added to an album.
+   * @param key the key of the album
+   * @param song the song to be added
+   * @return the new album instance
+   */
+  def add(key: AlbumKey, song: SongData): AlbumTableModel = {
+    val albumData = data.get(key).map (_.add(song)).getOrElse(AlbumData(List(song), ordered = true))
+    new AlbumTableModel(data + (key -> albumData))
+  }
+}
---
+++ browser/src/test/scala/de/oliver_heger/linedj/browser/media/AlbumTableModelSpec.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.browser.media
+
+import de.oliver_heger.linedj.browser.model.SongData
+import de.oliver_heger.splaya.metadata.MediaMetaData
+import org.scalatest.{Matchers, FlatSpec}
+
+object AlbumTableModelSpec {
+  /** Constant for an album key.*/
+  private val Album1 = AlbumKey("Queen", "A Night at the Opera")
+
+  /** Constant for another album.*/
+  private val Album2 = AlbumKey("Supertramp", "Crisis? What Crisis?")
+
+  /**
+   * Creates a song data object with the specified data.
+   * @param name the song name
+   * @param track the track number
+   * @return the song data object
+   */
+  private def song(name:String, track: Int): SongData =
+    SongData("song://" + name, MediaMetaData(title = Some(name), trackNumber = Some(track)))
+
+  /**
+   * Adds the given songs to a model.
+   * @param model the model
+   *              @param key the album key
+   * @param songs the songs to be added
+   * @return the resulting table model
+   */
+  private def appendSongs(model: AlbumTableModel = AlbumTableModel.empty)
+                         (key: AlbumKey, songs: SongData*): AlbumTableModel =
+  songs.foldLeft(model)((m, s) => m.add(key, s))
+}
+
+/**
+ * Test class for ''AlbumTableModel''.
+ */
+class AlbumTableModelSpec extends FlatSpec with Matchers {
+  import AlbumTableModelSpec._
+  "An AlbumTableModel" should "return an empty sequence for unknown data" in {
+    AlbumTableModel.empty songsFor AlbumKey("Unknown", "Unknown, too") shouldBe 'empty
+  }
+
+  it should "allow appending songs in order" in {
+    val song1 = song("You are my best friend", 4)
+    val song2 = song("39", 5)
+    val song3 = song("Bohemian Rhapsody", 11)
+
+    val model = appendSongs()(Album1, song1, song2, song3)
+    model songsFor Album1 should contain inOrderOnly(song1, song2, song3)
+  }
+
+  it should "support multiple albums" in {
+    val song1 = song("You are my best friend", 4)
+    val song2 = song("39", 5)
+    val song3 = song("Bohemian Rhapsody", 11)
+    val song4 = song("Sister Moonshine", 2)
+    val song5 = song("A Soapbox opera", 4)
+
+    val m1 = appendSongs()(Album1, song1, song2)
+    val m2 = appendSongs(m1)(Album2, song4, song5)
+    val model = appendSongs(m2)(Album1, song3)
+    model songsFor Album1 should contain inOrderOnly(song1, song2, song3)
+    model songsFor Album2 should contain inOrderOnly(song4, song5)
+  }
+
+  it should "support adding songs in arbitrary order" in {
+    val song1 = song("You are my best friend", 4)
+    val song2 = song("39", 5)
+    val song3 = song("Bohemian Rhapsody", 11)
+
+    val model = appendSongs()(Album1, song2, song1, song3)
+    model songsFor Album1 should contain inOrderOnly(song1, song2, song3)
+  }
+}
