fe0bebad852488ad89fc908d9b659f215e078c91
oheger <oliver.heger@oliver-heger.de>
2015-08-30 20:26:38 +0200

    Added SelectionChangeHandler class.
    This class listens for selection change events on controls and delegates them
    to the media controller.

---
+++ browser/src/main/scala/de/oliver_heger/linedj/browser/media/SelectionChangeHandler.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.browser.media
+
+import net.sf.jguiraffe.gui.builder.components.model.{ListComponentHandler, TreeHandler}
+import net.sf.jguiraffe.gui.builder.event.{FormChangeEvent, FormChangeListener}
+
+/**
+ * An event listener class which reacts on selection change events on multiple
+ * controls and notifies the [[MediaController]].
+ *
+ * When the user selects another medium in the combo box the media controller
+ * is notified so that the data of the new medium can be requested.
+ *
+ * When another artist or album is selected in the tree view, a notification is
+ * sent as well; then the table view has to be updated.
+ *
+ * @param controller the ''MediaController''
+ */
+class SelectionChangeHandler(controller: MediaController) extends FormChangeListener {
+  override def elementChanged(formChangeEvent: FormChangeEvent): Unit = {
+    formChangeEvent.getHandler match {
+      case listHandler: ListComponentHandler =>
+        controller selectMedium listHandler.getData.asInstanceOf[String]
+
+      case treeHandler: TreeHandler =>
+        controller selectAlbums treeHandler.getSelectedPaths
+    }
+  }
+}
---
+++ browser/src/test/scala/de/oliver_heger/linedj/browser/media/SelectionChangeHandlerSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.browser.media
+
+import net.sf.jguiraffe.gui.builder.components.model.{ListComponentHandler, TreeHandler,
+TreeNodePath}
+import net.sf.jguiraffe.gui.builder.event.FormChangeEvent
+import net.sf.jguiraffe.gui.forms.ComponentHandler
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Test class for ''SelectionChangeHandler''.
+ */
+class SelectionChangeHandlerSpec extends FlatSpec with Matchers with MockitoSugar {
+  /**
+   * Creates a change event for the specified component handler.
+   * @param handler the component handler
+   * @return the change event
+   */
+  private def createChangeEvent(handler: ComponentHandler[_]): FormChangeEvent =
+    new FormChangeEvent(this, handler, "some name")
+
+  "A SelectionChangeHandler" should "react on a changed medium selection" in {
+    val Medium = "Some Test Medium"
+    val controller = mock[MediaController]
+    val listHandler = mock[ListComponentHandler]
+    doReturn(Medium).when(listHandler).getData
+    val handler = new SelectionChangeHandler(controller)
+
+    handler elementChanged createChangeEvent(listHandler)
+    verify(controller).selectMedium(Medium)
+  }
+
+  it should "react on a changed album selection" in {
+    val SelectedPaths = Array(mock[TreeNodePath], mock[TreeNodePath])
+    val controller = mock[MediaController]
+    val treeHandler = mock[TreeHandler]
+    when(treeHandler.getSelectedPaths).thenReturn(SelectedPaths)
+    val handler = new SelectionChangeHandler(controller)
+
+    handler elementChanged createChangeEvent(treeHandler)
+    verify(controller).selectAlbums(SelectedPaths)
+  }
+}
