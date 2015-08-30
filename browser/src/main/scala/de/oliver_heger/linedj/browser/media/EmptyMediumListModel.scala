af12fc6c9bc8bcf924cb0dc8d4906dd2bd682443
oheger <oliver.heger@oliver-heger.de>
2015-08-30 19:54:28 +0200

    Added EmptyMediumListModel class.
    This is the initial list model assigned to the media combo box. The actual
    content of this combobox is then added dynamically.

---
+++ browser/src/main/scala/de/oliver_heger/linedj/browser/media/EmptyMediumListModel.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.browser.media
+
+import net.sf.jguiraffe.gui.builder.components.model.ListModel
+
+/**
+ * A class representing the initial empty list model passed to the combo box
+ * for media.
+ *
+ * The content of the media combo box is calculated dynamically and added to
+ * the handler object. Nevertheless, an initial model is needed. This class
+ * implements this initial model. It is merely a dummy.
+ */
+class EmptyMediumListModel extends ListModel {
+  override val getType = classOf[String]
+
+  override def getDisplayObject(i: Int): AnyRef = null
+
+  override def size(): Int = 0
+
+  override def getValueObject(i: Int): AnyRef = null
+}
---
+++ browser/src/test/scala/de/oliver_heger/linedj/browser/media/EmptyMediumListModelSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.browser.media
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Test class for ''EmptyMediumListModel''.
+ */
+class EmptyMediumListModelSpec extends FlatSpec with Matchers {
+  "An EmptyMediumListModel" should "have size 0" in {
+    val model = new EmptyMediumListModel
+    model.size() should be(0)
+  }
+
+  it should "have type String" in {
+    val model = new EmptyMediumListModel
+    model.getType should be(classOf[String])
+  }
+
+  it should "return null display objects" in {
+    val model = new EmptyMediumListModel
+    model getDisplayObject 28 should be(null)
+  }
+
+  it should "return null value objects" in {
+    val model = new EmptyMediumListModel
+    model getValueObject 42 should be(null)
+  }
+}
