efb24adaf25462f432936970c76e4bc32c24e663
oheger <oliver.heger@oliver-heger.de>
2015-10-06 21:48:15 +0200

    Added ActorFactory class.
    This is a useful helper class for all UI components which have to create an
    actor. It manages a reference to the central ActorSystem and offers a method to
    create a new actor.

---
+++ browser/src/main/scala/de/oliver_heger/linedj/remoting/ActorFactory.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.remoting
+
+import akka.actor.{ActorRef, Props, ActorSystem}
+
+/**
+ * A class for creating actors.
+ *
+ * This class holds an instance of an ''ActorSystem'' and provides a method for
+ * creating an actor in this system. Some controller classes need to create
+ * specific actors; therefore, it makes sense to have this functionality on a
+ * central place. This also simplifies testing.
+ *
+ * @param actorSystem the current ''ActorSystem''
+ */
+class ActorFactory(val actorSystem: ActorSystem) {
+  def createActor(props: Props, name: String): ActorRef =
+    actorSystem.actorOf(props, name)
+}
---
+++ browser/src/test/scala/de/oliver_heger/linedj/remoting/ActorFactorySpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.remoting
+
+import akka.actor.{ActorRef, Props, ActorSystem}
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * Test class for ''ActorFactory''.
+ */
+class ActorFactorySpec extends FlatSpec with Matchers with MockitoSugar {
+  "An ActorFactory" should "allow creating a new actor" in {
+    val system = mock[ActorSystem]
+    val ref = mock[ActorRef]
+    val props = Props(classOf[RemoteRelayActor], "remoteHost", 1111, mock[MessageBus])
+    val Name = "MyTestActor"
+    when(system.actorOf(props, Name)).thenReturn(ref)
+
+    val factory = new ActorFactory(system)
+    factory.createActor(props, Name) should be(ref)
+  }
+}
