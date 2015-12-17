a80b7a128d3b24875091062a77fd3384ce2e4d8d
oheger <oliver.heger@oliver-heger.de>
2015-12-17 22:04:51 +0100

    Added JavaFxSharedWindowManager class.
    This is a window manager implementation which uses a shared StageFactory
    for creating windows. Therefore, multiple logic applications can run in a
    single physical JavaFX application.

---
+++ client/src/main/scala/de/oliver_heger/linedj/client/app/JavaFxSharedWindowManager.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.client.app
+
+import net.sf.jguiraffe.di.{BeanContext, BeanContextClient}
+import net.sf.jguiraffe.gui.platform.javafx.builder.window.{StageFactory, StyleSheetProvider,
+JavaFxWindowManager}
+
+object JavaFxSharedWindowManager {
+  /** The name of the bean with the stage factory. */
+  val BeanStageFactory = "lineDJ_stageFactory"
+}
+
+/**
+  * A specialized ''WindowManager'' implementation to be used by LineDJ client
+  * applications.
+  *
+  * On the LineDJ client platform multiple applications are running in a
+  * single physical JavaFX application. This has to be taken into account when
+  * windows are created. The platform uses a shared ''StageFactory'' which is
+  * created and provided by [[ClientManagementApplication]]. It has to be
+  * ensured that this factory is used by the ''WindowManager'' used by a visual
+  * client application.
+  *
+  * This class is a special window manager implementation which differs from
+  * its base class by obtaining the ''StageFactory'' in a different way: It is
+  * fetched from the bean context under a reserved name. This makes it possible
+  * to access a factory which has been stored into the context dynamically.
+  *
+  * @param styleSheetProvider the ''StyleSheetProvider''
+  */
+class JavaFxSharedWindowManager(styleSheetProvider: StyleSheetProvider) extends
+JavaFxWindowManager(styleSheetProvider, null) with BeanContextClient {
+
+  import JavaFxSharedWindowManager._
+
+  /**
+    * The stage factory which is obtained from the bean context on first
+    * access.
+    */
+  override lazy val stageFactory = fetchStageFactoryFromBeanContext()
+
+  /** The bean context. */
+  private var beanContext: BeanContext = _
+
+  /**
+    * Sets the bean context. This method is called by the dependency injection
+    * framework.
+    * @param context the bean context
+    */
+  override def setBeanContext(context: BeanContext): Unit = {
+    beanContext = context
+  }
+
+  /**
+    * Obtains the ''StageFactory'' from the ''BeanContext''.
+    * @return the bean for the ''StageFactory''
+    */
+  private def fetchStageFactoryFromBeanContext(): StageFactory =
+    beanContext.getBean(BeanStageFactory).asInstanceOf[StageFactory]
+}
---
+++ client/src/test/scala/de/oliver_heger/linedj/client/app/JavaFxSharedWindowManagerSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.client.app
+
+import net.sf.jguiraffe.di.BeanContext
+import net.sf.jguiraffe.gui.platform.javafx.builder.window.{StageFactory, StyleSheetProvider}
+import org.mockito.Mockito
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+  * Test class for ''JavaFxSharedWindowManager''.
+  */
+class JavaFxSharedWindowManagerSpec extends FlatSpec with Matchers with MockitoSugar {
+  "A JavaFxSharedWindowManager" should "pass the style sheet provider to the base class" in {
+    val provider = mock[StyleSheetProvider]
+
+    val manager = new JavaFxSharedWindowManager(provider)
+    manager.styleSheetProvider should be(provider)
+  }
+
+  it should "obtain the stage factory from the bean context" in {
+    val beanContext = mock[BeanContext]
+    val factory = mock[StageFactory]
+    doReturn(factory).when(beanContext).getBean(JavaFxSharedWindowManager.BeanStageFactory)
+
+    val manager = new JavaFxSharedWindowManager(mock[StyleSheetProvider])
+    manager setBeanContext beanContext
+    manager.stageFactory should be(factory)
+  }
+
+  it should "cache the stage factory after it has been retrieved" in {
+    val beanContext = mock[BeanContext]
+    val factory = mock[StageFactory]
+    doReturn(factory).when(beanContext).getBean(JavaFxSharedWindowManager.BeanStageFactory)
+
+    val manager = new JavaFxSharedWindowManager(mock[StyleSheetProvider])
+    manager setBeanContext beanContext
+    val currentFactory = manager.stageFactory
+    manager.stageFactory should be(currentFactory)
+    verify(beanContext, Mockito.atMost(1)).getBean(JavaFxSharedWindowManager.BeanStageFactory)
+  }
+}
