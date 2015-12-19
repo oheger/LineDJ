ab42dd5eb81eea6be6de9018584941f672d03c3c
oheger <oliver.heger@oliver-heger.de>
2015-12-19 16:51:06 +0100

    Added ClientApplication class.
    This is a base class for applications running on the LineDJ client OSGi
    platform.

---
+++ client/src/main/scala/de/oliver_heger/linedj/client/app/ClientApplication.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.client.app
+
+import net.sf.jguiraffe.gui.app.{ApplicationContext, Application}
+import org.osgi.service.component.ComponentContext
+
+object ClientApplication {
+  /** The prefix used by all beans managed by this application. */
+  val BeanPrefix = "LineDJ_"
+
+  /** The bean for the client-side actor system. */
+  val BeanActorSystem = BeanPrefix + "ActorSystem"
+
+  /** The bean for the actor factory. */
+  val BeanActorFactory = BeanPrefix + "ActorFactory"
+
+  /** The bean for the message bus. */
+  val BeanMessageBus = BeanPrefix + "MessageBus"
+
+  /** The bean for the remote message bus. */
+  val BeanRemoteMessageBus = BeanPrefix + "RemoteMessageBus"
+}
+
+/**
+  * A class representing a visual client application running on the LineDJ
+  * (OSGi) platform.
+  *
+  * This class is more or less a regular JGUIraffe application with an
+  * arbitrary main window. However, it implements functionality to obtain some
+  * central platform beans from the [[ClientApplicationContext]] and make them
+  * available in its own ''BeanContext''.
+  *
+  * This class is intended to be used as a declarative services component. It
+  * needs a static reference to a [[ClientApplicationContext]] service; as soon
+  * as this object becomes available, the required properties are fetched from
+  * there, and this application can start up. It has to register itself as
+  * service of type ''Application''; this establishes the connection to the
+  * management application.
+  *
+  * While this class is fully functional, in order to implement a valid
+  * declarative services component, it has to be extended, and the name of the
+  * configuration file has to be passed to the constructor.
+  * @author hacker
+  * @version $Id$
+  */
+class ClientApplication(configName: String) extends Application {
+
+  import ClientApplication._
+
+  /** The client application context. */
+  private var clientContext: ClientApplicationContext = _
+
+  /**
+    * Initializes the reference to the ''ClientApplicationContext''. This
+    * method is called by the SCR.
+    * @param context the ''ClientApplicationContext''
+    */
+  def initClientContext(context: ClientApplicationContext): Unit = {
+    clientContext = context
+  }
+
+  /**
+    * Returns the ''ClientApplicationContext'' used by this application. This
+    * object is available after the initialization of this application.
+    * @return the ''ClientApplicationContext''
+    */
+  def clientApplicationContext: ClientApplicationContext = clientContext
+
+  /**
+    * Activates this component. This method is called by the SCR. It starts
+    * the application.
+    * @param compContext the component context
+    */
+  def activate(compContext: ComponentContext): Unit = {
+    setConfigResourceName(configName)
+    Application.startup(this, Array.empty)
+  }
+
+  /**
+    * @inheritdoc This implementation adds some beans defined in the client
+    *             application context to this application's ''BeanContext'',
+    *             so that they are available everywhere in this application.
+    */
+  override def createApplicationContext(): ApplicationContext = {
+    val appCtx = super.createApplicationContext()
+    addBeanDuringApplicationStartup(JavaFxSharedWindowManager.BeanStageFactory, clientContext
+      .stageFactory)
+    addBeanDuringApplicationStartup(BeanActorSystem, clientContext.actorSystem)
+    addBeanDuringApplicationStartup(BeanActorFactory, clientContext.actorFactory)
+    addBeanDuringApplicationStartup(BeanMessageBus, clientContext.messageBus)
+    addBeanDuringApplicationStartup(BeanRemoteMessageBus, clientContext.remoteMessageBus)
+    appCtx
+  }
+
+  /**
+    * @inheritdoc This implementation queries the status of the remote message
+    *             bus after the UI has been initialized. This causes a status
+    *             message to be sent which triggers some further initialization
+    *             of the UI.
+    */
+  override def initGUI(appCtx: ApplicationContext): Unit = {
+    super.initGUI(appCtx)
+    clientApplicationContext.remoteMessageBus.queryServerState()
+  }
+}
---
+++ client/src/test/scala/de/oliver_heger/linedj/client/app/ClientApplicationSpec.scala
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.client.app
+
+import akka.actor.ActorSystem
+import de.oliver_heger.linedj.client.remoting.{ActorFactory, MessageBus, RemoteMessageBus}
+import net.sf.jguiraffe.gui.app.{ApplicationContext, Application}
+import net.sf.jguiraffe.gui.platform.javafx.builder.window.StageFactory
+import org.apache.commons.configuration.PropertiesConfiguration
+import org.mockito.Mockito._
+import org.osgi.service.component.ComponentContext
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''ClientApplication''.
+  */
+class ClientApplicationSpec extends FlatSpec with Matchers with MockitoSugar {
+
+  /**
+    * Queries the given application for a bean with a specific name. This bean
+    * is checked against a type. If this type is matched, the bean is returned;
+    * otherwise, an exception is thrown.
+    * @param app the application
+    * @param name the name of the bean
+    * @param m the manifest
+    * @tparam T the expected bean type
+    * @return the bean of this type
+    */
+  private def queryBean[T](app: Application, name: String)(implicit m: Manifest[T]): T = {
+    app.getApplicationContext.getBeanContext.getBean(name) match {
+      case t: T => t
+      case b =>
+        throw new AssertionError(s"Unexpected bean for name '$name': $b")
+    }
+  }
+
+  /**
+    * Creates a test application.
+    * @return the test application
+    */
+  private def createApp(): ClientApplication =
+    new ClientApplication("testconfig.xml")
+
+  /**
+    * Creates a test application and starts it so that it is correctly
+    * initialized.
+    * @return the test application and the context with mock objects
+    */
+  private def setUpApp(): (ClientApplication, ClientApplicationContext) = {
+    val app = createApp()
+    val clientContext = new ClientApplicationContextImpl
+    app initClientContext clientContext
+    val compContext = mock[ComponentContext]
+    app activate compContext
+    (app, clientContext)
+  }
+
+  "A ClientApplication" should "use the correct configuration" in {
+    val (app, _) = setUpApp()
+
+    app.getConfigResourceName should be("testconfig.xml")
+  }
+
+  it should "define a bean for the stage factory" in {
+    val (app, clientContext) = setUpApp()
+
+    queryBean[StageFactory](app, JavaFxSharedWindowManager.BeanStageFactory) should be
+    clientContext.stageFactory
+  }
+
+  it should "define a bean for the actor system" in {
+    val (app, clientContext) = setUpApp()
+
+    queryBean[ActorSystem](app, ClientApplication.BeanActorSystem) should be(clientContext
+      .actorSystem)
+  }
+
+  it should "define a bean for the actor factory" in {
+    val (app, clientContext) = setUpApp()
+
+    queryBean[ActorFactory](app, ClientApplication.BeanActorFactory) should be(clientContext
+      .actorFactory)
+  }
+
+  it should "define a bean for the message bus" in {
+    val (app, clientContext) = setUpApp()
+
+    queryBean[MessageBus](app, ClientApplication.BeanMessageBus) should be(clientContext.messageBus)
+  }
+
+  it should "define a bean for the remote message bus" in {
+    val (app, clientContext) = setUpApp()
+
+    queryBean[RemoteMessageBus](app, ClientApplication.BeanRemoteMessageBus) should be
+    clientContext.remoteMessageBus
+  }
+
+  it should "query the status of the remote message bus" in {
+    val app = createApp()
+    val appContext = mock[ApplicationContext]
+    val config = new PropertiesConfiguration
+    when(appContext.getConfiguration).thenReturn(config)
+    val clientContext = new ClientApplicationContextImpl
+    app initClientContext clientContext
+
+    app.initGUI(appContext)
+    verify(appContext).getConfiguration
+    verify(clientContext.remoteMessageBus).queryServerState()
+  }
+
+  /**
+    * An implementation of ''ClientApplicationContext'' which stores a bunch of
+    * mock objects.
+    */
+  private class ClientApplicationContextImpl extends ClientApplicationContext {
+    override val actorSystem = mock[ActorSystem]
+
+    override val remoteMessageBus = mock[RemoteMessageBus]
+
+    override val messageBus = mock[MessageBus]
+
+    override val stageFactory = mock[StageFactory]
+
+    override val actorFactory = mock[ActorFactory]
+  }
+
+}
