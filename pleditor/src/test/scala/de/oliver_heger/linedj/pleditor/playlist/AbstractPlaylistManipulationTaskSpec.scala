6b23d96fe819cc72533d1cf689e61a7372703b4a
oheger <oliver.heger@oliver-heger.de>
2016-01-01 18:18:58 +0100

    Added AbstractPlaylistManipulationTask class.
    This is a base class for action tasks that manipulate the current
    playlist.

---
+++ pleditor/src/test/scala/de/oliver_heger/linedj/pleditor/playlist/AbstractPlaylistManipulationTaskSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.playlist
+
+import org.mockito.Mockito._
+import org.scalatest.FlatSpec
+import org.scalatest.mock.MockitoSugar
+
+/**
+  * Test class for ''AbstractPlaylistManipulationTask''.
+  */
+class AbstractPlaylistManipulationTaskSpec extends FlatSpec with MockitoSugar {
+  "An AbstractPlaylistManipulationTask" should "trigger a playlist update" in {
+    val controller = mock[PlaylistController]
+    val task = new AbstractPlaylistManipulationTask(controller) with PlaylistManipulator {
+      override def updatePlaylist(context: PlaylistSelectionContext): Unit = {
+        fail("Unexpected invocation!")
+      }
+
+      override def isEnabled(context: PlaylistSelectionContext): Boolean = true
+    }
+
+    task.run()
+    verify(controller).updatePlaylist(task)
+  }
+}
