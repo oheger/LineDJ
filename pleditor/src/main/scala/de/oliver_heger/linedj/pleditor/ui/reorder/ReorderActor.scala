b1f6f929b53b2bf1ac65036f7a5b77e4001fa8c6
oheger <oliver.heger@oliver-heger.de>
2016-01-14 21:13:47 +0100

    Added ReorderActor class.
    This is a simple actor implementation which wraps a PlaylistReorderer
    service instance. Such services can be added and removed dynamically;
    for each service a ReorderActor instance is created.

---
+++ pleditor/src/main/scala/de/oliver_heger/linedj/pleditor/ui/reorder/ReorderActor.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.reorder
+
+import akka.actor.Actor
+import de.oliver_heger.linedj.client.model.SongData
+import de.oliver_heger.linedj.pleditor.spi.PlaylistReorderer
+import de.oliver_heger.linedj.pleditor.ui.reorder.ReorderActor.{ReorderResponse, ReorderRequest}
+
+object ReorderActor {
+
+  /**
+    * A message processed by [[ReorderActor]] that requests a reorder
+    * operation. The contained sequence of songs is passed to the wrapped
+    * ''PlaylistReorderer'' object.
+    * @param songs the sequence of songs to be reordered
+    * @param startIndex the first index of a song in the playlist
+    */
+  case class ReorderRequest(songs: Seq[SongData], startIndex: Int)
+
+  /**
+    * A message produced by [[ReorderActor]] representing the result of a
+    * reorder operation. Objects of this class are published on the system
+    * message bus when a reorder operation was finished successfully.
+    * @param reorderedSongs the reordered sequence of songs
+    * @param request the request which produced this response
+    */
+  case class ReorderResponse(reorderedSongs: Seq[SongData], request: ReorderRequest)
+
+}
+
+/**
+  * An actor which wraps a [[de.oliver_heger.linedj.pleditor.spi.PlaylistReorderer]]
+  * service.
+  *
+  * This actor implementation can handle a single message which triggers a
+  * reorder operation on a sequence of songs. This message is processed by
+  * invoking a wrapped ''PlaylistReorderer'' object. The resulting reordered
+  * sequence of songs is then sent back to the original sender.
+  *
+  * For each available ''PlaylistReorderer'' implementation a single instance
+  * of this actor class is created. When executing a reorder operation the
+  * corresponding actor is called. If this fails - because the reorder service
+  * is no longer available -, the actor instance is stopped.
+  *
+  * @param reorder the wrapped ''PlaylistReorderer''
+  */
+class ReorderActor(reorder: PlaylistReorderer) extends Actor {
+  override def receive: Receive = {
+    case request: ReorderRequest =>
+      sender ! ReorderResponse(reorder.reorder(request.songs), request)
+  }
+}
---
+++ pleditor/src/test/scala/de/oliver_heger/linedj/pleditor/ui/reorder/ReorderActorSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.reorder
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import de.oliver_heger.linedj.client.ActorSystemTestHelper
+import de.oliver_heger.linedj.client.model.SongData
+import de.oliver_heger.linedj.pleditor.spi.PlaylistReorderer
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike}
+
+/**
+  * Test class for ''ReorderActor''.
+  */
+class ReorderActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with ImplicitSender
+with FlatSpecLike with BeforeAndAfterAll with MockitoSugar {
+  def this() = this(ActorSystemTestHelper.createActorSystem("ReorderActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    system.shutdown()
+    ActorSystemTestHelper waitForShutdown system
+  }
+
+  /**
+    * Creates a sequence of test songs of the given length.
+    * @param count the number of test songs to create
+    * @return the sequence with test songs
+    */
+  def createSongs(count: Int): Seq[SongData] =
+    (1 to count) map (_ => mock[SongData])
+
+  "A ReorderActor" should "handle a reorder request" in {
+    val reorder = mock[PlaylistReorderer]
+    val songs = createSongs(8)
+    val orderedSongs = createSongs(7)
+    when(reorder.reorder(songs)).thenReturn(orderedSongs)
+    val request = ReorderActor.ReorderRequest(songs, 42)
+    val actor = system.actorOf(Props(classOf[ReorderActor], reorder))
+
+    actor ! request
+    expectMsg(ReorderActor.ReorderResponse(orderedSongs, request))
+  }
+}
