a6d2e329604266c8a890395c8d9b3c793752ca25
oheger <oliver.heger@oliver-heger.de>
2016-01-16 16:49:51 +0100

    Added ReorderManagerActor class.
    This actor class manages a set of ReorderActor child actors which wrap
    the currently available reorder services.

---
+++ pleditor/src/main/scala/de/oliver_heger/linedj/pleditor/ui/reorder/ReorderManagerActor.scala
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.reorder
+
+import akka.actor.SupervisorStrategy.Stop
+import akka.actor._
+import de.oliver_heger.linedj.client.remoting.MessageBus
+import de.oliver_heger.linedj.pleditor.spi.PlaylistReorderer
+import de.oliver_heger.linedj.pleditor.ui.reorder.ReorderManagerActor._
+import de.oliver_heger.linedj.utils.ChildActorFactory
+
+object ReorderManagerActor {
+
+  /**
+    * A message processed by [[ReorderManagerActor]] which requests information
+    * about the currently available reorder services. As a response to this
+    * message an [[AvailableReorderServices]] object is created.
+    */
+  case object GetAvailableReorderServices
+
+  /**
+    * A message produced by [[ReorderManagerActor]] as answer for a request for
+    * the currently available reorder services. The message contains a list of
+    * services with their (user-readable) names.
+    *
+    * @param services the list of currently available services
+    */
+  case class AvailableReorderServices(services: Seq[(PlaylistReorderer, String)])
+
+  /**
+    * A message processed by [[ReorderManagerActor]] telling it that another
+    * ''PlaylistReorderer'' object is now available. Information about this
+    * service is stored internally, so that it can be invoked.
+    *
+    * @param service the ''PlaylistReorderer'' service
+    * @param name    the name under which this service is registered
+    */
+  case class AddReorderService(service: PlaylistReorderer, name: String)
+
+  /**
+    * A message processed by [[ReorderManagerActor]] telling it that a
+    * ''PlaylistReorderer'' service is no longer available. The service is
+    * removed from the internal data structure and can no longer be invoked.
+    *
+    * @param service the removed service
+    */
+  case class RemoveReorderService(service: PlaylistReorderer)
+
+  /**
+    * A message processed by [[ReorderManagerActor]] that triggers an
+    * invocation or a reorder service. The specified service is invoked on
+    * a sequence of song data.
+    *
+    * @param service the service to be invoked
+    * @param request the request describing the reorder operation
+    */
+  case class ReorderServiceInvocation(service: PlaylistReorderer, request: ReorderActor
+  .ReorderRequest)
+
+  /**
+    * A message indicating a failed invocation of a reorder service. An object
+    * of this class is published on the message bus if an invocation did not
+    * succeed. This typically means that the reorder service is no longer
+    * available.
+    *
+    * @param service the service which could not be invoked
+    */
+  case class FailedReorderServiceInvocation(service: PlaylistReorderer)
+
+  /**
+    * An internally used data class for storing information about reorder
+    * services.
+    *
+    * @param name       the name of the service
+    * @param childActor the associated child actor
+    */
+  private case class ServiceData(name: String, childActor: ActorRef)
+
+  private class ReorderManagerActorImpl(bus: MessageBus) extends ReorderManagerActor(bus) with
+    ChildActorFactory
+
+  /**
+    * Creates a ''Props'' object for creating actor instances.
+    *
+    * @param bus the ''MessageBus''
+    * @return the creation ''Props''
+    */
+  def apply(bus: MessageBus): Props =
+    Props(classOf[ReorderManagerActorImpl], bus)
+}
+
+/**
+  * An actor implementation which manages all available reorder services.
+  *
+  * Service objects implementing the
+  * [[de.oliver_heger.linedj.pleditor.spi.PlaylistReorderer]] interface can be
+  * added and removed dynamically. This actor is responsible for managing the
+  * services available and processing requests to reorder parts of the
+  * playlist. This is achieved by creating [[ReorderActor]] child actors for
+  * each reorder service that is currently available.
+  *
+  * This actor class supports messages for adding and removing reorder
+  * services. Such messages are processed by updating the child actors
+  * accordingly. A message for executing a reorder operation is also supported.
+  * It is delegated to the corresponding child actor, and the result is
+  * published on the system message bus.
+  *
+  * @param bus the ''MessageBus''
+  */
+class ReorderManagerActor(bus: MessageBus) extends Actor with ActorLogging {
+  this: ChildActorFactory =>
+
+  /**
+    * A map storing information about the currently available reorder
+    * services.
+    */
+  private val reorderServices = collection.mutable.Map.empty[PlaylistReorderer, ServiceData]
+
+  /**
+    * This actor uses a supervision strategy that stops failing child actors.
+    * This means that every time a reorder operation fails - because the
+    * wrapped reorder service is no longer available -, the child actor is
+    * stopped, and a ''Terminated'' message is received. This is handled by
+    * removing the corresponding reorder service.
+    */
+  override val supervisorStrategy = OneForOneStrategy() {
+    case _: Exception =>
+      Stop
+  }
+
+  override def receive: Receive = {
+    case GetAvailableReorderServices =>
+      sender ! AvailableReorderServices(availableReorderServices())
+
+    case AddReorderService(service, name) =>
+      val child = createChildActor(Props(classOf[ReorderActor], service))
+      context watch child
+      reorderServices += service -> ServiceData(name, child)
+
+    case RemoveReorderService(service) =>
+      val optData = reorderServices remove service
+      optData foreach { d =>
+        context unwatch d.childActor
+        context stop d.childActor
+      }
+
+    case ReorderServiceInvocation(service, request) =>
+      handleServiceInvocation(service, request, reorderServices get service)
+
+    case response: ReorderActor.ReorderResponse =>
+      bus publish response
+
+    case t: Terminated =>
+      handleChildActorTermination(t.actor)
+  }
+
+  /**
+    * Returns a list with meta data about all currently available reorder
+    * services.
+    *
+    * @return the list with service information
+    */
+  private def availableReorderServices(): List[(PlaylistReorderer, String)] =
+    reorderServices.toList map (e => (e._1, e._2.name))
+
+  /**
+    * Handles a request for invoking a reorder service. The invocation may fail
+    * if no corresponding service data can be obtained.
+    *
+    * @param service the service to be invoked
+    * @param request the request to be executed
+    * @param optData an option with the associated service data
+    */
+  private def handleServiceInvocation(service: PlaylistReorderer, request: ReorderActor
+  .ReorderRequest, optData: Option[ServiceData]): Unit = {
+    optData match {
+      case Some(data) =>
+        data.childActor ! request
+
+      case None =>
+        bus publish FailedReorderServiceInvocation(service)
+    }
+  }
+
+  /**
+    * Handles a message that a child actor has been terminated. This typically
+    * happens when a reorder operation is executed, but the reorder service is
+    * suddenly no longer available. In this case, an error message is sent on
+    * the message bus, and the service is removed from the internal list.
+    *
+    * @param childActor the affected child actor
+    */
+  private def handleChildActorTermination(childActor: ActorRef): Unit = {
+    log.warning("Invocation of a child actor failed!")
+    val item = reorderServices.find(p => p._2.childActor == childActor)
+    item foreach { p =>
+      val service = p._1
+      reorderServices remove service
+      bus publish FailedReorderServiceInvocation(service)
+    }
+  }
+}
---
+++ pleditor/src/test/scala/de/oliver_heger/linedj/pleditor/ui/reorder/ReorderManagerActorSpec.scala
@@ -0,0 +1,263 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.reorder
+
+import akka.actor._
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
+import de.oliver_heger.linedj.SupervisionTestActor
+import de.oliver_heger.linedj.client.ActorSystemTestHelper
+import de.oliver_heger.linedj.client.model.SongData
+import de.oliver_heger.linedj.client.remoting.MessageBus
+import de.oliver_heger.linedj.pleditor.spi.PlaylistReorderer
+import de.oliver_heger.linedj.utils.ChildActorFactory
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object ReorderManagerActorSpec {
+  /** A name for a reorder service. */
+  private val ServiceName = "Test reorder service"
+}
+
+/**
+  * Test class for ''ReorderManagerActor''.
+  */
+class ReorderManagerActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+
+  import ReorderManagerActorSpec._
+
+  def this() = this(ActorSystemTestHelper createActorSystem "ReorderManagerActorSpec")
+
+  override protected def afterAll(): Unit = {
+    system.shutdown()
+    ActorSystemTestHelper waitForShutdown system
+  }
+
+  "A ReorderManagerActor" should "create correct Props" in {
+    val bus = mock[MessageBus]
+
+    val props = ReorderManagerActor(bus)
+    classOf[ReorderManagerActor] isAssignableFrom props.actorClass() shouldBe true
+    classOf[ChildActorFactory] isAssignableFrom props.actorClass() shouldBe true
+    props.args should have length 1
+    props.args.head should be(bus)
+  }
+
+  it should "have no reorder services initially" in {
+    val helper = new ReorderManagerActorTestHelper
+
+    helper.actor ! ReorderManagerActor.GetAvailableReorderServices
+    expectMsgType[ReorderManagerActor.AvailableReorderServices].services shouldBe 'empty
+  }
+
+  it should "allow adding reorder services" in {
+    val reorder = mock[PlaylistReorderer]
+    val helper = new ReorderManagerActorTestHelper
+
+    helper.addReorderService(reorder, ServiceName)
+    helper.actor ! ReorderManagerActor.GetAvailableReorderServices
+    val msg = expectMsgType[ReorderManagerActor.AvailableReorderServices]
+    msg.services should have length 1
+    msg.services should contain(reorder, ServiceName)
+  }
+
+  it should "allow removing reorder services" in {
+    val reorder = mock[PlaylistReorderer]
+    val reorderOther = mock[PlaylistReorderer]
+    val helper = new ReorderManagerActorTestHelper
+    helper.addReorderService(reorder, ServiceName)
+    helper.addReorderService(reorderOther, ServiceName + "_other")
+
+    helper removeReorderService reorder
+    helper.actor ! ReorderManagerActor.GetAvailableReorderServices
+    val msg = expectMsgType[ReorderManagerActor.AvailableReorderServices]
+    msg.services should have length 1
+    msg.services should not contain(reorder, ServiceName)
+  }
+
+  it should "stop the associated child actor when a service is removed" in {
+    val reorder = mock[PlaylistReorderer]
+    val helper = new ReorderManagerActorTestHelper
+    helper.addReorderService(reorder, ServiceName)
+
+    val probe = TestProbe()
+    val child = helper removeReorderService reorder
+    probe watch child.ref
+    probe.expectMsgType[Terminated].actor should be(child.ref)
+  }
+
+  it should "handle a remove request for an unknown service gracefully" in {
+    val helper = new ReorderManagerActorTestHelper
+
+    helper send ReorderManagerActor.RemoveReorderService(mock[PlaylistReorderer])
+  }
+
+  /**
+    * Creates a message for invoking the specified reorder service.
+    *
+    * @param reorder the reorder service
+    * @return the message for invoking this service
+    */
+  private def createReorderMessage(reorder: PlaylistReorderer): ReorderManagerActor
+  .ReorderServiceInvocation =
+    ReorderManagerActor.ReorderServiceInvocation(reorder,
+      ReorderActor.ReorderRequest(createSongList(), 2))
+
+  /**
+    * Creates a sequence with some test songs.
+    *
+    * @return the sequence with test songs
+    */
+  private def createSongList(): List[SongData] =
+    List(mock[SongData], mock[SongData])
+
+  it should "allow invoking a reorder service" in {
+    val reorder = mock[PlaylistReorderer]
+    val invocation = createReorderMessage(reorder)
+    val response = ReorderActor.ReorderResponse(createSongList(), invocation.request)
+    val helper = new ReorderManagerActorTestHelper
+    val child = helper.addReorderService(reorder, ServiceName)
+
+    helper send invocation
+    child.expectMsg(invocation.request)
+    helper send response
+    helper expectMessageOnBus response
+  }
+
+  it should "send an error response for an unknown reorder service" in {
+    val reorder = mock[PlaylistReorderer]
+    val helper = new ReorderManagerActorTestHelper
+
+    helper send createReorderMessage(reorder)
+    helper expectMessageOnBus ReorderManagerActor.FailedReorderServiceInvocation(reorder)
+  }
+
+  it should "react on a failed invocation via a child actor" in {
+    val reorder = mock[PlaylistReorderer]
+    val helper = new ReorderManagerActorTestHelper
+    val child = helper.addReorderService(reorder, ServiceName)
+
+    system stop child.ref
+    awaitCond {
+      helper.actor ! ReorderManagerActor.GetAvailableReorderServices
+      expectMsgType[ReorderManagerActor.AvailableReorderServices].services.isEmpty
+    }
+    helper expectMessageOnBus ReorderManagerActor.FailedReorderServiceInvocation(reorder)
+  }
+
+  it should "use a supervision strategy that stops a failing child actor" in {
+    val helper = new ReorderManagerActorTestHelper
+    val strategy = helper.actor.underlyingActor.supervisorStrategy
+
+    strategy shouldBe a[OneForOneStrategy]
+    val supervisionTestActor = SupervisionTestActor(system, strategy, Props(new Actor {
+      override def receive: Receive = {
+        case ServiceName =>
+          throw new RuntimeException("Service not available!")
+      }
+    }))
+    val probe = TestProbe()
+    val childActor = supervisionTestActor.underlyingActor.childActor
+    probe watch childActor
+
+    childActor ! ServiceName
+    probe.expectMsgType[Terminated]
+  }
+
+  /**
+    * A test helper class which collects required mock and helper objects
+    * needed by the actor under test.
+    */
+  class ReorderManagerActorTestHelper {
+    /** The mock message bus. */
+    val messageBus = mock[MessageBus]
+
+    /** A map with managed services and the corresponding child actors. */
+    val managedServices = collection.mutable.Map.empty[PlaylistReorderer, TestProbe]
+
+    /** Test reference to the test actor. */
+    val actor = TestActorRef[ReorderManagerActor](createProps(messageBus))
+
+    /**
+      * Sends the specified message (synchronously) to the test actor.
+      *
+      * @param msg the message to be sent
+      */
+    def send(msg: Any): Unit = {
+      actor receive msg
+    }
+
+    /**
+      * Makes the specified reorder service available to the test actor. The
+      * test probe returned by the child actor factory is returned.
+      *
+      * @param reorder the reorder service
+      * @param name    the name of the service
+      * @return the probe representing the wrapping child actor
+      */
+    def addReorderService(reorder: PlaylistReorderer, name: String): TestProbe = {
+      send(ReorderManagerActor.AddReorderService(reorder, name))
+      managedServices(reorder)
+    }
+
+    /**
+      * Tells the test actor that the specified reorder service is gone. The
+      * test probe representing the wrapping child actor is returned.
+      *
+      * @param reorder the reorder service to be removed
+      * @return the probe representing the wrapping child actor
+      */
+    def removeReorderService(reorder: PlaylistReorderer): TestProbe = {
+      send(ReorderManagerActor.RemoveReorderService(reorder))
+      managedServices.remove(reorder).get
+    }
+
+    /**
+      * Checks whether the specified message has been published on the
+      * message bus.
+      *
+      * @param msg the expected message
+      */
+    def expectMessageOnBus(msg: Any): Unit = {
+      verify(messageBus).publish(msg)
+    }
+
+    /**
+      * Creates a ''Props'' object for creating a test actor instance. Here a
+      * ''ChildActorFactory'' is used which stores the association to the
+      * reorder service and returns a test probe.
+      *
+      * @param bus the message bus
+      * @return the ''Props'' for the test actor
+      */
+    private def createProps(bus: MessageBus): Props = {
+      Props(new ReorderManagerActor(bus) with ChildActorFactory {
+        override def createChildActor(p: Props): ActorRef = {
+          p.actorClass() should be(classOf[ReorderActor])
+          p.args should have length 1
+          val reorder = p.args.head.asInstanceOf[PlaylistReorderer]
+          managedServices should not contain reorder
+          val probe = TestProbe()
+          managedServices += reorder -> probe
+          probe.ref
+        }
+      })
+    }
+  }
+
+}
