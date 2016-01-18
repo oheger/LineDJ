1ee3c531a43a31e4fc6c70036f027b879a54a7cb
oheger <oliver.heger@oliver-heger.de>
2016-01-18 21:45:25 +0100

    Added OpenReorderDlgCommand.
    This class is responsible for opening a dialog for reordering parts of
    the playlist.

---
+++ pleditor/src/main/scala/de/oliver_heger/linedj/pleditor/ui/playlist/OpenReorderDlgCommand.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.playlist
+
+import net.sf.jguiraffe.gui.app.{ApplicationBuilderData, OpenWindowCommand}
+import net.sf.jguiraffe.gui.builder.components.model.TableHandler
+import net.sf.jguiraffe.locators.Locator
+
+object OpenReorderDlgCommand {
+  /** The name of the property under which the songs to be reordered are stored. */
+  val ReorderSongsPropertyKey = "reorderSongs"
+
+  /**
+    * The name of the property under which the start index of the songs to be
+    * reordered is stored.
+    */
+  val StartIndexPropertyKey = "reorderStartIndex"
+}
+
+/**
+  * A command class which opens the dialog for reordering parts of the
+  * playlist.
+  *
+  * This is a specialized command for opening a dialog window which ensures
+  * that information about the songs to be reordered is stored in the Jelly
+  * context. It also implements the [[PlaylistManipulator]] trait to
+  * determine when the command is enabled.
+  *
+  * @param scriptLocator the locator to the script to be interpreted
+  * @param tabHandler    the handler for the playlist table
+  */
+class OpenReorderDlgCommand(scriptLocator: Locator, tabHandler: TableHandler) extends
+  OpenWindowCommand(scriptLocator) with PlaylistManipulator {
+
+  import OpenReorderDlgCommand._
+
+  /**
+    * @inheritdoc This is just a dummy. This command does not modify the
+    *             playlist itself.
+    */
+  override def updatePlaylist(context: PlaylistSelectionContext): Unit = {}
+
+  /**
+    * @inheritdoc This implementation returns '''true''' if and only if a
+    *             selection exists.
+    */
+  override def isEnabled(context: PlaylistSelectionContext): Boolean = context.hasSelection
+
+  /**
+    * @inheritdoc This implementation stores information about the currently
+    *             selected songs in the builder data.
+    */
+  override protected[playlist] def prepareBuilderData(builderData: ApplicationBuilderData): Unit = {
+    import collection.JavaConversions._
+    val context = new PlaylistSelectionContext(tabHandler)
+    val selection = tabHandler.getModel.subList(context.minimumSelectionIndex, context
+      .maximumSelectionIndex + 1)
+    builderData.addProperty(ReorderSongsPropertyKey, selection.toList)
+    builderData.addProperty(StartIndexPropertyKey, context.minimumSelectionIndex)
+  }
+}
---
+++ pleditor/src/test/scala/de/oliver_heger/linedj/pleditor/ui/playlist/OpenReorderDlgCommandSpec.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.playlist
+
+import de.oliver_heger.linedj.client.model.SongData
+import net.sf.jguiraffe.gui.app.ApplicationBuilderData
+import net.sf.jguiraffe.gui.builder.components.model.TableHandler
+import net.sf.jguiraffe.locators.URLLocator
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{Matchers, FlatSpec}
+
+object OpenReorderDlgCommandSpec {
+  /** The test locator. */
+  private val Locator = URLLocator getInstance "http://line-dj-test.org"
+}
+
+/**
+  * Test class for ''OpenReorderDlgCommand''.
+  */
+class OpenReorderDlgCommandSpec extends FlatSpec with Matchers with MockitoSugar {
+
+  import OpenReorderDlgCommandSpec._
+
+  "An OpenReorderDlgCommand" should "store the script locator" in {
+    val command = new OpenReorderDlgCommand(Locator, mock[TableHandler])
+
+    command.getLocator should be(Locator)
+  }
+
+  it should "store information about the reorder operation in the builder data" in {
+    val builderData = mock[ApplicationBuilderData]
+    val handler = mock[TableHandler]
+    val model = java.util.Arrays.asList(mock[SongData], mock[SongData], mock[SongData],
+      mock[SongData], mock[SongData], mock[SongData], mock[SongData], mock[SongData])
+    doReturn(model).when(handler).getModel
+    when(handler.getSelectedIndices).thenReturn(Array(1, 3, 4, 6))
+
+    def song(idx: Int): SongData = model get idx
+    val expectedSongs = List(song(1), song(2), song(3), song(4), song(5), song(6))
+    val command = new OpenReorderDlgCommand(Locator, handler)
+
+    command prepareBuilderData builderData
+    verify(builderData).addProperty("reorderSongs", expectedSongs)
+    verify(builderData).addProperty("reorderStartIndex", 1)
+  }
+
+  it should "have a dummy implementation for updatePlaylist()" in {
+    val context = mock[PlaylistSelectionContext]
+    val command = new OpenReorderDlgCommand(Locator, mock[TableHandler])
+
+    command.updatePlaylist(context)
+    verifyZeroInteractions(context)
+  }
+
+  it should "be enabled if a selection exists" in {
+    val context = mock[PlaylistSelectionContext]
+    when(context.hasSelection).thenReturn(true)
+    val command = new OpenReorderDlgCommand(Locator, mock[TableHandler])
+
+    command isEnabled context shouldBe true
+  }
+
+  it should "be disabled if no selection exists" in {
+    val context = mock[PlaylistSelectionContext]
+    when(context.hasSelection).thenReturn(false)
+    val command = new OpenReorderDlgCommand(Locator, mock[TableHandler])
+
+    command isEnabled context shouldBe false
+  }
+}
