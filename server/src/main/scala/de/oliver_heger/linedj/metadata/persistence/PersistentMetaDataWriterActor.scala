4842b7715d4b7e9a9a560312b9c943fab4d31564
oheger <oliver.heger@oliver-heger.de>
2016-03-23 21:15:08 +0100

    Added PersistentMetaDataWriterActor.
    The actor listens for meta data chunks for media with unresolved
    files. When a certain number of new elements has been retrieved, the
    meta data file for this medium is written to disk.
    
    Writing the file is done via akka streams.

---
+++ server/src/main/scala/de/oliver_heger/linedj/metadata/persistence/PersistentMetaDataWriterActor.scala
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.metadata.persistence
+
+import java.nio.file.Path
+
+import akka.actor.{Actor, ActorContext, ActorLogging, ActorRef}
+import akka.event.LoggingAdapter
+import akka.stream.scaladsl.{FileIO, Source}
+import akka.stream.{ActorMaterializer, IOResult}
+import akka.util.ByteString
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.media.MediumID
+import de.oliver_heger.linedj.metadata.persistence.PersistentMetaDataWriterActor.{MediumData,
+ProcessMedium, StreamOperationComplete}
+import de.oliver_heger.linedj.metadata.{GetMetaData, MediaMetaData, MetaDataChunk}
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+
+object PersistentMetaDataWriterActor {
+
+  /**
+    * A message processed by [[PersistentMetaDataWriterActor]] that tells it to
+    * write meta data files for a specific medium. This message causes the
+    * actor to register for this medium and to handle notifications about
+    * chunks of meta data.
+    *
+    * @param mediumID        the ID of the affected medium
+    * @param target          the path to the meta data file to be written
+    * @param metaDataManager the meta data manager actor
+    * @param uriPathMapping  a URI to path mapping
+    * @param resolvedSize    the number of resolved files on this medium
+    */
+  case class ProcessMedium(mediumID: MediumID, target: Path, metaDataManager: ActorRef,
+                           uriPathMapping: Map[String, FileData], resolvedSize: Int)
+
+  /**
+    * An internally used message that is passed to this actor when a stream
+    * operation has finished. Only a single stream operation should be done at
+    * any given time. Therefore, when an operation is finished a notification
+    * has to be sent to trigger the next operation if one is pending.
+    */
+  private[persistence] case object StreamOperationComplete
+
+  /**
+    * A case class storing information about a medium to be processed. This
+    * class keeps track about the number of elements that have already been
+    * written; so it can be determined when the file for this medium has to be
+    * written again.
+    *
+    * @param process         the ''ProcessMedium'' message for this medium
+    * @param elementsWritten the number of elements that have been written
+    * @param elements        the elements recorded for this medium
+    */
+  private case class MediumData(process: ProcessMedium, elementsWritten: Int,
+                                elements: Map[String, MediaMetaData])
+
+}
+
+/**
+  * An actor that produces meta data files for media while they are processed
+  * by the meta data manager.
+  *
+  * This actor comes into play when media are detected for which no or
+  * incomplete meta data files exist. In this case, the files on the medium are
+  * scanned to extract meta data. When this is done (or if a sufficient number
+  * of files has been processed) the obtained meta data should be written out.
+  *
+  * This actor is notified about media that fall under this category. It
+  * registers itself as listener for these media and handles update
+  * notifications about incoming meta data. When a configurable number of files
+  * on a medium has been processed the meta data file for this medium is
+  * written to disk.
+  *
+  * @param blockSize the number of meta data files processed on a medium
+  *                  before the meta data file is written
+  */
+class PersistentMetaDataWriterActor(blockSize: Int,
+                                    private[persistence] val resultHandler:
+                                    FutureIOResultHandler) extends Actor with ActorLogging {
+  def this(blockSize: Int) = this(blockSize, new FutureIOResultHandler)
+
+  /** The object for materializing streams. */
+  private implicit val materializer = ActorMaterializer()
+
+  /** The JSON converter for meta data. */
+  private val metaDataConverter = new MetaDataJsonConverter
+
+  /**
+    * Stores information about media which are currently processed.
+    */
+  private var mediaInProgress = Map.empty[MediumID, MediumData]
+
+  /**
+    * A map that stores the media and corresponding data that are pending to be
+    * written to disk.
+    */
+  private var mediaToBeWritten = Map.empty[MediumID, MediumData]
+
+  /**
+    * Flag whether currently a write operation is in progress. This is used to
+    * allow only a single write operation at any time.
+    */
+  private var writeInProgress = false
+
+  override def receive: Receive = {
+    case p: ProcessMedium =>
+      p.metaDataManager ! GetMetaData(p.mediumID, registerAsListener = true)
+      mediaInProgress += p.mediumID -> MediumData(p, p.resolvedSize, Map.empty)
+
+    case c: MetaDataChunk =>
+      mediaInProgress.get(c.mediumID).foreach { mediumData =>
+        val nextElements = mediumData.elements ++ c.data
+        val nextData = if (nextElements.size - mediumData.elementsWritten >= blockSize || c
+          .complete) {
+          val d = mediumData.copy(elementsWritten = nextElements.size, elements = nextElements)
+          mediaToBeWritten += c.mediumID -> d
+          startWriteOperationIfPossible()
+          d
+        } else mediumData.copy(elements = nextElements)
+
+        if (c.complete) mediaInProgress -= c.mediumID
+        else mediaInProgress += (c.mediumID -> nextData)
+      }
+
+    case StreamOperationComplete =>
+      writeInProgress = false
+      startWriteOperationIfPossible()
+  }
+
+  /**
+    * Checks if a write operation can be started. If so, the write is
+    * triggered.
+    */
+  private def startWriteOperationIfPossible(): Unit = {
+    if (!writeInProgress && mediaToBeWritten.nonEmpty) {
+      val mid = mediaToBeWritten.keys.head
+      val result = triggerWriteMetaDataFile(mediaToBeWritten(mid))
+      resultHandler.handleFutureResult(context, result, self, log)
+      writeInProgress = true
+      mediaToBeWritten -= mid
+    }
+  }
+
+  /**
+    * Triggers writing of a file with meta data. The specified map with meta
+    * data is written into the target file for the given medium.
+    *
+    * @param mediumData the object with data about the medium in question
+    * @return a future for the result of the write operation
+    */
+  private def triggerWriteMetaDataFile(mediumData: MediumData): Future[IOResult] = {
+    log.info("Writing meta data file {}.", mediumData.process.target)
+    val source = Source(mediumData.elements)
+    val sourceCount = Source(0 until mediumData.elements.size)
+    val result = source.zip(sourceCount)
+      .map(e => ByteString(processElement(e._1._1, mediumData.process.uriPathMapping(e._1._1), e
+        ._1._2, e._2)))
+      .concat(Source.single(ByteString("\n]\n", "UTF-8")))
+      .runWith(FileIO.toFile(mediumData.process.target.toFile))
+    result
+  }
+
+  /**
+    * Processes a single media file in the stream for writing media files.
+    * The element is an entry of the map from a meta data chunk. It is
+    * converted to a JSON representation in binary form. This method also
+    * ensures that separators between elements are set correctly.
+    *
+    * @param uri   the URI of the song
+    * @param file  the ''FileData'' for the song
+    * @param data  the meta data for song
+    * @param index the index in the stream of elements
+    * @return a JSON representation for this song
+    */
+  private def processElement(uri: String, file: FileData, data: MediaMetaData, index: Int):
+  String = {
+    val buf = new StringBuilder
+    if (index == 0) {
+      buf ++= "[\n"
+    } else {
+      buf ++= ",\n"
+    }
+    buf ++= metaDataConverter.convert(uri, file.path, data)
+    buf.toString()
+  }
+}
+
+/**
+  * An internally used helper class that handles the future with the IO result
+  * produced by a stream operation.
+  *
+  * The main purpose of this class is to notify the owning actor when writing
+  * of the stream is complete. Then the next write operation can be triggered.
+  */
+private class FutureIOResultHandler {
+  /**
+    * Handles the specified future IO result.
+    *
+    * @param context      the actor context
+    * @param futureResult the future IO result
+    * @param actor        the owning actor
+    * @param log          the logger for producing log output
+    */
+  def handleFutureResult(context: ActorContext, futureResult: Future[IOResult],
+                         actor: ActorRef, log: LoggingAdapter): Unit = {
+    import context._
+    futureResult onComplete { r =>
+      onResultComplete(r.flatMap(_.status), actor, log)
+    }
+  }
+
+  /**
+    * Callback method invoked when the future result completes. The method is
+    * invoked with one of the involved ''Try'' objects: Either the
+    * ''Try[IOResult]'' or the ''Try[Done]'' from the ''IOResult''. There is
+    * need to distinguish here because we are only interested in failures.
+    *
+    * @param result the completed result
+    * @param actor  the owning actor
+    * @param log    the logger for producing log output
+    */
+  protected def onResultComplete(result: Try[_], actor: ActorRef, log: LoggingAdapter): Unit = {
+    actor ! PersistentMetaDataWriterActor.StreamOperationComplete
+    result match {
+      case Failure(ex) =>
+        log.error(ex, "Stream operation caused an exception!")
+      case Success(_) =>
+        log.info("Meta data file written successfully.")
+    }
+  }
+}
---
+++ server/src/test/scala/de/oliver_heger/linedj/metadata/persistence/PersistentMetaDataWriterActorSpec.scala
@@ -0,0 +1,390 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.metadata.persistence
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path, Paths}
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.event.LoggingAdapter
+import akka.stream.{ActorMaterializer, IOResult}
+import akka.stream.scaladsl.{FileIO, Source}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
+import akka.util.ByteString
+import de.oliver_heger.linedj.FileTestHelper
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.media.MediumID
+import de.oliver_heger.linedj.metadata.persistence.PersistentMetaDataWriterActor.ProcessMedium
+import de.oliver_heger.linedj.metadata.persistence.parser.{JSONParser, MetaDataParser, ParserImpl}
+import de.oliver_heger.linedj.metadata.{GetMetaData, MediaMetaData, MetaDataChunk,
+MetaDataProcessingResult}
+import org.mockito.Mockito._
+import org.mockito.Matchers.{anyString, eq => eqArg}
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.{Await, Promise}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+object PersistentMetaDataWriterActorSpec {
+  /** A test medium ID. */
+  private val TestMedium = MediumID("testMedium", Some("Test"))
+
+  /** Another medium ID. */
+  private val OtherMedium = MediumID("otherMedium", None)
+
+  /** A mapping from URIs to paths. */
+  private val UriPathMapping = createUriPathMapping()
+
+  /** The block size used by tests. */
+  private val BlockSize = 20
+
+  /** A JSON parser used by tests. */
+  private val Parser = new MetaDataParser(ParserImpl, JSONParser.jsonParser(ParserImpl))
+
+  /**
+    * Generates a meta data object based on the given index.
+    *
+    * @param index the index
+    * @return the meta data object
+    */
+  private def metaData(index: Int): MediaMetaData =
+    MediaMetaData(title = Some("TestSöng" + index))
+
+  /**
+    * Generates the URI for a test song based on the given index.
+    *
+    * @param index the index
+    * @return the URI of this test song
+    */
+  private def uri(index: Int): String = "song://TestSong" + index
+
+  /**
+    * Generates the path for a test song based on the given index.
+    *
+    * @param index the index
+    * @return the path of this test song
+    */
+  private def path(index: Int): Path = Paths.get("testPath" + index + ".mp3")
+
+  /**
+    * Generates a chunk of meta data containing test songs in a given index
+    * range.
+    *
+    * @param startIndex the start index (inclusive)
+    * @param endIndex   the end index (inclusive)
+    * @param complete   the complete flag
+    * @param mediumID   the ID of the medium
+    * @return the chunk of meta data
+    */
+  private def chunk(startIndex: Int, endIndex: Int, complete: Boolean, mediumID: MediumID =
+  TestMedium): MetaDataChunk = {
+    val songMapping = (startIndex to endIndex) map (i => (uri(i), metaData(i)))
+    MetaDataChunk(mediumID, songMapping.toMap, complete)
+  }
+
+  /**
+    * Generates a global mapping from URIs to files.
+    *
+    * @return the URI to path mapping
+    */
+  private def createUriPathMapping(): Map[String, FileData] =
+    (1 to 100).map(i => (uri(i), FileData(path(i), i))).toMap
+}
+
+/**
+  * Test class for ''PersistentMetaDataWriterActor''.
+  */
+class PersistentMetaDataWriterActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers with FileTestHelper with
+  MockitoSugar {
+
+  import PersistentMetaDataWriterActorSpec._
+
+  def this() = this(ActorSystem("PersistentMetaDataWriterActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+    tearDownTestFile()
+  }
+
+  "A PersistentMetaDataWriterActor" should "register at the meta data manager" in {
+    val msg = PersistentMetaDataWriterActor.ProcessMedium(TestMedium, createPathInDirectory("data" +
+      ".mdt"), testActor, Map.empty, 0)
+    val actor = system.actorOf(Props(classOf[PersistentMetaDataWriterActor], 50))
+
+    actor ! msg
+    expectMsg(GetMetaData(TestMedium, registerAsListener = true))
+  }
+
+  /**
+    * Creates a test reference to the test actor.
+    *
+    * @return the test reference
+    */
+  private def createTestActorRef(): TestActorRef[PersistentMetaDataWriterActor] =
+    TestActorRef[PersistentMetaDataWriterActor](Props
+    (classOf[PersistentMetaDataWriterActor], 10))
+
+  it should "create a default FutureIOResultHandler" in {
+    val actor = createTestActorRef()
+
+    actor.underlyingActor.resultHandler should not be null
+  }
+
+  it should "use a result handler that logs failures when creating the future" in {
+    val log = mock[LoggingAdapter]
+    val promise = Promise[IOResult]()
+    val ex = new Exception("Test exception")
+    val actor = createTestActorRef()
+    val writerActor = actor.underlyingActor
+
+    writerActor.resultHandler.handleFutureResult(writerActor.context, promise.future, testActor,
+      log)
+    promise complete Failure(ex)
+    expectMsg(PersistentMetaDataWriterActor.StreamOperationComplete)
+    verify(log).error(eqArg(ex), anyString())
+  }
+
+  /**
+    * Creates an ''IOResult'' for a failed operation. Unfortunately,
+    * ''IOResult'' objects cannot be created directly nor mocked. Therefore, a
+    * real operation has to be executed.
+    *
+    * @return the ''IOResult''
+    */
+  private def createFailedIOResult(): IOResult = {
+    implicit val mat = ActorMaterializer()
+    val path = createPathInDirectory("Some path").resolve("someFile.tst")
+    val futureResult = Source.single(ByteString("Test")).runWith(FileIO.toFile(path.toFile))
+    Await.result(futureResult, 5.seconds)
+  }
+
+  it should "use a result handler that logs failed IOResults" in {
+    val log = mock[LoggingAdapter]
+    val ioResult = createFailedIOResult()
+    val promise = Promise[IOResult]()
+    val ex = ioResult.status.asInstanceOf[Failure[IOResult]].exception
+    val actor = createTestActorRef()
+    val writerActor = actor.underlyingActor
+
+    writerActor.resultHandler.handleFutureResult(writerActor.context, promise.future, testActor,
+      log)
+    promise complete Success(ioResult)
+    expectMsg(PersistentMetaDataWriterActor.StreamOperationComplete)
+    verify(log).error(eqArg(ex), anyString())
+  }
+
+  /**
+    * Checks whether the specified sequence of results contains all expected
+    * results.
+    *
+    * @param results    the sequence with results
+    * @param startIndex the start index (inclusive)
+    * @param endIndex   the end index (inclusive)
+    * @param mid        the medium ID
+    */
+  private def checkProcessingResults(results: Seq[MetaDataProcessingResult], startIndex: Int,
+                                     endIndex: Int, mid: MediumID = TestMedium): Unit = {
+    val expResults = (startIndex to endIndex) map (i => MetaDataProcessingResult(path(i), mid,
+      uri(i), metaData(i)))
+    results should contain theSameElementsAs expResults
+  }
+
+  /**
+    * Creates a test actor instance and sends it a ''ProcessMedium'' message.
+    *
+    * @param handler      the handler to be used
+    * @param target       the target file to be written
+    * @param mid          the medium ID
+    * @param resolvedSize the resolved size
+    * @return the test actor
+    */
+  private def createActorForMedium(handler: FutureIOResultHandler, target: Path, mid: MediumID =
+  TestMedium, resolvedSize: Int = 0): ActorRef = {
+    val actor = system.actorOf(Props(classOf[PersistentMetaDataWriterActor], BlockSize, handler))
+    actor ! processMessage(target, mid, resolvedSize)
+    actor
+  }
+
+  /**
+    * Returns a message that triggers the processing of a medium.
+    *
+    * @param target       the target file
+    * @param mid          the medium ID
+    * @param resolvedSize the number of resolved files
+    * @return the message
+    */
+  private def processMessage(target: Path, mid: MediumID, resolvedSize: Int): ProcessMedium =
+    PersistentMetaDataWriterActor.ProcessMedium(mid, target, TestProbe().ref, UriPathMapping,
+      resolvedSize)
+
+  /**
+    * Parses a file with meta data and returns all extracted results.
+    *
+    * @param file the file to be parsed
+    * @param mid  the medium ID
+    * @return the extracted results
+    */
+  private def parseMetaData(file: Path, mid: MediumID = TestMedium):
+  Seq[MetaDataProcessingResult] = {
+    val json = new String(Files.readAllBytes(file), StandardCharsets.UTF_8)
+    val (results, failure) = Parser.processChunk(json, mid, lastChunk = true, None)
+    failure shouldBe 'empty
+    results
+  }
+
+  it should "write a meta data file when sufficient meta data is available" in {
+    val handler = new TestFutureResultHandler(new CountDownLatch(1))
+    val target = createPathInDirectory("meta.mdt")
+    val actor = createActorForMedium(handler, target, resolvedSize = 10)
+
+    actor ! chunk(1, 10 + BlockSize, complete = false)
+    handler.await()
+    checkProcessingResults(parseMetaData(target), 1, 10 + BlockSize)
+  }
+
+  it should "not write a file before sufficient meta data is available" in {
+    val handler = new TestFutureResultHandler(new CountDownLatch(1))
+    val target = createPathInDirectory("other.mdt")
+    val actor = createActorForMedium(handler, createPathInDirectory("meta.mdt"), resolvedSize = 0)
+    actor ! processMessage(target, OtherMedium, 0)
+
+    actor ! chunk(1, BlockSize - 1, complete = false, mediumID = OtherMedium)
+    actor ! chunk(1, BlockSize, complete = false)
+    handler.await()
+    Files exists target shouldBe false
+  }
+
+  it should "write a file for a chunk with complete flag set to true" in {
+    val handler = new TestFutureResultHandler(new CountDownLatch(1))
+    val target = createPathInDirectory("metaSmall.mdt")
+    val actor = createActorForMedium(handler, target, resolvedSize = 0)
+
+    actor ! chunk(1, BlockSize - 1, complete = true)
+    handler.await()
+    checkProcessingResults(parseMetaData(target), 1, BlockSize - 1)
+  }
+
+  it should "ignore a chunk for an unknown medium" in {
+    val actor = createTestActorRef()
+
+    actor receive chunk(1, BlockSize, complete = false)
+  }
+
+  it should "handle multiple chunks for a medium" in {
+    val handler = new TestFutureResultHandler(new CountDownLatch(2))
+    val target = createPathInDirectory("metaMulti.mdt")
+    val actor = createActorForMedium(handler, target, resolvedSize = 1)
+
+    actor ! chunk(1, BlockSize - 1, complete = false)
+    actor ! chunk(BlockSize, BlockSize + 5, complete = false)
+    actor ! chunk(BlockSize + 6, 2 * BlockSize + 7, complete = false)
+    handler.await()
+    checkProcessingResults(parseMetaData(target), 1, 2 * BlockSize + 7)
+  }
+
+  it should "take the initial resolved count into account" in {
+    val handler = new TestFutureResultHandler(new CountDownLatch(1))
+    val target = createPathInDirectory("otherUnresolved.mdt")
+    val actor = createActorForMedium(handler, createPathInDirectory("meta.mdt"), resolvedSize = 0)
+    actor ! processMessage(target, OtherMedium, 1)
+
+    actor ! chunk(1, BlockSize, complete = false, mediumID = OtherMedium)
+    actor ! chunk(1, BlockSize, complete = false)
+    handler.await()
+    Files exists target shouldBe false
+  }
+
+  it should "remove a medium from the process map when the last chunk was received" in {
+    val handler = new TestFutureResultHandler(new CountDownLatch(2))
+    val target = createPathInDirectory("metaRemoved.mdt")
+    val target2 = createPathInDirectory("metaStandard.mdt")
+    val actor = createActorForMedium(handler, target, resolvedSize = 0)
+    actor ! processMessage(target2, OtherMedium, resolvedSize = 0)
+
+    actor ! chunk(1, 10, complete = true)
+    actor ! chunk(11, 3 * BlockSize, complete = false)
+    actor ! chunk(1, 2, complete = true, mediumID = OtherMedium)
+    handler.await()
+    checkProcessingResults(parseMetaData(target), 1, 10)
+  }
+
+  it should "only write a single file at a given time" in {
+    val counter = new AtomicInteger
+    val target = createPathInDirectory("metaFirstMedium.mdt")
+    val target2 = createPathInDirectory("metaSecondMedium.mdt")
+    val handler = new TestFutureResultHandler(new CountDownLatch(2)) {
+      /**
+        * Checks that the 2nd file has not been created yet.
+        */
+      override protected def performChecks(): Unit = {
+        if (counter.incrementAndGet() == 1) {
+          Files exists target2 shouldBe false
+        }
+      }
+    }
+    val actor = createActorForMedium(handler, target, resolvedSize = 0)
+    actor ! processMessage(target2, OtherMedium, resolvedSize = 0)
+
+    actor ! chunk(1, 64, complete = false)
+    actor ! chunk(1, 1, complete = true, mediumID = OtherMedium)
+    handler.await()
+    checkProcessingResults(parseMetaData(target), 1, 64)
+    checkProcessingResults(parseMetaData(target2, OtherMedium), 1, 1, OtherMedium)
+  }
+
+  /**
+    * A specialized ''FutureIOResultHandler'' implementation that can execute
+    * additional checks and notify test code on completion of a stream
+    * operation.
+    *
+    * @param latch the latch for notifying test code
+    */
+  private class TestFutureResultHandler(latch: CountDownLatch) extends FutureIOResultHandler {
+    /**
+      * Waits until the stream operation is complete.
+      */
+    def await(): Unit = {
+      latch.await(5, TimeUnit.SECONDS) shouldBe true
+    }
+
+    /**
+      * @inheritdoc This implementation invokes the test code and triggers the
+      *             latch to notify test code that the stream operation is
+      *             complete.
+      */
+    override protected def onResultComplete(result: Try[_], actor: ActorRef, log: LoggingAdapter)
+    : Unit = {
+      performChecks()
+      super.onResultComplete(result, actor, log)
+      latch.countDown()
+    }
+
+    /**
+      * Executes some checks directly after the stream was written. This can be
+      * used in derived classes to implement some additional test conditions.
+      * This base implementation is empty.
+      */
+    protected def performChecks(): Unit = {
+    }
+  }
+
+}
