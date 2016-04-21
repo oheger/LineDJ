26f505b30235fc2ffa305b5a509ced877c4eef61
oheger <oliver.heger@oliver-heger.de>
2016-04-21 22:03:13 +0200

    Added M3uReaderActor.
    This actor can extract the URI of an audio stream from a URI
    pointing to an m3u file. Some radio streams use such a level of
    indirection.

---
+++ playerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/impl/M3uReaderActor.scala
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.impl
+
+import java.nio.charset.StandardCharsets
+
+import akka.actor.{Actor, ActorLogging}
+import de.oliver_heger.linedj.player.engine.impl.M3uReaderActor.{AudioStreamResolved,
+ResolveAudioStream}
+
+import scala.io.{BufferedSource, Source}
+
+private[impl] object M3uReaderActor {
+
+  /**
+    * A message class processed by [[M3uReaderActor]] telling it to process the
+    * specified URL and determine the correct audio stream.
+    *
+    * @param streamRef a reference to the stream with the m3u file
+    */
+  case class ResolveAudioStream(streamRef: StreamReference)
+
+  /**
+    * A message sent by [[M3uReaderActor]] when an audio stream could be
+    * resolved. The m3u data was read, and the final audio stream URL was
+    * discovered. Both references are part of this message.
+    *
+    * @param m3uStreamRef   the reference to the m3u stream
+    * @param audioStreamRef the resolved audio stream reference
+    */
+  case class AudioStreamResolved(m3uStreamRef: StreamReference, audioStreamRef: StreamReference)
+
+}
+
+/**
+  * An actor class that reads the content of a radio stream URL that ends on
+  * ''.m3u''.
+  *
+  * Some radio streams have an ''.m3u'' extension; they do not contain MP3 data
+  * directly, but a reference to the actual audio stream. This actor class is
+  * responsible for evaluating such a URL and obtain the correct URL to the
+  * audio stream.
+  *
+  * Reading and processing the m3u URL is a blocking operation. Therefore, the
+  * actor should run on a dedicated dispatcher.
+  *
+  * Exceptions during processing of the URL are not handled and thus are passed
+  * to the parent actor's supervision strategy (as IOExceptions). If no valid
+  * audio stream URL can be extracted, an ''IOException'' is thrown as well.
+  */
+class M3uReaderActor extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case ResolveAudioStream(streamRef) =>
+      sender ! AudioStreamResolved(streamRef, processM3uFile(streamRef))
+  }
+
+  /**
+    * Processes the m3u file specified by the given reference and extracts the
+    * referenced audio stream. If this is not possible, an ''IOException'' is
+    * thrown.
+    *
+    * @param streamRef the reference to the m3u file
+    * @return a reference to the extracted audio stream
+    */
+  private def processM3uFile(streamRef: StreamReference): StreamReference = {
+    log.info("Processing m3u URI {}.", streamRef.uri)
+    val source = Source.fromURL(streamRef.uri, StandardCharsets.UTF_8.toString)
+    try {
+      StreamReference(extractAudioStreamUri(source))
+    } finally source.close()
+  }
+
+  /**
+    * Extracts the URI of the audio stream from the specified source object.
+    * Throws an exception if this is not possible.
+    *
+    * @param source the source
+    * @return the extracted URI of the audio stream
+    */
+  private def extractAudioStreamUri(source: BufferedSource): String = {
+    val iter = source.getLines().filter(l => !l.isEmpty && !l.startsWith("#"))
+    if (iter.hasNext) iter.next()
+    else throw new java.io.IOException("Cannot extract audio stream URI from m3u file!")
+  }
+}
---
+++ playerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/impl/M3uReaderActorSpec.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.impl
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import de.oliver_heger.linedj.FileTestHelper
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object M3uReaderActorSpec {
+  /** A test URI of an audio stream. */
+  private val AudioStreamUri = "http://aud.io/music.mp3"
+
+  /** A reference to the resolved audio stream. */
+  private val AudioStreamRef = StreamReference(AudioStreamUri)
+}
+
+/**
+  * Test class for ''M3uReaderActor''.
+  */
+class M3uReaderActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with ImplicitSender
+  with FlatSpecLike with BeforeAndAfterAll with Matchers with FileTestHelper {
+
+  import M3uReaderActorSpec._
+
+  def this() = this(ActorSystem("M3uReaderActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    tearDownTestFile()
+  }
+
+  /**
+    * Creates a test m3u file with the specified content and returns a
+    * reference to it. The content can consist of multiple lines; each provided
+    * string is added as a single line, terminated by a newline character.
+    *
+    * @param content the content of the file (as single lines)
+    * @return a reference to the newly created file
+    */
+  private def createM3uFile(content: String*): StreamReference = {
+    val file = createDataFile(content.mkString("\n"))
+    StreamReference(file.toUri.toString)
+  }
+
+  /**
+    * Tests an invocation of the test actor to resolve an audio stream.
+    *
+    * @param ref the reference to the audio stream
+    */
+  private def checkResolveAudioStream(ref: StreamReference): Unit = {
+    val actor = system.actorOf(Props[M3uReaderActor])
+
+    actor ! M3uReaderActor.ResolveAudioStream(ref)
+    expectMsg(M3uReaderActor.AudioStreamResolved(ref, AudioStreamRef))
+  }
+
+  "A M3uReaderActor" should "extract the audio stream URI from a simple m3u file" in {
+    checkResolveAudioStream(createM3uFile(AudioStreamUri))
+  }
+
+  it should "skip comment lines" in {
+    checkResolveAudioStream(createM3uFile("# Comment", "#other comment", AudioStreamUri))
+  }
+
+  it should "skip empty lines" in {
+    checkResolveAudioStream(createM3uFile("", "#Header", AudioStreamUri))
+  }
+
+  it should "throw an exception if no stream URI is found" in {
+    val ref = createM3uFile("#only comments", "", "# and empty lines", "")
+    val actor = TestActorRef[M3uReaderActor](Props[M3uReaderActor])
+
+    intercept[java.io.IOException] {
+      actor.underlyingActor receive M3uReaderActor.ResolveAudioStream(ref)
+    }
+  }
+}
