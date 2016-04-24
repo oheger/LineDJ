e43a7eec73ed524314fbef5731e680b5ea6b4e7d
oheger <oliver.heger@oliver-heger.de>
2016-04-24 21:59:47 +0200

    Added SourceStreamReaderActor.
    This actor class handles reading of a radio stream. It can also deal
    with stream URIs pointing to m3u files.

---
+++ playerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/impl/SourceStreamReaderActor.scala
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.impl
+
+import akka.actor.{Actor, ActorRef, Props}
+import de.oliver_heger.linedj.io.ChannelHandler.ArraySource
+import de.oliver_heger.linedj.io.{CloseAck, CloseRequest}
+import de.oliver_heger.linedj.player.engine.PlayerConfig
+import de.oliver_heger.linedj.player.engine.impl.PlaybackActor.GetAudioData
+import de.oliver_heger.linedj.utils.ChildActorFactory
+
+object SourceStreamReaderActor {
+  /** The extension for m3u URIs. */
+  private val ExtM3u = ".m3u"
+
+  /** Error message for an unexpected audio data request. */
+  private val ErrUnexpectedRequest =
+    "[SourceStreamReaderActor] Unexpected request for audio data!"
+
+  /**
+    * Returns a flag whether the specified audio stream needs to be resolved
+    * first before it can be played. This is the case if the stream does not
+    * point to audio data, but to a playlist which references the actual
+    * audio stream.
+    *
+    * @param ref the reference in question
+    * @return '''true''' if this reference needs to be resolved
+    */
+  private def needToResolveAudioStream(ref: StreamReference): Boolean =
+    ref.uri endsWith ExtM3u
+
+  private class SourceStreamReaderActorImpl(config: PlayerConfig, streamRef: StreamReference)
+    extends SourceStreamReaderActor(config, streamRef) with ChildActorFactory
+
+  /**
+    * Creates a ''Props'' object for creating a new instance of this actor
+    * class.
+    *
+    * @param config    the player configuration
+    * @param streamRef the reference to the audio stream for playback
+    * @return creation properties for a new actor instance
+    */
+  def apply(config: PlayerConfig, streamRef: StreamReference): Props =
+    Props(classOf[SourceStreamReaderActorImpl], config, streamRef)
+}
+
+/**
+  * An actor class that reads audio data from an internet radio stream.
+  *
+  * This actor implements part of the functionality of a data source for
+  * [[PlaybackActor]]. It mainly combines the two actor classes
+  * [[M3uReaderActor]] and [[StreamBufferActor]]. An instance is initialized
+  * with a [[StreamReference]]. If this reference points to a m3u file,
+  * an [[M3uReaderActor]] has to be created to process the file and extract the
+  * actual URI of the audio stream. Otherwise, the reference is considered to
+  * already represent the audio stream.
+  *
+  * With the audio stream at hand a [[StreamBufferActor]] is created. This
+  * actor handles ''GetAudioData'' messages by delegating to the stream buffer
+  * actor. It also deals with the case that the buffer is not yet filled yet,
+  * and thus an empty read result is returned. In this case, the request is
+  * repeated.
+  *
+  * Supervision is implemented by delegating to the parent actor.
+  *
+  * An instance of this actor class can only be used for reading a single
+  * audio stream. It cannot be reused and has to be closed afterwards.
+  *
+  * @param config    the player configuration
+  * @param streamRef the reference to the audio stream for playback
+  */
+class SourceStreamReaderActor(config: PlayerConfig, streamRef: StreamReference) extends Actor {
+  this: ChildActorFactory =>
+
+  import SourceStreamReaderActor._
+
+  /** Stores the stream buffer actor. */
+  private var bufferActor: Option[ActorRef] = None
+
+  /** Stores a pending request for audio data. */
+  private var pendingDataRequest: Option[PlaybackActor.GetAudioData] = None
+
+  /** Stores the client that triggered a request for audio data. */
+  private var dataClient: Option[ActorRef] = None
+
+  @scala.throws[Exception](classOf[Exception])
+  override def preStart(): Unit = {
+    if (needToResolveAudioStream(streamRef)) {
+      val m3uReader = createChildActor(config.applyBlockingDispatcher(Props[M3uReaderActor]))
+      m3uReader ! M3uReaderActor.ResolveAudioStream(streamRef)
+    } else {
+      bufferActor = createBufferActor(streamRef)
+    }
+  }
+
+  override def receive: Receive = {
+    case M3uReaderActor.AudioStreamResolved(m3uRef, ref) if streamRef == m3uRef =>
+      if (bufferActor.isEmpty) {
+        bufferActor = createBufferActor(ref)
+        pendingDataRequest foreach bufferActor.get.!
+      }
+
+    case req: PlaybackActor.GetAudioData =>
+      if (dataClient.isDefined) {
+        sender ! PlaybackProtocolViolation(req, ErrUnexpectedRequest)
+      } else {
+        sendDataRequest(req)
+        pendingDataRequest = Some(req)
+        dataClient = Some(sender())
+      }
+
+    case data: ArraySource =>
+      if (data.length > 0) {
+        dataClient foreach (_ ! data)
+        dataClient = None
+      } else {
+        pendingDataRequest foreach sendDataRequest
+      }
+
+    case CloseRequest =>
+      val bufferRef = bufferActor match {
+        case Some(ref) =>
+          ref ! CloseRequest
+          ref
+        case None =>
+          sender ! CloseAck(self)
+          null
+      }
+      context become closing(sender(), bufferRef)
+  }
+
+  /**
+    * A receive function that becomes active when this actor receives a
+    * ''CloseRequest''. Afterwards no more messages are handled, only the
+    * close ack from the buffer actor.
+    *
+    * @param client    the client that triggered the close operation
+    * @param bufferRef reference to the buffer actor that is expected to send a
+    *                  ''CloseAck'' message
+    * @return the receive function
+    */
+  private def closing(client: ActorRef, bufferRef: ActorRef): Receive = {
+    case CloseAck(ref) if ref == bufferRef =>
+      client ! CloseAck(self)
+  }
+
+  /**
+    * Sends a request to the buffer actor for audio data. If the buffer actor
+    * is not available yet, this method does nothing.
+    *
+    * @param req the request to be sent
+    */
+  private def sendDataRequest(req: GetAudioData): Unit = {
+    bufferActor foreach (_ ! req)
+  }
+
+  /**
+    * Creates the actor that buffers the audio stream to be played.
+    *
+    * @param ref the reference to the audio stream
+    * @return the ''StreamBufferActor'' reference
+    */
+  private def createBufferActor(ref: StreamReference): Option[ActorRef] =
+    Some(createChildActor(config.applyBlockingDispatcher(Props(classOf[StreamBufferActor],
+      config, ref))))
+}
---
+++ playerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/impl/SourceStreamReaderActorSpec.scala
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.impl
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
+import de.oliver_heger.linedj.io.{CloseAck, CloseRequest}
+import de.oliver_heger.linedj.player.engine.PlayerConfig
+import de.oliver_heger.linedj.utils.ChildActorFactory
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import scala.concurrent.duration._
+
+object SourceStreamReaderActorSpec {
+  /** A reference pointing to an audio stream. */
+  private val AudioStreamRef = StreamReference("music.mp3")
+
+  /** A reference to a m3u file. */
+  private val PlaylistStreamRef = StreamReference("playlist.m3u")
+
+  /** Name of a blocking dispatcher. */
+  private val BlockingDispatcherName = "BlockingDispatcher"
+
+  /** The class for the buffer actor. */
+  private val ClassBufferActor = classOf[StreamBufferActor]
+
+  /** The class for the m3u reader actor. */
+  private val ClassM3uReaderActor = classOf[M3uReaderActor]
+
+  /** A default request message for audio data. */
+  private val DataRequest = PlaybackActor.GetAudioData(42)
+
+  /** A test player configuration. */
+  private val Config = PlayerConfig(mediaManagerActor = null, actorCreator = (props, name) => null,
+    blockingDispatcherName = Some(BlockingDispatcherName))
+
+  /**
+    * Creates an object with pseudo audio data.
+    *
+    * @return the data object
+    */
+  private def audioData(): ArraySourceImpl =
+    new ArraySourceImpl(new Array[Byte](42), 32)
+}
+
+/**
+  * Test class for ''SourceStreamReaderActor''.
+  */
+class SourceStreamReaderActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers {
+
+  import SourceStreamReaderActorSpec._
+
+  def this() = this(ActorSystem("SourceStreamReaderActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  "A SourceStreamReaderActor" should "create a buffer child actor" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    helper.createTestActor(AudioStreamRef)
+
+    awaitCond(helper.numberOfChildrenCreated == 1)
+  }
+
+  it should "pass a request for audio data to the buffer actor" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+    helper.numberOfChildrenCreated should be(1)
+  }
+
+  it should "create a m3u reader actor for a corresponding stream reference" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    helper.createTestActor(PlaylistStreamRef)
+
+    helper.m3uReaderActor.expectMsg(M3uReaderActor.ResolveAudioStream(PlaylistStreamRef))
+    helper.numberOfChildrenCreated should be(1)
+  }
+
+  it should "create a buffer actor when the audio stream is resolved" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(PlaylistStreamRef)
+
+    actor ! M3uReaderActor.AudioStreamResolved(PlaylistStreamRef, AudioStreamRef)
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+    helper.numberOfChildrenCreated should be(2)
+  }
+
+  it should "ignore an AudioStreamResolved message for a wrong stream" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(PlaylistStreamRef)
+
+    actor ! M3uReaderActor.AudioStreamResolved(StreamReference("other"), AudioStreamRef)
+    actor ! M3uReaderActor.AudioStreamResolved(PlaylistStreamRef, AudioStreamRef)
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+    helper.numberOfChildrenCreated should be(2)
+  }
+
+  it should "ignore further AudioStreamResolved messages" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(PlaylistStreamRef)
+
+    actor ! M3uReaderActor.AudioStreamResolved(PlaylistStreamRef, AudioStreamRef)
+    actor ! M3uReaderActor.AudioStreamResolved(PlaylistStreamRef, AudioStreamRef)
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+    helper.numberOfChildrenCreated should be(2)
+  }
+
+  it should "park an audio data request until the buffer actor becomes available" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(PlaylistStreamRef)
+
+    actor ! DataRequest
+    actor ! M3uReaderActor.AudioStreamResolved(PlaylistStreamRef, AudioStreamRef)
+    helper.probeBufferActor.expectMsg(DataRequest)
+  }
+
+  it should "send back the answer to an audio data request" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! DataRequest
+    val data = audioData()
+
+    actor ! data
+    expectMsg(data)
+  }
+
+  it should "ignore an unexpected audio data message" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+
+    actor receive audioData()
+  }
+
+  it should "reject data requests if a request is in progress" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! DataRequest
+
+    val req2 = PlaybackActor.GetAudioData(1)
+    actor ! req2
+    val err = expectMsgType[PlaybackProtocolViolation]
+    err.msg should be(req2)
+    err.errorText should be("[SourceStreamReaderActor] Unexpected request for audio data!")
+  }
+
+  it should "process multiple data requests" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+    val data = audioData()
+    actor ! data
+    expectMsg(data)
+
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+  }
+
+  it should "repeat data requests if no data is returned" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! DataRequest
+    helper.probeBufferActor.expectMsg(DataRequest)
+
+    actor ! new ArraySourceImpl(Array.empty, 0)
+    helper.probeBufferActor.expectMsg(DataRequest)
+    val data = audioData()
+    actor ! data
+    expectMsg(data)
+  }
+
+  it should "ignore an unexpected data message with length 0" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+
+    actor receive new ArraySourceImpl(Array.empty, 0)
+  }
+
+  it should "delegate a close request to the buffer actor" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+
+    actor ! CloseRequest
+    helper.probeBufferActor.expectMsg(CloseRequest)
+  }
+
+  it should "ignore further messages after a close request" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! CloseRequest
+    helper.probeBufferActor.expectMsg(CloseRequest)
+
+    actor ! DataRequest
+    helper.probeBufferActor.expectNoMsg(500.milliseconds)
+  }
+
+  it should "send a CloseAck directly if there is not yet a buffer actor" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(PlaylistStreamRef)
+
+    actor ! CloseRequest
+    expectMsg(CloseAck(actor))
+  }
+
+  it should "send a CloseAck when the buffer actor has been closed" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! CloseRequest
+
+    actor.tell(CloseAck(helper.probeBufferActor.ref), helper.probeBufferActor.ref)
+    expectMsg(CloseAck(actor))
+  }
+
+  it should "ignore an unexpected CloseAck message" in {
+    val helper = new SourceStreamReaderActorTestHelper
+    val actor = helper.createTestActor(AudioStreamRef)
+    actor ! CloseRequest
+
+    actor ! CloseAck(testActor)
+    expectNoMsg(500.milliseconds)
+  }
+
+  it should "create a correct Props object" in {
+    val props = SourceStreamReaderActor(Config, AudioStreamRef)
+
+    classOf[SourceStreamReaderActor].isAssignableFrom(props.actorClass()) shouldBe true
+    classOf[ChildActorFactory].isAssignableFrom(props.actorClass()) shouldBe true
+    props.args should contain theSameElementsAs List(Config, AudioStreamRef)
+  }
+
+  /**
+    * A test helper class managing dependencies of a test actor.
+    */
+  private class SourceStreamReaderActorTestHelper {
+    /** Test probe for the buffer actor. */
+    val probeBufferActor = TestProbe()
+
+    /** Test probe for the m3u reader actor. */
+    val m3uReaderActor = TestProbe()
+
+    /** A counter for the child actors that have been created. */
+    private val childCount = new AtomicInteger
+
+    /**
+      * Creates a test actor that operates on the specified reference.
+      *
+      * @param streamRef the stream reference
+      * @return the test actor reference
+      */
+    def createTestActor(streamRef: StreamReference): TestActorRef[SourceStreamReaderActor] =
+      TestActorRef[SourceStreamReaderActor](createProps(streamRef))
+
+    /**
+      * Returns the number of children that have been created for the test
+      * actor.
+      *
+      * @return the number of created child actors
+      */
+    def numberOfChildrenCreated: Int = childCount.get()
+
+    /**
+      * Creates the properties for a test actor.
+      *
+      * @param streamRef the reference to the stream to be processed
+      * @return the properties
+      */
+    private def createProps(streamRef: StreamReference): Props =
+      Props(new SourceStreamReaderActor(Config, streamRef) with ChildActorFactory {
+        /**
+          * @inheritdoc This implementation returns corresponding test
+          *             probes after testing the arguments.
+          */
+        override def createChildActor(p: Props): ActorRef = {
+          childCount.incrementAndGet()
+          p.dispatcher should be(BlockingDispatcherName)
+          p.actorClass() match {
+            case ClassBufferActor =>
+              p.args should contain theSameElementsAs List(Config, AudioStreamRef)
+              probeBufferActor.ref
+
+            case ClassM3uReaderActor =>
+              p.args shouldBe 'empty
+              m3uReaderActor.ref
+          }
+        }
+      })
+  }
+
+}
