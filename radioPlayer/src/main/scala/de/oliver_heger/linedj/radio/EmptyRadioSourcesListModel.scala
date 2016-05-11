5719fc338887fa536075cfc157fc190492bcf368
oheger <oliver.heger@oliver-heger.de>
2016-05-11 22:20:48 +0200

    Added EmptyRadioSourcesListModel class.
    This class represents the initially empty list model for the combo
    box with available radio sources.

---
+++ radioPlayer/src/main/scala/de/oliver_heger/linedj/radio/EmptyRadioSourcesListModel.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import de.oliver_heger.linedj.client.model.EmptyListModel
+import de.oliver_heger.linedj.player.engine.RadioSource
+
+/**
+  * The list model class for the combo box with radio sources.
+  *
+  * This model is empty initially. It is then filled when the radio sources
+  * available are read from the application's configuration.
+  */
+class EmptyRadioSourcesListModel extends EmptyListModel {
+  override val getType = classOf[RadioSource]
+}
---
+++ radioPlayer/src/test/scala/de/oliver_heger/linedj/radio/EmptyRadioSourcesListModelSpec.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import de.oliver_heger.linedj.player.engine.RadioSource
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''EmptyRadioSourcesListModel''.
+  */
+class EmptyRadioSourcesListModelSpec extends FlatSpec with Matchers {
+  "An EmptyRadioSourcesListModel" should "return the correct element type" in {
+    val model = new EmptyRadioSourcesListModel
+
+    model.getType should be(classOf[RadioSource])
+  }
+}
