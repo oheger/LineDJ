d6d3735ad6b26ca5ee99a70d25a7eb300e68d6fa
oheger <oliver.heger@oliver-heger.de>
2016-06-30 19:34:54 +0200

    Added RadioSourceConfig class.
    This class represents the configuration of the radio player
    application related to radio sources. An instance can be initialized
    from a Configuration object.

---
+++ radioPlayer/src/main/scala/de/oliver_heger/linedj/radio/RadioSourceConfig.scala
@@ -0,0 +1,253 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import java.time.DayOfWeek
+
+import de.oliver_heger.linedj.player.engine.RadioSource
+import de.oliver_heger.linedj.player.engine.interval.IntervalQueries
+import de.oliver_heger.linedj.player.engine.interval.IntervalTypes.IntervalQuery
+import org.apache.commons.configuration.{Configuration, ConversionException,
+HierarchicalConfiguration}
+import org.slf4j.LoggerFactory
+
+/**
+  * Companion object for ''RadioSourceConfig''.
+  *
+  * This object allows the creation of ''RadioSourceConfig'' instances.
+  */
+object RadioSourceConfig {
+  /** Configuration key for the radio sources. */
+  private val KeySources = "radio.sources.source"
+
+  /** Configuration key for the name of a radio source. */
+  private val KeySourceName = "name"
+
+  /** Configuration key for the URI of a radio source. */
+  private val KeySourceURI = "uri"
+
+  /** Configuration key for the extension of a radio source. */
+  private val KeySourceExtension = "extension"
+
+  /** Configuration key for the exclusions of a radio source. */
+  private val KeyExclusions = "exclusions.exclusion"
+
+  /** The key for the from attribute for interval queries. */
+  private val KeyAttrFrom = "[@from]"
+
+  /** The key for the to attribute for interval queries. */
+  private val KeyAttrTo = "[@to]"
+
+  /** The key for a minutes interval. */
+  private val KeyIntervalMinutes = "minutes"
+
+  /** The key for an hours interval. */
+  private val KeyIntervalHours = "hours"
+
+  /** The key for a day-of-week interval. */
+  private val KeyIntervalDays = "days.day"
+
+  /** The logger. */
+  private val log = LoggerFactory.getLogger(classOf[RadioSourceConfig])
+
+  /**
+    * Creates a new instance of ''RadioSourceConfig'' with the content of the
+    * specified ''Configuration'' object.
+    *
+    * @param config the ''Configuration''
+    * @return the new ''RadioSourceConfig''
+    */
+  def apply(config: Configuration): RadioSourceConfig = {
+    val srcData = readSourcesFromConfig(config)
+    val sources = srcData map (t => (t._1, t._2))
+    val exclusions = srcData map(t => (t._2, t._3))
+    RadioSourceConfigImpl(sources, exclusions.toMap)
+  }
+
+  /**
+    * Reads the defined radio sources from the configuration.
+    *
+    * @return a sequence with the extracted radio sources
+    */
+  private def readSourcesFromConfig(config: Configuration): Seq[(String, RadioSource,
+    Seq[IntervalQuery])] = {
+    val srcConfigs = config.asInstanceOf[HierarchicalConfiguration].configurationsAt(KeySources)
+    import collection.JavaConversions._
+    val sources = srcConfigs.par filter { c =>
+      c.containsKey(KeySourceName) && c.containsKey(KeySourceURI)
+    } map { c =>
+      (c.getString(KeySourceName), RadioSource(c.getString(KeySourceURI), Option(c.getString
+      (KeySourceExtension))), readExclusions(c))
+    }
+    sources.seq sortWith (_._1 < _._1)
+  }
+
+  /**
+    * Parses the exclusions definition for a radio source declaration.
+    *
+    * @param config the sub configuration for the current source
+    * @return a sequence with all extracted interval queries
+    */
+  private def readExclusions(config: HierarchicalConfiguration): Seq[IntervalQuery] = {
+    val exConfigs = config configurationsAt KeyExclusions
+    import collection.JavaConversions._
+    exConfigs.foldLeft(List.empty[IntervalQuery]) { (q, c) =>
+      parseIntervalQuery(c) match {
+        case Some(query) => IntervalQueries.cyclic(query) :: q
+        case None => q
+      }
+    }
+  }
+
+  /**
+    * Tries to parse an interval query from an exclusion element in the
+    * configuration.
+    *
+    * @param c the sub configuration for a single source
+    * @return an option for the extracted interval query
+    */
+  private def parseIntervalQuery(c: Configuration): Option[IntervalQuery] = {
+    val minutes = parseRangeQuery(c, 60, KeyIntervalMinutes)(IntervalQueries.minutes)
+    val hours = parseRangeQuery(c, 24, KeyIntervalHours)(IntervalQueries.hours)
+    val days = parseWeekDayQuery(c)
+
+    val parts = days :: hours :: minutes :: Nil
+    parts.flatten.reduceOption(IntervalQueries.combine)
+  }
+
+  /**
+    * Parses an exclusion definition for a range-based query.
+    *
+    * @param c   the configuration
+    * @param max the maximum value for a parameter value
+    * @param key the configuration key prefix for query parameters
+    * @param f   the function for creating the query
+    * @return an option for the parsed query
+    */
+  private def parseRangeQuery(c: Configuration, max: Int, key: String)(f: (Int, Int) =>
+    IntervalQuery): Option[IntervalQuery] = {
+    def checkValue(value: Int): Boolean =
+      value >= 0 && value <= max
+
+    if (c.containsKey(key + KeyAttrFrom) && c.containsKey(key + KeyAttrTo)) {
+      try {
+        val from = c.getInt(key + KeyAttrFrom)
+        val to = c.getInt(key + KeyAttrTo)
+        if (checkValue(from) && checkValue(to) && from < to)
+          Some(f(from, to))
+        else {
+          log.warn(s"Invalid query parameters ($from, $to) for key $key.")
+          None
+        }
+      } catch {
+        case e: ConversionException =>
+          log.warn("Non parsable values in exclusion definition!", e)
+          None
+      }
+    } else None
+  }
+
+  /**
+    * Parses an exclusion definition based on days of week.
+    *
+    * @param c the configuration
+    * @return an option for the parsed query
+    */
+  private def parseWeekDayQuery(c: Configuration): Option[IntervalQuery] = {
+    val days = c.getStringArray(KeyIntervalDays)
+    try {
+      val daySet = days.map(DayOfWeek.valueOf(_).getValue).toSet
+      if (daySet.nonEmpty) Some(IntervalQueries.weekDaySet(daySet))
+      else None
+    } catch {
+      case i: IllegalArgumentException =>
+        log.warn("Could not parse day-of-week query!", i)
+        None
+    }
+  }
+
+  /**
+    * Internal implementation of the radio source config trait as a plain case
+    * class.
+    *
+    * @param sources    the list with sources
+    * @param exclusions the map with exclusions
+    */
+  private case class RadioSourceConfigImpl(override val sources: Seq[(String, RadioSource)],
+                                           override val exclusions: Map[RadioSource,
+                                             Seq[IntervalQuery]]) extends RadioSourceConfig
+
+}
+
+/**
+  * A trait allowing access to configuration information about radio sources.
+  *
+  * Instances of this class are created from the current configuration of the
+  * radio player application. They contain all information available about
+  * radio sources, i.e. the source declarations (including name and URI) plus
+  * optional exclusions for each source.
+  *
+  * An exclusion is an interval in which a radio source should not be played;
+  * for instance, a station might send commercials for each half or full hour.
+  * Then an ''interval query'' can be defined excluding these times.
+  *
+  * Below is an example configuration for radio sources showing all supported
+  * elements:
+  *
+  * {{{
+  * <sources>
+  *   <source>
+  *     <name>HR 1</name>
+  *     <uri>http://metafiles.gl-systemhaus.de/hr/hr1_2.m3u</uri>
+  *     <extension>mp3</extension>
+  *     <exclusions>
+  *       <exclusion>
+  *         <days>
+  *           <day>MONDAY</day>
+  *           <day>TUESDAY</day>
+  *           <day>WEDNESDAY</day>
+  *           <day>THURSDAY</day>
+  *           <day>FRIDAY</day>
+  *         </days>
+  *         <hours from="6" to="20"/>
+  *         <minutes from="25" to="30"/>
+  *       </exclusion>
+  *     </exclusions>
+  *   </source>
+  * </sources>
+  * }}}
+  */
+trait RadioSourceConfig {
+  /**
+    * Returns a list with all available radio sources. A source is described
+    * by a tuple: The first element is a human-readable name of the source,
+    * the second element is the ''RadioSource'' object itself.
+    *
+    * @return a sequence with all available radio sources
+    */
+  def sources: Seq[(String, RadioSource)]
+
+  /**
+    * Returns a map with information about exclusions for radio sources. This
+    * map defines time intervals in which specific sources should not be
+    * played. If a radio source is not contained in the map, this means that
+    * there are no exclusions defined for it.
+    *
+    * @return a map with exclusions for radio sources
+    */
+  def exclusions: Map[RadioSource, Seq[IntervalQuery]]
+}
---
+++ radioPlayer/src/test/scala/de/oliver_heger/linedj/radio/RadioSourceConfigSpec.scala
@@ -0,0 +1,324 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import java.time.{LocalDateTime, Month}
+
+import de.oliver_heger.linedj.player.engine.RadioSource
+import de.oliver_heger.linedj.player.engine.interval.IntervalTypes.{Before, Inside, IntervalQuery}
+import org.apache.commons.configuration.{Configuration, HierarchicalConfiguration}
+import org.scalatest.{FlatSpec, Matchers}
+
+object RadioSourceConfigSpec {
+  /** Prefix for a radio source name. */
+  private val RadioSourceName = "Radio_"
+
+  /** Prefix for a radio source URI. */
+  private val RadioSourceURI = "http://rad.io/"
+
+  /**
+    * Generates the name for the source with the given index.
+    *
+    * @param idx the index
+    * @return the name for this radio source
+    */
+  private def sourceName(idx: Int): String = RadioSourceName + idx
+
+  /**
+    * Determines whether the radio source with the given index has a default
+    * file extension. All sources with an odd index have.
+    *
+    * @param idx the index
+    * @return a flag whether this source has a default extension
+    */
+  private def hasDefaultExt(idx: Int): Boolean = idx % 2 != 0
+
+  /**
+    * Generates a radio source object based on the given index.
+    *
+    * @param idx the index
+    * @return the radio source for this index
+    */
+  private def radioSource(idx: Int): RadioSource =
+    RadioSource(RadioSourceURI + idx, if (hasDefaultExt(idx)) Some("mp3") else None)
+
+  /**
+    * Creates a configuration that contains the given number of radio sources.
+    * All sources are defined with their name and URI. Sources with an odd
+    * index have a default file extension.
+    *
+    * @param count the number of radio sources
+    * @return the configuration defining the number of radio sources
+    */
+  private def createSourceConfiguration(count: Int): Configuration = {
+    val sources = (1 to count) map (i => (sourceName(i), radioSource(i)))
+    createSourceConfiguration(sources)
+  }
+
+  /**
+    * Creates a configuration that contains the specified radio sources.
+    *
+    * @param sources a list with the sources to be contained
+    * @return the configuration with these radio sources
+    */
+  private def createSourceConfiguration(sources: Seq[(String, RadioSource)]): Configuration = {
+    val config = new HierarchicalConfiguration
+    sources foreach { t =>
+      config.addProperty("radio.sources.source(-1).name", t._1)
+      config.addProperty("radio.sources.source.uri", t._2.uri)
+      t._2.defaultExtension foreach (config.addProperty("radio.sources.source.extension", _))
+    }
+    config
+  }
+
+  /**
+    * Generates the key for an exclusion for a radio source.
+    *
+    * @param srcIdx  the index of the source
+    * @param suffix  the suffix of the exclusion key
+    * @param exclIdx an optional index for the index of the exclusion element
+    * @return the full key
+    */
+  private def exclusionKey(srcIdx: Int, suffix: String, exclIdx: Option[Int] = None): String =
+    s"radio.sources.source($srcIdx).exclusions.exclusion${exclIdx.map(i => s"($i)").getOrElse("")
+    }.$suffix"
+}
+
+/**
+  * Test class for ''RadioSourceConfig''.
+  */
+class RadioSourceConfigSpec extends FlatSpec with Matchers {
+
+  import RadioSourceConfigSpec._
+
+  /**
+    * Checks whether the specified interval query yields a Before result for
+    * the given date with the provided start time.
+    *
+    * @param query the query
+    * @param date  the reference date
+    * @param start the start time
+    */
+  private def assertBefore(query: IntervalQuery, date: LocalDateTime, start: LocalDateTime): Unit
+  = {
+    query(date) match {
+      case Before(d) => d.value should be(start)
+      case r => fail("Unexpected result: " + r)
+    }
+  }
+
+  /**
+    * Checks whether the specified interval query yields an Inside result for
+    * the given date with the provided until time.
+    *
+    * @param query the query
+    * @param date  the reference date
+    * @param until the until time
+    */
+  private def assertInside(query: IntervalQuery, date: LocalDateTime, until: LocalDateTime): Unit
+  = {
+    query(date) match {
+      case Inside(d) => d.value should be(until)
+      case r => fail("Unexpected result: " + r)
+    }
+  }
+
+  "A RadioSourceConfig" should "provide a correct list of sources" in {
+    val Count = 4
+    val config = createSourceConfiguration(Count)
+
+    val sourceConfig = RadioSourceConfig(config)
+    val expSrcList = (1 to Count) map (i => (sourceName(i), radioSource(i)))
+    sourceConfig.sources should be(expSrcList)
+  }
+
+  it should "ignore incomplete source definitions" in {
+    val Count = 2
+    val config = createSourceConfiguration(Count)
+    config.addProperty("radio.sources.source(-1).name", "someName")
+    config.addProperty("radio.sources.source(-1).uri", "someURI")
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.sources should have size 2
+    sourceConfig.sources.head._1 should be(sourceName(1))
+    sourceConfig.sources(1)._1 should be(sourceName(2))
+  }
+
+  it should "order radio sources by name" in {
+    val sources = List((sourceName(8), radioSource(8)), (sourceName(2), radioSource(2)),
+      (sourceName(3), radioSource(3)), (sourceName(9), radioSource(9)))
+    val sourcesSorted = List((sourceName(2), radioSource(2)), (sourceName(3), radioSource(3)),
+      (sourceName(8), radioSource(8)), (sourceName(9), radioSource(9)))
+    val config = createSourceConfiguration(sources)
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.sources should be(sourcesSorted)
+  }
+
+  it should "return a map with all sources and empty lists if no exclusions are defined" in {
+    val config = createSourceConfiguration(2)
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions should have size 2
+    sourceConfig.exclusions.values.forall(_.isEmpty) shouldBe true
+  }
+
+  it should "process a minutes exclusion" in {
+    val config = createSourceConfiguration(4)
+    config.addProperty(exclusionKey(2, "minutes[@from]"), "22")
+    config.addProperty(exclusionKey(2, "minutes[@to]"), 25)
+
+    val sourceConfig = RadioSourceConfig(config)
+    val exclusions = sourceConfig.exclusions(radioSource(3))
+    exclusions should have size 1
+    assertBefore(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 29, 19, 21),
+      LocalDateTime.of(2016, Month.JUNE, 29, 19, 22))
+    assertInside(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 30, 22, 23),
+      LocalDateTime.of(2016, Month.JUNE, 30, 22, 25))
+  }
+
+  it should "drop an undefined exclusion" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "unsupported"), "1")
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "drop a minute exclusion with an invalid from" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "minutes[@from]"), "61")
+    config.addProperty(exclusionKey(0, "minutes[@to]"), 52)
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "drop a minute exclusion with an invalid to" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "minutes[@from]"), "55")
+    config.addProperty(exclusionKey(0, "minutes[@to]"), "-1")
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "ignore a minute exclusion with from >= to" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "minutes[@from]"), "58")
+    config.addProperty(exclusionKey(0, "minutes[@to]"), "58")
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "ignore a minute exclusion with a non-string parameter" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "minutes[@from]"), "noNumber")
+    config.addProperty(exclusionKey(0, "minutes[@to]"), "58")
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "combine multiple interval queries" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "minutes[@from]"), "14")
+    config.addProperty(exclusionKey(0, "minutes[@to]"), "20")
+    config.addProperty(exclusionKey(0, "hours[@from]"), "21")
+    config.addProperty(exclusionKey(0, "hours[@to]"), "23")
+
+    val sourceConfig = RadioSourceConfig(config)
+    val exclusions = sourceConfig.exclusions(radioSource(1))
+    exclusions should have size 1
+    assertBefore(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 29, 20, 10),
+      LocalDateTime.of(2016, Month.JUNE, 29, 21, 14))
+    assertInside(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 30, 22, 14),
+      LocalDateTime.of(2016, Month.JUNE, 30, 22, 20))
+  }
+
+  it should "ignore an hours exclusion with invalid values" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "hours[@from]"), "21")
+    config.addProperty(exclusionKey(0, "hours[@to]"), "25")
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "parse a days-of-week query" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "days.day"), Array("MONDAY", "WEDNESDAY", "SATURDAY"))
+
+    val sourceConfig = RadioSourceConfig(config)
+    val exclusions = sourceConfig.exclusions(radioSource(1))
+    exclusions should have size 1
+    assertInside(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 27, 21, 29),
+      LocalDateTime.of(2016, Month.JUNE, 28, 0, 0))
+    assertInside(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 29, 21, 29),
+      LocalDateTime.of(2016, Month.JUNE, 30, 0, 0))
+    assertInside(exclusions.head, LocalDateTime.of(2016, Month.JULY, 2, 21, 29),
+      LocalDateTime.of(2016, Month.JULY, 3, 0, 0))
+    assertBefore(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 28, 21, 31),
+      LocalDateTime.of(2016, Month.JUNE, 29, 0, 0))
+    assertBefore(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 30, 21, 31),
+      LocalDateTime.of(2016, Month.JULY, 2, 0, 0))
+  }
+
+  it should "ignore a days-of-week query with an invalid day" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "days.day"), Array("TUESDAY", "FRIDAY", "unknown day"))
+
+    val sourceConfig = RadioSourceConfig(config)
+    sourceConfig.exclusions(radioSource(1)) shouldBe 'empty
+  }
+
+  it should "create cyclic interval queries" in {
+    val config = createSourceConfiguration(1)
+    config.addProperty(exclusionKey(0, "days.day"),
+      Array("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"))
+    config.addProperty(exclusionKey(0, "hours[@from]"), "21")
+    config.addProperty(exclusionKey(0, "hours[@to]"), "23")
+
+    val sourceConfig = RadioSourceConfig(config)
+    val exclusions = sourceConfig.exclusions(radioSource(1))
+    exclusions should have size 1
+    assertBefore(exclusions.head, LocalDateTime.of(2016, Month.JULY, 2, 8, 0),
+      LocalDateTime.of(2016, Month.JULY, 4, 21, 0))
+  }
+
+  it should "support multiple exclusion per radio source" in {
+    val config = createSourceConfiguration(2)
+    config.addProperty(exclusionKey(0, "days.day"),
+      Array("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"))
+    config.addProperty(exclusionKey(0, "hours[@from]"), "6")
+    config.addProperty(exclusionKey(0, "hours[@to]"), "20")
+    config.addProperty(exclusionKey(0, "minutes[@from]"), "27")
+    config.addProperty(exclusionKey(0, "minutes[@to]"), "30")
+    config.addProperty(exclusionKey(0, "days.day", Some(-1)), "SATURDAY")
+    config.addProperty(exclusionKey(0, "hours[@from]", Some(1)), "9")
+    config.addProperty(exclusionKey(0, "hours[@to]", Some(1)), "12")
+
+    val sourceConfig = RadioSourceConfig(config)
+    val exclusions = sourceConfig.exclusions(radioSource(1))
+    exclusions should have size 2
+    assertBefore(exclusions(1), LocalDateTime.of(2016, Month.JUNE, 29, 21, 59),
+      LocalDateTime.of(2016, Month.JUNE, 30, 6, 27))
+    assertBefore(exclusions.head, LocalDateTime.of(2016, Month.JUNE, 29, 22, 0),
+      LocalDateTime.of(2016, Month.JULY, 2, 9, 0))
+  }
+}
