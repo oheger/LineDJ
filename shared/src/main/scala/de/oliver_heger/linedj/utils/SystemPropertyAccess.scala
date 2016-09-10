1c8f9db95e49dd5a6990494f72df5f39c41f6e0d
oheger <oliver.heger@oliver-heger.de>
2016-09-10 17:50:03 +0200

    Added utility trait SystemPropertyAccess.
    This trait offers functionality for querying system properties. This
    is needed by some classes in multiple projects.
    
    It does not seem possible to change system properties in tests
    executed by sbt. Therefore, access to system properties is hard to
    test. With this new trait, arbitrary system properties can be
    injected quite easily.

---
+++ shared/src/main/scala/de/oliver_heger/linedj/utils/SystemPropertyAccess.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.utils
+
+/**
+  * A trait that provides access to system properties.
+  *
+  * This trait defines a function for querying the value of a system
+  * property. An option with the value is returned.
+  *
+  * By overriding this method in test classes, it is easy to inject specific
+  * property values without having to deal with real system properties.
+  * (Obviously, SBT does not support changing system properties at runtime.)
+  */
+trait SystemPropertyAccess {
+  /**
+    * Queries a system property and returns its value.
+    *
+    * @param key the key of the property
+    * @return an option for the property's value
+    */
+  def getSystemProperty(key: String): Option[String] = Option(System getProperty key)
+}
---
+++ shared/src/test/scala/de/oliver_heger/linedj/utils/SystemPropertyAccessSpec.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.utils
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''SystemPropertyAccess''.
+  */
+class SystemPropertyAccessSpec extends FlatSpec with Matchers {
+  "A SystemPropertyAccess" should "support querying system properties" in {
+    val access = new SystemPropertyAccess {}
+
+    access.getSystemProperty("user.home").get should be(System.getProperty("user.home"))
+  }
+
+  it should "return None for an undefined system property" in {
+    val access = new SystemPropertyAccess {}
+
+    access getSystemProperty "an undefined property" should be(None)
+  }
+}
