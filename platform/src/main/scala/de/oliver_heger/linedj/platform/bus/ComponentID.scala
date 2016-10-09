e53b8d049ab0491365f56a396d5715a1d34ef167
oheger <oliver.heger@oliver-heger.de>
2016-10-09 19:48:00 +0200

    Added ComponentID class.
    This class represents a generic, unique ID that can be used in
    various places in the LineDJ platform.
    
    (It is also going to replace the consumerID currently used for
    extensions of the media archive interface.)

---
+++ platform/src/main/scala/de/oliver_heger/linedj/platform/bus/ComponentID.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.bus
+
+import scala.util.Random
+
+object ComponentID {
+  /** An object for generating unique numeric IDs. */
+  private val random = new Random
+
+  /**
+    * Creates a new instance of ''ComponentID''. This factory method is the
+    * only way to create new instances of this class. The caller has no
+    * influence on the internal structure of the generated object.
+    *
+    * @return the new ''ComponentID'' instance
+    */
+  def apply(): ComponentID = new ComponentID(random.nextInt())
+}
+
+/**
+  * A class representing an ID of a component.
+  *
+  * When applications use the UI message bus it is sometimes necessary that
+  * they can be uniquely identified; so some sort of ID is required. This class
+  * is used as a component ID.
+  *
+  * In principle, it would be possible to use the object references of
+  * component instances directly as IDs; they fulfill the uniqueness criterion.
+  * Using a separate class for this purpose, however, makes it more explicit
+  * that the objects involved are IDs, and that they therefore have to have
+  * specific properties.
+  *
+  * The internal structure of a ''ComponentID'' object is not visible to the
+  * outside. New instances are created via the companion object and cannot be
+  * modified afterwards. It is also not possible to create a ''ComponentID''
+  * object that matches another instance.
+  *
+  * @param numID a numeric ID used for generating a unuqie string representation
+  */
+final class ComponentID private(numID: Int) {
+  /**
+    * @inheritdoc This implementation returns a string that contains a numeric
+    *             ID. This ID is only used to support debug output, to have an
+    *             assignment between an object and a readable ID. It is not
+    *             used for other purposes like equals calculation.
+    */
+  override def toString: String = s"ComponentID [$numID]"
+}
---
+++ platform/src/test/scala/de/oliver_heger/linedj/platform/bus/ComponentIDSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.bus
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''ComponentID''.
+  */
+class ComponentIDSpec extends FlatSpec with Matchers {
+  "A ComponentID" should "be equal to itself" in {
+    val id = ComponentID()
+
+    id should be(id)
+  }
+
+  it should "not be equal to other instances" in {
+    val Count = 128
+    val ids = (1 to Count).map(_ => ComponentID()).toSet
+
+    ids should have size Count
+  }
+
+  it should "generate different hash codes" in {
+    val Count = 128
+    val ids = (1 to Count).map(_ => ComponentID().hashCode()).toSet
+
+    ids.size should be > 1
+  }
+
+  it should "have a toString method returning a numeric ID" in {
+    val reg = """ComponentID \[-?\d+\]""".r
+    val id = ComponentID()
+
+    id.toString match {
+      case reg(_*) =>
+      case s => fail("Invalid string representation: " + s)
+    }
+  }
+
+  it should "have different string representations for different instances" in {
+    val Count = 128
+    val ids = (1 to Count).map(_ => ComponentID().toString()).toSet
+
+    ids should have size Count
+  }
+}
