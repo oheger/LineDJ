28f3e17b94747529563dff55f001adcebd793c72
oheger <oliver.heger@oliver-heger.de>
2016-10-28 21:45:09 +0200

    Added MetaDataFileRemoveActor.
    This actor can remove a set of meta data files.

---
+++ archive/src/main/scala/de/oliver_heger/linedj/archive/metadata/persistence/MetaDataFileRemoveActor.scala
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.metadata.persistence
+
+import java.nio.file.Path
+
+import akka.actor.SupervisorStrategy.Stop
+import akka.actor.{Actor, ActorRef, OneForOneStrategy, Props, SupervisorStrategy, Terminated}
+import de.oliver_heger.linedj.archive.metadata.persistence.MetaDataFileRemoveActor
+.{RemoveMetaDataFiles, RemoveMetaDataFilesResult, RemoveRequestData}
+import de.oliver_heger.linedj.io.RemoveFileActor
+import de.oliver_heger.linedj.utils.ChildActorFactory
+
+object MetaDataFileRemoveActor {
+
+  /**
+    * A message processed by [[MetaDataFileRemoveActor]] telling it to remove
+    * the given set of meta data files.
+    *
+    * @param fileIDs     the set of checksum values for the files to be removed
+    * @param pathMapping the mapping from checksum values to paths
+    * @param client      the original client that sent this request
+    */
+  private[persistence] case class RemoveMetaDataFiles(fileIDs: Set[String],
+                                                      pathMapping: Map[String, Path],
+                                                      client: ActorRef)
+
+  /**
+    * A message sent by [[MetaDataFileRemoveActor]] as a result of a remove
+    * operation. The message indicates which files could be removed
+    * successfully.
+    *
+    * @param request           the original request
+    * @param successfulDeleted a set with the checksum values of the files that
+    *                          could be removed successfully
+    */
+  private[persistence] case class RemoveMetaDataFilesResult(request: RemoveMetaDataFiles,
+                                                            successfulDeleted: Set[String])
+
+  /**
+    * An internally used data class that stores information about requests to
+    * remove meta data files.
+    *
+    * @param request the actual remove request
+    * @param client  the sender of this request
+    */
+  private case class RemoveRequestData(request: RemoveMetaDataFiles, client: ActorRef)
+
+  private class MetaDataFileRemoveActorImpl extends MetaDataFileRemoveActor
+    with ChildActorFactory
+
+  /**
+    * Creates a ''Props'' object for creating an instance of this actor class.
+    *
+    * @return a ''Props'' object for creating a new actor
+    */
+  def apply(): Props = Props[MetaDataFileRemoveActorImpl]
+}
+
+/**
+  * An actor class which supports removing a set of meta data files.
+  *
+  * This actor is used internally to remove a set of meta data files, e.g. the
+  * obsolete files no longer assigned to an active medium. The actor reacts on
+  * messages specifying the files to be removed (as checksum values) and the
+  * map with checksum values to concrete ''Path'' objects. Each file is removed
+  * one by one using a ''RemoveFileActor''. At the end of the operation, a
+  * message is returned listing successful and failed operations.
+  */
+class MetaDataFileRemoveActor extends Actor {
+  this: ChildActorFactory =>
+
+  /**
+    * Stores the current child actor which does the actual removing. The child
+    * is created on demand. It has to be recreated for failed remove
+    * operations.
+    */
+  private var removeActorCache: Option[ActorRef] = None
+
+  /**
+    * A list with the requests to be processed. A list is required if there
+    * are multiple requests concurrently.
+    */
+  private var requests = List.empty[RemoveRequestData]
+
+  /**
+    * A set with the file IDs of the current request that could be deleted
+    * successfully.
+    */
+  private var deletedFiles = Set.empty[String]
+
+  /**
+    * A list with the files to be deleted for the currently processed
+    * request.
+    */
+  private var deleteList = List.empty[String]
+
+  /**
+    * Stores the path that is currently deleted. This is used to find out
+    * whether a ''FileRemoved'' message is for the correct path.
+    */
+  private var pathInProgress: Path = _
+
+  /**
+    * A supervisor strategy which ensures that the child remove actor is
+    * stopped when an error occurs.
+    */
+  override val supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    case _ => Stop
+  }
+
+  override def receive: Receive = {
+    case req: RemoveMetaDataFiles =>
+      requests = requests :+ RemoveRequestData(req, sender())
+      if (deleteList.isEmpty) {
+        processNextRequest()
+      }
+
+    case RemoveFileActor.FileRemoved(path) if path == pathInProgress =>
+      deletedFiles += deleteList.head
+      fileProcessed()
+      processNextFile()
+
+    case Terminated(_) =>
+      removeActorCache = None
+      fileProcessed()
+      processNextFile()
+  }
+
+  /**
+    * Processes the next file to be deleted.
+    */
+  private def processNextFile(): Unit = {
+    deleteList match {
+      case h :: _ =>
+        val (removeActor, cache) = fetchRemoveActor(removeActorCache)
+        pathInProgress = requests.head.request.pathMapping(h)
+        removeActor ! RemoveFileActor.RemoveFile(pathInProgress)
+        removeActorCache = cache
+      case Nil =>
+        requests.head.client ! RemoveMetaDataFilesResult(requests.head.request,
+          deletedFiles)
+        requests = requests.tail
+        processNextRequest()
+    }
+  }
+
+  /**
+    * Updates the actor's state after a file has been removed.
+    */
+  private def fileProcessed(): Unit = {
+    pathInProgress = null
+    deleteList = deleteList.tail
+  }
+
+  /**
+    * Processes the next request if there is any in the queue.
+    */
+  private def processNextRequest(): Unit = {
+    requests.headOption foreach { d =>
+      deleteList = fetchFilesToBeRemoved(d.request)
+      deletedFiles = Set.empty
+      processNextFile()
+    }
+  }
+
+  /**
+    * Obtains a list of files to be deleted from the given request. This
+    * function also filters out invalid files that are not contained in the
+    * path mapping.
+    *
+    * @param req the request
+    * @return a list with files to be deleted
+    */
+  private def fetchFilesToBeRemoved(req: RemoveMetaDataFiles): List[String] =
+  req.fileIDs.filter(req.pathMapping.contains).toList
+
+  /**
+    * Returns a reference to the child actor for remove operations and an
+    * updated state for the cached actor reference. The child actor is
+    * created if necessary, and death watch is established.
+    *
+    * @param childRef the cached child actor reference
+    * @return the child actor and the updated reference cache
+    */
+  private def fetchRemoveActor(childRef: Option[ActorRef]): (ActorRef, Option[ActorRef]) =
+  childRef match {
+    case Some(r) => (r, childRef)
+    case None =>
+      val child = createChildActor(Props[RemoveFileActor])
+      context watch child
+      (child, Some(child))
+  }
+}
---
+++ archive/src/test/scala/de/oliver_heger/linedj/archive/metadata/persistence/MetaDataFileRemoveActorSpec.scala
@@ -0,0 +1,267 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.metadata.persistence
+
+import java.nio.file.{Path, Paths}
+import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue, TimeUnit}
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit, TestProbe}
+import de.oliver_heger.linedj.io.RemoveFileActor
+import de.oliver_heger.linedj.utils.ChildActorFactory
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object MetaDataFileRemoveActorSpec {
+  /** The number of test paths. */
+  private val TestPathCount = 16
+
+  /** The test checksum to path mapping. */
+  private val ChecksumMapping = createChecksumMapping()
+
+  /**
+    * Creates a test path with the given index.
+    *
+    * @param idx the index
+    * @return the test path with this index
+    */
+  private def createPath(idx: Int): Path = Paths.get("test", "file" + idx)
+
+  /**
+    * Creates a set of test paths in the given index range.
+    *
+    * @param fromIdx the from index (inclusive)
+    * @param toIdx   the end index (exclusive)
+    * @return the set with generated test paths
+    */
+  private def createPathSet(fromIdx: Int, toIdx: Int): Set[Path] =
+  (fromIdx until toIdx).map(createPath).toSet
+
+  /**
+    * Creates a test checksum.
+    *
+    * @param idx the index
+    * @return the checksum with this index
+    */
+  private def createChecksum(idx: Int): String = "Check" + idx
+
+  /**
+    * Creates a set of checksum values in the given index range.
+    *
+    * @param fromIdx the from index (inclusive)
+    * @param toIdx   the end index (exclusive)
+    * @return the set with generated test checksum values
+    */
+  private def createChecksumSet(fromIdx: Int, toIdx: Int): Set[String] =
+  (fromIdx until toIdx).map(createChecksum).toSet
+
+  /**
+    * Creates a mapping with test checksum values and paths.
+    *
+    * @return the mapping
+    */
+  private def createChecksumMapping(): Map[String, Path] =
+  (1 to TestPathCount).map(i => (createChecksum(i), createPath(i))).toMap
+
+  /**
+    * A simple actor implementation which simulates a remove file actor. The
+    * actor reports successful removals and tracks the passed in files in a
+    * queue.
+    *
+    * @param queue the queue for tracking removed paths
+    */
+  private class SimulatedRemoveFileActor(queue: BlockingQueue[Path]) extends Actor {
+    override def receive: Receive = {
+      case RemoveFileActor.RemoveFile(path) =>
+        queue offer path
+        sender ! RemoveFileActor.FileRemoved(path)
+    }
+  }
+
+}
+
+/**
+  * Test class for ''MetaDataFileRemoveActor''.
+  */
+class MetaDataFileRemoveActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers {
+
+  import MetaDataFileRemoveActorSpec._
+
+  def this() = this(ActorSystem("MetaDataFileRemoveActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  /**
+    * Creates a simulated file remove actor and returns a reference to it and
+    * the queue used to track remove operations.
+    *
+    * @return the actor reference and the tracking queue
+    */
+  private def createRemoveActor(): (ActorRef, BlockingQueue[Path]) = {
+    val queue = new LinkedBlockingQueue[Path]
+    val actor = system.actorOf(Props(classOf[SimulatedRemoveFileActor], queue))
+    (actor, queue)
+  }
+
+  /**
+    * Creates a test actor instance with a child actor factory returning the
+    * specified child actors.
+    *
+    * @return the test actor
+    */
+  private def createTestActor(childActors: List[ActorRef]):
+  TestActorRef[MetaDataFileRemoveActor] = {
+    val props = Props(new MetaDataFileRemoveActor with ChildActorFactory {
+      private var childActorList = childActors
+
+      override def createChildActor(p: Props): ActorRef = {
+        classOf[RemoveFileActor] isAssignableFrom p.actorClass() shouldBe true
+        p.args shouldBe 'empty
+        val child = childActorList.head
+        childActorList = childActorList.tail
+        child
+      }
+    })
+    TestActorRef[MetaDataFileRemoveActor](props)
+  }
+
+  /**
+    * Reads an element from the given queue with a timeout. Fails if no
+    * element is found.
+    *
+    * @param queue the queue
+    * @return the element that has been read
+    */
+  private def readQueue(queue: BlockingQueue[Path]): Path = {
+    awaitCond(!queue.isEmpty)
+    val result = queue.poll(1, TimeUnit.SECONDS)
+    result should not be null
+    result
+  }
+
+  /**
+    * Expects that the given number of remove operations have been tracked by
+    * the specified queue.
+    *
+    * @param queue the queue
+    * @param count the number of remove operations
+    * @return a set with the paths of the tracked remove operations
+    */
+  private def expectRemovedFiles(queue: BlockingQueue[Path], count: Int): Set[Path] =
+  (1 to count).map(_ => readQueue(queue)).toSet
+
+  "A MetaDataFileRemoveActor" should "create correct properties" in {
+    val props = MetaDataFileRemoveActor()
+
+    classOf[MetaDataFileRemoveActor] isAssignableFrom props.actorClass() shouldBe true
+    classOf[ChildActorFactory] isAssignableFrom props.actorClass() shouldBe true
+  }
+
+  it should "process a single remove request" in {
+    val removeFiles = createChecksumSet(1, TestPathCount)
+    val request = MetaDataFileRemoveActor.RemoveMetaDataFiles(removeFiles,
+      ChecksumMapping, testActor)
+    val (childActor, queue) = createRemoveActor()
+    val removeActor = createTestActor(List(childActor))
+
+    removeActor ! request
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request, removeFiles))
+    expectRemovedFiles(queue, removeFiles.size) should be(createPathSet(1, TestPathCount))
+  }
+
+  it should "handle a request with an empty file list" in {
+    val request = MetaDataFileRemoveActor.RemoveMetaDataFiles(Set.empty, ChecksumMapping,
+      testActor)
+    val (childActor, _) = createRemoveActor()
+    val removeActor = createTestActor(List(childActor))
+
+    removeActor ! request
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request, Set.empty))
+  }
+
+  it should "process multiple remove requests" in {
+    val removeFiles1 = createChecksumSet(1, TestPathCount / 2)
+    val removeFiles2 = createChecksumSet(TestPathCount / 2, TestPathCount + 1)
+    val request1 = MetaDataFileRemoveActor.RemoveMetaDataFiles(removeFiles1,
+      ChecksumMapping, testActor)
+    val request2 = MetaDataFileRemoveActor.RemoveMetaDataFiles(removeFiles2,
+      ChecksumMapping, testActor)
+    val (childActor, queue) = createRemoveActor()
+    val removeActor = createTestActor(List(childActor))
+
+    removeActor ! request1
+    removeActor ! request2
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request1, removeFiles1))
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request2, removeFiles2))
+    expectRemovedFiles(queue, TestPathCount) should be(createPathSet(1, TestPathCount + 1))
+  }
+
+  it should "handle failed delete operations" in {
+    val removeFiles = createChecksumSet(1, TestPathCount)
+    val request = MetaDataFileRemoveActor.RemoveMetaDataFiles(removeFiles,
+      ChecksumMapping, testActor)
+    val removeActor = system.actorOf(MetaDataFileRemoveActor())
+
+    removeActor ! request
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request, Set.empty))
+  }
+
+  it should "ignore unexpected FileRemoved messages" in {
+    val removeFiles = createChecksumSet(2, 4)
+    val request = MetaDataFileRemoveActor.RemoveMetaDataFiles(removeFiles,
+      ChecksumMapping, testActor)
+    val probeChild = TestProbe()
+    val removeActor1 = createTestActor(List(probeChild.ref))
+    removeActor1 ! MetaDataFileRemoveActor.RemoveMetaDataFiles(createChecksumSet(1, 2),
+      ChecksumMapping, testActor)
+    probeChild.expectMsgType[RemoveFileActor.RemoveFile]
+    val (childActor, _) = createRemoveActor()
+    val removeActor2 = createTestActor(List(childActor))
+
+    removeActor1 ! RemoveFileActor.FileRemoved(createPath(28))
+    removeActor2 ! request
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request, removeFiles))
+  }
+
+  it should "reset the path in progress" in {
+    val (childActor, _) = createRemoveActor()
+    val removeActor = createTestActor(List(childActor))
+    removeActor ! MetaDataFileRemoveActor.RemoveMetaDataFiles(Set(createChecksum(1)),
+      ChecksumMapping, testActor)
+    expectMsgType[MetaDataFileRemoveActor.RemoveMetaDataFilesResult]
+
+    removeActor receive RemoveFileActor.FileRemoved(createPath(1))
+    val request = MetaDataFileRemoveActor.RemoveMetaDataFiles(createChecksumSet(2, 5),
+      ChecksumMapping, testActor)
+    removeActor ! request
+    expectMsgType[MetaDataFileRemoveActor.RemoveMetaDataFilesResult].request should be(request)
+  }
+
+  it should "ignore files that are not contained in the path mapping" in {
+    val removeFiles = createChecksumSet(1, 5)
+    val request = MetaDataFileRemoveActor.RemoveMetaDataFiles(removeFiles + createChecksum(111),
+      ChecksumMapping, testActor)
+    val (child, queue) = createRemoveActor()
+    val removeActor = createTestActor(List(child))
+
+    removeActor ! request
+    expectMsg(MetaDataFileRemoveActor.RemoveMetaDataFilesResult(request, removeFiles))
+    expectRemovedFiles(queue, removeFiles.size) should be(createPathSet(1, 5))
+  }
+}
