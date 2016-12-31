9d5858dc588a33c73db8697cfe32655eee5c7f06
oheger <oliver.heger@oliver-heger.de>
2016-12-31 17:24:54 +0100

    Added support classes for operations with the system tray.
    The SystemTray class is hard to test. Therefore, it is accessed via
    these helper classes.

---
+++ trayWindowList/src/main/scala/de/oliver_heger/linedj/platform/app/tray/wndlist/TrayHandler.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app.tray.wndlist
+
+import java.awt.{Image, PopupMenu, SystemTray, TrayIcon}
+
+import org.slf4j.LoggerFactory
+
+/**
+  * An internally used helper trait which wraps access to the system tray.
+  *
+  * This trait is used rather than direct access to the tray, which typically
+  * requires static method calls. When using the trait code is easier to test.
+  */
+private trait TrayHandler {
+  /**
+    * Adds an icon to the tray and returns a handler object that can be used to
+    * manipulate it later. If it is not possible to add the icon, ''None'' is
+    * returned.
+    *
+    * @param icon the image of the tray icon
+    * @param tip  a tool tip for the icon
+    * @return an option for the icon handler
+    */
+  def addIcon(icon: Image, tip: String): Option[TrayIconHandler]
+}
+
+/**
+  * Default implementation of the ''TrayHandler'' trait.
+  */
+private object TrayHandlerImpl extends TrayHandler {
+  /** Logger. */
+  private val Log = LoggerFactory.getLogger(getClass)
+
+  /**
+    * Adds an icon to the tray and returns a handler object that can be used to
+    * manipulate it later. If it is not possible to add the icon, ''None'' is
+    * returned.
+    *
+    * @param icon the image of the tray icon
+    * @param tip  a tool tip for the icon
+    * @return an option for the icon handler
+    */
+  override def addIcon(icon: Image, tip: String): Option[TrayIconHandler] =
+    fetchSystemTray() flatMap (t => addIconToTray(t, icon, tip))
+
+  /**
+    * Implementation of the ''addIcon()'' method assuming that the system tray
+    * has already been obtained.
+    *
+    * @param tray the system tray
+    * @param icon the icon
+    * @param tip  the tool tip
+    * @return the icon handler
+    */
+  def addIconToTray(tray: SystemTray, icon: Image, tip: String): Option[TrayIconHandler] = {
+    Log.info("Adding system tray icon.")
+    val trayIcon = new TrayIcon(icon, tip)
+    try {
+      tray add trayIcon
+      Some(new TrayIconHandler(trayIcon, tray))
+    } catch {
+      case ex: Exception =>
+        Log.error("Could not add system tray icon!", ex)
+        None
+    }
+  }
+
+  /**
+    * Tries to obtain the system tray. If it is not supported, result is
+    * ''None''.
+    *
+    * @return an option for the system tray
+    */
+  private def fetchSystemTray(): Option[SystemTray] =
+    try Some(SystemTray.getSystemTray)
+    catch {
+      case ex: Exception =>
+        Log.error("Could not obtain system tray!", ex)
+        None
+    }
+}
+
+/**
+  * A wrapper around a ''TrayIcon'' object.
+  *
+  * An instance can be used to manipulate the icon and to remove it when it is
+  * no longer needed.
+  *
+  * @param icon the wrapped ''TrayIcon''
+  * @param tray the ''SystemTray''
+  */
+private class TrayIconHandler(val icon: TrayIcon, val tray: SystemTray) {
+  def updateMenu(menu: PopupMenu): Unit = {
+    icon setPopupMenu menu
+  }
+
+  /**
+    * Removes the wrapped icon from the system tray. This method should be
+    * called once when the icon is no longer needed.
+    */
+  def remove(): Unit = {
+    tray remove icon
+  }
+}
---
+++ trayWindowList/src/test/scala/de/oliver_heger/linedj/platform/app/tray/wndlist/TrayHandlerSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2015-2016 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app.tray.wndlist
+
+import java.awt.{Image, PopupMenu, SystemTray, TrayIcon}
+import javax.swing.ImageIcon
+
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.any
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+object TrayHandlerSpec {
+  /** A test tool tip. */
+  private val Tip = "Tray Tool Tip"
+
+  /**
+    * Returns an image to be used for the tray icon.
+    *
+    * @return the image
+    */
+  private def createImage(): Image = {
+    val url = classOf[TrayHandler].getResource("/icon.png")
+    new ImageIcon(url).getImage
+  }
+}
+
+/**
+  * Test class for ''TrayHandler''.
+  */
+class TrayHandlerSpec extends FlatSpec with Matchers with MockitoSugar {
+
+  import TrayHandlerSpec._
+
+  "TrayHandlerImpl" should "create a correct icon handler" in {
+    val tray = mock[SystemTray]
+    val image = createImage()
+
+    val handler = TrayHandlerImpl.addIconToTray(tray, image, Tip).get
+    val captor = ArgumentCaptor.forClass(classOf[TrayIcon])
+    verify(tray).add(captor.capture())
+    handler.tray should be(tray)
+    handler.icon should be(captor.getValue)
+    handler.icon.getImage should be(image)
+    handler.icon.getToolTip should be(Tip)
+  }
+
+  it should "handle an exception when adding the icon to the tray" in {
+    val tray = mock[SystemTray]
+    when(tray.add(any(classOf[TrayIcon]))).thenThrow(new IllegalArgumentException)
+
+    TrayHandlerImpl.addIconToTray(tray, createImage(), Tip) shouldBe 'empty
+  }
+
+  it should "do a complete add icon operation" in {
+    // as the tray may not be available, we can only check that no exception
+    // is thrown
+    val handler = TrayHandlerImpl.addIcon(createImage(), Tip)
+    handler foreach (_.remove())
+  }
+
+  "A TrayIconHandler" should "remove the icon from the tray" in {
+    val tray = mock[SystemTray]
+    val icon = mock[TrayIcon]
+    val handler = new TrayIconHandler(icon, tray)
+
+    handler.remove()
+    verify(tray).remove(icon)
+  }
+
+  it should "set the icon's popup menu" in {
+    val icon = new TrayIcon(createImage())
+    val menu = new PopupMenu
+    val handler = new TrayIconHandler(icon, mock[SystemTray])
+
+    handler updateMenu menu
+    icon.getPopupMenu should be(menu)
+  }
+}
