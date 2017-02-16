5ff2851aa6b4b5110314b2678f8584edbd78248a
oheger <oliver.heger@oliver-heger.de>
2017-02-16 21:44:23 +0100

    Added ActorManagement trait.
    This trait can be mixed in platform component implementations. It
    manages actors created by the component and stops them automatically
    when the component is deactivated.

---
+++ platform/src/main/scala/de/oliver_heger/linedj/platform/app/ActorManagement.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import java.util.concurrent.ConcurrentHashMap
+
+import akka.actor.{ActorRef, Props}
+import org.osgi.service.component.ComponentContext
+
+/**
+  * A trait supporting a [[PlatformComponent]] with actor management.
+  *
+  * A platform component that creates actors during its life time should make
+  * sure that these actors are stopped when itself is stopped. This trait
+  * helps to achieve this. It can be mixed in and provides an implementation of
+  * ''deactivate()'' that stops all actors which have been registered before by
+  * calling the ''registerActor()'' method.
+  *
+  * So a class that wants to use this functionality just has to call
+  * ''registerActor()'' for each actor it wants to have managed. (If an actor
+  * is stopped by the class itself or via some other means, it does not have to
+  * be registered.) Registered actors are stored in a thread-safe map using
+  * their names as keys, so that they can be accessed from every thread.
+  * Stopping of actors has to be done in the OSGi management thread when the
+  * component is deactivated, but actors are typically created in other
+  * threads; so thread-safe access is mandatory.
+  *
+  * An concrete subclass can rely on the storage of actors and access the
+  * managed actor references via the ''getActor()'' method. However, due to the
+  * thread-safe access, there might be a certain overhead which might not be
+  * necessary if actors are only accessed from a specific thread, e.g. the UI
+  * thread.
+  */
+trait ActorManagement extends PlatformComponent {
+  /** A map for storing registered actors. */
+  private val managedActors = new ConcurrentHashMap[String, ActorRef]
+
+  /**
+    * Registers the specified actor under the given key. The actor can then be
+    * queried via ''getActor()'', and it is stopped when this component is
+    * deactivated.
+    *
+    * @param name  the name of the actor
+    * @param actor the actor
+    * @return the passed in actor
+    */
+  def registerActor(name: String, actor: ActorRef): ActorRef = {
+    managedActors.put(name, actor)
+    actor
+  }
+
+  /**
+    * Creates a new actor based on the specified parameters (using the actor
+    * factory of the ''ClientApplicationContext'') and registers it. This is a
+    * convenience method allowing the creation and registration of an actor in
+    * a single step.
+    *
+    * @param props creation properties for the actor
+    * @param name  the actor's name
+    * @return the newly created actor
+    */
+  def createAndRegisterActor(props: Props, name: String): ActorRef =
+    registerActor(name, clientApplicationContext.actorFactory.createActor(props, name))
+
+  /**
+    * Returns the actor reference for the specified name. This actor must have
+    * been registered before; otherwise, a ''NoSuchElementException''
+    * exception is thrown.
+    *
+    * @param name the name of the desired actor
+    * @return the reference to this actor
+    */
+  def getActor(name: String): ActorRef = {
+    val ref = managedActors get name
+    if (ref == null)
+      throw new NoSuchElementException(s"No actor registered with name '$name'!")
+    ref
+  }
+
+  /**
+    * @inheritdoc This implementation stops all actors that have been
+    *             registered.
+    */
+  abstract override def deactivate(componentContext: ComponentContext): Unit = {
+    stopActors()
+    super.deactivate(componentContext)
+  }
+
+  /**
+    * Stops all managed actors and removes them from the internal map. This
+    * method is called by ''deactivate()''. It can also be called by derived
+    * classes if they want to reset their actors in the middle of their
+    * life cycle.
+    */
+  protected def stopActors(): Unit = {
+    import scala.collection.JavaConverters._
+    val actors = managedActors.values().asScala
+    actors foreach (a => clientApplicationContext.actorSystem stop a)
+    managedActors.clear()
+  }
+}
---
+++ platform/src/test/scala/de/oliver_heger/linedj/platform/app/ActorManagementSpec.scala
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{TestKit, TestProbe}
+import de.oliver_heger.linedj.io.FileReaderActor
+import org.mockito.Mockito._
+import org.osgi.service.component.ComponentContext
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object ActorManagementSpec {
+  /** Prefix for an actor name. */
+  private val ActorName = "testActor_"
+
+  /**
+    * Generates an actor name based on the given index.
+    *
+    * @param idx the index
+    * @return the actor name
+    */
+  private def genActorName(idx: Int): String = ActorName + idx
+
+  private class ComponentTestImpl extends PlatformComponent with ActorManagement {
+    /** A counter for invocations of ''deactivate()''. */
+    var deactivateCounter = 0
+
+    /**
+      * @inheritdoc This implementation is used to check whether the
+      *             original deactivate logic is correctly invoked.
+      */
+    override def deactivate(componentContext: ComponentContext): Unit = {
+      deactivateCounter += 1
+      super.deactivate(componentContext)
+    }
+
+    /**
+      * Overridden to increase visibility.
+      */
+    override def stopActors(): Unit = super.stopActors()
+  }
+}
+
+/**
+  * Test class for ''ActorManagement''.
+  */
+class ActorManagementSpec(testSystem: ActorSystem) extends TestKit(testSystem) with FlatSpecLike
+  with BeforeAndAfterAll with Matchers with MockitoSugar {
+
+  import ActorManagementSpec._
+
+  def this() = this(ActorSystem("ActorManagementSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  "An ActorManagement" should "call the original deactivate() method" in {
+    val helper = new ActorManagementTestHelper
+
+    helper.deactivateComponent().checkDeactivation()
+  }
+
+  it should "make registered actors accessible" in {
+    val helper = new ActorManagementTestHelper
+
+    val (name1, actor1) = helper.registerActor()
+    val (name2, actor2) = helper.registerActor()
+    helper.component.getActor(name1) should be(actor1)
+    helper.component.getActor(name2) should be(actor2)
+  }
+
+  it should "throw an exception when querying an unknown actor" in {
+    val helper = new ActorManagementTestHelper
+
+    intercept[NoSuchElementException] {
+      helper.component getActor genActorName(1)
+    }
+  }
+
+  it should "stop registered actors" in {
+    val helper = new ActorManagementTestHelper
+    val (_, actor1) = helper.registerActor()
+    val (_, actor2) = helper.registerActor()
+
+    helper.deactivateComponent()
+      .checkActorsStopped(actor1, actor2)
+  }
+
+  it should "allow creating and registering actors" in {
+    val helper = new ActorManagementTestHelper
+    val probe = TestProbe()
+    val name = genActorName(42)
+    val props = Props[FileReaderActor]
+    when(helper.clientContext.actorFactory.createActor(props, name)).thenReturn(probe.ref)
+
+    val actor = helper.component.createAndRegisterActor(props, name)
+    actor should be(probe.ref)
+    helper.component getActor name should be(actor)
+  }
+
+  it should "allow stopping managed actors directly" in {
+    val helper = new ActorManagementTestHelper
+    val (name, actor) = helper.registerActor()
+
+    helper.component.stopActors()
+    helper.checkActorsStopped(actor)
+    intercept[NoSuchElementException] {
+      helper.component getActor name
+    }
+  }
+
+  /**
+    * A helper class managing a test instance and its dependencies.
+    */
+  private class ActorManagementTestHelper {
+    /** The application context. */
+    val clientContext = new ClientApplicationContextImpl
+
+    /** A mock for the OSGi component context. */
+    val componentContext: ComponentContext = mock[ComponentContext]
+
+    /** The instance to be tested. */
+    val component: ComponentTestImpl = createTestInstance()
+
+    /** A counter for generating actor names. */
+    private var actorCount = 0
+
+    /**
+      * Calls ''deactivate()'' on the test component.
+      *
+      * @return this test helper
+      */
+    def deactivateComponent(): ActorManagementTestHelper = {
+      component deactivate componentContext
+      this
+    }
+
+    /**
+      * Checks whether deactivation logic was invoked on the test instance.
+      *
+      * @return this test helper
+      */
+    def checkDeactivation(): ActorManagementTestHelper = {
+      component.deactivateCounter should be(1)
+      this
+    }
+
+    /**
+      * Creates a mock actor and registers it at the test component.
+      *
+      * @return a pair of the actor name and the actor reference
+      */
+    def registerActor(): (String, ActorRef) = {
+      actorCount += 1
+      val name = genActorName(actorCount)
+      val actor = TestProbe().ref
+      component.registerActor(name, actor)
+      (name, actor)
+    }
+
+    /**
+      * Checks whether all of the specified actors have been stopped.
+      *
+      * @param refs the expected actor references
+      * @return this test helper
+      */
+    def checkActorsStopped(refs: ActorRef*): ActorManagementTestHelper = {
+      refs foreach (r => verify(clientContext.actorSystem).stop(r))
+      this
+    }
+
+    /**
+      * Creates a test instance.
+      *
+      * @return the test instance
+      */
+    private def createTestInstance(): ComponentTestImpl = {
+      val comp = new ComponentTestImpl
+      comp initClientContext clientContext
+      comp
+    }
+  }
+
+}
