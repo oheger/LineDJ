b8d1542da128c27e6093b3b4ea3f562d57e2218a
oheger <oliver.heger@oliver-heger.de>
2017-03-18 16:55:12 +0100

    Added HttpMediumDescParser class.
    This is a parser for the content document of an HTTP archive. It
    transforms the JSON-based content document into a sequence of
    HttpMediumDesc objects.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/HttpMediumDescParser.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import de.oliver_heger.linedj.archivecommon.parser.{AbstractModelParser, ChunkParser, JSONParser,
+ParserTypes}
+import de.oliver_heger.linedj.archivecommon.parser.ParserTypes.Failure
+
+object HttpMediumDescParser {
+  /** Property for the path to a medium description file. */
+  private val PropDescriptionPath = "mediumDescriptionPath"
+
+  /** Property for the path to a meta data file. */
+  private val PropMetaDataPath = "metaDataPath"
+
+  /**
+    * Converts a map representing a JSON object to an ''HttpMediumDesc''
+    * object.
+    *
+    * @param m the map
+    * @return the resulting ''HttpMediumDesc''
+    */
+  private def convertDescription(m: Map[String, String]): HttpMediumDesc =
+    HttpMediumDesc(m(PropDescriptionPath), m(PropMetaDataPath))
+}
+
+/**
+  * A class for parsing the content document of an HTTP archive.
+  *
+  * An HTTP archive contains a JSON-based document with descriptions of the
+  * media it stores. This parser can process this document and convert the data
+  * to a sequence of [[HttpMediumDesc]] objects.
+  *
+  * @param chunkParser the underlying ''ChunkParser''
+  * @param jsonParser  the underlying JSON parser
+  */
+class HttpMediumDescParser(chunkParser: ChunkParser[ParserTypes.Parser, ParserTypes.Result,
+  Failure], jsonParser: ParserTypes.Parser[JSONParser.JSONData])
+  extends AbstractModelParser[HttpMediumDesc, AnyRef](chunkParser, jsonParser) {
+
+  import HttpMediumDescParser._
+
+  /**
+    * @inheritdoc This implementation converts the passed in maps to objects of
+    *             type [[HttpMediumDesc]]. Only objects with all properties are
+    *             taken into account. The data parameter is not needed here.
+    */
+  override def convertJsonObjects(data: AnyRef, objects: IndexedSeq[Map[String, String]]):
+  IndexedSeq[HttpMediumDesc] =
+    objects filter { m =>
+      m.contains(PropDescriptionPath) && m.contains(PropMetaDataPath)
+    } map convertDescription
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/HttpMediumDescParserSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import de.oliver_heger.linedj.archivecommon.parser.{JSONParser, ParserImpl}
+import org.scalatest.{FlatSpec, Matchers}
+
+object HttpMediumDescParserSpec {
+  /**
+    * Converts a medium description object to a map.
+    *
+    * @param desc the description object
+    * @return the map with the properties of this object
+    */
+  private def toMap(desc: HttpMediumDesc): Map[String, String] =
+    Map("mediumDescriptionPath" -> desc.mediumDescriptionPath,
+      "metaDataPath" -> desc.metaDataPath)
+
+  /**
+    * Creates a new parser instance.
+    *
+    * @return the new parser
+    */
+  private def createParser(): HttpMediumDescParser =
+    new HttpMediumDescParser(ParserImpl, JSONParser.jsonParser(ParserImpl))
+}
+
+/**
+  * Test class for ''HttpMediumDescParser''.
+  */
+class HttpMediumDescParserSpec extends FlatSpec with Matchers {
+
+  import HttpMediumDescParserSpec._
+
+  "A HttpMediumDescParser" should "produce correct result objects" in {
+    val descs = Vector(HttpMediumDesc("medium1/desc.settings", "metadata/med1.mdt"),
+      HttpMediumDesc("medium2/playlist.settings", "metadata/med2.mdt"),
+      HttpMediumDesc("medium3/desc.settings", "metadata/45848481.mdt"))
+
+    val result = createParser().convertJsonObjects(null, descs map toMap)
+    result should be(descs)
+  }
+
+  it should "filter out incomplete medium descriptions" in {
+    val valid = Vector(HttpMediumDesc("medium1/desc.settings", "metadata/med1.mdt"),
+      HttpMediumDesc("medium3/desc.settings", "metadata/45848481.mdt"))
+    val descs = Map("mediumDescriptionPath" -> "somePath") +: valid.map(toMap)
+    val moreDescs = descs :+ Map("metaDataPath" -> "anotherPath")
+
+    val result = createParser().convertJsonObjects(null, moreDescs)
+    result should be(valid)
+  }
+}
