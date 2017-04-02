3bcee254f3b35e83707d75251c3f4a5d90f4b7e4
oheger <oliver.heger@oliver-heger.de>
2017-04-02 21:57:18 +0200

    Extracted a base class from MetaDataResponseProcessingActor.
    This new class - AbstractResponseProcessingActor - implements common
    functionality for actors that process responses for files requested
    from an HTTP archive.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/AbstractResponseProcessingActor.scala
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import akka.actor.{Actor, ActorRef}
+import akka.http.scaladsl.model.HttpResponse
+import akka.stream.scaladsl.Source
+import akka.stream.{ActorMaterializer, KillSwitch}
+import akka.util.ByteString
+import de.oliver_heger.linedj.archivehttp.impl.AbstractResponseProcessingActor.StreamCompleted
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+
+object AbstractResponseProcessingActor {
+
+  /**
+    * An internally used message class that is sent when a stream completes.
+    * In this case, the associated ''KillSwitch'' has to be removed.
+    *
+    * @param client       the client actor
+    * @param result       the result of stream processing
+    * @param killSwitchID the ID of the kill switch
+    */
+  private case class StreamCompleted(client: ActorRef, result: Any, killSwitchID: Int)
+
+}
+
+/**
+  * An abstract base actor class for processing responses for meta data or
+  * settings files received from an HTTP archive.
+  *
+  * A lot of functionality related to response handling is independent on the
+  * concrete file type that has been requested. For instance, failed responses
+  * have to be handled, support for cancellation has to be implemented,
+  * processing results have to be sent to the calling actor, etc. This base
+  * class implements this common functionality. Concrete subclasses mainly have
+  * to deal with setting up a stream to process the content of the response
+  * entity and to produce the results to be sent back to the caller.
+  *
+  * @param fileType a name for the file type processed by this actor; this is
+  *                 used when generating error messages
+  */
+abstract class AbstractResponseProcessingActor(val fileType: String) extends Actor
+  with CancelableStreamSupport {
+  /** The object for stream materialization. */
+  protected implicit val mat = ActorMaterializer()
+
+  import context.dispatcher
+
+  override def receive: Receive = {
+    case ProcessResponse(mid, triedResponse, config) =>
+      handleHttpResponse(mid, triedResponse, config)
+
+    case CancelProcessing =>
+      cancelCurrentStreams()
+
+    case StreamCompleted(client, result, killSwitchID) =>
+      client ! result
+      unregisterKillSwitch(killSwitchID)
+  }
+
+  /**
+    * Processes the specified source with the content of the response entity
+    * and returns a result and an object to cancel processing on an external
+    * request. This method has to be implemented by concrete subclasses to
+    * actually process the result of the requested file. When the returned
+    * ''Future'' completes its result is sent to the calling actor. The
+    * ''KillSwitch'' is stored temporarily, so that the stream can be canceled
+    * if necessary.
+    *
+    * @param source the source to be processed
+    * @param mid    the current ''MediumID''
+    * @return a ''Future'' for the processing result and a ''KillSwitch''
+    */
+  protected def processSource(source: Source[ByteString, Any], mid: MediumID):
+  (Future[Any], KillSwitch)
+
+  /**
+    * Creates the source for the stream of the response's data bytes.
+    *
+    * @param mid      the medium ID
+    * @param response the response
+    * @param config   the configuration of the HTTP archive
+    * @return the source of the stream for the response's data bytes
+    */
+  private[impl] def createResponseDataSource(mid: MediumID, response: HttpResponse,
+                                             config: HttpArchiveConfig):
+  Source[ByteString, Any] =
+    response.entity.dataBytes
+      .via(new ResponseSizeRestrictionStage(config.maxContentSize * 1024))
+
+  /**
+    * Handle a HTTP response for a meta data file. Checks whether the response
+    * is successful. If so, its entity is parsed and converted. Otherwise, a
+    * failure message is sent to the sending actor.
+    *
+    * @param mid           the medium ID
+    * @param triedResponse a ''Try'' for the HTTP response
+    * @param config        the HTTP archive configuration
+    */
+  private def handleHttpResponse(mid: MediumID, triedResponse: Try[HttpResponse],
+                                 config: HttpArchiveConfig): Unit = {
+    triedResponse match {
+      case Success(response) =>
+        if (response.status.isSuccess()) {
+          val client = sender()
+          val (futureStream, killSwitch) = processSource(
+            createResponseDataSource(mid, response, config), mid)
+          val killSwitchID = registerKillSwitch(killSwitch)
+          futureStream.onComplete { triedResult =>
+            handleStreamCompletion(mid, client, killSwitchID, triedResult)
+          }
+        } else {
+          sender() ! ResponseProcessingError(mid, fileType,
+            new IllegalStateException(s"Failed response: ${response.status}"))
+        }
+      case Failure(exception) =>
+        sender() ! ResponseProcessingError(mid, fileType, exception)
+    }
+  }
+
+  /**
+    * Handles the result when a stream completes. This method produces either
+    * a success or an error result. A special message is sent to ''self'',
+    * so that the result can be evaluated by the actor and cleanup for the
+    * completed stream can be done.
+    *
+    * @param mid          the medium ID
+    * @param client       the client actor
+    * @param killSwitchID the ID of the kill switch of the stream
+    * @param triedResult  the result of stream processing
+    */
+  private def handleStreamCompletion(mid: MediumID, client: ActorRef, killSwitchID: Int,
+                                     triedResult: Try[Any]): Unit = {
+    val procResult = triedResult match {
+      case Success(result) =>
+        result
+      case Failure(exception) =>
+        ResponseProcessingError(mid, fileType, exception)
+    }
+    self ! StreamCompleted(client, procResult, killSwitchID)
+  }
+}
--- mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/MetaDataResponseProcessingActor.scala
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/MetaDataResponseProcessingActor.scala
@@ -16,32 +16,18 @@
 
 package de.oliver_heger.linedj.archivehttp.impl
 
-import akka.actor.{Actor, ActorRef}
-import akka.http.scaladsl.model.HttpResponse
-import akka.stream.{ActorMaterializer, KillSwitch, KillSwitches}
 import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.{KillSwitch, KillSwitches}
 import akka.util.ByteString
 import de.oliver_heger.linedj.archivecommon.parser._
 import de.oliver_heger.linedj.shared.archive.media.MediumID
 import de.oliver_heger.linedj.shared.archive.union.MetaDataProcessingResult
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success, Try}
 
 object MetaDataResponseProcessingActor {
   /** Constant for the file type processed by this actor. */
   val FileType = "MetaData"
-
-  /**
-    * An internally used message class that is sent when a stream completes.
-    * In this case, the associated ''KillSwitch'' has to be removed.
-    *
-    * @param client       the client actor
-    * @param result       the result of stream processing
-    * @param killSwitchID the ID of the kill switch
-    */
-  private case class StreamCompleted(client: ActorRef, result: Any, killSwitchID: Int)
-
 }
 
 /**
@@ -51,39 +37,17 @@ object MetaDataResponseProcessingActor {
   * [[MetaDataProcessingResult]] objects. This sequence is then passed in a
   * [[MetaDataResponseProcessingResult]] message to the sender actor.
   */
-class MetaDataResponseProcessingActor extends Actor with CancelableStreamSupport {
-
-  import MetaDataResponseProcessingActor._
-
-  /** The object for stream materialization. */
-  private implicit val mat = ActorMaterializer()
-
+class MetaDataResponseProcessingActor
+  extends AbstractResponseProcessingActor(MetaDataResponseProcessingActor.FileType) {
   import context.dispatcher
 
-  override def receive: Receive = {
-    case ProcessResponse(mid, triedResponse, config) =>
-      handleHttpResponse(mid, triedResponse, config)
-
-    case CancelProcessing =>
-      cancelCurrentStreams()
-
-    case StreamCompleted(client, result, killSwitchID) =>
-      client ! result
-      unregisterKillSwitch(killSwitchID)
-  }
-
   /**
-    * Processes the specified source with the content of the response entity
-    * and returns a result and an object to cancel processing on an external
-    * request. When the ''Future'' completes its result is sent to the calling
-    * actor. The ''KillSwitch'' is stored temporarily, so that the stream can
-    * be canceled if necessary.
-    *
-    * @param source the source to be processed
-    * @param mid    the current ''MediumID''
-    * @return a ''Future'' for the processing result and a ''KillSwitch''
+    * @inheritdoc This implementation processes the content of a meta data
+    *             file and parses it into a sequence of
+    *             [[MetaDataProcessingResult]] objects. Based on this, a
+    *             result object is produced.
     */
-  protected def processSource(source: Source[ByteString, Any], mid: MediumID):
+  protected override def processSource(source: Source[ByteString, Any], mid: MediumID):
   (Future[Any], KillSwitch) = {
     val sink = Sink.fold[List[MetaDataProcessingResult],
       MetaDataProcessingResult](List.empty)((lst, r) => r :: lst)
@@ -92,71 +56,5 @@ class MetaDataResponseProcessingActor extends Actor with CancelableStreamSupport
       .toMat(sink)(Keep.both)
       .run
     (futStream.map(MetaDataResponseProcessingResult(mid, _)), killSwitch)
-  }
-
-  /**
-    * Creates the source for the stream of the response's data bytes.
-    *
-    * @param mid      the medium ID
-    * @param response the response
-    * @param config   the configuration of the HTTP archive
-    * @return the source of the stream for the response's data bytes
-    */
-  private[impl] def createResponseDataSource(mid: MediumID, response: HttpResponse,
-                                             config: HttpArchiveConfig):
-  Source[ByteString, Any] =
-    response.entity.dataBytes
-      .via(new ResponseSizeRestrictionStage(config.maxContentSize * 1024))
-
-  /**
-    * Handle a HTTP response for a meta data file. Checks whether the response
-    * is successful. If so, its entity is parsed and converted. Otherwise, a
-    * failure message is sent to the sending actor.
-    *
-    * @param mid           the medium ID
-    * @param triedResponse a ''Try'' for the HTTP response
-    * @param config        the HTTP archive configuration
-    */
-  private def handleHttpResponse(mid: MediumID, triedResponse: Try[HttpResponse],
-                                 config: HttpArchiveConfig): Unit = {
-    triedResponse match {
-      case Success(response) =>
-        if (response.status.isSuccess()) {
-          val client = sender()
-          val (futureStream, killSwitch) = processSource(
-            createResponseDataSource(mid, response, config), mid)
-          val killSwitchID = registerKillSwitch(killSwitch)
-          futureStream.onComplete { triedResult =>
-            handleStreamCompletion(mid, client, killSwitchID, triedResult)
-          }
-        } else {
-          sender() ! ResponseProcessingError(mid, FileType,
-            new IllegalStateException(s"Failed response: ${response.status}"))
-        }
-      case Failure(exception) =>
-        sender() ! ResponseProcessingError(mid, FileType, exception)
-    }
-  }
-
-  /**
-    * Handles the result when a stream completes. This method produces either
-    * a success or an error result. A special message is sent to ''self'',
-    * so that the result can be evaluated by the actor and cleanup for the
-    * completed stream can be done.
-    *
-    * @param mid          the medium ID
-    * @param client       the client actor
-    * @param killSwitchID the ID of the kill switch of the stream
-    * @param triedResult  the result of stream processing
-    */
-  private def handleStreamCompletion(mid: MediumID, client: ActorRef, killSwitchID: Int,
-                                     triedResult: Try[Any]): Unit = {
-    val procResult = triedResult match {
-      case Success(result) =>
-        result
-      case Failure(exception) =>
-        ResponseProcessingError(mid, FileType, exception)
-    }
-    self ! StreamCompleted(client, procResult, killSwitchID)
   }
 }
