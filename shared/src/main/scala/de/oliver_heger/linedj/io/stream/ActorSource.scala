f31b5ef1db058b1afca420d42d422486466bd0a6
oheger <oliver.heger@oliver-heger.de>
2017-06-18 17:27:22 +0200

    Added ActorSource class.
    This is a stream source that wraps an actor that provides data
    using a request-response protocol.

---
+++ shared/src/main/scala/de/oliver_heger/linedj/io/stream/ActorSource.scala
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.io.stream
+
+import akka.NotUsed
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.stream.scaladsl.Source
+import akka.stream.stage.{AsyncCallback, GraphStage, GraphStageLogic, OutHandler}
+import akka.stream.{Attributes, Outlet, SourceShape}
+import akka.util.Timeout
+import de.oliver_heger.linedj.io.stream.ActorSource.{ActorCompletionResult, ActorDataResult,
+ActorErrorResult, ResultFunc}
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+object ActorSource {
+  /**
+    * A default timeout value for invoking the data actor. If no response is
+    * received within this time span, processing stops with a failure.
+    */
+  val DefaultTimeout = Timeout(1.minute)
+
+  /**
+    * A trait to classify a response message received from the wrapped actor.
+    * Responses from the wrapped actor are passed to a transformation
+    * function that returns one of the sub types of this trait. This allows the
+    * source to decide how a specific result is to be handled.
+    *
+    * @tparam A the element type of the source
+    */
+  sealed trait ActorResult[A]
+
+  /**
+    * Data class defining an actor result with a data item to be passed
+    * downstream.
+    *
+    * @param data the data to be passed downstream
+    * @tparam A the element type of the source
+    */
+  case class ActorDataResult[A](data: A) extends ActorResult[A]
+
+  /**
+    * An actor result indicating that no more data is available. The source
+    * will then complete its stage.
+    *
+    * @tparam A the element type of the source
+    */
+  case class ActorCompletionResult[A]() extends ActorResult[A]
+
+  /**
+    * An actor result indicating a processing error. The source will fail with
+    * the specified error message.
+    *
+    * @param exception the exception causing the failure
+    * @tparam A the element type of the source
+    */
+  case class ActorErrorResult[A](exception: Throwable) extends ActorResult[A]
+
+  /**
+    * Type definition for a function that maps a response from the actor to an
+    * ''ActorResult'' object. Such a function has to be provided when
+    * constructing an ''ActorSource'', so that the responses received from the
+    * actor can be interpreted correctly.
+    *
+    * @tparam A the element type of the source
+    */
+  type ResultFunc[A] = Any => ActorResult[A]
+
+  /**
+    * Creates a ''Source'' that wraps the specified actor. Use this method to
+    * construct streams that are controlled by a data-providing actor.
+    *
+    * @param actor      the actor to be wrapped by this source
+    * @param requestMsg the message to request new data from the actor
+    * @param timeout    a timeout when waiting for the actor's response
+    * @param f          a function to map a response to an ''ActorResult''
+    * @param ec         the execution context to deal with futures
+    * @tparam A the type of elements produced by this source
+    * @return the ''Source'' wrapping the provided actor
+    */
+  def apply[A](actor: ActorRef, requestMsg: Any, timeout: Timeout = DefaultTimeout)
+              (f: ResultFunc[A])
+              (implicit ec: ExecutionContext): Source[A, NotUsed] = {
+    val actorSource = new ActorSource[A](actor, requestMsg, timeout)(f)(ec)
+    Source fromGraph actorSource
+  }
+}
+
+/**
+  * A stream source implementation that receives data from a specified actor.
+  *
+  * An instance can be created passing in an actor reference and some
+  * information how to communicate with this actor:
+  *  - A message to be sent to the actor in order to request a chunk of data.
+  * The source sends this message to the actor whenever new data can be
+  * consumed.
+  *  - A function to map a response from the actor to an
+  * [[de.oliver_heger.linedj.io.stream.ActorSource.ActorResult]] object. Based
+  * on this object, the source can decide how to proceed: pass new data
+  * downstream, finish processing, or fail with an exception.
+  *
+  * @param actor        the actor to be wrapped by this source
+  * @param requestMsg   the message to request new data from the actor
+  * @param actorTimeout a timeout when waiting for the actor's response
+  * @param f            a function to map a response to an ''ActorResult''
+  * @param ec           the execution context to deal with futures
+  * @tparam A the type of elements produced by this source
+  */
+class ActorSource[A](actor: ActorRef, requestMsg: Any, actorTimeout: Timeout)
+                    (f: ResultFunc[A])
+                    (implicit ec: ExecutionContext)
+  extends GraphStage[SourceShape[A]] {
+  val out: Outlet[A] = Outlet("ActorSource.out")
+
+  override def shape: SourceShape[A] = SourceShape(out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      var data: Option[A] = None
+
+      var callback: AsyncCallback[Try[Any]] = _
+
+      override def preStart(): Unit = {
+        super.preStart()
+        callback = getAsyncCallback[Try[Any]](handleActorMessage)
+        sendRequest()
+      }
+
+      /**
+        * Sends a request to the actor wrapped by this source.
+        */
+      private def sendRequest(): Unit = {
+        implicit val timeout = actorTimeout
+        data = None
+        val response = actor ? requestMsg
+        response onComplete callback.invoke
+      }
+
+      /**
+        * Handles a response from the wrapped actor. Depending on the
+        * ''ActorResult'' returned by the result function, data is pushed
+        * downstream, the stream completes, or a failure result is produced.
+        *
+        * @param m the response message received from the wrapped actor
+        */
+      private def handleActorMessage(m: Try[Any]): Unit = {
+        m match {
+          case Success(msg) =>
+            f(msg) match {
+              case ActorCompletionResult() =>
+                complete(out)
+              case ActorErrorResult(ex) =>
+                failStage(ex)
+              case ActorDataResult(d) =>
+                if (isAvailable(out)) {
+                  processElement(d)
+                } else {
+                  data = Some(d)
+                }
+            }
+
+          case Failure(ex) =>
+            failStage(ex)
+        }
+      }
+
+      /**
+        * Processes an element to be pushed downstream. Also sends a request
+        * for the next element to the wrapped actor.
+        *
+        * @param d the element
+        */
+      private def processElement(d: A): Unit = {
+        push(out, d)
+        sendRequest()
+      }
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          data match {
+            case Some(d) =>
+              processElement(d)
+            case _ =>
+          }
+        }
+      })
+    }
+}
---
+++ shared/src/test/scala/de/oliver_heger/linedj/io/stream/ActorSourceSpec.scala
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.io.stream
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.AskTimeoutException
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, DelayOverflowStrategy}
+import akka.testkit.TestKit
+import akka.util.Timeout
+import de.oliver_heger.linedj.io.stream.ActorSource.{ActorCompletionResult, ActorDataResult,
+ActorErrorResult}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+/**
+  * Test class for ''ActorSource''.
+  */
+class ActorSourceSpec(testSystem: ActorSystem) extends TestKit(testSystem) with FlatSpecLike with
+  BeforeAndAfterAll with Matchers {
+  def this() = this(ActorSystem("ActorSourceSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  /**
+    * Creates a test source based on a data actor sending the provided test
+    * data.
+    *
+    * @param data the list with data to be sent by the test data actor
+    * @return the source
+    */
+  private def createSource(data: List[String]): Source[String, Any] = {
+    implicit val ec = system.dispatcher
+    val actor = system.actorOf(Props(classOf[DataActor], data))
+    ActorSource[String](actor, Request)(resultFunc())
+  }
+
+  /**
+    * Returns the result function that maps responses from the test data
+    * actor to result objects.
+    *
+    * @return the result function
+    */
+  private def resultFunc(): ActorSource.ResultFunc[String] = {
+    case s: String => ActorDataResult(s)
+    case Completed => ActorCompletionResult()
+    case Error(s) => ActorErrorResult(new Exception(s))
+  }
+
+  /**
+    * Runs a stream with the specified test source.
+    *
+    * @param source the test source
+    * @return a future with the stream result
+    */
+  private def runStream(source: Source[String, Any]): Future[List[String]] = {
+    implicit val mat = ActorMaterializer()
+    val sink = Sink.fold[List[String], String](List.empty[String])((lst, s) => s :: lst)
+    source.runWith(sink)
+  }
+
+  /**
+    * Obtains the result from stream processing.
+    *
+    * @param f the future with the result
+    * @return the result
+    */
+  private def streamResult(f: Future[List[String]]): List[String] =
+    Await.result(f, 3.seconds).reverse
+
+  "An ActorSource" should "return expected data" in {
+    val data = List("This", "is", "test", "data", "for", "the", "ActorSource")
+
+    val result = streamResult(runStream(createSource(data)))
+    result should be(data)
+  }
+
+  it should "handle a slower source" in {
+    val data = List("Test", "data", "from", "a", "slower", "test", "source", "x", "y", "z")
+    val source = createSource(data).delay(50.millis, DelayOverflowStrategy.backpressure)
+
+    val result = streamResult(runStream(source))
+    result should be(data)
+  }
+
+  it should "handle a slower actor response" in {
+    val data = List("Test", "data", DataActor.Delay, "with", DataActor.Delay, "delay")
+
+    val result = streamResult(runStream(createSource(data)))
+    result should be(data)
+  }
+
+  it should "handle an error response" in {
+    val ErrMsg = "Failed processing!"
+    val data = List("Stream", "that", "fails", DataActor.ErrorPrefix + ErrMsg)
+
+    val fut = runStream(createSource(data))
+    val ex = intercept[Exception] {
+      Await.result(fut, 3.seconds)
+    }
+    ex.getMessage should be(ErrMsg)
+  }
+
+  it should "handle a timeout from the data actor" in {
+    val data = List("Stream", "with", "timeout", DataActor.Ignore, "foo")
+    implicit val ec = system.dispatcher
+    val actor = system.actorOf(Props(classOf[DataActor], data))
+    val source = ActorSource[String](actor, Request, Timeout(50.millis))(resultFunc())
+    val now = System.currentTimeMillis()
+
+    val fut = runStream(source)
+    intercept[AskTimeoutException] {
+      Await.result(fut, 10.seconds)
+    }
+    System.currentTimeMillis() - now should be < 5000L
+  }
+}
+
+/** Message to request data. */
+case object Request
+
+/** Message to indicate that the stream is complete. */
+case object Completed
+
+/**
+  * Message to indicate a processing failure.
+  *
+  * @param s the error message
+  */
+case class Error(s: String)
+
+object DataActor {
+  /** Prefix of a data item that causes an error message. */
+  val ErrorPrefix = "err:"
+
+  /** A data item causing a delay. */
+  val Delay = ":delay:"
+
+  /** A data item that causes a timeout because no response is sent. */
+  val Ignore = ":ignore:"
+}
+
+/**
+  * Test actor class providing the data for the test source. The actor returns
+  * the single strings from the provided list and sends a completed message
+  * afterwards. If a string starts with the prefix ''err:'', an error message
+  * is generated.
+  *
+  * @param data the list with data
+  */
+class DataActor(data: List[String]) extends Actor {
+
+  import DataActor._
+
+  private var currentData = data
+
+  override def receive: Receive = {
+    case Request =>
+      val response = currentData match {
+        case h :: t =>
+          currentData = t
+          dataResponse(h)
+        case _ => Some(Completed)
+      }
+      response foreach sender.!
+  }
+
+  /**
+    * Produces a response message for the given data item. Handles some
+    * special items.
+    *
+    * @param s the item
+    * @return the response message
+    */
+  private def dataResponse(s: String): Option[Any] = {
+    if (s startsWith ErrorPrefix) Some(Error(s.substring(ErrorPrefix.length)))
+    else {
+      if (s == Ignore) None
+      else {
+        if (s == Delay) {
+          Thread.sleep(50)
+        }
+        Some(s)
+      }
+    }
+  }
+}
