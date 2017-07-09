6c56485fbd7cbaceb61a7b455a27dd42e043328f
oheger <oliver.heger@oliver-heger.de>
2017-07-09 18:36:54 +0200

    Added TempPathGenerator class.
    This class generates the names for temporary files which are
    needed during HTTP download operations.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/download/TempPathGenerator.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.download
+
+import java.nio.file.Path
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+object TempPathGenerator {
+  /** A prefix used for download files and directories. */
+  val DownloadPrefix = "download-"
+}
+
+/**
+  * A helper class that generates path names for temporary files that are
+  * needed during download operations.
+  *
+  * This class generates path names that follow a specific pattern to ensure
+  * that multiple download operations for multiple HTTP archive instances can
+  * take place in parallel on the same directory for temporary files. To
+  * achieve this, each archive has a sub directory with its name and a
+  * timestamp reflecting the startup time. This is done to avoid conflicts
+  * with remaining temporary files from earlier runs; those files can be
+  * removed on startup, but in parallel new temporary files may need to be
+  * created.
+  *
+  * For each download operation multiple temporary files may have to be
+  * created. Therefore, a path names contains the sequence number for the
+  * download operation and an index for the temporary file in this operation.
+  * The paths are located in the sub directory of the owning archive.
+  *
+  * @param rootPath the root path for temporary files; all generated paths are
+  *                 children of this root path
+  * @param time     the startup time of the current run (this parameter is
+  *                 normally not specified when creating an instance)
+  */
+case class TempPathGenerator(rootPath: Path, time: Instant = Instant.now()) {
+
+  import TempPathGenerator._
+
+  /**
+    * A component for generated paths based on the time. So for each run of the
+    * archive, different paths are going to be generated.
+    */
+  private val timeComponent = DateTimeFormatter.ISO_INSTANT.format(time).replace(':', '_')
+
+  /**
+    * Generates the path to the sub directory with temporary files for the
+    * specified HTTP archive. The archive name is incorporated in the
+    * generated path name; therefore, it should not contain any special
+    * characters.
+    *
+    * @param archiveName the name of the HTTP archive
+    * @return the sub path for temp files for this archive
+    */
+  def generateArchivePath(archiveName: String): Path =
+    rootPath resolve s"$DownloadPrefix$archiveName-$timeComponent"
+
+  /**
+    * Generates the path for a temporary file for a download operation.
+    *
+    * @param archiveName the name of the HTTP archive
+    * @param downloadIdx the index of the download operation
+    * @param fileIdx     the index of the temporary file in the operation
+    * @return the path for this temporary file
+    */
+  def generateDownloadPath(archiveName: String, downloadIdx: Int, fileIdx: Int): Path =
+    generateArchivePath(archiveName).resolve(s"$DownloadPrefix${downloadIdx}_$fileIdx.tmp")
+
+  /**
+    * Checks whether the specified path points to a temporary download file
+    * that can be removed. This method is called when cleaning up the root
+    * directory for temporary download files. Only the paths accepted by the
+    * method are actually removed. This implementation checks whether the
+    * path has the prefix for download files and has not been generated for
+    * the current run.
+    *
+    * @param p the path to be checked
+    * @return a flag whether this path is a candidate to be removed
+    */
+  def isRemovableTempPath(p: Path): Boolean = {
+    !p.toString.contains(timeComponent) && p.getFileName.toString.contains(DownloadPrefix)
+  }
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/download/TempPathGeneratorSpec.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.download
+
+import java.nio.file.Paths
+import java.time.Instant
+
+import org.scalatest.{FlatSpec, Matchers}
+
+object TempPathGeneratorSpec {
+  /** A root path for temporary files. */
+  private val Root = Paths get "downloadTempFiles"
+
+  /** Name of a test archive. */
+  private val Archive = "CoolMusicRemote"
+}
+
+/**
+  * Test class for ''TempPathGenerator''.
+  */
+class TempPathGeneratorSpec extends FlatSpec with Matchers {
+
+  import TempPathGeneratorSpec._
+
+  "A TempPathGenerator" should "generate a valid sub directory for an archive" in {
+    val generator = TempPathGenerator(Root)
+
+    val path = generator generateArchivePath Archive
+    path.toString should include(Archive)
+    path.getParent should be(Root)
+  }
+
+  it should "generate other directories depending on the start time" in {
+    val time1 = Instant ofEpochSecond 42
+    val time2 = Instant ofEpochSecond 41
+    val gen1 = TempPathGenerator(Root, time1)
+    val gen2 = TempPathGenerator(Root, time2)
+
+    val path1 = gen1 generateArchivePath Archive
+    val path2 = gen2 generateArchivePath Archive
+    path1 should not be path2
+  }
+
+  it should "generate a valid path for a temporary download file" in {
+    val gen = TempPathGenerator(Root)
+
+    val path = gen.generateDownloadPath(Archive, 1, 1)
+    path.getParent should be(gen.generateArchivePath(Archive))
+    path.toString should endWith(".tmp")
+  }
+
+  it should "generate different download files for different downloads" in {
+    val gen = TempPathGenerator(Root)
+
+    val path1 = gen.generateDownloadPath(Archive, 1, 1)
+    val path2 = gen.generateDownloadPath(Archive, 2, 1)
+    path1 should not be path2
+  }
+
+  it should "generate different download files for different file indices" in {
+    val gen = TempPathGenerator(Root)
+
+    val path1 = gen.generateDownloadPath(Archive, 1, 1)
+    val path2 = gen.generateDownloadPath(Archive, 1, 2)
+    path1 should not be path2
+  }
+
+  it should "separate download index from file index" in {
+    val gen = TempPathGenerator(Root)
+
+    val path1 = gen.generateDownloadPath(Archive, 11, 1)
+    val path2 = gen.generateDownloadPath(Archive, 1, 11)
+    path1 should not be path2
+  }
+
+  it should "detect a removable path2" in {
+    val gen1 = TempPathGenerator(Root, Instant ofEpochSecond 1)
+    val gen2 = TempPathGenerator(Root, Instant ofEpochSecond 2)
+    val path = gen2 generateArchivePath Archive
+    val file = gen2.generateDownloadPath(Archive, 2, 3)
+
+    gen1 isRemovableTempPath path shouldBe true
+    gen1 isRemovableTempPath file shouldBe true
+  }
+
+  it should "reject a removable path for the current run" in {
+    val gen = TempPathGenerator(Root)
+    val path = gen generateArchivePath Archive
+
+    gen isRemovableTempPath path shouldBe false
+  }
+
+  it should "not remove an unrelated file" in {
+    val gen = TempPathGenerator(Root)
+    val path = Root resolve "anotherFile.tmp"
+
+    gen isRemovableTempPath path shouldBe false
+  }
+
+  it should "not remove an unrelated file in a download temp directory" in {
+    val gen1 = TempPathGenerator(Root, Instant ofEpochSecond 1)
+    val dir = gen1 generateArchivePath Archive
+    val path = dir resolve "noDownloadFile.tmp"
+    val gen = TempPathGenerator(Root, Instant ofEpochSecond 2)
+
+    gen isRemovableTempPath path shouldBe false
+  }
+}
