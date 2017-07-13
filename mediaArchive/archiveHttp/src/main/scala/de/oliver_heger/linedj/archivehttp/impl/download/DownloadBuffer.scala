d5836e867581e190e2b56df0d30357d80209d2c7
oheger <oliver.heger@oliver-heger.de>
2017-07-13 21:46:31 +0200

    Added DownloadBuffer class.
    This class manages a queue of chunks of data that has been
    received from the HTTP archive. It handles client requests for
    this data even if there are different block sizes.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/download/DownloadBuffer.scala
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.download
+
+import akka.util.ByteString
+
+import scala.collection.immutable.Queue
+
+private object DownloadBuffer {
+  /**
+    * Returns an empty ''DownloadBuffer''.
+    *
+    * @return the empty buffer
+    */
+  val empty: DownloadBuffer = new DownloadBuffer(Queue.empty, 0, None)
+
+  /**
+    * Splits a byte string if necessary. If the string is larger than the
+    * requested count, it has to be split in two parts.
+    *
+    * @param bs     the byte string to split
+    * @param length the requested length
+    * @return the string of the requested size and the optional remainder
+    */
+  private def splitBlock(bs: ByteString, length: Int): (ByteString, Option[ByteString]) =
+    if (bs.length <= length) (bs, None)
+    else {
+      val (b1, b2) = bs.splitAt(length)
+      (b1, Some(b2))
+    }
+}
+
+/**
+  * An internally used helper class that manages the in-memory buffer during a
+  * download operation.
+  *
+  * When downloading a file from an HTTP archive chunks of data are added to
+  * an in-memory buffer and requested from the client. Ideally, the client is
+  * fast enough to process the data in the same speed as it arrives from the
+  * HTTP server. If this is not the case, the single chunks have to be
+  * buffered. If the buffer becomes too big, it has to be written to a
+  * temporary file.
+  *
+  * This class offers the API to implement these use cases. It allows adding
+  * blocks of bytes and removing them in FIFO order. It also exposes all
+  * blocks currently stored, so that they can be written to disk.
+  *
+  * @param queue         stores the content of this buffer
+  * @param size          the number of types contained in this buffer
+  * @param optFirstBlock an optional first block; such a block is created if
+  *                      a chunk is requested that is smaller than the head
+  *                      chunk
+  */
+private class DownloadBuffer private(queue: Queue[ByteString], val size: Int,
+                                     optFirstBlock: Option[ByteString]) {
+
+  import DownloadBuffer._
+
+  /**
+    * Returns a new ''DownloadBuffer'' as a copy of this instance with the
+    * specified block added to the end of the queue.
+    *
+    * @param chunk the chunk to be added
+    * @return the updated buffer
+    */
+  def addChunk(chunk: ByteString): DownloadBuffer =
+    new DownloadBuffer(queue :+ chunk, size + chunk.length, optFirstBlock)
+
+  /**
+    * Obtains a block of data with the given size from this buffer and
+    * returns a modified buffer with the data removed. The block returned has
+    * at most the specified size; it may be smaller if this fits the chunk
+    * size used by the HTTP archive better. If this buffer is empty, the block
+    * returned is ''None''.
+    *
+    * @param blockSize the size of the desired block
+    * @return a tuple with the block result and the updated buffer
+    */
+  def fetchData(blockSize: Int): (Option[ByteString], DownloadBuffer) =
+    optFirstBlock.map(bs =>
+      fetchDataWithFirstBlock(blockSize, bs)) getOrElse fetchDataFromQueue(blockSize)
+
+  /**
+    * Returns an ''Iterable'' with all ''ByteString'' blocks that are
+    * contained in this buffer.
+    *
+    * @return an ''Iterable'' with all contained blocks
+    */
+  def chunks: Iterable[ByteString] =
+    optFirstBlock.map(_ :: queue.toList) getOrElse queue
+
+  /**
+    * Obtains a block with data of the requested size if there is a first
+    * block available.
+    *
+    * @param blockSize the desired block size
+    * @param bs        the content of the first block
+    * @return a tuple with the block result and the updated buffer
+    */
+  private def fetchDataWithFirstBlock(blockSize: Int, bs: ByteString):
+  (Some[ByteString], DownloadBuffer) = {
+    val (value, rest) = splitBlock(bs, blockSize)
+    (Some(value), new DownloadBuffer(queue, size - value.length, rest))
+  }
+
+  /**
+    * Obtains a block with data of the requested size if all data of this
+    * buffer is stored in the queue.
+    *
+    * @param blockSize the desired block size
+    * @return a tuple with the block result and the updated buffer
+    */
+  private def fetchDataFromQueue(blockSize: Int): (Option[ByteString], DownloadBuffer) =
+    if (queue.isEmpty) (None, this)
+    else {
+      val (bs, q2) = queue.dequeue
+      val (value, rest) = splitBlock(bs, blockSize)
+      (Some(value), new DownloadBuffer(q2, size - value.length, rest))
+    }
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/download/DownloadBufferSpec.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.download
+
+import akka.util.ByteString
+import de.oliver_heger.linedj.FileTestHelper
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.annotation.tailrec
+
+/**
+  * Test class for ''DownloadBuffer''.
+  */
+class DownloadBufferSpec extends FlatSpec with Matchers {
+  "A DownloadBuffer" should "return a correct size of an empty buffer" in {
+    DownloadBuffer.empty.size should be(0)
+  }
+
+  it should "return correct chunks for an empty buffer" in {
+    DownloadBuffer.empty.chunks.isEmpty shouldBe true
+  }
+
+  it should "correctly handle a fetch operation if it is empty" in {
+    val buffer = DownloadBuffer.empty
+
+    val (data, buffer2) = buffer fetchData 1
+    data should be(None)
+    buffer2 should be theSameInstanceAs buffer
+  }
+
+  it should "allow adding and querying chunks" in {
+    val chunks = ByteString(FileTestHelper.TestData).grouped(32).toList
+
+    val buffer = chunks.foldLeft(DownloadBuffer.empty)(_ addChunk _)
+    buffer.size should be(FileTestHelper.TestData.length)
+    buffer.chunks.toList should be(chunks)
+
+    @tailrec def queryChunk(count: Int, buf: DownloadBuffer, bs: ByteString): ByteString = {
+      if (count == 0) bs
+      else {
+        val (optBs, buf2) = buf.fetchData(32)
+        queryChunk(count - 1, buf2, bs ++ optBs.get)
+      }
+    }
+
+    val result = queryChunk(chunks.length, buffer, ByteString.empty)
+    result.utf8String should be(FileTestHelper.TestData)
+  }
+
+  it should "support querying smaller chunks" in {
+    val ChunkSize = 32
+    val buffer = DownloadBuffer.empty addChunk ByteString(FileTestHelper.TestData)
+
+    val (optChunk, buf2) = buffer fetchData ChunkSize
+    buf2.size should be(FileTestHelper.TestData.length - ChunkSize)
+    optChunk.get.utf8String should be(FileTestHelper.TestData.substring(0, ChunkSize))
+  }
+
+  it should "support querying multiple smaller chunks" in {
+    val ChunkSize = 16
+    val buffer = DownloadBuffer.empty addChunk ByteString(FileTestHelper.TestData)
+    val (_, buf2) = buffer fetchData ChunkSize
+
+    val (optChunk2, buf3) = buf2 fetchData ChunkSize
+    buf3.size should be(FileTestHelper.TestData.length - 2 * ChunkSize)
+    optChunk2.get.utf8String should be(FileTestHelper.TestData
+      .substring(ChunkSize, 2 * ChunkSize))
+  }
+
+  it should "correctly handle requests for larger blocks" in {
+    val Data = "Small data"
+    val buffer = DownloadBuffer.empty addChunk ByteString(Data)
+
+    val (optChunk, buf2) = buffer fetchData 1000
+    optChunk.get.utf8String should be(Data)
+    buf2.size should be(0)
+  }
+
+  it should "return the head chunk in the chunks list" in {
+    val ChunkSize = 64
+    val MoreData = "More text"
+    val buffer = DownloadBuffer.empty
+      .addChunk(ByteString(FileTestHelper.TestData))
+      .addChunk(ByteString(MoreData))
+    val (_, buf2) = buffer fetchData ChunkSize
+
+    val chunks = buf2.chunks
+    val remainingData = chunks.foldLeft(ByteString.empty)(_ ++ _)
+    remainingData.utf8String should be(FileTestHelper
+      .TestData.substring(ChunkSize) + MoreData)
+  }
+}
