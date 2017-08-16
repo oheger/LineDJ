27ae64d19033e02b33a871e948dfff244b9f3c14
oheger <oliver.heger@oliver-heger.de>
2017-08-16 21:28:41 +0200

    Added UriMappper class.
    This class can transform the URI of a media file as declared in a meta
    data file based on a UriMappingConfig.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/UriMapper.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+import de.oliver_heger.linedj.archivehttp.config.UriMappingConfig
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+
+object UriMapper {
+  /** Constant for an empty medium path. */
+  private val EmptyMediumPath = ""
+
+  /**
+    * Calculates the medium path based on the settings file of the given
+    * medium ID. Can also handle an undefined settings file.
+    *
+    * @param mid the medium ID
+    * @return the path to this medium
+    */
+  private def mediumPath(mid: MediumID): String =
+    mid.mediumDescriptionPath map { d =>
+      val filePos = d.lastIndexOf('/')
+      if (filePos >= 0) d.substring(0, filePos) else EmptyMediumPath
+    } getOrElse EmptyMediumPath
+
+  /**
+    * Applies the URI template from the given configuration to the specified
+    * parameters.
+    *
+    * @param config      the mapping config
+    * @param mid         the medium ID
+    * @param strippedUri the URI with the prefix already removed
+    * @return the resulting URI
+    */
+  private def applyUriTemplate(config: UriMappingConfig, mid: MediumID, strippedUri: String):
+  String =
+    config.uriTemplate.replace("${medium}", mediumPath(mid))
+      .replace("${uri}", urlEncode(config, strippedUri))
+
+  /**
+    * Removes the prefix from the URI if possible. If a prefix is defined, but
+    * the URI does not start with it, result is ''None''.
+    *
+    * @param prefix the prefix
+    * @param uri    the URI
+    * @return an Option for the URI with removed prefix
+    */
+  private def removePrefix(prefix: String, uri: String): Option[String] =
+    if (prefix == null) Some(uri)
+    else if (uri startsWith prefix) Some(uri substring prefix.length)
+    else None
+
+  /**
+    * Applies URL encoding to the components of the specified URI string.
+    *
+    * @param config the mapping config
+    * @param uri    the URI
+    * @return the encoded URI
+    */
+  private def urlEncode(config: UriMappingConfig, uri: String): String =
+    if (config.urlEncode) {
+      if (config.pathSeparator == null) encode(uri)
+      else {
+        val components = uri split config.pathSeparator
+        components.map(encode).mkString(config.pathSeparator)
+      }
+    }
+    else uri
+
+  /**
+    * Encodes the specified string.
+    *
+    * @param uri the URI string to be encoded
+    * @return the encoded string
+    */
+  private def encode(uri: String): String =
+    URLEncoder.encode(uri, StandardCharsets.UTF_8.name()).replace("+", "%20")
+}
+
+/**
+  * A class that processes URIs obtained from meta data files from an HTTP
+  * archive.
+  *
+  * This class applies the data stored in a
+  * [[de.oliver_heger.linedj.archivehttp.config.UriMappingConfig]] to URIs
+  * encountered in meta data files.
+  *
+  * If an URI cannot be mapped, the mapping function returns an empty
+  * option. In this case, the file is ignored.
+  */
+private class UriMapper {
+
+  import UriMapper._
+
+  /**
+    * Applies URI mapping to the specified URI.
+    *
+    * @param config the mapping configuration
+    * @param mid    the medium ID
+    * @param uriOrg the original URI to be processed
+    * @return an option for the processed URI
+    */
+  def mapUri(config: UriMappingConfig, mid: MediumID, uriOrg: String): Option[String] =
+    Option(uriOrg).flatMap(removePrefix(config.removePrefix, _))
+      .map(u => applyUriTemplate(config, mid, u))
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/UriMapperSpec.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import de.oliver_heger.linedj.archivehttp.config.UriMappingConfig
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+import org.scalatest.{FlatSpec, Matchers}
+
+object UriMapperSpec {
+  /** Path to the test medium. */
+  private val MediumPath = "/test/Prince1"
+
+  /** The default prefix for URIs. */
+  private val UriPrefix = "path://"
+
+  /** A default mapping configuration. */
+  private val MappingConfig = UriMappingConfig(removePrefix = UriPrefix,
+    uriTemplate = "${medium}/songs/${uri}", urlEncode = false,
+    pathSeparator = null)
+
+  /** A test medium ID. */
+  private val TestMedium = MediumID("someMedium", Some(MediumPath + "/playlist.settings"))
+}
+
+/**
+  * Test class for ''UriMapper''.
+  */
+class UriMapperSpec extends FlatSpec with Matchers {
+
+  import UriMapperSpec._
+
+  /**
+    * Helper method for mapping a test URI with the given parameters.
+    *
+    * @param u      the URI to be mapped
+    * @param config the mapping config
+    * @param mid    the medium ID
+    * @return the resulting URI
+    */
+  private def mapUri(u: String, config: UriMappingConfig = MappingConfig,
+                     mid: MediumID = TestMedium): Option[String] = {
+    val mapper = new UriMapper
+    mapper.mapUri(config, mid, u)
+  }
+
+  "A UriMapper" should "return None if there is no prefix match" in {
+    mapUri("other://song.mp3") shouldBe 'empty
+  }
+
+  it should "apply a template correctly" in {
+    val uri = UriPrefix + "Prince - (1988) - Lovesexy/03 - Glam Slam.mp3"
+    val exp = MediumPath + "/songs/Prince - (1988) - Lovesexy/03 - Glam Slam.mp3"
+
+    mapUri(uri).get should be(exp)
+  }
+
+  it should "handle a medium ID without a settings path gracefully" in {
+    val uri = UriPrefix + "song.mp3"
+    val exp = "/songs/song.mp3"
+
+    mapUri(uri, mid = TestMedium.copy(mediumDescriptionPath = None)).get should be(exp)
+  }
+
+  it should "handle a medium path with only a file component" in {
+    val uri = UriPrefix + "song.mp3"
+    val exp = "/songs/song.mp3"
+
+    mapUri(uri, mid = TestMedium.copy(mediumDescriptionPath = Some("foo"))).get should be(exp)
+  }
+
+  it should "support an undefined remove prefix" in {
+    val uri = "someFile.mp3"
+    val exp = MediumPath + "/songs/someFile.mp3"
+
+    mapUri(uri, config = MappingConfig.copy(removePrefix = null)).get should be(exp)
+  }
+
+  it should "apply URL encoding" in {
+    val uri = UriPrefix + "a/test song.mp3"
+    val exp = MediumPath + "/songs/a%2Ftest%20song.mp3"
+
+    mapUri(uri, config = MappingConfig.copy(urlEncode = true)).get should be(exp)
+  }
+
+  it should "URL encode URIs with multiple path components" in {
+    val uri = UriPrefix + "Prince - (1988) - Lovesexy/03 - Glam Slam.mp3"
+    val exp = MediumPath + "/songs/Prince%20-%20%281988%29%20-%20Lovesexy/03%20-%20Glam%20Slam.mp3"
+    val config = MappingConfig.copy(urlEncode = true, pathSeparator = "/")
+
+    mapUri(uri, config = config).get should be(exp)
+  }
+
+  it should "handle a null URI as input" in {
+    mapUri(null) shouldBe 'empty
+  }
+}
