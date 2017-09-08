e4959995a7db052a1121b6cbf8796a2a39223e2c
oheger <oliver.heger@oliver-heger.de>
2017-09-08 21:13:30 +0200

    Added LogoutTask class.
    This is the task implementation for the logout action. It simply
    delegates to the overview controller.

---
+++ mediaArchive/archiveHttpStartup/src/test/scala/de/oliver_heger/linedj/archivehttpstart/LogoutTaskSpec.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart
+
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''LogoutTask''.
+  */
+class LogoutTaskSpec extends FlatSpec with Matchers with MockitoSugar {
+  "A LogoutTask" should "delegate to the controller" in {
+    val controller = mock[HttpArchiveOverviewController]
+    val task = new LogoutTask(controller)
+
+    task.run()
+    verify(controller).logoutCurrentRealm()
+  }
+}
