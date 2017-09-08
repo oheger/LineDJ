f127459dec9458cc41c9bc77a2c088670dd2a364
oheger <oliver.heger@oliver-heger.de>
2017-09-08 21:07:25 +0200

    Added OpenLoginDlgCommand class.
    This is a command implementation to open the Login dialog for the
    currently selected realm.

---
+++ mediaArchive/archiveHttpStartup/src/test/scala/de/oliver_heger/linedj/archivehttpstart/OpenLoginDlgCommandSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart
+
+import java.util.concurrent.atomic.AtomicReference
+
+import net.sf.jguiraffe.gui.app.ApplicationBuilderData
+import net.sf.jguiraffe.locators.Locator
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''OpenLoginDlgCommand''.
+  */
+class OpenLoginDlgCommandSpec extends FlatSpec with Matchers with MockitoSugar {
+  "An OpenLoginDlgCommand" should "pass the locator to the super class" in {
+    val locator = mock[Locator]
+    val command = new OpenLoginDlgCommand(locator, new AtomicReference[String])
+
+    command.getLocator should be(locator)
+  }
+
+  it should "store the current realm name in the builder data" in {
+    val Realm = "MyTestRealm"
+    val builderData = mock[ApplicationBuilderData]
+    val realmRef = new AtomicReference(Realm)
+    val command = new OpenLoginDlgCommand(mock[Locator], realmRef)
+
+    command.prepareBuilderData(builderData)
+    verify(builderData).addProperty(OpenLoginDlgCommand.PropertyRealmName, Realm)
+  }
+}
