06b8b90ad20999fe2564afbc99580e0727321fa8
oheger <oliver.heger@oliver-heger.de>
2017-09-20 22:15:05 +0200

    Added ServiceDependenciesManager class.
    This is a class used internally to handle service registration
    messages sent via the system message bus. With such messages, LineDJ
    services can announce their availability. Then a corresponding
    registration is made in the OSGi service registry. This can be
    detected again by dependent components.

---
+++ platform/src/main/scala/de/oliver_heger/linedj/platform/app/ServiceDependenciesManager.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import java.util
+
+import akka.actor.Actor.Receive
+import de.oliver_heger.linedj.platform.comm.{MessageBusListener, ServiceDependencies}
+import de.oliver_heger.linedj.platform.comm.ServiceDependencies.{RegisterService,
+ServiceDependency, UnregisterService}
+import org.osgi.framework.{BundleContext, ServiceRegistration}
+
+/**
+  * An internally used class responsible for updating OSGi service
+  * registrations for LineDJ platform services.
+  *
+  * This class is listening on the message bus for requests to add or remove a
+  * service registration. It updates the OSGi service registry accordingly.
+  *
+  * @param bundleContext the bundle context
+  */
+private class ServiceDependenciesManager(val bundleContext: BundleContext)
+  extends MessageBusListener {
+  /** A map with service registrations managed by this instance. */
+  private var registrations = Map.empty[ServiceDependency, ServiceRegistration[ServiceDependency]]
+
+  override def receive: Receive = {
+    case RegisterService(s@ServiceDependency(name)) =>
+      val props = new util.Hashtable[String, AnyRef]
+      props.put(ServiceDependencies.PropertyServiceName, name)
+      val reg = bundleContext.registerService(classOf[ServiceDependency], s, props)
+      registrations += s -> reg
+
+    case UnregisterService(dep) =>
+      registrations.get(dep) foreach { reg =>
+        reg.unregister()
+        registrations -= dep
+      }
+  }
+}
---
+++ platform/src/test/scala/de/oliver_heger/linedj/platform/app/ServiceDependenciesManagerSpec.scala
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import java.util
+
+import de.oliver_heger.linedj.platform.comm.ServiceDependencies
+import de.oliver_heger.linedj.platform.comm.ServiceDependencies.{RegisterService,
+ServiceDependency, UnregisterService}
+import org.mockito.Mockito._
+import org.osgi.framework.{BundleContext, ServiceRegistration}
+import org.scalatest.FlatSpec
+import org.scalatest.mock.MockitoSugar
+
+object ServiceDependenciesManagerSpec {
+  /** A test dependency instance used by test cases. */
+  private val TestDependency = ServiceDependency("com.test.ServiceDep")
+
+  /**
+    * Creates a map with properties needed for the OSGi service registration.
+    *
+    * @return the map with expected registration properties
+    */
+  private def serviceProperties(): util.Hashtable[String, AnyRef] = {
+    val expProps = new util.Hashtable[String, AnyRef]
+    expProps.put(ServiceDependencies.PropertyServiceName, TestDependency.serviceName)
+    expProps
+  }
+}
+
+/**
+  * Test class for ''ServiceDependenciesManager''.
+  */
+class ServiceDependenciesManagerSpec extends FlatSpec with MockitoSugar {
+
+  import ServiceDependenciesManagerSpec._
+
+  /**
+    * Creates a test manager instance.
+    *
+    * @return the test instance
+    */
+  private def createManager(): ServiceDependenciesManager =
+    new ServiceDependenciesManager(mock[BundleContext])
+
+  /**
+    * Expects a registration of the test service instance and returns a new
+    * mock service registration object.
+    *
+    * @return the mock registration object
+    */
+  private def expectRegistration(manager: ServiceDependenciesManager):
+  ServiceRegistration[ServiceDependency] = {
+    val reg = mock[ServiceRegistration[ServiceDependency]]
+    when(manager.bundleContext.registerService(classOf[ServiceDependency], TestDependency,
+      serviceProperties())).thenReturn(reg)
+    reg
+  }
+
+  "A ServiceDependenciesManager" should "register a new service" in {
+    val manager = createManager()
+    expectRegistration(manager)
+
+    manager receive RegisterService(TestDependency)
+    verify(manager.bundleContext).registerService(classOf[ServiceDependency], TestDependency,
+      serviceProperties())
+  }
+
+  it should "remove a service registration again" in {
+    val manager = createManager()
+    val registration = expectRegistration(manager)
+    manager receive RegisterService(TestDependency)
+
+    manager receive UnregisterService(TestDependency)
+    verify(registration).unregister()
+  }
+
+  it should "ignore an unregister message for an unknown service" in {
+    val manager = createManager()
+
+    manager receive UnregisterService(TestDependency)
+  }
+
+  it should "remove a service dependency after an unregister message" in {
+    val manager = createManager()
+    val registration = expectRegistration(manager)
+    manager receive RegisterService(TestDependency)
+    manager receive UnregisterService(TestDependency)
+
+    manager receive UnregisterService(TestDependency)
+    verify(registration).unregister()
+  }
+}
