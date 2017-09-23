caf70ef707c4478bd399d1de8081feb13c8876ff
oheger <oliver.heger@oliver-heger.de>
2017-09-23 18:07:54 +0200

    Added MediaFacadeActorsServiceWrapper class.
    This class registers the actors for the media facade as OSGi service.
    This greatly simplifies access to these actors for interested
    components.

---
+++ platform/src/main/scala/de/oliver_heger/linedj/platform/app/MediaFacadeActorsServiceWrapper.scala
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import java.util.concurrent.atomic.AtomicReference
+
+import akka.actor.Actor.Receive
+import akka.util.Timeout
+import de.oliver_heger.linedj.platform.bus.Identifiable
+import de.oliver_heger.linedj.platform.mediaifc.MediaFacade
+import de.oliver_heger.linedj.platform.mediaifc.MediaFacade.MediaFacadeActors
+import de.oliver_heger.linedj.platform.mediaifc.ext.ArchiveAvailabilityExtension
+.{ArchiveAvailabilityRegistration, ArchiveAvailabilityUnregistration}
+import org.osgi.framework.{BundleContext, ServiceRegistration}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+
+object MediaFacadeActorsServiceWrapper {
+  /** Property for the initialization timeout of the local archive. */
+  val PropArchiveInitTimeout = "media.initTimeout"
+
+  /** The default initialization timeout (in seconds). */
+  private val DefaultInitTimeout = 10
+
+  /**
+    * An internally used message class to report the availability of facade
+    * actors via the message bus. This is used to sync the service registration
+    * with the UI thread where the internal state is managed.
+    *
+    * @param facadeActors the facade actors
+    */
+  private case class FacadeActorsAvailable(facadeActors: MediaFacadeActors)
+
+  /**
+    * Determines the initialization timeout for the local media archive. This
+    * value can be read from the configuration; if unspecified, a default value
+    * is used.
+    *
+    * @param ctx the ''ClientApplicationContext''
+    * @return the initialization timeout
+    */
+  private def fetchInitTimeout(ctx: ClientApplicationContext): Timeout =
+    Timeout(ctx.managementConfiguration.getInt(PropArchiveInitTimeout,
+      DefaultInitTimeout).seconds)
+}
+
+/**
+  * An internally used helper class that exposes the actors of the media facade
+  * as OSGi service.
+  *
+  * For complex interaction with the media facade access to the implementing
+  * actors is frequently required. Obtaining these actors is not trivial,
+  * however, as they have to retrieved in an asynchronous operation that might
+  * fail; also, the availability of the media archive has to be monitored
+  * because only if the archive is running, the facade actors are valid.
+  *
+  * To make the life easier for consumers of these actors, this class centrally
+  * implements all steps required to request the facade actors and monitor
+  * their validity. When the actors have been retrieved successfully, they are
+  * registered as a service (of type [[MediaFacadeActors]] in the OSGi registry.
+  *
+  * Consumers can use standard OSGi mechanisms to obtain the actors directly
+  * without having to do the full dance.
+  *
+  * @param clientAppContext the client application content
+  * @param bundleContext    the OSGi bundle context
+  */
+private class MediaFacadeActorsServiceWrapper(val clientAppContext: ClientApplicationContext,
+                                              val bundleContext: BundleContext)
+  extends Identifiable {
+
+  import MediaFacadeActorsServiceWrapper._
+
+  /** The logger. */
+  private val log = LoggerFactory.getLogger(getClass)
+
+  /** The registration ID for the message bus listener. */
+  private var msgBusListenerID = 0
+
+  /**
+    * Holds the registration for the facades service. Note that here an atomic
+    * reference must be used because the field accessed from both the UI
+    * thread and the OSGi management thread.
+    */
+  private val refRegFacadeActors: AtomicReference[Option[ServiceRegistration[MediaFacadeActors]]] =
+    new AtomicReference(None)
+
+  /** A flag whether the union archive is currently available. */
+  private var unionArchiveAvailable = false
+
+  /**
+    * Notifies this object that it is activated. Performs the necessary
+    * initialization.
+    */
+  def activate(): Unit = {
+    log.info("Activating MediaFacadeActorsServiceWrapper.")
+    msgBusListenerID = clientAppContext.messageBus registerListener messageBusReceive
+    clientAppContext.messageBus publish ArchiveAvailabilityRegistration(componentID,
+      archiveAvailabilityChanged)
+  }
+
+  /**
+    * Notifies this object that it has been deactivated. Performs the
+    * necessary cleanup.
+    */
+  def deactivate(): Unit = {
+    clientAppContext.messageBus removeListener msgBusListenerID
+    clientAppContext.messageBus publish ArchiveAvailabilityUnregistration(componentID)
+    ensureServiceRegistrationCleared()
+    log.info("Deactivated MediaFacadeActorsServiceWrapper.")
+  }
+
+  /**
+    * Returns the function for handling messages published on the message bus.
+    *
+    * @return the message handling function
+    */
+  private def messageBusReceive: Receive = {
+    case FacadeActorsAvailable(actors) =>
+      registerFacadeService(actors)
+  }
+
+  /**
+    * The consumer function for the archive availability extension. Here
+    * messages about the availability of the union media archive are processed.
+    *
+    * @param event the event
+    */
+  private def archiveAvailabilityChanged(event: MediaFacade.MediaArchiveAvailabilityEvent): Unit =
+    event match {
+      case MediaFacade.MediaArchiveAvailable =>
+        handleArchiveAvailable()
+      case MediaFacade.MediaArchiveUnavailable =>
+        handleArchiveUnavailable()
+    }
+
+  /**
+    * Handles the notification that the union archive is available. If not
+    * yet done, the local archive is now started.
+    */
+  private def handleArchiveAvailable(): Unit = {
+    unionArchiveAvailable = true
+    if (refRegFacadeActors.get().isEmpty) {
+      implicit val executionContext: ExecutionContextExecutor =
+        clientAppContext.actorSystem.dispatcher
+      implicit val timeout: Timeout = fetchInitTimeout(clientAppContext)
+
+      log.info("Archive available. Requesting facade actors.")
+      clientAppContext.mediaFacade.requestFacadeActors() foreach { actors =>
+        clientAppContext.messageBus publish FacadeActorsAvailable(actors)
+      }
+    }
+  }
+
+  /**
+    * Handles the notification that the union archive is no longer available.
+    * This also causes the local archive to be stopped.
+    */
+  private def handleArchiveUnavailable(): Unit = {
+    unionArchiveAvailable = false
+    log.info("Archive unavailable.")
+    ensureServiceRegistrationCleared()
+  }
+
+  /**
+    * Registers the facade actors as OSGi services if the union archive is
+    * still available.
+    *
+    * @param facadeActors the facade actors
+    */
+  private def registerFacadeService(facadeActors: MediaFacadeActors): Unit = {
+    if (unionArchiveAvailable) {
+      refRegFacadeActors.set(Some(bundleContext.registerService(classOf[MediaFacadeActors],
+        facadeActors, null)))
+      log.info("MediaFacadeActors service registered.")
+    }
+  }
+
+  /**
+    * Removes the service registration if available.
+    */
+  private def ensureServiceRegistrationCleared(): Unit = {
+    refRegFacadeActors.get() foreach { reg =>
+      reg.unregister()
+      refRegFacadeActors.set(None)
+    }
+  }
+}
---
+++ platform/src/test/scala/de/oliver_heger/linedj/platform/app/MediaFacadeActorsServiceWrapperSpec.scala
@@ -0,0 +1,363 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import akka.util.Timeout
+import de.oliver_heger.linedj.platform.MessageBusTestImpl
+import de.oliver_heger.linedj.platform.mediaifc.MediaFacade
+import de.oliver_heger.linedj.platform.mediaifc.MediaFacade.MediaFacadeActors
+import de.oliver_heger.linedj.platform.mediaifc.ext.ArchiveAvailabilityExtension
+.{ArchiveAvailabilityRegistration, ArchiveAvailabilityUnregistration}
+import org.apache.commons.configuration.PropertiesConfiguration
+import org.mockito.Matchers.{any, eq => eqArg}
+import org.mockito.Mockito._
+import org.osgi.framework.{BundleContext, ServiceRegistration}
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Promise}
+
+/**
+  * Test class for ''MediaFacadeActorsServiceWrapper''.
+  */
+class MediaFacadeActorsServiceWrapperSpec(testSystem: ActorSystem) extends TestKit(testSystem)
+  with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  def this() = this(ActorSystem("MediaFacadeActorsServiceWrapper"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  "A MediaFacadeActorsServiceWrapper" should "register itself for archive availability" in {
+    val helper = new ServiceWrapperTestHelper
+
+    helper.activate().verifyArcListenerRegistration()
+  }
+
+  it should "remove all listener registrations when it is deactivated" in {
+    val helper = new ServiceWrapperTestHelper
+
+    helper.activate().deactivate()
+      .verifyArcListenerDeRegistration()
+      .verifyMessageBusDeRegistration()
+  }
+
+  it should "register the facade actors when the archive is available" in {
+    val helper = new ServiceWrapperTestHelper
+
+    helper.activate()
+      .prepareArchiveActorsRequest()
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .verifyNoServiceRegistration()
+      .processUIFuture()
+      .verifyServiceRegistration()
+  }
+
+  it should "support a timeout in the configuration" in {
+    val timeOutSecs = 5
+    val helper = new ServiceWrapperTestHelper
+
+    helper.activate()
+      .initActorsRetrievalTimeout(timeOutSecs)
+      .prepareArchiveActorsRequest(timeout = Timeout(timeOutSecs.seconds))
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .processUIFuture()
+      .verifyServiceRegistration()
+  }
+
+  it should "remove the service registration on deactivation" in {
+    val helper = new ServiceWrapperTestHelper
+    helper.activate().prepareArchiveActorsRequest()
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .processUIFuture()
+      .verifyServiceRegistration()
+
+    helper.deactivate().verifyRegistrationRemoved()
+  }
+
+  it should "not register the service again on a 2nd archive available message" in {
+    val helper = new ServiceWrapperTestHelper
+    helper.activate().prepareArchiveActorsRequest()
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .processUIFuture()
+
+    helper.sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .expectNoMessageOnBus()
+  }
+
+  it should "remove the service when the union archive becomes unavailable" in {
+    val helper = new ServiceWrapperTestHelper
+    helper.activate().prepareArchiveActorsRequest()
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .processUIFuture()
+      .verifyServiceRegistration()
+
+    helper.sendAvailability(MediaFacade.MediaArchiveUnavailable)
+      .verifyRegistrationRemoved()
+  }
+
+  it should "register the service anew when the union archive is available again" in {
+    val helper = new ServiceWrapperTestHelper
+    helper.activate().prepareArchiveActorsRequest()
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .processUIFuture()
+      .sendAvailability(MediaFacade.MediaArchiveUnavailable)
+
+    helper.sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .processUIFuture()
+      .verifyServiceRegistration(count = 2)
+  }
+
+  it should "not register the service if fetching of facade actors fails" in {
+    val helper = new ServiceWrapperTestHelper
+    helper.activate()
+      .prepareArchiveActorsRequest(actors = Promise.failed(new Exception("BOOM")))
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+
+    helper.expectNoMessageOnBus()
+  }
+
+  it should "not register the archive if the facade becomes unavailable while fetching actors" in {
+    val helper = new ServiceWrapperTestHelper
+    helper.activate()
+      .prepareArchiveActorsRequest()
+      .sendAvailability(MediaFacade.MediaArchiveAvailable)
+      .sendAvailability(MediaFacade.MediaArchiveUnavailable)
+      .processUIFuture()
+
+    helper.verifyNoServiceRegistration()
+  }
+
+  /**
+    * A test helper class managing a test instance and its dependencies.
+    */
+  private class ServiceWrapperTestHelper {
+    /** The test message bus. */
+    private val messageBus = new MessageBusTestImpl
+
+    /** The client application context. */
+    private val clientContext = createClientContext()
+
+    /** Mock for the OSGi service registration for the facade actors. */
+    private val serviceRegistration = mock[ServiceRegistration[MediaFacadeActors]]
+
+    /** Mock for the facade actors object. */
+    private val facadeActors = mock[MediaFacadeActors]
+
+    /** The bundle context mock. */
+    private val bundleContext = createBundleContext()
+
+    /** The registration for the archive availability extension. */
+    private var arcRegistration: ArchiveAvailabilityRegistration = _
+
+    /** The service wrapper to be tested. */
+    private val serviceWrapper = new MediaFacadeActorsServiceWrapper(clientContext, bundleContext)
+
+    /**
+      * Activates the test instance.
+      *
+      * @return this test helper
+      */
+    def activate(): ServiceWrapperTestHelper = {
+      serviceWrapper.activate()
+      arcRegistration = fetchRegistration()
+      this
+    }
+
+    /**
+      * Deactivates the test instance.
+      *
+      * @return this test helper
+      */
+    def deactivate(): ServiceWrapperTestHelper = {
+      serviceWrapper.deactivate()
+      this
+    }
+
+    /**
+      * Verifies that the test instance has published a valid registration as
+      * archive availability listener.
+      *
+      * @return this test helper
+      */
+    def verifyArcListenerRegistration(): ServiceWrapperTestHelper = {
+      arcRegistration.id should be(serviceWrapper.componentID)
+      this
+    }
+
+    /**
+      * Checks that a de-registration message for the archive availability
+      * extension was sent on the message bus.
+      *
+      * @return this test helper
+      */
+    def verifyArcListenerDeRegistration(): ServiceWrapperTestHelper = {
+      val unReg = messageBus.expectMessageType[ArchiveAvailabilityUnregistration]
+      unReg.id should be(serviceWrapper.componentID)
+      this
+    }
+
+    /**
+      * Checks that the message bus listener registration has been removed.
+      *
+      * @return this test helper
+      */
+    def verifyMessageBusDeRegistration(): ServiceWrapperTestHelper = {
+      messageBus.currentListeners should have size 0
+      this
+    }
+
+    /**
+      * Prepares a request for the actors of the union media archive. The mock
+      * is prepared to return a ''Future'' object for the actors based on the
+      * provided promise.
+      *
+      * @param actors  promise for the archive actors
+      * @param timeout a timeout for the request
+      * @return this test helper
+      */
+    def prepareArchiveActorsRequest(actors: Promise[MediaFacadeActors] =
+                                    Promise.successful(facadeActors),
+                                    timeout: Timeout = Timeout(10.seconds))
+    : ServiceWrapperTestHelper = {
+      when(clientContext.mediaFacade.requestFacadeActors()(eqArg(timeout),
+        any(classOf[ExecutionContext])))
+        .thenReturn(actors.future)
+      this
+    }
+
+    /**
+      * Expects a message to be sent on the UI bus for completing a future in
+      * the UI thread. This message is delivered.
+      *
+      * @return this test helper
+      */
+    def processUIFuture(): ServiceWrapperTestHelper = {
+      messageBus.processNextMessage[Any]()
+      this
+    }
+
+    /**
+      * Checks that no message has been published on the message bus.
+      *
+      * @return this test helper
+      */
+    def expectNoMessageOnBus(): ServiceWrapperTestHelper = {
+      messageBus.expectNoMessage(300.millis)
+      this
+    }
+
+    /**
+      * Sends a message to the consumer function registered by the test
+      * instance.
+      *
+      * @param event the event to be sent
+      * @return this test helper
+      */
+    def sendAvailability(event: MediaFacade.MediaArchiveAvailabilityEvent):
+    ServiceWrapperTestHelper = {
+      arcRegistration.callback(event)
+      this
+    }
+
+    /**
+      * Verifies that a service registration for the facade actors has been
+      * created.
+      *
+      * @param count the expected number of registrations
+      * @return this test helper
+      */
+    def verifyServiceRegistration(count: Int = 1): ServiceWrapperTestHelper = {
+      verify(bundleContext, times(count))
+        .registerService(classOf[MediaFacadeActors], facadeActors, null)
+      this
+    }
+
+    /**
+      * Verifies that the service registration has been removed again.
+      *
+      * @return this test helper
+      */
+    def verifyRegistrationRemoved(): ServiceWrapperTestHelper = {
+      verify(serviceRegistration).unregister()
+      this
+    }
+
+    /**
+      * Verifies that no service registration was created.
+      *
+      * @return this test helper
+      */
+    def verifyNoServiceRegistration(): ServiceWrapperTestHelper = {
+      verify(bundleContext, never()).registerService(eqArg(classOf[MediaFacadeActors]),
+        any(), any())
+      this
+    }
+
+    /**
+      * Initializes the timeout property for retrieving the facade actors in
+      * the configuration.
+      *
+      * @param timeOutSecs the timeout in seconds
+      * @return this test helper
+      */
+    def initActorsRetrievalTimeout(timeOutSecs: Int): ServiceWrapperTestHelper = {
+      clientContext.managementConfiguration.addProperty("media.initTimeout", timeOutSecs)
+      this
+    }
+
+    /**
+      * Obtains the registration for the archive availability extension on the
+      * message bus.
+      *
+      * @return the registration object
+      */
+    private def fetchRegistration(): ArchiveAvailabilityRegistration =
+      messageBus.expectMessageType[ArchiveAvailabilityRegistration]
+
+    /**
+      * Creates a mock for the bundle context. The mock is already prepared
+      * to return a service registration.
+      *
+      * @return the mock for the bundle context
+      */
+    private def createBundleContext(): BundleContext = {
+      val ctx = mock[BundleContext]
+      when(ctx.registerService(classOf[MediaFacadeActors], facadeActors, null))
+        .thenReturn(serviceRegistration)
+      ctx
+    }
+
+    /**
+      * Creates a mock ''ClientApplicationContext''.
+      *
+      * @return the client context
+      */
+    private def createClientContext(): ClientApplicationContext = {
+      val clientContext = mock[ClientApplicationContext]
+      when(clientContext.managementConfiguration).thenReturn(new PropertiesConfiguration)
+      when(clientContext.actorSystem).thenReturn(system)
+      when(clientContext.messageBus).thenReturn(messageBus)
+      when(clientContext.mediaFacade).thenReturn(mock[MediaFacade])
+      clientContext
+    }
+  }
+
+}
