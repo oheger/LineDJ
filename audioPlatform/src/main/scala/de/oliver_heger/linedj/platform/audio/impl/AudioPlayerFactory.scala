b6d94b12903fc03a51adaa1a19c1879d2c6b9328
oheger <oliver.heger@oliver-heger.de>
2017-10-29 17:18:07 +0100

    Added AudioPlayerFactory class.
    This class can create fully initialized AudioPlayer objects. It is
    going to be used by the audio platform component to create the shared
    player instance.

---
+++ audioPlatform/src/main/scala/de/oliver_heger/linedj/platform/audio/impl/AudioPlayerFactory.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio.impl
+
+import akka.actor.ActorRef
+import de.oliver_heger.linedj.platform.app.support.ActorManagement
+import de.oliver_heger.linedj.player.engine.PlayerConfig.ActorCreator
+import de.oliver_heger.linedj.player.engine.facade.AudioPlayer
+import org.apache.commons.configuration.Configuration
+
+/**
+  * An internally used helper class that creates audio player objects.
+  *
+  * @param playerConfigFactory the factory for the player configuration
+  */
+private class AudioPlayerFactory(private[impl] val playerConfigFactory: PlayerConfigFactory) {
+  /**
+    * Creates a new instance of ''AudioPlayerFactory'' using a default
+    * configuration factory.
+    *
+    * @return the new instance
+    */
+  def this() = this(new PlayerConfigFactory)
+
+  /**
+    * Creates a new audio player instance.
+    *
+    * @param c            the application configuration
+    * @param prefix       the prefix for the config settings of the player
+    * @param mediaManager the media manager actor
+    * @param management   the actor management instance
+    * @return the new audio player instance
+    */
+  def createAudioPlayer(c: Configuration, prefix: String, mediaManager: ActorRef,
+                        management: ActorManagement): AudioPlayer = {
+    val creator: ActorCreator = (p, s) => management.createAndRegisterActor(p, s)
+    val config = playerConfigFactory.createPlayerConfig(c, prefix, mediaManager, creator)
+    AudioPlayer(config)
+  }
+}
---
+++ audioPlatform/src/test/scala/de/oliver_heger/linedj/platform/audio/impl/AudioPlayerFactorySpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio.impl
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{TestKit, TestProbe}
+import de.oliver_heger.linedj.platform.app.ClientApplicationContext
+import de.oliver_heger.linedj.platform.app.support.ActorManagement
+import de.oliver_heger.linedj.player.engine.PlayerConfig
+import de.oliver_heger.linedj.player.engine.PlayerConfig.ActorCreator
+import org.apache.commons.configuration.PropertiesConfiguration
+import org.mockito.Matchers.{any, eq => eqArg}
+import org.mockito.Mockito._
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+/**
+  * Test class for ''AudioPlayerFactory''.
+  */
+class AudioPlayerFactorySpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  def this() = this(ActorSystem("AudioPlayerFactorySpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  "An AudioPlayerFactory" should "create a correct default config factory" in {
+    val factory = new AudioPlayerFactory
+
+    factory.playerConfigFactory should not be null
+  }
+
+  it should "create a correct audio player" in {
+    val configFactory = mock[PlayerConfigFactory]
+    val mediaManager = TestProbe()
+    val Prefix = "audio.platform.config"
+    val BufSize = 8888
+    val appConfig = new PropertiesConfiguration
+    val actorCreations = collection.mutable.Map.empty[String, Props]
+    val management = new ActorManagement {
+      override def initClientContext(context: ClientApplicationContext): Unit = {}
+
+      override def clientApplicationContext: ClientApplicationContext = null
+
+      /**
+        * @inheritdoc Records this actor creation.
+        */
+      override def createAndRegisterActor(props: Props, name: String): ActorRef = {
+        actorCreations += name -> props
+        TestProbe().ref
+      }
+    }
+    when(configFactory.createPlayerConfig(eqArg(appConfig), eqArg(Prefix),
+      eqArg(mediaManager.ref), any())).thenAnswer(new Answer[PlayerConfig] {
+      override def answer(invocation: InvocationOnMock): PlayerConfig = {
+        val creator = invocation.getArguments()(3).asInstanceOf[ActorCreator]
+        PlayerConfig(inMemoryBufferSize = BufSize, mediaManagerActor = mediaManager.ref,
+          actorCreator = creator)
+      }
+    })
+    val factory = new AudioPlayerFactory(configFactory)
+
+    val player = factory.createAudioPlayer(appConfig, Prefix, mediaManager.ref, management)
+    player should not be null
+    val facadeProps = actorCreations("playerFacadeActor")
+    val actConfig = facadeProps.args.head.asInstanceOf[PlayerConfig]
+    actConfig.mediaManagerActor should be(mediaManager.ref)
+    actConfig.inMemoryBufferSize should be(BufSize)
+  }
+}
