ed6aba3a16f1405cc7b18e6d21d52531805cbafd
oheger <oliver.heger@oliver-heger.de>
2017-11-09 22:22:16 +0100

    Added a new LRUCache class.
    This functionality has been extracted from MetaDataCache. It will be
    useful for other cache implementations with restricted size as well.

---
+++ shared/src/main/scala/de/oliver_heger/linedj/utils/LRUCache.scala
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.utils
+
+import de.oliver_heger.linedj.utils.LRUCache.CacheItem
+
+import scala.annotation.tailrec
+
+object LRUCache {
+  /**
+    * A default size function which counts each entry of the cache as 1.
+    * This is appropriate if the cache is limited to a specific number of
+    * elements. In cases where container elements are stored that can contain
+    * an arbitrary number of elements, an alternative size function should
+    * probably be used.
+    *
+    * @tparam B the value type of the cache
+    * @return the standard size function for single elements
+    */
+  def SingleSize[B]: B => Int = _ => 1
+
+  /**
+    * A default removable function which states that all elements in the cache
+    * can be removed.
+    *
+    * @tparam B the value type of the cache
+    * @return the standard removable function which allows all removals
+    */
+  def AllRemovable[B]: B => Boolean = _ => true
+
+  /**
+    * Internally used class to construct a double-linked list of items stored
+    * in the cache. This is needed to implement LRU functionality.
+    *
+    * @param key the key of this item
+    * @tparam K the type of the key
+    * @tparam V the type of the value
+    */
+  private case class CacheItem[K, V](key: K) {
+    /** The value of this item. */
+    var value: V = _
+
+    /** Reference to the previous item in the list. */
+    var previous: CacheItem[K, V] = _
+
+    /** Reference to the next item in the list. */
+    var next: CacheItem[K, V] = _
+  }
+
+}
+
+/**
+  * A class implementing simple LRU cache functionality.
+  *
+  * For some use cases, it is necessary to cache data loaded from a media
+  * archive in a client application. For instance, an archive browser may
+  * display meta data for audio files, but it is not desirable that the meta
+  * data of the whole archive gets eventually downloaded to the client.
+  *
+  * With this class it is possible to limit the amount of data available on the
+  * client. Data which is accessed more frequently remains in the cache, while
+  * other entries are removed if space becomes rare.
+  *
+  * An instance can be configured with some parameters: The most important one
+  * is the maximum cache size. If this size is reached, entries are removed
+  * from the cache until it gets again below this threshold. (However, the
+  * cache will keep a single entry; so if this entry is bigger than the
+  * configured size, the cache can actually go out of its limits.)
+  *
+  * Per default, an entry in the cache is considered to have a size of 1. If
+  * the cache stores container objects, it is possible to specify a
+  * ''size function''. This function is invoked to determine the actual size
+  * of an entry.
+  *
+  * It is also possible to provide a ''removableFunc''. This function is called
+  * when items are to be removed from the cache because its capacity limit has
+  * been exceeded. Only items are removed from the cache for which this
+  * function returns '''true'''.
+  *
+  * Note that this class is not a general-purpose LRU cache implementation; it
+  * is rather designed to handle the use cases described in a pretty
+  * straight-forward way. Therefore, its functionality is limited to the most
+  * essential operations. It is also not thread-safe.
+  *
+  * @param cacheSize     the size threshold of this cache
+  * @param sizeFunc      function to determine the size of an entry
+  * @param removableFunc function to determine removable entries
+  * @tparam A the type of the keys of the cache
+  * @tparam B the type of the values of the cache
+  */
+class LRUCache[A, B](cacheSize: Int)(sizeFunc: B => Int = LRUCache.SingleSize,
+                                     removableFunc: B => Boolean = LRUCache.AllRemovable) {
+  /** A map for storing the items contained in the cache. */
+  private var items = Map.empty[A, CacheItem[A, B]]
+
+  /**
+    * Start of the list with cache items. Items at the beginning have
+    * been accessed recently.
+    */
+  private var first: CacheItem[A, B] = _
+
+  /**
+    * End of the list with cache items. Items at the end have not been
+    * accessed for a while; they are removed when the cache runs out of space.
+    */
+  private var last: CacheItem[A, B] = _
+
+  /** Keeps track about the current size of the cache. */
+  private var currentCacheSize = 0
+
+  /**
+    * Queries the item with the specified key and returns an ''Option'' with
+    * the result. If the key is contained in the cache, this operation is
+    * considered an access to it; therefore, the key is moved to the front and
+    * will not be removed directly when the capacity limit is reached.
+    *
+    * @param k the key
+    * @return an ''Option'' with the value of this key
+    */
+  def get(k: A): Option[B] =
+    items get k match {
+      case Some(item) =>
+        moveToFront(item)
+        Some(item.value)
+      case None => None
+    }
+
+  /**
+    * Obtains the item with the given key from the cache or returns the
+    * alternative if the key is not contained. If the key is found, this
+    * operation is considered an access, and the key is moved to the front of
+    * the LRU list, so that it will not be removed directly when the capacity
+    * limit is reached.
+    *
+    * @param k   the key
+    * @param alt the alternative value if the key is not found
+    * @return the value from the cache or the alternative
+    */
+  def getOrElse(k: A, alt: => B): B =
+    get(k) getOrElse alt
+
+  /**
+    * Checks whether the specified key is contained in this cache. This does
+    * not affect the order in the LRU list.
+    *
+    * @param k the key to be checked
+    * @return a flag whether this key is contained in this cache
+    */
+  def contains(k: A): Boolean = items contains k
+
+  /**
+    * Adds the specified key-value pair to the cache. This method assumes that
+    * the key is not yet contained in the cache. Callers are responsible to
+    * ensure this; otherwise, the cache may become inconsistent. The key is
+    * added to the front of the LRU list, so it will not be removed directly
+    * when the capacity limit is reached.
+    *
+    * @param k the key
+    * @param v the value
+    */
+  def addItem(k: A, v: B): Unit = {
+    val item = addToLRUList(k, v)
+    items += k -> item
+    handleCacheOverflow()
+  }
+
+  /**
+    * Performs an update of a key if it is contained in the cache. With this
+    * method the value of a key can be updated without changing its position
+    * in the LRU list. If the key is contained in the cache, the update
+    * function is invoked. Otherwise, this call has no effect, and result is
+    * '''false'''.
+    *
+    * @param k the key in question
+    * @param f a function that performs the update
+    * @return a flag whether the update was done
+    */
+  def updateItem(k: A)(f: B => B): Boolean =
+    items get k match {
+      case Some(item) =>
+        val oldVal = item.value
+        item.value = f(item.value)
+        currentCacheSize += sizeFunc(item.value) - sizeFunc(oldVal)
+        handleCacheOverflow()
+        true
+      case None =>
+        false
+    }
+
+  /**
+    * Removes the item with the specified key from this cache. The old value
+    * is returned (''None'' if the key was not contained in this cache).
+    *
+    * @param k the key of the item to be removed
+    * @return an ''Option'' with the last value of this key
+    */
+  def removeItem(k: A): Option[B] =
+    items get k match {
+      case Some(item) =>
+        removeCacheItem(item)
+        Some(item.value)
+      case None => None
+    }
+
+  /**
+    * Returns the current size of the cache.
+    *
+    * @return the cache size
+    */
+  def size: Int = currentCacheSize
+
+  /**
+    * Adds an item for the given value to the beginning of the item list.
+    *
+    * @param key   the key
+    * @param value the value to be added
+    * @return the new cache item
+    */
+  private def addToLRUList(key: A, value: B): CacheItem[A, B] = {
+    val item = CacheItem[A, B](key)
+    item.value = value
+    item.next = first
+    if (first != null) {
+      first.previous = item
+    }
+    first = item
+    if (last == null) last = item
+    currentCacheSize += sizeFunc(value)
+    item
+  }
+
+  /**
+    * Moves the specified cache item to the beginning of the LRU list.
+    *
+    * @param cacheItem the chunk item
+    */
+  private def moveToFront(cacheItem: CacheItem[A, B]): Unit = {
+    if (cacheItem.previous != null) {
+      removeFromList(cacheItem)
+      cacheItem.next = first
+      first.previous = cacheItem
+      cacheItem.previous = null
+      first = cacheItem
+    }
+  }
+
+  /**
+    * Removes the specified item from the LRU list and the cache.
+    *
+    * @param cacheItem the item
+    */
+  private def removeCacheItem(cacheItem: CacheItem[A, B]): Unit = {
+    removeFromList(cacheItem)
+    items -= cacheItem.key
+    currentCacheSize -= sizeFunc(cacheItem.value)
+  }
+
+  /**
+    * Removes the specified cache item from the LRU list.
+    *
+    * @param cacheItem the cache item
+    */
+  private def removeFromList(cacheItem: CacheItem[A, B]): Unit = {
+    if (last == cacheItem) {
+      last = cacheItem.previous
+    } else {
+      cacheItem.next.previous = cacheItem.previous
+    }
+
+    if (first == cacheItem) {
+      first = cacheItem.next
+    } else {
+      cacheItem.previous.next = cacheItem.next
+    }
+  }
+
+  /**
+    * Checks whether the cache has exceeded its limit. If so, items that were
+    * not accessed recently are removed.
+    */
+  private def handleCacheOverflow(): Unit = {
+    @tailrec def removeItems(pos: CacheItem[A, B]): Unit = {
+      if (currentCacheSize > cacheSize && (pos ne first)) {
+        if (removableFunc(pos.value)) {
+          removeCacheItem(pos)
+        }
+        removeItems(pos.previous)
+      }
+    }
+
+    removeItems(last)
+  }
+}
---
+++ shared/src/test/scala/de/oliver_heger/linedj/utils/LRUCacheSpec.scala
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.utils
+
+import org.scalatest.{FlatSpec, Matchers}
+
+object LRUCacheSpec {
+  /**
+    * Generates a test key based on the given index.
+    *
+    * @param idx the index
+    * @return the test key with this index
+    */
+  private def entryKey(idx: Int): String = "k" + idx
+
+  /**
+    * Generates a test cache entry based on the given index.
+    *
+    * @param idx the index
+    * @return the key-value pair based on this index
+    */
+  private def entry(idx: Int): (String, Int) = (entryKey(idx), idx)
+
+  /**
+    * Generates a sequence of test cache entries with indices in the provided
+    * range.
+    *
+    * @param from the from index
+    * @param to   the to index (including)
+    * @return the sequence of entries
+    */
+  private def entries(from: Int, to: Int): Seq[(String, Int)] =
+    (from to to) map entry
+
+  /**
+    * Adds the specified entries to the given cache.
+    *
+    * @param cache the cache
+    * @param pairs the entries to be added
+    * @return the cache with entries added
+    */
+  private def addEntries(cache: LRUCache[String, Int], pairs: Seq[(String, Int)]):
+  LRUCache[String, Int] = {
+    pairs foreach (p => cache.addItem(p._1, p._2))
+    cache
+  }
+}
+
+/**
+  * Test class for ''LRUCache''.
+  */
+class LRUCacheSpec extends FlatSpec with Matchers {
+
+  import LRUCacheSpec._
+
+  /**
+    * Checks that the given cache contains all the specified entries.
+    *
+    * @param cache the cache
+    * @param pairs the entries to be checked
+    */
+  private def assertContains(cache: LRUCache[String, Int], pairs: (String, Int)*): Unit = {
+    pairs foreach { p =>
+      cache contains p._1 shouldBe true
+      cache get p._1 should be(Some(p._2))
+    }
+  }
+
+  "A LRUCache" should "allow adding entries up to its capacity" in {
+    val pairs = entries(1, 10)
+
+    val cache = addEntries(new LRUCache[String, Int](pairs.size)(), pairs)
+    cache.size should be(pairs.size)
+    assertContains(cache, pairs: _*)
+  }
+
+  it should "remove older entries to keep its maximum capacity" in {
+    val pairs = entries(1, 8)
+
+    val cache = addEntries(new LRUCache[String, Int](pairs.size - 1)(), pairs)
+    cache.size should be(pairs.size - 1)
+    cache contains pairs.head._1 shouldBe false
+    cache get pairs.head._1 shouldBe 'empty
+    assertContains(cache, pairs.tail: _*)
+  }
+
+  it should "move an entry to the front when it is accessed" in {
+    val pairs = entries(1, 8)
+    val cache = addEntries(new LRUCache[String, Int](pairs.size)(), pairs)
+
+    cache get pairs.head._1 should be(Some(pairs.head._2))
+    cache.addItem(entryKey(42), 42)
+    cache get pairs.head._1 should be(Some(pairs.head._2))
+    cache contains pairs.drop(1).head._1 shouldBe false
+  }
+
+  it should "allow updating an entry" in {
+    val pairs = entries(1, 4)
+    val modIdx = 2
+    val modEntry = entryKey(modIdx)
+    val cache = addEntries(new LRUCache[String, Int](pairs.size)(), pairs)
+
+    cache.updateItem(modEntry)(_ + 1) shouldBe true
+    cache.get(modEntry).get should be(modIdx + 1)
+  }
+
+  it should "not change LRU order by an update" in {
+    val pairs = entries(1, 16)
+    val cache = addEntries(new LRUCache[String, Int](pairs.size)(), pairs)
+
+    cache.updateItem(pairs.head._1)(_ => 42)
+    cache.addItem(entryKey(100), 200)
+    cache contains pairs.head._1 shouldBe false
+  }
+
+  it should "handle an update operation for a non-existing key" in {
+    val pairs = entries(1, 16)
+    val cache = addEntries(new LRUCache[String, Int](pairs.size)(), pairs)
+
+    cache.updateItem("non existing key")(_ => 42) shouldBe false
+    assertContains(cache, pairs: _*)
+  }
+
+  it should "support a get with an alternative" in {
+    val alt = 42
+    val cache = new LRUCache[String, Int](10)()
+
+    cache.getOrElse("foo", alt) should be(alt)
+  }
+
+  it should "support an alternative size function" in {
+    val pairs = entries(1, 7)
+    val cache = new LRUCache[String, Int](16)(sizeFunc = i => i)
+
+    addEntries(cache, pairs)
+    cache.size should be(13)
+    assertContains(cache, entry(6), entry(7))
+  }
+
+  it should "correctly update the size during an update operation" in {
+    val e = entry(1)
+    val cache = new LRUCache[String, Int](10)(sizeFunc = i => i)
+    addEntries(cache, List(e))
+
+    val newSize = 5
+    cache.updateItem(e._1)(_ => newSize)
+    cache.size should be(newSize)
+  }
+
+  it should "respect the maximum capacity during update operations" in {
+    val pairs = entries(1, 7)
+    val cache = new LRUCache[String, Int](21)(sizeFunc = i => i)
+    addEntries(cache, pairs)
+
+    cache.updateItem(entryKey(7))(_ => 10)
+    cache.size should not be >(21)
+    cache contains entryKey(1) shouldBe false
+  }
+
+  it should "support an alternative remove function" in {
+    val cache = new LRUCache[String, Int](10)(removableFunc = i => i != 4)
+
+    addEntries(cache, entries(1, 10))
+    addEntries(cache, entries(20, 30))
+    assertContains(cache, entry(4))
+  }
+
+  it should "allow removing an item from the cache" in {
+    val removeIdx = 4
+    val removeKey = entryKey(removeIdx)
+    val cache = addEntries(new LRUCache[String, Int](32)(), entries(1, 8))
+
+    cache removeItem removeKey should be(Some(removeIdx))
+    cache.size should be(7)
+    cache contains removeKey shouldBe false
+  }
+
+  it should "handle a remove operation for a non-existing key" in {
+    val cache = addEntries(new LRUCache[String, Int](8)(), entries(1, 2))
+
+    cache removeItem entryKey(42) shouldBe 'empty
+  }
+}
