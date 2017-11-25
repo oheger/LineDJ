c67938c22a1ec4dee3062c4c94fb926cb13a23cc
oheger <oliver.heger@oliver-heger.de>
2017-11-25 17:10:52 +0100

    Added SongData class.
    This is a modified copy of the class from the platform project. Some
    logic for resolving default values has been removed. This should be
    handled when instances are created.

---
+++ audioPlatform/src/main/scala/de/oliver_heger/linedj/platform/audio/model/SongData.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio.model
+
+import de.oliver_heger.linedj.shared.archive.media.MediaFileID
+import de.oliver_heger.linedj.shared.archive.metadata.MediaMetaData
+
+import scala.beans.BeanProperty
+
+object SongData {
+  /** Constant for an unknown song duration. */
+  val UnknownDuration: Int = -1
+
+  /** Constant for an unknown track number. */
+  val UnknownTrackNumber: Int = -1
+}
+
+/**
+  * A data class that holds information about a song.
+  *
+  * The song is mainly described by its unique ID and the meta data available
+  * for it. As meta data is optional, some important properties like title or
+  * artist are provided explicitly. They can be resolved when an instance is
+  * created and are then available for direct access.
+  *
+  * This class may also be used by model classes for UI controls displaying song
+  * information. Therefore, it has to expose properties following the Java Beans
+  * standard.
+  *
+  * @param id       the ID of this song in the media archive
+  * @param metaData meta data about this song
+  */
+case class SongData(id: MediaFileID, metaData: MediaMetaData,
+                    @BeanProperty title: String,
+                    @BeanProperty artist: String,
+                    @BeanProperty album: String) {
+
+  import SongData._
+
+  /**
+    * The duration of this song (in milliseconds) as bean property. If the
+    * duration is unknown, result is less than zero.
+    */
+  lazy val getDuration: Int = metaData.duration getOrElse UnknownDuration
+
+  /**
+    * Returns a flag whether a duration is defined for this song in meta data.
+    * If this function returns '''true''', ''getDuration'' will return a
+    * correct duration value. Otherwise, a special negative value is returned.
+    *
+    * @return a flag whether the duration is defined for this song
+    */
+  def hasDuration: Boolean = metaData.duration.isDefined
+
+  /**
+    * The track number as bean property. If the track number is unknown, a
+    * negative number is returned.
+    */
+  lazy val getTrackNumber: Int = metaData.trackNumber getOrElse UnknownTrackNumber
+
+  /**
+    * Returns a flag whether a track number is defined for this song in meta
+    * data. If this function returns '''true''', ''getTrackNumber'' will return
+    * a correct track number. Otherwise, a special negative value is returned.
+    *
+    * @return a flag whether the track number is defined for this song
+    */
+  def hasTrackNumber: Boolean = metaData.trackNumber.isDefined
+}
---
+++ audioPlatform/src/test/scala/de/oliver_heger/linedj/platform/audio/model/SongDataSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio.model
+
+import de.oliver_heger.linedj.shared.archive.media.{MediaFileID, MediumID}
+import de.oliver_heger.linedj.shared.archive.metadata.MediaMetaData
+import org.scalatest.{FlatSpec, Matchers}
+
+object SongDataSpec {
+  /** Test song ID. */
+  private val SongID = MediaFileID(MediumID("medium", Some("settings")), "someUri")
+
+  /** Test track number. */
+  private val TrackNumber = 17
+
+  /** Test duration. */
+  private val Duration = 4444
+
+  /** A meta data object containing all relevant information. */
+  private val CompleteMetaData = MediaMetaData(trackNumber = Some(TrackNumber),
+    duration = Some(Duration))
+}
+
+/**
+  * Test class for ''SongData''.
+  */
+class SongDataSpec extends FlatSpec with Matchers {
+
+  import SongDataSpec._
+
+  "A SongData" should "return the duration from meta data" in {
+    val song = SongData(SongID, CompleteMetaData, "title", "artist", "album")
+
+    song.getDuration should be(Duration)
+    song.hasDuration shouldBe true
+  }
+
+  it should "return a special duration if undefined in meta data" in {
+    val song = SongData(SongID, MediaMetaData(), "title", "artist", "album")
+
+    song.getDuration should be(SongData.UnknownDuration)
+    song.hasDuration shouldBe false
+  }
+
+  it should "return the track number from meta data" in {
+    val song = SongData(SongID, CompleteMetaData, "title", "artist", "album")
+
+    song.getTrackNumber should be(TrackNumber)
+    song.hasTrackNumber shouldBe true
+  }
+
+  it should "return a special track number if undefined in meta data" in {
+    val song = SongData(SongID, MediaMetaData(), "title", "artist", "album")
+
+    song.getTrackNumber should be(SongData.UnknownTrackNumber)
+    song.hasTrackNumber shouldBe false
+  }
+}
