530eb29e14ce29d966a8fe1dfeab5f3f9534ff91
oheger <oliver.heger@oliver-heger.de>
2017-11-29 21:32:07 +0100

    Added UnknownPropertyResourceResolver class.
    This is a default implementation of the UnknownPropertyResolver trait
    which returns constant strings obtained from the application's
    resources.

---
+++ audioPlatform/src/main/scala/de/oliver_heger/linedj/platform/audio/model/UnknownPropertyResourceResolver.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio.model
+
+import de.oliver_heger.linedj.shared.archive.media.MediaFileID
+import net.sf.jguiraffe.gui.app.ApplicationContext
+
+/**
+  * A specialized implementation of an ''UnknownPropertyResolver'' that
+  * fetches the names of unknown artists or albums from resource constants.
+  *
+  * The ''ApplicationContext'' to resolve resources and the resource IDs to
+  * be used are passed to the constructor.
+  *
+  * @param appCtx           the ''ApplicationContext''
+  * @param resUnknownArtist resource ID for the unknown artist name
+  * @param resUnknownAlbum  resource ID for the unknown album name
+  */
+class UnknownPropertyResourceResolver(val appCtx: ApplicationContext,
+                                      val resUnknownArtist: String,
+                                      val resUnknownAlbum: String)
+  extends UnknownPropertyResolver {
+  /** The resolved name for an unknown artist. */
+  lazy val unknownArtistName: String = appCtx getResourceText resUnknownArtist
+
+  /** The resolved name for an unknown album. */
+  lazy val unknownAlbumName: String = appCtx getResourceText resUnknownAlbum
+
+  override def resolveArtistName(songID: MediaFileID): String = unknownArtistName
+
+  override def resolveAlbumName(songID: MediaFileID): String = unknownAlbumName
+}
---
+++ audioPlatform/src/test/scala/de/oliver_heger/linedj/platform/audio/model/UnknownPropertyResourceResolverSpec.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio.model
+
+import de.oliver_heger.linedj.shared.archive.media.{MediaFileID, MediumID}
+import net.sf.jguiraffe.gui.app.ApplicationContext
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+object UnknownPropertyResourceResolverSpec {
+  /** Test media ID. */
+  private val TestID = MediaFileID(MediumID("someMedium", None), "someSongUri")
+
+  /** Resource ID for an unknown artist. */
+  private val ResUnknownArtist = "unknownArtist"
+
+  /** Resource ID for an unknown album. */
+  private val ResUnknownAlbum = "unknownAlbum"
+
+  /** The name to be used for an unknown artist. */
+  private val UnknownArtistName = "Unknown Artist"
+
+  /** The name to be used for an unknown album. */
+  private val UnknownAlbumName = "Whatever Album"
+}
+
+/**
+  * Test class for ''UnknownPropertyResourceResolver''.
+  */
+class UnknownPropertyResourceResolverSpec extends FlatSpec with Matchers with MockitoSugar {
+
+  import UnknownPropertyResourceResolverSpec._
+
+  /**
+    * Creates a test resolver instance.
+    *
+    * @return the test instance
+    */
+  private def createResolver(): UnknownPropertyResourceResolver = {
+    val ctx = mock[ApplicationContext]
+    when(ctx.getResourceText(ResUnknownArtist)).thenReturn(UnknownArtistName)
+    when(ctx.getResourceText(ResUnknownAlbum)).thenReturn(UnknownAlbumName)
+    new UnknownPropertyResourceResolver(ctx, ResUnknownArtist, ResUnknownAlbum)
+  }
+
+  "An UnknownPropertyResourceResolver" should "resolve an unknown artist" in {
+    val resolver = createResolver()
+
+    resolver resolveArtistName TestID should be(UnknownArtistName)
+  }
+
+  it should "resolve an unknown album" in {
+    val resolver = createResolver()
+
+    resolver resolveAlbumName TestID should be(UnknownAlbumName)
+  }
+
+  it should "cache the names for unknown properties" in {
+    val resolver = createResolver()
+
+    resolver resolveArtistName TestID
+    resolver resolveArtistName TestID
+    resolver resolveAlbumName TestID
+    resolver resolveAlbumName TestID
+    verify(resolver.appCtx).getResourceText(ResUnknownArtist)
+    verify(resolver.appCtx).getResourceText(ResUnknownAlbum)
+  }
+}
