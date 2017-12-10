408f3ac1f7666c618e7676a9b069192bbabf1fb8
oheger <oliver.heger@oliver-heger.de>
2017-12-10 15:42:55 +0100

    Added PlaylistFileWriterActor class.
    This is a specialized file writer actor implementation that writes
    files with information about the current playlist.

---
+++ persistentPLHandler/src/main/scala/de/oliver_heger/linedj/playlist/persistence/PlaylistFileWriterActor.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.playlist.persistence
+
+import java.nio.file.Path
+
+import akka.actor.{ActorLogging, ActorRef}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import de.oliver_heger.linedj.io.stream.AbstractFileWriterActor.StreamFailure
+import de.oliver_heger.linedj.io.stream.{AbstractFileWriterActor, CancelableStreamSupport}
+import de.oliver_heger.linedj.playlist.persistence.PlaylistFileWriterActor.{FileWritten, WriteFile}
+
+object PlaylistFileWriterActor {
+
+  /**
+    * A message processed by [[PlaylistFileWriterActor]] telling it to write
+    * a file. The content of the file is determined by the given ''Source''.
+    * The file is written to the given target path.
+    *
+    * @param source the source defining the file content
+    * @param target the target path
+    */
+  case class WriteFile(source: Source[ByteString, Any], target: Path)
+
+  /**
+    * A message sent by [[PlaylistFileWriterActor]] in response of a file write
+    * operation. The message indicates that the write operation is now
+    * complete; by inspecting the ''Option'' with the exception, it can be
+    * determined if it has been successful.
+    *
+    * @param target    the target path that has been written
+    * @param exception an optional exception that caused the operation to fail
+    */
+  case class FileWritten(target: Path, exception: Option[Throwable])
+
+}
+
+/**
+  * An actor class that handles write operations for files that store
+  * information about a playlist.
+  *
+  * This actor is responsible for the actual IO operations. It slightly
+  * extends its base class by processing of a corresponding write message, and
+  * by a different error handling: If a write operation fails, the actor does
+  * not stop itself, but sends a response message to the caller. It is
+  * important that for each write request a response is returned.
+  */
+class PlaylistFileWriterActor extends AbstractFileWriterActor with CancelableStreamSupport with
+  ActorLogging {
+  override protected def customReceive: Receive = {
+    case WriteFile(source, target) =>
+      writeFile(source, target, FileWritten(target, None))
+  }
+
+  /**
+    * @inheritdoc This implementation just sends a failure notification to the
+    *             caller.
+    */
+  override protected def handleFailure(client: ActorRef, failure: StreamFailure): Unit = {
+    client ! FileWritten(failure.path, Some(failure.ex))
+  }
+}
---
+++ persistentPLHandler/src/test/scala/de/oliver_heger/linedj/playlist/persistence/PlaylistFileWriterActorSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.playlist.persistence
+
+import java.nio.file.Files
+
+import akka.actor.{ActorSystem, Props}
+import akka.stream.scaladsl.Source
+import akka.testkit.{ImplicitSender, TestKit}
+import akka.util.ByteString
+import de.oliver_heger.linedj.FileTestHelper
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+/**
+  * Test class for ''PlaylistFileWriterActor''.
+  */
+class PlaylistFileWriterActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  ImplicitSender with FlatSpecLike with BeforeAndAfterAll with Matchers with FileTestHelper {
+  def this() = this(ActorSystem("PlaylistFileWriterActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    tearDownTestFile()
+  }
+
+  "A PlaylistFileWriterActor" should "write a file" in {
+    val target = createPathInDirectory("test.dat")
+    val content = FileTestHelper.testBytes().grouped(32).map(a => ByteString(a))
+    val src = Source[ByteString](content.toList)
+    val writer = system.actorOf(Props[PlaylistFileWriterActor])
+
+    writer ! PlaylistFileWriterActor.WriteFile(src, target)
+    expectMsg(PlaylistFileWriterActor.FileWritten(target, None))
+    readDataFile(target) should be(FileTestHelper.TestData)
+  }
+
+  it should "handle an error when writing a file gracefully" in {
+    val target = createPathInDirectory("error")
+    Files createDirectory target
+    val source = Source.single(ByteString("This will not work"))
+    val writer = system.actorOf(Props[PlaylistFileWriterActor])
+
+    writer ! PlaylistFileWriterActor.WriteFile(source, target)
+    val msg = expectMsgType[PlaylistFileWriterActor.FileWritten]
+    msg.target should be(target)
+    msg.exception.isDefined shouldBe true
+
+    val target2 = createPathInDirectory("out.txt")
+    val source2 = Source.single(ByteString(FileTestHelper.TestData))
+    writer ! PlaylistFileWriterActor.WriteFile(source2, target2)
+    expectMsg(PlaylistFileWriterActor.FileWritten(target2, None))
+  }
+}
