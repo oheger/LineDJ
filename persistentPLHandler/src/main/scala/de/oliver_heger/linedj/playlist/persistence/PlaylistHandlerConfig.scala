6ca3410d508f44f564a73b4dd784e5553c265854
oheger <oliver.heger@oliver-heger.de>
2017-12-17 18:06:00 +0100

    Added PlaylistHandlerConfig class.
    This class is used internally to extract all supported settings from
    the application's configuration.

---
+++ persistentPLHandler/src/main/scala/de/oliver_heger/linedj/playlist/persistence/PlaylistHandlerConfig.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.playlist.persistence
+
+import java.nio.file.{Path, Paths}
+
+import org.apache.commons.configuration.Configuration
+
+import scala.concurrent.duration._
+import scala.util.Try
+
+object PlaylistHandlerConfig {
+  /** Prefix of all configuration properties used by this module. */
+  val ConfigPrefix = "audio.playlist.persistence."
+
+  /**
+    * The configuration property for the path to the persistent playlist. The
+    * songs contained in the current playlist are stored here.
+    */
+  val PropPlaylistPath: String = ConfigPrefix + "path"
+
+  /**
+    * Configuration property for the path to the file with position
+    * information. This file contains data like the current index in the
+    * playlist. It is written in regular intervals and when there is a change
+    * in the state of the playlist.
+    */
+  val PropPositionPath: String = ConfigPrefix + "pathPosition"
+
+  /**
+    * Configuration property for the auto save interval for the position file
+    * (in seconds). Per default, the position file is written each time the
+    * index in the current playlist changes. By specifying an auto save
+    * interval, it is written after this amount of time has been played. This
+    * increases the likelihood that playback starts at the same position even
+    * if the application crashes. This property is optional; if it is
+    * undefined, no additional save operations are executed.
+    */
+  val PropAutoSaveInterval: String = ConfigPrefix + "autoSave"
+
+  /**
+    * Configuration property for the maximum size of a persistent playlist
+    * file (in bytes). This is used to prevent OutOfMemory errors if the
+    * component is passed huge files. This property is optional; if it is
+    * undefined, no size restriction is applied.
+    */
+  val PropMaxFileSize: String = ConfigPrefix + "maxFileSize"
+
+  /**
+    * Configuration property defining a timeout for a shutdown operation (in
+    * milliseconds). When the playlist handler service is deactivated it may be
+    * necessary to write latest changes on the playlist state to disk. The
+    * service has to wait until this is done. With this property the maximum
+    * time the service will wait for the completion of the save operation can
+    * be specified. This property is optional; if unspecified, a default
+    * timeout value is set.
+    */
+  val PropShutdownTimeout: String = ConfigPrefix + "shutdownTimeout"
+
+  /**
+    * Default value for the ''shutdownTimeout'' property.
+    */
+  val DefaultShutdownTimeout: FiniteDuration = 5.seconds
+
+  /**
+    * Tries to create a [[PlaylistHandlerConfig]] object from the settings
+    * defined in the provided ''Configuration'' object. If all mandatory
+    * properties are defined and valid, a ''Success'' instance is returned.
+    * Otherwise, an error message is available.
+    *
+    * @param conf the ''Configuration'' object
+    * @return a ''Try'' with the extracted configuration data
+    */
+  def apply(conf: Configuration): Try[PlaylistHandlerConfig] = Try {
+    PlaylistHandlerConfig(Paths get conf.getString(PropPlaylistPath),
+      Paths get conf.getString(PropPositionPath),
+      conf.getInt(PropMaxFileSize, Integer.MAX_VALUE),
+      conf.getInt(PropAutoSaveInterval, Integer.MAX_VALUE).seconds,
+      conf.getLong(PropShutdownTimeout, DefaultShutdownTimeout.toMillis).millis)
+  }
+}
+
+/**
+  * A class storing all configuration settings supported by the playlist
+  * handler module.
+  *
+  * @param pathPlaylist     path to the playlist file
+  * @param pathPosition     path to the position file
+  * @param maxFileSize      the maximum file size (in bytes)
+  * @param autoSaveInterval the auto-save interval
+  * @param shutdownTimeout  the shutdown timeout
+  */
+case class PlaylistHandlerConfig(pathPlaylist: Path, pathPosition: Path,
+                                 maxFileSize: Int, autoSaveInterval: FiniteDuration,
+                                 shutdownTimeout: FiniteDuration)
---
+++ persistentPLHandler/src/test/scala/de/oliver_heger/linedj/playlist/persistence/PlaylistHandlerConfigSpec.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.playlist.persistence
+
+import java.nio.file.Paths
+
+import org.apache.commons.configuration.{Configuration, PropertiesConfiguration}
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.duration._
+
+object PlaylistHandlerConfigSpec {
+  /** Path to the playlist file. */
+  private val PathPlaylist = Paths get "persistedPlaylist.json"
+
+  /** Path to the position file. */
+  private val PathPosition = Paths get "position.json"
+
+  /** The maximum size of a playlist file. */
+  private val MaxFileSize = 8 * 1024
+
+  /** The test auto save interval. */
+  private val AutoSaveInterval = 90.seconds
+
+  /** The test shutdown timeout. */
+  private val ShutdownTimeout = 11.seconds
+
+  /**
+    * Creates a configuration that contains all the test settings.
+    *
+    * @return the initialized configuration
+    */
+  private def createFullConfig(): Configuration = {
+    val config = new PropertiesConfiguration
+    config.addProperty(PlaylistHandlerConfig.PropPlaylistPath, PathPlaylist.toString)
+    config.addProperty(PlaylistHandlerConfig.PropPositionPath, PathPosition.toString)
+    config.addProperty(PlaylistHandlerConfig.PropMaxFileSize, MaxFileSize)
+    config.addProperty(PlaylistHandlerConfig.PropAutoSaveInterval, AutoSaveInterval.toSeconds)
+    config.addProperty(PlaylistHandlerConfig.PropShutdownTimeout, ShutdownTimeout.toMillis)
+    config
+  }
+
+  /**
+    * Convenience function to remove a property from a configuration.
+    *
+    * @param c    the configuration
+    * @param prop the property to be removed
+    * @return the updated configuration
+    */
+  private def without(c: Configuration, prop: String): Configuration = {
+    c clearProperty prop
+    c
+  }
+}
+
+/**
+  * Test class for ''PlaylistHandlerConfig''.
+  */
+class PlaylistHandlerConfigSpec extends FlatSpec with Matchers {
+
+  import PlaylistHandlerConfigSpec._
+
+  "A PlaylistHandlerConfig" should "read all settings from a Configuration" in {
+    val config = PlaylistHandlerConfig(createFullConfig()).get
+
+    config.pathPlaylist should be(PathPlaylist)
+    config.pathPosition should be(PathPosition)
+    config.maxFileSize should be(MaxFileSize)
+    config.autoSaveInterval should be(AutoSaveInterval)
+    config.shutdownTimeout should be(ShutdownTimeout)
+  }
+
+  it should "detect a missing playlist path" in {
+    val settings = without(createFullConfig(), PlaylistHandlerConfig.PropPlaylistPath)
+
+    PlaylistHandlerConfig(settings).isFailure shouldBe true
+  }
+
+  it should "detect a missing position path" in {
+    val settings = without(createFullConfig(), PlaylistHandlerConfig.PropPositionPath)
+
+    PlaylistHandlerConfig(settings).isFailure shouldBe true
+  }
+
+  it should "disable auto-save if the property is not defined" in {
+    val settings = without(createFullConfig(), PlaylistHandlerConfig.PropAutoSaveInterval)
+    val config = PlaylistHandlerConfig(settings).get
+
+    config.autoSaveInterval.toSeconds should be(Integer.MAX_VALUE)
+  }
+
+  it should "disable the maximum file size check if the property is not defined" in {
+    val settings = without(createFullConfig(), PlaylistHandlerConfig.PropMaxFileSize)
+    val config = PlaylistHandlerConfig(settings).get
+
+    config.maxFileSize should be(Integer.MAX_VALUE)
+  }
+
+  it should "set a default value for shutdown timeout if the property is not defined" in {
+    val settings = without(createFullConfig(), PlaylistHandlerConfig.PropShutdownTimeout)
+    val config = PlaylistHandlerConfig(settings).get
+
+    config.shutdownTimeout should be(PlaylistHandlerConfig.DefaultShutdownTimeout)
+  }
+}
