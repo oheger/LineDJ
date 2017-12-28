b41314d9a18e4996ead92422696b50950e04d003
oheger <oliver.heger@oliver-heger.de>
2017-12-28 21:47:56 +0100

    Added UIController class.
    This is the main controller of the audio player application.

---
+++ audioPlayerUI/src/main/scala/de/oliver_heger/linedj/player/ui/UIController.scala
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.ui
+
+import akka.actor.Actor.Receive
+import de.oliver_heger.linedj.platform.audio.playlist.{Playlist, PlaylistMetaData,
+  PlaylistMetaDataRegistration, PlaylistService}
+import de.oliver_heger.linedj.platform.audio.{AudioPlayerState,
+  AudioPlayerStateChangeRegistration, AudioPlayerStateChangedEvent}
+import de.oliver_heger.linedj.platform.bus.{ConsumerSupport, Identifiable}
+import de.oliver_heger.linedj.platform.comm.{MessageBus, MessageBusListener}
+import de.oliver_heger.linedj.platform.mediaifc.ext.MediaIfcExtension.ConsumerRegistrationProvider
+import de.oliver_heger.linedj.player.engine.{AudioSource, PlaybackProgressEvent}
+import net.sf.jguiraffe.gui.builder.action.ActionStore
+
+object UIController {
+  /** Name of the group that contains all player-related actions. */
+  val PlayerActionGroup = "PlayerActions"
+
+  /** Name of the action to start playback. */
+  val ActionStartPlayback = "startPlaybackAction"
+
+  /** Name of the action to stop playback. */
+  val ActionStopPlayback = "stopPlaybackAction"
+
+  /** Name of the action to move to the next song. */
+  val ActionNextSong = "nextSongAction"
+
+  /** Name of the action to move to the previous song. */
+  val ActionPreviousSong = "previousSongAction"
+
+  /** Name of the action to move to a specific song. */
+  val ActionGotoSong = "gotoSongAction"
+
+  /**
+    * The initial player state. This is used only to have a defined
+    * value. It should be overridden immediately with a real state.
+    */
+  private val InitialPlayerState =
+    AudioPlayerState(Playlist(Nil, Nil), 0, playbackActive = false, playlistClosed = false)
+
+  /**
+    * Initial value for the last progress event. Here some plausible values are
+    * defined. This is overridden as soon as a real progress event is received.
+    */
+  private val InitialProgressEvent =
+    PlaybackProgressEvent(bytesProcessed = 0, playbackTime = 0,
+      currentSource = AudioSource("", 1, 0, 0))
+}
+
+/**
+  * The main controller class for the audio player application.
+  *
+  * This class is responsible for controlling the main window of the audio
+  * player application. It manages all required registrations at the message
+  * bus and for consumers and is the top-level entry point for event
+  * processing. It also handles the enabled states of actions affecting the
+  * audio player.
+  *
+  * In order to update the UI, the class relies on some helper classes. Actions
+  * themselves are also handled by specialized action task classes. However,
+  * the latter make use of some data maintained by this class.
+  *
+  * @param messageBus              the UI message bus
+  * @param actionStore             the action store
+  * @param playlistTableController the table controller
+  * @param currentSongController   the current song controller
+  * @param plService               the playlist service
+  */
+class UIController(val messageBus: MessageBus, actionStore: ActionStore,
+                   playlistTableController: PlaylistTableController,
+                   currentSongController: CurrentSongController,
+                   plService: PlaylistService) extends MessageBusListener with
+  ConsumerRegistrationProvider with Identifiable {
+
+  import UIController._
+
+  /**
+    * @inheritdoc This class is a consumer for playlist change events and
+    *             updates of playlist meta data.
+    */
+  override val registrations: Iterable[ConsumerSupport.ConsumerRegistration[_]] =
+    List(AudioPlayerStateChangeRegistration(componentID, consumePlayerStateChangeEvent),
+      PlaylistMetaDataRegistration(componentID, consumePlaylistMetaDataChanged))
+
+  /** Stores the last known player state. */
+  private var currentState = InitialPlayerState
+
+  /** Stores the last playback progress event. */
+  private var currentProgress = InitialProgressEvent
+
+  /**
+    * The message bus receiver function. It mainly receives events about
+    * playback progress.
+    *
+    * @return the message handling function
+    */
+  override def receive: Receive = {
+    case event: PlaybackProgressEvent =>
+      currentSongController playlistProgress event
+      currentProgress = event
+  }
+
+  /**
+    * Returns the latest player state received by this object.
+    *
+    * @return the latest player state
+    */
+  def lastPlayerState: AudioPlayerState = currentState
+
+  /**
+    * Returns the last ''PlaybackProgressEvent'' received by this controller.
+    *
+    * @return the last ''PlaybackProgressEvent''
+    */
+  def lastProgressEvent: PlaybackProgressEvent = currentProgress
+
+  /**
+    * Notifies this controller that a player-related action has been executed.
+    * This causes all actions to be disabled until the updated player state
+    * is received. Based on this state, the enabled state of actions is set.
+    */
+  def playerActionTriggered(): Unit = {
+    actionStore.enableGroup(PlayerActionGroup, false)
+  }
+
+  /**
+    * Consumer function for notifications of changes of the audio player state.
+    *
+    * @param event the state change event
+    */
+  private def consumePlayerStateChangeEvent(event: AudioPlayerStateChangedEvent): Unit = {
+    playlistTableController handlePlayerStateUpdate event.state
+    currentSongController.playlistStateChanged()
+    updateActionStates(event.state)
+    currentState = event.state
+  }
+
+  /**
+    * Consumer function for updates of playlist meta data.
+    *
+    * @param meta the updated meta data
+    */
+  private def consumePlaylistMetaDataChanged(meta: PlaylistMetaData): Unit = {
+    playlistTableController handleMetaDataUpdate meta
+    currentSongController.playlistStateChanged()
+  }
+
+  /**
+    * Updates the enabled states of all player-relevant actions after a change
+    * in the state of the audio player.
+    *
+    * @param state the current state of the audio player
+    */
+  private def updateActionStates(state: AudioPlayerState): Unit = {
+    val hasCurrent = plService.currentSong(state.playlist).isDefined
+    val isPlaying = hasCurrent && state.playbackActive
+    enableAction(ActionStopPlayback, isPlaying)
+    enableAction(ActionPreviousSong, isPlaying)
+    enableAction(ActionNextSong, isPlaying)
+    enableAction(ActionGotoSong, isPlaying)
+    enableAction(ActionStartPlayback, hasCurrent && !state.playbackActive)
+  }
+
+  /**
+    * Enables the specified action.
+    *
+    * @param name  the name of the action
+    * @param state the new enabled state
+    */
+  private def enableAction(name: String, state: Boolean): Unit = {
+    actionStore.getAction(name) setEnabled state
+  }
+}
---
+++ audioPlayerUI/src/test/scala/de/oliver_heger/linedj/player/ui/UIControllerSpec.scala
@@ -0,0 +1,392 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.ui
+
+import de.oliver_heger.linedj.platform.ActionTestHelper
+import de.oliver_heger.linedj.platform.audio.{AudioPlayerState,
+  AudioPlayerStateChangeRegistration, AudioPlayerStateChangedEvent}
+import de.oliver_heger.linedj.platform.audio.playlist.{Playlist, PlaylistMetaData,
+  PlaylistMetaDataRegistration, PlaylistService}
+import de.oliver_heger.linedj.platform.bus.ConsumerSupport.ConsumerRegistration
+import de.oliver_heger.linedj.platform.comm.MessageBus
+import de.oliver_heger.linedj.player.engine.{AudioSource, AudioSourcePlaylistInfo,
+  PlaybackProgressEvent}
+import net.sf.jguiraffe.gui.builder.action.ActionStore
+import org.mockito.Matchers.any
+import org.mockito.Mockito
+import org.mockito.Mockito._
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.reflect.ClassTag
+
+/**
+  * Test class for ''UIController''.
+  */
+class UIControllerSpec extends FlatSpec with Matchers {
+  /**
+    * Generates a state object for the audio player.
+    *
+    * @param playing flag whether playback is active.
+    * @return the player state
+    */
+  private def createState(playing: Boolean): AudioPlayerState =
+    AudioPlayerState(Playlist(Nil, Nil), 1, playing, playlistClosed = true)
+
+  /**
+    * Creates a test playback progress event.
+    *
+    * @param pos  the position offset
+    * @param time the time offset
+    * @return the event
+    */
+  private def createProgressEvent(pos: Long, time: Long): PlaybackProgressEvent = {
+    PlaybackProgressEvent(bytesProcessed = pos, playbackTime = time,
+      currentSource = AudioSource("test", 123456, 0, 0))
+  }
+
+  "A UIController" should "pass a player state to its sub controllers" in {
+    val state = createState(playing = false)
+    val helper = new ControllerTestHelper
+
+    helper.playerStateChanged(state)
+      .verifyStatePassedToSubControllers(state)
+  }
+
+  it should "report the last player state" in {
+    val state = createState(playing = false)
+    val helper = new ControllerTestHelper
+
+    helper.playerStateChanged(state)
+      .verifyLastPlayerState(state)
+  }
+
+  it should "report an initial player state" in {
+    val helper = new ControllerTestHelper
+
+    val state = helper.lastPlayerState
+    state.playbackActive shouldBe false
+    state.playlist.playedSongs should have size 0
+    state.playlist.pendingSongs should have size 0
+  }
+
+  it should "pass playlist meta data to the playlist table controller" in {
+    val meta = PlaylistMetaData(Map.empty)
+    val helper = new ControllerTestHelper
+
+    helper.metaDataChanged(meta)
+      .verifyMetaDataPassedToSubController(meta)
+  }
+
+  it should "pass a playlist progress event to the current song controller" in {
+    val event = createProgressEvent(100, 200)
+    val helper = new ControllerTestHelper
+
+    helper.sendOnBus(event)
+      .verifyProgressEventPassedToSubController(event)
+  }
+
+  it should "report the last progress event" in {
+    val event = createProgressEvent(1, 2)
+    val helper = new ControllerTestHelper
+
+    helper.sendOnBus(event)
+      .verifyLastProgressEvent(event)
+  }
+
+  it should "report an initial progress event" in {
+    val helper = new ControllerTestHelper
+
+    val event = helper.lastProgressEvent
+    event.currentSource.length should be > 0L
+    event.bytesProcessed should be(0)
+    event.playbackTime should be(0)
+  }
+
+  it should "enable actions if playback is active" in {
+    val helper = new ControllerTestHelper
+
+    helper.playerStateChanged(createState(playing = true))
+      .verifyActionsEnabled(UIController.ActionStopPlayback, UIController.ActionNextSong,
+        UIController.ActionGotoSong, UIController.ActionPreviousSong)
+  }
+
+  it should "enable actions if playback is not active" in {
+    val helper = new ControllerTestHelper
+
+    helper.playerStateChanged(createState(playing = false))
+      .verifyActionsEnabled(UIController.ActionStartPlayback)
+  }
+
+  it should "update action enabled states after each player state change" in {
+    val helper = new ControllerTestHelper
+
+    helper.playerStateChanged(createState(playing = true))
+      .playerStateChanged(createState(playing = false))
+      .verifyActionsEnabled(UIController.ActionStartPlayback)
+  }
+
+  it should "disable all actions after one has been triggered" in {
+    val helper = new ControllerTestHelper
+
+    helper.playerStateChanged(createState(playing = true))
+      .playerActionTriggered()
+      .verifyActionsEnabled()
+  }
+
+  it should "disable all actions if there is no current song" in {
+    val state = createState(playing = false)
+    val helper = new ControllerTestHelper
+
+    helper.prepareEmptyPlaylist(state)
+      .playerStateChanged(state)
+      .verifyActionsEnabled()
+  }
+
+  /**
+    * A test helper class managing a test instance and its dependencies.
+    */
+  private class ControllerTestHelper extends ActionTestHelper with MockitoSugar {
+    /** Mock for the table controller. */
+    private val tableController = mock[PlaylistTableController]
+
+    /** Mock for the current song controller. */
+    private val currentSongController = mock[CurrentSongController]
+
+    /** Mock for the playlist service. */
+    private val plService = createPlaylistService()
+
+    /** A set with the names of all relevant actions. */
+    private val actionNames = Set(UIController.ActionStartPlayback,
+      UIController.ActionStopPlayback, UIController.ActionNextSong,
+      UIController.ActionPreviousSong, UIController.ActionGotoSong)
+
+    /** The controller to be tested. */
+    private val controller = new UIController(mock[MessageBus], initActionMocks(), tableController,
+      currentSongController, plService)
+
+    /** The consumer function for player state change events. */
+    private lazy val stateConsumer = findRegistration[AudioPlayerStateChangeRegistration].callback
+
+    /** The consumer function for meta data updates. */
+    private lazy val metaDataConsumer = findRegistration[PlaylistMetaDataRegistration].callback
+
+    /**
+      * Sends a notification about an update in the player state to the test
+      * controller.
+      *
+      * @param state the state
+      * @return this test helper
+      */
+    def playerStateChanged(state: AudioPlayerState): ControllerTestHelper = {
+      val event = AudioPlayerStateChangedEvent(state)
+      stateConsumer(event)
+      this
+    }
+
+    /**
+      * Verifies that the player state is passed to helper objects.
+      *
+      * @param state the state
+      * @return this test helper
+      */
+    def verifyStatePassedToSubControllers(state: AudioPlayerState): ControllerTestHelper = {
+      val io = Mockito.inOrder(tableController, currentSongController)
+      io.verify(tableController).handlePlayerStateUpdate(state)
+      io.verify(currentSongController).playlistStateChanged()
+      this
+    }
+
+    /**
+      * Returns the last player state received by the controller test
+      * instance.
+      *
+      * @return the last player state
+      */
+    def lastPlayerState: AudioPlayerState = controller.lastPlayerState
+
+    /**
+      * Verifies that the last player state reported by the test controller
+      * equals the passed in state.
+      *
+      * @param state the expected state
+      * @return this test helper
+      */
+    def verifyLastPlayerState(state: AudioPlayerState): ControllerTestHelper = {
+      lastPlayerState should be(state)
+      this
+    }
+
+    /**
+      * Passes the specified meta data to the corresponding consumer function.
+      *
+      * @param meta the meta data
+      * @return this test helper
+      */
+    def metaDataChanged(meta: PlaylistMetaData): ControllerTestHelper = {
+      metaDataConsumer(meta)
+      this
+    }
+
+    /**
+      * Verifies that the specified meta data has been passed to the correct
+      * sub controllers.
+      *
+      * @param meta the expected meta data
+      * @return this test helper
+      */
+    def verifyMetaDataPassedToSubController(meta: PlaylistMetaData): ControllerTestHelper = {
+      val io = Mockito.inOrder(tableController, currentSongController)
+      io.verify(tableController).handleMetaDataUpdate(meta)
+      io.verify(currentSongController).playlistStateChanged()
+      this
+    }
+
+    /**
+      * Sends the specified message to the test controller via the message bus
+      * receiver function.
+      *
+      * @param msg the message
+      * @return this test helper
+      */
+    def sendOnBus(msg: AnyRef): ControllerTestHelper = {
+      controller receive msg
+      this
+    }
+
+    /**
+      * Verifies that the specified event has been passed to the correct sub
+      * controller.
+      *
+      * @param event the event
+      * @return this test helper
+      */
+    def verifyProgressEventPassedToSubController(event: PlaybackProgressEvent):
+    ControllerTestHelper = {
+      verify(currentSongController).playlistProgress(event)
+      this
+    }
+
+    /**
+      * Returns the last progress event reported by the test controller.
+      *
+      * @return the last progress event
+      */
+    def lastProgressEvent: PlaybackProgressEvent = controller.lastProgressEvent
+
+    /**
+      * Verifies that the test controller reports the specified progress event
+      * as the last received one.
+      *
+      * @param event the expected event
+      * @return this test helper
+      */
+    def verifyLastProgressEvent(event: PlaybackProgressEvent): ControllerTestHelper = {
+      lastProgressEvent should be(event)
+      this
+    }
+
+    /**
+      * Verifies that only the specified actions are enabled. All other
+      * actions are expected to be disabled.
+      *
+      * @param actions the names of actions that should be enabled
+      * @return this test helper
+      */
+    def verifyActionsEnabled(actions: String*): ControllerTestHelper = {
+      actionNames foreach { a =>
+        val expState = actions contains a
+        isActionEnabled(a) shouldBe expState
+      }
+      this
+    }
+
+    /**
+      * Notifies the test controller about an action that was triggered.
+      *
+      * @return this test helper
+      */
+    def playerActionTriggered(): ControllerTestHelper = {
+      controller.playerActionTriggered()
+      this
+    }
+
+    /**
+      * Prepares the mock for the playlist service to report an undefined
+      * current song for the playlist in the provided state object.
+      *
+      * @param state the state
+      * @return this test helper
+      */
+    def prepareEmptyPlaylist(state: AudioPlayerState): ControllerTestHelper = {
+      when(plService.currentSong(state.playlist)).thenReturn(None)
+      this
+    }
+
+    /**
+      * Creates all mock actions required by the test class and an action store
+      * that provides access to them.
+      *
+      * @return the mock action store
+      */
+    private def initActionMocks(): ActionStore = {
+      createActions(actionNames.toSeq: _*)
+      resetActionStates()
+      val store = createActionStore()
+      doAnswer(new Answer[AnyRef] {
+        override def answer(invocation: InvocationOnMock): AnyRef = {
+          resetActionStates()
+          null
+        }
+      }).when(store).enableGroup(UIController.PlayerActionGroup, false)
+      store
+    }
+
+    /**
+      * Creates an initialized mock for the playlist service. The mock is
+      * prepared to return a current song for an arbitrary playlist. (This is
+      * needed to test updates of action states; only if a playlist has a
+      * current song, some actions can be enabled.)
+      *
+      * @return the mock playlist service
+      */
+    private def createPlaylistService(): PlaylistService = {
+      val service = mock[PlaylistService]
+      when(service.currentSong(any())).thenReturn(Some(mock[AudioSourcePlaylistInfo]))
+      service
+    }
+
+    /**
+      * Finds a registration of the specified type.
+      *
+      * @param t the class tag of the type
+      * @tparam T the type of the registration
+      * @return the consumer registration of this type
+      */
+    private def findRegistration[T <: ConsumerRegistration[_]](implicit t: ClassTag[T]): T =
+      controller.registrations.find(_.getClass == t.runtimeClass) match {
+        case None =>
+          throw new AssertionError("No registration of type " + t.runtimeClass)
+        case Some(reg) =>
+          reg.id should be(controller.componentID)
+          reg.asInstanceOf[T]
+      }
+  }
+
+}
