2cd926dd3b9a5ac3fe0f3a3270f070b6b491d6b8
oheger <oliver.heger@oliver-heger.de>
2017-12-30 21:16:42 +0100

    Added AudioPlayerConfig class.
    This class encapsulates access to all configuration properties
    supported by the audio player application. It is cleaner to have this
    logic located in a single class.

---
+++ audioPlayerUI/src/main/scala/de/oliver_heger/linedj/player/ui/AudioPlayerConfig.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.ui
+
+import org.apache.commons.configuration.Configuration
+
+object AudioPlayerConfig {
+  /** Common prefix for all configuration keys. */
+  val ConfigPrefix = "audio.ui."
+
+  /**
+    * Configuration property for the maximum size of fields in the UI. If a
+    * text to be displayed exceeds this value, it is "rotated"; i.e. it is
+    * scrolled through the field, so that the full content can be seen.
+    */
+  val PropMaxFieldSize: String = ConfigPrefix + "maxFieldSize"
+
+  /**
+    * Configuration property defining the speed for scrolling in UI fields. The
+    * property value is an integer interpreted as divisor for the playback
+    * time. A value of 1 means that for each second 1 letter is scrolled; 2
+    * means that every 2 seconds one letter is scrolled, etc.
+    */
+  val PropRotationSpeed: String = ConfigPrefix + "rotationSpeed"
+
+  /**
+    * Configuration property that determines the behavior of the move backwards
+    * action. The property defines a threshold in seconds. If the playback time
+    * of the current song is bigger than this threshold, the backwards action
+    * causes this song to be played again (from the start). Otherwise, playback
+    * starts with the previous song in the playlist.
+    */
+  val PropSkipBackwardsThreshold: String = ConfigPrefix + "skipBackwardsThreshold"
+
+  /**
+    * Configuration property that determines whether playback should start
+    * automatically when a playback is available. If this property is set to
+    * '''true''', a change in the current playlist automatically causes
+    * playback to start if it is not yet enabled. The default value is
+    * '''false'''.
+    */
+  val PropAutoStartPlayback: String = ConfigPrefix + "autoStartPlayback"
+
+  /**
+    * Default value for the ''rotationSpeed'' property. Per default, the
+    * scrolling speed is one character per playback second.
+    */
+  val DefRotationSpeed = 1
+
+  /**
+    * Default value for the ''skipBackwardsThreshold'' property.
+    */
+  val DefSkipBackwardsThreshold = 5
+
+  /**
+    * Returns a new instance of ''AudioPlayerConfig'' that is initialized from
+    * the specified ''Configuration'' object.
+    *
+    * @param config the ''Configuration''
+    * @return the new ''AudioPlayerConfig'' instance
+    */
+  def apply(config: Configuration): AudioPlayerConfig =
+    AudioPlayerConfig(config.getInt(PropMaxFieldSize, Integer.MAX_VALUE),
+      config.getInt(PropRotationSpeed, DefRotationSpeed),
+      config.getInt(PropSkipBackwardsThreshold, DefSkipBackwardsThreshold),
+      config.getBoolean(PropAutoStartPlayback, false))
+}
+
+/**
+  * A class allowing convenient access to all configuration properties
+  * supported by the audio player application.
+  *
+  * @param maxUIFieldSize         the maximum size of fields in the UI
+  * @param rotationSpeed          the rotation speed
+  * @param skipBackwardsThreshold threshold for skipping backwards
+  * @param autoStartPlayback      the auto start playback flag
+  */
+case class AudioPlayerConfig(maxUIFieldSize: Int, rotationSpeed: Int,
+                             skipBackwardsThreshold: Int, autoStartPlayback: Boolean)
---
+++ audioPlayerUI/src/test/scala/de/oliver_heger/linedj/player/ui/AudioPlayerConfigSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.ui
+
+import org.apache.commons.configuration.PropertiesConfiguration
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''AudioPlayerConfig''.
+  */
+class AudioPlayerConfigSpec extends FlatSpec with Matchers {
+  "An AudioPlayerConfig" should "provide access to all properties" in {
+    val MaxFieldSize = 42
+    val RotationSpeed = 3
+    val SkipBackwardsThreshold = 8
+    val AutoStart = true
+    val c = new PropertiesConfiguration
+    c.addProperty(AudioPlayerConfig.PropMaxFieldSize, MaxFieldSize)
+    c.addProperty(AudioPlayerConfig.PropRotationSpeed, RotationSpeed)
+    c.addProperty(AudioPlayerConfig.PropSkipBackwardsThreshold, SkipBackwardsThreshold)
+    c.addProperty(AudioPlayerConfig.PropAutoStartPlayback, AutoStart)
+
+    val config = AudioPlayerConfig(c)
+    config.maxUIFieldSize should be(MaxFieldSize)
+    config.rotationSpeed should be(RotationSpeed)
+    config.skipBackwardsThreshold should be(SkipBackwardsThreshold)
+    config.autoStartPlayback shouldBe AutoStart
+  }
+
+  it should "use correct default values" in {
+    val config = AudioPlayerConfig(new PropertiesConfiguration)
+
+    config.maxUIFieldSize should be(Integer.MAX_VALUE)
+    config.rotationSpeed should be(AudioPlayerConfig.DefRotationSpeed)
+    config.skipBackwardsThreshold should be(AudioPlayerConfig.DefSkipBackwardsThreshold)
+    config.autoStartPlayback shouldBe false
+  }
+}
