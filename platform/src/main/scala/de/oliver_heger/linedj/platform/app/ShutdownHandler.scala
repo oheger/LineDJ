c2a2b4b43f05bd5c0daac84d9f59805554625d6b
oheger <oliver.heger@oliver-heger.de>
2018-01-08 14:49:38 +0100

    Added ShutdownHandler class.
    This class is now responsible for handling a shutdown operation. It
    supports shutdown observers and waits for the actual shutdown until
    all observers have sent a confirmation.

---
+++ platform/src/main/scala/de/oliver_heger/linedj/platform/app/ShutdownHandler.scala
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import akka.actor.Actor.Receive
+import de.oliver_heger.linedj.platform.app.ShutdownHandler.{RegisterShutdownObserver,
+  RemoveShutdownObserver, Shutdown, ShutdownDone}
+import de.oliver_heger.linedj.platform.bus.ComponentID
+import de.oliver_heger.linedj.platform.comm.MessageBusListener
+import org.slf4j.LoggerFactory
+
+object ShutdownHandler {
+
+  /**
+    * A message processed by [[ShutdownHandler]] that causes the
+    * shutdown of the current system. This message is typically sent by a
+    * component responsible for shutdown handling. The current application
+    * context must be provided to ensure that this message was not sent from
+    * an external system (e.g. via a remote connection); the application
+    * context should only be known in the local application.
+    *
+    * Components registered as shutdown observers must handle and confirm
+    * this message before the platform can go down.
+    *
+    * @param applicationContext the ''ClientApplicationContext''
+    */
+  case class Shutdown(applicationContext: ClientApplicationContext)
+
+  /**
+    * A message processed by [[ShutdownHandler]] that registers a shutdown
+    * observer. This message causes the handler to wait for a shutdown
+    * confirmation from this observer before actually shutting down the
+    * platform.
+    *
+    * @param observerID the ID of the observer
+    */
+  case class RegisterShutdownObserver(observerID: ComponentID)
+
+  /**
+    * A message processed by [[ShutdownHandler]] that removes the registration
+    * of a shutdown observer. During a shutdown operation, the handler will no
+    * longer wait for a confirmation of this observer.
+    *
+    * @param observerID the ID of the observer to be removed
+    */
+  case class RemoveShutdownObserver(observerID: ComponentID)
+
+  /**
+    * A message processed by [[ShutdownHandler]] telling it that the specified
+    * shutdown observer has completed its shutdown handling. When such messages
+    * have arrived for all registered components the system can actually go
+    * down.
+    *
+    * @param observerID the ID of the observer
+    */
+  case class ShutdownDone(observerID: ComponentID)
+
+}
+
+/**
+  * A class responsible for a controlled shutdown of the LineDJ platform.
+  *
+  * During shutdown some components may have to execute some specific steps
+  * like saving their current state to disk. Such actions may be hard to be
+  * implemented using normal OSGi deactivation logic: It has to be ensured that
+  * all dependent services are still available that may be needed for a
+  * graceful shutdown operation. It may also be the case that asynchronous
+  * calls need to be executed which would have to be synchronized with the OSGi
+  * management thread.
+  *
+  * To simplify shutdown handling and make it more robust, this class
+  * introduces another concept for shutdown handling. Components that have to
+  * execute steps during a shutdown operation can register at this component by
+  * publishing a ''RegisterShutdownObserver'' message on the system message
+  * bus. If there is at least one observer registered, a shutdown operation
+  * will not be executed immediately, but the handler waits for confirmations
+  * from all registered shutdown observers. So an observer must handle a
+  * ''Shutdown'' message and eventually answer it with a ''ShutdownDone''
+  * message. Only after all observers have sent such a ''ShutdownDone''
+  * message, the platform is actually shut down.
+  *
+  * @param app the management application
+  */
+class ShutdownHandler(app: ClientManagementApplication) extends MessageBusListener {
+  /** The logger. */
+  private val log = LoggerFactory.getLogger(getClass)
+
+  /** Stores the IDs of shutdown observers registered at this component. */
+  private var observers = Set.empty[ComponentID]
+
+  /** A flag whether a shutdown operation is ongoing. */
+  private var shutdownInProgress = false
+
+  /**
+    * Returns the function for handling messages published on the message bus.
+    *
+    * @return the message handling function
+    */
+  override def receive: Receive = {
+    case Shutdown(ctx) =>
+      log.info("Received Shutdown command.")
+      if (ctx == app) {
+        if (!shutdownInProgress) {
+          initiateShutdown()
+        } else log.info("Ignoring Shutdown command while shutdown is in progress.")
+      }
+      else log.warn("Ignoring invalid Shutdown command: " + ctx)
+
+    case RegisterShutdownObserver(id) =>
+      log.info("Added shutdown observer " + id)
+      observers += id
+
+    case RemoveShutdownObserver(id) =>
+      observers -= id
+      log.info("Removed shutdown observer " + id)
+      if (shutdownInProgress) shutdownIfPossible()
+
+    case ShutdownDone(id) =>
+      log.info("Received shutdown confirmation from " + id)
+      if (shutdownInProgress) {
+        observers -= id
+        shutdownIfPossible()
+      }
+  }
+
+  /**
+    * Initiates a shutdown operation in reaction of a ''Shutdown'' message.
+    * If possible, the platform is shutdown immediately; otherwise, waiting for
+    * confirmation messages starts.
+    */
+  private def initiateShutdown(): Unit = {
+    shutdownInProgress = true
+    if (observers.isEmpty) {
+      shutdownPlatform()
+    }
+  }
+
+  /**
+    * Shuts down the platform if all observers have sent their confirmation.
+    */
+  private def shutdownIfPossible(): Unit = {
+    if (observers.isEmpty) shutdownPlatform()
+  }
+
+  /**
+    * Shuts down the whole platform. This method is called when all
+    * prerequisites for a shutdown operation are fulfilled.
+    */
+  private def shutdownPlatform(): Unit = {
+    app.shutdown()
+  }
+}
---
+++ platform/src/test/scala/de/oliver_heger/linedj/platform/app/ShutdownHandlerSpec.scala
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.app
+
+import de.oliver_heger.linedj.platform.bus.ComponentID
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''ShutdownHandler''.
+  */
+class ShutdownHandlerSpec extends FlatSpec with Matchers with MockitoSugar {
+  "A ShutdownHandler" should "shutdown the platform directly if there is no observer" in {
+    val helper = new HandlerTestHelper
+
+    helper.triggerShutdown()
+      .expectShutdown()
+  }
+
+  it should "not shutdown the platform for an invalid Shutdown message" in {
+    val helper = new HandlerTestHelper
+
+    helper.triggerShutdown(ctx = mock[ClientApplicationContext])
+      .expectNoShutdown()
+  }
+
+  it should "wait for confirmations of observers before shutting down" in {
+    val helper = new HandlerTestHelper
+    val obs1 = helper.registerObserver()
+    val obs2 = helper.registerObserver()
+
+    helper.triggerShutdown()
+      .expectNoShutdown()
+      .confirmShutdown(obs2)
+      .confirmShutdown(obs1)
+      .expectShutdown()
+  }
+
+  it should "handler messages to remove observers" in {
+    val helper = new HandlerTestHelper
+    val obs1 = helper.registerObserver()
+    val obs2 = helper.registerObserver()
+
+    helper.removeObserver(obs2)
+      .triggerShutdown()
+      .confirmShutdown(obs1)
+      .expectShutdown()
+  }
+
+  it should "ignore shutdown confirmations if no shutdown is in progress" in {
+    val helper = new HandlerTestHelper
+    val obs = helper.registerObserver()
+
+    helper.confirmShutdown(obs)
+      .triggerShutdown()
+      .expectNoShutdown()
+  }
+
+  it should "ignore multiple shutdown triggers" in {
+    val helper = new HandlerTestHelper
+
+    helper.triggerShutdown()
+      .triggerShutdown()
+      .expectShutdown()
+  }
+
+  it should "handle a remove observer operation while shutdown is in progress" in {
+    val helper = new HandlerTestHelper
+    val obs = helper.registerObserver()
+
+    helper.triggerShutdown()
+      .removeObserver(obs)
+      .expectShutdown()
+  }
+
+  /**
+    * A test helper class managing a test instance and its dependencies.
+    */
+  private class HandlerTestHelper {
+    /** Mock for the management application. */
+    private val application = mock[ClientManagementApplication]
+
+    /** The handler to be tested. */
+    private val handler = new ShutdownHandler(application)
+
+    /**
+      * Sends a shutdown message to the handler under test.
+      *
+      * @param ctx the context to be passed to the handler
+      * @return this test helper
+      */
+    def triggerShutdown(ctx: ClientApplicationContext = application): HandlerTestHelper = {
+      send(ShutdownHandler.Shutdown(ctx))
+      this
+    }
+
+    /**
+      * Verifies that a shutdown has been executed.
+      *
+      * @return this test helper
+      */
+    def expectShutdown(): HandlerTestHelper = {
+      verify(application).shutdown()
+      this
+    }
+
+    /**
+      * Verifies that no shutdown operation has been completed yet.
+      *
+      * @return this test helper
+      */
+    def expectNoShutdown(): HandlerTestHelper = {
+      verify(application, never()).shutdown()
+      this
+    }
+
+    /**
+      * Registers a shutdown observer at the test handler and returns its
+      * component ID.
+      *
+      * @return the component ID of the new observer
+      */
+    def registerObserver(): ComponentID = {
+      val id = ComponentID()
+      send(ShutdownHandler.RegisterShutdownObserver(id))
+      id
+    }
+
+    /**
+      * Sends a shutdown confirmation message for the specified observer to the
+      * test handler.
+      *
+      * @param observerID the observer ID
+      * @return this test helper
+      */
+    def confirmShutdown(observerID: ComponentID): HandlerTestHelper = {
+      send(ShutdownHandler.ShutdownDone(observerID))
+      this
+    }
+
+    /**
+      * Removes the observer with the specified ID from the test handler.
+      *
+      * @param observerID the observer ID
+      * @return this test helper
+      */
+    def removeObserver(observerID: ComponentID): HandlerTestHelper = {
+      send(ShutdownHandler.RemoveShutdownObserver(observerID))
+      this
+    }
+
+    /**
+      * Sens the specified message to the test handler.
+      *
+      * @param msg the message
+      * @return this test helper
+      */
+    private def send(msg: Any): HandlerTestHelper = {
+      handler receive msg
+      this
+    }
+  }
+
+}
