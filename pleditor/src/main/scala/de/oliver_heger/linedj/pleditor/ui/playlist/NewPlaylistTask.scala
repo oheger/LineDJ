3374e85e3a1df493461b1c89cb4105a47cc1aa37
oheger <oliver.heger@oliver-heger.de>
2018-01-18 22:13:09 +0100

    Added NewPlaylistTask.
    This is the task for the action which clears the currently edited
    playlist and resets the playlist of the audio platform.

---
+++ pleditor/src/main/scala/de/oliver_heger/linedj/pleditor/ui/playlist/NewPlaylistTask.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.playlist
+
+import de.oliver_heger.linedj.platform.audio.SetPlaylist
+import de.oliver_heger.linedj.platform.audio.playlist.Playlist
+import de.oliver_heger.linedj.platform.comm.MessageBus
+
+/**
+  * The task implementation for the ''new playlist'' action.
+  *
+  * This task resets the current playlist of the platform audio player by
+  * setting an empty one that is not closed. It also removes all songs from the
+  * current table model.
+  *
+  * @param controller the ''PlaylistController''
+  * @param messageBus the message bus
+  */
+class NewPlaylistTask(controller: PlaylistController, messageBus: MessageBus)
+  extends AbstractPlaylistManipulationTask(controller) with PlaylistManipulator {
+  /**
+    * @inheritdoc This implementation clears the table model and send a
+    *             ''SetPlaylist'' message with an empty, open playlist.
+    */
+  override def updatePlaylist(context: PlaylistSelectionContext): Unit = {
+    context.tableHandler.getModel.clear()
+    context.tableHandler.tableDataChanged()
+    messageBus publish SetPlaylist(playlist = Playlist(Nil, Nil), closePlaylist = false)
+  }
+
+  /**
+    * @inheritdoc This implementation returns always '''true'''.
+    */
+  override def isEnabled(context: PlaylistSelectionContext): Boolean = true
+}
---
+++ pleditor/src/test/scala/de/oliver_heger/linedj/pleditor/ui/playlist/NewPlaylistTaskSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.playlist
+
+import java.util
+
+import de.oliver_heger.linedj.platform.audio.SetPlaylist
+import de.oliver_heger.linedj.platform.audio.playlist.Playlist
+import de.oliver_heger.linedj.platform.comm.MessageBus
+import net.sf.jguiraffe.gui.builder.components.model.TableHandler
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''NewPlaylistTask''.
+  */
+class NewPlaylistTaskSpec extends FlatSpec with Matchers with MockitoSugar {
+  "A NewPlaylistTask" should "always be enabled" in {
+    val context = mock[PlaylistSelectionContext]
+    val task = new NewPlaylistTask(mock[PlaylistController], mock[MessageBus])
+
+    task isEnabled context shouldBe true
+    verifyZeroInteractions(context)
+  }
+
+  it should "clear the table model and reset the playlist" in {
+    val context = mock[PlaylistSelectionContext]
+    val tableHandler = mock[TableHandler]
+    val model = new util.ArrayList(util.Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8))
+    when(context.tableHandler).thenReturn(tableHandler)
+    doReturn(model).when(tableHandler).getModel
+    val bus = mock[MessageBus]
+
+    val task = new NewPlaylistTask(mock[PlaylistController], bus)
+    task.updatePlaylist(context)
+
+    model.size() should be(0)
+    verify(tableHandler).tableDataChanged()
+    verify(bus).publish(SetPlaylist(Playlist(Nil, Nil), closePlaylist = false))
+  }
+}
