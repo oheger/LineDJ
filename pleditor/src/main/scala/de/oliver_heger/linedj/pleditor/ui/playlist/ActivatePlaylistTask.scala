670418ffcd2d8a19a887c4008708cb7df5cb4502
oheger <oliver.heger@oliver-heger.de>
2018-01-18 21:55:25 +0100

    Added ActivatePlaylistTask.
    This is the task for the action which activates the playlist that is
    currently edited in the playlist editor application.

---
+++ pleditor/src/main/scala/de/oliver_heger/linedj/pleditor/ui/playlist/ActivatePlaylistTask.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.playlist
+
+import de.oliver_heger.linedj.platform.audio.SetPlaylist
+import de.oliver_heger.linedj.platform.audio.model.SongData
+import de.oliver_heger.linedj.platform.audio.playlist.{Playlist, PlaylistService}
+import de.oliver_heger.linedj.platform.comm.MessageBus
+import de.oliver_heger.linedj.shared.archive.media.MediaFileID
+
+/**
+  * Task implementation for the ''activate playlist'' action.
+  *
+  * This action generates a playlist from the current songs in the table model
+  * and passes it the the audio platform via the message bus.
+  *
+  * @param playlistService the playlist service
+  * @param messageBus      the message bus
+  * @param tableModel      the table model collection
+  */
+class ActivatePlaylistTask(playlistService: PlaylistService[Playlist, MediaFileID],
+                           messageBus: MessageBus,
+                           tableModel: java.util.List[SongData]) extends Runnable {
+  override def run(): Unit = {
+    import collection.JavaConverters._
+    val playlist = playlistService.toPlaylist(tableModel.asScala.map(_.id).toList, 0)
+    playlist foreach (messageBus publish SetPlaylist(_))
+  }
+}
---
+++ pleditor/src/test/scala/de/oliver_heger/linedj/pleditor/ui/playlist/ActivatePlaylistTaskSpec.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015-2017 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.pleditor.ui.playlist
+
+import java.util
+
+import de.oliver_heger.linedj.platform.audio.SetPlaylist
+import de.oliver_heger.linedj.platform.audio.model.SongData
+import de.oliver_heger.linedj.platform.audio.playlist.{Playlist, PlaylistService}
+import de.oliver_heger.linedj.platform.comm.MessageBus
+import de.oliver_heger.linedj.shared.archive.media.{MediaFileID, MediumID}
+import de.oliver_heger.linedj.shared.archive.metadata.MediaMetaData
+import org.mockito.Matchers.{any, anyInt}
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+object ActivatePlaylistTaskSpec {
+  /** A test medium used by test songs. */
+  private val TestMedium = MediumID("testMedium", None)
+
+  /**
+    * Creates the ID for a test song in the playlist.
+    *
+    * @param idx the index of the test song
+    * @return the ID of this test song
+    */
+  private def fileID(idx: Int): MediaFileID =
+    MediaFileID(TestMedium, "testSong" + idx)
+
+  /**
+    * Creates a ''SongData'' object from the specified index.
+    *
+    * @param idx the index of the test song
+    * @return the data object representing this test song
+    */
+  private def testSong(idx: Int): SongData =
+    SongData(fileID(idx), MediaMetaData(), "title" + idx, "artist" + idx, "album" + idx)
+}
+
+/**
+  * Test class for ''ActivatePlaylistTask''.
+  */
+class ActivatePlaylistTaskSpec extends FlatSpec with Matchers with MockitoSugar {
+
+  import ActivatePlaylistTaskSpec._
+  import collection.JavaConverters._
+
+  "An ActivatePlaylistTask" should "send a correct SetPlaylist message" in {
+    val indices = List(1, 2, 4, 8, 11, 23)
+    val model = new util.ArrayList[SongData](indices.size)
+    model.addAll((indices map testSong).asJava)
+    val plService = mock[PlaylistService[Playlist, MediaFileID]]
+    val bus = mock[MessageBus]
+    val playlist = mock[Playlist]
+    val expSongIds = indices map fileID
+    when(plService.toPlaylist(expSongIds, 0)).thenReturn(Some(playlist))
+
+    val task = new ActivatePlaylistTask(plService, bus, model)
+    task.run()
+    verify(bus).publish(SetPlaylist(playlist))
+  }
+
+  it should "handle a None result of the playlist service" in {
+    val model = new util.ArrayList[SongData]
+    val plService = mock[PlaylistService[Playlist, MediaFileID]]
+    val bus = mock[MessageBus]
+    when(plService.toPlaylist(any(), anyInt())).thenReturn(None)
+
+    val task = new ActivatePlaylistTask(plService, bus, model)
+    task.run()
+    verifyZeroInteractions(bus)
+  }
+}
