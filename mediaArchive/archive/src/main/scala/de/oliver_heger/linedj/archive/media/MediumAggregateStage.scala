491675b159e2a7800f67e5d4ac7091815bc68b3d
oheger <oliver.heger@oliver-heger.de>
2018-03-31 22:58:18 +0200

    Added MediumAggregateStage class.
    This is a specialized stage that assigns media files to the media they
    belong to during a scan operation.

---
+++ mediaArchive/archive/src/main/scala/de/oliver_heger/linedj/archive/media/MediumAggregateStage.scala
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.media
+
+import java.nio.file.{Files, Path}
+
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import de.oliver_heger.linedj.archive.media.MediumAggregateStage.FileDataFactory
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+import scalaz.State
+
+object MediumAggregateStage {
+  /** Constant for the file extension for medium description files. */
+  private val SettingsExtension = ".settings"
+
+  /**
+    * An internally used data class that stores the aggregated file information
+    * for a single medium.
+    *
+    * @param path  the root path of this medium
+    * @param id    the ID of the medium
+    * @param files the aggregated files on this medium
+    */
+  private case class MediumAggregateData(path: Path, id: MediumID, files: List[FileData]) {
+    /**
+      * Checks whether the specified file is potentially in scope of the
+      * medium represented by this instance. A result of '''false''' means that
+      * this medium is now complete.
+      *
+      * @param file the file in question
+      * @return a flag whether this file is in scope of this medium
+      */
+    def isInScope(file: Path): Boolean = file startsWith path
+
+    /**
+      * Checks whether the specified file is owned by this medium. Provided
+      * that ''isInScope()'' returned '''true''' for the file, with this method
+      * it can be checked that the file actually belongs to this medium.
+      *
+      * @param file the file in question
+      * @return a flag whether this file is owned by this medium
+      */
+    def owns(file: Path): Boolean = file.getParent != path
+
+    /**
+      * Adds the specified file to the list of files that belong to this
+      * medium.
+      *
+      * @param file the file to be added
+      * @return an updated instance that contains this file
+      */
+    def addFile(file: FileData): MediumAggregateData = copy(files = file :: files)
+  }
+
+  /**
+    * Type alias for the state managed by this stage. The state consists of
+    * information about the media that are currently processed. Usually, there
+    * will be only a single current medium and the artificial medium for songs
+    * not assigned to a specific medium. But in case of nested media, there may
+    * be more.
+    */
+  private type MediaState = List[MediumAggregateData]
+
+  /**
+    * Type alias for a function that converts ''Path'' objects to corresponding
+    * ''FileData'' instances.
+    */
+  type FileDataFactory = Path => FileData
+
+  /**
+    * Converts the specified path to a ''FileData'' object.
+    *
+    * @param p the path to be converted
+    * @return the resulting ''FileData'' object
+    */
+  private def toFileData(p: Path): FileData = {
+    val fileSize = Files size p
+    FileData(p.toString, fileSize)
+  }
+
+  /**
+    * Checks whether the specified path represents a medium description file.
+    *
+    * @param p the path to be checked
+    * @return a flag whether this path is a medium description file
+    */
+  private def isSettingsFile(p: Path): Boolean =
+    p.toString.endsWith(SettingsExtension)
+
+  /**
+    * A function to calculate the next state of this stage after a path is
+    * processed. Here the logic of aggregating files to media and determining
+    * when a medium is complete is contained. In addition to the next state,
+    * an optional ''MediaScanResult'' is returned that contains the data of all
+    * media that are now complete.
+    *
+    * @param file        the current file to be processed
+    * @param root        the root path of the current media archive
+    * @param archiveName the name of the media archive
+    * @param converter   the function to create ''FileData'' objects
+    * @return the object to calculate the next state
+    */
+  private def nextState(file: Path, root: Path, archiveName: String, converter: FileDataFactory):
+  State[MediaState, Option[MediaScanResult]] =
+    State { s =>
+      if (isSettingsFile(file)) {
+        val mid = MediumID.fromDescriptionPath(file, archiveName)
+        val newAgg = MediumAggregateData(file.getParent, mid, Nil)
+        val (completed, active) = s.span(!_.isInScope(file))
+        (newAgg :: active, createScanResultForCompleteMedia(completed, root))
+      } else {
+        val (next, completed) = processFile(s, file, converter)
+        (next, createScanResultForCompleteMedia(completed, root))
+      }
+    }
+
+  /**
+    * Processes a file from the iteration over the media archive, updates the
+    * aggregation state accordingly, and determines the media that are now
+    * complete.
+    *
+    * @param converter the function to create ''FileData'' objects
+    * @param state     the current state of processing
+    * @param file      the file to be processed
+    * @return the updated state and a sequence of completed media
+    */
+  private def processFile(state: MediaState, file: Path, converter: FileDataFactory):
+  (MediaState, List[MediumAggregateData]) = {
+    val medium = state.head
+    if (state.size == 1)
+      (List(medium addFile converter(file)), Nil)
+    else if (medium isInScope file) {
+      if (medium owns file)
+        (medium.addFile(converter(file)) :: state.tail, Nil)
+      else {
+        val (tempState, complete) = processFile(state.tail, file, converter)
+        (medium :: tempState, complete)
+      }
+    } else {
+      val (nextState, complete) = processFile(state.tail, file, converter)
+      (nextState, medium :: complete)
+    }
+  }
+
+  /**
+    * Converts a sequence of completed media to an optional scan result. All
+    * completed media are added to the map contained in the scan result object.
+    * If there are no completed media, result is ''None''.
+    *
+    * @param media the sequence with completed media
+    * @param root  the root path of the current media archive
+    * @return an optional ''MediaScanResult''
+    */
+  private def createScanResultForCompleteMedia(media: Seq[MediumAggregateData], root: Path):
+  Option[MediaScanResult] =
+    if (media.isEmpty) None
+    else {
+      val fileMap = media.filter(_.files.nonEmpty)
+        .foldLeft(Map.empty[MediumID, List[FileData]]) { (map, medium) =>
+          map + (medium.id -> medium.files)
+        }
+      Some(MediaScanResult(root, fileMap))
+    }
+}
+
+/**
+  * A specialized flow stage used during scanning of media files that
+  * aggregates the files encountered to the media they belong to.
+  *
+  * This stage assumes that the file structure to be processed is traversed in
+  * DFS order. It determines which file belongs to which medium and passes
+  * [[MediaScanResult]] objects downstream as soon as a medium has been
+  * processed completely. This makes it possible to pass data to the union
+  * archive while the scan operation is ongoing.
+  *
+  * Note that if no files are encountered, the stage yields a single
+  * ''MediaScanResult'' object with no content. That way there is always some
+  * feedback about a scan operation.
+  *
+  * @param root        the root directory that is currently scanned
+  * @param archiveName the name of the archive (for generating medium IDs)
+  * @param converter   a function to convert a path to a ''FileData'' object
+  */
+private class MediumAggregateStage(val root: Path, val archiveName: String,
+                                   val converter: FileDataFactory =
+                                   MediumAggregateStage.toFileData)
+  extends GraphStage[FlowShape[Path, MediaScanResult]] {
+  val in = Inlet[Path]("MediumAggregateStage.in")
+  val out = Outlet[MediaScanResult]("MediumAggregateStage.out")
+
+  override val shape = FlowShape.of(in, out)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+
+      import MediumAggregateStage._
+
+      var state: MediaState = List(MediumAggregateData(root, MediumID(root.toString, None,
+        archiveName), Nil))
+
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          val file = grab(in)
+          val func = nextState(file, root, archiveName, converter)
+          val (next, outcome) = func(state)
+          state = next
+          outcome match {
+            case Some(result) =>
+              push(out, result)
+            case None =>
+              pull(in)
+          }
+        }
+
+        override def onUpstreamFinish(): Unit = {
+          createScanResultForCompleteMedia(state, root) foreach (emit(out, _))
+          complete(out)
+        }
+      })
+
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
+}
---
+++ mediaArchive/archive/src/test/scala/de/oliver_heger/linedj/archive/media/MediumAggregateStageSpec.scala
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.media
+
+import java.nio.file.{Path, Paths}
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import akka.testkit.TestKit
+import de.oliver_heger.linedj.FileTestHelper
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object MediumAggregateStageSpec {
+  /** The name of the root directory in the test directory structure. */
+  private val RootName = "archiveRoot"
+
+  /** The root directory of the test archive. */
+  private val RootPath = Paths get RootName
+
+  /** The name of the media archive. */
+  private val ArchiveName = "MyTestArchive"
+
+  /** Prefix for medium directories. */
+  private val MediumDirPrefix = "medium"
+
+  /** A list with the names of sub directories for path generation. */
+  private val SubDirectories = List("sub1", "Sub2", "sub3")
+
+  /** The name of a settings (medium description) file. */
+  private val SettingsName = "playlist.settings"
+
+  /**
+    * Generates a ''Path'' for a directory in the test archive structure.
+    *
+    * @param mediumIdx the index of the medium
+    * @param subLevel  the sub directory level
+    * @return the resulting path
+    */
+  private def generateDirPath(mediumIdx: Int, subLevel: Int): Path =
+    Paths.get(RootName, (MediumDirPrefix + mediumIdx) :: (SubDirectories take subLevel): _*)
+
+  /**
+    * Generates a ''Path'' for a test file in the directory structure to be
+    * passed. This method generates the path for a file on the given medium,
+    * on a specific sub directory level, and with the given name.
+    *
+    * @param mediumIdx the index of the medium
+    * @param subLevel  the sub directory level
+    * @param name      the name of the file
+    * @return the resulting path
+    */
+  private def generateFilePath(mediumIdx: Int, subLevel: Int, name: String): Path =
+    generateDirPath(mediumIdx, subLevel) resolve name
+
+  /**
+    * Generates a ''FileData'' object for the specified path.
+    *
+    * @param path the path
+    * @return the new ''FileData'' object
+    */
+  private def createFileData(path: Path): FileData =
+    FileData(path.toString, path.toString.length)
+
+  /**
+    * Generates a ''Path'' representing a media file in the test directory
+    * structure.
+    *
+    * @param mediumIdx the index of the medium
+    * @param subLevel  the sub directory level
+    * @param songIdx   the index of the media file
+    * @return the resulting path
+    */
+  private def generateMediaFilePath(mediumIdx: Int, subLevel: Int, songIdx: Int): Path =
+    generateFilePath(mediumIdx, subLevel, s"song$songIdx.mp3")
+
+  /**
+    * Generates a ''Path'' representing a medium description file in the test
+    * directory structure.
+    *
+    * @param mediumIdx the index of the medium
+    * @param subLevel  the sub directory level
+    * @return the resulting path
+    */
+  private def generateSettingsPath(mediumIdx: Int, subLevel: Int = 0): Path =
+    generateFilePath(mediumIdx, subLevel, SettingsName)
+
+  /**
+    * Generates the ID of a medium based on the given path of the medium
+    * description file.
+    *
+    * @param settingsPath the path to the settings file
+    * @return the medium ID
+    */
+  private def generateMediumID(settingsPath: Path): MediumID =
+    MediumID.fromDescriptionPath(settingsPath, ArchiveName)
+
+  /**
+    * Generates a ''MediaScanResult'' object that contains the paths of the
+    * given medium.
+    *
+    * @param mid   the medium ID
+    * @param paths the list of paths
+    * @return the scan result
+    */
+  private def generateScanResult(mid: MediumID, paths: Path*): MediaScanResult = {
+    val files = paths.map(createFileData).toList
+    MediaScanResult(RootPath, Map(mid -> files))
+  }
+
+  /**
+    * Combines the given results to a combined media scan result containing the
+    * file information of both source results.
+    *
+    * @param sr1 scan result 1
+    * @param sr2 scan result 2
+    * @return the combined scan result
+    */
+  private def combineScanResults(sr1: MediaScanResult, sr2: MediaScanResult): MediaScanResult =
+    sr1.copy(mediaFiles = sr1.mediaFiles ++ sr2.mediaFiles)
+}
+
+/**
+  * Test class for ''MediumAggregateStage''.
+  */
+class MediumAggregateStageSpec(testSystem: ActorSystem) extends TestKit(testSystem) with
+  FlatSpecLike with BeforeAndAfterAll with Matchers with FileTestHelper {
+  def this() = this(ActorSystem("MediumAggregateStageSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    tearDownTestFile()
+  }
+
+  import MediumAggregateStageSpec._
+
+  /**
+    * Checks whether a scan result contains the expected data. The order of
+    * files is irrelevant; therefore, a special comparision function has to be
+    * used.
+    *
+    * @param expected the expected scan result
+    * @param actual   the actual scan result
+    */
+  private def checkScanResult(expected: MediaScanResult, actual: MediaScanResult): Unit = {
+    actual.root should be(expected.root)
+    actual.mediaFiles.keySet should contain theSameElementsAs expected.mediaFiles.keySet
+    expected.mediaFiles.keys foreach { mid =>
+      actual.mediaFiles(mid) should contain theSameElementsAs expected.mediaFiles(mid)
+    }
+  }
+
+  /**
+    * Runs a stream with the stage under test using the given files as input
+    * and returns the result passed to the sink.
+    *
+    * @param files the files to be processed
+    * @return the result from the sink
+    */
+  private def runStream(files: List[Path]): List[MediaScanResult] = {
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+    val source = Source(files)
+    val stage = new MediumAggregateStage(RootPath, ArchiveName, createFileData)
+    val sink = Sink.fold[List[MediaScanResult], MediaScanResult](List.empty)((lst, res) =>
+      res :: lst)
+    val futStream = source.via(stage).runWith(sink)
+    Await.result(futStream, 5.seconds).reverse
+  }
+
+  "A MediumAggregateStage" should "handle an empty directory structure" in {
+    val result = runStream(List.empty)
+    result should be(List(MediaScanResult(RootPath, Map.empty)))
+  }
+
+  it should "use a correct default file data converter function" in {
+    val path = createDataFile()
+    val stage = new MediumAggregateStage(RootPath, ArchiveName)
+
+    stage.converter(path) should be(FileData(path.toString, FileTestHelper.TestData.length))
+  }
+
+  it should "aggregate the files of a single medium" in {
+    val settingsPath = generateSettingsPath(1)
+    val song1 = generateMediaFilePath(1, 1, 1)
+    val song2 = generateMediaFilePath(1, 1, 2)
+    val song3 = generateMediaFilePath(1, 2, 3)
+    val mid = generateMediumID(settingsPath)
+    val expScanResult = generateScanResult(mid, song1, song2, song3)
+
+    val result = runStream(List(settingsPath, song1, song2, song3))
+    result should have size 1
+    checkScanResult(expScanResult, result.head)
+  }
+
+  it should "aggregate the files of multiple media" in {
+    val settings1 = generateSettingsPath(1)
+    val settings2 = generateSettingsPath(2)
+    val mid1 = generateMediumID(settings1)
+    val mid2 = generateMediumID(settings2)
+    val song1 = generateMediaFilePath(1, 1, 1)
+    val song2 = generateMediaFilePath(1, 1, 2)
+    val song3 = generateMediaFilePath(2, 1, 3)
+    val song4 = generateMediaFilePath(2, 2, 4)
+    val song5 = generateMediaFilePath(2, 3, 5)
+
+    val result = runStream(List(settings1, song1, song2, settings2, song3, song4, song5))
+    result should have size 2
+    checkScanResult(generateScanResult(mid1, song1, song2), result.head)
+    checkScanResult(generateScanResult(mid2, song3, song4, song5), result(1))
+  }
+
+  it should "deal with nested media" in {
+    val settings1 = generateSettingsPath(1)
+    val settings2 = generateSettingsPath(1, subLevel = 1)
+    val settings3 = generateSettingsPath(2)
+    val mid1 = generateMediumID(settings1)
+    val mid2 = generateMediumID(settings2)
+    val mid3 = generateMediumID(settings3)
+    val song1 = generateMediaFilePath(1, 1, 1)
+    val song2 = generateMediaFilePath(1, 2, 2)
+    val song3 = generateMediaFilePath(1, 1, 3)
+    val song4 = generateMediaFilePath(2, 1, 4)
+
+    val result = runStream(List(settings1, song1, settings2, song2, song3, settings3, song4))
+    result should have size 2
+    val expRes1 = combineScanResults(generateScanResult(mid2, song2),
+      generateScanResult(mid1, song1, song3))
+    checkScanResult(expRes1, result.head)
+    checkScanResult(generateScanResult(mid3, song4), result(1))
+  }
+
+  it should "support the undefined medium" in {
+    val settings = generateSettingsPath(1)
+    val mid = generateMediumID(settings)
+    val song1 = RootPath resolve "TopLevelSong.mp3"
+    val song2 = generateMediaFilePath(1, 1, 2)
+    val song3 = generateMediaFilePath(2, 0, 3)
+    val song4 = generateMediaFilePath(2, 0, 4)
+    val song5 = generateMediaFilePath(3, 1, 5)
+
+    val result = runStream(List(song1, settings, song2, song3, song4, song5))
+    result should have size 2
+    checkScanResult(generateScanResult(mid, song2), result.head)
+    val midUndef = MediumID(RootPath.toString, None, ArchiveName)
+    checkScanResult(generateScanResult(midUndef, song1, song3, song4, song5), result(1))
+  }
+}
