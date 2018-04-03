daa2f6e4b9b1e512ea63045c5e60f8f1786cf674
oheger <oliver.heger@oliver-heger.de>
2018-04-03 17:24:11 +0200

    Added ScanResultEnhancer class.
    This is a service that produces an enhanced scan result from a basic
    one. It takes care of calculating a checksum for each media affected
    and constructing mappings from URIs to the corresponding file data
    objects.

---
+++ mediaArchive/archive/src/main/scala/de/oliver_heger/linedj/archive/media/ScanResultEnhancer.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.media
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Path, Paths}
+import java.security.MessageDigest
+
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+import de.oliver_heger.linedj.shared.archive.union.MediaFileUriHandler
+import javax.xml.bind.DatatypeConverter
+
+/**
+  * An object that calculates the information required to transform a
+  * [[MediaScanResult]] to an [[EnhancedMediaScanResult]].
+  *
+  * This object offers a function to determine ID values and URI mappings for
+  * all the media contained in a ''MediaScanResult'' object.
+  */
+private object ScanResultEnhancer {
+  /** The hash algorithm for checksum generation. */
+  private val HashAlgorithm = "SHA-1"
+
+  /**
+    * Transforms the given ''MediaScanResult'' into an
+    * ''EnhancedMediaScanResult''.
+    *
+    * @param result the source result
+    * @return the enhanced result
+    */
+  def enhance(result: MediaScanResult): EnhancedMediaScanResult = {
+    val init = (Map.empty[MediumID, String], Map.empty[String, FileData])
+    val (checksumMapping, uriMapping) = result.mediaFiles.foldLeft(init) { (ms, e) =>
+      calculateMappings(result, ms._1, ms._2, e._1, e._2)
+    }
+    EnhancedMediaScanResult(result, checksumMapping, uriMapping)
+  }
+
+  /**
+    * Calculates the mappings for a specific medium ID and aggregates them for
+    * all media.
+    *
+    * @param result          the scan result
+    * @param checksumMapping the aggregated checksum mapping
+    * @param uriMapping      the aggregated URI mapping
+    * @param mid             the current medium ID
+    * @param files           the files for this medium
+    * @return the updated mappings
+    */
+  private def calculateMappings(result: MediaScanResult,
+                                checksumMapping: Map[MediumID, String],
+                                uriMapping: Map[String, FileData],
+                                mid: MediumID,
+                                files: List[FileData]):
+  (Map[MediumID, String], Map[String, FileData]) = {
+    val fileUris = generateUris(result.root, files)
+    val relativeFileUris = mid.mediumDescriptionPath match {
+      case Some(desc) =>
+        val mediumPath = (Paths get desc).getParent
+        generateUris(mediumPath, files)
+      case None => fileUris
+    }
+    val checksum = calculateChecksum(relativeFileUris)
+    (checksumMapping + (mid -> checksum), uriMapping ++ fileUris.toMap)
+  }
+
+  /**
+    * Calculates the checksum for a medium based on the URIs for the files it
+    * contains and the data objects representing these files.
+    *
+    * @param fileUriData data about the files and their relative URIs
+    * @return the resulting checksum
+    */
+  private def calculateChecksum(fileUriData: Seq[(String, FileData)]): String = {
+    val digest = MessageDigest getInstance HashAlgorithm
+    fileUriData map { t => t._1 + ':' + t._2.size } sortWith (_ < _) foreach (u =>
+      digest.update(u.getBytes(StandardCharsets.UTF_8)))
+    DatatypeConverter.printHexBinary(digest.digest())
+  }
+
+  /**
+    * Transforms a sequence of ''FileData'' objects to their URIs relative to
+    * the given root directory. Result is a sequence of tuples with the URI and
+    * the original ''FileData'' object.
+    *
+    * @param root  the root path
+    * @param files the files to be transformed
+    * @return the sequence with URIs and original data objects
+    */
+  private def generateUris(root: Path, files: Seq[FileData]): Seq[(String, FileData)] =
+    files.map(f => MediaFileUriHandler.generateMediaFileUri(root, Paths get f.path))
+      .zip(files)
+}
---
+++ mediaArchive/archive/src/test/scala/de/oliver_heger/linedj/archive/media/ScanResultEnhancerSpec.scala
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.media
+
+import java.nio.file.{Path, Paths}
+
+import de.oliver_heger.linedj.FileTestHelper
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.shared.archive.media.MediumID
+import de.oliver_heger.linedj.shared.archive.union.MediaFileUriHandler
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+import scala.io.Source
+
+object ScanResultEnhancerSpec {
+  /** Constant for a medium directory. */
+  private val Medium1 = "someMedium"
+
+  /** Constant for another test medium directory. */
+  private val Medium2 = "anotherMedium"
+
+  /** The name of the file with the test medium content. */
+  private val MediumFile = "test.plist"
+}
+
+/**
+  * Test class for ''ScanResultEnhancer''.
+  */
+class ScanResultEnhancerSpec extends FlatSpec with Matchers with BeforeAndAfter with
+  FileTestHelper {
+
+  import ScanResultEnhancerSpec._
+
+  /**
+    * Generates a medium ID based on the given directory name. The directory is
+    * resolved based on the test root directory.
+    *
+    * @param path the relative root path of the medium
+    * @return the resulting medium ID
+    */
+  private def createMediumID(path: String): MediumID =
+    createMediumIDForPath(testDirectory resolve path)
+
+  /**
+    * Generates a medium ID for the specified root path.
+    *
+    * @param mediumRoot the root path for the medium
+    * @return the resulting medium ID
+    */
+  private def createMediumIDForPath(mediumRoot: Path): MediumID =
+    MediumID(mediumRoot.toString,
+      Some(mediumRoot.resolve("playlist.settings").toString))
+
+  /**
+    * Converts a string read from the data file to a path.
+    *
+    * @param s        the string
+    * @param rootPath the root path to resolve relative paths
+    * @return the resulting path
+    */
+  private def toPath(s: String, rootPath: Path): String =
+    rootPath.resolve(Paths.get(s.trim())).toString
+
+  /**
+    * Creates a sequence with paths representing the content of a test medium.
+    *
+    * @param mid the test medium
+    * @return the sequence with paths
+    */
+  private def createContentList(mid: MediumID): List[FileData] = {
+    val rootPath = Paths get mid.mediumURI
+    val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(MediumFile))
+    (source.getLines() map (s => FileData(toPath(s, rootPath), s.length))).toList
+  }
+
+  /**
+    * Returns a map with file information for the first test medium.
+    *
+    * @return the map with file information for medium 1
+    */
+  private def createMediaFileMap(): Map[MediumID, List[FileData]] = {
+    val mid = createMediumID(Medium1)
+    Map(mid -> createContentList(mid))
+  }
+
+  /**
+    * Creates a scan result object with the specified media files.
+    *
+    * @param media the map with media files
+    * @return the scan result
+    */
+  private def createScanResult(media: Map[MediumID, List[FileData]] = createMediaFileMap()):
+  MediaScanResult = MediaScanResult(testDirectory, media)
+
+  "A ScanResultEnhancer" should "calculate a correct checksum" in {
+    val mid = createMediumID(Medium1)
+    val esr = ScanResultEnhancer enhance createScanResult()
+
+    val checksum = esr.checksumMapping(mid)
+    checksum.length should be > 8
+    val validChars = (('A' to 'F') ++ ('0' to '9')).toSet
+    checksum.forall(validChars.contains) shouldBe true
+  }
+
+  it should "generate the same checksum for the same content" in {
+    val mid = createMediumID(Medium1)
+    val esr1 = ScanResultEnhancer enhance createScanResult()
+    val esr2 = ScanResultEnhancer enhance createScanResult()
+
+    val checksum1 = esr1.checksumMapping(mid)
+    val checksum2 = esr2.checksumMapping(mid)
+    checksum1 should be(checksum2)
+  }
+
+  it should "generate the same checksum independent on absolute paths" in {
+    val mid1 = createMediumID(Medium1)
+    val mid2 = createMediumID(Medium2)
+    val media = createMediaFileMap() + (mid2 -> createContentList(mid2))
+    val esr = ScanResultEnhancer enhance createScanResult(media)
+
+    esr.checksumMapping(mid1) should be(esr.checksumMapping(mid2))
+  }
+
+  it should "generate a correct URI mapping" in {
+    val mid2 = createMediumID(Medium2)
+    val contentList = createContentList(mid2)
+    val media = createMediaFileMap() + (mid2 -> contentList)
+    val scanResult = createScanResult(media)
+    val esr = ScanResultEnhancer enhance scanResult
+
+    esr.fileUriMapping should have size 2 * contentList.size
+    esr.fileUriMapping.values foreach { f =>
+      val uri = MediaFileUriHandler.generateMediaFileUri(testDirectory,
+        Paths get f.path)
+      esr.fileUriMapping(uri) should be(f)
+    }
+  }
+
+  it should "generate a checksum for a medium without a description file" in {
+    val mid1 = createMediumID(Medium1)
+    val mid2 = createMediumIDForPath(testDirectory).copy(mediumDescriptionPath = None)
+    val media = createMediaFileMap() + (mid2 -> createContentList(mid2))
+    val esr = ScanResultEnhancer enhance createScanResult(media)
+
+    esr.checksumMapping(mid1) should be(esr.checksumMapping(mid2))
+  }
+
+  it should "generate the same checksum independent on the order of media files" in {
+    val mid1 = createMediumID(Medium1)
+    val mid2 = createMediumID(Medium2)
+    val media = createMediaFileMap() + (mid2 -> createContentList(mid2).reverse)
+    val esr = ScanResultEnhancer enhance createScanResult(media)
+
+    esr.checksumMapping(mid1) should be(esr.checksumMapping(mid2))
+  }
+
+  it should "use file sizes for checksum generation" in {
+    val mid1 = createMediumID(Medium1)
+    val mid2 = createMediumIDForPath(testDirectory).copy(mediumDescriptionPath = None)
+    val contentList = createContentList(mid2)
+    val modifiedContent = contentList.head.copy(
+      size = contentList.head.size + 1) :: contentList.tail
+    val media = createMediaFileMap() + (mid2 -> modifiedContent)
+    val esr = ScanResultEnhancer enhance createScanResult(media)
+
+    esr.checksumMapping(mid1) should not be esr.checksumMapping(mid2)
+  }
+
+  it should "pass the scan result to the enhanced result" in {
+    val scanResult = createScanResult()
+
+    val esr = ScanResultEnhancer enhance scanResult
+    esr.scanResult should be(scanResult)
+  }
+}
