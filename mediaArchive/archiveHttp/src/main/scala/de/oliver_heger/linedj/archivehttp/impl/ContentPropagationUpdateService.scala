3f935a31442f35cf67740bb8be6ab96a9d8c161f
oheger <oliver.heger@oliver-heger.de>
2018-06-18 20:46:28 +0200

    Added ContentPropagationUpdateService.
    This service is used when propagating media data from an HTTP archive
    to the union archive.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/ContentPropagationUpdateService.scala
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import akka.actor.ActorRef
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.shared.archive.union.{AddMedia, ArchiveComponentRemoved, MediaContribution}
+import scalaz.State
+import scalaz.State._
+
+/**
+  * Data class that represents a sequence of messages to an actor to be sent.
+  *
+  * With instances of this class the update service indicates the messages to
+  * be sent when a state transition occurs.
+  *
+  * @param target   the target to receive the messages
+  * @param messages the actual messages
+  */
+case class MessageData(target: ActorRef, messages: Iterable[Any])
+
+/**
+  * Data class collecting the actors involved in a result propagation
+  * operation.
+  *
+  * These actor references are required by the propagation update service to
+  * generate the messages to be sent.
+  *
+  * @param mediaManager the union media manager actor
+  * @param metaManager  the union meta data manager actor
+  * @param client       the client actor to ACK when results are processed
+  */
+case class PropagationActors(mediaManager: ActorRef, metaManager: ActorRef, client: ActorRef)
+
+/**
+  * A message class indicating that the data of a medium has been propagated to
+  * the union archive.
+  *
+  * @param seqNo the sequence number of the current operation
+  */
+case class MediumPropagated(seqNo: Int)
+
+/**
+  * A class representing the propagation state to the union archive.
+  *
+  * The state contains the messages that are to be sent to the actors of the
+  * union archive. If the ACK of a remove request is pending, messages cannot
+  * be sent directly, but have to be buffered.
+  *
+  * @param messages        a list with messages to be sent
+  * @param pendingMessages messages that are pending to be sent
+  * @param removeAck       flag whether a remove ACK has been received
+  */
+case class ContentPropagationState(messages: List[MessageData],
+                                   pendingMessages: List[MessageData],
+                                   removeAck: Boolean)
+
+/**
+  * A service that manages the state of the propagation of media data to the
+  * union archive during content processing of an HTTP archive.
+  *
+  * While reading the content document and the referenced documents of an HTTP
+  * archive, the data extracted must be eventually passed to the union archive.
+  * This is not too difficult for the media data, but becomes more complex when
+  * another scan starts and the content of the current HTTP archive has to be
+  * removed first from the union archive.
+  *
+  * This service takes care about this by providing corresponding update
+  * functions for a propagation state object.
+  */
+trait ContentPropagationUpdateService {
+  /** Type for a state update. */
+  type StateUpdate[T] = State[ContentPropagationState, T]
+
+  /**
+    * Updates the state for another medium that has been processed. Messages to
+    * be sent for this medium to the union archive actors are generated. If the
+    * boolean remove parameter is '''true''', a request to remove the content
+    * of the current HTTP archive from the union archive is generated as well.
+    *
+    * @param result     the medium processing result
+    * @param actors     involved actors
+    * @param archiveUri URI for the current HTTP archive
+    * @param remove     flag whether archive content is to be removed
+    * @param seqNo      the sequence number of the current operation
+    * @return the updated state
+    */
+  def mediumProcessed(result: MediumProcessingResult, actors: PropagationActors,
+                      archiveUri: String, remove: Boolean, seqNo: Int): StateUpdate[Unit]
+
+  /**
+    * Updates the state when an ACK for a remove request arrives. Then pending
+    * messages with results can be actually sent.
+    *
+    * @return the updated state
+    */
+  def removalConfirmed(): StateUpdate[Unit]
+
+  /**
+    * Updates the state regarding messages to be sent. With this function the
+    * messages that are ready to be sent can be fetched. The state is updated
+    * as if the messages were already sent to their receivers.
+    *
+    * @return the updated state and the messages to be sent
+    */
+  def messagesToSend(): StateUpdate[Iterable[MessageData]]
+
+  /**
+    * Handles a notification about a processed medium by updating the state
+    * accordingly and returning a sequence with messages that have to be sent
+    * now.
+    *
+    * @param result     the medium processing result
+    * @param actors     involved actors
+    * @param archiveUri URI for the current HTTP archive
+    * @param remove     flag whether archive content is to be removed
+    * @param seqNo      the sequence number of the current operation
+    * @return the updated state and messages to be sent
+    */
+  def handleMediumProcessed(result: MediumProcessingResult, actors: PropagationActors,
+                            archiveUri: String, remove: Boolean, seqNo: Int):
+  StateUpdate[Iterable[MessageData]] = for {
+    _ <- mediumProcessed(result, actors, archiveUri, remove, seqNo)
+    msg <- messagesToSend()
+  } yield msg
+
+  /**
+    * Handles an incoming confirmation of a removal request by updating the
+    * state accordingly and returning a sequence of messages that have to be
+    * sent now.
+    *
+    * @return the updated state and messages to be sent
+    */
+  def handleRemovalConfirmed(): StateUpdate[Iterable[MessageData]] = for {
+    _ <- removalConfirmed()
+    msg <- messagesToSend()
+  } yield msg
+}
+
+/**
+  * The implementation of the ''ContentPropagationUpdateService''.
+  */
+object ContentPropagationUpdateServiceImpl extends ContentPropagationUpdateService {
+  /** Constant for the initial propagation state. */
+  val InitialState: ContentPropagationState =
+    ContentPropagationState(messages = Nil, pendingMessages = Nil, removeAck = true)
+
+  override def mediumProcessed(result: MediumProcessingResult, actors: PropagationActors,
+                               archiveUri: String, remove: Boolean, seqNo: Int):
+  StateUpdate[Unit] = modify { s =>
+    if (remove)
+      ContentPropagationState(messages = List(MessageData(actors.mediaManager,
+        Seq(ArchiveComponentRemoved(archiveUri)))), removeAck = false,
+        pendingMessages = createMessagesForMedium(result, actors, archiveUri, seqNo))
+    else {
+      val orgMessages = if (s.removeAck) s.messages else s.pendingMessages
+      val updateMessages = createMessagesForMedium(result, actors, archiveUri,
+        seqNo) ::: orgMessages
+      val (newMessages, newPending) =
+        if (s.removeAck) (updateMessages, s.pendingMessages)
+        else (s.messages, updateMessages)
+      s.copy(messages = newMessages, pendingMessages = newPending)
+    }
+  }
+
+  override def removalConfirmed(): StateUpdate[Unit] = modify { s =>
+    if (s.removeAck) s
+    else ContentPropagationState(messages = s.pendingMessages, pendingMessages = Nil,
+      removeAck = true)
+  }
+
+  override def messagesToSend(): StateUpdate[Iterable[MessageData]] = State { s =>
+    (s.copy(messages = Nil), s.messages)
+  }
+
+  /**
+    * Creates a list with ''MessageData'' objects representing the messages to
+    * be sent for the result of a processed medium.
+    *
+    * @param result     the ''MediumProcessingResult''
+    * @param actors     actors involved in propagation
+    * @param archiveUri the URI of the archive
+    * @param seqNo      the sequence number of the current operation
+    * @return a list with the messages to propagate this result
+    */
+  private def createMessagesForMedium(result: MediumProcessingResult, actors: PropagationActors,
+                                      archiveUri: String, seqNo: Int): List[MessageData] =
+    List(createAddMediaMessage(result, actors, archiveUri),
+      createMetaDataMessages(result, actors), createPropagatedMessage(seqNo, actors))
+
+  /**
+    * Creates the message to the media manager actor that adds a medium
+    * description to the union archive.
+    *
+    * @param result     the current result object
+    * @param actors     actors involved in propagation
+    * @param archiveUri the URI of the archive
+    * @return the ''MessageData'' object with the message
+    */
+  private def createAddMediaMessage(result: MediumProcessingResult, actors: PropagationActors,
+                                    archiveUri: String): MessageData =
+    MessageData(actors.mediaManager,
+      Seq(AddMedia(Map(result.mediumInfo.mediumID -> result.mediumInfo), archiveUri,
+        Some(actors.client))))
+
+  /**
+    * Creates a ''MessageData'' with the messages to be sent to the meta data
+    * manager to add the content of a medium to the union archive.
+    *
+    * @param result the result object for the medium
+    * @param actors actors involved in propagation
+    * @return the ''MessageData'' object
+    */
+  private def createMetaDataMessages(result: MediumProcessingResult, actors: PropagationActors):
+  MessageData = {
+    val files = result.metaData map (m => FileData(m.path, m.metaData.size))
+    val contribution = MediaContribution(Map(result.mediumInfo.mediumID -> files))
+    val messages = contribution :: result.metaData.toList
+    MessageData(actors.metaManager, messages)
+  }
+
+  /**
+    * Creates a ''MessageData'' object with an ACK message indicating that the
+    * results of a medium have been propagated to the union archive.
+    *
+    * @param seqNo  the sequence number of the current operation
+    * @param actors actors involved in propagation
+    * @return the ''MessageData'' object
+    */
+  private def createPropagatedMessage(seqNo: Int, actors: PropagationActors): MessageData =
+    MessageData(actors.client, Seq(MediumPropagated(seqNo)))
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/ContentPropagationUpdateServiceSpec.scala
@@ -0,0 +1,379 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestKit, TestProbe}
+import de.oliver_heger.linedj.io.FileData
+import de.oliver_heger.linedj.shared.archive.media.{MediumID, MediumInfo}
+import de.oliver_heger.linedj.shared.archive.metadata.MediaMetaData
+import de.oliver_heger.linedj.shared.archive.union.{AddMedia, ArchiveComponentRemoved, MediaContribution, MetaDataProcessingSuccess}
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object ContentPropagationUpdateServiceSpec {
+  /** The name of the archive. */
+  private val ArchiveName = "TestArchive"
+
+  /** Test sequence number. */
+  private val SeqNo = 20180617
+
+  /**
+    * Creates an ID for a test medium with the given index.
+    *
+    * @param idx the index
+    * @return the ID for the test medium with this index
+    */
+  private def mediumID(idx: Int): MediumID =
+    MediumID("testMedium" + idx, Some(s"medium$idx.settings"))
+
+  /**
+    * Generates a ''MediumInfo'' object for a test medium.
+    *
+    * @param idx the index of the test medium
+    * @return the info object for this test medium
+    */
+  private def mediumInfo(idx: Int): MediumInfo =
+    MediumInfo(name = "TestMedium" + idx, description = "", orderMode = "", checksum = "",
+      orderParams = "", mediumID = mediumID(idx))
+
+  /**
+    * Generates the path for the media files contained on the given test
+    * medium.
+    *
+    * @param idx the index of the test medium
+    * @return the path for files on this test medium
+    */
+  private def mediumPath(idx: Int): String = s"mediumPath$idx/"
+
+  /**
+    * Generates the path of a file contained on the given test medium.
+    *
+    * @param medIdx  the index of the test medium
+    * @param fileIdx the index of the test file
+    * @return the resulting path to this medium file
+    */
+  private def mediumFilePath(medIdx: Int, fileIdx: Int): String =
+    s"${mediumPath(medIdx)}file$fileIdx.mp3"
+
+  /**
+    * Calculates the size of a file on a test medium.
+    *
+    * @param medIdx  the index of the test medium
+    * @param fileIdx the index of the test file
+    * @return the size of this test file
+    */
+  private def fileSize(medIdx: Int, fileIdx: Int): Int =
+    medIdx * 100 + fileIdx * 10
+
+  /**
+    * Generates the media files for the test medium with the given index.
+    *
+    * @param idx the index
+    * @return the map with the content of this medium
+    */
+  private def mediumContent(idx: Int): Map[MediumID, Iterable[FileData]] = {
+    val files = (1 to idx).map(i => FileData(mediumFilePath(idx, i), fileSize(idx, i)))
+    Map(mediumID(idx) -> files)
+  }
+
+  /**
+    * Generates a meta data processing result object for the specified test
+    * file.
+    *
+    * @param medIdx  the medium index
+    * @param fileIdx the file index
+    * @return the test meta data processing result
+    */
+  private def metaDataProcessingResult(medIdx: Int, fileIdx: Int): MetaDataProcessingSuccess = {
+    val filePath = mediumFilePath(medIdx, fileIdx)
+    MetaDataProcessingSuccess(path = filePath, mediumID = mediumID(medIdx),
+      uri = "song://" + filePath, metaData = MediaMetaData(title = Some(filePath),
+        size = fileSize(medIdx, fileIdx)))
+  }
+
+  /**
+    * Generates the sequence of meta data processing result objects for the
+    * specified test medium.
+    *
+    * @param idx the medium index
+    * @return the meta data results for this medium
+    */
+  private def metaDataProcessingResults(idx: Int): Iterable[MetaDataProcessingSuccess] =
+    (1 to idx) map (metaDataProcessingResult(idx, _))
+
+  /**
+    * Generates a processing result for a test medium.
+    *
+    * @param idx the index of the test medium
+    * @return the processing result for this medium
+    */
+  private def mediumResult(idx: Int): MediumProcessingResult = {
+    val metaData = metaDataProcessingResults(idx)
+    MediumProcessingResult(mediumInfo(idx), metaData, SeqNo)
+  }
+
+  /**
+    * Generates a media contribution message for the given test medium.
+    *
+    * @param idx the index of the test medium
+    * @return the ''MediaContribution''
+    */
+  private def mediaContribution(idx: Int): MediaContribution =
+    MediaContribution(mediumContent(idx))
+
+  /**
+    * Generates an ''AddMedia'' message for the specified test medium.
+    *
+    * @param idx    the index of the test medium
+    * @param actors actors involved in propagation
+    * @return the ''AddMedia'' message
+    */
+  private def addMedia(idx: Int, actors: PropagationActors): AddMedia = {
+    val info = mediumInfo(idx)
+    AddMedia(Map(info.mediumID -> info), ArchiveName, Some(actors.client))
+  }
+
+  /**
+    * Generates a ''MessageData'' object with the messages to be sent for the
+    * media manager actor for the given test medium.
+    *
+    * @param idx    the index of the test medium
+    * @param actors actors involved in propagation
+    * @return the ''MessageData'' object
+    */
+  private def mediaManagerMessages(idx: Int, actors: PropagationActors): MessageData =
+    MessageData(actors.mediaManager, List(addMedia(idx, actors)))
+
+  /**
+    * Generates a ''MessageData'' object with the messages to be sent for the
+    * meta data manager actor for the given test medium.
+    *
+    * @param idx    the index of the test medium
+    * @param actors actors involved in propagation
+    * @return the ''MessageData'' object
+    */
+  private def metaManagerMessages(idx: Int, actors: PropagationActors): MessageData = {
+    val results = metaDataProcessingResults(idx).toList
+    val messages = mediaContribution(idx) :: results
+    MessageData(actors.metaManager, messages)
+  }
+
+  /**
+    * Generates a ''MessageData'' object with the ACK message for propagation
+    * of the results of a medium.
+    *
+    * @param actors actors involved in propagation
+    * @return the ''MessageData'' object
+    */
+  private def ackMessage(actors: PropagationActors): MessageData =
+    MessageData(actors.client, Seq(MediumPropagated(SeqNo)))
+
+  /**
+    * Generates a ''MessageData'' object with a request to remove the content
+    * of the test archive from the union archive.
+    *
+    * @param actors actors involved in propagation
+    * @return the ''MessageData'' object
+    */
+  private def removeMessage(actors: PropagationActors): MessageData =
+    MessageData(actors.mediaManager, Seq(ArchiveComponentRemoved(ArchiveName)))
+
+  /**
+    * Convenience method to execute a ''State'' object to produce the updated
+    * state and an additional result.
+    *
+    * @param s        the ''State''
+    * @param oldState the original state
+    * @tparam A the type of the additional result
+    * @return a tuple with the updated state and the additional result
+    */
+  private def updateState[A](s: ContentPropagationUpdateServiceImpl.StateUpdate[A],
+                             oldState: ContentPropagationState =
+                             ContentPropagationUpdateServiceImpl.InitialState):
+  (ContentPropagationState, A) = s(oldState)
+
+  /**
+    * Convenience method to modify a ''State'' object. This means, the state is
+    * only manipulated without producing an additional result.
+    *
+    * @param s        the ''State''
+    * @param oldState the original state
+    * @return the updated state
+    */
+  private def modifyState(s: ContentPropagationUpdateServiceImpl.StateUpdate[Unit],
+                          oldState: ContentPropagationState =
+                          ContentPropagationUpdateServiceImpl.InitialState):
+  ContentPropagationState = {
+    val (next, _) = updateState(s, oldState)
+    next
+  }
+}
+
+/**
+  * Test class for ''ContentPropagationUpdateServiceImpl''.
+  */
+class ContentPropagationUpdateServiceSpec(testSystem: ActorSystem) extends TestKit(testSystem)
+  with FlatSpecLike with BeforeAndAfterAll with Matchers {
+  def this() = this(ActorSystem("ContentPropagationUpdateServiceSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  import ContentPropagationUpdateServiceSpec._
+
+  /**
+    * Creates an object with test actors involved in propagation.
+    *
+    * @return the ''PropagationActors'' object
+    */
+  private def createActors(): PropagationActors =
+    PropagationActors(TestProbe().ref, TestProbe().ref, TestProbe().ref)
+
+  "ContentPropagationUpdateServiceImpl" should "have a valid initial state" in {
+    val state = ContentPropagationUpdateServiceImpl.InitialState
+
+    state.messages shouldBe 'empty
+    state.pendingMessages shouldBe 'empty
+    state.removeAck shouldBe true
+  }
+
+  it should "process results if no removal ACK is pending" in {
+    val Idx = 1
+    val actors = createActors()
+    val expMessages = List(mediaManagerMessages(Idx, actors),
+      metaManagerMessages(Idx, actors), ackMessage(actors))
+
+    val next = modifyState(ContentPropagationUpdateServiceImpl
+      .mediumProcessed(mediumResult(Idx), actors, ArchiveName, remove = false, SeqNo))
+    next.pendingMessages shouldBe 'empty
+    next.removeAck shouldBe true
+    next.messages should contain theSameElementsInOrderAs expMessages
+  }
+
+  it should "append data for another medium to messages to be sent" in {
+    val actors = createActors()
+    val orgMessages = List(mediaManagerMessages(1, actors),
+      metaManagerMessages(1, actors), ackMessage(actors))
+    val newMessages = List(mediaManagerMessages(2, actors),
+      metaManagerMessages(2, actors), ackMessage(actors))
+    val expMessages = newMessages ::: orgMessages
+    val state = ContentPropagationUpdateServiceImpl.InitialState.copy(messages = orgMessages)
+
+    val next = modifyState(ContentPropagationUpdateServiceImpl
+      .mediumProcessed(mediumResult(2), actors, ArchiveName, remove = false, SeqNo), state)
+    next.messages should contain theSameElementsInOrderAs expMessages
+  }
+
+  it should "append data to pending messages if remove is not confirmed" in {
+    val actors = createActors()
+    val orgMessages = List(metaManagerMessages(2, actors))
+    val newMessages = List(mediaManagerMessages(2, actors),
+      metaManagerMessages(2, actors), ackMessage(actors))
+    val expMessages = newMessages ::: orgMessages
+    val state = ContentPropagationUpdateServiceImpl.InitialState.copy(removeAck = false,
+      pendingMessages = orgMessages)
+
+    val next = modifyState(ContentPropagationUpdateServiceImpl
+      .mediumProcessed(mediumResult(2), actors, ArchiveName, remove = false, SeqNo), state)
+    next.pendingMessages should contain theSameElementsInOrderAs expMessages
+    next.messages shouldBe 'empty
+  }
+
+  it should "evaluate the remove parameter" in {
+    val actors = createActors()
+    val orgMessages = List(mediaManagerMessages(1, actors),
+      metaManagerMessages(1, actors), ackMessage(actors))
+    val orgPending = List(mediaManagerMessages(2, actors),
+      metaManagerMessages(2, actors), ackMessage(actors))
+    val newPending = List(mediaManagerMessages(3, actors),
+      metaManagerMessages(3, actors), ackMessage(actors))
+    val state = ContentPropagationUpdateServiceImpl.InitialState.copy(messages = orgMessages,
+      pendingMessages = orgPending)
+
+    val next = modifyState(ContentPropagationUpdateServiceImpl
+      .mediumProcessed(mediumResult(3), actors, ArchiveName, remove = true, SeqNo), state)
+    next.removeAck shouldBe false
+    next.messages should contain only removeMessage(actors)
+    next.pendingMessages should contain theSameElementsInOrderAs newPending
+  }
+
+  it should "ignore a remove ACK if one already arrived" in {
+    val actors = createActors()
+    val state = ContentPropagationUpdateServiceImpl.InitialState
+      .copy(messages = List(metaManagerMessages(1, actors)))
+
+    val next = modifyState(ContentPropagationUpdateServiceImpl.removalConfirmed(), state)
+    next should be theSameInstanceAs state
+  }
+
+  it should "update the state for a remove ACK" in {
+    val actors = createActors()
+    val pending = List(metaManagerMessages(1, actors), mediaManagerMessages(1, actors),
+      ackMessage(actors))
+    val state = ContentPropagationState(messages = List(ackMessage(actors)),
+      pendingMessages = pending, removeAck = false)
+
+    val next = modifyState(ContentPropagationUpdateServiceImpl.removalConfirmed(), state)
+    next.removeAck shouldBe true
+    next.messages should be(pending)
+    next.pendingMessages shouldBe 'empty
+  }
+
+  it should "return the messages to be sent" in {
+    val actors = createActors()
+    val messages = List(metaManagerMessages(1, actors), mediaManagerMessages(1, actors),
+      ackMessage(actors))
+    val pending = List(metaManagerMessages(2, actors), mediaManagerMessages(2, actors),
+      ackMessage(actors))
+    val state = ContentPropagationState(messages = messages, pendingMessages = pending,
+      removeAck = false)
+
+    val (next, sendMsg) = updateState(ContentPropagationUpdateServiceImpl.messagesToSend(), state)
+    sendMsg should be(messages)
+    next should be(state.copy(messages = Nil))
+  }
+
+  it should "handle a notification about a processed medium" in {
+    val Idx = 1
+    val actors = createActors()
+    val expMessages = List(mediaManagerMessages(Idx, actors),
+      metaManagerMessages(Idx, actors), ackMessage(actors))
+
+    val (next, sendMsg) = updateState(ContentPropagationUpdateServiceImpl
+      .handleMediumProcessed(mediumResult(Idx), actors, ArchiveName, remove = false, SeqNo))
+    next.pendingMessages shouldBe 'empty
+    next.removeAck shouldBe true
+    next.messages shouldBe 'empty
+    sendMsg.toSeq should contain theSameElementsInOrderAs expMessages
+  }
+
+  it should "handle a removal confirmation" in {
+    val actors = createActors()
+    val pending = List(metaManagerMessages(1, actors), mediaManagerMessages(1, actors),
+      ackMessage(actors))
+    val state = ContentPropagationState(messages = Nil, pendingMessages = pending,
+      removeAck = false)
+
+    val (next, sendMsg) = updateState(ContentPropagationUpdateServiceImpl.handleRemovalConfirmed(),
+      state)
+    next.removeAck shouldBe true
+    next.messages shouldBe 'empty
+    next.pendingMessages shouldBe 'empty
+    sendMsg should be(pending)
+  }
+}
