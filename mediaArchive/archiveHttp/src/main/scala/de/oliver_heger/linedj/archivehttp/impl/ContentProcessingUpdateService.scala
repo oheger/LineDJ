7788071f90d352d90365035eaa21f158e7a4d47d
oheger <oliver.heger@oliver-heger.de>
2018-06-20 19:21:45 +0200

    Added ContentProcessingUpdateService.
    This is a service managing the processing state while reading the
    content of an HTTP archive. It is going to be used by the archive
    content processor actor in future.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/ContentProcessingUpdateService.scala
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import akka.actor.ActorRef
+import scalaz.State
+import scalaz.State._
+
+/**
+  * A class representing the processing state while reading the content of an
+  * HTTP archive.
+  *
+  * @param mediaInProgress  the number of media that are currently processed
+  * @param ack              an actor that waits for an ACK message
+  * @param propagateMsg     a message to send to the propagation actor
+  * @param contentInArchive flag whether content
+  * @param removeTriggered  flag whether a remove request has been sent
+  * @param pendingResult    a result that is pending to be sent; it cannot be
+  *                         sent until a propagation operation was confirmed
+  * @param pendingClient    a client that sent the last pending result
+  * @param seqNo            the sequence number of the current scan operation
+  * @param scanInProgress   flag whether a scan operation is currently ongoing
+  */
+case class ContentProcessingState(mediaInProgress: Int,
+                                  ack: Option[ActorRef],
+                                  propagateMsg: Option[PropagateMediumResult],
+                                  contentInArchive: Boolean,
+                                  removeTriggered: Boolean,
+                                  pendingResult: Option[MediumProcessingResult],
+                                  pendingClient: Option[ActorRef],
+                                  seqNo: Int,
+                                  scanInProgress: Boolean)
+
+/**
+  * A class storing information about actions to be executed when there is a
+  * transition of the content processing state.
+  *
+  * When the processing state changes, typically some actions need to be
+  * performed, such as propagating a new result or sending an ACK message.
+  *
+  * @param propagateMsg an optional result message to be propagated
+  * @param actorToAck   an actor to send an ACK message to
+  */
+case class ProcessingStateTransitionData(propagateMsg: Option[PropagateMediumResult],
+                                         actorToAck: Option[ActorRef])
+
+/**
+  * A service responsible for managing the processing state when reading the
+  * content from an HTTP archive.
+  *
+  * The content of an HTTP archive is loaded and processed using a stream that
+  * yields [[MediumProcessingResult]] objects. Whenever such a result becomes
+  * available the data it contains has to be sent to the union media archive.
+  * This service controls these send operations by creating the corresponding
+  * propagation messages. It also takes care that only a configurable number of
+  * media results are processed at a given point in time (to avoid uncontrolled
+  * memory usage). In addition, it has to be ensured that content of the HTTP
+  * archive is removed from the union archive before another scan operation
+  * starts.
+  */
+trait ContentProcessingUpdateService {
+  /** The type for updates of the internal state. */
+  type StateUpdate[A] = State[ContentProcessingState, A]
+
+  /**
+    * Updates the state for a newly started scan operation. Note that this
+    * function is limited to basic state changes. Starting of a scan requires
+    * some complex actions (like setting up the processing stream) that are not
+    * handled by this service. If an operation is already in progress, no state
+    * changes are performed, and a value of '''false''' is returned.
+    *
+    * @return the updated state and a flag whether an operation was started
+    */
+  def processingStarts(): StateUpdate[Boolean]
+
+  /**
+    * Updates the state for a new result becoming available. A corresponding
+    * message to the propagation actor is created. If possible, this message
+    * can be sent directly; otherwise, it has to be parked until results have
+    * been propagated.
+    *
+    * @param result        the new result
+    * @param client        the client actor
+    * @param maxInProgress the maximum number of results in progress
+    * @return the updated state
+    */
+  def resultAvailable(result: MediumProcessingResult, client: ActorRef, maxInProgress: Int):
+  StateUpdate[Unit]
+
+  /**
+    * Updates the state when a confirmation arrives that a result has been
+    * propagated to the union archive. This decrements the counter for the
+    * results in progress. If there is a result pending for propagation, it
+    * can be handled now.
+    *
+    * @param seqNo         the sequence number from the confirmation message
+    * @param maxInProgress the maximum number of results in progress
+    * @return the updated state
+    */
+  def resultPropagated(seqNo: Int, maxInProgress: Int): StateUpdate[Unit]
+
+  /**
+    * Updates the state when processing of the HTTP archive is complete.
+    *
+    * @return the updated state
+    */
+  def processingDone(): StateUpdate[Unit]
+
+  /**
+    * Updates the state to reflect that transition actions are executed and
+    * returns an object with information about these actions. It is assumed
+    * that the caller directly executes the actions described by the returned
+    * object.
+    *
+    * @return the updated state and transition data
+    */
+  def fetchTransitionData(): StateUpdate[ProcessingStateTransitionData]
+
+  /**
+    * Handles a newly available result by updating the state accordingly and
+    * returning information about transition actions.
+    *
+    * @param result        the new result
+    * @param client        the client actor
+    * @param maxInProgress the maximum number of results in progress
+    * @return the updated state and transition data
+    */
+  def handleResultAvailable(result: MediumProcessingResult, client: ActorRef, maxInProgress: Int):
+  StateUpdate[ProcessingStateTransitionData] = for {
+    _ <- resultAvailable(result, client, maxInProgress)
+    data <- fetchTransitionData()
+  } yield data
+
+  /**
+    * Handles a result propagated confirmation by updating the state
+    * accordingly and returning information about transition actions.
+    *
+    * @param seqNo         the sequence number from the confirmation message
+    * @param maxInProgress the maximum number of results in progress
+    * @return the updated state and transition data
+    */
+  def handleResultPropagated(seqNo: Int, maxInProgress: Int):
+  StateUpdate[ProcessingStateTransitionData] = for {
+    _ <- resultPropagated(seqNo, maxInProgress)
+    data <- fetchTransitionData()
+  } yield data
+}
+
+object ContentProcessingUpdateServiceImpl extends ContentProcessingUpdateService {
+  /** Constant for the initial content processing state. */
+  val InitialState: ContentProcessingState = ContentProcessingState(mediaInProgress = 0,
+    ack = None, propagateMsg = None, pendingResult = None, pendingClient = None,
+    contentInArchive = false, removeTriggered = true, seqNo = 0, scanInProgress = false)
+
+  override def processingStarts(): StateUpdate[Boolean] = State { s =>
+    if (s.scanInProgress) (s, false)
+    else (s.copy(mediaInProgress = 0, ack = None, propagateMsg = None, pendingClient = None,
+      pendingResult = None, seqNo = s.seqNo + 1, removeTriggered = false,
+      scanInProgress = true), true)
+  }
+
+  override def resultAvailable(result: MediumProcessingResult, client: ActorRef,
+                               maxInProgress: Int): StateUpdate[Unit] = modify { s =>
+    if (s.propagateMsg.isDefined || s.pendingResult.isDefined || result.seqNo != s.seqNo) s
+    else if (s.mediaInProgress >= maxInProgress)
+      s.copy(pendingClient = Some(client), pendingResult = Some(result))
+    else s.copy(mediaInProgress = s.mediaInProgress + 1, ack = Some(client),
+      propagateMsg = Some(PropagateMediumResult(result, removeContent = shouldTriggerRemove(s))),
+      contentInArchive = true, removeTriggered = true)
+  }
+
+  override def resultPropagated(seqNo: Int, maxInProgress: Int): StateUpdate[Unit] = modify { s =>
+    if (seqNo == s.seqNo) {
+      val nextInProgress = math.max(0, s.mediaInProgress - 1)
+      s.pendingResult match {
+        case Some(result) if nextInProgress < maxInProgress =>
+          s.copy(ack = s.pendingClient, pendingClient = None,
+            propagateMsg = Some(PropagateMediumResult(result, removeContent = false)),
+            pendingResult = None)
+        case _ =>
+          s.copy(mediaInProgress = nextInProgress)
+      }
+    }
+    else s
+  }
+
+  override def processingDone(): StateUpdate[Unit] = modify { s =>
+    s.copy(scanInProgress = false)
+  }
+
+  override def fetchTransitionData(): StateUpdate[ProcessingStateTransitionData] = State { s =>
+    (s.copy(ack = None, propagateMsg = None),
+      ProcessingStateTransitionData(s.propagateMsg, s.ack))
+  }
+
+  /**
+    * Returns a flag whether for an incoming result a remove operation from the
+    * union archive needs to be triggered. The result depends on the current
+    * state: whether information is stored in the union archive and whether
+    * this is the first result of a scan operation.
+    *
+    * @param s the state
+    * @return a flag whether a remove operation should be triggered
+    */
+  private def shouldTriggerRemove(s: ContentProcessingState): Boolean =
+    s.contentInArchive && !s.removeTriggered
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/ContentProcessingUpdateServiceSpec.scala
@@ -0,0 +1,305 @@
+/*
+ * Copyright 2015-2018 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+object ContentProcessingUpdateServiceSpec {
+  /** Test value for the max in progress counter. */
+  private val MaxInProgress = 8
+
+  /**
+    * Convenience method to execute a ''State'' object to produce the updated
+    * state and an additional result.
+    *
+    * @param s        the ''State''
+    * @param oldState the original state
+    * @tparam A the type of the additional result
+    * @return a tuple with the updated state and the additional result
+    */
+  private def updateState[A](s: ContentProcessingUpdateServiceImpl.StateUpdate[A],
+                             oldState: ContentProcessingState =
+                             ContentProcessingUpdateServiceImpl.InitialState):
+  (ContentProcessingState, A) = s(oldState)
+
+  /**
+    * Convenience method to modify a ''State'' object. This means, the state is
+    * only manipulated without producing an additional result.
+    *
+    * @param s        the ''State''
+    * @param oldState the original state
+    * @return the updated state
+    */
+  private def modifyState(s: ContentProcessingUpdateServiceImpl.StateUpdate[Unit],
+                          oldState: ContentProcessingState =
+                          ContentProcessingUpdateServiceImpl.InitialState):
+  ContentProcessingState = {
+    val (next, _) = updateState(s, oldState)
+    next
+  }
+}
+
+/**
+  * Test class for ''ContentProcessingUpdateService''.
+  */
+class ContentProcessingUpdateServiceSpec(testSystem: ActorSystem) extends TestKit(testSystem)
+  with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  def this() = this(ActorSystem("ContentProcessingUpdateServiceSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  import ContentProcessingUpdateServiceSpec._
+
+  /**
+    * Creates a mock result object.
+    *
+    * @param seqNo the sequence number of the result
+    * @return the mock result
+    */
+  private def createResult(seqNo: Int = 0): MediumProcessingResult = {
+    val result = mock[MediumProcessingResult]
+    when(result.seqNo).thenReturn(seqNo)
+    result
+  }
+
+  "ContentProcessingUpdateService" should "have a correct initial state" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+
+    state.mediaInProgress should be(0)
+    state.ack shouldBe 'empty
+    state.pendingResult shouldBe 'empty
+    state.pendingClient shouldBe 'empty
+    state.propagateMsg shouldBe 'empty
+    state.contentInArchive shouldBe false
+    state.removeTriggered shouldBe true
+    state.seqNo should be(0)
+    state.scanInProgress shouldBe false
+  }
+
+  it should "update the state for a newly started processing operation" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState.copy(mediaInProgress = 28,
+      ack = Some(testActor), propagateMsg = Some(mock[PropagateMediumResult]),
+      pendingResult = Some(createResult()), pendingClient = Some(testActor))
+
+    val (next, f) = updateState(ContentProcessingUpdateServiceImpl.processingStarts(), state)
+    f shouldBe true
+    next.propagateMsg shouldBe 'empty
+    next.ack shouldBe 'empty
+    next.pendingResult shouldBe 'empty
+    next.pendingClient shouldBe 'empty
+    next.seqNo should not be state.seqNo
+    next.mediaInProgress should be(0)
+    next.removeTriggered shouldBe false
+    next.scanInProgress shouldBe true
+  }
+
+  it should "ignore a processing starts call if a scan is already in progress" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState.copy(scanInProgress = true,
+      ack = Some(testActor))
+
+    val (next, f) = updateState(ContentProcessingUpdateServiceImpl.processingStarts(), state)
+    next should be theSameInstanceAs state
+    f shouldBe false
+  }
+
+  it should "reject a new result if the last one has not been propagated yet" in {
+    val propagate = PropagateMediumResult(createResult(), removeContent = true)
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(propagateMsg = Some(propagate))
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(createResult(),
+      testActor, MaxInProgress), state)
+    next should be theSameInstanceAs state
+  }
+
+  it should "reject a new result if another one is pending" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(pendingResult = Some(createResult()))
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(createResult(),
+      testActor, MaxInProgress), state)
+    next should be theSameInstanceAs state
+  }
+
+  it should "update the state for a new result" in {
+    val ProgressCount = MaxInProgress - 1
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(mediaInProgress = ProgressCount)
+    val result = createResult()
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(result, testActor,
+      MaxInProgress), state)
+    next.removeTriggered shouldBe true
+    next.mediaInProgress should be(ProgressCount + 1)
+    next.propagateMsg should be(Some(PropagateMediumResult(result, removeContent = false)))
+    next.contentInArchive shouldBe true
+    next.ack should be(Some(testActor))
+    next.pendingClient shouldBe 'empty
+    next.pendingResult shouldBe 'empty
+  }
+
+  it should "ignore a result with a wrong sequence number" in {
+    val result = createResult(ContentProcessingUpdateServiceImpl.InitialState.seqNo + 1)
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(result, testActor,
+      MaxInProgress))
+    next should be theSameInstanceAs ContentProcessingUpdateServiceImpl.InitialState
+  }
+
+  it should "take the maximum in progress count into account" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(mediaInProgress = MaxInProgress)
+    val result = createResult()
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(result, testActor,
+      MaxInProgress), state)
+    next.mediaInProgress should be(MaxInProgress)
+    next.propagateMsg shouldBe 'empty
+    next.ack shouldBe 'empty
+    next.pendingResult should be(Some(result))
+    next.pendingClient should be(Some(testActor))
+  }
+
+  it should "set the remove flag for the first result if content in archive" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState.copy(contentInArchive = true,
+      removeTriggered = false)
+    val result = createResult()
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(result, testActor,
+      MaxInProgress), state)
+    next.propagateMsg should be(Some(PropagateMediumResult(result, removeContent = true)))
+    next.removeTriggered shouldBe true
+  }
+
+  it should "set the remove flag only for the first result if content in archive" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState.copy(removeTriggered = false)
+    val result = createResult()
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultAvailable(result, testActor,
+      MaxInProgress), state)
+    next.propagateMsg should be(Some(PropagateMediumResult(result, removeContent = false)))
+    next.removeTriggered shouldBe true
+  }
+
+  it should "decrement the progress counter when a result was propagated" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(mediaInProgress = MaxInProgress)
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultPropagated(state.seqNo,
+      MaxInProgress), state)
+    next should be(state.copy(mediaInProgress = MaxInProgress - 1))
+  }
+
+  it should "not decrement the progress counter below 0" in {
+    val next = modifyState(ContentProcessingUpdateServiceImpl
+      .resultPropagated(ContentProcessingUpdateServiceImpl.InitialState.seqNo, MaxInProgress))
+
+    next.mediaInProgress should be(0)
+  }
+
+  it should "ignore result propagation messages with a wrong seq number" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState.copy(mediaInProgress = 1)
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl
+      .resultPropagated(state.seqNo + 1, MaxInProgress), state)
+    next should be theSameInstanceAs state
+  }
+
+  it should "deal with a pending result when a result was propagated" in {
+    val result = createResult()
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(mediaInProgress = MaxInProgress, pendingClient = Some(testActor),
+        pendingResult = Some(result))
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultPropagated(state.seqNo,
+      MaxInProgress), state)
+    next.mediaInProgress should be(MaxInProgress)
+    next.pendingClient shouldBe 'empty
+    next.pendingResult shouldBe 'empty
+    next.propagateMsg should be(Some(PropagateMediumResult(result, removeContent = false)))
+    next.ack should be(Some(testActor))
+  }
+
+  it should "only deal with a pending result if the progress count is low enough" in {
+    val result = createResult()
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(mediaInProgress = MaxInProgress + 1, pendingClient = Some(testActor),
+        pendingResult = Some(result))
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.resultPropagated(state.seqNo,
+      MaxInProgress), state)
+    next.pendingClient should be(Some(testActor))
+    next.pendingResult should be(Some(result))
+    next.ack shouldBe 'empty
+    next.propagateMsg shouldBe 'empty
+    next.mediaInProgress should be(MaxInProgress)
+  }
+
+  it should "update the state when processing is complete" in {
+    val state = ContentProcessingUpdateServiceImpl.InitialState.copy(scanInProgress = true)
+
+    val next = modifyState(ContentProcessingUpdateServiceImpl.processingDone(), state)
+    next.scanInProgress shouldBe false
+  }
+
+  it should "fetch transition data" in {
+    val propMsg = PropagateMediumResult(createResult(), removeContent = false)
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(propagateMsg = Some(propMsg), ack = Some(testActor))
+
+    val (next, data) = updateState(ContentProcessingUpdateServiceImpl.fetchTransitionData(), state)
+    next.ack shouldBe 'empty
+    next.propagateMsg shouldBe 'empty
+    data.propagateMsg should be(Some(propMsg))
+    data.actorToAck should be(Some(testActor))
+  }
+
+  it should "handle a new result" in {
+    val result = createResult()
+
+    val (next, data) = updateState(ContentProcessingUpdateServiceImpl
+      .handleResultAvailable(result, testActor, MaxInProgress))
+    next.mediaInProgress should be(1)
+    next.ack shouldBe 'empty
+    next.propagateMsg shouldBe 'empty
+    data.propagateMsg should be(Some(PropagateMediumResult(result, removeContent = false)))
+    data.actorToAck should be(Some(testActor))
+  }
+
+  it should "handle a result propagated confirmation" in {
+    val result = createResult()
+    val state = ContentProcessingUpdateServiceImpl.InitialState
+      .copy(mediaInProgress = MaxInProgress, pendingClient = Some(testActor),
+        pendingResult = Some(result))
+
+    val (next, data) = updateState(ContentProcessingUpdateServiceImpl
+      .handleResultPropagated(state.seqNo, MaxInProgress), state)
+    next.mediaInProgress should be(MaxInProgress)
+    next.pendingClient shouldBe 'empty
+    next.pendingResult shouldBe 'empty
+    next.propagateMsg shouldBe 'empty
+    next.ack shouldBe 'empty
+    data.propagateMsg should be(Some(PropagateMediumResult(result, removeContent = false)))
+    data.actorToAck should be(Some(testActor))
+  }
+}
