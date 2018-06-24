6b32ddffe2aaa950b8d9ae6fe219b02f2859afee
oheger <oliver.heger@oliver-heger.de>
2018-06-24 21:12:40 +0200

    Added an optional checksum to MediaFileID.
    The checksum is an alternative way to identify the medium that
    contains the media file. It is independent on the archive component
    that hosts the medium. This will be the preferred way to select a
    medium.

--- shared/src/main/scala/de/oliver_heger/linedj/shared/archive/media/MediaFileID.scala
+++ shared/src/main/scala/de/oliver_heger/linedj/shared/archive/media/MediaFileID.scala
@@ -19,12 +19,19 @@ package de.oliver_heger.linedj.shared.archive.media
 /**
   * A data class which uniquely identifies a media file.
   *
-  * An audio file in the archive is uniquely identified by the ID of the medium
-  * it belongs to and the (relative) URI within this medium. With an instance
-  * of this class a specific file or its meta data can be requested from the
-  * media archive.
+  * An audio file in the archive is uniquely identified by the medium it
+  * belongs to and the (relative) URI within this medium. The medium is
+  * defined either by a [[MediumID]] (which depends on a concrete archive
+  * component) or a checksum string. The checksum is calculated based on the
+  * content of the medium and thus remains the same no matter from which
+  * archive component it is served.
+  *
+  * With an instance of this class a specific file or its meta data can be
+  * requested from the media archive.
   *
   * @param mediumID the ID of the medium the desired source belongs to
   * @param uri      the URI of the desired source relative to the medium
+  * @param checksum an optional checksum identifying the medium in a way
+  *                 independent on a specific archive component
   */
-case class MediaFileID(mediumID: MediumID, uri: String)
+case class MediaFileID(mediumID: MediumID, uri: String, checksum: Option[String] = None)
