61d7e8feb9c68b424364fb66810768a80c110b6f
oheger <oliver.heger@oliver-heger.de>
2019-02-02 21:28:15 +0100

    Added HttpRequestActor.
    This actor allows sending requests to an HTTP archive and takes care
    about authorization and cookie management. It is going to replace the
    HttpRequestSupport trait.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/io/HttpRequestActor.scala
@@ -0,0 +1,329 @@
+/*
+ * Copyright 2015-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.io
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers._
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source, SourceQueueWithComplete}
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult}
+import de.oliver_heger.linedj.archivehttp.config.HttpArchiveConfig
+import de.oliver_heger.linedj.utils.SystemPropertyAccess
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success, Try}
+
+object HttpRequestActor {
+  /** The name of the header to set cookies. */
+  private val SetCookieHeader = "set-cookie"
+
+  /**
+    * A message triggering [[HttpRequestActor]] to send an HTTP request.
+    *
+    * The actor sends the given request to the target server. It then returns a
+    * [[ResponseData]] object to the sender. When called via the ''ask''
+    * pattern the future fails if the request was not successful.
+    *
+    * @param request the request to be executed
+    * @param data    additional request data that is included in the response
+    */
+  case class SendRequest(request: HttpRequest, data: Any)
+
+  /**
+    * A data class representing the response of a request.
+    *
+    * This class is used by [[HttpRequestActor]] to send the result of a
+    * request execution back to the sender.
+    *
+    * @param response the response from the server
+    * @param data     additional request data
+    */
+  case class ResponseData(response: HttpResponse, data: Any)
+
+  /**
+    * Returns an object with properties to create a new actor instance.
+    *
+    * @param config the configuration of the HTTP archive
+    * @return the properties to create a new actor instance
+    */
+  def apply(config: HttpArchiveConfig): Props =
+    Props(classOf[HttpRequestActorImpl], config)
+
+  private class HttpRequestActorImpl(config: HttpArchiveConfig) extends HttpRequestActor(config)
+    with HttpFlowFactory with SystemPropertyAccess
+
+  /**
+    * An internally data class to store information about requests that may be
+    * retried.
+    *
+    * @param request  the request to be executed
+    * @param data     additional data about the request
+    * @param response the original (failed) response
+    * @param client   the client that sent this request
+    */
+  private case class RetryRequest(request: HttpRequest, data: Any, response: HttpResponse, client: ActorRef)
+
+  /**
+    * Checks whether a failed response can be retried. This is the case for
+    * certain status codes and if it contains cookies.
+    *
+    * @param response the response
+    * @return a flag whether this response can be retried
+    */
+  private def canRetry(response: HttpResponse): Boolean =
+    response.status == StatusCodes.Unauthorized && hasCookies(response)
+
+  /**
+    * Checks whether the given response contains at least one header to set a
+    * cookie.
+    *
+    * @param response the response
+    * @return a flag whether a set-cookie header was found
+    */
+  private def hasCookies(response: HttpResponse): Boolean =
+    response.headers.exists(_.is(SetCookieHeader))
+
+  /**
+    * Returns a sequence with all the cookie information defined by the given
+    * response.
+    *
+    * @param response the response
+    * @return a list of all cookies
+    */
+  private def extractCookies(response: HttpResponse): List[HttpCookiePair] = {
+    val cookies = response.headers.foldLeft(List.empty[HttpCookie]) { (lst, h) =>
+      h match {
+        case `Set-Cookie`(cookie) => cookie :: lst
+        case _ => lst
+      }
+    }
+    cookies.map(c => HttpCookiePair(c.name, c.value))
+  }
+
+  /**
+    * Returns a sequence with the headers of the given request that are not
+    * managed by this actor class.
+    *
+    * @param request the request
+    * @return the filtered headers of this request
+    */
+  private def filterHeaders(request: HttpRequest): Seq[HttpHeader] =
+    request.headers.filter {
+      case Authorization(_) => false
+      case Cookie(_) => false
+      case _ => true
+    }
+}
+
+/**
+  * An actor that sends HTTP requests to a specific server and also handles
+  * authorization cookies.
+  *
+  * This actor can handle ''SendRequest'' messages by sending the requests
+  * specified to the associated server. This is done via an HTTP flow that is
+  * created when this actor is initialized.
+  *
+  * The responses returned by this flow are then sent back to the sender. In
+  * case they have not been successful, their entity is discarded, and a future
+  * that failed with a [[FailedRequestException]] is returned.
+  *
+  * If a request fails with the status UNAUTHORIZED, the actor checks whether
+  * the response contains a ''Set-Cookie'' header. If so, the corresponding
+  * cookie is added the request, and the request is retried. This is done for
+  * all following requests. This behavior seems to be necessary to deal with
+  * certain proxies.
+  *
+  * @param config the configuration of the HTTP archive
+  */
+class HttpRequestActor(config: HttpArchiveConfig) extends Actor with ActorLogging {
+  this: HttpFlowFactory =>
+
+  import HttpRequestActor._
+
+  /** The execution context. */
+  implicit private val ec: ExecutionContext = context.system.dispatcher
+
+  /** The object to materialize streams. */
+  implicit private val mat: ActorMaterializer = ActorMaterializer()
+
+  /** The authorization header to be added to each request. */
+  private val authHeader = Authorization(BasicHttpCredentials(config.credentials.userName,
+    config.credentials.password))
+
+  /** The flow for executing requests. */
+  private var httpFlow: Flow[(HttpRequest, Promise[HttpResponse]),
+    (Try[HttpResponse], Promise[HttpResponse]), Any] = _
+
+  /** Stores the cookies to be added to requests. */
+  private var cookieHeader: Option[Cookie] = None
+
+  /** The queue acting as source for the stream of requests and a kill switch. */
+  private var queue: SourceQueueWithComplete[(HttpRequest, Promise[HttpResponse])] = _
+
+  override def preStart(): Unit = {
+    super.preStart()
+    implicit val actorSystem: ActorSystem = context.system
+    httpFlow = createHttpFlow[Promise[HttpResponse]](config.archiveURI)
+    queue = createRequestQueue()
+  }
+
+  override def receive: Receive = {
+    case SendRequest(request, data) =>
+      val headers = request.headers.toList
+      val headersWithCookie = cookieHeader.map(_ :: headers).getOrElse(headers)
+      val extendedHeaders = authHeader :: headersWithCookie
+      handleRequest(request.copy(headers = extendedHeaders), data, sender())
+
+    case rd: RetryRequest =>
+      retryRequest(rd)
+  }
+
+  override def postStop(): Unit = {
+    requestQueue.complete()
+    super.postStop()
+  }
+
+  /**
+    * Returns the request queue used by this actor. This is exposed only for
+    * testing purposes.
+    *
+    * @return the actor's request queue
+    */
+  def requestQueue: SourceQueueWithComplete[(HttpRequest, Promise[HttpResponse])] = queue
+
+  /**
+    * Creates the source of the stream for sending requests to the archive.
+    *
+    * @return the source for sending requests
+    */
+  private def createRequestQueue(): SourceQueueWithComplete[(HttpRequest, Promise[HttpResponse])] =
+    Source.queue[(HttpRequest, Promise[HttpResponse])](config.requestQueueSize, OverflowStrategy.dropNew)
+      .via(httpFlow)
+      .toMat(Sink.foreach({
+        case (Success(resp), p) => p.success(resp)
+        case (Failure(e), p) => p.failure(e)
+      }))(Keep.left)
+      .run()
+
+  /**
+    * Handles the execution of a request. The request is added to the request
+    * queue, and the resulting future is evaluated. If possible, the result is
+    * sent to the caller directly. Otherwise, a retry may be necessary.
+    *
+    * @param request the request
+    * @param data    the additional request data
+    * @param client  the sender of this request
+    */
+  private def handleRequest(request: HttpRequest, data: Any, client: ActorRef): Unit = {
+    log.info("{} {}", request.method.value, request.uri)
+    enqueueRequest(request, data) onComplete {
+      case Success((response, d)) =>
+        handleResponse(request, d, response, client)
+      case Failure(exception) =>
+        sendException(client, exception)
+    }
+  }
+
+  /**
+    * Puts a request into the queue and returns a ''Future'' with the response
+    * returned from the archive.
+    *
+    * @param request the request
+    * @param data    the additional request data
+    * @return a ''Future'' with the response and the data
+    */
+  private def enqueueRequest(request: HttpRequest, data: Any): Future[(HttpResponse, Any)] = {
+    val responsePromise = Promise[HttpResponse]()
+    requestQueue.offer(request -> responsePromise).flatMap {
+      case QueueOfferResult.Enqueued =>
+        responsePromise.future.map((_, data))
+      case QueueOfferResult.Dropped =>
+        Future.failed(new RuntimeException("Queue overflowed."))
+      case QueueOfferResult.Failure(ex) =>
+        Future.failed(ex)
+      case QueueOfferResult.QueueClosed =>
+        Future.failed(new RuntimeException("Queue was closed."))
+    }
+  }
+
+  /**
+    * Handles a response received from the server. Evaluates the response's
+    * status code and transforms it to a failure result if it is not
+    * successful. If possible, a failed response is retried.
+    *
+    * @param request  the request
+    * @param data     additional request data
+    * @param response the response
+    * @param client   the sender of the request
+    */
+  private def handleResponse(request: HttpRequest, data: Any, response: HttpResponse, client: ActorRef): Unit = {
+    log.info("{} {} - {} {}", request.method.value, request.uri, response.status.intValue(),
+      response.status.reason())
+    verifyResponse(response) onComplete {
+      case Success(_) => client ! ResponseData(response, data)
+      case Failure(FailedRequestException(failedResponse)) if canRetry(failedResponse) =>
+        self ! RetryRequest(request, data, failedResponse, client)
+      case Failure(exception) => sendException(client, exception)
+    }
+  }
+
+  /**
+    * Verifies whether a response was successful. If this is the case, a future
+    * with the response is returned. Otherwise, the future fails with a
+    * [[FailedRequestException]] exception, and the response's entity is
+    * discarded.
+    *
+    * @param response the response
+    * @return a future with the verified response
+    */
+  private def verifyResponse(response: HttpResponse): Future[HttpResponse] =
+    if (response.status.isSuccess()) Future.successful(response)
+    else response.discardEntityBytes().future() flatMap (_ => Future.failed(FailedRequestException(response)))
+
+  /**
+    * Retries a request if possible. It is checked whether the original failed
+    * response contained a different cookie header than the cookies managed
+    * currently. If this is the case, the cookies are updated, and the request
+    * is sent again.
+    *
+    * @param retryData the data about the request to be retried
+    */
+  private def retryRequest(retryData: RetryRequest): Unit = {
+    val newCookies = extractCookies(retryData.response)
+    if (cookieHeader.exists(_.cookies == newCookies))
+      sendException(retryData.client, FailedRequestException(retryData.response))
+    else {
+      val cookie = Cookie(newCookies)
+      log.info("Updated cookies to {}.", newCookies)
+      val nextHeaders = authHeader :: cookie :: filterHeaders(retryData.request).toList
+      val nextRequest = retryData.request.copy(headers = nextHeaders)
+      handleRequest(nextRequest, retryData.data, retryData.client)
+      cookieHeader = Some(cookie)
+    }
+  }
+
+  /**
+    * Sends an exception to the given client as a response for a failed
+    * request.
+    *
+    * @param client the sender of the request
+    * @param ex     the exception to be sent
+    */
+  private def sendException(client: ActorRef, ex: Throwable): Unit = {
+    client ! akka.actor.Status.Failure(ex)
+  }
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/io/HttpRequestActorSpec.scala
@@ -0,0 +1,357 @@
+/*
+ * Copyright 2015-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.io
+
+import java.io.IOException
+import java.util.concurrent.LinkedBlockingQueue
+
+import akka.Done
+import akka.actor.{ActorSystem, Props, Terminated}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers._
+import akka.pattern.ask
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Source, SourceQueueWithComplete}
+import akka.testkit.{TestActorRef, TestKit, TestProbe}
+import akka.util.{ByteString, Timeout}
+import de.oliver_heger.linedj.archivehttp.config.{HttpArchiveConfig, UserCredentials}
+import de.oliver_heger.linedj.utils.SystemPropertyAccess
+import org.mockito.Matchers.any
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future, Promise}
+import scala.reflect.ClassTag
+import scala.util.{Failure, Success, Try}
+
+object HttpRequestActorSpec {
+  /** A timeout for request execution. */
+  private implicit val RequestTimeout: Timeout = Timeout(3.seconds)
+
+  /** The URI to the test server. */
+  private val ServerUri = Uri("https://my.server.biz")
+
+  /** Path for a test request. */
+  private val RequestPath = "/foo"
+
+  /** A data object to be passed to a request. */
+  private val Data = new Object
+
+  /** The size of the request queue. */
+  private val QueueSize = 42
+
+  /** An object with test user credentials. */
+  private val Credentials = UserCredentials("scott", "tiger")
+
+  /** A test configuration for an HTTP archive. */
+  private val TestConfig = HttpArchiveConfig(archiveURI = ServerUri, credentials = Credentials,
+    requestQueueSize = QueueSize, archiveName = "MyTestArchive", processorCount = 1,
+    processorTimeout = Timeout(1.minute), propagationBufSize = 128, maxContentSize = 10000,
+    downloadBufferSize = 65536, downloadMaxInactivity = 30.seconds, downloadReadChunkSize = 100,
+    timeoutReadSize = 1024, downloadConfig = null, metaMappingConfig = null, contentMappingConfig = null)
+
+  /** The expected authorization header. */
+  private val AuthHeader = Authorization(BasicHttpCredentials(Credentials.userName, Credentials.password))
+
+  /** A test cookie. */
+  private val Cookie1 = HttpCookie(name = "cuckoo", value = "beep")
+
+  /** Another test cookie. */
+  private val Cookie2 = HttpCookie(name = "foo", value = "bar", path = Some("/"))
+
+  /** A list with default headers of the test request. */
+  private val DefaultHeaders = List(ETag("some_tag"))
+
+  /** A test request. */
+  private val TestRequest = HttpRequest(uri = RequestPath, headers = DefaultHeaders)
+
+  /**
+    * A case class storing data about an expected request.
+    *
+    * @param request  the request
+    * @param response the tried response to be returned
+    */
+  private case class ExpectedRequest(request: HttpRequest, response: Try[HttpResponse])
+
+  /**
+    * Adds the authorized header to the given request.
+    *
+    * @param request the request
+    * @return the request with the test authorized header
+    */
+  private def authorized(request: HttpRequest): HttpRequest =
+    withHeader(request, AuthHeader)
+
+  /**
+    * Adds the expected cookie header to the given request.
+    *
+    * @param request the request
+    * @return the request with a cookie header
+    */
+  private def withCookie(request: HttpRequest): HttpRequest = {
+    val cookieHeader = Cookie(List(HttpCookiePair(Cookie2.name, Cookie2.value),
+      HttpCookiePair(Cookie1.name, Cookie1.value)))
+    withHeader(request, cookieHeader)
+  }
+
+  /**
+    * Adds the given header to the headers of the passed in request.
+    *
+    * @param request the request
+    * @param header  the header to be added
+    * @return the updated request
+    */
+  private def withHeader(request: HttpRequest, header: HttpHeader): HttpRequest =
+    request.copy(headers = header :: request.headers.toList)
+
+  /**
+    * Returns a list with headers to set the test cookies.
+    *
+    * @return the list with set-cookie headers
+    */
+  private def setCookieHeaders(): List[HttpHeader] =
+    List(Cookie1, Cookie2).map(`Set-Cookie`(_))
+}
+
+/**
+  * Test class for ''HttpRequestActor''.
+  */
+class HttpRequestActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  def this() = this(ActorSystem("HttpRequestActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+  }
+
+  import HttpRequestActorSpec._
+
+  "A HttpRequestActor" should "create correct properties" in {
+    val props = HttpRequestActor(TestConfig)
+
+    classOf[HttpRequestActor].isAssignableFrom(props.actorClass()) shouldBe true
+    classOf[HttpFlowFactory].isAssignableFrom(props.actorClass()) shouldBe true
+    props.args should be(List(TestConfig))
+  }
+
+  it should "handle a successful request" in {
+    val expResponse = HttpResponse()
+    val helper = new RequestActorTestHelper
+
+    val response = helper.expectRequest(authorized(TestRequest), expResponse)
+      .sendRequest()
+    response should be(expResponse)
+  }
+
+  it should "handle a request that causes an exception" in {
+    val exception = new IOException("Failed request")
+    val helper = new RequestActorTestHelper
+
+    helper.expectFailedRequest(authorized(TestRequest), exception)
+      .sendRequestAndExpectFailure[IOException] should be(exception)
+  }
+
+  it should "fail the response future for a non-success response" in {
+    val response = HttpResponse(status = StatusCodes.BadRequest)
+    val helper = new RequestActorTestHelper
+
+    val exception = helper.expectRequest(authorized(TestRequest), response)
+      .sendRequestAndExpectFailure[FailedRequestException]
+    exception.response should be(response)
+  }
+
+  it should "discard the entity bytes of a non-success response" in {
+    val content = ByteString("This is the body of the test response.")
+    val entity = mock[ResponseEntity]
+    val discardedEntity = new HttpMessage.DiscardedEntity(Future.successful(Done))
+    when(entity.dataBytes).thenReturn(Source.single(content))
+    when(entity.discardBytes()(any(classOf[Materializer]))).thenReturn(discardedEntity)
+    val response = HttpResponse(status = StatusCodes.Unauthorized, entity = entity)
+    val helper = new RequestActorTestHelper
+
+    helper.expectRequest(authorized(TestRequest), response)
+      .sendRequestAndExpectFailure[FailedRequestException]
+    verify(entity).discardBytes()(any())
+  }
+
+  it should "retry an unauthorized request if there is a cookie from the server" in {
+    val response1 = HttpResponse(status = StatusCodes.Unauthorized, headers = setCookieHeaders())
+    val response2 = HttpResponse()
+    val helper = new RequestActorTestHelper
+
+    helper.expectRequest(authorized(TestRequest), response1)
+      .expectRequest(authorized(withCookie(TestRequest)), response2)
+      .sendRequest() should be(response2)
+  }
+
+  it should "only retry an unauthorized request once" in {
+    val response = HttpResponse(status = StatusCodes.Unauthorized, headers = setCookieHeaders())
+    val helper = new RequestActorTestHelper
+
+    helper.expectRequest(authorized(TestRequest), response)
+      .expectRequest(authorized(withCookie(TestRequest)), response)
+      .sendRequestAndExpectFailure[FailedRequestException].response should be(response)
+  }
+
+  it should "only retry unauthorized requests" in {
+    val response = HttpResponse(status = StatusCodes.Forbidden, headers = setCookieHeaders())
+    val helper = new RequestActorTestHelper
+
+    helper.expectRequest(authorized(TestRequest), response)
+      .sendRequestAndExpectFailure[FailedRequestException].response should be(response)
+  }
+
+  it should "store a cookie used for authorization" in {
+    val response1 = HttpResponse(status = StatusCodes.Unauthorized, headers = setCookieHeaders())
+    val response2 = HttpResponse()
+    val response3 = HttpResponse(status = StatusCodes.Accepted)
+    val otherReq = HttpRequest(method = HttpMethods.POST, uri = "/someEndpoint")
+    val helper = new RequestActorTestHelper
+    helper.expectRequest(authorized(TestRequest), response1)
+      .expectRequest(authorized(withCookie(TestRequest)), response2)
+      .expectRequest(authorized(withCookie(otherReq)), response3)
+      .sendRequest()
+
+    helper.sendRequest(otherReq) should be(response3)
+  }
+
+  it should "update the authorization cookie when it becomes invalid" in {
+    val pair = HttpCookiePair("update", "recent")
+    val newCookie = Cookie(pair)
+    val response1 = HttpResponse(status = StatusCodes.Unauthorized, headers = setCookieHeaders())
+    val response2 = HttpResponse()
+    val response3 = HttpResponse(status = StatusCodes.Unauthorized,
+      headers = List(`Set-Cookie`(HttpCookie(pair.name, pair.value))))
+    val otherReq = HttpRequest(method = HttpMethods.POST, uri = "/someEndpoint")
+    val helper = new RequestActorTestHelper
+
+    helper.expectRequest(authorized(TestRequest), response1)
+      .expectRequest(authorized(withCookie(TestRequest)), response2)
+      .expectRequest(authorized(withCookie(otherReq)), response3)
+      .expectRequest(authorized(withHeader(otherReq, newCookie)), response2)
+      .sendRequest()
+    helper.sendRequest(otherReq) should be(response2)
+  }
+
+  it should "close the request queue when it is stopped" in {
+    val queue = mock[SourceQueueWithComplete[(HttpRequest, Promise[HttpResponse])]]
+    val requestActor = system.actorOf(Props(new HttpRequestActor(TestConfig) with HttpFlowFactory with SystemPropertyAccess {
+      override def requestQueue: SourceQueueWithComplete[(HttpRequest, Promise[HttpResponse])] = queue
+    }))
+
+    val watcher = TestProbe()
+    watcher watch requestActor
+    system stop requestActor
+    watcher.expectMsgType[Terminated]
+    verify(queue).complete()
+  }
+
+  /**
+    * A test helper class that manages a test actor instance and some helper
+    * objects.
+    */
+  private class RequestActorTestHelper {
+    /** A queue that stores expected requests and their responses. */
+    private val expectedRequestQueue = new LinkedBlockingQueue[(HttpRequest, Try[HttpResponse])]
+
+    /** Reference to the actor to be tested. */
+    private val requestActor = createTestActor()
+
+    /**
+      * Adds a request/response pair to the queue of expected requests. When a
+      * request is received it is checked against the next request in the
+      * queue. If successful, the given response is returned.
+      *
+      * @param request  the expected request
+      * @param response the response to be returned
+      * @return this test helper
+      */
+    def expectRequest(request: HttpRequest, response: HttpResponse): RequestActorTestHelper = {
+      expectedRequestQueue offer(request, Success(response))
+      this
+    }
+
+    /**
+      * Adds a request to the queue of expected requests that should trigger an
+      * exception. If the given request is received, the exception will be
+      * returned.
+      *
+      * @param request the expected request
+      * @param failure the exception to answer this request
+      * @return this test helper
+      */
+    def expectFailedRequest(request: HttpRequest, failure: Throwable): RequestActorTestHelper = {
+      expectedRequestQueue offer(request, Failure(failure))
+      this
+    }
+
+    /**
+      * Sends a test request to the test actor, expects a successful
+      * response and returns it.
+      *
+      * @param request the test request to be sent
+      * @return the response from the test actor
+      */
+    def sendRequest(request: HttpRequest = TestRequest): HttpResponse = {
+      val futResponse = requestActor ? HttpRequestActor.SendRequest(request, Data)
+      val responseData = Await.result(futResponse.mapTo[HttpRequestActor.ResponseData], RequestTimeout.duration)
+      responseData.data should be(Data)
+      responseData.response
+    }
+
+    /**
+      * Sends the test request to the test actor and expects an exception to be
+      * thrown. The exception is returned.
+      *
+      * @param t the class tag
+      * @tparam E the type of the expected exception
+      * @return the exception
+      */
+    def sendRequestAndExpectFailure[E <: AnyRef](implicit t: ClassTag[E]): E = intercept[E] {
+      sendRequest()
+    }
+
+    /**
+      * Creates a reference to the actor to be tested.
+      *
+      * @return the test actor
+      */
+    private def createTestActor(): TestActorRef[HttpRequestActor] =
+      TestActorRef(createTestActorProps())
+
+    /**
+      * Creates properties to create a test actor instance. The actor uses a
+      * mock HTTP flow that returns predefined responses.
+      *
+      * @return the ''Props'' for a test actor instance
+      */
+    private def createTestActorProps(): Props =
+      Props(new HttpRequestActor(TestConfig) with HttpFlowFactory with SystemPropertyAccess {
+        override def createHttpFlow[T](uri: Uri)(implicit mat: Materializer, system: ActorSystem):
+        Flow[(HttpRequest, T), (Try[HttpResponse], T), Any] = {
+          uri should be(TestConfig.archiveURI)
+          Flow[(HttpRequest, T)] map { t =>
+            val (req, triedResp) = expectedRequestQueue.poll()
+            t._1 should be(req)
+            (triedResp, t._2)
+          }
+        }
+      })
+  }
+
+}
