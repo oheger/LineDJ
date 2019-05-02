e002e3af6079bd1e4a236994acb7daf564ea8c4e
oheger <oliver.heger@oliver-heger.de>
2019-05-02 18:08:32 +0200

    Fixed a Java 11 incompatibility.
    In Java 11 a lines() method exists for String which collides with the
    Scala method. This clash has been fixed by using the linesIterator()
    method instead.

--- shared/src/main/scala/de/oliver_heger/linedj/io/parser/Parsers.scala
+++ shared/src/main/scala/de/oliver_heger/linedj/io/parser/Parsers.scala
@@ -198,7 +198,7 @@ case class Location(input: String, offset: Int = 0) {
   /* Returns the line corresponding to this location */
   def currentLine: String =
     if (input.length > 1) {
-      val it = input.lines.drop(line-1)
+      val it = input.linesIterator.drop(line-1)
       if(it.hasNext) it.next() else ""
     }
     else ""
