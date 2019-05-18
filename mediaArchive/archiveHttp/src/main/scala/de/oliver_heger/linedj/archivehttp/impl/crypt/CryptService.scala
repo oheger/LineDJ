f104cc2f407ff40b4e005cb6b0817cfd9c679d8a
oheger <oliver.heger@oliver-heger.de>
2019-05-18 17:19:58 +0200

    Added a service to decrypt data.
    The service offers functionality to decrypt file or folder names and
    whole sources of encrypted data.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/impl/crypt/CryptService.scala
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2015-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.crypt
+
+import java.nio.charset.StandardCharsets
+import java.security.{Key, SecureRandom}
+import java.util.Base64
+
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import akka.stream.scaladsl.Source
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.util.ByteString
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+
+/**
+  * A service offering functionality related to cryptographic operations.
+  *
+  * The functions provided by this service are used to decrypt file names and
+  * content from encrypted media archives.
+  */
+object CryptService {
+  /** Name of the cipher used for decryption. */
+  val CipherName = "AES/CTR/NoPadding"
+
+  /** The length of the initialization vector. */
+  val IvLength = 16
+
+  /**
+    * Decrypts the given name. The name is expected to be Base64 encoded. It is
+    * decoded to a byte array and then decrypted using the password provided.
+    *
+    * @param key       the key to be used for decryption
+    * @param name      the name to be decrypted
+    * @param secRandom the random object
+    * @return the decrypted name
+    */
+  def decryptName(key: Key, name: String)(implicit secRandom: SecureRandom): String = {
+    val cipher = createCipher()
+    val cryptBytes = Base64.getUrlDecoder.decode(name)
+    initCipher(key, secRandom, cipher, cryptBytes)
+
+    val plainBytes = cipher.doFinal(cryptBytes, IvLength, cryptBytes.length - IvLength)
+    new String(plainBytes, StandardCharsets.UTF_8)
+  }
+
+  /**
+    * Decrypts the given source. The source is modified, so that its content is
+    * automatically decrypted using the passed in key.
+    *
+    * @param key          the key o be used for decryption
+    * @param source       the source to be decrypted
+    * @param secureRandom the random object
+    * @tparam Mat the type for materialization
+    * @return the decrypted source
+    */
+  def decryptSource[Mat](key: Key, source: Source[ByteString, Mat])(implicit secureRandom: SecureRandom):
+  Source[ByteString, Mat] =
+    source.via(new CryptStage(key, secureRandom))
+
+  /**
+    * Creates a new ''Cipher'' object for a decrypt operation as performed by
+    * this service.
+    *
+    * @return the new ''Cipher'' object
+    */
+  private def createCipher(): Cipher = Cipher.getInstance(CipherName)
+
+  /**
+    * Initializes the given ''Cipher'' object for decryption using the chunk of
+    * data provided. This function expects that the initialization vector is
+    * contained at the beginning of the given data array.
+    *
+    * @param key        the key for decryption
+    * @param secRandom  the random object
+    * @param cipher     the ''Cipher'' to be initialized
+    * @param cryptBytes the data to be processed
+    */
+  private def initCipher(key: Key, secRandom: SecureRandom, cipher: Cipher, cryptBytes: Array[Byte]): Unit = {
+    val iv = new IvParameterSpec(cryptBytes, 0, IvLength)
+    cipher.init(Cipher.DECRYPT_MODE, key, iv, secRandom)
+  }
+
+  /**
+    * An internally used graph stage that decrypts the data passed through it.
+    *
+    * @param key    the key to be used for decryption
+    * @param random the secure random object
+    */
+  private class CryptStage(key: Key, random: SecureRandom) extends GraphStage[FlowShape[ByteString, ByteString]] {
+    /**
+      * Definition of a processing function. The function expects a block of data
+      * and a cipher and produces a block of data to be passed downstream.
+      */
+    type CryptFunc = (ByteString, Cipher) => ByteString
+
+    val in: Inlet[ByteString] = Inlet[ByteString]("CryptStage.in")
+    val out: Outlet[ByteString] = Outlet[ByteString]("CryptStage.out")
+
+    override val shape: FlowShape[ByteString, ByteString] = FlowShape.of(in, out)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+        /** The cipher object managed by this class. */
+        private lazy val cryptCipher = createCipher()
+
+        /** The current processing function. */
+        private var processingFunc: CryptFunc = initProcessing
+
+        /**
+          * A flag whether data has been received by this stage. This is used to
+          * determine whether a final block of data has to be handled when
+          * upstream ends.
+          */
+        private var dataProcessed = false
+
+        setHandler(in, new InHandler {
+          override def onPush(): Unit = {
+            val data = grab(in)
+            val processedData = processingFunc(data, cryptCipher)
+            push(out, processedData)
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            if (dataProcessed) {
+              val finalBytes = cryptCipher.doFinal()
+              if (finalBytes.nonEmpty) {
+                push(out, ByteString(finalBytes))
+              }
+            }
+            super.onUpstreamFinish()
+          }
+        })
+
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit = {
+            pull(in)
+          }
+        })
+
+        /**
+          * Handles initialization of data processing. This is the ''CryptFunc''
+          * that is called for the first block of data received. It initializes
+          * the managed cipher object, pushes the first data block downstream,
+          * and sets the actual processing function.
+          *
+          * @param data   the first chunk of data
+          * @param cipher the cipher object
+          * @return the data to be pushed downstream
+          */
+        private def initProcessing(data: ByteString, cipher: Cipher): ByteString = {
+          dataProcessed = true
+          processingFunc = cryptFunc
+          val dataBytes = data.toArray
+          initCipher(key, random, cipher, dataBytes)
+          ByteString(cipher.update(dataBytes, IvLength, dataBytes.length - IvLength))
+        }
+      }
+
+    /**
+      * Returns the function executing decryption logic. This function is
+      * called when initialization is done to process further data chunks.
+      *
+      * @return the function for processing data chunks
+      */
+    private def cryptFunc: CryptFunc = (chunk, cipher) => {
+      val encData = cipher.update(chunk.toArray)
+      ByteString(encData)
+    }
+  }
+
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/impl/crypt/CryptServiceSpec.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2015-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.impl.crypt
+
+import java.nio.file.Paths
+import java.security.SecureRandom
+import java.util.Base64
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{FileIO, Sink, Source}
+import akka.testkit.TestKit
+import akka.util.ByteString
+import de.oliver_heger.linedj.FileTestHelper
+import de.oliver_heger.linedj.archivehttp.crypt.AESKeyGenerator
+import de.oliver_heger.linedj.archivehttp.impl.crypt.CryptServiceSpec.Key
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object CryptServiceSpec {
+  /** The password for crypt operations. */
+  private val Password = "secret_password!"
+
+  /** Key specification for the test password. */
+  private val Key = new AESKeyGenerator().generateKey(Password)
+}
+
+/**
+  * Test class for ''CryptService''.
+  */
+class CryptServiceSpec(testSystem: ActorSystem) extends TestKit(testSystem) with FlatSpecLike with BeforeAndAfterAll with Matchers with FileTestHelper {
+  def this() = this(ActorSystem("CryptServiceSpec"))
+
+  override protected def afterAll(): Unit = {
+    super.afterAll()
+    TestKit shutdownActorSystem system
+  }
+
+  /**
+    * Reads all the data contained in the given source.
+    *
+    * @param source the source
+    * @return the content of this source
+    */
+  private def readSource(source: Source[ByteString, Any]): ByteString = {
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+    val sink = Sink.fold[ByteString, ByteString](ByteString.empty)(_ ++ _)
+    val futResult = source.runWith(sink)
+    Await.result(futResult, 3.seconds)
+  }
+
+  /**
+    * Applies the decrypt function from the test service to the given source
+    * and reads its content.
+    *
+    * @param source the source
+    * @return the decrypted content of this source
+    */
+  private def decryptAndReadSource(source: Source[ByteString, Any]): ByteString = {
+    implicit val random: SecureRandom = new SecureRandom()
+    readSource(CryptService.decryptSource(Key, source))
+  }
+
+  import CryptServiceSpec._
+
+  "CryptService" should "decrypt a file name" in {
+    implicit val random: SecureRandom = new SecureRandom()
+    val EncName = "Ah56qLiZLMv9NdTkF_Z0WXo32dDcu4g="
+    val PlainName = "folder1"
+
+    CryptService.decryptName(Key, EncName) should be(PlainName)
+  }
+
+  it should "throw an exception if a name to be decrypted does not start with an IV" in {
+    implicit val random: SecureRandom = new SecureRandom()
+
+    intercept[IllegalArgumentException] {
+      CryptService.decryptName(Key, "Ah56qLiZ")
+    }
+  }
+
+  it should "decrypt a source" in {
+    val path = Paths.get(getClass.getResource("/encrypted.dat").toURI)
+    val source = FileIO.fromPath(path)
+
+    val decrypted = decryptAndReadSource(source).utf8String
+    decrypted should be(FileTestHelper.TestData)
+  }
+
+  it should "handle an empty source to decrypt" in {
+    val decrypted = decryptAndReadSource(Source.empty)
+
+    decrypted should be(ByteString.empty)
+  }
+
+  it should "handle a source with a single chunk to decrypt" in {
+    val EncText = Base64.getUrlDecoder.decode("Ah56qLiZLMv9NdTkF_Z0WXo32dDcu4g=")
+    val PlainText = "folder1"
+    val source = Source.single(ByteString(EncText))
+
+    val decrypted = decryptAndReadSource(source).utf8String
+    decrypted should be(PlainText)
+  }
+}
