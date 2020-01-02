53f645ec453627707a738fae6cb322131c4b95f3
oheger <oliver.heger@oliver-heger.de>
2020-01-02 15:45:19 +0100

    Added ArchiveGroupActor.
    This actor creates the actors of a group of media archives as its
    child actors. It triggers an initial media scan and makes sure that
    only a single scan operation is active for all the archives in this
    group.

---
+++ mediaArchive/archive/src/main/scala/de/oliver_heger/linedj/archive/group/ArchiveGroupActor.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2015-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.group
+
+import akka.actor.{Actor, ActorRef, Props}
+import de.oliver_heger.linedj.archive.config.MediaArchiveConfig
+import de.oliver_heger.linedj.archive.media.MediaManagerActor
+import de.oliver_heger.linedj.shared.archive.media.ScanAllMedia
+import de.oliver_heger.linedj.utils.ChildActorFactory
+
+object ArchiveGroupActor {
+  /**
+    * Returns a ''Props'' object for creating an instance of this actor class.
+    *
+    * @param mediaUnionActor    the media actor of the union archive
+    * @param metaDataUnionActor the meta data actor of the union archive
+    * @param archiveConfigs     the configurations of the archives in the group
+    * @return a ''Props'' object to create a new instance
+    */
+  def apply(mediaUnionActor: ActorRef, metaDataUnionActor: ActorRef,
+            archiveConfigs: Seq[MediaArchiveConfig]): Props =
+    Props(classOf[ArchiveGroupActorImpl], mediaUnionActor, metaDataUnionActor, archiveConfigs,
+      GroupScanStateServiceImpl)
+
+  private class ArchiveGroupActorImpl(mediaUnionActor: ActorRef, metaDataUnionActor: ActorRef,
+                                      archiveConfigs: Seq[MediaArchiveConfig],
+                                      private val scanStateService: GroupScanStateService)
+    extends ArchiveGroupActor(mediaUnionActor, metaDataUnionActor, archiveConfigs, scanStateService)
+      with ArchiveActorFactory with ChildActorFactory
+
+}
+
+/**
+  * An actor that manages media archives that belong to a group.
+  *
+  * An instance is configured with the configurations of the archives it has to
+  * manage. For each configuration, an archive is started (i.e. the
+  * corresponding actors are created, and an initial scan operation is
+  * triggered).
+  *
+  * In addition, it makes sure that scan operations of the managed archives are
+  * coordinated: Only a single scan operation can be active at a given time for
+  * all of the archives that belong to the group.
+  *
+  * @param mediaUnionActor    the media actor of the union archive
+  * @param metaDataUnionActor the meta data actor of the union archive
+  * @param archiveConfigs     the configurations of the archives in the group
+  * @param scanStateService   the service to manage the scan state
+  */
+class ArchiveGroupActor(mediaUnionActor: ActorRef, metaDataUnionActor: ActorRef,
+                        archiveConfigs: Seq[MediaArchiveConfig],
+                        private val scanStateService: GroupScanStateService) extends Actor {
+  this: ArchiveActorFactory =>
+
+  /** The current scan state of the archive group. */
+  private var scanState = GroupScanStateServiceImpl.InitialState
+
+  /**
+    * @inheritdoc This implementation creates the actors for the archives in
+    *             the group and triggers an initial media scan.
+    */
+  override def preStart(): Unit = {
+    super.preStart()
+
+    archiveConfigs map { config =>
+      createArchiveActors(mediaUnionActor, metaDataUnionActor, self, config)
+    } foreach (_ ! ScanAllMedia)
+  }
+
+  override def receive: Receive = {
+    case ScanAllMedia =>
+      updateState(scanStateService.handleScanRequest(sender()))
+
+    case MediaManagerActor.MediaScanCompleted =>
+      updateState(scanStateService.handleScanCompleted())
+  }
+
+  /**
+    * Updates the internal group scan state based on the passed in update
+    * object. Sends new ''StartMediaScan'' messages as necessary.
+    *
+    * @param update the update object
+    */
+  private def updateState(update: GroupScanStateServiceImpl.StateUpdate[Option[ActorRef]]): Unit = {
+    val (next, target) = update(scanState)
+    target foreach (_ ! MediaManagerActor.StartMediaScan)
+    scanState = next
+  }
+}
---
+++ mediaArchive/archive/src/test/scala/de/oliver_heger/linedj/archive/group/ArchiveGroupActorSpec.scala
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2015-2019 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.group
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import de.oliver_heger.linedj.StateTestHelper
+import de.oliver_heger.linedj.archive.config.MediaArchiveConfig
+import de.oliver_heger.linedj.archive.media.MediaManagerActor
+import de.oliver_heger.linedj.shared.archive.media.ScanAllMedia
+import de.oliver_heger.linedj.utils.ChildActorFactory
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import org.scalatestplus.mockito.MockitoSugar
+
+/**
+  * Test class for ''ArchiveGroupActor''.
+  */
+class ArchiveGroupActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with ImplicitSender
+  with FlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  def this() = this(ActorSystem("ArchiveGroupActorSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  "An ArchiveGroupActor" should "return correct Props" in {
+    val mediaUnionActor = TestProbe().ref
+    val metaDataUnionActor = TestProbe().ref
+    val archiveConfigs = List(mock[MediaArchiveConfig], mock[MediaArchiveConfig])
+
+    val props = ArchiveGroupActor(mediaUnionActor, metaDataUnionActor, archiveConfigs)
+    classOf[ChildActorFactory].isAssignableFrom(props.actorClass()) shouldBe true
+    classOf[ArchiveActorFactory].isAssignableFrom(props.actorClass()) shouldBe true
+    classOf[ArchiveGroupActor].isAssignableFrom(props.actorClass()) shouldBe true
+    props.args should have size 4
+    props.args.head should be(mediaUnionActor)
+    props.args(1) should be(metaDataUnionActor)
+    props.args(2) should be(archiveConfigs)
+    props.args(3) should be(GroupScanStateServiceImpl)
+  }
+
+  it should "create and initialize the archives in the group" in {
+    val helper = new GroupActorTestHelper
+
+    helper.verifyArchiveInitialization()
+  }
+
+  it should "handle a media scan request from the group" in {
+    val probeTarget = TestProbe()
+    val state = GroupScanState(currentScanRequest = Some(probeTarget.ref), pendingScanRequests = Set.empty,
+      scanInProgress = false)
+    val helper = new GroupActorTestHelper
+
+    helper.stub(Option(probeTarget.ref), state)(_.handleScanRequest(testActor))
+      .post(ScanAllMedia)
+      .expectStateUpdate(GroupScanStateServiceImpl.InitialState)
+    probeTarget.expectMsg(MediaManagerActor.StartMediaScan)
+  }
+
+  it should "handle a media scan request if no scan should be triggered" in {
+    val state1 = GroupScanState(currentScanRequest = None, pendingScanRequests = Set.empty,
+      scanInProgress = true)
+    val state2 = GroupScanState(currentScanRequest = None, pendingScanRequests = Set(TestProbe().ref),
+      scanInProgress = true)
+    val sender2 = TestProbe().ref
+    val helper = new GroupActorTestHelper
+
+    helper.stub(Option(TestProbe().ref), state1)(_.handleScanRequest(testActor))
+      .stub(Option[ActorRef](null), state2)(_.handleScanRequest(sender2))
+      .post(ScanAllMedia)
+      .expectStateUpdate(GroupScanStateServiceImpl.InitialState)
+      .post(ScanAllMedia, sender2)
+      .expectStateUpdate(state1)
+  }
+
+  it should "handle a notification about a completed scan operation" in {
+    val probeTarget = TestProbe()
+    val state = GroupScanState(currentScanRequest = Some(probeTarget.ref), pendingScanRequests = Set.empty,
+      scanInProgress = false)
+    val helper = new GroupActorTestHelper
+
+    helper.stub(Option(probeTarget.ref), state)(_.handleScanCompleted())
+      .post(MediaManagerActor.MediaScanCompleted)
+      .expectStateUpdate(GroupScanStateServiceImpl.InitialState)
+    probeTarget.expectMsg(MediaManagerActor.StartMediaScan)
+  }
+
+  /**
+    * Test helper class that manages a test actor and its dependencies.
+    */
+  private class GroupActorTestHelper extends StateTestHelper[GroupScanState, GroupScanStateService] {
+    override val updateService: GroupScanStateService = mock[GroupScanStateService]
+
+    /** The media manager of the union archive. */
+    private val mediaUnionActor = TestProbe().ref
+
+    /** The meta data manager of the union archive. */
+    private val metaDataUnionActor = TestProbe().ref
+
+    /** A list with the configs for the archives in the group. */
+    private val archiveConfigs = List(mock[MediaArchiveConfig], mock[MediaArchiveConfig])
+
+    /**
+      * The test probes for the media manager actors of the archives in the
+      * group.
+      */
+    private val mediaManagers = List(TestProbe(), TestProbe())
+
+    /** The actor to be tested. */
+    private val groupActor = createTestActor()
+
+    /**
+      * Verifies that all archives have been created and initialized.
+      */
+    def verifyArchiveInitialization(): Unit = {
+      mediaManagers foreach { manager =>
+        manager.expectMsg(ScanAllMedia)
+      }
+    }
+
+    /**
+      * Passes the given message to the group test actor.
+      *
+      * @param msg    the message
+      * @param caller the actor that sends the message
+      * @return this test helper
+      */
+    def post(msg: Any, caller: ActorRef = testActor): GroupActorTestHelper = {
+      groupActor.tell(msg, caller)
+      this
+    }
+
+    /**
+      * Creates the actor to be tested. It uses a mock archive actor factory to
+      * inject test probes as media manager actors.
+      *
+      * @return the test actor
+      */
+    private def createTestActor(): ActorRef =
+      system.actorOf(Props(new ArchiveGroupActor(mediaUnionActor, metaDataUnionActor, archiveConfigs,
+        updateService) with ArchiveActorFactory with ChildActorFactory {
+        override def createArchiveActors(refMediaUnionActor: ActorRef, refMetaDataUnionActor: ActorRef,
+                                         groupManager: ActorRef, archiveConfig: MediaArchiveConfig): ActorRef = {
+          refMediaUnionActor should be(mediaUnionActor)
+          refMetaDataUnionActor should be(metaDataUnionActor)
+          groupManager should be(self)
+          if (archiveConfig == archiveConfigs.head) mediaManagers.head.ref
+          else if (archiveConfig == archiveConfigs(1)) mediaManagers(1).ref
+          else fail("Unexpected archive config: " + archiveConfig)
+        }
+      }))
+  }
+
+}
