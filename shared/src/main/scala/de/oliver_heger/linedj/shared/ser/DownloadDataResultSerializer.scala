188fda64af430c5c36e5379660c34a8f1752e192
oheger <oliver.heger@oliver-heger.de>
2020-05-27 21:33:36 +0200

    Added a special serializer for the DownloadDataResult message.
    The Jackson serializer could not handle this message, due to the
    ByteString data field. The new serializer can handle this, by just
    using the byte array from the ByteString as the binary representation.

---
+++ shared/src/main/scala/de/oliver_heger/linedj/shared/ser/DownloadDataResultSerializer.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2015-2020 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.shared.ser
+
+import akka.serialization.Serializer
+import akka.util.ByteString
+import de.oliver_heger.linedj.shared.archive.media.DownloadDataResult
+
+/**
+  * A special ''Serializer'' implementation for the [[DownloadDataResult]]
+  * message type.
+  *
+  * This type cannot be handled by the default Jackson serializer; therefore,
+  * we provide a custom serializer, which can handle the ''ByteString'' in the
+  * message effectively.
+  */
+class DownloadDataResultSerializer extends Serializer {
+  override def identifier: Int = 10001
+
+  override def includeManifest: Boolean = false
+
+  /**
+    * This implementation can only handle ''DownloadDataResult'' objects.
+    * Their binary representation consists of the content of their
+    * ''ByteString'' property.
+    *
+    * @param o the object to be serialized
+    * @return the serialized representation of the object
+    */
+  override def toBinary(o: AnyRef): Array[Byte] =
+    o.asInstanceOf[DownloadDataResult].data.toArray
+
+  /**
+    * This implementation creates a new ''DownloadDataResult'' object with the
+    * data from the given array.
+    *
+    * @param bytes    the binary object representation
+    * @param manifest the manifest (ignored)
+    * @return the newly created instance
+    */
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef =
+    DownloadDataResult(ByteString(bytes))
+}
---
+++ shared/src/test/scala/de/oliver_heger/linedj/shared/ser/DownloadDataResultSerializerSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015-2020 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.shared.ser
+
+import akka.util.ByteString
+import de.oliver_heger.linedj.FileTestHelper
+import de.oliver_heger.linedj.shared.archive.media.DownloadDataResult
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Test class for ''DownloadDataResultSerializer''.
+  */
+class DownloadDataResultSerializerSpec extends FlatSpec with Matchers {
+  "DownloadDataResultSerializer" should "report a valid identifier" in {
+    val serializer = new DownloadDataResultSerializer
+
+    serializer.identifier should be > 40 // 0 - 40 is reserved
+  }
+
+  it should "not need a manifest" in {
+    val serializer = new DownloadDataResultSerializer
+
+    serializer.includeManifest shouldBe false
+  }
+
+  it should "convert a message to binary" in {
+    val data = FileTestHelper.testBytes()
+    val msg = DownloadDataResult(ByteString(data))
+    val serializer = new DownloadDataResultSerializer
+
+    serializer.toBinary(msg) should be(FileTestHelper.testBytes())
+  }
+
+  it should "deserialize a message" in {
+    val serializer = new DownloadDataResultSerializer
+
+    val msg = serializer.fromBinary(FileTestHelper.testBytes()).asInstanceOf[DownloadDataResult]
+    msg.data.utf8String should be(FileTestHelper.TestData)
+  }
+}
