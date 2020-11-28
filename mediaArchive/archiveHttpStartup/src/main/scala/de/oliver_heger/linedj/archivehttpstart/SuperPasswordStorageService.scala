3ef3924a2fbf140873be721b5563b71ab0d0958d
oheger <oliver.heger@oliver-heger.de>
2020-11-28 17:15:50 +0100

    Implemented SuperPasswordStorageService.
    The service can now read and write the file containing passwords and
    credentials for the archives managed.

--- mediaArchive/archiveHttpStartup/src/main/scala/de/oliver_heger/linedj/archivehttpstart/SuperPasswordStorageService.scala
+++ mediaArchive/archiveHttpStartup/src/main/scala/de/oliver_heger/linedj/archivehttpstart/SuperPasswordStorageService.scala
@@ -16,14 +16,21 @@
 
 package de.oliver_heger.linedj.archivehttpstart
 
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 import java.nio.file.Path
-import java.security.Key
+import java.security.{Key, SecureRandom}
+import java.util.Base64
 
+import akka.NotUsed
 import akka.actor.ActorSystem
+import akka.stream.{IOOperationIncompleteException, IOResult}
+import akka.stream.scaladsl.{Concat, FileIO, Flow, Framing, Keep, Sink, Source}
+import akka.util.ByteString
 import de.oliver_heger.linedj.archivehttp.config.UserCredentials
-import de.oliver_heger.linedj.crypt.KeyGenerator
+import de.oliver_heger.linedj.crypt.{CryptStage, KeyGenerator, Secret}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Definition of a service for reading and writing the encrypted file with
@@ -69,4 +76,191 @@ trait SuperPasswordStorageService {
     */
   def readSuperPasswordFile(target: Path, keyGenerator: KeyGenerator, superPassword: String)
                            (implicit system: ActorSystem): Future[Iterable[ArchiveStateChangedMessage]]
+}
+
+/**
+  * The implementation of the [[SuperPasswordStorageService]] trait.
+  */
+object SuperPasswordStorageServiceImpl extends SuperPasswordStorageService {
+  /** Prefix for an entry with login information in the password file. */
+  private val LoginEntry = "LOGIN"
+
+  /** Prefix for an entry to unlock an archive in the password file. */
+  private val UnlockEntry = "UNLOCK"
+
+  /** The separator between two entries in the password file. */
+  private val EntrySeparator = "\n"
+
+  override def writeSuperPasswordFile(target: Path, keyGenerator: KeyGenerator, superPassword: String,
+                                      realms: Iterable[(String, UserCredentials)],
+                                      lockData: Iterable[(String, Key)])
+                                     (implicit system: ActorSystem): Future[Path] = {
+    val loginEntries = realms map realmDataToString
+    val unlockEntries = lockData map { lock =>
+      lockDataToString(keyGenerator, lock)
+    }
+    val source = Source.combine(Source(loginEntries.toList), Source(unlockEntries.toList))(Concat(_))
+    val sink = encryptSink(target, keyGenerator, superPassword)
+
+    implicit val ec: ExecutionContext = system.dispatcher
+    source
+      .map(ByteString(_))
+      .runWith(sink)
+      .map(_ => target)
+      .recoverWith {
+        case e: IOOperationIncompleteException =>
+          Future.failed(new IOException(e))
+      }(system.dispatcher)
+  }
+
+  override def readSuperPasswordFile(target: Path, keyGenerator: KeyGenerator, superPassword: String)
+                                    (implicit system: ActorSystem): Future[Iterable[ArchiveStateChangedMessage]] = {
+    val source = decryptSource(target, keyGenerator, superPassword)
+    val sink = Sink.fold[List[ArchiveStateChangedMessage],
+      ArchiveStateChangedMessage](List.empty[ArchiveStateChangedMessage]) { (lst, msg) => msg :: lst }
+    source.map { line =>
+      val fields = line.utf8String.split(",")
+      fields.head match {
+        case LoginEntry =>
+          createLoginStateChanged(fields)
+        case UnlockEntry =>
+          createLockStateChanged(keyGenerator, fields)
+        case e => throw new IllegalStateException("Unsupported entry: " + e)
+      }
+    }.runWith(sink)
+  }
+
+  /**
+    * Generates a sink that expects ''ByteString'' objects and writes them in
+    * encrypted form to a file. (This is package local for better testability.)
+    *
+    * @param target        path to the file to be written
+    * @param keyGenerator  the key generator
+    * @param superPassword the password to encrypt the file
+    * @return the ''Sink'' for writing encrypted strings to the file
+    */
+  private[archivehttpstart] def encryptSink(target: Path, keyGenerator: KeyGenerator, superPassword: String):
+  Sink[ByteString, Future[IOResult]] =
+    Flow[ByteString].via(CryptStage.encryptStage(keyGenerator.generateKey(superPassword), new SecureRandom))
+      .map { bs =>
+        ByteString(Base64.getEncoder.encodeToString(bs.toArray) + EntrySeparator)
+      }.toMat(FileIO.toPath(target))(Keep.right)
+
+  /**
+    * Generates a source for reading the lines of the encrypted password file.
+    * The source emits the decrypted lines.
+    *
+    * @param target        path to the file to read
+    * @param keyGenerator  the key generator
+    * @param superPassword the password to decrypt the file
+    * @return the ''Source'' to read and decrypt the file
+    */
+  private[archivehttpstart] def decryptSource(target: Path, keyGenerator: KeyGenerator, superPassword: String):
+  Source[ByteString, NotUsed] =
+    FileIO.fromPath(target)
+      .via(Framing.delimiter(ByteString(EntrySeparator), 1024))
+      .map { line =>
+        ByteString(decodeBase64(line.toArray))
+      }
+      .viaMat(CryptStage.decryptStage(keyGenerator.generateKey(superPassword), new SecureRandom))(Keep.right)
+
+  /**
+    * Converts the given data for a realm to a string to be written into the
+    * password file.
+    *
+    * @param realm the tuple with realm information
+    * @return the string for this realm
+    */
+  private def realmDataToString(realm: (String, UserCredentials)): String = {
+    val realmEnc = encodeBase64(realm._1)
+    val userEnc = encodeBase64(realm._2.userName)
+    val pwdEnc = encodeBase64(realm._2.password.secret)
+    s"$LoginEntry,$realmEnc,$userEnc,$pwdEnc"
+  }
+
+  /**
+    * Converts the given data for unlocking an archive to a string to be
+    * written to the password file.
+    *
+    * @param keyGenerator the key generator
+    * @param lock         the tuple with information about unlocking an archive
+    * @return
+    */
+  private def lockDataToString(keyGenerator: KeyGenerator, lock: (String, Key)): String = {
+    val archiveEnc = encodeBase64(lock._1)
+    val keyEnc = new String(keyGenerator.encodeKey(lock._2), StandardCharsets.UTF_8)
+    s"$UnlockEntry,$archiveEnc,$keyEnc"
+  }
+
+  /**
+    * Creates a ''LoginStateChanged'' message from the values provided.
+    *
+    * @param fields the array with the data
+    * @return the resulting ''LoginStateChanged''
+    */
+  private def createLoginStateChanged(fields: Array[String]): LoginStateChanged = {
+    checkFieldCount(fields, 4)
+    val realmDec = decodeBase64Str(fields(1))
+    val userDec = decodeBase64Str(fields(2))
+    val pwdDec = decodeBase64Str(fields(3))
+    LoginStateChanged(realmDec, Some(UserCredentials(userDec, Secret(pwdDec))))
+  }
+
+  /**
+    * Create a ''LockStateChanged'' message from the values provided.
+    *
+    * @param keyGenerator the key generator
+    * @param fields       the array with the data
+    * @return the resulting ''LockStateChanged''
+    */
+  private def createLockStateChanged(keyGenerator: KeyGenerator, fields: Array[String]): LockStateChanged = {
+    checkFieldCount(fields, 3)
+    val archiveDec = decodeBase64Str(fields(1))
+    LockStateChanged(archiveDec, Some(keyGenerator.decodeKey(fields(2).getBytes(StandardCharsets.UTF_8))))
+  }
+
+  /**
+    * Checks whether the given array has the expected number of elements.
+    * Throws an exception if this is not the case.
+    *
+    * @param array    the array to check
+    * @param expCount the expected number of elements
+    */
+  private def checkFieldCount(array: Array[String], expCount: Int): Unit = {
+    if (array.length != expCount) {
+      throw new IllegalStateException(s"Expected $expCount fields in line, but got ${array.length}.")
+    }
+  }
+
+  /**
+    * Decodes data with the Base64 decoder with error handling.
+    *
+    * @param data the data to be decoded
+    * @return the decoded data
+    */
+  private def decodeBase64(data: Array[Byte]): Array[Byte] =
+    try {
+      Base64.getDecoder.decode(data)
+    } catch {
+      case e: IllegalArgumentException =>
+        throw new IllegalStateException("Invalid file content: Not Base64-encoded.", e)
+    }
+
+  /**
+    * Convenience function to decode a Base64 string.
+    *
+    * @param s the string to decode
+    * @return the decoded string
+    */
+  private def decodeBase64Str(s: String): String =
+    new String(decodeBase64(s.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8)
+
+  /**
+    * Convenience function to encode a string in Base64.
+    *
+    * @param s the string to encode
+    * @return the encoded string
+    */
+  private def encodeBase64(s: String): String =
+    Base64.getEncoder.encodeToString(s.getBytes(StandardCharsets.UTF_8))
 }
---
+++ mediaArchive/archiveHttpStartup/src/test/scala/de/oliver_heger/linedj/archivehttpstart/SuperPasswordStorageServiceSpec.scala
@@ -0,0 +1,358 @@
+/*
+ * Copyright 2015-2020 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart
+
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Path, Paths}
+import java.security.Key
+import java.util.Base64
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.Source
+import akka.testkit.TestKit
+import akka.util.ByteString
+import de.oliver_heger.linedj.{AsyncTestHelper, FileTestHelper}
+import de.oliver_heger.linedj.archivehttp.config.UserCredentials
+import de.oliver_heger.linedj.crypt.{AESKeyGenerator, Secret}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.Future
+
+object SuperPasswordStorageServiceSpec {
+  /** The password for crypt operations. */
+  private val SuperPassword = "SuperDuperPwd!"
+
+  /** Prefix for test realm names. */
+  private val PrefixRealm = "realm"
+
+  /** Prefix for test user names. */
+  private val PrefixUser = "TestUser"
+
+  /** Prefix for test passwords. */
+  private val PrefixPassword = "_Secret_"
+
+  /** Prefix for test archives. */
+  private val PrefixArchive = "Archive"
+
+  /** Prefix for passwords used for encryption. */
+  private val PrefixCryptPassword = "crypt_"
+
+  /** The object to generate keys. */
+  private val keyGenerator = new AESKeyGenerator
+
+  /**
+    * Generates string-based test data with a prefix and an index.
+    *
+    * @param prefix the prefix
+    * @param index  the index
+    * @return the resulting test data
+    */
+  private def generate(prefix: String, index: Int): String = prefix + index
+
+  /**
+    * Generates test user credentials with the given index.
+    *
+    * @param index the index
+    * @return test user credentials with this index
+    */
+  private def generateCredentials(index: Int): UserCredentials =
+    UserCredentials(generate(PrefixUser, index), Secret(generate(PrefixPassword, index)))
+
+  /**
+    * Generates a test key with the given index.
+    *
+    * @param index the index
+    * @return the test key with this index
+    */
+  private def generateKey(index: Int): Key =
+    keyGenerator.generateKey(generate(PrefixCryptPassword, index))
+
+  /**
+    * Generates a sequence of test data with a given length using the generator
+    * function provided.
+    *
+    * @param count the number of elements to generate
+    * @param gen   the generator function
+    * @tparam A the type of elements to generate
+    * @return the sequence with generated test data
+    */
+  private def generateSequence[A](count: Int)(gen: Int => A): IndexedSeq[A] =
+    (1 to count) map gen
+
+  /**
+    * Generates a sequence with the given number of test realms with user
+    * credentials.
+    *
+    * @param count the number of elements to generate
+    * @return the sequence with test realms
+    */
+  private def generateTestRealms(count: Int): IndexedSeq[(String, UserCredentials)] =
+    generateSequence(count) { index =>
+      (generate(PrefixRealm, index), generateCredentials(index))
+    }
+
+  /**
+    * Generates a sequence with the given number of test archives and their
+    * encryption passwords.
+    *
+    * @param count the number of elements to generate
+    * @return the sequence with test archives
+    */
+  private def generateLockData(count: Int): IndexedSeq[(String, Key)] =
+    generateSequence(count) { index =>
+      (generate(PrefixArchive, index), generateKey(index))
+    }
+
+  /**
+    * Generates a sequence with messages to log into test realms.
+    *
+    * @param count the number of elements to generate
+    * @return the sequence with login messages
+    */
+  private def generateLoginMessages(count: Int): IndexedSeq[LoginStateChanged] =
+    generateTestRealms(count) map { t =>
+      LoginStateChanged(t._1, Some(t._2))
+    }
+
+  /**
+    * Generates a sequence with messages to unlock encrypted archives.
+    *
+    * @param count the number of elements to generate
+    * @return the sequence with unlock messages
+    */
+  private def generateUnlockMessages(count: Int): IndexedSeq[LockStateChanged] =
+    generateLockData(count) map { t =>
+      LockStateChanged(t._1, Some(t._2))
+    }
+
+  /**
+    * Generates a sequence with the given numbers of login and unlock messages.
+    *
+    * @param loginCount  the number of login messages
+    * @param unlockCount the number of unlock messages
+    * @return the sequence with these messages
+    */
+  private def generateStateMessages(loginCount: Int, unlockCount: Int): IndexedSeq[ArchiveStateChangedMessage] =
+    generateLoginMessages(loginCount) ++ generateUnlockMessages(unlockCount)
+
+  /**
+    * Transforms the given list of state messages to a sequence that can be
+    * compared. This is unfortunately not possible with the original messages
+    * because the [[Secret]] class has no implementation of ''equals()''.
+    * Therefore, messages containing a secret are converted to strings.
+    *
+    * @param messages the list of messages
+    * @return the transformed list
+    */
+  private def comparableMessages(messages: Iterable[ArchiveStateChangedMessage]): Iterable[Any] =
+    messages map {
+      case LoginStateChanged(realm, Some(credentials)) =>
+        s"LoginStateChanged($realm, {${credentials.userName}, ${credentials.password.secret})"
+      case m => m
+    }
+}
+
+/**
+  * Test class for ''SuperPasswordStorageService''.
+  */
+class SuperPasswordStorageServiceSpec(testSystem: ActorSystem) extends TestKit(testSystem) with AnyFlatSpecLike
+  with BeforeAndAfterAll with BeforeAndAfterEach with Matchers with FileTestHelper with AsyncTestHelper {
+  def this() = this(ActorSystem("SuperPasswordStorageServiceSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  override protected def afterEach(): Unit = {
+    tearDownTestFile()
+    super.afterEach()
+  }
+
+  import SuperPasswordStorageServiceSpec._
+  import system.dispatcher
+
+  /**
+    * Invokes the service under test to write the given test data into a new
+    * temporary file.
+    *
+    * @param realms   the test realms
+    * @param lockData the test lock data
+    * @param optPath  optional target path; is generated if None
+    * @return a future with the path to the file that was written
+    */
+  private def writeFile(realms: Iterable[(String, UserCredentials)], lockData: Iterable[(String, Key)],
+                        optPath: Option[Path] = None): Future[Path] = {
+    val path = optPath.getOrElse(createFileReference())
+    SuperPasswordStorageServiceImpl.writeSuperPasswordFile(path, keyGenerator, SuperPassword,
+      realms, lockData)
+  }
+
+  /**
+    * Invokes the service under test to generate an encrypted file with the
+    * lines produced by the passed in source. Using this function allows
+    * testing files with arbitrary content.
+    *
+    * @param data the data source for the file to write
+    * @return the path to the file that was written
+    */
+  private def writeFileWithContent(data: Source[ByteString, Any]): Path = {
+    val path = createFileReference()
+    futureResult(data.runWith(SuperPasswordStorageServiceImpl.encryptSink(path, keyGenerator,
+      SuperPassword)).map(_ => path))
+  }
+
+  /**
+    * Invokes the service under test to read the file with the given path.
+    *
+    * @param target the path of the test file
+    * @return the future with the messages that have been loaded
+    */
+  private def readFile(target: Path): Future[Iterable[ArchiveStateChangedMessage]] =
+    SuperPasswordStorageServiceImpl.readSuperPasswordFile(target, keyGenerator, SuperPassword)
+
+  "SuperPasswordStorageServiceImpl" should "support a round-trip with the super password file" in {
+    val futMessages = for {
+      path <- writeFile(generateTestRealms(4), generateLockData(6))
+      msg <- readFile(path)
+    } yield msg
+
+    val messages = comparableMessages(futureResult(futMessages))
+    messages should contain theSameElementsAs comparableMessages(generateStateMessages(4, 6))
+  }
+
+  it should "encrypt the password file" in {
+    val realms = generateTestRealms(8)
+    val lockData = generateLockData(12)
+
+    val path1 = futureResult(writeFile(realms, lockData))
+    val path2 = futureResult(writeFile(realms, lockData))
+    val data1 = readDataFile(path1)
+    val data2 = readDataFile(path2)
+    data1 should not be data2
+  }
+
+  it should "handle an invalid entry" in {
+    val line = ByteString("invalid,foo,bar\n")
+    val path = writeFileWithContent(Source.single(line))
+
+    expectFailedFuture[IllegalStateException](readFile(path))
+  }
+
+  it should "handle a login entry with an unexpected number of fields" in {
+    val line = ByteString("LOGIN,realm,user,password,additional")
+    val path = writeFileWithContent(Source.single(line))
+
+    expectFailedFuture[IllegalStateException](readFile(path))
+  }
+
+  it should "handle an unlock entry with an unexpected number of fields" in {
+    val line = ByteString("UNLOCK,archive")
+    val path = writeFileWithContent(Source.single(line))
+
+    expectFailedFuture[IllegalStateException](readFile(path))
+  }
+
+  it should "handle a file that is not correctly encoded" in {
+    val path = createDataFile()
+
+    expectFailedFuture[IllegalStateException](readFile(path))
+  }
+
+  it should "handle a file that is not correctly encrypted" in {
+    val data =
+      Base64.getEncoder.encode("This is Base64-encoded, but not encrypted.\n".getBytes(StandardCharsets.UTF_8))
+    val line = ByteString(data)
+    val path = writeFileWithContent(Source.single(line))
+
+    expectFailedFuture[IllegalStateException](readFile(path))
+  }
+
+  /**
+    * Checks whether separator characters in specific strings are correctly
+    * encoded.
+    *
+    * @param realmName the realm name
+    * @param user      the user name
+    * @param password  the password
+    */
+  private def checkLoginDecoding(realmName: String, user: String, password: String): Unit = {
+    val credentials = UserCredentials(user, Secret(password))
+    val realms = generateTestRealms(3) ++ IndexedSeq((realmName, credentials))
+    val expMessages = generateStateMessages(3, 0) ++
+      IndexedSeq(LoginStateChanged(realmName, Some(credentials)))
+    val futMessages = for {
+      path <- writeFile(realms, List.empty)
+      msg <- readFile(path)
+    } yield msg
+
+    val messages = comparableMessages(futureResult(futMessages))
+    messages should contain theSameElementsAs comparableMessages(expMessages)
+  }
+
+  it should "handle a separator character in a user name" in {
+    checkLoginDecoding(PrefixRealm, "strange,user", "a_secret")
+  }
+
+  it should "handle a separator character in a password" in {
+    checkLoginDecoding(PrefixRealm, "theUser", "a,secret")
+  }
+
+  it should "handle a separator character in a realm name" in {
+    checkLoginDecoding("oh,my realm", PrefixUser, PrefixPassword)
+  }
+
+  it should "handle a separator character in an archive name" in {
+    val archiveName = "oh,my-archive"
+    val archiveKey = generateKey(42)
+    val lockData = generateLockData(3) ++ IndexedSeq((archiveName, archiveKey))
+    val expMessages = generateStateMessages(0, 3) ++
+      IndexedSeq(LockStateChanged(archiveName, Some(archiveKey)))
+    val futMessages = for {
+      path <- writeFile(List.empty, lockData)
+      msg <- readFile(path)
+    } yield msg
+
+    val messages = futureResult(futMessages)
+    messages should contain theSameElementsAs expMessages
+  }
+
+  it should "handle a non-existing file when reading" in {
+    expectFailedFuture[IOException](readFile(Paths.get("non-existing-file")))
+  }
+
+  it should "handle a non-existing directory when writing" in {
+    val target = Paths.get("path", "to", "non", "existing", "file")
+    val futWrite = writeFile(generateTestRealms(1), generateLockData(1), Some(target))
+
+    expectFailedFuture[IOException](futWrite)
+  }
+
+  it should "override an existing super password file" in {
+    val target = createDataFile()
+    val futMessages = for {
+      path <- writeFile(generateTestRealms(1), generateLockData(1), Some(target))
+      msg <- readFile(path)
+    } yield msg
+
+    val messages = comparableMessages(futureResult(futMessages))
+    messages should contain theSameElementsAs comparableMessages(generateStateMessages(1, 1))
+  }
+}
