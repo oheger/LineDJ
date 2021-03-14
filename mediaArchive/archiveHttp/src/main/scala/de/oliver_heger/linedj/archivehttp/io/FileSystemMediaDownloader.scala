25dc7195f8e49890a26b37c7f41295fc119cf4e0
oheger <oliver.heger@oliver-heger.de>
2021-03-14 18:58:38 +0100

    Added FileSystemMediaDownloader class.
    This is an implementation of the MediaDownloaderTrait making use of a
    FileSystem from the CloudFiles library. Thus, this implementation can
    deal with different protocols out of the box - only a proper
    FileSystem implementation has to be provided.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/io/FileSystemMediaDownloader.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.io
+
+import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.http.scaladsl.model.Uri
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import com.github.cloudfiles.core.FileSystem
+import com.github.cloudfiles.core.http.HttpRequestSender
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * An implementation of the [[MediaDownloader]] trait that is based on a
+  * ''FileSystem''.
+  *
+  * This implementation uses the ''FileSystem'' and the HTTP request sender
+  * actor passed to the constructor to download media files from HTTP archives.
+  * The URIs passed to the ''downloadMediaFile()'' function are interpreted as
+  * relative paths to the files (based on the file system's root). These paths
+  * are resolved, and the resulting files are downloaded.
+  * @param fileSystem the ''FileSystem'' to access the HTTP archive
+  * @param httpSender the actor for sending HTTP requests
+  *                   @param rootPath the root path of the file system
+  * @param system the actor system
+  * @tparam ID the type of IDs in the ''FileSystem''
+  */
+class FileSystemMediaDownloader[ID](val fileSystem: FileSystem[ID, _, _, _],
+                                    val httpSender: ActorRef[HttpRequestSender.HttpCommand],
+                                    val rootPath: String)
+                                   (implicit system: ActorSystem[_]) extends MediaDownloader {
+  override def downloadMediaFile(uri: Uri): Future[Source[ByteString, Any]] = {
+    implicit val ec: ExecutionContext = system.executionContext
+    val op = for {
+      id <- fileSystem.resolvePath(fileSystemPath(uri))
+      entity <- fileSystem.downloadFile(id)
+    } yield entity.dataBytes
+
+    op.run(httpSender)
+  }
+
+  /**
+    * Extracts the path to resolve from the given URI. If the URI's path starts
+    * with the configured root path, this prefix is stripped.
+    * @param uri the URI to resolve
+    * @return the path to pass to the file system
+    */
+  private def fileSystemPath(uri: Uri): String = {
+    val uriPath = uri.path.toString()
+    if(uriPath.startsWith(rootPath)) uriPath.substring(rootPath.length)
+    else uriPath
+  }
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/io/FileSystemMediaDownloaderSpec.scala
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2015-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.io
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, Uri}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import com.github.cloudfiles.core.http.HttpRequestSender
+import com.github.cloudfiles.core.{FileSystem, Model}
+import de.oliver_heger.linedj.{AsyncTestHelper, FileTestHelper}
+import org.mockito.Mockito._
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import scala.concurrent.Future
+
+object FileSystemMediaDownloaderSpec {
+  /** The ID of the test file to be loaded. */
+  private val FileID = "fileToDownload"
+
+  /** The root path of the file system. */
+  private val RootPath = "/music/archive"
+
+  /**
+    * Generates a source that reflects the content of the file to download.
+    * @return the test source
+    */
+  private def fileSource(): Source[ByteString, Any] =
+    Source.single(ByteString(FileTestHelper.TestData))
+}
+
+/**
+  * Test class for ''FileSystemMediaDownloader''.
+  */
+class FileSystemMediaDownloaderSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers
+  with MockitoSugar with AsyncTestHelper {
+
+  import FileSystemMediaDownloaderSpec._
+
+  "FileSystemMediaDownloader" should "resolve and download a URI from the configured file system" in {
+    val DownloadUri = Uri("https://archive.example.org/path/to/file.mp3")
+    val FileSource = fileSource()
+    val entity = HttpEntity(ContentTypes.`application/octet-stream`, FileTestHelper.TestData.length, FileSource)
+    val helper = new DownloaderTestHelper
+
+    helper.prepareFileSystem { fs =>
+      when(fs.resolvePath(DownloadUri.path.toString())).thenReturn(helper.stubOperation(FileID))
+      doReturn(helper.stubOperation(entity)).when(fs).downloadFile(FileID)
+    }.invokeDownloader(DownloadUri, FileSource)
+  }
+
+  it should "strip the root prefix from a URL" in {
+    val RelativePath = "/my-album/my-song.mp3"
+    val DownloadUri = Uri(s"https://archive.example.org$RootPath$RelativePath")
+    val FileSource = fileSource()
+    val entity = HttpEntity(ContentTypes.`application/octet-stream`, FileTestHelper.TestData.length, FileSource)
+    val helper = new DownloaderTestHelper
+
+    helper.prepareFileSystem { fs =>
+      when(fs.resolvePath(RelativePath)).thenReturn(helper.stubOperation(FileID))
+      doReturn(helper.stubOperation(entity)).when(fs).downloadFile(FileID)
+    }.invokeDownloader(DownloadUri, FileSource)
+  }
+
+  /**
+    * A test helper class managing a test instance and its dependencies.
+    */
+  private class DownloaderTestHelper {
+    /**
+      * A test HTTP sender actor which is required for interactions with a file
+      * system. Note that this actor is not actually invoked; hence the URI
+      * passed to it is irrelevant.
+      */
+    private val httpSender = testKit.spawn(HttpRequestSender("http://www.example.org"))
+
+    /** Mock for the underlying file system. */
+    private val mockFileSystem = mock[FileSystem[String, Model.File[String], Model.Folder[String],
+    Model.FolderContent[String, Model.File[String], Model.Folder[String]]]]
+
+    /** The downloader to be tested. */
+    private val downloader = new FileSystemMediaDownloader(mockFileSystem, httpSender, RootPath)
+
+    /**
+      * Invokes the given function to initialize the mock file system.
+      * @param init the init function
+      * @return this test helper
+      */
+    def prepareFileSystem(init: FileSystem[String, _, _, _] => Unit): DownloaderTestHelper = {
+      init(mockFileSystem)
+      this
+    }
+
+    /**
+      * Returns a stub operation that yields the value provided.
+      * @param result the result value for the operation
+      * @tparam A the type of the result
+      * @return the stub operation yielding this result
+      */
+    def stubOperation[A](result: A): FileSystem.Operation[A] = FileSystem.Operation { sender =>
+      sender should be(httpSender)
+      Future.successful(result)
+    }
+
+    /**
+      * Invokes the test downloader instance with the given URI and checks
+      * whether the expected result is returned.
+      * @param uri the URI to request
+      * @param expResult the expected result
+      * @return this test helper
+      */
+    def invokeDownloader(uri: Uri, expResult: Source[ByteString, Any]): DownloaderTestHelper = {
+      futureResult(downloader.downloadMediaFile(uri)) should be(expResult)
+      this
+    }
+  }
+}
