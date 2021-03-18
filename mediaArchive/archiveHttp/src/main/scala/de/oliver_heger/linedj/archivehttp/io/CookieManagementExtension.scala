9dea7d1dd8f71ab16eda5d668f246aed26cb039a
oheger <oliver.heger@oliver-heger.de>
2021-03-18 21:14:02 +0100

    Reimplemented HttpCookieManagementActor as typesafe extension actor.
    When HTTP communication is done via CloudFiles, the cookie management
    functionality must be implemented based on HttpRequestSender. This is
    what the new class does.

---
+++ mediaArchive/archiveHttp/src/main/scala/de/oliver_heger/linedj/archivehttp/io/CookieManagementExtension.scala
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2015-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.io
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.http.scaladsl.model.headers.{Cookie, HttpCookiePair, `Set-Cookie`}
+import akka.http.scaladsl.model.{HttpHeader, HttpRequest, HttpResponse, StatusCodes}
+import com.github.cloudfiles.core.http.HttpRequestSender
+import com.github.cloudfiles.core.http.HttpRequestSender.{FailedResponseException, FailedResult, ForwardedResult}
+
+/**
+  * An HTTP extension actor implementation that adds a special cookie handling
+  * to support certain proxies.
+  *
+  * This actor has the goal to make HTTP requests sent by the framework
+  * compatible with certain proxy servers. In specific environments the
+  * behavior was observed that a proxy server rejected a valid request with the
+  * status code ''UNAUTHORIZED'', but added a special ''Set-Cookie'' header to
+  * the response. The request was only successful if the cookie was passed in
+  * the header. This kind of cookie management is implemented here.
+  */
+object CookieManagementExtension {
+  /** The name of the header to set cookies. */
+  private val SetCookieHeader = "set-cookie"
+
+  /**
+    * Returns a behavior that adds the described cookie management to the
+    * request sender actor provided.
+    *
+    * @param requestSender the request sender actor to decorate
+    * @return the behavior of the cookie management extension
+    */
+  def apply(requestSender: ActorRef[HttpRequestSender.HttpCommand]): Behavior[HttpRequestSender.HttpCommand] =
+    handleRequests(requestSender, None)
+
+  private def handleRequests(requestSender: ActorRef[HttpRequestSender.HttpCommand], cookieHeader: Option[Cookie]):
+  Behavior[HttpRequestSender.HttpCommand] =
+    Behaviors.receivePartial {
+      case (context, req: HttpRequestSender.SendRequest) =>
+        val updatedRequest = cookieHeader.map(c => addCookieHeader(req, c)) getOrElse req.request
+        HttpRequestSender.forwardRequest(context, requestSender, updatedRequest, req, req.discardEntityMode)
+        Behaviors.same
+
+      case (context, ForwardedResult(result@FailedResult(HttpRequestSender.SendRequest(_,
+      data: HttpRequestSender.SendRequest, _, _), cause: FailedResponseException)))
+        if canRetry(cause.response) =>
+        val newCookies = extractCookies(cause.response)
+        if (cookieHeader.exists(_.cookies == newCookies)) {
+          sendResult(result)
+        } else {
+
+          implicit val mat: ActorSystem[Nothing] = context.system
+          result.ensureResponseEntityDiscarded()
+          val cookie = Cookie(newCookies)
+          context.log.info("Updated cookies to {}.", newCookies)
+          val nextRequest = addCookieHeader(data, cookie)
+          HttpRequestSender.forwardRequest(context, requestSender, nextRequest, data, data.discardEntityMode)
+          handleRequests(requestSender, Some(cookie))
+        }
+
+      case (_, ForwardedResult(result)) =>
+        sendResult(result)
+
+      case (_, HttpRequestSender.Stop) =>
+        requestSender ! HttpRequestSender.Stop
+        Behaviors.stopped
+    }
+
+  private def sendResult(srcResult: HttpRequestSender.Result): Behavior[HttpRequestSender.HttpCommand] = {
+    val result = HttpRequestSender.resultFromForwardedRequest(srcResult)
+    result.request.replyTo ! result
+    Behaviors.same
+  }
+
+  /**
+    * Returns a sequence with all the cookie information defined by the given
+    * response.
+    *
+    * @param response the response
+    * @return a list of all cookies
+    */
+  private def extractCookies(response: HttpResponse): List[HttpCookiePair] =
+    response.headers.foldLeft(List.empty[HttpCookiePair]) { (lst, h) =>
+      h match {
+        case `Set-Cookie`(cookie) => HttpCookiePair(cookie.name, cookie.value) :: lst
+        case _ => lst
+      }
+    }
+
+  /**
+    * Returns a sequence with the headers of the given request that are not
+    * managed by this actor class.
+    *
+    * @param request the request
+    * @return the filtered headers of this request
+    */
+  private def filterHeaders(request: HttpRequest): Seq[HttpHeader] =
+    request.headers.filter {
+      case Cookie(_) => false
+      case _ => true
+    }
+
+  /**
+    * Adds the given cookie header to the request specified. The headers
+    * managed by this actor class are also normalized.
+    *
+    * @param request the request to be manipulated
+    * @param cookie  the cookie header to be added
+    * @return the updated request
+    */
+  private def addCookieHeader(request: HttpRequestSender.SendRequest, cookie: Cookie): HttpRequest = {
+    val nextHeaders = cookie :: filterHeaders(request.request).toList
+    request.request.withHeaders(nextHeaders)
+  }
+
+  /**
+    * Checks whether a failed response can be retried. This is the case for
+    * certain status codes and if it contains cookies.
+    *
+    * @param response the response
+    * @return a flag whether this response can be retried
+    */
+  private def canRetry(response: HttpResponse): Boolean =
+    response.status == StatusCodes.Unauthorized && hasCookies(response)
+
+  /**
+    * Checks whether the given response contains at least one header to set a
+    * cookie.
+    *
+    * @param response the response
+    * @return a flag whether a set-cookie header was found
+    */
+  private def hasCookies(response: HttpResponse): Boolean =
+    response.headers.exists(_.is(SetCookieHeader))
+}
---
+++ mediaArchive/archiveHttp/src/test/scala/de/oliver_heger/linedj/archivehttp/io/CookieManagementExtensionSpec.scala
@@ -0,0 +1,324 @@
+/*
+ * Copyright 2015-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttp.io
+
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, ScalaTestWithActorTestKit}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{Cookie, HttpCookie, HttpCookiePair, `Set-Cookie`}
+import com.github.cloudfiles.core.http.HttpRequestSender
+import com.github.cloudfiles.core.http.HttpRequestSender.DiscardEntityMode.DiscardEntityMode
+import com.github.cloudfiles.core.http.HttpRequestSender.{DiscardEntityMode, FailedResponseException}
+import org.mockito.Mockito.{never, verify}
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+object CookieManagementExtensionSpec {
+  /** A test HTTP request sent to the test actor. */
+  private val TestRequest = HttpRequest(uri = "https://test.example.org/foo")
+
+  /** A data object associated with the test request. */
+  private val RequestData = "someRequestData"
+
+  /** A test cookie. */
+  private val Cookie1 = HttpCookie(name = "cuckoo", value = "beep")
+
+  /** Another test cookie. */
+  private val Cookie2 = HttpCookie(name = "foo", value = "bar", path = Some("/"))
+
+  /**
+    * Creates a failed result object with a response that failed with the status
+    * code provided.
+    *
+    * @param request the request to be answered
+    * @param status  the status code
+    * @param headers headers of the response
+    * @param entity  the response entity
+    * @return the resulting ''FailedResult'' object
+    */
+  private def failedResult(request: HttpRequestSender.SendRequest, status: StatusCode,
+                           headers: List[HttpHeader] = Nil, entity: ResponseEntity = HttpEntity.Empty):
+  HttpRequestSender.FailedResult =
+    HttpRequestSender.FailedResult(request,
+      FailedResponseException(HttpResponse(status = status, headers = headers, entity = entity)))
+
+  /**
+    * Adds the expected cookie header to the given request.
+    *
+    * @param request the request
+    * @return the request with a cookie header
+    */
+  private def withCookie(request: HttpRequest): HttpRequest = {
+    val cookieHeader = Cookie(List(HttpCookiePair(Cookie2.name, Cookie2.value),
+      HttpCookiePair(Cookie1.name, Cookie1.value)))
+    withHeader(request, cookieHeader)
+  }
+
+  /**
+    * Adds the given header to the headers of the passed in request.
+    *
+    * @param request the request
+    * @param header  the header to be added
+    * @return the updated request
+    */
+  private def withHeader(request: HttpRequest, header: HttpHeader): HttpRequest =
+    request.withHeaders(header :: request.headers.toList)
+
+  /**
+    * Returns a list with headers to set the test cookies.
+    *
+    * @return the list with set-cookie headers
+    */
+  private def setCookieHeaders(): List[HttpHeader] =
+    List(Cookie1, Cookie2).map(`Set-Cookie`(_))
+}
+
+/**
+  * Test class for ''CookieManagementExtension''.
+  */
+class CookieManagementExtensionSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers
+  with MockitoSugar {
+
+  import CookieManagementExtensionSpec._
+
+  /**
+    * Checks the data that was passed when forwarding a request.
+    *
+    * @param request the request that was forwarded
+    */
+  private def checkForwardedRequestData(request: HttpRequestSender.SendRequest): Unit = {
+    request.data match {
+      case HttpRequestSender.SendRequest(req, data, _, _) =>
+        req should be(TestRequest)
+        data should be(RequestData)
+      case d => fail("Unexpected request data: " + d)
+    }
+  }
+
+  "CookieManagementExtension" should "handle a successful request" in {
+    val response = HttpResponse(status = StatusCodes.Accepted)
+    val helper = new CookieExtensionTestHelper
+
+    val finalResult = helper.sendRequest()
+      .expectForwardedRequestAndAnswer { request =>
+        request.request should be(TestRequest)
+        request.discardEntityMode should be(DiscardEntityMode.OnFailure)
+        checkForwardedRequestData(request)
+        HttpRequestSender.SuccessResult(request, response)
+      }.expectSuccessResponse()
+    finalResult.request.request should be(TestRequest)
+    finalResult.request.data should be(RequestData)
+    finalResult.response should be(response)
+  }
+
+  it should "propagate the discard mode when forwarding requests" in {
+    val helper = new CookieExtensionTestHelper
+
+    val fwdRequest = helper.sendRequest(discardMode = DiscardEntityMode.Never)
+      .expectForwardedRequest()
+    fwdRequest.discardEntityMode should be(DiscardEntityMode.Never)
+  }
+
+  it should "retry an unauthorized request if there is a cookie from the server" in {
+    val responseEntity = mock[ResponseEntity]
+    val helper = new CookieExtensionTestHelper
+
+    val reqWithCookies = helper.sendRequest()
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders(), entity = responseEntity)
+      }.expectForwardedRequest()
+    reqWithCookies.request should be(withCookie(TestRequest))
+    reqWithCookies.discardEntityMode should be(DiscardEntityMode.OnFailure)
+    checkForwardedRequestData(reqWithCookies)
+    verify(responseEntity, never()).discardBytes()
+  }
+
+  it should "propagate the discard mode when retrying a request" in {
+    val helper = new CookieExtensionTestHelper
+
+    val fwdRequest = helper.sendRequest(discardMode = DiscardEntityMode.Never)
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders())
+      }.expectForwardedRequest()
+    fwdRequest.discardEntityMode should be(DiscardEntityMode.Never)
+  }
+
+  it should "discard entity bytes when retrying a failed request if necessary" in {
+    val responseEntity = mock[ResponseEntity]
+    val helper = new CookieExtensionTestHelper
+
+    helper.sendRequest(discardMode = DiscardEntityMode.Never)
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders(), entity = responseEntity)
+      }.expectForwardedRequest()
+    verify(responseEntity).discardBytes()
+  }
+
+  it should "only retry an unauthorized request once" in {
+    val helper = new CookieExtensionTestHelper
+
+    val finalResult = helper.sendRequest()
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders())
+      }.expectForwardedRequestAndAnswer { fwdRequest =>
+      failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders())
+    }.expectFailureResponseWithStatus(StatusCodes.Unauthorized)
+    finalResult.cause.asInstanceOf[FailedResponseException].response.status should be(StatusCodes.Unauthorized)
+  }
+
+  it should "retry only unauthorized requests" in {
+    val helper = new CookieExtensionTestHelper
+
+    val finalResult = helper.sendRequest()
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Forbidden)
+      }.expectFailureResponseWithStatus(StatusCodes.Forbidden)
+    finalResult.request.request should be(TestRequest)
+  }
+
+  it should "store a cookie used for authorization" in {
+    val otherRequest = HttpRequest(method = HttpMethods.POST, uri = "/someEndpoint")
+    val helper = new CookieExtensionTestHelper
+
+    helper.sendRequest()
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders())
+      }.expectForwardedRequestAndAnswer { fwdRequest2 =>
+      HttpRequestSender.SuccessResult(fwdRequest2, HttpResponse())
+    }.expectSuccessResponse()
+
+    val fwdRequest3 = helper.sendRequest(otherRequest)
+      .expectForwardedRequest()
+    fwdRequest3.request should be(withCookie(otherRequest))
+  }
+
+  it should "update the authorization cookie when it becomes invalid" in {
+    val pair = HttpCookiePair("update", "recent")
+    val newCookie = Cookie(pair)
+    val otherReq = HttpRequest(method = HttpMethods.POST, uri = "/someEndpoint")
+    val helper = new CookieExtensionTestHelper
+
+    val fwdRequest = helper.sendRequest()
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized, headers = setCookieHeaders())
+      }.expectForwardedRequestAndAnswer { fwdRequest =>
+      HttpRequestSender.SuccessResult(fwdRequest, HttpResponse())
+    }.sendRequest(otherReq)
+      .expectForwardedRequestAndAnswer { fwdRequest =>
+        failedResult(fwdRequest, StatusCodes.Unauthorized,
+          headers = List(`Set-Cookie`(HttpCookie(pair.name, pair.value))))
+      }.expectForwardedRequest()
+    fwdRequest.request should be(withHeader(otherReq, newCookie))
+  }
+
+  it should "react on a Stop message" in {
+    val probeRequest = testKit.createTestProbe[HttpRequestSender.HttpCommand]()
+    val btk = BehaviorTestKit(CookieManagementExtension(probeRequest.ref))
+
+    btk.run(HttpRequestSender.Stop)
+    btk.returnedBehavior should be(Behaviors.stopped)
+    probeRequest.expectMessage(HttpRequestSender.Stop)
+  }
+
+  /**
+    * A test helper class managing a test actor instance and its dependencies.
+    */
+  private class CookieExtensionTestHelper {
+    /** A test probe simulating the underlying request actor. */
+    private val probeRequest = testKit.createTestProbe[HttpRequestSender.HttpCommand]()
+
+    /** A test probe acting as the client of the extension actor. */
+    private val probeReply = testKit.createTestProbe[HttpRequestSender.Result]()
+
+    /** The cookie extension to test. */
+    private val cookieExtension = testKit.spawn(CookieManagementExtension(probeRequest.ref))
+
+    /**
+      * Sends a request to the test extension actor.
+      *
+      * @param request     the request to sent
+      * @param discardMode the discard entity mode
+      * @return this test helper
+      */
+    def sendRequest(request: HttpRequest = TestRequest, discardMode: DiscardEntityMode = DiscardEntityMode.OnFailure):
+    CookieExtensionTestHelper = {
+      val sendReq = HttpRequestSender.SendRequest(request, RequestData, probeReply.ref, discardMode)
+      cookieExtension ! sendReq
+      this
+    }
+
+    /**
+      * Expects a success response to be returned by the test actor and returns
+      * it.
+      *
+      * @return the success response
+      */
+    def expectSuccessResponse(): HttpRequestSender.SuccessResult =
+      probeReply.expectMessageType[HttpRequestSender.SuccessResult]
+
+    /**
+      * Expects a failure response to be returned by the test actor and returns
+      * it.
+      *
+      * @return the failure response
+      */
+    def expectFailureResponse(): HttpRequestSender.FailedResult =
+      probeReply.expectMessageType[HttpRequestSender.FailedResult]
+
+    /**
+      * Expects a failure response indicating a failed request with the given
+      * status code.
+      *
+      * @param status the expected status code
+      * @return the failure response
+      */
+    def expectFailureResponseWithStatus(status: StatusCode): HttpRequestSender.FailedResult = {
+      val failureResponse = expectFailureResponse()
+      failureResponse.cause match {
+        case e: FailedResponseException =>
+          e.response.status should be(status)
+        case r => fail("Unexpected failure: " + r)
+      }
+      failureResponse
+    }
+
+    /**
+      * Expects that a request has been forwarded to the underlying request
+      * sender actor and returns it.
+      *
+      * @return the request that has been forwarded
+      */
+    def expectForwardedRequest(): HttpRequestSender.SendRequest =
+      probeRequest.expectMessageType[HttpRequestSender.SendRequest]
+
+    /**
+      * Expects that a request has been forwarded to the underlying request
+      * sender actor and sends a response back.
+      *
+      * @param fResponse a function to generate the response to be sent
+      * @return this test helper
+      */
+    def expectForwardedRequestAndAnswer(fResponse: HttpRequestSender.SendRequest => HttpRequestSender.Result):
+    CookieExtensionTestHelper = {
+      val request = expectForwardedRequest()
+      request.replyTo ! fResponse(request)
+      this
+    }
+  }
+
+}
