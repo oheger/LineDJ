567d3a36f43061f3bb8c9e8a0fa3d9625f31f05a
oheger <oliver.heger@oliver-heger.de>
2021-03-28 21:54:18 +0200

    Added AuthConfigFactory class.
    This class creates an AuthConfig object for a realm and its
    credentials. This config can then be used when setting up the
    downloader for an HTTP archive.

---
+++ mediaArchive/archiveHttpStartup/src/main/scala/de/oliver_heger/linedj/archivehttpstart/app/AuthConfigFactory.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart.app
+
+import akka.actor.ActorSystem
+import com.github.cloudfiles.core.http.Secret
+import com.github.cloudfiles.core.http.auth.{AuthConfig, BasicAuthConfig, OAuthConfig => CloudOAuthConfig, OAuthTokenData => CloudTokenData}
+import de.oliver_heger.linedj.archivehttp.config.{OAuthStorageConfig, UserCredentials}
+import de.oliver_heger.linedj.archivehttp.impl.io.oauth.{OAuthConfig, OAuthStorageService, OAuthTokenData}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * A helper class that produces an [[AuthConfig]] from credential information
+  * for a specific archive.
+  *
+  * An instance of this class is used when starting up HTTP archives. In order
+  * to configure the archives to correctly access media files, the proper
+  * authentication information must be present.
+  *
+  * @param oauthStorageService the service to access OAuth credentials
+  */
+class AuthConfigFactory(val oauthStorageService:
+                        OAuthStorageService[OAuthStorageConfig, OAuthConfig, Secret, OAuthTokenData]) {
+  /**
+    * Returns an ''AuthConfig'' for the given realm and credentials. The steps
+    * necessary to obtain all the information required for the correct type of
+    * ''AuthConfig'' are performed. As this may involve asynchronous
+    * operations, result is a ''Future''.
+    *
+    * @param realm       the realm for which authentication is needed
+    * @param credentials the credentials for this realm
+    * @param system      the actor system
+    * @return a ''Future'' with the ''AuthConfig'' that was created
+    */
+  def createAuthConfig(realm: ArchiveRealm, credentials: UserCredentials)
+                      (implicit system: ActorSystem): Future[AuthConfig] = {
+    realm match {
+      case _: BasicAuthRealm =>
+        Future.successful(BasicAuthConfig(credentials.userName, credentials.password))
+      case r: OAuthRealm =>
+        implicit val ec: ExecutionContext = system.dispatcher
+        val storageConfig = r.createIdpConfig(credentials.password)
+        for {
+          config <- oauthStorageService.loadConfig(storageConfig)
+          secret <- oauthStorageService.loadClientSecret(storageConfig)
+          tokens <- oauthStorageService.loadTokens(storageConfig)
+        } yield CloudOAuthConfig(tokenEndpoint = config.tokenEndpoint, redirectUri = config.redirectUri,
+          clientID = config.clientID, clientSecret = secret,
+          initTokenData = CloudTokenData(tokens.accessToken, tokens.refreshToken))
+    }
+  }
+}
---
+++ mediaArchive/archiveHttpStartup/src/test/scala/de/oliver_heger/linedj/archivehttpstart/app/AuthConfigFactorySpec.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2015-2021 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart.app
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import com.github.cloudfiles.core.http.Secret
+import com.github.cloudfiles.core.http.auth.BasicAuthConfig
+import com.github.cloudfiles.core.http.auth.{OAuthConfig => CloudOAuthConfig}
+import com.github.cloudfiles.core.http.auth.{OAuthTokenData => CloudTokenData}
+import de.oliver_heger.linedj.AsyncTestHelper
+import de.oliver_heger.linedj.archivehttp.config.{OAuthStorageConfig, UserCredentials}
+import de.oliver_heger.linedj.archivehttp.impl.io.oauth.{OAuthConfig, OAuthStorageService, OAuthTokenData}
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import java.nio.file.Paths
+import scala.concurrent.{ExecutionContext, Future}
+
+object AuthConfigFactorySpec {
+  /** The user ID to access a test archive. */
+  private val User = "myArchiveUser"
+
+  /** The test password. */
+  private val Password = Secret("myPassword")
+
+  /** Name of a realm used by tests. */
+  private val RealmName = "myTestReal"
+}
+
+/**
+  * Test class for ''AuthConfigFactory''.
+  */
+class AuthConfigFactorySpec(testSystem: ActorSystem) extends TestKit(testSystem) with AnyFlatSpecLike
+  with BeforeAndAfterAll with Matchers with MockitoSugar with AsyncTestHelper {
+  def this() = this(ActorSystem("AuthConfigFactorySpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  import AuthConfigFactorySpec._
+
+  "AuthConfigFactory" should "create a configuration for BasicAuth" in {
+    val service = mock[OAuthStorageService[OAuthStorageConfig, OAuthConfig, Secret, OAuthTokenData]]
+    val realm = BasicAuthRealm(RealmName)
+    val credentials = UserCredentials(User, Password)
+    val factory = new AuthConfigFactory(service)
+
+    val authConfig = futureResult(factory.createAuthConfig(realm, credentials))
+    authConfig should be(BasicAuthConfig(User, Password))
+  }
+
+  it should "create a configuration for OAuth" in {
+    val IdpName = "MyIDP"
+    val StoragePath = Paths.get("/my/idp/data")
+    val service = mock[OAuthStorageService[OAuthStorageConfig, OAuthConfig, Secret, OAuthTokenData]]
+    val realm = OAuthRealm(RealmName, StoragePath, IdpName)
+    val credentials = UserCredentials(null, Password)
+    val oauthConfig = OAuthConfig(authorizationEndpoint = "https://authorization.example.org/",
+      tokenEndpoint = "https://tokens.example.org/", scope = "testing", redirectUri = "https://redirect.example.org/",
+      clientID = "testOAuthClient")
+    val tokenData = OAuthTokenData("someAccessToken", "someRefreshToken")
+    val storageConfig = realm.createIdpConfig(Password)
+    val expConfig = CloudOAuthConfig(tokenEndpoint = oauthConfig.tokenEndpoint, redirectUri = oauthConfig.redirectUri,
+      clientID = oauthConfig.clientID, clientSecret = Password,
+      initTokenData = CloudTokenData(tokenData.accessToken, tokenData.refreshToken))
+    implicit val ec: ExecutionContext = system.dispatcher
+    when(service.loadConfig(storageConfig)).thenReturn(Future.successful(oauthConfig))
+    when(service.loadClientSecret(storageConfig)).thenReturn(Future.successful(Password))
+    when(service.loadTokens(storageConfig)).thenReturn(Future.successful(tokenData))
+    val factory = new AuthConfigFactory(service)
+
+    val authConfig = futureResult(factory.createAuthConfig(realm, credentials))
+    authConfig should be(expConfig)
+  }
+}
