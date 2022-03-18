16bbc2a84cc03bc0929107a01d99d73cc8c71e38
oheger <oliver.heger@oliver-heger.de>
2022-03-18 20:46:47 +0100

    ChildActorFactory: Names for actors are now generated.
    The default names used so far are pretty meaningless and do not allow
    an identification of actors in log messages. Now names are generated
    based on the actor class, which should be by far more helpful.

--- shared/src/main/scala/de/oliver_heger/linedj/utils/ChildActorFactory.scala
+++ shared/src/main/scala/de/oliver_heger/linedj/utils/ChildActorFactory.scala
@@ -17,25 +17,48 @@
 package de.oliver_heger.linedj.utils
 
 import akka.actor.{Actor, ActorRef, Props}
+import de.oliver_heger.linedj.utils.ChildActorFactory.actorNameFor
+
+import java.util.concurrent.ConcurrentHashMap
+
+object ChildActorFactory {
+  /** A map to generate actor names for actor classes. */
+  private val actorClassCounters = new ConcurrentHashMap[String, Integer]()
+
+  /**
+    * Generates a unique name for an actor created from the specified ''Props''
+    * instance.
+    *
+    * @param props the creation properties of the actor
+    * @return a unique name for this actor
+    */
+  private def actorNameFor(props: Props): String = {
+    val clsName = props.actorClass().getSimpleName
+    clsName + actorClassCounters.compute(clsName, (_, count) => if (count == null) 1 else count + 1)
+  }
+}
 
 /**
- * A trait offering a generic method for creating child actors.
- *
- * When testing actors that dynamically create child actors there is often the
- * need to inject specific mock actor references. This trait provides a
- * solution for this problem. It defines a generic method for creating actors
- * (using the same ''Props'' mechanism as regular actor creation). Test classes
- * can override this method to return arbitrary test actor references.
- *
- * This trait is fully functional; it can be directly mixed into actor classes
- * that need to create child actors.
- */
+  * A trait offering a generic method for creating child actors.
+  *
+  * When testing actors that dynamically create child actors there is often the
+  * need to inject specific mock actor references. This trait provides a
+  * solution for this problem. It defines a generic method for creating actors
+  * (using the same ''Props'' mechanism as regular actor creation). Test classes
+  * can override this method to return arbitrary test actor references.
+  *
+  * This trait is fully functional; it can be directly mixed into actor classes
+  * that need to create child actors.
+  */
 trait ChildActorFactory extends Actor {
   /**
-   * Creates a child actor based on the specified ''Props'' object. This
-   * implementation uses the actor's context to actually create the child.
-   * @param p the ''Props'' defining the actor to be created
-   * @return the ''ActorRef'' to the new child actor
-   */
-  def createChildActor(p: Props): ActorRef = context.actorOf(p)
+    * Creates a child actor based on the specified ''Props'' object. This
+    * implementation uses the actor's context to actually create the child. It
+    * generates a unique actor name based on the actor class and a counter that
+    * is managed per actor class.
+    *
+    * @param p the ''Props'' defining the actor to be created
+    * @return the ''ActorRef'' to the new child actor
+    */
+  def createChildActor(p: Props): ActorRef = context.actorOf(p, actorNameFor(p))
 }
