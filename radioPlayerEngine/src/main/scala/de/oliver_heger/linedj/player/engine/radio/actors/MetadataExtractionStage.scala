b52e7fcab759f4243ba46fd8a9d60e0ceecc1167
Oliver Heger <oliver.heger@oliver-heger.de>
2022-12-08 22:13:47 +0100

    Radio player engine: Added MetadataExtractionStage class.
    This is a custom flow stage implementation that uses a
    MetadataExtractionService to split a radio stream into audio data and
    metadata.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/radio/actors/MetadataExtractionStage.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.actors
+
+import akka.stream.{Attributes, FanOutShape2, Inlet, Outlet}
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.util.ByteString
+
+private object MetadataExtractionStage {
+  /**
+    * Returns an instance of [[MetadataExtractionStage]] that can handle a
+    * radio stream with the given optional chunk size for audio data. If the
+    * chunk size is undefined, the whole stream is considered to be audio data.
+    * Otherwise, metadata is extracted accordingly.
+    *
+    * @param optAudioChunkSize the optional chunk size for audio data
+    * @return a [[MetadataExtractionStage]] to process this radio stream
+    */
+  def apply(optAudioChunkSize: Option[Int]): MetadataExtractionStage =
+    optAudioChunkSize map { chunkSize =>
+      new MetadataExtractionStage(SupportedMetadataExtractionService, chunkSize)
+    } getOrElse new MetadataExtractionStage(UnsupportedMetadataExtractionService, 0)
+}
+
+/**
+  * A stage implementation that manages the state of a
+  * [[MetadataExtractionService]] to separate a radio stream into the actual
+  * audio data and metadata.
+  *
+  * The stage has two outputs: One yields audio data, the other metadata. If
+  * the radio stream does not support metadata, the latter produces no results.
+  *
+  * @param extractionService the service responsible for metadata extraction
+  * @param audioChunkSize    the size of chunks of audio data
+  */
+private class MetadataExtractionStage private(extractionService: MetadataExtractionService,
+                                              audioChunkSize: Int)
+  extends GraphStage[FanOutShape2[ByteString, ByteString, ByteString]] {
+  val in: Inlet[ByteString] = Inlet[ByteString]("MetadataExtractionStage.in")
+  val outAudio: Outlet[ByteString] = Outlet[ByteString]("MetadataExtractionStage.extractedAudioData")
+  val outMeta: Outlet[ByteString] = Outlet[ByteString]("MetadataExtractionStage.extractedMetadata")
+
+  override def shape: FanOutShape2[ByteString, ByteString, ByteString] =
+    new FanOutShape2[ByteString, ByteString, ByteString](in, outAudio, outMeta)
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      /** The current state of the extraction process. */
+      private var extractionState = MetadataExtractionState.initial(audioChunkSize)
+
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          processChunk(grab(in))
+        }
+      })
+
+      setHandler(outAudio, new OutHandler {
+        override def onPull(): Unit = {}
+      })
+
+      setHandler(outMeta, new OutHandler {
+        override def onPull(): Unit = {}
+      })
+
+      override def preStart(): Unit = {
+        pull(in)
+      }
+
+      /**
+        * Handles a chunk of data received from upstream with the help of the
+        * [[MetadataExtractionService]] provided at construction time.
+        *
+        * @param data the data from upstream
+        */
+      private def processChunk(data: ByteString): Unit = {
+        val stateUpdate = extractionService.handleData(data)
+
+        val (nextState, extracted) = stateUpdate.run(extractionState)
+        extractionState = nextState
+
+        if (extracted.audioChunks.nonEmpty) {
+          emitMultiple(outAudio, extracted.audioChunks)
+        }
+        extracted.metadataChunk foreach {
+          push(outMeta, _)
+        }
+
+        pull(in)
+      }
+    }
+}
---
+++ radioPlayerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/radio/actors/MetadataExtractionStageSpec.scala
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.actors
+
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ClosedShape
+import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink, Source}
+import akka.testkit.TestKit
+import akka.util.ByteString
+import de.oliver_heger.linedj.AsyncTestHelper
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.Future
+
+object MetadataExtractionStageSpec {
+  /** The chunk size of audio blocks. */
+  private val AudioChunkSize = 256
+
+  /**
+    * Generates a metadata string based on the given index. The resulting
+    * string has a length that is a multitude of 16.
+    *
+    * @param index the index
+    * @return the metadata string with this index
+    */
+  private def generateMetadata(index: Int): String = {
+    val content = "Metadata block " + index
+    content + " " * ((16 - content.length % 16) % 16)
+  }
+
+  /**
+    * Returns a sink that aggregates all input into a single ''ByteString''.
+    *
+    * @return the aggregating sink
+    */
+  private def aggregateSink(): Sink[ByteString, Future[ByteString]] = Sink.fold(ByteString.empty)(_ ++ _)
+}
+
+/**
+  * Test class for [[MetadataExtractionStage]].
+  */
+class MetadataExtractionStageSpec(testSystem: ActorSystem) extends TestKit(testSystem) with AnyFlatSpecLike
+  with BeforeAndAfterAll with Matchers with AsyncTestHelper {
+  def this() = this(ActorSystem("MetadataExtractionStageSpec"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  import MetadataExtractionStageSpec._
+
+  /**
+    * Runs a stream with the given source and an extraction stage. Returns the
+    * aggregated data from the two output channels of the extraction stage.
+    *
+    * @param source       the source of the stream
+    * @param optChunkSize the optional size of audio chunks
+    * @return a tuple with the audio and metadata
+    */
+  private def runStream(optChunkSize: Option[Int], source: Source[ByteString, NotUsed]): (ByteString, ByteString) = {
+    val graph = RunnableGraph.fromGraph(GraphDSL.createGraph(aggregateSink(), aggregateSink())((_, _)) {
+      implicit builder =>
+        (sinkAudio, sinkMeta) =>
+          import GraphDSL.Implicits._
+
+          val extractionStage = builder.add(MetadataExtractionStage(optChunkSize))
+
+          source ~> extractionStage.in
+          extractionStage.out0 ~> sinkAudio
+          extractionStage.out1 ~> sinkMeta
+          ClosedShape
+    })
+
+    val (futAudio, futMeta) = graph.run()
+    (futureResult(futAudio), futureResult(futMeta))
+  }
+
+  "MetadataExtractionStage" should "handle a radio stream that supports metadata" in {
+    val ChunkCount = 16
+    val audioData = (0 until ChunkCount).map(RadioStreamTestHelper.dataBlock(AudioChunkSize, _))
+    val metadata = (1 to ChunkCount).map { idx =>
+      RadioStreamTestHelper.metadataBlock(ByteString(generateMetadata(idx)))
+    }
+    val dataChunks = audioData.zip(metadata)
+      .foldLeft(ByteString.empty) { (d, t) => d ++ t._1 ++ t._2 }
+      .grouped(100)
+    val expectedAudioData = ByteString(RadioStreamTestHelper.refData(ChunkCount * AudioChunkSize))
+    val expectedMetadata = (1 to ChunkCount).map(generateMetadata)
+      .foldLeft(ByteString.empty) { (aggregate, chunk) => aggregate ++ ByteString(chunk) }
+
+    val (extractedAudioData, extractedMetadata) = runStream(Some(AudioChunkSize), Source(dataChunks.toList))
+
+    extractedAudioData should be(expectedAudioData)
+    extractedMetadata should be(expectedMetadata)
+  }
+
+  it should "handle a radio stream that does not support metadata" in {
+    val ChunkCount = 10
+    val audioData = ByteString(RadioStreamTestHelper.refData(ChunkCount * AudioChunkSize))
+    val source = Source(audioData.grouped(333).toList)
+
+    val (extractedAudioData, extractedMetadata) = runStream(None, source)
+
+    extractedAudioData should be(audioData)
+    extractedMetadata shouldBe empty
+  }
+}
