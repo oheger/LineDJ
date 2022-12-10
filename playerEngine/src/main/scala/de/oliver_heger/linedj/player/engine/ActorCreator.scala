fe137ba8ddbd73346f053787b9f4f229f20ab8fa
Oliver Heger <oliver.heger@oliver-heger.de>
2022-12-10 22:24:53 +0100

    Player engine: Added a new ActorCreator trait.
    This trait is going to replace the ActorCreator function type declared
    by the PlayerConfig companion object. In addition to creating classic
    actors, it also allows the creation of typed actors.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ playerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/ActorCreator.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine
+
+import akka.actor.Props
+import akka.{actor => classics}
+import akka.actor.typed.{ActorRef, Behavior}
+
+/**
+  * Definition of a trait that allows the player engine to create actors
+  * without having to know details about the concrete setup with an actor
+  * system, etc. The functions defined here are invoked in order to create the
+  * several helper actors making up the audio player engine implementation.
+  *
+  * Note that the player engine does not stop actors; this is in the
+  * responsibility of the creator component. So, an implementation may want to
+  * keep track on newly created actors, so that they can be stopped later.
+  */
+trait ActorCreator {
+  /**
+    * Creates a typed actor for the given behavior with the specified name.
+    * Since there is no default way to stop typed actors, it is possible to
+    * specify a command that can be used for this purpose.
+    *
+    * @param behavior       the behavior for the new actor
+    * @param name           the name to use for this actor
+    * @param optStopCommand an optional command to stop the actor
+    * @tparam T the type of messages processed by the actor
+    * @return the reference to the newly created actor
+    */
+  def createActor[T](behavior: Behavior[T], name: String, optStopCommand: Option[T]): ActorRef[T]
+
+  /**
+    * Creates a classic actor based on the given ''Props'' with the specified
+    * name.
+    *
+    * @param props the ''Props'' for the new actor instance
+    * @param name  the name to use for this actor
+    * @return the reference to the newly created actor
+    */
+  def createActor(props: Props, name: String): classics.ActorRef
+}
