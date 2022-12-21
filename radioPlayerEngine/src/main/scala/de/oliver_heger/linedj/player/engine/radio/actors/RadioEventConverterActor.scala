d2ca29fbbb7da063d6d0708ed5831ace1bf66554
Oliver Heger <oliver.heger@oliver-heger.de>
2022-12-21 21:06:37 +0100

    Radio player engine: Added RadioEventConverterActor class.
    This is an actor implementation that can be registered as an event
    listener for audio player events and converts these events to
    corresponding radio events. It is going to be needed when the
    player engines have been reworked to generate type-safe events. Then
    some events generated by the playback actor must be converted to radio
    events.
    
    The hierarchy of radio events has been extended to mirror some missing
    events of the audio player.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/radio/actors/RadioEventConverterActor.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.actors
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import de.oliver_heger.linedj.player.engine.{AudioSource, PlaybackContextCreationFailedEvent, PlaybackErrorEvent, PlaybackProgressEvent, PlayerEvent}
+import de.oliver_heger.linedj.player.engine.radio.{RadioEvent, RadioPlaybackContextCreationFailedEvent, RadioPlaybackErrorEvent, RadioPlaybackProgressEvent, RadioSource}
+
+import scala.concurrent.duration._
+
+/**
+  * An actor implementation that converts events from the audio player to the
+  * events of the [[RadioEvent]] hierarchy.
+  *
+  * An instance of this implementation is passed to the actors used by the
+  * radio player that generate audio player events. The relevant events are
+  * then mapped to radio events.
+  */
+object RadioEventConverterActor {
+  /**
+    * The base trait for the commands supported by this actor implementation.
+    */
+  sealed trait RadioEventConverterCommand
+
+  /**
+    * A command that triggers the conversion of a [[PlayerEvent]]. If this
+    * event is relevant, the actor generates a corresponding [[RadioEvent]] and
+    * passes it to the associated listener.
+    *
+    * @param event the event to be converted
+    */
+  case class ConvertEvent(event: PlayerEvent) extends RadioEventConverterCommand
+
+  /**
+    * A command that can be used to query the listener for player events. This
+    * listener can then be passed to actors used by the audio player. It
+    * handles events by converting them and propagating them to the listener of
+    * radio events.
+    *
+    * @param client the client actor waiting for the response
+    */
+  case class GetPlayerListener(client: ActorRef[PlayerListenerReference]) extends RadioEventConverterCommand
+
+  /**
+    * A command that stops an actor of this class.
+    */
+  case object Stop extends RadioEventConverterCommand
+
+  /**
+    * A message class sent by this actor as a response of a
+    * [[GetPlayerListener]] command. It contains the reference to the actor
+    * that acts as listener for player events.
+    *
+    * @param listener the listener for player events
+    */
+  case class PlayerListenerReference(listener: ActorRef[PlayerEvent])
+
+  /**
+    * Returns a behavior to create an instance of this actor. Events processed
+    * by this actor are forwarded to the specified radio event listener.
+    *
+    * @param radioEventListener the listener for radio events
+    * @return the behavior to create an instance of this actor
+    */
+  def apply(radioEventListener: ActorRef[RadioEvent]): Behavior[RadioEventConverterCommand] =
+    Behaviors.setup[RadioEventConverterCommand] { context =>
+      val listener: ActorRef[PlayerEvent] = context.messageAdapter(event => ConvertEvent(event))
+
+      def handleMessages(): Behavior[RadioEventConverterCommand] =
+        Behaviors.receiveMessage {
+          case GetPlayerListener(client) =>
+            client ! PlayerListenerReference(listener)
+            Behaviors.same
+
+          case ConvertEvent(event) =>
+            convertEvent(event) foreach radioEventListener.!
+            Behaviors.same
+
+          case Stop =>
+            Behaviors.stopped
+        }
+
+      handleMessages()
+    }
+
+  /**
+    * Converts relevant player events to their counterparts from the
+    * [[RadioEvent]] hierarchy. A return value of ''None'' means that this
+    * event can be ignored.
+    *
+    * @param playerEvent the [[PlayerEvent]] to convert
+    * @return an ''Option'' with the converted event
+    */
+  private def convertEvent(playerEvent: PlayerEvent): Option[RadioEvent] =
+    playerEvent match {
+      case PlaybackProgressEvent(bytesProcessed, playbackTime, currentSource, time) =>
+        Some(RadioPlaybackProgressEvent(convertSource(currentSource), bytesProcessed, playbackTime.seconds, time))
+
+      case PlaybackContextCreationFailedEvent(source, time) =>
+        Some(RadioPlaybackContextCreationFailedEvent(convertSource(source), time))
+
+      case PlaybackErrorEvent(source, time) =>
+        Some(RadioPlaybackErrorEvent(convertSource(source), time))
+
+      case _ => None
+    }
+
+  /**
+    * Constructs a [[RadioSource]] for the given [[AudioSource]].
+    *
+    * @param audioSource the audio source
+    * @return the corresponding radio source
+    */
+  private def convertSource(audioSource: AudioSource): RadioSource = RadioSource(uri = audioSource.uri)
+}
+
---
+++ radioPlayerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/radio/actors/RadioEventConverterActorSpec.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.actors
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.typed.scaladsl.AskPattern._
+import de.oliver_heger.linedj.AsyncTestHelper
+import de.oliver_heger.linedj.player.engine.{AudioSource, PlaybackContextCreationFailedEvent, PlaybackErrorEvent, PlaybackProgressEvent, PlayerEvent}
+import de.oliver_heger.linedj.player.engine.radio.{RadioEvent, RadioPlaybackContextCreationFailedEvent, RadioPlaybackErrorEvent, RadioPlaybackProgressEvent, RadioSource}
+import org.scalatest.flatspec.{AnyFlatSpec, AnyFlatSpecLike}
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+object RadioEventConverterActorSpec {
+  /** A test audio source used in player events. */
+  private val TestAudioSource = AudioSource("https://audio.example.org/test/stream.mp3", -1, 0, 0)
+
+  /** The corresponding test radio source. */
+  private val TestRadioSource = RadioSource(TestAudioSource.uri)
+}
+
+/**
+  * Test class for [[RadioEventConverterActor]].
+  */
+class RadioEventConverterActorSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers
+  with AsyncTestHelper {
+
+  import RadioEventConverterActorSpec._
+
+  "RadioEventConverterActor" should "convert a playback progress event" in {
+    val playerEvent = PlaybackProgressEvent(bytesProcessed = 20221221203108L,
+      playbackTime = 90 * 60,
+      currentSource = TestAudioSource)
+    val radioEvent = RadioPlaybackProgressEvent(source = TestRadioSource,
+      bytesProcessed = playerEvent.bytesProcessed,
+      playbackTime = playerEvent.playbackTime.seconds,
+      time = playerEvent.time)
+    val helper = new ConverterActorTestHelper
+
+    helper.expectConversion(playerEvent, radioEvent)
+  }
+
+  it should "convert a playback context creation failed event" in {
+    val playerEvent = PlaybackContextCreationFailedEvent(source = TestAudioSource)
+    val radioEvent = RadioPlaybackContextCreationFailedEvent(source = TestRadioSource, time = playerEvent.time)
+    val helper = new ConverterActorTestHelper
+
+    helper.expectConversion(playerEvent, radioEvent)
+  }
+
+  it should "convert a playback error event" in {
+    val playerEvent = PlaybackErrorEvent(source = TestAudioSource)
+    val radioEvent = RadioPlaybackErrorEvent(source = TestRadioSource, time = playerEvent.time)
+    val helper = new ConverterActorTestHelper
+
+    helper.expectConversion(playerEvent, radioEvent)
+  }
+
+  it should "stop itself on receiving a Stop command" in {
+    val helper = new ConverterActorTestHelper
+
+    helper.checkStop()
+  }
+
+  /**
+    * A test helper class managing the dependencies of a test instance.
+    */
+  private class ConverterActorTestHelper {
+    /** A test probe acting as listener for radio events. */
+    private val probeRadioListener = testKit.createTestProbe[RadioEvent]()
+
+    /** The actor to be tested. */
+    private val converterActor = testKit.spawn(RadioEventConverterActor(probeRadioListener.ref))
+
+    /**
+      * Executes a test for an event that is expected to be converted.
+      *
+      * @param playerEvent the original player event
+      * @param radioEvent  the expected converted radio event
+      */
+    def expectConversion(playerEvent: PlayerEvent, radioEvent: RadioEvent): Unit = {
+      val futPlayerListener = converterActor.ask[RadioEventConverterActor.PlayerListenerReference] { ref =>
+        RadioEventConverterActor.GetPlayerListener(ref)
+      }
+      val playerListener = futureResult(futPlayerListener).listener
+
+      playerListener ! playerEvent
+      probeRadioListener.expectMessage(radioEvent)
+    }
+
+    /**
+      * Tests whether the converter actor can stop itself.
+      */
+    def checkStop(): Unit = {
+      converterActor ! RadioEventConverterActor.Stop
+
+      probeRadioListener.expectTerminated(converterActor)
+    }
+  }
+}
