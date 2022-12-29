266f18fd9744d1433cc492bef056628302a17006
Oliver Heger <oliver.heger@oliver-heger.de>
2022-12-29 17:20:16 +0100

    Radio player: Added a new RadioPlayerConfig class.
    The class collects all configuration properties supported by the radio
    player application. Before, the configuration was distributed over
    multiple classes. The new class still has to be integrated.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayer/src/main/scala/de/oliver_heger/linedj/radio/RadioPlayerConfig.scala
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import org.apache.commons.configuration.Configuration
+
+import scala.annotation.tailrec
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+/**
+  * A data class defining the configuration options used for handling errors.
+  *
+  * The strategy for handling errors depends on multiple properties:
+  *  - ''retryInterval'': When an error occurs the strategy will pause at least
+  *    for this interval and then retry. For multiple errors in series, the
+  *    interval can be increased with the ''retryIncrementFactor''.
+  *  - ''retryIncrementFactor'': A factor for incrementing the retry interval
+  *    if there are multiple errors from the same radio source. This typically
+  *    indicates a more permanent problem with this source. Therefore, it does
+  *    not make sense to retry it directly, but make the pauses between two
+  *    attempts longer and longer, until a configurable maximum is reached.
+  *    This factor must be greater than 1.
+  *  - ''maxRetries'': Defines the maximum number of retries for a failing
+  *    radio source before the player switches to another source. Note that
+  *    this setting also determines the maximum interval between two retries.
+  *  - ''recoveryTime'': The time of successful playback after an error to
+  *    reset the error state.
+  *  - ''recoveryMinFailedSources'': The minimum number of sources that must be
+  *    marked as dysfunctional before a recovery from an error is attempted.
+  *    Switching back to the original source makes only sense if there was a
+  *    general network problem which is now fixed. Then we expect that multiple
+  *    sources have been marked as dysfunctional. If there are only a few
+  *    sources affected, this may indicate a (more permanent) problem with
+  *    these sources, e.g. an incorrect URL. In this case, switching back to
+  *    such a source is likely to fail again; and we should not interrupt
+  *    playback every time the recovery interval is reached.
+  *
+  * @param retryInterval           the retry interval
+  * @param retryIncrementFactor    the factor to increment the retry interval
+  *                                if the failure persists
+  * @param maxRetries              the maximum number of retries
+  * @param recoveryTime            time after which to recover from error state
+  * @param recoverMinFailedSources number of sources to recover from error
+  *                                state
+  */
+case class ErrorHandlingConfig(retryInterval: FiniteDuration,
+                               retryIncrementFactor: Double,
+                               maxRetries: Int,
+                               recoveryTime: Long,
+                               recoverMinFailedSources: Int) {
+  /**
+    * The maximum retry interval based on the minimum interval and the
+    * maximum number of retries.
+    */
+  lazy val maxRetryInterval: Long = calcMaxRetry(maxRetries, retryInterval.toMillis)
+
+  /**
+    * Calculates the maximum retry interval based on the allowed number of
+    * retries.
+    *
+    * @param count the current counter
+    * @return the maximum retry interval
+    */
+  @tailrec private def calcMaxRetry(count: Int, value: Long): Long =
+    if (count == 0) value
+    else calcMaxRetry(count - 1, math.round(retryIncrementFactor * value))
+}
+
+object RadioPlayerConfig {
+  /**
+    * The default value for the ''retryInterval'' configuration property (in
+    * milliseconds).
+    */
+  final val DefaultRetryInterval = 1000
+
+  /** Minimum value for the retry interval (in milliseconds). */
+  final val MinimumRetryInterval = 10
+
+  /**
+    * The default value for the ''retryIncrement'' configuration property. This
+    * value causes a pretty fast increment of retry intervals.
+    */
+  final val DefaultRetryIncrement = 2.0
+
+  /** Minimum retry increment factor. */
+  final val MinimumRetryIncrement = 1.1
+
+  /**
+    * The default value for the ''maxRetries'' configuration property.
+    */
+  final val DefaultMaxRetries = 5
+
+  /**
+    * Constant for an initial delay before starting playback (in milliseconds).
+    *
+    * It might be necessary to wait for a while until the audio player engine
+    * is set up, and all playback context factories have been registered. The
+    * time to wait can be specified in the configuration. If this is not done,
+    * this default value is used.
+    */
+  final val DefaultInitialDelay = 5000
+
+  /** Default time interval for error recovery (in seconds). */
+  final val DefaultRecoveryTime = 600L
+
+  /** Default minimum number of dysfunctional sources before recovery. */
+  final val DefaultMinFailuresForRecovery = 1
+
+  /**
+    * The common prefix for all configuration keys.
+    */
+  private val KeyPrefix = "radio."
+
+  /** Configuration key for the initial delay. */
+  private val KeyInitialDelay = KeyPrefix + "initialDelay"
+
+  /** The common prefix for keys related to error handling. */
+  private val ErrorKeyPrefix = KeyPrefix + "error."
+
+  /** Configuration key for the (minimum) retry interval. */
+  private val KeyInterval = ErrorKeyPrefix + "retryInterval"
+
+  /** Configuration key for the retry interval increment factor. */
+  private val KeyIncrement = ErrorKeyPrefix + "retryIncrement"
+
+  /** Configuration key for the maximum number of retries for a failing source. */
+  private val KeyMaxRetries = ErrorKeyPrefix + "maxRetries"
+
+  /** Configuration key prefix for error recovery keys. */
+  private val RecoveryKeyPrefix = ErrorKeyPrefix + "recovery."
+
+  /** Configuration key for the error recovery time. */
+  private val KeyRecoveryTime = RecoveryKeyPrefix + "time"
+
+  /** Configuration key for the minimum failed sources before recovery. */
+  private val KeyRecoveryMinFailures = RecoveryKeyPrefix + "minFailedSources"
+
+  /**
+    * Creates a [[RadioPlayerConfig]] instance from the passed in configuration
+    * object.
+    *
+    * @param config the configuration
+    * @return the [[RadioPlayerConfig]] instance
+    */
+  def apply(config: Configuration): RadioPlayerConfig = {
+    val sourceConfig = RadioSourceConfig(config)
+    val errorConfig = readErrorConfig(config)
+
+    new RadioPlayerConfig(sourceConfig, errorConfig, config.getInt(KeyInitialDelay, DefaultInitialDelay))
+  }
+
+  /**
+    * Creates an [[ErrorHandlingConfig]] instance from the given configuration.
+    *
+    * @param config the configuration
+    * @return the configuration related to error handling
+    */
+  private def readErrorConfig(config: Configuration): ErrorHandlingConfig = {
+    val interval = math.max(MinimumRetryInterval,
+      config.getInt(KeyInterval, DefaultRetryInterval)).millis
+    val increment = math.max(MinimumRetryIncrement,
+      config.getDouble(KeyIncrement, DefaultRetryIncrement))
+    val maxRetryCount = config.getInt(KeyMaxRetries, DefaultMaxRetries)
+    val recoveryTime = config.getLong(KeyRecoveryTime, DefaultRecoveryTime)
+    val recoveryMinFailures = config.getInt(KeyRecoveryMinFailures, DefaultMinFailuresForRecovery)
+    ErrorHandlingConfig(interval, increment, maxRetryCount, recoveryTime, recoveryMinFailures)
+  }
+}
+
+/**
+  * A data class storing the full configuration of the radio player
+  * application.
+  *
+  * @param sourceConfig the configuration of radio sources
+  * @param errorConfig  the error handling configuration
+  * @param initialDelay the delay before starting playback
+  */
+case class RadioPlayerConfig(sourceConfig: RadioSourceConfig,
+                             errorConfig: ErrorHandlingConfig,
+                             initialDelay: Int)
---
+++ radioPlayer/src/test/scala/de/oliver_heger/linedj/radio/RadioPlayerConfigSpec.scala
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2015-2022 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import org.apache.commons.configuration.{HierarchicalConfiguration, XMLConfiguration}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+object RadioPlayerConfigSpec {
+  /** The name of the test configuration file. */
+  private val ConfigFile = "test-radio-configuration.xml"
+
+  /**
+    * Reads the test configuration file.
+    *
+    * @return the configuration
+    */
+  private def readConfigurationFile(): HierarchicalConfiguration = new XMLConfiguration(ConfigFile)
+
+  /**
+    * Loads a [[RadioPlayerConfig]] from the test configuration file.
+    *
+    * @return the configuration created from the file
+    */
+  private def loadConfig(): RadioPlayerConfig = RadioPlayerConfig(readConfigurationFile())
+}
+
+/**
+  * Test class for [[RadioPlayerConfig]].
+  */
+class RadioPlayerConfigSpec extends AnyFlatSpec with Matchers {
+
+  import RadioPlayerConfigSpec._
+
+  "A radio player configuration" should "contain radio sources" in {
+    val srcConfig = loadConfig().sourceConfig
+
+    srcConfig.sources.size should be > 2
+  }
+
+  it should "order radio sources by their ranking" in {
+    val srcConfig = loadConfig().sourceConfig
+
+    val firstSource = srcConfig.sources.head._2
+    val lastSource = srcConfig.sources.last._2
+    srcConfig.ranking(firstSource) should be > srcConfig.ranking(lastSource)
+  }
+
+  it should "support exclusions for radio sources" in {
+    val srcConfig = loadConfig().sourceConfig
+
+    srcConfig.exclusions.size should be(srcConfig.sources.size)
+    srcConfig.exclusions.values.exists(_.nonEmpty) shouldBe true
+  }
+
+  it should "support an error configuration" in {
+    val errConfig = loadConfig().errorConfig
+
+    errConfig.retryInterval should be(1000.millis)
+    errConfig.retryIncrementFactor should be(2.0)
+    errConfig.maxRetries should be(3)
+    errConfig.recoveryTime should be(20)
+    errConfig.recoverMinFailedSources should be(2)
+  }
+
+  it should "correct a retry interval that is too small" in {
+    val playerConfig = new HierarchicalConfiguration
+    playerConfig.addProperty("radio.error.retryInterval", 9)
+
+    val config = RadioPlayerConfig(playerConfig)
+    config.errorConfig.retryInterval should be(10.millis)
+  }
+
+  it should "correct an increment factor that is too small" in {
+    val playerConfig = new HierarchicalConfiguration
+    playerConfig.addProperty("radio.error.retryIncrement", 1)
+
+    val config = RadioPlayerConfig(playerConfig)
+    config.errorConfig.retryIncrementFactor should be(1.1)
+  }
+
+  it should "read further configuration properties" in {
+    val config = loadConfig()
+
+    config.initialDelay should be(1500)
+  }
+
+  it should "apply defaults for unspecified configuration settings" in {
+    val config = RadioPlayerConfig(new HierarchicalConfiguration)
+
+    config.sourceConfig.sources shouldBe empty
+    config.sourceConfig.exclusions shouldBe empty
+    config.errorConfig.retryInterval should be(RadioPlayerConfig.DefaultRetryInterval.millis)
+    config.errorConfig.retryIncrementFactor should be(RadioPlayerConfig.DefaultRetryIncrement)
+    config.errorConfig.maxRetries should be(RadioPlayerConfig.DefaultMaxRetries)
+    config.errorConfig.recoveryTime should be(RadioPlayerConfig.DefaultRecoveryTime)
+    config.errorConfig.recoverMinFailedSources should be(RadioPlayerConfig.DefaultMinFailuresForRecovery)
+    config.initialDelay should be(RadioPlayerConfig.DefaultInitialDelay)
+  }
+}
