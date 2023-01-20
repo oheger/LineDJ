c87a6fe0a3b38aa357c7d31a0fbc2c41b8161e12
Oliver Heger <oliver.heger@oliver-heger.de>
2023-01-20 21:12:46 +0100

    Radio player engine: Added ReplacementSourceSelectionService trait.
    This trait (and its implementation) is going to replace the
    ReplacementSourceSelectionStrategy class, but the distribution of
    logic is slightly different. The service is responsible for the
    evaluation of all interval queries, and - based on the results - the
    selection of a replacement source.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/radio/actors/schedule/ReplacementSourceSelectionService.scala
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.actors.schedule
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Sink, Source}
+import de.oliver_heger.linedj.player.engine.interval.IntervalTypes.{After, Before, Inside, IntervalQueryResult}
+import de.oliver_heger.linedj.player.engine.radio.actors.schedule.ReplacementSourceSelectionService.SelectedReplacementSource
+import de.oliver_heger.linedj.player.engine.radio.{RadioSource, RadioSourceConfig}
+
+import java.time.LocalDateTime
+import scala.collection.immutable.SortedMap
+import scala.concurrent.Future
+import scala.util.Random
+
+object ReplacementSourceSelectionService {
+  /**
+    * A case class representing the selection of a replacement radio source. An
+    * instance contains the source to be played as replacement and the date how
+    * long this source can be played.
+    *
+    * @param source    the replacement radio source
+    * @param untilDate the date until which this source can be played
+    */
+  case class SelectedReplacementSource(source: RadioSource, untilDate: LocalDateTime)
+}
+
+/**
+  * A trait defining a service that selects a replacement source from a given
+  * set of sources.
+  */
+trait ReplacementSourceSelectionService {
+  /**
+    * Tries to find a replacement source based on the given parameters.
+    *
+    * @param sourcesConfig   the configuration with the available sources
+    * @param rankedSources   a map grouping radio sources by their ranking
+    * @param untilDate       the date until when the replacement is needed
+    * @param evaluateService the service to evaluate interval queries
+    * @param system          the actor system
+    * @return an ''Option'' with the result of the selection process
+    */
+  def selectReplacementSource(sourcesConfig: RadioSourceConfig,
+                              rankedSources: SortedMap[Int, Seq[RadioSource]],
+                              untilDate: LocalDateTime,
+                              evaluateService: EvaluateIntervalsService)
+                             (implicit system: ActorSystem): Future[Option[SelectedReplacementSource]]
+}
+
+/**
+  * The default implementation of [[ReplacementSourceSelectionService]].
+  *
+  * This implementation tries to find a random radio source with a possible
+  * high ranking that can be played the full time until the original source
+  * becomes available again. If no such source can be found, a random source
+  * with the highest ranking is searched that partly bridges the required time.
+  * If this fails, too, result is ''None''.
+  */
+object ReplacementSourceSelectionServiceImpl extends ReplacementSourceSelectionService {
+  /**
+    * A source of randomness used to select an arbitrary radio source if there
+    * are multiple with the same ranking.
+    */
+  private val random = new Random()
+
+  override def selectReplacementSource(sourcesConfig: RadioSourceConfig,
+                                       rankedSources: SortedMap[Int, Seq[RadioSource]],
+                                       untilDate: LocalDateTime,
+                                       evaluateService: EvaluateIntervalsService)
+                                      (implicit system: ActorSystem): Future[Option[SelectedReplacementSource]] = {
+    import system.dispatcher
+
+    // Look for full replacement sources.
+    val futFullReplacement = runSelectionStream(sourcesConfig,
+      rankedSources,
+      untilDate,
+      evaluateService) { (source, queryResult) =>
+      (queryResult match {
+        case After(_) => Some(source)
+        case Before(d) if d.value.isAfter(untilDate) || d.value == untilDate =>
+          Some(source)
+        case _ => None
+      }) map { selected => SelectedReplacementSource(selected, untilDate) }
+    }
+
+    // If no full replacement was found, also accept partial replacements.
+    // Currently all partial replacements are accepted, no further evaluation
+    // is done (e.g. to find one with a small forbidden range).
+    // TODO: The current implementation will re-evaluate interval queries
+    //  again; this should be prevented by reusing the results of the first
+    //  evaluation.
+    futFullReplacement flatMap { optReplacement =>
+      optReplacement.map(replacement => Future.successful(Some(replacement))) getOrElse {
+        runSelectionStream(sourcesConfig,
+          rankedSources,
+          untilDate,
+          evaluateService) { (source, queryResult) =>
+          queryResult match {
+            case Inside(d) if d.value.isAfter(untilDate) => None
+            case Before(start) => Some(SelectedReplacementSource(source, start.value))
+            case _ => Some(SelectedReplacementSource(source, untilDate))
+          }
+        }
+      }
+    }
+  }
+
+  /**
+    * Runs a stream to generate [[SelectedReplacementSource]] object using the
+    * given parameters. A function is provided that determines whether a source
+    * is selectable and how it should be represented as a selection result.
+    *
+    * @param sourcesConfig   the configuration of radio sources
+    * @param rankedSources   a map with radio sources ordered by their ranking
+    * @param untilDate       the until date when to play the source
+    * @param evaluateService the service to evaluate interval queries
+    * @param resultFunc      the function to generate result objects
+    * @param system          the actor system
+    * @return a future with the optional selected source
+    */
+  private def runSelectionStream(sourcesConfig: RadioSourceConfig,
+                                 rankedSources: SortedMap[Int, Seq[RadioSource]],
+                                 untilDate: LocalDateTime,
+                                 evaluateService: EvaluateIntervalsService)
+                                (resultFunc: (RadioSource, IntervalQueryResult) => Option[SelectedReplacementSource])
+                                (implicit system: ActorSystem): Future[Option[SelectedReplacementSource]] = {
+    import system.dispatcher
+    val streamSource = Source(rankedSources.values.toSeq)
+    val sink = Sink.headOption[SelectedReplacementSource]
+    streamSource.mapConcat(sources => random.shuffle(sources))
+      .mapAsync(4) { src =>
+        evaluateService.evaluateIntervals(sourcesConfig.exclusions(src), untilDate) map {
+          (src, _)
+        }
+      }.map { t => resultFunc(t._1, t._2) }
+      .filter(_.isDefined)
+      .map(_.get)
+      .runWith(sink)
+  }
+}
---
+++ radioPlayerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/radio/actors/schedule/ReplacementSourceSelectionServiceSpec.scala
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.actors.schedule
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import de.oliver_heger.linedj.AsyncTestHelper
+import de.oliver_heger.linedj.player.engine.interval.IntervalTypes._
+import de.oliver_heger.linedj.player.engine.interval.LazyDate
+import de.oliver_heger.linedj.player.engine.radio.actors.schedule.RadioSourceConfigTestHelper.radioSource
+import de.oliver_heger.linedj.player.engine.radio.actors.schedule.ReplacementSourceSelectionService.SelectedReplacementSource
+import de.oliver_heger.linedj.player.engine.radio.{RadioSource, RadioSourceConfig}
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import java.time.{LocalDateTime, Month}
+import scala.collection.immutable.{SortedMap, TreeMap}
+import scala.concurrent.Future
+
+object ReplacementSourceSelectionServiceSpec {
+  /** A reference date used for interval queries. */
+  private val ReferenceDate = LocalDateTime.of(2023, Month.JANUARY, 16, 21, 57, 59)
+
+  /**
+    * Convenience function to create a sorted map from a single radio source.
+    *
+    * @param source the radio source
+    * @return the sorted map containing only this source
+    */
+  private def singleSortedSource(source: RadioSource): SortedMap[Int, Seq[RadioSource]] = TreeMap(1 -> List(source))
+}
+
+/**
+  * Test class for [[ReplacementSourceSelectionService]].
+  */
+class ReplacementSourceSelectionServiceSpec(testSystem: ActorSystem) extends TestKit(testSystem) with AnyFlatSpecLike
+  with BeforeAndAfterAll with Matchers with MockitoSugar with AsyncTestHelper {
+  def this() = this(ActorSystem("ReplacementSourceSelectionService"))
+
+  override protected def afterAll(): Unit = {
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  import ReplacementSourceSelectionServiceSpec._
+  import system.dispatcher
+
+  "ReplacementSourceSelectionServiceImpl" should "return None if no sources are available" in {
+    val sourcesConfig = mock[RadioSourceConfig]
+    val helper = new SelectionServiceTestHelper
+
+    val result = helper.callSelectionService(sourcesConfig, SortedMap.empty)
+
+    result shouldBe empty
+  }
+
+  it should "select an available source that can be currently played" in {
+    val helper = new SelectionServiceTestHelper
+    val source = helper.createSourceWithQueries(1, After(_.plusHours(1)))
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(source))
+
+    val result = helper.callSelectionService(sourcesConfig, singleSortedSource(source._1))
+
+    result should be(Some(SelectedReplacementSource(source._1, ReferenceDate)))
+  }
+
+  it should "filter out sources that cannot be played" in {
+    val helper = new SelectionServiceTestHelper
+    val source = helper.createSourceWithQueries(1, Inside(new LazyDate(ReferenceDate.plusSeconds(1))))
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(source))
+
+    val result = helper.callSelectionService(sourcesConfig, singleSortedSource(source._1))
+
+    result shouldBe empty
+  }
+
+  it should "select a source that reaches a forbidden interval later" in {
+    val beforeDate = ReferenceDate.minusSeconds(1)
+    val helper = new SelectionServiceTestHelper
+    val source = helper.createSourceWithQueries(1, Before(new LazyDate(beforeDate)))
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(source))
+
+    val result = helper.callSelectionService(sourcesConfig, singleSortedSource(source._1))
+
+    result should be(Some(SelectedReplacementSource(source._1, beforeDate)))
+  }
+
+  it should "select a source that leaves a forbidden interval earlier" in {
+    val helper = new SelectionServiceTestHelper
+    val source = helper.createSourceWithQueries(1, Inside(new LazyDate(ReferenceDate.minusSeconds(1))))
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(source))
+
+    val result = helper.callSelectionService(sourcesConfig, singleSortedSource(source._1))
+
+    result should be(Some(SelectedReplacementSource(source._1, ReferenceDate)))
+  }
+
+  it should "select a random source of same ranking" in {
+    val queryResult = After(_ => ReferenceDate)
+    val Attempts = 32
+    val helper = new SelectionServiceTestHelper
+    val sources = (1 to 16) map { idx => helper.createSourceWithQueries(idx, queryResult) }
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(sources: _*))
+    val rankedSources = TreeMap(1 -> sourcesConfig.sources)
+
+    val selectedSources = (1 to Attempts).foldLeft(Set.empty[RadioSource]) { (set, _) =>
+      val result = helper.callSelectionService(sourcesConfig, rankedSources)
+      result should not be empty
+      set + result.get.source
+    }
+
+    selectedSources.size should be > 1
+  }
+
+  it should "select a source with highest ranking" in {
+    val queryResult = After(_ => ReferenceDate)
+    val Attempts = 32
+    val helper = new SelectionServiceTestHelper
+    val highlyRankedSource = helper.createSourceWithQueries(1, queryResult)
+    val otherSources = (2 to 16) map { idx => helper.createSourceWithQueries(idx, queryResult) }
+    val allSources = highlyRankedSource :: otherSources.toList
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(allSources: _*))
+    val rankedSources = TreeMap(0 -> List(highlyRankedSource._1),
+      1 -> otherSources.map(_._1))
+    val expectedResult = Some(SelectedReplacementSource(highlyRankedSource._1, ReferenceDate))
+
+    (1 to Attempts) foreach { _ =>
+      helper.callSelectionService(sourcesConfig, rankedSources) should be(expectedResult)
+    }
+  }
+
+  /**
+    * Tests whether a source with a lower ranking that can be played until the
+    * reference date is preferred over a partial replacement with a higher
+    * ranking.
+    *
+    * @param queryResult the query result for the replacement source
+    */
+  private def checkFullReplacementIsPreferred(queryResult: IntervalQueryResult): Unit = {
+    val helper = new SelectionServiceTestHelper
+    val partialReplacement = helper.createSourceWithQueries(1, Before(new LazyDate(ReferenceDate.minusSeconds(1))))
+    val fullReplacement = helper.createSourceWithQueries(2, queryResult)
+    val sourcesConfig = RadioSourceConfigTestHelper.createSourceConfig(Map(partialReplacement, fullReplacement))
+    val rankedSources = TreeMap(1 -> List(partialReplacement._1), 2 -> List(fullReplacement._1))
+
+    val result = helper.callSelectionService(sourcesConfig, rankedSources)
+
+    result should be(Some(SelectedReplacementSource(fullReplacement._1, ReferenceDate)))
+  }
+
+  it should "select a full replacement source with an After result over a higher ranked partial replacement" in {
+    checkFullReplacementIsPreferred(After(_ => ReferenceDate))
+  }
+
+  it should "select a full replacement source with a Before result over a higher ranked partial replacement" in {
+    checkFullReplacementIsPreferred(Before(new LazyDate(ReferenceDate.plusSeconds(1))))
+  }
+
+  it should "select a full replacement source with equal Before result over a higher ranked partial replacement" in {
+    checkFullReplacementIsPreferred(Before(new LazyDate(ReferenceDate)))
+  }
+
+  /**
+    * A test helper class managing the dependencies of the test service.
+    */
+  private class SelectionServiceTestHelper {
+    /** A mock for the service to evaluate interval queries. */
+    private val evaluateService = mock[EvaluateIntervalsService]
+
+    /**
+      * Returns a tuple with a radio source and a sequence of interval queries.
+      * The managed mock for the evaluation service is prepared to return the
+      * given query result when it is asked to evaluate the interval queries
+      * for this source.
+      *
+      * @param idx         the index of the radio source
+      * @param queryResult the query result to return for this source
+      * @return a tuple with the source and its interval queries
+      */
+    def createSourceWithQueries(idx: Int, queryResult: IntervalQueryResult): (RadioSource, Seq[IntervalQuery]) = {
+      val source = radioSource(idx)
+      val queries = mock[Seq[IntervalQuery]]
+      when(evaluateService.evaluateIntervals(queries, ReferenceDate)).thenReturn(Future.successful(queryResult))
+      source -> queries
+    }
+
+    /**
+      * Invokes the selection service under test with the given parameters.
+      *
+      * @param sourcesConfig the configuration with radio sources
+      * @param rankedSources the sources ordered by their rankings
+      * @return the result returned by the selection service
+      */
+    def callSelectionService(sourcesConfig: RadioSourceConfig,
+                             rankedSources: SortedMap[Int, Seq[RadioSource]]): Option[SelectedReplacementSource] =
+      futureResult(ReplacementSourceSelectionServiceImpl.selectReplacementSource(sourcesConfig, rankedSources,
+        ReferenceDate, evaluateService))
+  }
+}
