025be280be88b9c8cbff1cc427bd5ceaecd65ee2
Oliver Heger <oliver.heger@oliver-heger.de>
2023-02-11 16:21:40 +0100

    Player engine: Added PlaybackContextFactoryActor class.
    This is an actor implementation to manage the available playback
    context factories. It is going to be used by PlaybackActor, which will
    no longer be responsible for managing the factories itself.
    
    Letting the factories be managed by a dedicated actor makes the code
    cleaner. It also enables the sharing of factories between multiple
    player actors.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ playerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/actors/PlaybackContextFactoryActor.scala
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.actors
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.{actor => classic}
+import de.oliver_heger.linedj.player.engine.{PlaybackContext, PlaybackContextFactory}
+
+import java.io.InputStream
+
+/**
+  * An actor implementation that manages the [[PlaybackContextFactory]]s known
+  * by the player engine.
+  *
+  * The actor supports messages for adding and removing factories. In addition,
+  * it can be asked to create a [[PlaybackContext]] for a given audio stream.
+  */
+object PlaybackContextFactoryActor {
+  /**
+    * The base trait for the commands supported by this actor.
+    */
+  sealed trait PlaybackContextCommand
+
+  /**
+    * A command to add a new [[PlaybackContextFactory]].
+    *
+    * @param factory the factory to add
+    */
+  case class AddPlaybackContextFactory(factory: PlaybackContextFactory) extends PlaybackContextCommand
+
+  /**
+    * A command to remove a [[PlaybackContextFactory]].
+    *
+    * @param factory the factory to remove
+    */
+  case class RemovePlaybackContextFactory(factory: PlaybackContextFactory) extends PlaybackContextCommand
+
+  /**
+    * A command to request the creation of a [[PlaybackContext]] for an audio
+    * stream. The actor checks the managed factories whether one is able to
+    * handle the stream and create a context. The result is then sent back to
+    * the given receiver via a [[CreatePlaybackContextResult]] message.
+    *
+    * @param stream   the audio input stream
+    * @param uri      the URI associated with the stream
+    * @param receiver the receiver of the result
+    */
+  case class CreatePlaybackContext(stream: InputStream,
+                                   uri: String,
+                                   receiver: classic.ActorRef) extends PlaybackContextCommand
+
+  /**
+    * A command telling this actor to stop itself.
+    */
+  case object Stop extends PlaybackContextCommand
+
+  /**
+    * A message sent by this actor as response to a [[CreatePlaybackContext]]
+    * message. The context creation can fail; therefore, this class stores an
+    * ''Option''.
+    *
+    * @param optContext the result of the creation
+    */
+  case class CreatePlaybackContextResult(optContext: Option[PlaybackContext])
+
+  /**
+    * A specialized implementation of a [[PlaybackContextFactory]] which contains
+    * a number of sub factories.
+    *
+    * When a new [[PlaybackContext]] is to be created the sub factories are
+    * invoked one after each other until a one is found that returns a valid
+    * context object. This result is then returned.
+    *
+    * There are methods for adding and removing sub factories. These methods do
+    * not change the current instance, but create a new one with an updated list
+    * of sub factories.
+    */
+  private class CombinedPlaybackContextFactory(val subFactories: List[PlaybackContextFactory])
+    extends PlaybackContextFactory {
+    /**
+      * Creates a new instance of ''CombinedPlaybackContextFactory'' which has the
+      * specified ''PlaybackContextFactory'' as a sub factory.
+      *
+      * @param factory the factory to be added
+      * @return a new combined factory containing the specified sub factory
+      */
+    def addSubFactory(factory: PlaybackContextFactory): CombinedPlaybackContextFactory =
+      new CombinedPlaybackContextFactory(factory :: subFactories)
+
+    /**
+      * Creates a new instance of ''CombinedPlaybackContextFactory'' which does
+      * not contain the specified ''PlaybackContextFactory'' as a sub factory.
+      *
+      * @param factory the factory to be removed
+      * @return a new combined factory without the specified sub factory
+      */
+    def removeSubFactory(factory: PlaybackContextFactory): CombinedPlaybackContextFactory =
+      new CombinedPlaybackContextFactory(subFactories filterNot (_ == factory))
+
+    /**
+      * @inheritdoc This implementation iterates over the contained sub factories
+      *             (in no specific order). The first ''PlaybackContext'' which is
+      *             created is returned.
+      */
+    override def createPlaybackContext(stream: InputStream, uri: String): Option[PlaybackContext] = {
+      subFactories.foldLeft[Option[PlaybackContext]](None) { (optCtx, f) =>
+        optCtx orElse f.createPlaybackContext(stream, uri)
+      }
+    }
+  }
+
+  /**
+    * Returns a ''Behavior'' to create a new actor instance.
+    *
+    * @return the ''Behavior'' for the new instance
+    */
+  def apply(): Behavior[PlaybackContextCommand] =
+    handle(new CombinedPlaybackContextFactory(Nil))
+
+  /**
+    * The main message handling function of this actor.
+    *
+    * @param combinedFactory the combined factory storing all currently
+    *                        available factories
+    * @return the updated ''Behavior''
+    */
+  private def handle(combinedFactory: CombinedPlaybackContextFactory): Behavior[PlaybackContextCommand] =
+    Behaviors.receiveMessage {
+      case AddPlaybackContextFactory(factory) =>
+        handle(combinedFactory.addSubFactory(factory))
+
+      case RemovePlaybackContextFactory(factory) =>
+        handle(combinedFactory.removeSubFactory(factory))
+
+      case CreatePlaybackContext(stream, uri, receiver) =>
+        val context = combinedFactory.createPlaybackContext(stream, uri)
+        receiver ! CreatePlaybackContextResult(context)
+        Behaviors.same
+
+      case Stop =>
+        Behaviors.stopped
+    }
+}
---
+++ playerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/actors/PlaybackContextFactoryActorSpec.scala
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.actors
+
+import akka.actor.ActorSystem
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.testkit.{ImplicitSender, TestKit}
+import de.oliver_heger.linedj.player.engine.{PlaybackContext, PlaybackContextFactory}
+import org.mockito.ArgumentMatchers.{any, anyString}
+import org.mockito.Mockito.{verify, verifyNoInteractions, when}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import java.io.InputStream
+
+/**
+  * Test class for [[PlaybackContextFactoryActor]].
+  */
+class PlaybackContextFactoryActorSpec(testSystem: ActorSystem) extends TestKit(testSystem) with ImplicitSender
+  with AnyFlatSpecLike with BeforeAndAfterAll with Matchers with MockitoSugar {
+  def this() = this(ActorSystem("PlaybackContextFactoryActorSpec"))
+
+  /** A test URI. */
+  private val Uri = "Some test URI to a test file"
+
+  /** The test kit for testing typed actors. */
+  private val testKit = ActorTestKit()
+
+  override protected def afterAll(): Unit = {
+    testKit.shutdownTestKit()
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  /**
+    * Returns a mock for a factory. The mock is initialized to return None.
+    *
+    * @return the mock sub factory
+    */
+  private def createFactory(): PlaybackContextFactory = {
+    val subFactory = mock[PlaybackContextFactory]
+    when(subFactory.createPlaybackContext(any(classOf[InputStream]), anyString())).thenReturn(None)
+    subFactory
+  }
+
+  "PlaybackContextFactoryActor" should "return None if there are no factories" in {
+    val actor = testKit.spawn(PlaybackContextFactoryActor())
+
+    actor ! PlaybackContextFactoryActor.CreatePlaybackContext(mock[InputStream], Uri, testActor)
+
+    expectMsg(PlaybackContextFactoryActor.CreatePlaybackContextResult(None))
+  }
+
+  it should "invoke the available factories" in {
+    val factory1 = createFactory()
+    val factory2 = createFactory()
+    val stream = mock[InputStream]
+    val actor = testKit.spawn(PlaybackContextFactoryActor())
+
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory1)
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory2)
+    actor ! PlaybackContextFactoryActor.CreatePlaybackContext(stream, Uri, testActor)
+
+    expectMsg(PlaybackContextFactoryActor.CreatePlaybackContextResult(None))
+    verify(factory1).createPlaybackContext(stream, Uri)
+    verify(factory2).createPlaybackContext(stream, Uri)
+  }
+
+  it should "return the first valid playback context" in {
+    val factory1 = createFactory()
+    val factory2 = createFactory()
+    val factory3 = createFactory()
+    val stream = mock[InputStream]
+    val context = mock[PlaybackContext]
+    when(factory2.createPlaybackContext(stream, Uri)).thenReturn(Some(context))
+    val actor = testKit.spawn(PlaybackContextFactoryActor())
+
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory1)
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory2)
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory3)
+    actor ! PlaybackContextFactoryActor.CreatePlaybackContext(stream, Uri, testActor)
+
+    expectMsg(PlaybackContextFactoryActor.CreatePlaybackContextResult(Some(context)))
+    verify(factory2).createPlaybackContext(stream, Uri)
+    verifyNoInteractions(factory1)
+  }
+
+  it should "support removing a factory" in {
+    val factory1 = createFactory()
+    val factory2 = createFactory()
+    val stream = mock[InputStream]
+    val actor = testKit.spawn(PlaybackContextFactoryActor())
+
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory1)
+    actor ! PlaybackContextFactoryActor.AddPlaybackContextFactory(factory2)
+    actor ! PlaybackContextFactoryActor.RemovePlaybackContextFactory(factory2)
+    actor ! PlaybackContextFactoryActor.CreatePlaybackContext(stream, Uri, testActor)
+
+    expectMsg(PlaybackContextFactoryActor.CreatePlaybackContextResult(None))
+    verifyNoInteractions(factory2)
+  }
+
+  it should "stop itself on receiving a Stop command" in {
+    val actor = testKit.spawn(PlaybackContextFactoryActor())
+
+    actor ! PlaybackContextFactoryActor.Stop
+
+    val probeWatcher = testKit.createDeadLetterProbe()
+    probeWatcher.expectTerminated(actor)
+  }
+}
