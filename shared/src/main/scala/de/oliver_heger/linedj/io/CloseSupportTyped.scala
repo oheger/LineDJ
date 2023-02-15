b5c034d9afd6c4da028827be08b14669e7a3acb5
Oliver Heger <oliver.heger@oliver-heger.de>
2023-02-15 21:40:19 +0100

    Shared: Added close support for typed client actors.
    The new CloseSupportTyped module implements similar functionality for
    closing dependencies for typed actors than the CloseSupport trait.
    
    This is going to be used by the radio engine when testing whether
    radio sources are available.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ shared/src/main/scala/de/oliver_heger/linedj/io/CloseSupportTyped.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.io
+
+import akka.actor.Actor
+import akka.{actor => classic}
+import akka.actor.typed.ActorRef
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.adapter._
+import de.oliver_heger.linedj.io.CloseHandlerActor.CloseComplete
+import de.oliver_heger.linedj.utils.ChildActorFactory
+
+/**
+  * A module offering functionality related to the handling of actors
+  * supporting the close protocol.
+  *
+  * The functionality provided here is similar to what [[CloseSupport]] has for
+  * classic actors. Basically, there is a function to trigger and control the
+  * closing of a number of dependent actors. The close operation happens
+  * asynchronously, and when it is done, the triggering typed actor is sent a
+  * configurable message.
+  */
+object CloseSupportTyped {
+  /**
+    * Triggers a close operation on the given dependencies. This function sends
+    * a close request to each actor in the dependencies and waits for their
+    * responses. Only after all Ack messages have arrived (or the actors have
+    * died), the provided message is sent to the client actor, indicating the
+    * completion of the operation.
+    *
+    * @param context      the context of the client actor
+    * @param client       the actor to notify when the operation is complete
+    * @param ackMessage   the message to sent to the client actor
+    * @param dependencies the actors to close
+    * @tparam M the type of notify message
+    */
+  def triggerClose[M](context: ActorContext[M],
+                      client: ActorRef[M],
+                      ackMessage: M,
+                      dependencies: Iterable[classic.ActorRef]): Unit = {
+    context.actorOf(
+      classic.Props(new Actor with ChildActorFactory with CloseSupport with classic.ActorLogging {
+        override def preStart(): Unit = {
+          onCloseRequest(self, dependencies, self, this)
+          log.info("Triggering typed close request for {}.", client.path)
+        }
+
+        override def receive: Receive = {
+          case CloseComplete =>
+            log.info("CloseComplete received for {}.", client.path)
+            client ! ackMessage
+            context stop self
+        }
+      }))
+  }
+}
---
+++ shared/src/test/scala/de/oliver_heger/linedj/io/CloseSupportTypedSpec.scala
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.io
+
+import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.testkit.{TestKit, TestProbe}
+import akka.{actor => classic}
+import de.oliver_heger.linedj.io.CloseSupportTypedSpec.{CloseTestResult, closeTestActor}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+object CloseSupportTypedSpec {
+  /**
+    * A trait defining the commands of the close test actor.
+    */
+  sealed trait CloseTestCommand
+
+  /**
+    * A command indicating a completed close operation.
+    */
+  case object CloseComplete extends CloseTestCommand
+
+  /**
+    * A confirmation message sent by the close test actor when receiving the
+    * message that the close operation is complete. The message contains the
+    * children of the test actor. This can be used to check whether temporary
+    * actors involved in the close operation have all terminated.
+    *
+    * @param remainingChildren the remaining children of the test actor
+    */
+  case class CloseTestResult(remainingChildren: Iterable[ActorRef[Nothing]])
+
+  /**
+    * Returns the behavior of an actor to test a close operation. The actor
+    * triggers the operation on the given dependencies. When it is successful
+    * a notification to the client actor is sent.
+    *
+    * @param client       the actor monitoring the test
+    * @param dependencies the dependencies to be closed
+    * @return the behavior for the close test actor
+    */
+  private def closeTestActor(client: ActorRef[CloseTestResult],
+                             dependencies: Iterable[classic.ActorRef]): Behavior[CloseTestCommand] =
+    Behaviors.setup { context =>
+      CloseSupportTyped.triggerClose(context, context.self, CloseComplete, dependencies)
+
+      Behaviors.receiveMessage {
+        case CloseComplete =>
+          client ! CloseTestResult(context.children)
+          Behaviors.same
+      }
+    }
+}
+
+/**
+  * Test class for [[CloseSupportTyped]].
+  */
+class CloseSupportTypedSpec(testSystem: classic.ActorSystem) extends TestKit(testSystem) with AnyFlatSpecLike
+  with BeforeAndAfterAll with Matchers {
+  def this() = this(classic.ActorSystem("CloseSupportTypedSpec"))
+
+  /** The test kit to test typed actors. */
+  private val testKit = ActorTestKit()
+
+  override protected def afterAll(): Unit = {
+    testKit.shutdownTestKit()
+    TestKit shutdownActorSystem system
+    super.afterAll()
+  }
+
+  "CloseSupportTyped" should "control a close operation" in {
+    val dep1 = TestProbe()
+    val dep2 = TestProbe()
+    val client = testKit.createTestProbe[CloseTestResult]()
+
+    testKit.spawn(closeTestActor(client.ref, List(dep1.ref, dep2.ref)))
+
+    dep1.expectMsg(CloseRequest)
+    dep1.reply(CloseAck(dep1.ref))
+    dep2.expectMsg(CloseRequest)
+    dep2.reply(CloseAck(dep2.ref))
+    client.expectMessageType[CloseTestResult]
+  }
+
+  it should "not send the completion message before all dependencies are closed" in {
+    val dep1 = TestProbe()
+    val dep2 = TestProbe()
+    val client = testKit.createTestProbe[CloseTestResult]()
+
+    testKit.spawn(closeTestActor(client.ref, List(dep1.ref, dep2.ref)))
+
+    dep1.expectMsg(CloseRequest)
+    dep1.reply(CloseAck(dep1.ref))
+    client.expectNoMessage(250.millis)
+  }
+
+  it should "handle dependencies that died" in {
+    val dep1 = TestProbe()
+    val dep2 = TestProbe()
+    val client = testKit.createTestProbe[CloseTestResult]()
+
+    testKit.spawn(closeTestActor(client.ref, List(dep1.ref, dep2.ref)))
+
+    dep1.expectMsg(CloseRequest)
+    dep1.reply(CloseAck(dep1.ref))
+    system.stop(dep2.ref)
+    client.expectMessageType[CloseTestResult]
+  }
+
+  it should "stop the internal child actor controlling the close operation" in {
+    val dep = TestProbe()
+    val client = testKit.createTestProbe[CloseTestResult]()
+
+    testKit.spawn(closeTestActor(client.ref, List(dep.ref)))
+    dep.expectMsg(CloseRequest)
+    dep.reply(CloseAck(dep.ref))
+
+    lazy val watcherProbe = testKit.createDeadLetterProbe()
+    val result = client.expectMessageType[CloseTestResult]
+    result.remainingChildren foreach { child =>
+      watcherProbe.expectTerminated(child)
+    }
+  }
+}
