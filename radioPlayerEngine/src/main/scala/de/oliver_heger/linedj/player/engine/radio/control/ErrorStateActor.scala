4bd61514008caf50c9084f2a65f2f45c0350c40f
Oliver Heger <oliver.heger@oliver-heger.de>
2023-02-24 22:30:23 +0100

    ErrorStateActor: Added an actor to check successful playback.
    This actor creates a source and a playback actor and checks whether a
    specific radio source can be played. This check is executed
    periodically for radio sources in error state to find out when they
    are available again.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- radioPlayerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/radio/control/ErrorStateActor.scala
+++ radioPlayerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/radio/control/ErrorStateActor.scala
@@ -16,13 +16,15 @@
 
 package de.oliver_heger.linedj.player.engine.radio.control
 
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.adapter._
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior, Props}
 import akka.{actor => classic}
+import de.oliver_heger.linedj.io.CloseSupportTyped
 import de.oliver_heger.linedj.player.engine.actors.{LineWriterActor, PlaybackActor, PlaybackContextFactoryActor}
-import de.oliver_heger.linedj.player.engine.radio.RadioEvent
 import de.oliver_heger.linedj.player.engine.radio.stream.RadioDataSourceActor
-import de.oliver_heger.linedj.player.engine.{ActorCreator, PlayerConfig, PlayerEvent}
+import de.oliver_heger.linedj.player.engine.radio.{RadioEvent, RadioPlaybackContextCreationFailedEvent, RadioPlaybackErrorEvent, RadioSource, RadioSourceErrorEvent}
+import de.oliver_heger.linedj.player.engine._
 
 import scala.concurrent.duration._
 
@@ -102,6 +104,136 @@ object ErrorStateActor {
   }
 
   /**
+    * A message processed by the radio source check actor that indicates that
+    * the radio source in question is now functional again.
+    */
+  private[control] case class RadioSourceCheckSuccessful()
+
+  /**
+    * The base command trait of an internal actor that checks whether playback
+    * of a specific radio source is currently possible.
+    */
+  private[control] sealed trait CheckPlaybackCommand
+
+  /**
+    * A command received by the check playback actor that tells it to stop the
+    * ongoing test due to a timeout. The test is considered as failed.
+    */
+  private[control] case object CheckTimeout extends CheckPlaybackCommand
+
+  /**
+    * A command received by the check playback actor that tells it that the
+    * ongoing close operation is complete.
+    */
+  private[control] case object CloseComplete extends CheckPlaybackCommand
+
+  /**
+    * A command received by the check playback actor informing it that one of
+    * the actors for playing audio data died. This should cause the check to
+    * fail.
+    */
+  private[control] case object PlaybackActorDied extends CheckPlaybackCommand
+
+  /**
+    * A command received by the check playback actor when an event was fired
+    * during playback of the radio source to check. This event determines the
+    * result of the check.
+    *
+    * @param event the event
+    */
+  private[control] case class PlaybackEventReceived(event: AnyRef) extends CheckPlaybackCommand
+
+  /**
+    * A trait defining a factory function for creating an internal actor
+    * instance to check whether playback of a specific radio source is
+    * currently possible. The actor sets up the infrastructure to play this
+    * source. If this is successful, it sends a corresponding success message
+    * to the receiver actor. In all cases, it stops itself after the test.
+    */
+  private[control] trait CheckPlaybackActorFactory {
+    /**
+      * Returns a ''Behavior'' to create an instance of the actor to check a
+      * specific radio source.
+      *
+      * @param radioSource           the radio source to be checked
+      * @param namePrefix            the prefix to generate actor names
+      * @param factoryActor          the playback context factory actor
+      * @param config                the configuration for the audio player
+      * @param playbackActorsFactory factory for creating playback actors
+      * @return the ''Behavior'' for the check playback actor
+      */
+    def apply(receiver: ActorRef[RadioSourceCheckSuccessful],
+              radioSource: RadioSource,
+              namePrefix: String,
+              factoryActor: ActorRef[PlaybackContextFactoryActor.PlaybackContextCommand],
+              config: PlayerConfig,
+              playbackActorsFactory: PlaybackActorsFactory = ErrorStateActor.playbackActorsFactory):
+    Behavior[CheckPlaybackCommand]
+  }
+
+  /**
+    * A default [[CheckPlaybackActorFactory]] implementation that can be used
+    * to create instances of the check playback actor.
+    */
+  private[control] val checkPlaybackBehavior: CheckPlaybackActorFactory =
+    (receiver: ActorRef[RadioSourceCheckSuccessful],
+     radioSource: RadioSource,
+     namePrefix: String,
+     factoryActor: ActorRef[PlaybackContextFactoryActor.PlaybackContextCommand],
+     config: PlayerConfig,
+     playbackActorsFactory: PlaybackActorsFactory) => Behaviors.setup { context =>
+      context.log.info("Checking error state of radio source {}.", radioSource)
+      val playerEventAdapter = context.messageAdapter[PlayerEvent] { event => PlaybackEventReceived(event) }
+      val radioEventAdapter = context.messageAdapter[RadioEvent] { event => PlaybackEventReceived(event) }
+
+      val playbackActors = playbackActorsFactory.createPlaybackActors(namePrefix,
+        playerEventAdapter,
+        radioEventAdapter,
+        factoryActor,
+        config,
+        childActorCreator(context))
+      playbackActors.sourceActor ! radioSource
+      playbackActors.playActor ! PlaybackActor.StartPlayback
+      context.watchWith(playbackActors.sourceActor, PlaybackActorDied)
+      context.watchWith(playbackActors.playActor, PlaybackActorDied)
+
+      def checking(): Behavior[CheckPlaybackCommand] =
+        Behaviors.receiveMessagePartial {
+          case CheckTimeout =>
+            closeAndStop()
+
+          case PlaybackActorDied =>
+            closeAndStop()
+
+          case PlaybackEventReceived(event) =>
+            val success = isSuccessEvent(event)
+            val error = isErrorEvent(event)
+            if (success || error) {
+              if (success) receiver ! RadioSourceCheckSuccessful()
+              closeAndStop()
+            } else Behaviors.same
+        }
+
+      def closing(): Behavior[CheckPlaybackCommand] =
+        Behaviors.receiveMessagePartial {
+          case CloseComplete =>
+            Behaviors.stopped
+
+          case m =>
+            context.log.info("Ignoring message {} while waiting for CloseAck.", m)
+            Behaviors.same
+        }
+
+      def closeAndStop(): Behavior[CheckPlaybackCommand] = {
+        CloseSupportTyped.triggerClose(context, context.self, CloseComplete,
+          List(playbackActors.sourceActor, playbackActors.playActor))
+        closing()
+      }
+
+      checking()
+    }
+
+  /**
     * Returns the behavior for a dummy line writer actor. This behavior just
     * simulates writing, so that playback progress events are generated.
     *
@@ -116,5 +248,51 @@ object ErrorStateActor {
       case LineWriterActor.DrainLine(_, replayTo) =>
         replayTo ! LineWriterActor.LineDrained
         Behaviors.same
+    }
+
+  /**
+    * Creates an [[ActorCreator]] that uses the given context to create child
+    * actors of the owning actor.
+    *
+    * @param context the context
+    * @tparam M the type of the actor
+    * @return the [[ActorCreator]] using this context
+    */
+  private def childActorCreator[M](context: ActorContext[M]): ActorCreator =
+    new ActorCreator {
+      override def createActor[T](behavior: Behavior[T],
+                                  name: String,
+                                  optStopCommand: Option[T],
+                                  props: Props): ActorRef[T] = context.spawn(behavior, name, props)
+
+      override def createActor(props: classic.Props, name: String): classic.ActorRef = context.actorOf(props, name)
+    }
+
+  /**
+    * Checks whether the given event indicates a successful playback.
+    *
+    * @param event the event in question
+    * @return a flag whether this is a success event
+    */
+  private def isSuccessEvent(event: AnyRef): Boolean =
+    event match {
+      case _: PlaybackProgressEvent => true
+      case _ => false
+    }
+
+  /**
+    * Checks whether the given event indicates a playback error.
+    *
+    * @param event the event in question
+    * @return a flag whether this is an error event
+    */
+  private def isErrorEvent(event: AnyRef): Boolean =
+    event match {
+      case _: PlaybackContextCreationFailedEvent => true
+      case _: PlaybackErrorEvent => true
+      case _: RadioSourceErrorEvent => true
+      case _: RadioPlaybackErrorEvent => true
+      case _: RadioPlaybackContextCreationFailedEvent => true
+      case _ => false
     }
 }
--- radioPlayerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/radio/control/ErrorStateActorSpec.scala
+++ radioPlayerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/radio/control/ErrorStateActorSpec.scala
@@ -16,18 +16,24 @@
 
 package de.oliver_heger.linedj.player.engine.radio.control
 
+import akka.actor.Actor
 import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe => TypedTestProbe}
+import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior, Props}
 import akka.testkit.{TestKit, TestProbe}
 import akka.util.ByteString
 import akka.{actor => classic}
+import de.oliver_heger.linedj.io.{CloseAck, CloseRequest}
 import de.oliver_heger.linedj.player.engine.actors.{LineWriterActor, PlaybackActor, PlaybackContextFactoryActor}
-import de.oliver_heger.linedj.player.engine.radio.RadioEvent
+import de.oliver_heger.linedj.player.engine.radio.{RadioEvent, RadioPlaybackContextCreationFailedEvent, RadioPlaybackErrorEvent, RadioPlaybackProgressEvent, RadioSource, RadioSourceChangedEvent, RadioSourceErrorEvent, RadioSourceReplacementEndEvent, RadioSourceReplacementStartEvent}
 import de.oliver_heger.linedj.player.engine.radio.stream.RadioDataSourceActor
-import de.oliver_heger.linedj.player.engine.{ActorCreator, PlayerConfig, PlayerEvent}
+import de.oliver_heger.linedj.player.engine.{ActorCreator, AudioSource, AudioSourceFinishedEvent, AudioSourceStartedEvent, PlaybackContextCreationFailedEvent, PlaybackErrorEvent, PlaybackProgressEvent, PlayerConfig, PlayerEvent}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
+
+import java.util.concurrent.atomic.AtomicReference
+import scala.concurrent.duration._
 
 object ErrorStateActorSpec {
   /** A prefix for actor names. */
@@ -35,6 +41,9 @@ object ErrorStateActorSpec {
 
   /** A test player configuration. */
   private val TestPlayerConfig = PlayerConfig(actorCreator = null, mediaManagerActor = null)
+
+  /** A radio source used by tests */
+  private val TestRadioSource = RadioSource("http://radio.example.org/music.mp3")
 }
 
 /**
@@ -54,6 +63,17 @@ class ErrorStateActorSpec(testSystem: classic.ActorSystem) extends TestKit(testS
   }
 
   import ErrorStateActorSpec._
+
+  /**
+    * Tests whether the given actor has been stopped.
+    *
+    * @param actor the actor to check
+    * @tparam T the message type of this actor
+    */
+  private def checkStopped[T](actor: ActorRef[T]): Unit = {
+    val probe = testKit.createDeadLetterProbe()
+    probe.expectTerminated(actor)
+  }
 
   "playbackActorsFactory" should "create correct playback actors" in {
     val creator = new ActorCreatorForPlaybackActors
@@ -86,6 +106,144 @@ class ErrorStateActorSpec(testSystem: classic.ActorSystem) extends TestKit(testS
     creator.lineWriterActor ! LineWriterActor.DrainLine(null, probe.ref)
 
     probe.expectMsg(LineWriterActor.LineDrained)
+  }
+
+  "Check playback actor" should "trigger the check of the current radio source" in {
+    val factory = new PlaybackActorsFactoryTestImpl
+
+    factory.createCheckPlaybackActor()
+
+    factory.probeSourceActor.expectMsg(TestRadioSource)
+    factory.probePlayActor.expectMsg(PlaybackActor.StartPlayback)
+  }
+
+  it should "handle a timeout command" in {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+
+    checkActor ! ErrorStateActor.CheckTimeout
+
+    factory.expectCheckResult(checkActor, success = false)
+  }
+
+  it should "trigger only a single close operation and wait for its completion" in {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+    val deadLetters = testKit.createDeadLetterProbe()
+    checkActor ! ErrorStateActor.CheckTimeout
+    factory.expectCloseRequest(factory.probeSourceActor, reply = false)
+
+    checkActor ! ErrorStateActor.CheckTimeout
+
+    factory.probeSourceActor.expectNoMessage(250.millis)
+    deadLetters.expectNoMessage(250.millis)
+  }
+
+  it should "stop itself if the source actor dies" in {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+
+    system.stop(factory.probeSourceActor.ref)
+
+    factory.expectCloseRequest(factory.probePlayActor)
+      .expectNoSuccess()
+    checkStopped(checkActor)
+  }
+
+  it should "stop itself if the play actor dies" in {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+
+    system.stop(factory.probePlayActor.ref)
+
+    factory.expectCloseRequest(factory.probeSourceActor)
+      .expectNoSuccess()
+    checkStopped(checkActor)
+  }
+
+  it should "report a successful check on receiving a playback progress event" in {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+
+    factory.playerEventActor ! PlaybackProgressEvent(1024L, 1.second, AudioSource("test", 10000, 0, 0))
+
+    factory.expectCheckResult(checkActor, success = true)
+  }
+
+  it should "ignore irrelevant player events" in {
+    val audioSource = AudioSource.infinite(TestRadioSource.uri)
+    val events = List(AudioSourceStartedEvent(audioSource), AudioSourceFinishedEvent(audioSource))
+    val factory = new PlaybackActorsFactoryTestImpl
+    factory.createCheckPlaybackActor()
+
+    events foreach factory.playerEventActor.!
+
+    factory.probePlayActor.expectMsgType[AnyRef] // The original start playback message
+    factory.probePlayActor.expectNoMessage(200.millis)
+  }
+
+  it should "ignore irrelevant radio events" in {
+    val events = List(RadioSourceChangedEvent(TestRadioSource),
+      RadioSourceReplacementStartEvent(TestRadioSource, TestRadioSource),
+      RadioSourceReplacementEndEvent(TestRadioSource),
+      RadioPlaybackProgressEvent(TestRadioSource, 1024L, 1.second))
+    val factory = new PlaybackActorsFactoryTestImpl
+    factory.createCheckPlaybackActor()
+
+    events foreach factory.radioEventActor.!
+
+    factory.probePlayActor.expectMsgType[AnyRef] // The original start playback message
+    factory.probePlayActor.expectNoMessage(200.millis)
+  }
+
+  /**
+    * Checks whether an error event sent to the player event actor is correctly
+    * detected.
+    *
+    * @param event the event
+    */
+  private def checkErrorPlayerEvent(event: PlayerEvent): Unit = {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+
+    factory.playerEventActor ! event
+
+    factory.expectCheckResult(checkActor, success = false)
+  }
+
+  it should "handle a PlaybackContextCreationFailedEvent event" in {
+    checkErrorPlayerEvent(PlaybackContextCreationFailedEvent(AudioSource.infinite(TestRadioSource.uri)))
+  }
+
+  it should "handle a PlaybackErrorEvent event" in {
+    checkErrorPlayerEvent(PlaybackErrorEvent(AudioSource.infinite(TestRadioSource.uri)))
+  }
+
+  /**
+    * Checks whether an error event sent to the radio event actor is correctly
+    * detected.
+    *
+    * @param event the event
+    */
+  private def checkErrorRadioEvent(event: RadioEvent): Unit = {
+    val factory = new PlaybackActorsFactoryTestImpl
+    val checkActor = factory.createCheckPlaybackActor()
+
+    factory.radioEventActor ! event
+
+    factory.expectCheckResult(checkActor, success = false)
+  }
+
+  it should "handle a RadioSourceErrorEvent" in {
+    checkErrorRadioEvent(RadioSourceErrorEvent(TestRadioSource))
+  }
+
+  it should "handle a RadioPlaybackContextCreationFailedEvent" in {
+    checkErrorRadioEvent(RadioPlaybackContextCreationFailedEvent(TestRadioSource))
+  }
+
+  it should "handle a RadioPlaybackErrorEvent" in {
+    checkErrorRadioEvent(RadioPlaybackErrorEvent(TestRadioSource))
   }
 
   /**
@@ -148,6 +306,188 @@ class ErrorStateActorSpec(testSystem: classic.ActorSystem) extends TestKit(testS
           props should be(expProps)
           probePlayActor.ref
       }
+    }
+  }
+
+  /**
+    * A test implementation of the factory for playback actors. This class
+    * manages test probes for the playback actors to check the interaction with
+    * them.
+    */
+  private class PlaybackActorsFactoryTestImpl extends ErrorStateActor.PlaybackActorsFactory {
+    /** Test probe for the radio data source actor. */
+    val probeSourceActor: TestProbe = TestProbe()
+
+    /** Test probe for the playback actor. */
+    val probePlayActor: TestProbe = TestProbe()
+
+    /** Test probe for the playback context factory actor. */
+    private val probeFactoryActor = testKit.createTestProbe[PlaybackContextFactoryActor.PlaybackContextCommand]()
+
+    /**
+      * Test probe for the actor receiving the result of a radio source check.
+      */
+    private val probeReceiverActor = testKit.createTestProbe[ErrorStateActor.RadioSourceCheckSuccessful]()
+
+    /** Stores the actor for receiving player events. */
+    private val refPlayerEventActor = new AtomicReference[ActorRef[PlayerEvent]]
+
+    /** Stores the actor for receiving radio events. */
+    private val refRadioEventActor = new AtomicReference[ActorRef[RadioEvent]]
+
+    /**
+      * Creates a check playback actor instance that is configured with test
+      * settings and this factory.
+      *
+      * @return the check actor instance
+      */
+    def createCheckPlaybackActor(): ActorRef[ErrorStateActor.CheckPlaybackCommand] = {
+      val behavior = ErrorStateActor.checkPlaybackBehavior(probeReceiverActor.ref, TestRadioSource, ActorNamePrefix,
+        probeFactoryActor.ref, TestPlayerConfig, this)
+      testKit.spawn(behavior)
+    }
+
+    /**
+      * Expects that a success message was sent to the receiver actor.
+      *
+      * @return this factory
+      */
+    def expectSuccess(): PlaybackActorsFactoryTestImpl = {
+      probeReceiverActor.expectMessage(ErrorStateActor.RadioSourceCheckSuccessful())
+      this
+    }
+
+    /**
+      * Expects that no success message was sent to the receiver actor.
+      *
+      * @return this factory
+      */
+    def expectNoSuccess(): PlaybackActorsFactoryTestImpl = {
+      probeReceiverActor.expectNoMessage(250.millis)
+      this
+    }
+
+    /**
+      * Expects that the given probe receives a close request. Optionally, the
+      * request is answered with an ACK.
+      *
+      * @param probe the test probe
+      * @param reply flag whether to reply with an ACK
+      * @return this factory
+      */
+    def expectCloseRequest(probe: TestProbe, reply: Boolean = true): PlaybackActorsFactoryTestImpl = {
+      probe.fishForSpecificMessage() {
+        case CloseRequest => CloseRequest
+      }
+      if (reply) {
+        probe.reply(CloseAck(probe.ref))
+      }
+      this
+    }
+
+    /**
+      * Expects that the check actor triggers a close operation and stops
+      * itself with a result determined by the success flag.
+      *
+      * @param checkActor the check actor
+      * @param success    the success flag
+      */
+    def expectCheckResult(checkActor: ActorRef[ErrorStateActor.CheckPlaybackCommand], success: Boolean): Unit = {
+      expectCloseRequest(probeSourceActor)
+      expectCloseRequest(probePlayActor)
+      if (success) expectSuccess() else expectNoSuccess()
+      checkStopped(checkActor)
+    }
+
+    /**
+      * Returns the actor for player events passed to this factory.
+      *
+      * @return the actor for receiving player events
+      */
+    def playerEventActor: ActorRef[PlayerEvent] = fetchFactoryParameter(refPlayerEventActor)
+
+    /**
+      * Returns the actor for radio events passed to this factory.
+      *
+      * @return the actor for receiving radio events
+      */
+    def radioEventActor: ActorRef[RadioEvent] = fetchFactoryParameter(refRadioEventActor)
+
+    override def createPlaybackActors(namePrefix: String,
+                                      playerEventActor: ActorRef[PlayerEvent],
+                                      radioEventActor: ActorRef[RadioEvent],
+                                      factoryActor: ActorRef[PlaybackContextFactoryActor.PlaybackContextCommand],
+                                      config: PlayerConfig,
+                                      creator: ActorCreator): ErrorStateActor.PlaybackActorsFactoryResult = {
+      namePrefix should be(ActorNamePrefix)
+      config should be(TestPlayerConfig)
+      refPlayerEventActor.set(playerEventActor)
+      refRadioEventActor.set(radioEventActor)
+      testActorCreatorTyped(creator)
+      testActorCreatorClassic(creator)
+
+      ErrorStateActor.PlaybackActorsFactoryResult(sourceActor = probeSourceActor.ref,
+        playActor = probePlayActor.ref)
+    }
+
+    /**
+      * Tests whether the [[ActorCreator]] passed to this factory can be used
+      * to create a typed actor.
+      *
+      * @param creator the creator to test
+      */
+    private def testActorCreatorTyped(creator: ActorCreator): Unit = {
+      val Message = new Object
+      val ActorName = ActorNamePrefix + "typedTestActor"
+      val ref = new AtomicReference[AnyRef]
+
+      val behavior = Behaviors.receiveMessagePartial[AnyRef] {
+        case msg =>
+          ref.set(msg)
+          Behaviors.same
+      }
+      val testActor = creator.createActor(behavior, ActorName, None)
+
+      testActor.path.name should endWith(ActorName)
+      testActor ! Message
+      awaitCond(ref.get() == Message)
+    }
+
+    /**
+      * Tests whether the [[ActorCreator]] passed to this factory can be used
+      * to create a classic actor.
+      *
+      * @param creator the creator to test
+      */
+    private def testActorCreatorClassic(creator: ActorCreator): Unit = {
+      val Message = new Object
+      val ActorName = ActorNamePrefix + "classicTestActor"
+      val ref = new AtomicReference[Any]
+
+      val props = classic.Props(new Actor {
+        override def receive: Receive = {
+          case msg => ref.set(msg)
+        }
+      })
+      val testActor = creator.createActor(props, ActorName)
+
+      testActor.path.name should endWith(ActorName)
+      testActor ! Message
+      awaitCond(ref.get() == Message)
+    }
+
+    /**
+      * Returns one of the parameters passed to the factory method. Since the
+      * factory is called asynchronously, we might have to wait until the value
+      * becomes available.
+      *
+      * @param ref the reference storing the value
+      * @tparam T the type of the parameter
+      * @return the parameter value
+      */
+    private def fetchFactoryParameter[T](ref: AtomicReference[T]): T = {
+      awaitCond(ref.get() != null)
+      ref.get()
     }
   }
 }
