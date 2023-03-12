772ed5cb6e17d2942e55a5ba1394d1ffa4446118
Oliver Heger <oliver.heger@oliver-heger.de>
2023-03-12 18:56:45 +0100

    Radio player: Extracted the logic to update the status line.
    Setting the status text, updating the playback time, and managing the
    error indicator is now in the responsibility of a new
    RadioStatusLineController class. This simplifies the main controller.
    
    The logic to indicate an error state has been changed. The indicator
    is now displayed only if the current (selected) source cannot be
    played due to a playback error. Also, the logic to distinguish between
    the current source and a replacement source could be simplified.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayer/src/main/scala/de/oliver_heger/linedj/radio/RadioStatusLineController.scala
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import de.oliver_heger.linedj.platform.ui.TextTimeFunctions
+import de.oliver_heger.linedj.player.engine.radio.RadioSource
+import net.sf.jguiraffe.gui.app.ApplicationContext
+import net.sf.jguiraffe.gui.builder.components.WidgetHandler
+import net.sf.jguiraffe.gui.builder.components.model.StaticTextHandler
+import net.sf.jguiraffe.resources.Message
+
+import scala.concurrent.duration.FiniteDuration
+
+object RadioStatusLineController {
+  /** Resource key for the status text for normal playback. */
+  private val ResKeyStatusPlaybackNormal = "txt_status_playback"
+
+  /** Resource key for the status text for replacement playback. */
+  private val ResKeyStatusPlaybackReplace = "txt_status_replacement"
+
+  /** Resource key for the initialization error message. */
+  private val ResKeyStatusPlayerInitError = "txt_status_error"
+}
+
+/**
+  * A helper class to manage the status line of the radio player application.
+  *
+  * An instance is notified about certain changes in the status of radio
+  * playback, such as playback progress, errors, or replacement sources. It is
+  * then responsible for updating the status line accordingly.
+  *
+  * All functions must be invoked in the event thread only.
+  *
+  * @param applicationContext the application context
+  * @param statusText         the handler for the status line
+  * @param playbackTime       the handler for the field with the playback time
+  * @param errorIndicator     handler to the error indicator field
+  */
+class RadioStatusLineController(applicationContext: ApplicationContext,
+                                statusText: StaticTextHandler,
+                                playbackTime: StaticTextHandler,
+                                errorIndicator: WidgetHandler) {
+
+  import RadioStatusLineController._
+
+  /** Stores information about radio sources and their names. */
+  private val radioSources = RadioPlayerClientConfig(applicationContext).sourceConfig.namedSources
+
+  /** The function to generate the playback time text. */
+  private val playbackTimeFunc = TextTimeFunctions.formattedTime()
+
+  /** Stores the currently selected source. */
+  private var currentSource: RadioSource = _
+
+  /**
+    * Stores the source that is currently played. This is not necessarily the
+    * selected source, but could also be a replacement.
+    */
+  private var playedSource: RadioSource = _
+
+  /**
+    * Stores a flag whether an error was recorded when playing the selected
+    * source. In this state, the error indicator is displayed until the
+    * selected source could be played successfully again.
+    */
+  private var errorState = false
+
+  /**
+    * Notifies this object of a change in the selected radio source.
+    *
+    * @param source the new current radio source
+    */
+  def updateCurrentSource(source: RadioSource): Unit = {
+    currentSource = source
+  }
+
+  /**
+    * Notifies this object about a playback error of the given source. If this
+    * is the current radio source, error state is entered.
+    *
+    * @param source the source affected by the error
+    */
+  def playbackError(source: RadioSource): Unit = {
+    if (source == currentSource) {
+      errorState = true
+    }
+    updateErrorIndicator()
+  }
+
+  /**
+    * Notifies this object that the given source has been played for a while.
+    * This may cause some state updates.
+    *
+    * @param source the source that is currently played
+    * @param time   the playback time
+    */
+  def playbackTimeProgress(source: RadioSource, time: FiniteDuration): Unit = {
+    playbackTime setText playbackTimeFunc(time)
+
+    if (source != playedSource) {
+      playedSource = source
+      statusText.setText(generateStatusText(source))
+    }
+
+    if (source == currentSource) {
+      errorState = false
+    }
+    updateErrorIndicator()
+  }
+
+  /**
+    * Notifies this object that the radio player could not be initialized. The
+    * controller then displays a corresponding message in the status line.
+    */
+  def playerInitializationFailed(): Unit = {
+    statusText.setText(applicationContext.getResourceText(ResKeyStatusPlayerInitError))
+  }
+
+  /**
+    * Generates the text for the status line when playback of a source starts.
+    *
+    * @param src the source which is currently played
+    * @return the corresponding text for the status line
+    */
+  private def generateStatusText(src: RadioSource): String = {
+    if (src == currentSource) {
+      applicationContext.getResourceText(ResKeyStatusPlaybackNormal)
+    } else {
+      val srcData = radioSources.find(_._2 == src)
+      applicationContext.getResourceText(new Message(null, ResKeyStatusPlaybackReplace,
+        srcData.map(_._1) getOrElse src.uri))
+    }
+  }
+
+  /**
+    * Changes the visibility of the error indicator to match the current
+    * error state. If the visibility already corresponds to the error state, no
+    * action is taken.
+    */
+  private def updateErrorIndicator(): Unit = {
+    if (errorIndicator.isVisible != errorState) {
+      errorIndicator.setVisible(errorState)
+    }
+  }
+}
---
+++ radioPlayer/src/test/scala/de/oliver_heger/linedj/radio/RadioStatusLineControllerSpec.scala
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.radio
+
+import de.oliver_heger.linedj.player.engine.radio.RadioSource
+import net.sf.jguiraffe.gui.app.ApplicationContext
+import net.sf.jguiraffe.gui.builder.components.WidgetHandler
+import net.sf.jguiraffe.gui.builder.components.model.StaticTextHandler
+import net.sf.jguiraffe.resources.Message
+import org.apache.commons.configuration.XMLConfiguration
+import org.mockito.Mockito._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.mockito.MockitoSugar
+
+import scala.concurrent.duration._
+
+object RadioStatusLineControllerSpec {
+  /** A text resource for the status line. */
+  private val StatusText = "Text for the status line"
+
+  /** A test radio source from the test configuration file. */
+  private val TestRadioSource = RadioSource("http://metafiles.gl-systemhaus.de/hr/hr1_2.m3u", Some("mp3"))
+
+  /** The name of the test source. */
+  private val TestRadioSourceName = "HR 1"
+}
+
+/**
+  * Test class for [[RadioStatusLineController]].
+  */
+class RadioStatusLineControllerSpec extends AnyFlatSpec with Matchers with MockitoSugar {
+
+  import RadioStatusLineControllerSpec._
+
+  "RadioStatusLineController" should "update the status text if the current source is played" in {
+    val helper = new StatusLineControllerTestHelper
+    helper.expectResource("txt_status_playback")
+
+    helper.controller.updateCurrentSource(TestRadioSource)
+    helper.controller.playbackTimeProgress(TestRadioSource, 10.seconds)
+
+    helper.verifyStatusUpdated()
+  }
+
+  it should "update the status text if a replacement source is played" in {
+    val msg = new Message(null, "txt_status_replacement", TestRadioSourceName)
+    val helper = new StatusLineControllerTestHelper
+    helper.expectMessageResource(msg)
+
+    helper.controller.updateCurrentSource(RadioSource("anotherSource"))
+    helper.controller.playbackTimeProgress(TestRadioSource, 11.seconds)
+
+    helper.verifyStatusUpdated()
+  }
+
+  it should "update the status text if an unknown replacement source is played" in {
+    val replacementSource = RadioSource("https://radio.example.org/unknown_source")
+    val msg = new Message(null, "txt_status_replacement", replacementSource.uri)
+    val helper = new StatusLineControllerTestHelper
+    helper.expectMessageResource(msg)
+
+    helper.controller.updateCurrentSource(TestRadioSource)
+    helper.controller.playbackTimeProgress(replacementSource, 12.seconds)
+
+    helper.verifyStatusUpdated()
+  }
+
+  it should "not update the status text if there is no change in the source" in {
+    val helper = new StatusLineControllerTestHelper
+    helper.expectResource("txt_status_playback")
+    helper.controller.updateCurrentSource(TestRadioSource)
+    helper.controller.playbackTimeProgress(TestRadioSource, 10.seconds)
+
+    helper.controller.playbackTimeProgress(TestRadioSource, 20.seconds)
+
+    helper.verifyStatusUpdated()
+  }
+
+  it should "update the playback time" in {
+    val helper = new StatusLineControllerTestHelper
+
+    helper.controller.playbackTimeProgress(TestRadioSource, 65.seconds)
+
+    verify(helper.playbackTimeHandler).setText("1:05")
+  }
+
+  it should "show the error indicator if the current source is in error state" in {
+    val helper = new StatusLineControllerTestHelper
+
+    helper.controller.updateCurrentSource(TestRadioSource)
+    helper.controller.playbackError(TestRadioSource)
+
+    verify(helper.errorIndicator).setVisible(true)
+  }
+
+  it should "not show the error indicator if an error for another source is reported" in {
+    val helper = new StatusLineControllerTestHelper
+
+    helper.controller.playbackError(TestRadioSource)
+
+    verify(helper.errorIndicator, never()).setVisible(true)
+  }
+
+  it should "hide the error indicator if the current source is played again" in {
+    val helper = new StatusLineControllerTestHelper
+    when(helper.errorIndicator.isVisible).thenReturn(true)
+    helper.controller.updateCurrentSource(TestRadioSource)
+    helper.controller.playbackError(TestRadioSource)
+
+    helper.controller.playbackTimeProgress(TestRadioSource, 1.second)
+
+    verify(helper.errorIndicator).setVisible(false)
+  }
+
+  it should "not hide the error indicator if another source is played" in {
+    val otherSource = RadioSource("anotherSource")
+    val helper = new StatusLineControllerTestHelper
+    when(helper.errorIndicator.isVisible).thenReturn(true)
+    helper.controller.updateCurrentSource(TestRadioSource)
+    helper.controller.playbackError(TestRadioSource)
+
+    helper.controller.playbackTimeProgress(otherSource, 1.second)
+
+    verify(helper.errorIndicator, never()).setVisible(false)
+  }
+
+  it should "not hide the error indicator if it is not visible" in {
+    val helper = new StatusLineControllerTestHelper
+    when(helper.errorIndicator.isVisible).thenReturn(false)
+    helper.controller.updateCurrentSource(TestRadioSource)
+
+    helper.controller.playbackTimeProgress(TestRadioSource, 1.second)
+
+    verify(helper.errorIndicator, never()).setVisible(false)
+  }
+
+  it should "hide the error indicator if playback starts with a replacement source" in {
+    val replacementSource = RadioSource("replacement")
+    val helper = new StatusLineControllerTestHelper
+    when(helper.errorIndicator.isVisible).thenReturn(true)
+    helper.controller.updateCurrentSource(TestRadioSource)
+
+    helper.controller.playbackTimeProgress(replacementSource, 2.seconds)
+
+    verify(helper.errorIndicator).setVisible(false)
+  }
+
+  it should "update the status text with an initialization error message" in {
+    val helper = new StatusLineControllerTestHelper
+    helper.expectResource("txt_status_error")
+
+    helper.controller.playerInitializationFailed()
+
+    helper.verifyStatusUpdated()
+  }
+
+  /**
+    * A test helper class managing a test instance and its dependencies.
+    */
+  private class StatusLineControllerTestHelper {
+    /** Mock for the application context. */
+    private val applicationContext: ApplicationContext = createApplicationContext()
+
+    /** Mock for the status line handler. */
+    private val statusHandler: StaticTextHandler = mock[StaticTextHandler]
+
+    /** Mock for the handler for the playback time. */
+    val playbackTimeHandler: StaticTextHandler = mock[StaticTextHandler]
+
+    /** Mock for the error indicator control. */
+    val errorIndicator: WidgetHandler = mock[WidgetHandler]
+
+    /** The controller to be tested. */
+    val controller = new RadioStatusLineController(applicationContext, statusHandler, playbackTimeHandler,
+      errorIndicator)
+
+    /**
+      * Prepares the mock application context for a resource request based on a
+      * resource key.
+      *
+      * @param key  the resource key
+      * @param text the text to be returned
+      * @return this test helper
+      */
+    def expectResource(key: AnyRef, text: String = StatusText): StatusLineControllerTestHelper = {
+      when(applicationContext.getResourceText(key)).thenReturn(text)
+      this
+    }
+
+    /**
+      * Prepares the mock application context for a resource request based on a
+      * ''Message'' object.
+      *
+      * @param msg  the expected message object
+      * @param text the text to be returned
+      * @return this test helper
+      */
+    def expectMessageResource(msg: Message, text: String = StatusText): StatusLineControllerTestHelper = {
+      when(applicationContext.getResourceText(msg)).thenReturn(text)
+      this
+    }
+
+    /**
+      * Verifies that the status text has been updated as expected.
+      */
+    def verifyStatusUpdated(): Unit = {
+      verify(statusHandler).setText(StatusText)
+    }
+
+    /**
+      * Creates a mock for the application context and configures it with a
+      * test radio player configuration.
+      *
+      * @return the initialized mock application context
+      */
+    private def createApplicationContext(): ApplicationContext = {
+      val appCtx = mock[ApplicationContext]
+      val config = new XMLConfiguration("test-radio-configuration.xml")
+      when(appCtx.getConfiguration).thenReturn(config)
+      appCtx
+    }
+  }
+}
