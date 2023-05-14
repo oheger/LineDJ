7d34706529ae8f74ae7ae4e3598852e51fe47a9a
Oliver Heger <oliver.heger@oliver-heger.de>
2023-05-14 17:08:12 +0200

    Radio player engine: Added PlaybackGuardianActor.
    This actor checks whether playback has stalled. This can obviously
    happen when switching to another radio source or if there is a net
    outage.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/radio/control/PlaybackGuardianActor.scala
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.control
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import de.oliver_heger.linedj.player.engine.actors.{EventManagerActor, ScheduledInvocationActor}
+import de.oliver_heger.linedj.player.engine.radio.{RadioEvent, RadioPlaybackProgressEvent, RadioPlaybackStoppedEvent, RadioSource, RadioSourceChangedEvent}
+
+import scala.concurrent.duration.FiniteDuration
+
+/**
+  * A module providing an actor implementation that can detect and remedy a
+  * stalled radio playback.
+  *
+  * It has been observed in practice that a radio stream suddenly stalls
+  * without any error being detected. This happens mostly after switching to
+  * another radio source, but also in the midst of playback (maybe caused by a
+  * network hick-up?). This actor implementation should guard against such
+  * problems.
+  *
+  * The idea behind this actor is that it registers itself as listener for
+  * radio events. As long as playback is not stopped it checks periodically
+  * whether it has retrieved a playback progress event. If this is the case,
+  * everything is considered to be normal. Otherwise, the radio stream seems to
+  * be hanging. It then disables the current radio source. This should trigger
+  * the playback of another source including a reset of the player engine. On
+  * receiving a playback progress event again, it enables all the sources it
+  * has currently disabled again, so that playback can be retried.
+  */
+object PlaybackGuardianActor {
+  /**
+    * The base command trait for this actor.
+    */
+  sealed trait PlaybackGuardianCommand
+
+  /**
+    * An internal command the guardian actor sends to itself to process a
+    * received radio event.
+    *
+    * @param event the event to process
+    */
+  private case class RadioEventReceived(event: RadioEvent) extends PlaybackGuardianCommand
+
+  /**
+    * An internal command that triggers a check whether the current source has
+    * generated a playback progress event. The command is scheduled in regular
+    * intervals.
+    */
+  private case object CheckPlaybackProgress extends PlaybackGuardianCommand
+
+  /**
+    * A data class representing the current state of the guardian actor.
+    *
+    * @param currentSource   an ''Option'' with the source that is currently
+    *                        played
+    * @param disabledSources the sources that have been disabled
+    * @param gotProgress     flag whether a playback progress event was
+    *                        received
+    */
+  private case class GuardianState(currentSource: Option[RadioSource],
+                                   disabledSources: List[RadioSource],
+                                   gotProgress: Boolean) {
+    /**
+      * Returns an updated state with the given progress flag. This is an
+      * optimization that prevents creating a new object if the state is not
+      * changed.
+      *
+      * @param f the new progress state
+      * @return an instance with the correct progress state
+      */
+    def updateProgressState(f: Boolean): GuardianState =
+      if (gotProgress == f) this else copy(gotProgress = f)
+  }
+
+  /**
+    * A trait defining a factory function for creating new instances of the
+    * playback guardian actor.
+    */
+  trait Factory {
+    /**
+      * Returns a ''Behavior'' for creating a new instance of this actor class.
+      *
+      * @param checkInterval     the interval in which playback checks should be
+      *                          done
+      * @param enabledStateActor the actor to disable or enable sources
+      * @param scheduleActor     the scheduler actor
+      * @param eventActor        the event manager actor
+      * @return the ''Behavior'' to create a new actor instance
+      */
+    def apply(checkInterval: FiniteDuration,
+              enabledStateActor: ActorRef[RadioControlProtocol.SourceEnabledStateCommand],
+              scheduleActor: ActorRef[ScheduledInvocationActor.ScheduledInvocationCommand],
+              eventActor: ActorRef[EventManagerActor.EventManagerCommand[RadioEvent]]):
+    Behavior[PlaybackGuardianCommand]
+  }
+
+  /**
+    * A default [[Factory]] implementation allowing the creation of new actor
+    * instances.
+    */
+  val behavior: Factory = (checkInterval: FiniteDuration, enabledStateActor: ActorRef[RadioControlProtocol.SourceEnabledStateCommand], scheduleActor: ActorRef[ScheduledInvocationActor.ScheduledInvocationCommand], eventActor: ActorRef[EventManagerActor.EventManagerCommand[RadioEvent]]) => Behaviors.setup { context =>
+    val eventAdapter = context.messageAdapter[RadioEvent] { event => RadioEventReceived(event) }
+    eventActor ! EventManagerActor.RegisterListener(eventAdapter)
+    val scheduleCommand = ScheduledInvocationActor.typedInvocationCommand(checkInterval, context.self,
+      CheckPlaybackProgress)
+
+    def handle(state: GuardianState): Behavior[PlaybackGuardianCommand] =
+      Behaviors.receiveMessage {
+        case RadioEventReceived(event) =>
+          handleEvent(state, event)
+
+        case CheckPlaybackProgress if state.currentSource.isDefined =>
+          if (!state.gotProgress) {
+            val source = state.currentSource.get
+            context.log.warn("Detected stalled playback for source '{}'.", source)
+            enabledStateActor ! RadioControlProtocol.DisableSource(source)
+            handle(state.copy(currentSource = None, disabledSources = source :: state.disabledSources))
+          } else {
+            scheduleActor ! scheduleCommand
+            handle(state.updateProgressState(false))
+          }
+
+        case CheckPlaybackProgress =>
+          Behaviors.same // Playback has been stopped, so do nothing.
+      }
+
+    def handleEvent(state: GuardianState, event: RadioEvent): Behavior[PlaybackGuardianCommand] =
+      event match {
+        case RadioSourceChangedEvent(source, _) =>
+          if (state.currentSource.isEmpty) {
+            scheduleActor ! scheduleCommand
+          }
+          handle(state.copy(currentSource = Some(source)))
+
+        case _: RadioPlaybackProgressEvent =>
+          val markedState = state.updateProgressState(true)
+          val nextState = if (markedState.disabledSources.isEmpty) markedState
+          else {
+            markedState.disabledSources.foreach { source =>
+              enabledStateActor ! RadioControlProtocol.EnableSource(source)
+            }
+            markedState.copy(disabledSources = Nil)
+          }
+          handle(nextState)
+
+        case _: RadioPlaybackStoppedEvent =>
+          handle(state.copy(currentSource = None))
+
+        case _ => Behaviors.same
+      }
+
+    val initState = GuardianState(currentSource = None,
+      gotProgress = false,
+      disabledSources = Nil)
+    handle(initState)
+  }
+}
---
+++ radioPlayerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/radio/control/PlaybackGuardianActorSpec.scala
@@ -0,0 +1,247 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.control
+
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
+import akka.actor.typed.ActorRef
+import de.oliver_heger.linedj.player.engine.actors.{EventManagerActor, ScheduledInvocationActor}
+import de.oliver_heger.linedj.player.engine.radio.control.RadioSourceConfigTestHelper.radioSource
+import de.oliver_heger.linedj.player.engine.radio._
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+object PlaybackGuardianActorSpec {
+  /** The check interval used by test actor instances. */
+  private val CheckInterval = 5.seconds
+}
+
+/**
+  * Test class for [[PlaybackGuardianActor]].
+  */
+class PlaybackGuardianActorSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers {
+
+  import PlaybackGuardianActorSpec._
+
+  "PlaybackGuardianActor" should "schedule a check when playback starts" in {
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(radioSource(1)))
+      .expectScheduledInvocation(execute = false)
+  }
+
+  it should "not schedule a check if only the source is changed" in {
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(radioSource(1)))
+      .expectScheduledInvocation(execute = false)
+      .sendEvent(RadioSourceChangedEvent(radioSource(2)))
+      .expectNoScheduledInvocation()
+  }
+
+  it should "ignore unrelated radio events" in {
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioPlaybackErrorEvent(radioSource(1)))
+      .sendEvent(RadioSourceChangedEvent(radioSource(2)))
+      .expectScheduledInvocation(execute = false)
+  }
+
+  it should "disable a radio source if no progress event arrives" in {
+    val source = radioSource(11)
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(source))
+      .expectScheduledInvocation(execute = true)
+      .expectStateCommand(RadioControlProtocol.DisableSource(source))
+  }
+
+  it should "reset the current source when it is disabled" in {
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(radioSource(1)))
+      .expectScheduledInvocation(execute = true)
+      .expectNoScheduledInvocation()
+      .sendEvent(RadioSourceChangedEvent(radioSource(2)))
+      .expectScheduledInvocation(execute = false)
+  }
+
+  it should "not disable the current source if a playback progress event was received" in {
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(radioSource(1)))
+      .sendEvent(RadioPlaybackProgressEvent(radioSource(2), 0, 1.second))
+      .expectScheduledInvocation(execute = true)
+      .expectNoStateCommand()
+  }
+
+  it should "reschedule checks" in {
+    val source = radioSource(1)
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(source))
+      .sendEvent(RadioPlaybackProgressEvent(source, 1, 100.millis))
+      .expectScheduledInvocation(execute = true)
+      .expectScheduledInvocation(execute = true)
+      .expectStateCommand(RadioControlProtocol.DisableSource(source))
+  }
+
+  it should "enable sources again on receiving a playback progress event" in {
+    val source1 = radioSource(1)
+    val source2 = radioSource(2)
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(source1))
+      .expectScheduledInvocation(execute = true)
+      .expectStateCommand(RadioControlProtocol.DisableSource(source1))
+      .sendEvent(RadioSourceChangedEvent(source2))
+      .expectScheduledInvocation(execute = true)
+      .expectStateCommand(RadioControlProtocol.DisableSource(source2))
+      .sendEvent(RadioSourceChangedEvent(radioSource(3)))
+      .sendEvent(RadioPlaybackProgressEvent(radioSource(4), 5, 333.millis))
+      .expectStateCommand(RadioControlProtocol.EnableSource(source2))
+      .expectStateCommand(RadioControlProtocol.EnableSource(source1))
+  }
+
+  it should "clear disabled sources after they have been re-enabled" in {
+    val source = radioSource(1)
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(source))
+      .expectScheduledInvocation(execute = true)
+      .expectStateCommand(RadioControlProtocol.DisableSource(source))
+      .sendEvent(RadioSourceChangedEvent(source))
+      .sendEvent(RadioPlaybackProgressEvent(source, 8, 100.millis))
+      .expectStateCommand(RadioControlProtocol.EnableSource(source))
+      .sendEvent(RadioPlaybackProgressEvent(source, 16, 200.millis))
+      .expectNoStateCommand()
+  }
+
+  it should "handle a playback stopped event" in {
+    val helper = new GuardianTestHelper
+
+    helper.sendEvent(RadioSourceChangedEvent(radioSource(1)))
+      .sendEvent(RadioPlaybackStoppedEvent(radioSource(1)))
+      .expectScheduledInvocation(execute = true)
+      .expectNoStateCommand()
+      .expectNoScheduledInvocation()
+  }
+
+  /**
+    * A test helper class managing a test actor reference and its dependencies.
+    */
+  private class GuardianTestHelper {
+    /** Test probe for the enabled state actor. */
+    private val probeStateActor = testKit.createTestProbe[RadioControlProtocol.SourceEnabledStateCommand]()
+
+    /** Test probe for the scheduler actor. */
+    private val probeSchedulerActor = testKit.createTestProbe[ScheduledInvocationActor.ScheduledInvocationCommand]()
+
+    /** Stores the listener registered at the event actor. */
+    private var eventListener: ActorRef[RadioEvent] = _
+
+    createGuardianActor()
+
+    /**
+      * Sends the given event to the event listener that the guardian actor has
+      * registered.
+      *
+      * @param event the event to send
+      * @return this test helper
+      */
+    def sendEvent(event: RadioEvent): GuardianTestHelper = {
+      eventListener ! event
+      this
+    }
+
+    /**
+      * Expects that a scheduled invocation has been triggered at the scheduler
+      * actor. Optionally, the invocation can be executed directly.
+      *
+      * @param execute flag whether the invocation should be executed right now
+      * @return this test helper
+      */
+    def expectScheduledInvocation(execute: Boolean): GuardianTestHelper = {
+      val command = probeSchedulerActor.expectMessageType[ScheduledInvocationActor.ActorInvocationCommand]
+      command.delay should be(CheckInterval)
+      if (execute) {
+        command.invocation.send()
+      }
+      this
+    }
+
+    /**
+      * Expects that no message has been sent to the scheduler actor.
+      *
+      * @return this test helper
+      */
+    def expectNoScheduledInvocation(): GuardianTestHelper =
+      expectNoMessage(probeSchedulerActor)
+
+    /**
+      * Expects that the given command has been sent to the enabled state
+      * actor.
+      *
+      * @param command the expected command
+      * @return this test helper
+      */
+    def expectStateCommand(command: RadioControlProtocol.SourceEnabledStateCommand): GuardianTestHelper = {
+      probeStateActor.expectMessage(command)
+      this
+    }
+
+    /**
+      * Expects that no command has been sent to the enabled state actor.
+      *
+      * @return this test helper
+      */
+    def expectNoStateCommand(): GuardianTestHelper =
+      expectNoMessage(probeStateActor)
+
+    /**
+      * Helper function to check whether the given test probe did not receive a
+      * message.
+      *
+      * @param probe the probe
+      * @tparam M the message type of the probe
+      * @return this test helper
+      */
+    private def expectNoMessage[M](probe: TestProbe[M]): GuardianTestHelper = {
+      probe.expectNoMessage(250.millis)
+      this
+    }
+
+    /**
+      * Creates the actor to be tested.
+      *
+      * @return the test actor reference
+      */
+    private def createGuardianActor(): ActorRef[PlaybackGuardianActor.PlaybackGuardianCommand] = {
+      val probeEventActor = testKit.createTestProbe[EventManagerActor.EventManagerCommand[RadioEvent]]()
+      val guardianBehavior = PlaybackGuardianActor.behavior(CheckInterval,
+        probeStateActor.ref,
+        probeSchedulerActor.ref,
+        probeEventActor.ref)
+      val guardianActor = testKit.spawn(guardianBehavior)
+
+      val registration = probeEventActor.expectMessageType[EventManagerActor.RegisterListener[RadioEvent]]
+      eventListener = registration.listener
+      guardianActor
+    }
+  }
+}
