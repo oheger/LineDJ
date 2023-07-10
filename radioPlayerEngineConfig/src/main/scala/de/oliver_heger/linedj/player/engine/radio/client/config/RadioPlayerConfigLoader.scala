33515fe5eb1e7b653de15b0e4596a6ff8b76b54a
Oliver Heger <oliver.heger@oliver-heger.de>
2023-07-10 22:16:17 +0200

    Radio engine config: Added RadioPlayerConfigLoader object.
    This object offers functionality for creating a RadioPlayerConfig from
    a configuration object.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ radioPlayerEngineConfig/src/main/scala/de/oliver_heger/linedj/player/engine/radio/client/config/RadioPlayerConfigLoader.scala
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.client.config
+
+import de.oliver_heger.linedj.player.engine.PlayerConfig
+import de.oliver_heger.linedj.player.engine.radio.config.RadioPlayerConfig
+import de.oliver_heger.linedj.player.engine.client.config.ConfigurationExtensions._
+import de.oliver_heger.linedj.player.engine.client.config.PlayerConfigLoader
+import org.apache.commons.configuration.Configuration
+
+import scala.concurrent.duration._
+
+/**
+  * A module providing functionality for loading a configuration file with
+  * options for the radio player. The options that can be defined here
+  * correspond to the properties of [[RadioPlayerConfig]].
+  */
+object RadioPlayerConfigLoader {
+  /**
+    * Name of the maximum eval delay property. It specifies the maximum delay
+    * after which a new evaluation of the current radio source takes place,
+    * even if no restrictions were found.
+    */
+  final val PropMaxEvalDelay = "maxEvalDelay"
+
+  /**
+    * Name of the retry failed replacement property. This property defines the
+    * delay after which another attempt to find a replacement source is made.
+    */
+  final val PropRetryFailedReplacement = "retryFailedReplacement"
+
+  /**
+    * Name of the retry failed source property. If playback of a radio source
+    * failed, the source is retried after a delay defined by this property.
+    */
+  final val PropRetryFailedSource = "retryFailedSource"
+
+  /**
+    * Name of the retry failed source increment property. For radio sources
+    * whose playback failed multiple times, the delay for further retry
+    * attempts is increased by this factor.
+    */
+  final val PropRetryFailedSourceIncrement = "retryFailedSourceIncrement"
+
+  /**
+    * Name of the maximum retry failed source property. This is the maximum
+    * delay between retries for a failing source. If this value is reached, the
+    * factor defined by [[PropRetryFailedSourceIncrement]] is no longer
+    * applied.
+    */
+  final val PropMaxRetryFailedSource = "maxRetryFailedSource"
+
+  /**
+    * Name of the source check timeout property. This property defines the
+    * maximum time a check of a failed source can take. If playback was not
+    * successful or no error was received in this time, the check is aborted,
+    * and the source is considered to be still in error state. This is useful
+    * if the download from the source URL just hangs.
+    */
+  final val PropSourceCheckTimeout = "sourceCheckTimeout"
+
+  /**
+    * Name of the metadata check timeout property. It determines the maximum
+    * duration of a metadata check. If no metadata that is not matched by an
+    * exclusion was found within this time frame, the affected radio source
+    * remains in disabled state.
+    */
+  final val PropMetadataCheckTimeout = "metadataCheckTimeout"
+
+  /**
+    * Name of the stream cache time property. A radio stream opened for a
+    * metadata check is kept for a while in a cache in case it is reused when
+    * the associated radio source is enabled and starts playback again. This
+    * property defines the time how long the stream should be kept open.
+    */
+  final val PropStreamCacheTime = "streamCacheTime"
+
+  /**
+    * Name of the stalled playback check property. The playback guardian actor
+    * checks periodically if playback is still running. If not, it resets the
+    * current radio source. This property defines the interval in which such
+    * checks are performed.
+    */
+  final val PropStalledPlaybackCheck = "stalledPlaybackCheck"
+
+  /** The default value for the max eval delay property. */
+  final val DefaultMaxEvalDelay = 1.hour
+
+  /** The default value for the retry failed replacement property. */
+  final val DefaultRetryFailedReplacement = 1.minute
+
+  /** The default value for the retry failed source property. */
+  final val DefaultRetryFailedSource = 5.seconds
+
+  /** The default value for the retry failed source increment factor. */
+  final val DefaultRetryFailedSourceIncrement = 2.0
+
+  /** The default value for the max retry failed source property. */
+  final val DefaultMaxRetryFailedSource = 6.hours
+
+  /** The default value for the source check timeout property. */
+  final val DefaultSourceCheckTimeout = 60.seconds
+
+  /** The default value for the metadata check timeout property. */
+  final val DefaultMetadataCheckTimeout = 30.seconds
+
+  /** The default value for the stream cache time property. */
+  final val DefaultStreamCacheTime = 4.seconds
+
+  /** The default value for the stalled playback check property. */
+  final val DefaultStalledPlaybackCheck = 5.seconds
+
+  /**
+    * Constant for a [[RadioPlayerConfig]] with default values. The contained
+    * [[PlayerConfig]] is also set to default values.
+    */
+  final val DefaultRadioPlayerConfig: RadioPlayerConfig =
+    RadioPlayerConfig(playerConfig = PlayerConfigLoader.DefaultPlayerConfig,
+      maximumEvalDelay = DefaultMaxEvalDelay,
+      retryFailedReplacement = DefaultRetryFailedReplacement,
+      retryFailedSource = DefaultRetryFailedSource,
+      retryFailedSourceIncrement = DefaultRetryFailedSourceIncrement,
+      maxRetryFailedSource = DefaultMaxRetryFailedSource,
+      sourceCheckTimeout = DefaultSourceCheckTimeout,
+      metadataCheckTimeout = DefaultMetadataCheckTimeout,
+      streamCacheTime = DefaultStreamCacheTime,
+      stalledPlaybackCheck = DefaultStalledPlaybackCheck)
+
+  /**
+    * Creates a [[RadioPlayerConfig]] from the given [[Configuration]].
+    *
+    * @param c            the configuration
+    * @param pathPrefix   the prefix for all keys
+    * @param playerConfig the configuration of the audio player
+    * @return the newly created [[RadioPlayerConfig]]
+    */
+  def loadRadioPlayerConfig(c: Configuration,
+                            pathPrefix: String,
+                            playerConfig: PlayerConfig): RadioPlayerConfig = {
+    val normalizedPrefix = if (pathPrefix.endsWith(".")) pathPrefix
+    else pathPrefix + "."
+
+    def key(k: String): String = s"$normalizedPrefix$k"
+
+    RadioPlayerConfig(playerConfig = playerConfig,
+      maximumEvalDelay = c.getDuration(key(PropMaxEvalDelay), DefaultMaxEvalDelay),
+      retryFailedReplacement = c.getDuration(key(PropRetryFailedReplacement), DefaultRetryFailedReplacement),
+      retryFailedSource = c.getDuration(key(PropRetryFailedSource), DefaultRetryFailedSource),
+      retryFailedSourceIncrement = c.getDouble(key(PropRetryFailedSourceIncrement), DefaultRetryFailedSourceIncrement),
+      maxRetryFailedSource = c.getDuration(key(PropMaxRetryFailedSource), DefaultMaxRetryFailedSource),
+      sourceCheckTimeout = c.getDuration(key(PropSourceCheckTimeout), DefaultSourceCheckTimeout),
+      metadataCheckTimeout = c.getDuration(key(PropMetadataCheckTimeout), DefaultMetadataCheckTimeout),
+      streamCacheTime = c.getDuration(key(PropStreamCacheTime), DefaultStreamCacheTime),
+      stalledPlaybackCheck = c.getDuration(key(PropStalledPlaybackCheck), DefaultStalledPlaybackCheck))
+  }
+}
---
+++ radioPlayerEngineConfig/src/test/scala/de/oliver_heger/linedj/player/engine/radio/client/config/RadioPlayerConfigLoaderSpec.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2015-2023 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.radio.client.config
+
+import de.oliver_heger.linedj.player.engine.client.config.PlayerConfigLoader
+import de.oliver_heger.linedj.player.engine.radio.config.RadioPlayerConfig
+import org.apache.commons.configuration.HierarchicalConfiguration
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.duration._
+
+/**
+  * Test class for [[RadioPlayerConfigLoader]].
+  */
+class RadioPlayerConfigLoaderSpec extends AnyFlatSpec with Matchers {
+  "RadioPlayerConfigLoader" should "read the properties from a configuration" in {
+    val Prefix = "radio.player.config"
+    val config = new HierarchicalConfiguration
+
+    def set(key: String, value: Any): Unit =
+      config.addProperty(s"$Prefix.$key", value)
+
+    def setWithUnit(key: String, value: Int, unit: String): Unit = {
+      set(key, value)
+      set(key + "[@unit]", unit)
+    }
+
+    setWithUnit(RadioPlayerConfigLoader.PropMaxEvalDelay, 123, "minutes")
+    setWithUnit(RadioPlayerConfigLoader.PropRetryFailedReplacement, 21, "seconds")
+    setWithUnit(RadioPlayerConfigLoader.PropRetryFailedSource, 17, "seconds")
+    set(RadioPlayerConfigLoader.PropRetryFailedSourceIncrement, 1.5)
+    setWithUnit(RadioPlayerConfigLoader.PropMaxRetryFailedSource, 24, "hours")
+    setWithUnit(RadioPlayerConfigLoader.PropSourceCheckTimeout, 55, "seconds")
+    setWithUnit(RadioPlayerConfigLoader.PropMetadataCheckTimeout, 29, "seconds")
+    setWithUnit(RadioPlayerConfigLoader.PropStreamCacheTime, 4444, "milliseconds")
+    setWithUnit(RadioPlayerConfigLoader.PropStalledPlaybackCheck, 11, "seconds")
+
+    val playerConfig = PlayerConfigLoader.DefaultPlayerConfig.copy(inMemoryBufferSize = 987456321)
+    val radioConfig = RadioPlayerConfigLoader.loadRadioPlayerConfig(config, Prefix, playerConfig)
+
+    radioConfig.playerConfig should be(playerConfig)
+    radioConfig.maximumEvalDelay should be(123.minutes)
+    radioConfig.retryFailedReplacement should be(21.seconds)
+    radioConfig.retryFailedSource should be(17.seconds)
+    radioConfig.retryFailedSourceIncrement should be(1.5)
+    radioConfig.maxRetryFailedSource should be(24.hours)
+    radioConfig.sourceCheckTimeout should be(55.seconds)
+    radioConfig.metadataCheckTimeout should be(29.seconds)
+    radioConfig.streamCacheTime should be(4444.millis)
+    radioConfig.stalledPlaybackCheck should be(11.seconds)
+  }
+
+  /**
+    * Helper function for testing whether the given radio configuration
+    * contains only default values.
+    *
+    * @param radioConfig the configuration to check
+    */
+  private def checkDefaultConfig(radioConfig: RadioPlayerConfig): Unit = {
+    radioConfig.playerConfig should be(PlayerConfigLoader.DefaultPlayerConfig)
+    radioConfig.maximumEvalDelay should be(RadioPlayerConfigLoader.DefaultMaxEvalDelay)
+    radioConfig.retryFailedReplacement should be(RadioPlayerConfigLoader.DefaultRetryFailedReplacement)
+    radioConfig.retryFailedSource should be(RadioPlayerConfigLoader.DefaultRetryFailedSource)
+    radioConfig.retryFailedSourceIncrement should be(RadioPlayerConfigLoader.DefaultRetryFailedSourceIncrement)
+    radioConfig.maxRetryFailedSource should be(RadioPlayerConfigLoader.DefaultMaxRetryFailedSource)
+    radioConfig.sourceCheckTimeout should be(RadioPlayerConfigLoader.DefaultSourceCheckTimeout)
+    radioConfig.metadataCheckTimeout should be(RadioPlayerConfigLoader.DefaultMetadataCheckTimeout)
+    radioConfig.streamCacheTime should be(RadioPlayerConfigLoader.DefaultStreamCacheTime)
+    radioConfig.stalledPlaybackCheck should be(RadioPlayerConfigLoader.DefaultStalledPlaybackCheck)
+  }
+
+  it should "create a configuration with default values" in {
+    val radioConfig = RadioPlayerConfigLoader.loadRadioPlayerConfig(new HierarchicalConfiguration, "test",
+      PlayerConfigLoader.DefaultPlayerConfig)
+
+    checkDefaultConfig(radioConfig)
+  }
+
+  it should "handle a path prefix with a trailing dot" in {
+    val Prefix = "radio.player.config.dot."
+    val config = new HierarchicalConfiguration
+    config.addProperty(Prefix + RadioPlayerConfigLoader.PropRetryFailedSourceIncrement, 1.25)
+
+    val radioConfig = RadioPlayerConfigLoader.loadRadioPlayerConfig(config, Prefix,
+      PlayerConfigLoader.DefaultPlayerConfig)
+
+    radioConfig.retryFailedSourceIncrement should be(1.25)
+  }
+
+  it should "provide a default radio player config" in {
+    val radioConfig = RadioPlayerConfigLoader.DefaultRadioPlayerConfig
+
+    checkDefaultConfig(radioConfig)
+  }
+}
