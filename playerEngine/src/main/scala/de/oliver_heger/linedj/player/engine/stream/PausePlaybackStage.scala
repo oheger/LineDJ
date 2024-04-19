255ade2bd9c6d6a16563f27d2b69d5859a1b1cd6
Oliver Heger <oliver.heger@oliver-heger.de>
2024-04-19 22:03:27 +0200

    Player engine: Added initial PausePlaybackStage module.
    This module is going to provide a flow stage that allows pausing audio
    playback by not passing data through the stream while the playback
    state is set to paused. The current implementation provides an actor
    that manages the playback state.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ playerEngine/src/main/scala/de/oliver_heger/linedj/player/engine/stream/PausePlaybackStage.scala
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2015-2024 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.stream
+
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.{ActorRef, Behavior}
+
+/**
+  * A module providing a special stage implementation that allows pausing audio
+  * playback temporarily.
+  *
+  * The pause functionality is implemented by a stage that does not let data
+  * pass the stream if playback is paused. The current pause status is managed
+  * by an actor; by sending specific messages to the actor, playback can be
+  * started or stopped.
+  */
+object PausePlaybackStage {
+  /**
+    * A trait defining the hierarchy of commands supported by the actor to
+    * pause playback.
+    */
+  sealed trait PausePlaybackCommand
+
+  /**
+    * A command processed by the pause playback actor to control the flow of
+    * audio data. The pause protocol expects that clients send this message to
+    * the actor whenever audio data is to be passed downstream. The actor then
+    * responds with a [[PlaybackPossible]] message when playback is not (or no
+    * longer) paused. If playback is paused and multiple commands are received
+    * with the same ''replyTo'' field, this actor receives only a single
+    * notification.
+    *
+    * @param replyTo the actor to send the response to
+    */
+  case class WaitForPlaybackPossible(replyTo: ActorRef[PlaybackPossible]) extends PausePlaybackCommand
+
+  /**
+    * A command that sets the internal state of the pause playback actor to
+    * ''playback possible''. If clients are waiting for a [[PlaybackPossible]]
+    * response, they are notified now. Clients that sent a new
+    * [[WaitForPlaybackPossible]] message are answered immediately.
+    */
+  case object StartPlayback extends PausePlaybackCommand
+
+  /**
+    * A command that sets the internal state of the pause playback actor to
+    * ''playback paused''. Incoming [[WaitForPlaybackPossible]] message will be
+    * stored and answered only after receiving a [[StartPlayback]] message.
+    */
+  case object StopPlayback extends PausePlaybackCommand
+
+  /**
+    * A command that can be used to query the current [[PlaybackState]] managed
+    * by an actor instance. The actor responds with a [[CurrentPlaybackState]]
+    * message.
+    *
+    * @param replyTo the actor to send the response to
+    */
+  case class GetCurrentPlaybackState(replyTo: ActorRef[CurrentPlaybackState]) extends PausePlaybackCommand
+
+  /**
+    * A command that stops a pause playback actor instance.
+    */
+  case object Stop extends PausePlaybackCommand
+
+  /**
+    * A message the pause playback actor sends to clients when playback is now
+    * possible. On receiving this message, audio data can flow through the
+    * stream.
+    */
+  case class PlaybackPossible()
+
+  /**
+    * A message the pause playback actor sends to clients as response of a
+    * [[GetCurrentPlaybackState]] command. From this message, the current
+    * [[PlaybackState]] can be determined.
+    *
+    * @param state the current [[PlaybackState]]
+    */
+  case class CurrentPlaybackState(state: PlaybackState)
+
+  /**
+    * An enumeration defining the possible values for the internal playback
+    * state managed by this actor. This can be used to find out whether
+    * playback is currently enabled or paused.
+    */
+  enum PlaybackState:
+    case PlaybackPossible
+    case PlaybackPaused
+  end PlaybackState
+
+  /**
+    * Returns the behavior for a new instance of the pause playback actor. The
+    * actor is initialized with the given [[PlaybackState]].
+    *
+    * @param initialState the initial [[PlaybackState]] of the actor instance
+    * @return the behavior for the new actor instance
+    */
+  def pausePlaybackActor(initialState: PlaybackState): Behavior[PausePlaybackCommand] =
+    handlePausePlaybackCommand(initialState)
+
+  /**
+    * The command handler function of the pause playback actor.
+    *
+    * @param playbackState the current [[PlaybackState]]
+    * @param pendingWaits  set of clients waiting for a notification that 
+    *                      playback is possible
+    * @return the updated behavior
+    */
+  private def handlePausePlaybackCommand(playbackState: PlaybackState,
+                                         pendingWaits: Set[ActorRef[PlaybackPossible]] = Set.empty):
+  Behavior[PausePlaybackCommand] =
+    Behaviors.receiveMessage {
+      case GetCurrentPlaybackState(replyTo) =>
+        replyTo ! CurrentPlaybackState(playbackState)
+        Behaviors.same
+
+      case StartPlayback =>
+        pendingWaits.foreach(_ ! PlaybackPossible())
+        handlePausePlaybackCommand(PlaybackState.PlaybackPossible)
+
+      case StopPlayback =>
+        handlePausePlaybackCommand(PlaybackState.PlaybackPaused)
+
+      case WaitForPlaybackPossible(replyTo) =>
+        if playbackState == PlaybackState.PlaybackPossible then
+          replyTo ! PlaybackPossible()
+          Behaviors.same
+        else
+          handlePausePlaybackCommand(playbackState, pendingWaits + replyTo)
+
+      case Stop =>
+        Behaviors.stopped
+    }
+}
---
+++ playerEngine/src/test/scala/de/oliver_heger/linedj/player/engine/stream/PausePlaybackActorSpec.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2015-2024 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.player.engine.stream
+
+import de.oliver_heger.linedj.ActorTestKitSupport
+import org.scalatest.Inspectors.forAll
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers
+
+import scala.concurrent.duration.*
+
+/**
+  * Test class for the pause playback actor implementation in [[PausePlaybackStage]].
+  */
+class PausePlaybackActorSpec extends AnyFlatSpec with Matchers with ActorTestKitSupport:
+
+  import PausePlaybackStage.PlaybackState
+
+  "Pause playback actor" should "report the initial playback state" in :
+    val client = testKit.createTestProbe[PausePlaybackStage.CurrentPlaybackState]
+    val actor = testKit.spawn(PausePlaybackStage.pausePlaybackActor(PlaybackState.PlaybackPossible))
+
+    actor ! PausePlaybackStage.GetCurrentPlaybackState(client.ref)
+
+    client.expectMessage(PausePlaybackStage.CurrentPlaybackState(PlaybackState.PlaybackPossible))
+
+  it should "report an updated playback state" in :
+    val client = testKit.createTestProbe[PausePlaybackStage.CurrentPlaybackState]
+    val queryMessage = PausePlaybackStage.GetCurrentPlaybackState(client.ref)
+    val actor = testKit.spawn(PausePlaybackStage.pausePlaybackActor(PlaybackState.PlaybackPaused))
+
+    def expectPlaybackState(expectedState: PlaybackState) =
+      actor ! queryMessage
+      client.expectMessage(PausePlaybackStage.CurrentPlaybackState(expectedState))
+
+    expectPlaybackState(PlaybackState.PlaybackPaused)
+
+    actor ! PausePlaybackStage.StartPlayback
+    expectPlaybackState(PlaybackState.PlaybackPossible)
+
+    actor ! PausePlaybackStage.StopPlayback
+    expectPlaybackState(PlaybackState.PlaybackPaused)
+
+  it should "reply to a WaitForPlaybackPossible command when playback is possible" in :
+    val client = testKit.createTestProbe[PausePlaybackStage.PlaybackPossible]
+    val actor = testKit.spawn(PausePlaybackStage.pausePlaybackActor(PlaybackState.PlaybackPossible))
+
+    actor ! PausePlaybackStage.WaitForPlaybackPossible(client.ref)
+
+    client.expectMessage(PausePlaybackStage.PlaybackPossible())
+
+  it should "not reply to a WaitForPlaybackPossible command when playback is paused" in :
+    val client = testKit.createTestProbe[PausePlaybackStage.PlaybackPossible]
+    val actor = testKit.spawn(PausePlaybackStage.pausePlaybackActor(PlaybackState.PlaybackPaused))
+
+    actor ! PausePlaybackStage.WaitForPlaybackPossible(client.ref)
+
+    client.expectNoMessage(500.millis)
+
+  it should "answer pending WaitForPlaybackPossible commands when playback is started" in :
+    val client1 = testKit.createTestProbe[PausePlaybackStage.PlaybackPossible]
+    val client2 = testKit.createTestProbe[PausePlaybackStage.PlaybackPossible]
+    val client3 = testKit.createTestProbe[PausePlaybackStage.PlaybackPossible]
+    val clients = List(client1, client2, client3)
+    val actor = testKit.spawn(PausePlaybackStage.pausePlaybackActor(PlaybackState.PlaybackPaused))
+
+    clients.foreach { client => actor ! PausePlaybackStage.WaitForPlaybackPossible(client.ref) }
+    actor ! PausePlaybackStage.StartPlayback
+
+    forAll(clients) { client =>
+      client.expectMessage(PausePlaybackStage.PlaybackPossible())
+    }
+
+  it should "terminate on receiving a Stop command" in :
+    val watcher = testKit.createDeadLetterProbe()
+    val actor = testKit.spawn(PausePlaybackStage.pausePlaybackActor(PlaybackState.PlaybackPaused))
+
+    actor ! PausePlaybackStage.Stop
+
+    watcher.expectTerminated(actor)
+
