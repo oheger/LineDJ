58f2532c242650b70e270d3c962286b0b80dcd18
Oliver Heger <oliver.heger@oliver-heger.de>
2024-09-14 21:53:23 +0200

    ActorSystem: Reworked the `Activator` class.
    After switching to Pekko 1.1.x, the logs of the actor system no longer
    showed up. Reason was a race condition in the startup order of
    bundles: When the actor system bundle started the logger (actually
    provided by the SpiFly dynamic weaving bundle) was not yet available.
    
    The `Activator` class now deals with this scenario by waiting until
    the SpiFly bundle is started. This is a work-around; there may be a
    cleaner solution. To be investigated later.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- actorSystem/src/main/scala/de/oliver_heger/linedj/actorsystem/Activator.scala
+++ actorSystem/src/main/scala/de/oliver_heger/linedj/actorsystem/Activator.scala
@@ -16,48 +16,213 @@
 
 package de.oliver_heger.linedj.actorsystem
 
+import com.typesafe.config.ConfigFactory
 import de.oliver_heger.linedj.utils.SystemPropertyAccess
 import org.apache.pekko.actor.ActorSystem
-import org.apache.pekko.osgi.ActorSystemActivator
-import org.osgi.framework.BundleContext
+import org.apache.pekko.osgi.OsgiActorSystemFactory
+import org.osgi.framework.*
+
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.{Executors, ScheduledExecutorService, TimeUnit}
 
 object Activator:
   /**
     * Constant for a system property that defines the name of the actor
-    * system. The property is evaluated by ''getActorSystemName()''.
+    * system.
     */
-  val PropActorSystemName = "LineDJ_ActorSystemName"
+  final val PropActorSystemName = "LineDJ_ActorSystemName"
+
+  /**
+    * Constant for a system property that defines the delay after the SpiFly
+    * weaving bundle has started to wait for its completion. The property value
+    * is interpreted as a number in milliseconds.
+    */
+  final val PropSpiFlyDelayMs = "LineDJ_SpiFlyDelayMs"
 
   /** The default name of the actor system. */
-  val DefaultActorSystemName = "LineDJ_PlatformActorSystem"
+  final val DefaultActorSystemName = "LineDJ_PlatformActorSystem"
+
+  /** The default delay for waiting for the SpiFly weaving process. */
+  final val DefaultSpiFlyDelay = 500
+
+  /** The name of the SpiFly dynamic weaving bundle. */
+  private val SpiFlyBundleName = "org.apache.aries.spifly.dynamic.bundle"
+
+  /**
+    * Checks whether the SpiFly bundle is already running.
+    *
+    * @param context the bundle context
+    * @return a flag whether the SpiFly bundle is active
+    */
+  private def isSpiFlyBundleActive(context: BundleContext): Boolean =
+    context.getBundles.exists { bundle =>
+      bundle.getState == Bundle.ACTIVE && bundle.getSymbolicName == SpiFlyBundleName
+    }
+
+  /**
+    * Performs a safe cleanup of a resource in an [[AtomicReference]]. Checks
+    * whether the reference contains a value. If this is the case, and it has
+    * not been cleaned up concurrently (which could theoretically happen if the
+    * activator's ''stop()'' method is called at the same time when the SpiFly
+    * delay is reached), then the given cleanup function is called which should
+    * free the resource.
+    *
+    * @param ref the [[AtomicReference]] to be cleaned up
+    * @param f   the cleanup function
+    * @tparam A the type of the value to be cleaned up
+    */
+  private def safeCleanup[A](ref: AtomicReference[A])(f: A => Unit): Unit =
+    Option(ref.get()).foreach { value =>
+      if ref.compareAndSet(value, null.asInstanceOf[A]) then
+        f(value)
+    }
+end Activator
 
 /**
-  * A bundle activator which registers the central client-side actor system as
-  * OSGi service.
+  * A bundle activator which creates and registers the central client-side
+  * actor system as OSGi service.
   *
-  * This class uses functionality provided by the Pekko OSGi integration. Pekko
-  * already creates and configures a working actor system and passes it to a
-  * method implemented by this class. This method just registers the actor
-  * system in the OSGi registry so that it can be used by other components of
-  * the LineDJ client platform.
+  * This class uses functionality provided by the Pekko OSGi integration to
+  * correctly set up an actor system in an OSGi environment. Actually, the
+  * functionality provided by Pekko would be sufficient for the use case at
+  * hand. However, there is currently one problem with logging:
   *
-  * Some components have a dependency on this actor system. They can start
-  * automatically as soon as this object becomes available.
+  * Pekko uses slf4j 2.x as logging facade. This library uses a service loader
+  * approach to find a logger implementation. For this to work, the dynamic
+  * weaving bundle of Apache Aries SpiFly must be active first; otherwise, the
+  * service loader does not yield any services, causing logging to be disabled
+  * for the actor system. To prevent this, this activator implementation
+  * contains logic which checks whether the weaving bundle is already active or
+  * waits until it gets started. Then it waits for another configurable time
+  * span to make sure that the bundle has done its work. Only then it is safe
+  * to create the actor system.
+  *
+  * The actor system is then registered as an OSGi service. Some components
+  * have a dependency on this actor system. They can start automatically as
+  * soon as this object becomes available.
   */
-class Activator extends ActorSystemActivator with SystemPropertyAccess:
-  import Activator._
+class Activator extends BundleActivator with SystemPropertyAccess:
+
+  import Activator.*
+
+  /** Stores the executor service. */
+  private val executorService = new AtomicReference[ScheduledExecutorService]
+
+  /** Stores the registered bundle listener. */
+  private val bundleListener = new AtomicReference[BundleListener]
 
   /**
-    * @inheritdoc This implementation just registers the actor system as a
-    *             service.
+    * Stores the registration for the actor system service, so that it can be
+    * unregistered when the bundle is stopped.
     */
-  override def configure(context: BundleContext, system: ActorSystem): Unit =
-    context.registerService(classOf[ActorSystem], system, null)
+  private val serviceRegistration = new AtomicReference[ServiceRegistration[ActorSystem]]
+
+  override def start(context: BundleContext): Unit =
+    println("Starting LineDJ ActorSystem activator.")
+
+    executorService.set(createExecutor())
+    if isSpiFlyBundleActive(context) then
+      triggerDelayedRegistration(context)
+    else
+      println("Waiting for the start of the SpiFly dynamic weaving bundle.")
+      val listener = createBundleListener(context)
+      context.addBundleListener(listener)
+      bundleListener.set(listener)
+
+  override def stop(context: BundleContext): Unit =
+    println("Stopping LineDJ ActorSystem activator.")
+    shutdownExecutorService()
+    removeBundleListener(context)
+    safeCleanup(serviceRegistration)(_.unregister())
 
   /**
-    * @inheritdoc This implementation checks whether the name of the actor
-    *             system is defined as a system property. Otherwise, a default
-    *             name is returned.
+    * Returns the [[OsgiActorSystemFactory]] to create the actor system.
+    *
+    * @param context the current [[BundleContext]]
+    * @return the factory for creating the actor system
     */
-  override def getActorSystemName(context: BundleContext): String =
-  getSystemProperty(PropActorSystemName) getOrElse DefaultActorSystemName
+  private[actorsystem] def createActorSystemFactory(context: BundleContext): OsgiActorSystemFactory =
+    OsgiActorSystemFactory(context, ConfigFactory.empty())
+
+  /**
+    * Returns a [[ScheduledExecutorService]] that is used to wait for the
+    * completion of the SpiFly weaving process.
+    *
+    * @return the executor to wait for the configured delay
+    */
+  private[actorsystem] def createExecutor(): ScheduledExecutorService =
+    Executors.newSingleThreadScheduledExecutor()
+
+  /**
+    * Creates the [[BundleListener]] that waits for the start of the SpiFly
+    * weaving bundle and then triggers the creation and registration of the
+    * actor system.
+    *
+    * @param context the bundle context
+    * @return the bundle listener
+    */
+  private def createBundleListener(context: BundleContext): BundleListener =
+    (event: BundleEvent) =>
+      if event.getType == BundleEvent.STARTED && event.getBundle.getSymbolicName == SpiFlyBundleName then
+        triggerDelayedRegistration(context)
+
+  /**
+    * Schedules a task that creates and registers the actor system after a
+    * proper delay.
+    *
+    * @param context the bundle context
+    */
+  private def triggerDelayedRegistration(context: BundleContext): Unit =
+    Option(executorService.get()).foreach {
+      val delay = fetchSpiFlyDelay()
+      println(s"Waiting $delay ms for the completion of the SpiFly weaving process.")
+      _.schedule(createRegistrationTask(context), delay, TimeUnit.MILLISECONDS)
+    }
+
+  /**
+    * Returns a task for creating and registering the actor system.
+    *
+    * @param context the bundle context
+    * @return the task
+    */
+  private def createRegistrationTask(context: BundleContext): Runnable =
+    () =>
+      val factory = createActorSystemFactory(context)
+      val system = factory.createActorSystem(fetchActorSystemName())
+      serviceRegistration.set(context.registerService(classOf[ActorSystem], system, null))
+      shutdownExecutorService()
+      removeBundleListener(context)
+
+  /**
+    * Makes sure that the managed executor service is shutdown exactly once.
+    */
+  private def shutdownExecutorService(): Unit =
+    safeCleanup(executorService)(_.shutdownNow())
+
+  /**
+    * Removes the [[BundleListener]] used by this instance if it exists.
+    *
+    * @param context the bundle context
+    */
+  private def removeBundleListener(context: BundleContext): Unit =
+    safeCleanup(bundleListener)(context.removeBundleListener)
+
+  /**
+    * Determines the name of the actor system to be created. The name can be
+    * specified using a system property. If it is not provided, a default name
+    * is used.
+    *
+    * @return the name of the actor system
+    */
+  private def fetchActorSystemName(): String =
+    getSystemProperty(PropActorSystemName) getOrElse DefaultActorSystemName
+
+  /**
+    * Determines the delay to wait for the SpiFly weaving process from the
+    * corresponding system property. If it is not provided, a default delay is
+    * used.
+    *
+    * @return the SpiFly delay (in milliseconds)
+    */
+  private def fetchSpiFlyDelay(): Long =
+    getSystemProperty(PropSpiFlyDelayMs).map(_.toLong) getOrElse DefaultSpiFlyDelay
--- actorSystem/src/test/scala/de/oliver_heger/linedj/actorsystem/ActivatorSpec.scala
+++ actorSystem/src/test/scala/de/oliver_heger/linedj/actorsystem/ActivatorSpec.scala
@@ -17,36 +17,313 @@
 package de.oliver_heger.linedj.actorsystem
 
 import org.apache.pekko.actor.ActorSystem
-import org.mockito.Mockito._
-import org.osgi.framework.BundleContext
+import org.apache.pekko.osgi.OsgiActorSystemFactory
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.{any, anyLong, eq as argEq}
+import org.mockito.Mockito.*
+import org.osgi.framework.{Bundle, BundleContext, BundleEvent, BundleListener, ServiceRegistration}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.mockito.MockitoSugar
+
+import java.util.concurrent.{ScheduledExecutorService, TimeUnit}
 
 /**
   * Test class for ''Activator''.
   */
 class ActivatorSpec extends AnyFlatSpec with Matchers with MockitoSugar:
-  "An Activator" should "register the actor system" in:
-    val context = mock[BundleContext]
-    val system = mock[ActorSystem]
+  /**
+    * Creates a mock for a [[Bundle]] with the given properties.
+    *
+    * @param name  the symbolic bundle name
+    * @param state the state of the bundle
+    * @return the mock bundle
+    */
+  private def createBundle(name: String, state: Int = Bundle.ACTIVE): Bundle =
+    val bundle = mock[Bundle]
+    when(bundle.getSymbolicName).thenReturn(name)
+    when(bundle.getState).thenReturn(state)
+    bundle
+
+  "An Activator" should "create a correct executor service" in :
     val activator = new Activator
 
-    activator.configure(context, system)
-    verify(context).registerService(classOf[ActorSystem], system, null)
+    val service = activator.createExecutor()
 
-  it should "return a default actor system name" in:
+    // Can only test that a valid object is returned.
+    service.close()
+
+  it should "create a correct actor system factory" in :
+    val bundle = mock[Bundle]
     val context = mock[BundleContext]
+    when(context.getBundle).thenReturn(bundle)
+
     val activator = new Activator
+    val factory = activator.createActorSystemFactory(context)
 
-    activator.getActorSystemName(context) should be("LineDJ_PlatformActorSystem")
+    factory.context should be(context)
 
-  it should "read the actor system name from a system property" in:
-    val SystemName = "ActivatorSpecActorSystem"
-    val context = mock[BundleContext]
-    val activator = new Activator:
-      override def getSystemProperty(key: String): Option[String] =
-        if "LineDJ_ActorSystemName" == key then Some(SystemName)
-        else super.getSystemProperty(key)
+  it should "create the actor system if the weaving bundle is active after a delay" in :
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle")
+    val helper = new ActivatorTestHelper
 
-    activator.getActorSystemName(context) should be(SystemName)
+    helper.initExistingBundles(Array(spiFlyBundle))
+      .start()
+      .expectSchedule(Activator.DefaultSpiFlyDelay)
+      .verifyActorSystemRegistration()
+      .verifyExecutorClosed()
+
+    verify(helper.bundleContext, never()).addBundleListener(any())
+
+  it should "obtain the name of the actor system from system properties" in :
+    val ActorSystemName = "myTestActorSystem"
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle")
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array(spiFlyBundle))
+      .setSystemProperty(Activator.PropActorSystemName, ActorSystemName)
+      .start()
+      .expectSchedule(Activator.DefaultSpiFlyDelay)
+      .verifyActorSystemRegistration(ActorSystemName)
+      .verifyExecutorClosed()
+
+  it should "obtain the SpiFly delay from system properties" in :
+    val SpiFlyDelay = 1234L
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle")
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array(spiFlyBundle))
+      .setSystemProperty(Activator.PropSpiFlyDelayMs, SpiFlyDelay.toString)
+      .start()
+      .expectSchedule(SpiFlyDelay)
+      .verifyActorSystemRegistration()
+      .verifyExecutorClosed()
+
+  it should "unregister the actor system when the bundle is stopped" in :
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle")
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array(spiFlyBundle))
+      .start()
+      .expectSchedule(Activator.DefaultSpiFlyDelay)
+      .stop()
+      .verifyServiceUnregistered()
+
+  it should "handle a stop call before the registration of the actor system" in :
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array.empty)
+      .start()
+      .stop()
+      .verifyExecutorClosed()
+    verify(helper.bundleContext).removeBundleListener(any())
+
+  it should "wait for the SpiFly bundle to start up" in :
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle")
+    val someBundle = createBundle("some-bundle")
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array(someBundle))
+      .start()
+      .expectNoSchedule()
+      .sendBundleEvent(new BundleEvent(BundleEvent.STARTED, spiFlyBundle))
+      .expectSchedule(Activator.DefaultSpiFlyDelay)
+      .verifyActorSystemRegistration()
+      .verifyExecutorClosed()
+
+    verify(helper.bundleContext).removeBundleListener(any())
+
+  it should "correctly evaluate the state of the SpiFly bundle" in :
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle", Bundle.STARTING)
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array(spiFlyBundle))
+      .start()
+      .sendBundleEvent(new BundleEvent(BundleEvent.INSTALLED, createBundle("some-bundle")))
+
+  it should "correctly evaluate the bundle event type" in :
+    val event = new BundleEvent(BundleEvent.STARTING, createBundle("org.apache.aries.spifly.dynamic.bundle"))
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array.empty)
+      .start()
+      .sendBundleEvent(event)
+      .expectNoSchedule()
+
+  it should "handle a stop() call before the delay is over" in :
+    val spiFlyBundle = createBundle("org.apache.aries.spifly.dynamic.bundle")
+    val helper = new ActivatorTestHelper
+
+    helper.initExistingBundles(Array.empty)
+      .start()
+      .stop()
+      .sendBundleEvent(new BundleEvent(BundleEvent.STARTED, spiFlyBundle))
+      .expectNoSchedule()
+
+  /**
+    * A test helper class that manages a test instance and its mock
+    * dependencies.
+    */
+  private class ActivatorTestHelper:
+    /** Mock for the bundle context. */
+    val bundleContext: BundleContext = mock[BundleContext]
+
+    /** The actor system created by the activator. */
+    private val actorSystem = mock[ActorSystem]
+
+    /** The mock executor service. */
+    private val executorService = mock[ScheduledExecutorService]
+
+    /** The mock for the actor system factory. */
+    private val actorSystemFactory = createFactoryMock()
+
+    /** The service registration for the actor system. */
+    private val serviceRegistration = createServiceRegistration()
+
+    /** A map to simulate system properties. */
+    private val systemProperties = collection.mutable.Map.empty[String, String]
+
+    /** The instance to be tested. */
+    private val testActivator = createActivator()
+
+    /**
+      * Adds a simulated system property.
+      *
+      * @param key   the property key
+      * @param value the property value
+      * @return this test helper
+      */
+    def setSystemProperty(key: String, value: String): ActivatorTestHelper =
+      systemProperties += key -> value
+      this
+
+    /**
+      * Prepares the mock bundle context to return the given existing bundles.
+      *
+      * @param bundles the bundles existing in the system
+      * @return this test helper
+      */
+    def initExistingBundles(bundles: Array[Bundle]): ActivatorTestHelper =
+      when(bundleContext.getBundles).thenReturn(bundles)
+      this
+
+    /**
+      * Invokes the ''start'' method on the test activator.
+      *
+      * @return this test helper
+      */
+    def start(): ActivatorTestHelper =
+      testActivator.start(bundleContext)
+      this
+
+    /**
+      * Invokes the ''stop'' method on the test activator.
+      *
+      * @return this test helper
+      */
+    def stop(): ActivatorTestHelper =
+      testActivator.stop(bundleContext)
+      this
+
+    /**
+      * Verifies that an actor system was correctly created and registered as
+      * an OSGi service.
+      *
+      * @param name the expected name of the actor system
+      * @return this test helper
+      */
+    def verifyActorSystemRegistration(name: String = Activator.DefaultActorSystemName): ActivatorTestHelper =
+      verify(actorSystemFactory).createActorSystem(name)
+      verify(bundleContext).registerService(classOf[ActorSystem], actorSystem, null)
+      this
+
+    /**
+      * Expects that a task was scheduled on the executor service with the
+      * given delay. Simulates the execution of this task.
+      *
+      * @param delayMs the expected delay in milliseconds
+      * @return this test helper
+      */
+    def expectSchedule(delayMs: Long): ActivatorTestHelper =
+      val captor = ArgumentCaptor.forClass(classOf[Runnable])
+      verify(executorService).schedule(captor.capture(), argEq(delayMs), argEq(TimeUnit.MILLISECONDS))
+      captor.getValue.run()
+      this
+
+    /**
+      * Checks that no task has been scheduled on the executor service yet.
+      *
+      * @return this test helper
+      */
+    def expectNoSchedule(): ActivatorTestHelper =
+      verify(executorService, never()).schedule(any[Runnable], anyLong(), any())
+      this
+
+    /**
+      * Sends the given event to the registered bundle listener.
+      *
+      * @param event the event to send
+      * @return this test helper
+      */
+    def sendBundleEvent(event: BundleEvent): ActivatorTestHelper =
+      val captor = ArgumentCaptor.forClass(classOf[BundleListener])
+      verify(bundleContext).addBundleListener(captor.capture())
+      captor.getValue.bundleChanged(event)
+      this
+
+    /**
+      * Verifies that the executor service has been correctly closed.
+      *
+      * @return this test helper
+      */
+    def verifyExecutorClosed(): ActivatorTestHelper =
+      verify(executorService, times(1)).shutdownNow()
+      this
+
+    /**
+      * Verifies that the service for the actor system has been unregistered.
+      *
+      * @return this test helper
+      */
+    def verifyServiceUnregistered(): ActivatorTestHelper =
+      verify(serviceRegistration, times(1)).unregister()
+      this
+
+    /**
+      * Creates a mock for the actor system factory.
+      *
+      * @return the mock actor system factory
+      */
+    private def createFactoryMock(): OsgiActorSystemFactory =
+      val factory = mock[OsgiActorSystemFactory]
+      when(factory.createActorSystem(any[String]())).thenReturn(actorSystem)
+      factory
+
+    /**
+      * Creates a mock for the service registration that can be used to verify
+      * that the service is correctly unregistered when the activator is
+      * stopped.
+      *
+      * @return the mock registration
+      */
+    private def createServiceRegistration(): ServiceRegistration[ActorSystem] =
+      val registration = mock[ServiceRegistration[ActorSystem]]
+      when(bundleContext.registerService(classOf[ActorSystem], actorSystem, null)).thenReturn(registration)
+      registration
+
+    /**
+      * Creates the instance to be tested.
+      *
+      * @return the test [[Activator]] instance.
+      */
+    private def createActivator(): Activator =
+      new Activator:
+        override private[actorsystem] def createActorSystemFactory(context: BundleContext): OsgiActorSystemFactory =
+          context should be(bundleContext)
+          actorSystemFactory
+
+        override private[actorsystem] def createExecutor(): ScheduledExecutorService =
+          executorService
+
+        override def getSystemProperty(key: String): Option[String] =
+          systemProperties.get(key)
