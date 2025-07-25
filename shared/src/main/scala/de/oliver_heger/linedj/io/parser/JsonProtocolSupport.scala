3750965d393f253d5b4a4e0738e6c316dd9fa722
Oliver Heger <oliver.heger@oliver-heger.de>
2025-07-25 18:54:26 +0200

    shared: Added `JsonProtocolSupport` object.
    This object provides helper functionality for custom JSON protocol
    implementations. Initially, there is a helper function for dealing
    with `enum` types.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ shared/src/main/scala/de/oliver_heger/linedj/io/parser/JsonProtocolSupport.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.io.parser
+
+import spray.json.*
+
+/**
+  * An object providing some helper functionality for dealing with JSON
+  * serialization and deserialization.
+  */
+object JsonProtocolSupport:
+  /**
+    * Produces a [[RootJsonFormat]] for a type that supports a native
+    * ''toString()'' conversion and a conversion function to convert strings
+    * back to the type. This can be used for instance for ''enum'' types.
+    *
+    * @param c the conversion function from a string to the target type
+    * @tparam T the target type
+    * @return a [[RootJsonFormat]] to handle this type
+    */
+  def enumFormat[T](c: String => T): RootJsonFormat[T] = new RootJsonFormat[T]:
+    override def read(json: JsValue): T = json match
+      case JsString(value) => c(value)
+      case _ => deserializationError("String expected with an enum literal.")
+
+    override def write(obj: T): JsValue = JsString(obj.toString)
---
+++ shared/src/test/scala/de/oliver_heger/linedj/io/parser/JsonProtocolSupportSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.io.parser
+
+import de.oliver_heger.linedj.io.parser.JsonProtocolSupportSpec.{TestResult, TestStatus}
+import org.scalatest.Inspectors.forEvery
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import spray.json.*
+import spray.json.DefaultJsonProtocol.*
+
+object JsonProtocolSupportSpec extends DefaultJsonProtocol:
+  /**
+    * A test enumeration to be serialized to JSON.
+    */
+  enum TestStatus:
+    case Succeeded, Failure, Unclear
+
+  given RootJsonFormat[TestStatus] = JsonProtocolSupport.enumFormat(TestStatus.valueOf)
+
+  /**
+    * A simple data class to be serialized to JSON.
+    *
+    * @param status     the status of the test execution
+    * @param durationMs the duration of the test
+    */
+  case class TestResult(status: TestStatus,
+                        durationMs: Long)
+
+  given RootJsonFormat[TestResult] = jsonFormat2(TestResult.apply)
+
+/**
+  * Test class for [[JsonProtocolSupport]].
+  */
+class JsonProtocolSupportSpec extends AnyFlatSpec with Matchers:
+  "enumFormat" should "handle the conversion of an enum type" in :
+    forEvery(TestStatus.values) { status =>
+      val result = TestResult(status, 20250725121954L)
+      val resultJson = result.toJson.prettyPrint
+      val jsonAst = resultJson.parseJson
+      val result2 = jsonAst.convertTo[TestResult]
+
+      result2 should be(result)
+    }
+
+  it should "handle unexpected input" in :
+    val invalidStatusJson =
+      """
+        |{
+        |  "durationMs": 5000,
+        |  "status": false
+        |}""".stripMargin
+    val jsonAst = invalidStatusJson.parseJson
+
+    val exception = intercept[DeserializationException]:
+      jsonAst.convertTo[TestResult]
+
+    exception.getMessage should include("with an enum literal")
