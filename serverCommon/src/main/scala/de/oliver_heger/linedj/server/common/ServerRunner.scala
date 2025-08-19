8d89bc560d19623b1f2d8dd0e21cc384ba6734c6
Oliver Heger <oliver.heger@oliver-heger.de>
2025-08-19 22:05:17 +0200

    serverCommon: Added `ServerRunner` class.
    This class is responsible for starting up an HTTP server controlled by
    a `ServerController` and to manage its life-cycle.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ serverCommon/src/main/scala/de/oliver_heger/linedj/server/common/ServerRunner.scala
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.server.common
+
+import de.oliver_heger.linedj.server.common.ServerController.given
+import de.oliver_heger.linedj.shared.actors.ManagingActorFactory
+import org.apache.logging.log4j.LogManager
+import org.apache.pekko.http.scaladsl.Http
+import org.apache.pekko.http.scaladsl.Http.ServerBinding
+import org.apache.pekko.http.scaladsl.server.Route
+import org.apache.pekko.{Done, actor as classic}
+
+import scala.concurrent.duration.*
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success}
+
+object ServerRunner:
+  /** The hard timeout when terminating the actor system. */
+  private val TerminationTimeout = 5.seconds
+
+  /** The logger. */
+  private val log = LogManager.getLogger(classOf[ServerRunner])
+
+  /**
+    * A trait that can be used to interact with a running server and to stop
+    * it. An implementation is returned by the [[ServerRunner.launch]]
+    * function.
+    */
+  trait ServerHandle:
+    /**
+      * Returns a [[Future]] that completes when the server has shut down,
+      * i.e. after the [[ServerController.afterShutdown]] callback has been
+      * executed. This can be used for instance to wait until all shutdown
+      * actions have been performed.
+      *
+      * @return a [[Future]] to report the server's shutdown
+      */
+    def shutdownFuture: Future[Done]
+
+    /**
+      * Allows triggering the server's shutdown by an external party. After
+      * calling this function, the server stops listening for incoming 
+      * requests, frees resources, and invokes the shutdown callback. Calling
+      * this function again, has no effect.
+      */
+    def shutdown(): Unit
+  end ServerHandle
+
+  /**
+    * An internally used data class to record information while the server is
+    * starting up.
+    *
+    * @param binding the [[ServerBinding]] of the server
+    * @param context the server's context
+    * @tparam CONTEXT the type of the server's context
+    */
+  private[common] case class ServerStartupData[CONTEXT](binding: ServerBinding,
+                                                        context: CONTEXT)
+
+  /**
+    * Enables the server to shut down gracefully when the given shutdown future
+    * completes. This function installs hooks that stop all managed actors and
+    * terminate the actor system when both the startup future and the shutdown
+    * future are completed. It returns a [[Future]] with the server's context
+    * that completes when the actor system has been terminated. This [[Future]]
+    * is in failed state when the server could not be started.
+    *
+    * @param startupFuture  the [[Future]] controlling server startup
+    * @param shutdownFuture the [[Future]] to trigger the shutdown
+    * @param services       the object with services
+    * @tparam CONTEXT the type of the server's context
+    * @return a [[Future]] that completes when shutdown is done with the 
+    *         server's context
+    */
+  private[common] def enableGracefulShutdown[CONTEXT](startupFuture: Future[ServerStartupData[CONTEXT]],
+                                                      shutdownFuture: Future[Done])
+                                                     (using services: ServerController.ServerServices): Future[CONTEXT] =
+    val shutdownStartupFuture = startupFuture.map: startup =>
+      val shutdownBinding = startup.binding.addToCoordinatedShutdown(TerminationTimeout)
+      startup.copy(binding = shutdownBinding)
+    val futCanShutdown = for
+      startup <- shutdownStartupFuture
+      _ <- shutdownFuture
+    yield startup.context
+
+    val promiseTerminated = Promise[CONTEXT]()
+    futCanShutdown.onComplete: triedContext =>
+      log.info("Stopping actors and terminating actor system.")
+      services.managingActorFactory.stopActors()
+      services.system.terminate() onComplete : _ =>
+        triedContext match
+          case Failure(exception) =>
+            log.error("Error when setting up server.", exception)
+            promiseTerminated.failure(exception)
+          case Success(value) =>
+            promiseTerminated.success(value)
+
+    promiseTerminated.future
+
+  /**
+    * Starts the Pekko HTTP server for a managed server based on the given
+    * parameters. Returns a [[Future]] with information about the startup.
+    *
+    * @param bindingParameters the parameters how to bind the server
+    * @param context           the context of the server
+    * @param route             the route specification for the server
+    * @param services          the object with services
+    * @tparam CONTEXT the type of the server's context
+    * @return a [[Future]] with startup information
+    */
+  private def startHttpServer[CONTEXT](bindingParameters: ServerController.BindingParameters,
+                                       context: CONTEXT,
+                                       route: Route)
+                                      (using services: ServerController.ServerServices):
+  Future[ServerStartupData[CONTEXT]] =
+    log.info(
+      "Starting HTTP server and binding it to '{}:{}'.",
+      bindingParameters.bindInterface,
+      bindingParameters.bindPort
+    )
+    Http().newServerAt(bindingParameters.bindInterface, bindingParameters.bindPort)
+      .bind(route)
+      .map(binding => ServerStartupData(binding, context))
+end ServerRunner
+
+/**
+  * A class to manage the whole life-cycle of an HTTP server.
+  *
+  * An instance can be used to start and manage a server defined by a 
+  * [[ServerController]] object whose life-cycle is connected to an actor
+  * system. The server is bound according to the parameters provided by its
+  * handler. Once started, the server can do whatever it wants. It can trigger
+  * its own termination. Alternatively, the shutdown of the server can be 
+  * initiated externally using the [[ServerRunner.ServerHandle]] returned by
+  * this runner.
+  *
+  * @param system the [[classic.ActorSystem]] to operate the servers
+  */
+class ServerRunner(using system: classic.ActorSystem):
+
+  import ServerRunner.*
+
+  /**
+    * Starts a server that is managed by the given controller. This is a 
+    * non-blocking operation. It returns a [[ServerHandle]] for further
+    * interaction with the server immediately, while the server starts serving
+    * requests in background.
+    *
+    * @param controller the controller for the server
+    * @return a [[ServerHandle]] for this server
+    */
+  def launch(controller: ServerController): ServerHandle =
+    log.info("Launching server controlled by {}.", controller.getClass.getSimpleName)
+
+    val factory = ManagingActorFactory.newDefaultManagingActorFactory
+    val shutdownPromise = Promise[Done]()
+    given ServerController.ServerServices(system, factory)
+
+    val startFuture = (for
+      context <- controller.createContext
+      bindingParams <- controller.bindingParameters(context)
+      route = controller.route(context, shutdownPromise)
+      startup <- startHttpServer(bindingParams, context, route)
+    yield startup) andThen :
+      case Success(startupData) =>
+        log.info("HTTP server is listening on port {}.", startupData.binding.localAddress.getPort)
+      case Failure(exception) =>
+        log.error("Failed to start HTTP server.", exception)
+
+    val donePromise = Promise[Done]()
+    val gracefulShutdownFuture = enableGracefulShutdown(startFuture, shutdownPromise.future) map : context =>
+      log.info("HTTP server has shut down.")
+      controller.afterShutdown(context)
+      Done
+
+    new ServerHandle:
+      override val shutdownFuture: Future[Done] = gracefulShutdownFuture
+
+      override def shutdown(): Unit =
+        if shutdownPromise.trySuccess(Done) then
+          log.info("HTTP server was shut down using its handle.")
---
+++ serverCommon/src/test/scala/de/oliver_heger/linedj/server/common/ServerRunnerSpec.scala
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.server.common
+
+import de.oliver_heger.linedj.server.common
+import de.oliver_heger.linedj.shared.actors.ManagingActorFactory
+import org.apache.pekko.actor.Terminated
+import org.apache.pekko.http.scaladsl.Http
+import org.apache.pekko.http.scaladsl.Http.ServerBinding
+import org.apache.pekko.http.scaladsl.model.{HttpMethods, HttpRequest, StatusCodes, Uri}
+import org.apache.pekko.http.scaladsl.server.Directives.*
+import org.apache.pekko.http.scaladsl.server.{Directives, Route}
+import org.apache.pekko.http.scaladsl.unmarshalling.Unmarshal
+import org.apache.pekko.testkit.TestKit
+import org.apache.pekko.{Done, actor as classic}
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+import org.scalatest.concurrent.Eventually
+import org.scalatest.flatspec.AsyncFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{Assertion, BeforeAndAfterAll, Succeeded}
+import org.scalatestplus.mockito.MockitoSugar
+
+import java.net.ServerSocket
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Future, Promise}
+import scala.util.Using
+
+object ServerRunnerSpec:
+  /** Timeout in milliseconds when waiting for something to happen. */
+  private val WaitTimeoutMs = 500
+
+  /** An object used as simulated context of the test server. */
+  private val ServerContext = "The context of the test server"
+
+  /**
+    * Determines a free port number that can be used by the test server.
+    *
+    * @return the free port number
+    */
+  private def findFreePort(): Int =
+    Using(new ServerSocket(0)) {
+      _.getLocalPort
+    }.get
+
+  /**
+    * Type alias for a function that can test a running server instance. The
+    * function is passed the base [[Uri]] to the server and a handle to it.
+    * During its execution, it should trigger the shutdown of the server.
+    */
+  type ServerTestFunc = (Uri, ServerRunner.ServerHandle) => Future[Assertion]
+end ServerRunnerSpec
+
+/**
+  * Test class for [[ServerRunner]].
+  */
+class ServerRunnerSpec(testSystem: classic.ActorSystem) extends TestKit(testSystem) with AsyncFlatSpecLike
+  with BeforeAndAfterAll with Matchers with MockitoSugar with Eventually:
+  def this() = this(classic.ActorSystem("ServerRunnerSpec"))
+
+  override protected def afterAll(): Unit =
+    TestKit.shutdownActorSystem(system)
+    super.afterAll()
+
+  import ServerRunnerSpec.*
+
+  /**
+    * Expects that the given [[Future]] does not complete within the configured
+    * interval.
+    *
+    * @param f the future
+    * @tparam T the type of the future
+    */
+  private def expectNoCompletion[T](f: Future[T]): Assertion =
+    val latch = new CountDownLatch(1)
+    f.onComplete(_ => latch.countDown())(using system.dispatcher)
+
+    latch.await(WaitTimeoutMs, TimeUnit.MILLISECONDS) shouldBe false
+
+  "enableGracefulShutdown" should "call the shutdown when all conditions are met" in :
+    val mockBinding = mock[ServerBinding]
+    val mockSystem = mock[classic.ActorSystem]
+    val mockActorFactory = mock[ManagingActorFactory]
+    when(mockSystem.dispatcher).thenReturn(system.dispatcher)
+    when(mockSystem.terminate()).thenReturn(Future.successful(mock[Terminated]))
+    val startupData = ServerRunner.ServerStartupData(mockBinding, ServerContext)
+    given ServerController.ServerServices(mockSystem, mockActorFactory)
+
+    val futTerminate = ServerRunner.enableGracefulShutdown(
+      Future.successful(startupData),
+      Future.successful(Done)
+    )
+    futTerminate map : t =>
+      verify(mockBinding).addToCoordinatedShutdown(5.seconds)(mockSystem)
+      verify(mockActorFactory).stopActors()
+      verify(mockSystem).terminate()
+      t shouldBe ServerContext
+
+  it should "not shut down before the server has been fully started" in :
+    val mockSystem = mock[classic.ActorSystem]
+    val mockActorFactory = mock[ManagingActorFactory]
+    when(mockSystem.dispatcher).thenReturn(system.dispatcher)
+    val promiseStartup = Promise[ServerRunner.ServerStartupData[Any]]()
+    given ServerController.ServerServices(mockSystem, mockActorFactory)
+
+    expectNoCompletion(
+      ServerRunner.enableGracefulShutdown(
+        promiseStartup.future,
+        Future.successful(Done)
+      )
+    )
+
+    verify(mockActorFactory, never()).stopActors()
+    verify(mockSystem, never()).terminate()
+    Succeeded
+
+  it should "not shut down before the shutdown future has completed" in :
+    val mockBinding = mock[ServerBinding]
+    val startupData = ServerRunner.ServerStartupData(mockBinding, "some context")
+    val mockSystem = mock[classic.ActorSystem]
+    val mockActorFactory = mock[ManagingActorFactory]
+    when(mockSystem.dispatcher).thenReturn(system.dispatcher)
+    val promiseShutdown = Promise[Done]()
+    given ServerController.ServerServices(mockSystem, mockActorFactory)
+
+    expectNoCompletion(
+      ServerRunner.enableGracefulShutdown(
+        Future.successful(startupData),
+        promiseShutdown.future
+      )
+    )
+
+    verify(mockActorFactory, never()).stopActors()
+    verify(mockSystem, never()).terminate()
+    Succeeded
+
+  it should "shut down the actor system even if the startup future failed" in :
+    val mockSystem = mock[classic.ActorSystem]
+    when(mockSystem.terminate()).thenReturn(Future.successful(mock[Terminated]))
+    when(mockSystem.dispatcher).thenReturn(system.dispatcher)
+    val mockActorFactory = mock[ManagingActorFactory]
+    val promiseStartup = Promise[ServerRunner.ServerStartupData[Any]]()
+    given ServerController.ServerServices(mockSystem, mockActorFactory)
+
+    val futureTerminated = ServerRunner.enableGracefulShutdown(
+      promiseStartup.future,
+      Future.successful(Done)
+    )
+
+    val startUpException = new IllegalStateException("Test exception")
+    val futureException = recoverToExceptionIf[IllegalStateException]:
+      promiseStartup.failure(startUpException)
+      futureTerminated
+
+    futureException map : e =>
+      verify(mockActorFactory).stopActors()
+      verify(mockSystem).terminate()
+      e should be(startUpException)
+
+  /**
+    * Returns a spy for the actor system that prevents that it gets terminated
+    * when the test server shuts down.
+    *
+    * @return the spy for the actor system
+    */
+  private def actorSystemSpy: classic.ActorSystem =
+    val systemSpy = spy(system)
+    doReturn(Future.successful(Terminated)).when(systemSpy).terminate()
+    systemSpy
+
+  /**
+    * Creates a [[ServerRunner]] to be used for tests which does not operate
+    * on the implicit actor system of this test class, but a spy that cannot be
+    * terminated.
+    *
+    * @return the runner for test execution
+    */
+  private def createRunner(): ServerRunner =
+    new ServerRunner(using actorSystemSpy)
+
+  /**
+    * Checks whether a server is up and can be accessed via the given URI.
+    *
+    * @param serverUri the URI of the server
+    * @return a [[Future]] with the test result
+    */
+  private def checkRunningServer(serverUri: Uri): Future[Assertion] =
+    val request = HttpRequest(uri = serverUri.withPath(Uri.Path("/test/result")))
+    Http().singleRequest(request) flatMap : response =>
+      response.status should be(StatusCodes.OK)
+      Unmarshal(response).to[String] map : responseStr =>
+        responseStr should be("42")
+
+  /**
+    * Creates a [[ServerController]] object to manage a test server instance.
+    * The controller records whether its shutdown callback is called.
+    * Optionally, it can simulate a failure to create its context; this should
+    * prevent the server from starting up.
+    *
+    * @param serverPort          the port to bind the server to
+    * @param afterShutdownCalled flag to record the invocation of the shutdown
+    *                            callback
+    * @param contextException    an exception to throw when creating the 
+    *                            context
+    * @return the controller object
+    */
+  private def createTestController(serverPort: Int,
+                                   afterShutdownCalled: AtomicBoolean = new AtomicBoolean,
+                                   contextException: Option[Throwable] = None): ServerController =
+    new ServerController:
+      override type Context = String
+
+      override def createContext(using services: ServerController.ServerServices): Future[String] =
+        contextException match
+          case None => Future.successful(ServerContext)
+          case Some(exception) => Future.failed(exception)
+
+      override def bindingParameters(context: String)
+                                    (using services: ServerController.ServerServices):
+      Future[ServerController.BindingParameters] =
+        context should be(ServerContext)
+        Future.successful(ServerController.BindingParameters("localhost", serverPort))
+
+      override def route(context: String, shutdownPromise: Promise[Done])
+                        (using services: ServerController.ServerServices): Route =
+        pathPrefix("test"):
+          concat(
+            path("shutdown"):
+              post:
+                shutdownPromise.success(Done)
+                Directives.complete(StatusCodes.Accepted),
+            path("result"):
+              Directives.complete("42")
+          )
+
+      override def afterShutdown(context: String): Unit =
+        context should be(ServerContext)
+        afterShutdownCalled.set(true)
+
+  /**
+    * Starts up a test server and executes a [[ServerTestFunc]] to interact
+    * with it. The test server exposes a GET endpoint for querying a simple
+    * value and a POST endpoint for triggering its shutdown.
+    *
+    * @param testFunc the function to execute the test
+    * @return the result from the test function
+    */
+  private def testServer(testFunc: ServerTestFunc): Future[Assertion] =
+    val serverPort = findFreePort()
+    val afterShutdownCalled = new AtomicBoolean
+
+    val controller = createTestController(serverPort, afterShutdownCalled)
+    val runner = createRunner()
+    val handle = runner.launch(controller)
+    val serverUri = Uri(s"http://localhost:$serverPort")
+
+    eventually:
+      checkRunningServer(serverUri)
+
+    val testResult = testFunc(serverUri, handle)
+
+    awaitCond(afterShutdownCalled.get())
+    testResult
+
+  "launch" should "connect the server's route with the shutdown future" in :
+    testServer: (uri, handle) =>
+      val shutdownRequest = HttpRequest(method = HttpMethods.POST, uri = uri.withPath(Uri.Path("/test/shutdown")))
+      Http().singleRequest(shutdownRequest) map : response =>
+        response.status should be(StatusCodes.Accepted)
+
+  it should "return a handle that can be used to shut down the server" in :
+    testServer: (_, handle) =>
+      handle.shutdown()
+
+      handle.shutdownFuture map (_ => Succeeded)
+
+  it should "handle a failure to create the server context" in :
+    val shutdownCalled = new AtomicBoolean
+    val exception = new IllegalStateException("Test exception: Could not create server context.")
+    val controller = createTestController(findFreePort(), shutdownCalled, contextException = Some(exception))
+
+    val runner = createRunner()
+    val handle = runner.launch(controller)
+
+    recoverToExceptionIf[IllegalStateException](handle.shutdownFuture) map : handleException =>
+      shutdownCalled.get() shouldBe false
+      handleException.getMessage should be(exception.getMessage)  
+      
