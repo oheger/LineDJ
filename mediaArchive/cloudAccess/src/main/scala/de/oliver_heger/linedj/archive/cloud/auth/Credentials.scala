407fbfe1f4a4603a8f42b22ea2b8ac913aa6028c
Oliver Heger <oliver.heger@oliver-heger.de>
2026-02-03 22:08:35 +0100

    cloudAccess: Implemented an actor-based resolver function.
    The `Credentials` module now supports creating a function to query
    credentials. The function is backed by an actor that manages
    credentials.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- mediaArchive/cloudAccess/src/main/scala/de/oliver_heger/linedj/archive/cloud/auth/Credentials.scala
+++ mediaArchive/cloudAccess/src/main/scala/de/oliver_heger/linedj/archive/cloud/auth/Credentials.scala
@@ -17,8 +17,15 @@
 package de.oliver_heger.linedj.archive.cloud.auth
 
 import com.github.cloudfiles.core.http.Secret
+import de.oliver_heger.linedj.shared.actors.ActorFactory
+import org.apache.pekko.actor.typed.scaladsl.AskPattern.{Askable, schedulerFromActorSystem}
+import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.scaladsl.adapter.*
+import org.apache.pekko.actor.typed.{ActorRef, ActorSystem, Behavior}
+import org.apache.pekko.util.Timeout
 
-import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * A module providing functionality to deal with credentials.
@@ -31,6 +38,17 @@ import scala.concurrent.Future
   * to clients that need these credentials.
   */
 object Credentials:
+  /** The default name for the credentials manager actor. */
+  final val CredentialsManagerName = "credentialsManager"
+
+  /**
+    * A default timeout value for querying credentials from a credentials
+    * manager actor. In the default use cases, it is expected that credentials
+    * are eventually entered by a user. Therefore, this timeout value is set
+    * very high. If needed, a custom timeout can be provided.
+    */
+  given queryCredentialTimeout: Timeout = Timeout(30.days)
+
   /**
     * An alias for a function that can resolve credentials (for cloud archives)
     * based on keys. It is used by different components to obtain the concrete
@@ -38,3 +56,148 @@ object Credentials:
     * and returns a [[Future]] with the resolved [[Secret]].
     */
   type ResolverFunc = String => Future[Secret]
+
+  /**
+    * A data class representing the information to be stored for a specific
+    * credential. This is basically a pair consisting of a (unique) key and the
+    * secret value of the credential.
+    *
+    * @param key   the key of the credential
+    * @param value the secret value
+    */
+  final case class CredentialData(key: String, value: Secret)
+
+  /**
+    * A data class representing a command to query information about a specific
+    * credential. The credentials manager actor sends an answer immediately if
+    * the credential is known. Otherwise, it records this request and waits
+    * until the credential becomes available. So, a result is eventually sent,
+    * but maybe with a delay.
+    *
+    * @param key      the key of the desired credential
+    * @param replayTo the actor to send the reply to
+    */
+  final case class QueryCredential(key: String,
+                                   replayTo: ActorRef[CredentialData])
+
+  /**
+    * A command that tells the credentials manager actor to stop itself.
+    */
+  case object Stop
+
+  /**
+    * Definition of the command type that is handled by the credentials manager
+    * actor. This is a union type, which makes it possible to let clients
+    * operate on narrowed actor references that support only the subcommands
+    * they need.
+    */
+  type CredentialsManagerCommand = CredentialData | QueryCredential | Stop.type
+
+  /**
+    * A helper class to manage the state of known credentials.
+    *
+    * This class holds the information required by the credential manager 
+    * actor. It provides methods to update the state according to the supported
+    * use cases.
+    *
+    * @param credentials a map storing the known credentials
+    * @param clients     a map with the clients waiting for a specific key
+    */
+  private class CredentialsState(val credentials: Map[String, CredentialData],
+                                 val clients: Map[String, List[ActorRef[CredentialData]]]):
+    /**
+      * Updates this state for a query credential operation. If the requested
+      * credential is known, the request can be answered, and the state does 
+      * not need to be changed. Otherwise, the client has to be stored, so that
+      * it can be notified when the credential becomes available. The function
+      * returns a tuple with the updated state and the optional credential, so
+      * that the caller can answer the request if possible.
+      *
+      * @param key    the key of the desired credential
+      * @param client the requesting actor
+      * @return a tuple with the updated state and the credential if known
+      */
+    def queryCredential(key: String, client: ActorRef[CredentialData]): (CredentialsState, Option[CredentialData]) =
+      val optData = credentials.get(key)
+      val nextState = optData match
+        case Some(_) => this
+        case None =>
+          val waitingClients = clients.getOrElse(key, Nil)
+          CredentialsState(credentials, clients + (key -> (client :: waitingClients)))
+      (nextState, optData)
+
+    /**
+      * Updates this state for a credential becoming available. The credential 
+      * is stored. If there are clients waiting for the affected key, they can
+      * now be notified. The function returns a tuple with the updated state 
+      * and a collection of actors that are waiting for this credential.
+      *
+      * @param data the data about the incoming credential
+      * @return a tuple with the updated state and the actors to notify
+      */
+    def addCredential(data: CredentialData): (CredentialsState, Iterable[ActorRef[CredentialData]]) =
+      (CredentialsState(credentials + (data.key -> data), clients - data.key), clients.getOrElse(data.key, Nil))
+  end CredentialsState
+
+  /** Constant for an initial, empty credentials state instance. */
+  private val InitialCredentialsState = CredentialsState(Map.empty, Map.empty)
+
+  /**
+    * Creates a credentials manager actor and a [[ResolverFunc]] that queries
+    * this actor. The resulting tuple can be used to implement an 
+    * application-specific credentials management: Credentials obtained via 
+    * whatever mechanism can be passed to the actor. Clients query the required
+    * credentials then from the [[ResolverFunc]] which internally delegates to
+    * the actor. 
+    *
+    * @param factory      the factory to create the actor
+    * @param actorName    the name for the actor
+    * @param queryTimeout a timeout applied by the resolver function when 
+    *                     querying the actor
+    * @return a tuple with the actor reference and the resolver function
+    */
+  def setUpCredentialsManager(factory: ActorFactory, actorName: String = CredentialsManagerName)
+                             (using queryTimeout: Timeout): (ActorRef[CredentialsManagerCommand], ResolverFunc) =
+    val actor = factory.createTypedActor(
+      handleCredentialsCommand(InitialCredentialsState),
+      actorName,
+      optStopCommand = Some(Stop)
+    )
+    (actor, createResolverFunc(actor, factory))
+
+  /**
+    * The command handler function of the credentials manager actor.
+    *
+    * @param state the current state of the actor
+    * @return the updated behavior
+    */
+  private def handleCredentialsCommand(state: CredentialsState): Behavior[CredentialsManagerCommand] =
+    Behaviors.receiveMessage:
+      case Stop =>
+        Behaviors.stopped
+      case QueryCredential(key, replayTo) =>
+        val (nextState, optData) = state.queryCredential(key, replayTo)
+        optData.foreach(replayTo.!)
+        handleCredentialsCommand(nextState)
+      case data: CredentialData =>
+        val (nextState, clients) = state.addCredential(data)
+        clients.foreach(c => c ! data)
+        handleCredentialsCommand(nextState)
+
+  /**
+    * Returns a [[ResolverFunc]] that queries credentials from the given 
+    * credentials manager actor.
+    *
+    * @param actor   the actor
+    * @param factory the actor factory
+    * @param timeout the timeout for queries
+    * @return the resolver function
+    */
+  private def createResolverFunc(actor: ActorRef[CredentialsManagerCommand], factory: ActorFactory)
+                                (using timeout: Timeout): ResolverFunc =
+    given ActorSystem[_] = factory.actorSystem.toTyped
+
+    given ExecutionContext = factory.actorSystem.dispatcher
+
+    key =>
+      actor.ask[CredentialData](ref => QueryCredential(key, ref)).map(_.value)
---
+++ mediaArchive/cloudAccess/src/test/scala/de/oliver_heger/linedj/archive/cloud/auth/CredentialsSpec.scala
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.cloud.auth
+
+import com.github.cloudfiles.core.http.Secret
+import de.oliver_heger.linedj.shared.actors.{ActorFactory, ManagingActorFactory}
+import org.apache.pekko.actor as classic
+import org.apache.pekko.actor.testkit.typed.scaladsl.ActorTestKit
+import org.apache.pekko.testkit.TestKit
+import org.apache.pekko.util.Timeout
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AsyncFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import java.util.concurrent.TimeoutException
+import scala.concurrent.duration.DurationInt
+
+/**
+  * Test class for [[Credentials]].
+  */
+class CredentialsSpec(testSystem: classic.ActorSystem) extends TestKit(testSystem), AsyncFlatSpecLike,
+  BeforeAndAfterAll, Matchers:
+  def this() = this(classic.ActorSystem("CredentialsSpec"))
+
+  /** The test kit for typed actors. */
+  private val typedTestKit = ActorTestKit()
+
+  override protected def afterAll(): Unit =
+    typedTestKit.shutdownTestKit()
+    TestKit.shutdownActorSystem(system)
+    super.afterAll()
+
+  import Credentials.given
+
+  /**
+    * Returns the [[ActorFactory]] based on the actor system of this test
+    * class.
+    *
+    * @return the actor factory for tests
+    */
+  private def actorFactory: ActorFactory = implicitly
+
+  "A credentials manager actor" should "stop itself on receiving a Stop command" in :
+    val (actor, _) = Credentials.setUpCredentialsManager(actorFactory)
+
+    actor ! Credentials.Stop
+
+    val probe = typedTestKit.createDeadLetterProbe()
+    probe.expectTerminated(actor)
+    succeed
+
+  it should "pass existing credentials to requesting clients" in :
+    val credentialData = Credentials.CredentialData("someKey", Secret("very-secret"))
+    val probe = typedTestKit.createTestProbe[Credentials.CredentialData]()
+    val (actor, _) = Credentials.setUpCredentialsManager(actorFactory)
+
+    actor ! credentialData
+    actor ! Credentials.QueryCredential(credentialData.key, probe.ref)
+
+    probe.expectMessage(credentialData)
+    succeed
+
+  it should "notify clients when their credentials become available" in :
+    val credentialData1 = Credentials.CredentialData("cred1", Secret("sec1"))
+    val credentialData2 = Credentials.CredentialData("cred2", Secret("sec2"))
+    val client1 = typedTestKit.createTestProbe[Credentials.CredentialData]()
+    val client2 = typedTestKit.createTestProbe[Credentials.CredentialData]()
+    val client3 = typedTestKit.createTestProbe[Credentials.CredentialData]()
+    val (actor, _) = Credentials.setUpCredentialsManager(actorFactory, actorName = "queryThenSet")
+
+    actor ! Credentials.QueryCredential(credentialData1.key, client1.ref)
+    actor ! Credentials.QueryCredential(credentialData2.key, client2.ref)
+    actor ! Credentials.QueryCredential(credentialData1.key, client3.ref)
+    actor ! credentialData1
+    actor ! credentialData2
+
+    client1.expectMessage(credentialData1)
+    client2.expectMessage(credentialData2)
+    client3.expectMessage(credentialData1)
+    succeed
+
+  it should "notify clients only once about incoming credentials" in :
+    val credentialData = Credentials.CredentialData("key", Secret("onlyOnce"))
+    val client = typedTestKit.createTestProbe[Credentials.CredentialData]()
+    val (actor, _) = Credentials.setUpCredentialsManager(actorFactory, actorName = "multiSet")
+
+    actor ! Credentials.QueryCredential(credentialData.key, client.ref)
+    actor ! credentialData
+    actor ! credentialData.copy(value = Secret("anotherValue"))
+
+    client.expectMessage(credentialData)
+    client.expectNoMessage(100.millis)
+    succeed
+
+  "The resolver function" should "return the correct credentials" in :
+    val credentialData1 = Credentials.CredentialData("cred1", Secret("sec1"))
+    val credentialData2 = Credentials.CredentialData("cred2", Secret("sec2"))
+    val (actor, resolver) = Credentials.setUpCredentialsManager(actorFactory, actorName = "resolverFunc")
+
+    val futSecrets = for
+      sec1 <- resolver(credentialData1.key)
+      sec2 <- resolver(credentialData2.key)
+    yield (sec1, sec2)
+
+    actor ! credentialData2
+    actor ! credentialData1
+    futSecrets map : (sec1, sec2) =>
+      sec1 should be(credentialData1.value)
+      sec2 should be(credentialData2.value)
+
+  "setUpCredentialsManager" should "set the stop command for the actor" in :
+    val managingFactory = ManagingActorFactory.newDefaultManagingActorFactory(using actorFactory)
+
+    val (actor, _) = Credentials.setUpCredentialsManager(managingFactory, actorName = "withManagingFactory")
+    managingFactory.stopActors()
+
+    val probe = typedTestKit.createDeadLetterProbe()
+    probe.expectTerminated(actor)
+    succeed
+
+  it should "take the timeout into account" in :
+    val shortTimeout = Timeout(10.millis)
+    val processingTime = 500.millis
+    val (actor, resolver) = Credentials.setUpCredentialsManager(
+      actorFactory, actorName = "runIntoTimeout"
+    )(using shortTimeout)
+
+    typedTestKit.scheduler.scheduleOnce(processingTime, () => actor ! Credentials.CredentialData("k", Secret("v")))
+    recoverToSucceededIf[TimeoutException]:
+      resolver("k")
