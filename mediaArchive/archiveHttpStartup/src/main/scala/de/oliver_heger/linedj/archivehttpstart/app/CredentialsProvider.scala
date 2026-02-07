7cb49ff997205994a418ffcb55533c0d1ce327a0
Oliver Heger <oliver.heger@oliver-heger.de>
2026-02-07 21:11:05 +0100

    archiveHttpStartup: Added a `CredentialsProvider` class.
    The class is responsible for passing the credentials of a realm to the
    credentials manager actor, so that they are available when
    constructing the file downloader for an archive in the realm to be
    started.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ mediaArchive/archiveHttpStartup/src/main/scala/de/oliver_heger/linedj/archivehttpstart/app/CredentialsProvider.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart.app
+
+import com.github.cloudfiles.core.http.Secret
+import de.oliver_heger.linedj.archive.cloud.auth.Credentials
+import de.oliver_heger.linedj.archivehttp.config.UserCredentials
+import org.apache.pekko.actor.typed.ActorRef
+
+/**
+  * A helper class to pass the credentials for a specific realm to a
+  * credentials manager actor instance.
+  *
+  * This class mainly implements a work-around that allows for an easy
+  * integration of the ''cloudAccess'' module with the HTTP archive startup
+  * application - without major changes on this application. The preferred way
+  * to deal with credentials would be to directly pass them to the actor when
+  * they become available and to request futures for the required credentials
+  * from the actor. When these futures complete, the required archives can
+  * start.
+  *
+  * But, since this workflow does not really fit to the current application
+  * design, the integration is done differently: The application manages the
+  * required credentials itself. When they are available, during the archive
+  * startup operation, they are passed to the credentials manager actor, so
+  * that they can be obtained from the factory for the cloud file downloader.
+  *
+  * @param credentialsManagerActor the actor managing credentials
+  */
+class CredentialsProvider(credentialsManagerActor: ActorRef[Credentials.CredentialData]):
+  /**
+    * Passes the credentials for a specific realm to the credentials manager
+    * actor referenced by this instance. The keys used for these credentials
+    * depend on the type of the realm.
+    *
+    * @param realm       the affected realm
+    * @param credentials the credentials for this realm
+    */
+  def passCredentials(realm: ArchiveRealm, credentials: UserCredentials): Unit =
+    realm match
+      case BasicAuthRealm(name) =>
+        passCredential(s"$name.username", Secret(credentials.userName))
+        passCredential(s"$name.password", credentials.password)
+
+      case OAuthRealm(name, _) =>
+        passCredential(name, credentials.password)
+
+  /**
+    * Passes the given combination of key and secret value for a credential to
+    * the associated credentials manager actor.
+    *
+    * @param key   the key of the credential
+    * @param value the secret value of the credential
+    */
+  private def passCredential(key: String, value: Secret): Unit =
+    credentialsManagerActor ! Credentials.CredentialData(key, value)
---
+++ mediaArchive/archiveHttpStartup/src/test/scala/de/oliver_heger/linedj/archivehttpstart/app/CredentialsProviderSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015-2025 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archivehttpstart.app
+
+import com.github.cloudfiles.core.http.Secret
+import de.oliver_heger.linedj.archive.cloud.auth.Credentials
+import de.oliver_heger.linedj.archivehttp.config.UserCredentials
+import org.apache.pekko.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import java.nio.file.Paths
+
+/**
+  * Test class for [[CredentialsProvider]].
+  */
+class CredentialsProviderSpec extends ScalaTestWithActorTestKit with AnyFlatSpecLike with Matchers:
+  "A CredentialsProvider" should "pass credentials for a BasicAuth realm" in :
+    val RealmName = "Test_Basic_Auth"
+    val credentials = UserCredentials("scott", Secret("tiger"))
+    val realm = BasicAuthRealm(RealmName)
+    val probeCredentialsActor = testKit.createTestProbe[Credentials.CredentialData]()
+
+    val provider = new CredentialsProvider(probeCredentialsActor.ref)
+    provider.passCredentials(realm, credentials)
+
+    val expectedCredentials = List(
+      (s"$RealmName.username", credentials.userName),
+      (s"$RealmName.password", credentials.password.secret)
+    )
+    val providedCredentials = List(
+      probeCredentialsActor.expectMessageType[Credentials.CredentialData],
+      probeCredentialsActor.expectMessageType[Credentials.CredentialData]
+    ).map(cd => cd.key -> cd.value.secret)
+    providedCredentials should contain theSameElementsAs expectedCredentials
+
+  it should "pass credentials for an OAuth realm" in :
+    val RealmName = "Test_OAuth"
+    val credentials = UserCredentials("usernameDoesNotMatter", Secret("idp-secret"))
+    val realm = OAuthRealm(RealmName, Paths.get("path", "does", "not", "matter"))
+    val probeCredentialsActor = testKit.createTestProbe[Credentials.CredentialData]()
+
+    val provider = new CredentialsProvider(probeCredentialsActor.ref)
+    provider.passCredentials(realm, credentials)
+
+    probeCredentialsActor.expectMessage(Credentials.CredentialData(RealmName, credentials.password))
