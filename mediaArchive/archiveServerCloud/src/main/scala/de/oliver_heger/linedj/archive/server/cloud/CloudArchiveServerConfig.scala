839c5b3c3add14ca0aa5d63711f1c7495bc7ef83
Oliver Heger <oliver.heger@oliver-heger.de>
2026-03-22 22:10:58 +0100

    archiveServerCloud: Implemented parsing of the configuration.
    Added a function to the companion object of CloudArchiveServerConfig
    which extracts a configuration instance from a configuration file. So,
    the external configuration format has been modeled.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- mediaArchive/archiveServerCloud/src/main/scala/de/oliver_heger/linedj/archive/server/cloud/CloudArchiveServerConfig.scala
+++ mediaArchive/archiveServerCloud/src/main/scala/de/oliver_heger/linedj/archive/server/cloud/CloudArchiveServerConfig.scala
@@ -16,11 +16,16 @@
 
 package de.oliver_heger.linedj.archive.server.cloud
 
-import de.oliver_heger.linedj.archive.cloud.ArchiveCryptConfig
-import de.oliver_heger.linedj.archive.cloud.auth.AuthMethod
+import de.oliver_heger.linedj.archive.cloud.{ArchiveCryptConfig, CloudArchiveConfig}
+import de.oliver_heger.linedj.archive.cloud.auth.{AuthMethod, BasicAuthMethod, OAuthMethod}
+import de.oliver_heger.linedj.shared.config.ConfigExtensions.toDuration
+import org.apache.commons.configuration2.Configuration
+import org.apache.commons.configuration2.ex.ConfigurationException
 import org.apache.pekko.util.Timeout
 
-import java.nio.file.Path
+import java.nio.file.{Path, Paths}
+import java.util.Locale
+import scala.util.Try
 
 /**
   * A data class storing the configuration properties of a single cloud archive
@@ -62,6 +67,270 @@ case class ArchiveConfig(archiveBaseUri: String,
                          optRequestQueueSize: Option[Int],
                          optTimeout: Option[Timeout],
                          optCryptConfig: Option[ArchiveCryptConfig])
+
+object CloudArchiveServerConfig:
+  /** The name of the section storing the relevant configuration properties. */
+  private val ConfigSection = "media.cloudArchives."
+
+  /**
+    * The name of the configuration property for the path of the directory in
+    * which credential files and OAuth configurations are expected.
+    */
+  private val PropCredDirectory = ConfigSection + "credentialsDirectory"
+
+  /**
+    * The name of the configuration property defining the root path of the
+    * local cache for archive metadata.
+    */
+  private val PropCacheDirectory = ConfigSection + "cacheDirectory"
+
+  /**
+    * The name of the configuration property for the list of cloud archives to
+    * be managed by this server application.
+    */
+  private val PropCloudArchives = ConfigSection + "cloudArchive"
+
+  /**
+    * The name of the configuration property defining the name of a cloud
+    * archive.
+    */
+  private val PropArchiveName = "name"
+
+  /**
+    * The name of the configuration property defining the base URI of a cloud
+    * archive.
+    */
+  private val PropArchiveUri = "baseUri"
+
+  /**
+    * The name of the configuration property to specify the type of the
+    * authentication method required to access this archive. This must be a
+    * supported type like "BASIC" or "OAUTH", ignoring case.
+    */
+  private val PropAuthMethod = "authMethod"
+
+  /**
+    * The name of the configuration property for the authentication realm.
+    * This property, together with ''PropAuthMethod'' is used to construct an
+    * [[AuthMethod]] object.
+    */
+  private val PropAuthRealm = "authRealm"
+
+  /**
+    * The name of the optional configuration property to set the path to the
+    * "table of contents" document for this archive.
+    */
+  private val PropContentPath = "contentPath"
+
+  /**
+    * The name of the optional configuration property to set the root path for
+    * the media data contained in this archive.
+    */
+  private val PropMediaPath = "mediaPath"
+
+  /**
+    * The name of the optional configuration property to the set path under
+    * which metadata files are located in this archive.
+    */
+  private val PropMetadataPath = "metadataPath"
+
+  /**
+    * The name of the optional configuration property defining the parallelism
+    * to use when downloading metadata from this archive. This roughly
+    * corresponds to the number of media for which metadata is downloaded
+    * concurrently.
+    */
+  private val PropParallelism = "parallelism"
+
+  /**
+    * The name of the optional configuration property defining a maximum size
+    * for metadata documents. If a document has a larger size, its download is
+    * aborted, and the medium is ignored.
+    */
+  private val PropMaxSize = "maxContentSize"
+
+  /**
+    * The name of the optional configuration property defining the size of the
+    * queue for HTTP requests sent to this archive.
+    */
+  private val PropQueueSize = "requestQueueSize"
+
+  /**
+    * The name of the optional configuration property defining a timeout when
+    * accessing this archive. The value can be a number with an optional unit.
+    */
+  private val PropTimeout = "timeout"
+
+  /**
+    * The name of the optional configuration property defining the size of the
+    * cache for encrypted file names. If this property is present, the archive
+    * is considered to be encrypted.
+    */
+  private val PropCryptCacheSize = "cryptCacheSize"
+
+  /**
+    * The name of the optional configuration property defining the size of
+    * chunks in which file names are decrypted. If this property is present,
+    * the archive is considered to be encrypted.
+    */
+  private val PropCryptChunkSize = "cryptChunkSize"
+
+  /**
+    * The name of the optional configuration property to indicate that this
+    * archive is encrypted. If it is set to '''true''', all other properties
+    * related to encryption are set to their default values unless they are
+    * defined explicitly. If it is set to '''false''', encryption-related
+    * properties are ignored.
+    */
+  private val PropEncrypted = "[@encrypted]"
+
+  /**
+    * A set of properties that are related to encryption. If one of these
+    * properties is present, the archive is considered to be encrypted.
+    */
+  private val EncryptionProperties = Set(PropCryptCacheSize, PropCryptChunkSize)
+
+  /**
+    * Parses a configuration for this server application and returns a
+    * corresponding [[CloudArchiveServerConfig]]. The operation fails if the
+    * configuration is invalid.
+    *
+    * @param config the configuration to parse
+    * @return a [[Try]] with the resulting server configuration
+    */
+  def parseConfig(config: Configuration): Try[CloudArchiveServerConfig] = Try:
+    CloudArchiveServerConfig(
+      credentialsDirectory = Paths.get(mandatoryProperty(config, PropCredDirectory)),
+      cacheDirectory = Paths.get(mandatoryProperty(config, PropCacheDirectory)),
+      archives = parseArchives(config)
+    )
+
+  /**
+    * Parses the definitions of the cloud archives to be managed from the given
+    * configuration. Throws an exception if invalid configuration options are
+    * detected.
+    *
+    * @param config the configuration to parse
+    * @return a [[List]] with the extracted archive configurations
+    */
+  private def parseArchives(config: Configuration): List[ArchiveConfig] =
+    val archives = config.getList(PropCloudArchives + "." + PropArchiveName)
+    (0 until archives.size()).map: index =>
+      parseArchive(config, s"$PropCloudArchives($index)")
+    .toList
+
+  /**
+    * Parses the properties of a specific cloud archive from the server 
+    * configuration.
+    *
+    * @param config the configuration
+    * @param key    the key prefix for the archive in question
+    * @return the extracted archive configuration
+    */
+  private def parseArchive(config: Configuration, key: String): ArchiveConfig =
+    val archiveConfig = config.subset(key)
+    ArchiveConfig(
+      archiveName = mandatoryProperty(archiveConfig, PropArchiveName),
+      archiveBaseUri = mandatoryProperty(archiveConfig, PropArchiveUri),
+      authMethod = parseAuthMethod(archiveConfig),
+      optContentPath = optionalStringProperty(archiveConfig, PropContentPath),
+      optMediaPath = optionalStringProperty(archiveConfig, PropMediaPath),
+      optMetadataPath = optionalStringProperty(archiveConfig, PropMetadataPath),
+      optParallelism = optionalIntProperty(archiveConfig, PropParallelism),
+      optMaxContentSize = optionalIntProperty(archiveConfig, PropMaxSize),
+      optRequestQueueSize = optionalIntProperty(archiveConfig, PropQueueSize),
+      optTimeout = optionalTimeoutProperty(archiveConfig, PropTimeout),
+      optCryptConfig = parseCryptConfig(archiveConfig)
+    )
+
+  /**
+    * Returns the configuration property with the given key or throws a
+    * meaningful exception if it is undefined.
+    *
+    * @param config the configuration
+    * @param key    the desired key
+    * @return the string value of this property
+    */
+  private def mandatoryProperty(config: Configuration, key: String): String =
+    optionalStringProperty(config, key)
+      .getOrElse(throw new ConfigurationException(s"Missing mandatory property '$key'."))
+
+  /**
+    * Returns an [[Option]] for the value of an optional configuration
+    * property.
+    *
+    * @param config the configuration
+    * @param key    the desired key
+    * @return an [[Option]] for the value of this property
+    */
+  private def optionalStringProperty(config: Configuration, key: String): Option[String] =
+    Option(config.getString(key))
+
+  /**
+    * Returns an [[Option]] for the value of an optional '''Int'''
+    * configuration property.
+    *
+    * @param config the configuration
+    * @param key    the desired key
+    * @return an [[Option]] for the '''Int''' value of this property
+    */
+  private def optionalIntProperty(config: Configuration, key: String): Option[Int] =
+    if config.containsKey(key) then
+      Some(config.getInt(key))
+    else
+      None
+
+  /**
+    * Returns an [[Option]] for the value of an optional configuration property
+    * of type [[Timeout]].
+    *
+    * @param config the configuration
+    * @param key    the desired key
+    * @return an [[Option]] for the [[Timeout]] value of this property
+    */
+  private def optionalTimeoutProperty(config: Configuration, key: String): Option[Timeout] =
+    optionalStringProperty(config, key) map : strValue =>
+      Timeout(strValue.toDuration.get)
+
+  /**
+    * Extracts the [[AuthMethod]] of an archive from the configuration. Throws
+    * an exception if the method is invalid.
+    *
+    * @param config the configuration for the current archive
+    * @return the [[AuthMethod]] for this archive
+    */
+  private def parseAuthMethod(config: Configuration): AuthMethod =
+    val methodType = mandatoryProperty(config, PropAuthMethod)
+    val realm = mandatoryProperty(config, PropAuthRealm)
+    methodType.toLowerCase(Locale.ROOT) match
+      case "basic" => BasicAuthMethod(realm)
+      case "oauth" => OAuthMethod(realm)
+      case _ => throw new ConfigurationException(s"Invalid value for '$PropAuthMethod': '$methodType'.")
+
+  /**
+    * Parses the configuration related to encryption for a specific cloud
+    * archive. Whether the archive is encrypted is determined by the
+    * ''encrypted'' flag. If this is missing, the archive is treated as
+    * encrypted if there is at least one encryption-related property.
+    *
+    * @param config the configuration for the current archive
+    * @return the optional [[ArchiveCryptConfig]] for this archive
+    */
+  private def parseCryptConfig(config: Configuration): Option[ArchiveCryptConfig] =
+    val encrypted = if config.containsKey(PropEncrypted) then
+      config.getBoolean(PropEncrypted)
+    else
+      EncryptionProperties.exists(config.containsKey)
+
+    if encrypted then
+      val cryptConfig = ArchiveCryptConfig(
+        cryptCacheSize = config.getInt(PropCryptCacheSize, CloudArchiveConfig.DefaultCryptConfig.cryptCacheSize),
+        cryptChunkSize = config.getInt(PropCryptChunkSize, CloudArchiveConfig.DefaultCryptConfig.cryptChunkSize)
+      )
+      Some(cryptConfig)
+    else
+      None
+end CloudArchiveServerConfig
 
 /**
   * A data class holding the specific configuration for the cloud archive
---
+++ mediaArchive/archiveServerCloud/src/test/scala/de/oliver_heger/linedj/archive/server/cloud/CloudArchiveServerConfigSpec.scala
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2015-2026 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.archive.server.cloud
+
+import de.oliver_heger.linedj.archive.cloud.{ArchiveCryptConfig, CloudArchiveConfig}
+import de.oliver_heger.linedj.archive.cloud.auth.{BasicAuthMethod, OAuthMethod}
+import de.oliver_heger.linedj.archive.server.cloud.CloudArchiveServerConfigSpec.loadTestConfig
+import org.apache.commons.configuration2.Configuration
+import org.apache.commons.configuration2.builder.fluent.Configurations
+import org.apache.commons.configuration2.ex.ConfigurationException
+import org.apache.pekko.util.Timeout
+import org.scalatest.Inspectors.forEvery
+import org.scalatest.TryValues
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import java.nio.file.Paths
+import scala.concurrent.duration.DurationInt
+
+object CloudArchiveServerConfigSpec:
+  /** The name of the test configuration file. */
+  private val TestConfigName = "cloud-archive-server-config.xml"
+
+  /** The helper object for loading configuration files. */
+  private val configs = new Configurations
+
+  /**
+    * Loads the test configuration file from the test resources.
+    *
+    * @return the loaded configuration
+    */
+  private def loadTestConfig(): Configuration = configs.xml(TestConfigName)
+end CloudArchiveServerConfigSpec
+
+/**
+  * Test class for [[CloudArchiveServerConfig]].
+  */
+class CloudArchiveServerConfigSpec extends AnyFlatSpec, Matchers, TryValues:
+  /**
+    * Parses the test configuration and checks whether this is successful.
+    *
+    * @return the extracted [[CloudArchiveServerConfig]]
+    */
+  private def parseTestConfig(): CloudArchiveServerConfig =
+    CloudArchiveServerConfig.parseConfig(loadTestConfig()).success.value
+
+  "parseConfig()" should "parse the mandatory path properties" in :
+    val config = parseTestConfig()
+
+    config.credentialsDirectory should be(Paths.get("/data/credentials"))
+    config.cacheDirectory should be(Paths.get("/tmp/cloud-archives/cache"))
+
+  it should "return a failure if the credential path is not set" in :
+    val config = loadTestConfig()
+    config.clearProperty("media.cloudArchives.credentialsDirectory")
+
+    val result = CloudArchiveServerConfig.parseConfig(config)
+
+    result.failure.exception shouldBe a[ConfigurationException]
+    result.failure.exception.getMessage should include("credentialsDirectory")
+
+  it should "return a failure if the cache path is not set" in :
+    val config = loadTestConfig()
+    config.clearProperty("media.cloudArchives.cacheDirectory")
+
+    val result = CloudArchiveServerConfig.parseConfig(config)
+
+    result.failure.exception shouldBe a[ConfigurationException]
+    result.failure.exception.getMessage should include("cacheDirectory")
+
+  it should "parse the mandatory properties of cloud archives" in :
+    val config = parseTestConfig()
+
+    config.archives should have size 4
+
+    val rockArchive = config.archives.head
+    import rockArchive.*
+    archiveBaseUri should be("https://music.example.com/rock")
+    archiveName should be("Rock in the cloud")
+    authMethod should be(BasicAuthMethod("rock-realm"))
+
+  it should "handle undefined optional properties" in :
+    val config = parseTestConfig()
+
+    val defaultArchive = config.archives(1)
+    import defaultArchive.*
+    optContentPath shouldBe empty
+    optCryptConfig shouldBe empty
+    optMaxContentSize shouldBe empty
+    optMediaPath shouldBe empty
+    optMetadataPath shouldBe empty
+    optParallelism shouldBe empty
+    optRequestQueueSize shouldBe empty
+    optTimeout shouldBe empty
+
+  it should "handle defined optional properties" in :
+    val config = parseTestConfig()
+
+    val rockArchive = config.archives.head
+    import rockArchive.*
+    optContentPath should be(Some("meta/toc.json"))
+    optMaxContentSize should be(Some(128000))
+    optMediaPath should be(Some("meta/media"))
+    optMetadataPath should be(Some("meta/mdt"))
+    optParallelism should be(Some(3))
+    optRequestQueueSize should be(Some(19))
+    optTimeout should be(Some(Timeout(2.minutes)))
+
+  it should "parse auth methods correctly" in :
+    val config = parseTestConfig()
+
+    config.archives(1).authMethod should be(OAuthMethod("default-realm"))
+    config.archives(2).authMethod should be(BasicAuthMethod("test"))
+
+  it should "return a failure if a mandatory property of an archive is missing" in :
+    val properties = List("name", "baseUri", "authMethod", "authRealm")
+    forEvery(properties): property =>
+      val config = loadTestConfig()
+      config.clearProperty("media.cloudArchives.cloudArchive(1)." + property)
+
+      val result = CloudArchiveServerConfig.parseConfig(config)
+
+      result.failure.exception shouldBe a[ConfigurationException]
+      result.failure.exception.getMessage should include(property)
+
+  it should "return a failure for an invalid auth method" in :
+    val unsupportedAuthMethod = "unsupportedAuthMethod"
+    val config = loadTestConfig()
+    config.setProperty("media.cloudArchives.cloudArchive(0).authMethod", unsupportedAuthMethod)
+
+    val result = CloudArchiveServerConfig.parseConfig(config)
+
+    result.failure.exception shouldBe a[ConfigurationException]
+    result.failure.exception.getMessage should include(unsupportedAuthMethod)
+
+  it should "provide a crypt config if the corresponding options are set" in :
+    val config = parseTestConfig()
+
+    config.archives.head.optCryptConfig should be(Some(ArchiveCryptConfig(cryptCacheSize = 111, cryptChunkSize = 127)))
+
+  it should "provide a crypt config with default options if the encrypted flag is set" in :
+    val config = parseTestConfig()
+
+    config.archives(2).optCryptConfig should be(Some(CloudArchiveConfig.DefaultCryptConfig))
+
+  it should "ignore crypt settings if the encrypted flag is false" in :
+    val config = parseTestConfig()
+
+    config.archives(3).optCryptConfig shouldBe empty
