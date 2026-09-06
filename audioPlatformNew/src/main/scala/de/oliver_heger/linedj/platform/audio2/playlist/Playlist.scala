5e35c9b50fdc0d19d127df832513730455f9c41d
Oliver Heger <oliver.heger@oliver-heger.de>
2026-09-06 11:53:32 +0200

    audioPlatformNew: Added `Playlist` class.
    This class represents a playlist. It has been copied from the
    `audioPlatform` project with minor modifications. The playlist
    currently contains plain strings as references to media files. This is
    going to be changed later to a dedicated type for media file IDs.
    However, this requires a larger refactoring, since the type needs to
    be defined in the model of the archive.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ audioPlatformNew/src/main/scala/de/oliver_heger/linedj/platform/audio2/playlist/Playlist.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015-2026 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio2.playlist
+
+/**
+  * A class representing a playlist.
+  *
+  * A playlist basically consists of two lists: the list of songs (or their
+  * IDs) that are to be played, and the list of songs that have already been
+  * played. With this model it is easily possible to determine the current 
+  * position in the playlist and to move forwards and backwards.
+  *
+  * The _pendingSongs_ list contains the songs in the order they will be
+  * played. The head of the list is the current song. The _playedSongs_ list
+  * has reverse order; its head is the song that was played before the current
+  * one.
+  *
+  * @param pendingSongs the list of songs that are to be played
+  * @param playedSongs  the list of songs that have been played
+  */
+case class Playlist(pendingSongs: List[String], playedSongs: List[String])
