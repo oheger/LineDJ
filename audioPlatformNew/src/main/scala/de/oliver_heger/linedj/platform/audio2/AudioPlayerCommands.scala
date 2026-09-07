18ce19593c8be195ce1975782a4f0c781b54147f
Oliver Heger <oliver.heger@oliver-heger.de>
2026-09-07 19:30:36 +0200

    audioPlatformNew: Extended `AudioPlayerCommands` enum.
    Added further commands to be supported by the audio player engine. The
    final set of commands correspond to the commands defined in the
    `audioPlatform` module.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- audioPlatformNew/src/main/scala/de/oliver_heger/linedj/platform/audio2/AudioPlayerCommands.scala
+++ audioPlatformNew/src/main/scala/de/oliver_heger/linedj/platform/audio2/AudioPlayerCommands.scala
@@ -16,6 +16,10 @@
 
 package de.oliver_heger.linedj.platform.audio2
 
+import de.oliver_heger.linedj.platform.audio2.playlist.Playlist
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
 /**
   * An enumeration defining the commands supported by the audio player engine
   * provided by the audio platform.
@@ -49,3 +53,51 @@ enum AudioPlayerCommands:
   case AppendPlaylist(songIDs: Iterable[String],
                       closePlaylist: Boolean = false,
                       activate: Boolean = true)
+
+  /**
+    * A command which sets the playlist of the audio player.
+    *
+    * A currently existing playlist is replaced by the object defined in this
+    * message. If necessary (i.e., if a current playlist exists), a reset of
+    * the audio player is triggered. Then the pending songs are passed to the
+    * audio player. Optionally, the playlist is closed.
+    *
+    * For the current song in the playlist offsets for the playback position and
+    * time can be specified. This is useful if playback has been aborted and
+    * should now be continued at the very same position.
+    *
+    * @param playlist       the new ''Playlist''
+    * @param closePlaylist  flag whether the playlist is to be closed
+    * @param positionOffset the offset in the audio stream where to start
+    *                       playback
+    *
+    * @param timeOffset     the time offset where to start playback
+    */
+  case SetPlaylist(playlist: Playlist,
+                   closePlaylist: Boolean = true,
+                   positionOffset: Long = 0,
+                   timeOffset: FiniteDuration = 0.seconds)
+
+  /**
+    * A command to start playback of audio files.
+    *
+    * If playback is currently not active, it is started now. An update event
+    * with the modified state is generated.
+    */
+  case StartAudioPlayback
+
+  /**
+    * A command to stop playback of audio files.
+    *
+    * If playback is currently active, it is stopped now. An update event with
+    * the modified state is generated.
+    */
+  case StopAudioPlayback
+
+  /**
+    * A command to skip the current audio source.
+    *
+    * If playback is currently active, the current audio file is skipped, and
+    * playback starts with the next song in the playlist (if any).
+    */
+  case SkipCurrentSource
