8b3004037e7f02a54c546311b34b648d564c05dc
Oliver Heger <oliver.heger@oliver-heger.de>
2026-09-07 19:28:41 +0200

    audioPlatformNew: Added `AudioPlayerState` class.
    This is a data class representing the current state of the audio
    player managed by the platform. The class has been copied from the
    `audioPlatform` module with minor modifications.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ audioPlatformNew/src/main/scala/de/oliver_heger/linedj/platform/audio2/AudioPlayerState.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015-2026 The Developers Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.oliver_heger.linedj.platform.audio2
+
+import de.oliver_heger.linedj.platform.audio2.playlist.Playlist
+import de.oliver_heger.linedj.platform.audio2.playlist.PlaylistService
+
+object AudioPlayerState:
+  /**
+    * An initial audio player state that can be assumed at application startup.
+    * No playlist has been set, and all flags are set to initial values.
+    */
+  final val Initial = AudioPlayerState(
+    playlist = Playlist(Nil, Nil),
+    playlistSeqNo = PlaylistService.SeqNoInitial,
+    playbackActive = false,
+    playlistClosed = false,
+    playlistActivated = false
+  )
+end AudioPlayerState
+
+/**
+  * A class representing the current playback state of the audio player
+  * managed by the audio platform.
+  *
+  * An instance contains information about the songs that have already been
+  * played or are about to be played in form of a [[Playlist]]. It also holds
+  * several flags about the state of the audio playback and the playlist.
+  *
+  * @param playlist          the current [[Playlist]]
+  * @param playlistSeqNo     the sequence number of the playlist
+  * @param playbackActive    flag whether playback is currently active
+  * @param playlistClosed    flag whether the playlist has already been closed
+  * @param playlistActivated flag whether the playlist was passed to the player
+  */
+final case class AudioPlayerState(playlist: Playlist,
+                                  playlistSeqNo: Int,
+                                  playbackActive: Boolean,
+                                  playlistClosed: Boolean,
+                                  playlistActivated: Boolean)
